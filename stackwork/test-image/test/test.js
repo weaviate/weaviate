3209fb628c1d9d03421ea4c0bd5482ab87e2dd14
Quinten Krijger <qkrijger@gmail.com>
2016-04-17 12:43:31 +0200

    Fixed lint errors - "test code = production code"
    Signed-off-by: Quinten Krijger <qkrijger@gmail.com>

--- stackwork/test-image/test/test.js
+++ stackwork/test-image/test/test.js
@@ -1,13 +1,14 @@
+'use strict';
 var chakram = require('chakram');
 var expect = chakram.expect;
 
-describe("Weaviate", function () {
-  it("does not serve anything on it's root URL path (i.e. 404)", function () {
-    var response = chakram.get("http://service");
+describe('Weaviate', function () {
+  it('does not serve anything on its root URL path (i.e. 404)', function () {
+    var response = chakram.get('http://service');
     expect(response).to.have.status(404);
-    expect(response).to.have.header("content-type", "text/html; charset=utf-8");
+    expect(response).to.have.header('content-type', 'text/html; charset=utf-8');
     // expect(response).to.comprise.of.json({
-    //   args: { test: "chakram" }
+    //   args: { test: 'chakram' }
     // });
     return chakram.wait();
   });
