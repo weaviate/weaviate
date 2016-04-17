3025e7f56a7150ec29b11a025d623b00f7a7caab
Quinten Krijger <qkrijger@gmail.com>
2016-04-17 13:14:29 +0200

    Added copyright and using const for imports
    Signed-off-by: Quinten Krijger <qkrijger@gmail.com>

--- stackwork/test-image/test/test.js
+++ stackwork/test-image/test/test.js
@@ -1,6 +1,19 @@
 'use strict';
-var chakram = require('chakram');
-var expect = chakram.expect;
+/**                         _       _
+ *                         (_)     | |
+ *__      _____  __ ___   ___  __ _| |_ ___
+ *\ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+ * \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+ *  \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+ *
+ * Copyright © 2016 Weaviate. All rights reserved.
+ * See www.weaviate.com for details
+ * See package.json for author and maintainer info
+ * Contact: @weaviate_iot / yourfriends@weaviate.com
+ */
+
+const chakram = require('chakram');
+const expect = chakram.expect;
 
 describe('Weaviate', function () {
   it('does not serve anything on its root URL path (i.e. 404)', function () {
