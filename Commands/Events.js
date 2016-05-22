f13165ff068b297167851c8e5e896a9980ca8805
Bob van Luijt <me@bob.technology>
2016-05-22 12:40:39 -0700

    Add events

--- Commands/Events.js
+++ Commands/Events.js
@@ -28,4 +28,59 @@ module.exports = class Commands_Events { // Class: Commands_{resources.className
         this.next = next;
     }
 
+    /**
+     * Deletes all events associated with a particular device. Leaves an event to indicate deletion happened.
+     * @param {object} commandAttributes  - All attributes needed to exec the command
+     * @return {promise} Returns a promise with the correct object
+     */
+    getDeleteAll(commandAttributes) {
+        return new Promise((resolve, reject) => {
+            // resolve with kind and token
+            resolve({});
+        })
+    }
+
+    /**
+     * Lists events.
+     * @param {object} commandAttributes  - All attributes needed to exec the command
+     * @return {promise} Returns a promise with the correct object
+     */
+    getList(commandAttributes) {
+        return new Promise((resolve, reject) => {
+            // resolve with kind and token
+            resolve({
+              kind: 'weave#eventsListResponse',
+              events: [
+                {
+                commandPatch: {
+                        commandId: 'qwerty-12345',
+                        state: 'state'
+                },
+                connectionStatus: 'connection status',
+                deviceId: 'qwerty-12345',
+                id: '12345-qwerty',
+                kind: 'weave#event',
+                statePatch: {},
+                timeMs: 12345,
+                type: 'commandCreated',
+                userEmail: 'bob@weaviate.com'
+            }
+              ],
+              nextPageToken: '12345',
+              totalResults: 1
+            });
+        })
+    }
+
+    /**
+     * Enables or disables recording of a particular device's events based on a boolean parameter. Enabled by default.
+     * @param {object} commandAttributes  - All attributes needed to exec the command
+     * @return {promise} Returns a promise with the correct object
+     */
+    getRecordDeviceEvents(commandAttributes) {
+        return new Promise((resolve, reject) => {
+            // resolve with kind and token
+            resolve({})
+    }
+    
 };
