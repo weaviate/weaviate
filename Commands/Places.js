d1337ef0eb1154a0ef40eca94b76bf0694af7742
Bob van Luijt <me@bob.technology>
2016-05-21 18:39:38 -0700

    Create classes for commands

---
+++ Commands/Places.js
@@ -0,0 +1,31 @@
+'use strict';
+/*                          _       _
+ *__      _____  __ ___   ___  __ _| |_ ___
+ *\ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+ * \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+ *  \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+ *
+ * Copyright © 2016 Weaviate. All rights reserved.
+ * LICENSE: https://github.com/weaviate/weaviate/blob/master/LICENSE
+ * AUTHOR: Bob van Luijt (bob@weaviate.com)
+ * See www.weaviate.com for details
+ * See package.json for author and maintainer info
+ * Contact: @weaviate_iot / yourfriends@weaviate.com
+ */
+
+/** Class Commands_AclEntries */
+module.exports = class Commands_Places { // Class: Commands_{resources.className}
+
+    /**
+     * Constructor for this Command
+     * @param {object} req  - The request
+     * @param {object} res  - The response
+     * @param {object} next - Next() function
+     */
+    constructor(req, res, next) {
+        this.req  = req;
+        this.res  = res;
+        this.next = next;
+    }
+
+};
