e3c173567d6c359acbb3ee149f5950e701dae7e6
Bob van Luijt <me@bob.technology>
2016-05-21 18:56:31 -0700

    Add AuthorizedApps

--- weaviate.js
+++ weaviate.js
@@ -430,6 +430,108 @@ module.exports = (i) => {
                 })
     });
 
+    /*****************************
+     * AuthorizedApps COMMANDS
+     */
+
+    /**
+     * Id: weave.authorizedApps.createAppAuthenticationToken
+     * Generate a token used to authenticate an authorized app.
+     */
+    SERVER.post('/authorizedApps/createAppAuthenticationToken', (req, res, next) => {
+        return new Commands_AuthorizedApps(req, res, next)
+                .getCreateAppAuthenticationToken({
+                    requiredParams: [],
+                    requestObjectName: null,
+                    authScopes: ['/auth/weave.app']
+                })
+                .then(result => {
+                    // send the result
+                    res.send(result);
+                    // exec the onSuccess
+                    if(i.onSuccess !== undefined && typeof i.onSuccess === 'function'){
+                        i.onSuccess({
+                            params:         req.params,
+                            body:           req.body,
+                            response:       result,
+                            requestHeaders: req.headers
+                        })
+                    }
+                    // exec the debug
+                    if(i.debug === true){
+                        console.log(req.connection.remoteAddress, 'SUCCESS', 'weave.authorizedApps.createAppAuthenticationToken');
+                    }
+                    return next();
+                })
+                .catch(error => {
+                    res.send(
+                        new Helpers_ErrorHandling.createErrorMessage(error)
+                    );
+                    // exec the onError
+                    if(i.onError !== undefined && typeof i.onSuccess === 'function'){
+                        i.onError({
+                            params:         req.params,
+                            body:           req.body,
+                            requestHeaders: req.headers
+                        })
+                    }
+                    // exec the debug
+                    if(i.debug === true){
+                        console.log(req.connection.remoteAddress, 'ERROR', 'weave.authorizedApps.createAppAuthenticationToken');
+                    }
+                    return next();
+                })
+    });
+
+    /**
+     * Id: weave.authorizedApps.list
+     * The actual list of authorized apps.
+     */
+    SERVER.get('/authorizedApps', (req, res, next) => {
+        return new Commands_AuthorizedApps(req, res, next)
+                .getList({
+                    requiredParams: [],
+                    requestObjectName: null,
+                    authScopes: ['/auth/weave.app']
+                })
+                .then(result => {
+                    // send the result
+                    res.send(result);
+                    // exec the onSuccess
+                    if(i.onSuccess !== undefined && typeof i.onSuccess === 'function'){
+                        i.onSuccess({
+                            params:         req.params,
+                            body:           req.body,
+                            response:       result,
+                            requestHeaders: req.headers
+                        })
+                    }
+                    // exec the debug
+                    if(i.debug === true){
+                        console.log(req.connection.remoteAddress, 'SUCCESS', 'weave.authorizedApps.list');
+                    }
+                    return next();
+                })
+                .catch(error => {
+                    res.send(
+                        new Helpers_ErrorHandling.createErrorMessage(error)
+                    );
+                    // exec the onError
+                    if(i.onError !== undefined && typeof i.onSuccess === 'function'){
+                        i.onError({
+                            params:         req.params,
+                            body:           req.body,
+                            requestHeaders: req.headers
+                        })
+                    }
+                    // exec the debug
+                    if(i.debug === true){
+                        console.log(req.connection.remoteAddress, 'ERROR', 'weave.authorizedApps.list');
+                    }
+                    return next();
+                })
+    });
+
     /*
      * START THE SERVER
      */
