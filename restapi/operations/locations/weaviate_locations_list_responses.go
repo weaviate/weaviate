081090e38f87361b95a4f9af8728dd44095d316e
Bob van Luijt <bob@kubrickolo.gy>
2017-05-02 21:33:58 +0200

    gh-53: Update to Swagger v0.1.7. Closes #53

---
+++ restapi/operations/locations/weaviate_locations_list_responses.go
@@ -0,0 +1,109 @@
+/*                          _       _
+ *__      _____  __ ___   ___  __ _| |_ ___
+ *\ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+ * \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+ *  \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+ *
+ * Copyright © 2016 Weaviate. All rights reserved.
+ * LICENSE: https://github.com/weaviate/weaviate/blob/master/LICENSE
+ * AUTHOR: Bob van Luijt (bob@weaviate.com)
+ * See www.weaviate.com for details
+ * Contact: @weaviate_iot / yourfriends@weaviate.com
+ */
+ package locations
+
+
+
+
+import (
+	"net/http"
+
+	"github.com/go-openapi/runtime"
+
+	"github.com/weaviate/weaviate/models"
+)
+
+// WeaviateLocationsListOKCode is the HTTP code returned for type WeaviateLocationsListOK
+const WeaviateLocationsListOKCode int = 200
+
+/*WeaviateLocationsListOK Successful response.
+
+swagger:response weaviateLocationsListOK
+*/
+type WeaviateLocationsListOK struct {
+
+	/*
+	  In: Body
+	*/
+	Payload *models.LocationsListResponse `json:"body,omitempty"`
+}
+
+// NewWeaviateLocationsListOK creates WeaviateLocationsListOK with default headers values
+func NewWeaviateLocationsListOK() *WeaviateLocationsListOK {
+	return &WeaviateLocationsListOK{}
+}
+
+// WithPayload adds the payload to the weaviate locations list o k response
+func (o *WeaviateLocationsListOK) WithPayload(payload *models.LocationsListResponse) *WeaviateLocationsListOK {
+	o.Payload = payload
+	return o
+}
+
+// SetPayload sets the payload to the weaviate locations list o k response
+func (o *WeaviateLocationsListOK) SetPayload(payload *models.LocationsListResponse) {
+	o.Payload = payload
+}
+
+// WriteResponse to the client
+func (o *WeaviateLocationsListOK) WriteResponse(rw http.ResponseWriter, producer runtime.Producer) {
+
+	rw.WriteHeader(200)
+	if o.Payload != nil {
+		payload := o.Payload
+		if err := producer.Produce(rw, payload); err != nil {
+			panic(err) // let the recovery middleware deal with this
+		}
+	}
+}
+
+// WeaviateLocationsListNoContentCode is the HTTP code returned for type WeaviateLocationsListNoContent
+const WeaviateLocationsListNoContentCode int = 204
+
+/*WeaviateLocationsListNoContent Successful query result but no content
+
+swagger:response weaviateLocationsListNoContent
+*/
+type WeaviateLocationsListNoContent struct {
+}
+
+// NewWeaviateLocationsListNoContent creates WeaviateLocationsListNoContent with default headers values
+func NewWeaviateLocationsListNoContent() *WeaviateLocationsListNoContent {
+	return &WeaviateLocationsListNoContent{}
+}
+
+// WriteResponse to the client
+func (o *WeaviateLocationsListNoContent) WriteResponse(rw http.ResponseWriter, producer runtime.Producer) {
+
+	rw.WriteHeader(204)
+}
+
+// WeaviateLocationsListNotImplementedCode is the HTTP code returned for type WeaviateLocationsListNotImplemented
+const WeaviateLocationsListNotImplementedCode int = 501
+
+/*WeaviateLocationsListNotImplemented Not (yet) implemented.
+
+swagger:response weaviateLocationsListNotImplemented
+*/
+type WeaviateLocationsListNotImplemented struct {
+}
+
+// NewWeaviateLocationsListNotImplemented creates WeaviateLocationsListNotImplemented with default headers values
+func NewWeaviateLocationsListNotImplemented() *WeaviateLocationsListNotImplemented {
+	return &WeaviateLocationsListNotImplemented{}
+}
+
+// WriteResponse to the client
+func (o *WeaviateLocationsListNotImplemented) WriteResponse(rw http.ResponseWriter, producer runtime.Producer) {
+
+	rw.WriteHeader(501)
+}
---
+++ restapi/operations/locations/weaviate_locations_patch_responses.go
@@ -0,0 +1,88 @@
+/*                          _       _
+ *__      _____  __ ___   ___  __ _| |_ ___
+ *\ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+ * \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+ *  \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+ *
+ * Copyright © 2016 Weaviate. All rights reserved.
+ * LICENSE: https://github.com/weaviate/weaviate/blob/master/LICENSE
+ * AUTHOR: Bob van Luijt (bob@weaviate.com)
+ * See www.weaviate.com for details
+ * Contact: @weaviate_iot / yourfriends@weaviate.com
+ */
+ package locations
+
+
+
+
+import (
+	"net/http"
+
+	"github.com/go-openapi/runtime"
+
+	"github.com/weaviate/weaviate/models"
+)
+
+// WeaviateLocationsPatchOKCode is the HTTP code returned for type WeaviateLocationsPatchOK
+const WeaviateLocationsPatchOKCode int = 200
+
+/*WeaviateLocationsPatchOK Successful updated.
+
+swagger:response weaviateLocationsPatchOK
+*/
+type WeaviateLocationsPatchOK struct {
+
+	/*
+	  In: Body
+	*/
+	Payload *models.Location `json:"body,omitempty"`
+}
+
+// NewWeaviateLocationsPatchOK creates WeaviateLocationsPatchOK with default headers values
+func NewWeaviateLocationsPatchOK() *WeaviateLocationsPatchOK {
+	return &WeaviateLocationsPatchOK{}
+}
+
+// WithPayload adds the payload to the weaviate locations patch o k response
+func (o *WeaviateLocationsPatchOK) WithPayload(payload *models.Location) *WeaviateLocationsPatchOK {
+	o.Payload = payload
+	return o
+}
+
+// SetPayload sets the payload to the weaviate locations patch o k response
+func (o *WeaviateLocationsPatchOK) SetPayload(payload *models.Location) {
+	o.Payload = payload
+}
+
+// WriteResponse to the client
+func (o *WeaviateLocationsPatchOK) WriteResponse(rw http.ResponseWriter, producer runtime.Producer) {
+
+	rw.WriteHeader(200)
+	if o.Payload != nil {
+		payload := o.Payload
+		if err := producer.Produce(rw, payload); err != nil {
+			panic(err) // let the recovery middleware deal with this
+		}
+	}
+}
+
+// WeaviateLocationsPatchNotImplementedCode is the HTTP code returned for type WeaviateLocationsPatchNotImplemented
+const WeaviateLocationsPatchNotImplementedCode int = 501
+
+/*WeaviateLocationsPatchNotImplemented Not (yet) implemented.
+
+swagger:response weaviateLocationsPatchNotImplemented
+*/
+type WeaviateLocationsPatchNotImplemented struct {
+}
+
+// NewWeaviateLocationsPatchNotImplemented creates WeaviateLocationsPatchNotImplemented with default headers values
+func NewWeaviateLocationsPatchNotImplemented() *WeaviateLocationsPatchNotImplemented {
+	return &WeaviateLocationsPatchNotImplemented{}
+}
+
+// WriteResponse to the client
+func (o *WeaviateLocationsPatchNotImplemented) WriteResponse(rw http.ResponseWriter, producer runtime.Producer) {
+
+	rw.WriteHeader(501)
+}
