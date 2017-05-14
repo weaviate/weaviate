18fedaf73fdee1bb3de21acb5151ab5b37919df2
Bob van Luijt <bob@kubrickolo.gy>
2017-05-14 19:06:59 +0200

    gh-61: Add authentication keys

---
+++ models/key_children.go
@@ -0,0 +1,40 @@
+/*                          _       _
+ *__      _____  __ ___   ___  __ _| |_ ___
+ *\ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+ * \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+ *  \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+ *
+ * Copyright © 2016 Weaviate. All rights reserved.
+ * LICENSE: https://github.com/weaviate/weaviate/blob/master/LICENSE
+ * AUTHOR: Bob van Luijt (bob@weaviate.com)
+ * See www.weaviate.com for details
+ * Contact: @weaviate_iot / yourfriends@weaviate.com
+ */
+ package models
+
+
+
+
+import (
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/go-openapi/errors"
+)
+
+// KeyChildren key children
+// swagger:model KeyChildren
+type KeyChildren struct {
+
+	// Childeren one step deep.
+	Childeren []interface{} `json:"childeren"`
+}
+
+// Validate validates this key children
+func (m *KeyChildren) Validate(formats strfmt.Registry) error {
+	var res []error
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
+	return nil
+}
