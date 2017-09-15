fd9479e1ef1ad02d8db6a5896089b15646d040d9
Bob van Luijt <bob@kubrickolo.gy>
2017-09-15 12:31:51 +0200

    gh-174: Clean with gofmt

--- models/json_object.go
+++ models/json_object.go
@@ -10,12 +10,8 @@
  * See www.weaviate.com for details
  * Contact: @weaviate_iot / yourfriends@weaviate.com
  */
-   
 
 package models
-
- 
- 
 
 // JSONObject JSON object value.
 // swagger:model JsonObject
