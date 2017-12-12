1fb17acd622b7930e7856a6964286c3a609cd5d8
Erdal Yildiz <erdal.yildiz@kpn.com>
2017-12-12 16:40:39 +0100

    gh-280 rootkey, and gh-283 update binary dist url

--- docker/run-services.sh
+++ docker/run-services.sh
@@ -6,73 +6,121 @@
 #  \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
 #
 # Copyright © 2016 Weaviate. All rights reserved.
-# LICENSE: https://github.com/creativesoftwarefdn/weaviate/blob/develop/LICENSE.md
+# LICENSE: https://github.com/weaviate/weaviate/blob/develop/LICENSE.md
 # AUTHOR: Bob van Luijt (bob@weaviate.com)
 # See www.weaviate.com for details
-# Contact: @CreativeSofwFdn / yourfriends@weaviate.com
+# Contact: @weaviate_iot / yourfriends@weaviate.com
 #
+
+# uncomment net line for debugging info
+# set -x
 
 # Welkom message
 echo "Building weaviate with Docker..."
 
 # check OS
-if [[ "$OSTYPE" == "win32" ]]; then
+if [ "$OSTYPE" == "win32" ]; then
     echo "SORRY, NO WINDOWS SUPPORT"
     exit 1
 fi
 
-# install stuff
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    # check if jq is installed
-    jq --help >/dev/null 2>&1 || {
-        sudo apt-get -qq update
-        sudo apt-get -qq  -y install jq
-    }
+# check if jq is installed
+jq --help >/dev/null 2>&1 || {
+	echo we need jq, please run these commands, and try again 
+    echo sudo apt-get -qq update
+    echo sudo apt-get -qq install jq
+}
 
-    # check if docker is installed
-    sudo docker ps >/dev/null 2>&1 || {
-        sudo apt-get -qq update
-        sudo apt-get -qq  -y install \
-            apt-transport-https \
-            ca-certificates \
-            curl \
-            jq \
-            software-properties-common
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
-        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-        sudo apt-get -qq  update
-        sudo apt-get -qq  -y install docker-ce
-    }
-fi
+# check if docker is installed
+sudo docker ps >/dev/null 2>&1 || {
+    echo we need docker, please run these commands, and try again
+    echo sudo apt-get -qq update
+    echo sudo apt-get -qq install \
+    echo      apt-transport-https \
+    echo      ca-certificates \
+    echo      curl \
+    echo      jq \
+    echo      software-properties-common
+    echo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
+    echo sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    echo sudo apt-get -qq update
+    echo sudo apt-get -qq install docker-ce
+    echo sudo groupadd docker
+    echo sudo gpasswd -a $USER docker
+    echo docker run hello-world
+}
 
-# get all running docker container names
-containers=$(sudo docker ps | awk '{if(NR>1) print $NF}')
 host=$(hostname)
 
-# loop through all containers and stop weaviate and dgraph ones
-for container in $containers
-do
-    if [[ $container == *"dgraph"* ]] ||  [[ $container == *"weaviate"* ]]; then
-        echo "STOPPING: $container"
-        sudo docker kill $container &>/dev/null
-    fi
-done
+# kill dgraph and weaviate
+docker kill weaviate_dgraph &>/dev/null || true
+docker kill weaviate &>/dev/null || true
 
 # remove dgraph and weaviate
-sudo docker rm dgraph &>/dev/null || true
-sudo docker rm weaviate &>/dev/null || true
+docker rm weaviate_dgraph &>/dev/null || true
+docker rm weaviate &>/dev/null || true
 
-# build and start dgraph docker
-mkdir -p ~/dgraph
-DGRAPHID=$(sudo docker run -itd -p 8080:8080 -p 9080:9080 -v ~/dgraph:/dgraph dgraph/dgraph:v0.8.1 dgraph --bindall=true --memory_mb=2048)
-DGRAPHIP=$(sudo docker inspect $DGRAPHID | jq -r '.[0].NetworkSettings.IPAddress')
+# create dgraph, and weaviate  data directories
+mkdir -p ~/tryweaviate/dgraph
+
+#clean up at second run, needs sudo...
+# sudo rm ~/tryweaviate/dgraph/*
+
+mkdir -p ~/tryweaviate/weaviate/config
+cd ~/tryweaviate/
+
+# start dgraph docker
+docker run --name weaviate_dgraph -itd -p 8080:8080 -p 9080:9080 -v ~/tryweaviate/dgraph:/dgraph dgraph/dgraph:v0.8.1 dgraph --bindall=true --memory_mb=2048 &>/dev/null || true
+DGRAPHIP=$(docker inspect weaviate_dgraph | jq -r '.[0].NetworkSettings.IPAddress')
 
 # build and start weaviate docker
 echo "BUILDING WITH DGRAPH IP: $DGRAPHIP"
-sudo docker build --quiet --build-arg DGRAPHIP=$DGRAPHIP -t weaviate "https://raw.githubusercontent.com/weaviate/weaviate/develop/docker/Dockerfile?i=$(echo $((1 + RANDOM % 999999)))"
-WEAVIATEID=$(sudo docker run -d weaviate)
-WEAVIATEIP=$(sudo docker inspect $WEAVIATEID | jq -r '.[0].NetworkSettings.IPAddress')
+
+# when the install oneliner, and you need the online Dockerfile
+if [ -f "Dockerfile" ]
+then
+	# Dockerfile found.
+	echo . 
+else
+    # Dockerfile not found, download it"
+    wget -q -O Dockerfile https://raw.githubusercontent.com/weaviate/weaviate/develop/docker/Dockerfile?i=$(echo $((1 + RANDOM % 999999)))	
+fi
+
+#build locally
+#docker build --quiet --build-arg DGRAPHIP=$DGRAPHIP -t weaviate .
+#Dockerfile should be present in current directory 
+docker build --quiet -t weaviate .  &>/dev/null || true
+
+# get the config file 
+wget -q -O weaviate/config/example_weaviate.conf.json https://raw.githubusercontent.com/weaviate/weaviate/develop/weaviate.conf.json
+
+# replace the DGRAPH IP
+echo "UPDATE config WITH dgraph IP" ${DGRAPHIP} && \
+JSONRESULT=$(jq ".environments[] | select(.name | contains(\"docker\")) | .database.database_config.host = \"${DGRAPHIP}\"" weaviate/config/example_weaviate.conf.json) && \
+echo "{ \"environments\": [ $JSONRESULT ] }"  > weaviate/config/weaviate.conf.json
+
+
+
+docker run --name weaviate -v ~/tryweaviate/weaviate/config:/var/weaviate/config  -p 8070:80 -d weaviate &>/dev/null || true 
+WEAVIATEIP=$(sudo docker inspect weaviate | jq -r '.[0].NetworkSettings.IPAddress')
+
+ROOTKEY=NO_ROOT_KEY_YET
+echo "it takes a while to start dgraph and weaviate"
+
+while [ "$ROOTKEY" == "NO_ROOT_KEY_YET" ]
+do  
+	echo "waiting 10 seconds..."
+    sleep 10
+	GET_ROOTKEY=$(docker exec -it weaviate grep -s ROOTKEY /var/weaviate/first_run.log|sed 's/.*ROOTKEY=//')
+	if [ "$GET_ROOTKEY" != "" ]
+	then
+		ROOTKEY=$GET_ROOTKEY
+		echo "dgraph and weaviate started"
+    
+	fi
+done
 
 # Return end-point
-echo "Next line contains Weaviate IP + Weaviate root key seperated by a vertical bar (|). Make sure to update the root key!"
-echo "WEAVIATE|$WEAVIATEIP|ROOT-KEY"
+echo "Next line contains Weaviate IP + Weaviate ID seperated by a vertical bar (|)"
+echo "WEAVIATE|$WEAVIATEIP|$ROOTKEY"
+
