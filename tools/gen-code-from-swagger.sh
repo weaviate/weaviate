9a432c7fc67f5471bbab0ffd790e756047df8203
Maarten Hoogendoorn <maarten@moretea.nl>
2018-07-19 13:06:42 +0200

    gh-396: Remove all models/operations before generating new ones
    This is an improvement, because otherwise now non-existing models and
    operations will be left behind as cruft.

--- tools/gen-code-from-swagger.sh
+++ tools/gen-code-from-swagger.sh
@@ -12,6 +12,9 @@ if [ ! -f $SWAGGER ]; then
   chmod +x $SWAGGER
 fi
 
+# Remove old stuff.
+(cd $DIR/; rm -rf models restapi/operations/)
+
 (cd $DIR/..; $SWAGGER generate server --name=weaviate --spec=openapi-specs/schema.json --default-scheme=https)
 (cd $DIR/..; $SWAGGER generate client --spec=openapi-specs/schema.json --default-scheme=https)
 
