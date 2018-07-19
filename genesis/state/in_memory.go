2f0871e5e1534f9c6778adfa610a7b5c6897748f
Maarten Hoogendoorn <maarten@moretea.nl>
2018-07-19 13:01:29 +0200

    gh-396: gofmt

--- genesis/state/in_memory.go
+++ genesis/state/in_memory.go
@@ -1,81 +1,81 @@
 package state
 
 import (
-  "fmt"
-   log "github.com/sirupsen/logrus"
-  "github.com/go-openapi/strfmt"
-  "sync"
+	"fmt"
+	"github.com/go-openapi/strfmt"
+	log "github.com/sirupsen/logrus"
+	"sync"
 )
 
 type inMemoryState struct {
-  sync.Mutex
-  peers map[string]Peer
+	sync.Mutex
+	peers map[string]Peer
 }
 
 func NewInMemoryState() State {
-  return State(inMemoryState {
-    peers: make(map[string]Peer),
-  })
+	return State(inMemoryState{
+		peers: make(map[string]Peer),
+	})
 }
 
 func (im inMemoryState) RegisterPeer(name string, host strfmt.Hostname) error {
-  im.Lock()
-  defer im.Unlock()
-  log.Debugf("Registering peer '%v'", name)
-  _, ok := im.peers[name]
+	im.Lock()
+	defer im.Unlock()
+	log.Debugf("Registering peer '%v'", name)
+	_, ok := im.peers[name]
 
-  if ok {
-    log.Debugf("Already a peer named '%v'", name)
+	if ok {
+		log.Debugf("Already a peer named '%v'", name)
 
-    return fmt.Errorf("Such a peer already exists")
-  } else {
-    log.Debugf("Added the peer '%v'", name)
+		return fmt.Errorf("Such a peer already exists")
+	} else {
+		log.Debugf("Added the peer '%v'", name)
 
-    im.peers[name] = Peer {
-      name: name,
-      host: host,
-    }
+		im.peers[name] = Peer{
+			name: name,
+			host: host,
+		}
 
-    return nil
-  }
+		return nil
+	}
 }
 
 func (im inMemoryState) ListPeers() ([]Peer, error) {
-  im.Lock()
-  defer im.Unlock()
+	im.Lock()
+	defer im.Unlock()
 
-  peers := make([]Peer, 0)
+	peers := make([]Peer, 0)
 
-  for _, v := range(im.peers) {
-    peers = append(peers, v)
-  }
+	for _, v := range im.peers {
+		peers = append(peers, v)
+	}
 
-  return peers, nil
+	return peers, nil
 }
 
 func (im inMemoryState) RemovePeer(name string) error {
-  im.Lock()
-  defer im.Unlock()
+	im.Lock()
+	defer im.Unlock()
 
-  _, ok := im.peers[name]
+	_, ok := im.peers[name]
 
-  if ok {
-    delete(im.peers, name)
-  }
+	if ok {
+		delete(im.peers, name)
+	}
 
-  return nil
+	return nil
 }
 
 func (im inMemoryState) UpdatePeer(name string, update PeerInfo) error {
-  im.Lock()
-  defer im.Unlock()
+	im.Lock()
+	defer im.Unlock()
 
-  peer, ok := im.peers[name]
+	peer, ok := im.peers[name]
 
-  if ok {
-    peer.LastContactAt = update.LastContactAt
-    return nil
-  } else {
-    return fmt.Errorf("No such peer exists")
-  }
+	if ok {
+		peer.LastContactAt = update.LastContactAt
+		return nil
+	} else {
+		return fmt.Errorf("No such peer exists")
+	}
 }
--- genesis/state/state.go
+++ genesis/state/state.go
@@ -1,37 +1,35 @@
 package state
 
 import (
-  "time"
-  "github.com/go-openapi/strfmt"
+	"github.com/go-openapi/strfmt"
+	"time"
 )
 
-
-type PeerInfo  struct {
-  LastContactAt time.Time
+type PeerInfo struct {
+	LastContactAt time.Time
 }
 
 type Peer struct {
-  PeerInfo
-  name string
-  host strfmt.Hostname
+	PeerInfo
+	name string
+	host strfmt.Hostname
 }
 
 func (p Peer) Name() string {
-  return p.name
+	return p.name
 }
 
 func (p Peer) Host() strfmt.Hostname {
-  return p.host
+	return p.host
 }
-
 
 // Abstract interface over how the Genesis server should store state.
 type State interface {
-  RegisterPeer(name string, host strfmt.Hostname) error
-  ListPeers() ([]Peer, error)
+	RegisterPeer(name string, host strfmt.Hostname) error
+	ListPeers() ([]Peer, error)
 
-  // Idempotent remove; removing a non-existing peer should not fail.
-  RemovePeer(name string) error
+	// Idempotent remove; removing a non-existing peer should not fail.
+	RemovePeer(name string) error
 
-  UpdatePeer(name string, peer PeerInfo) error
+	UpdatePeer(name string, peer PeerInfo) error
 }
