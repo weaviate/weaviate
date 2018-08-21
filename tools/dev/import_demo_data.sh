25935ba6fed2fc914bca2405f449dad7e39f83d0
Maarten Hoogendoorn <maarten@moretea.nl>
2018-08-21 21:13:33 +0200

    gh-451: Supporting code to load fixture data (airports for now)

---
+++ tools/dev/import_demo_data.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Jump to root directory
+cd "$( dirname "${BASH_SOURCE[0]}" )"/../..
+
+# For now, we have hardcoded the credentials, host and location of dataset to import.
+go run ./tools/fixture_importer
