d7b31946f658cee0fbc980ef83616b366a14e8e7
Maarten Hoogendoorn <maarten@moretea.nl>
2018-10-15 11:19:45 +0200

    gh-486: Finish unit tests for local schema manager

--- database/schema_manager/local/manager.go
+++ database/schema_manager/local/manager.go
@@ -5,24 +5,64 @@ import (
 
 	"github.com/creativesoftwarefdn/weaviate/database"
 	"github.com/creativesoftwarefdn/weaviate/models"
+	"github.com/creativesoftwarefdn/weaviate/schema"
 	"github.com/creativesoftwarefdn/weaviate/schema/kind"
+
+	"github.com/go-openapi/strfmt"
 )
+
+func validateClassNameUniqueness(className string, schemaState *localSchemaState) error {
+	for _, otherClass := range schemaState.SchemaFor(kind.ACTION_KIND).Classes {
+		if className == otherClass.Class {
+			return fmt.Errorf("Name '%s' already used as a name for an Action class", className)
+		}
+	}
+
+	for _, otherClass := range schemaState.SchemaFor(kind.THING_KIND).Classes {
+		if className == otherClass.Class {
+			return fmt.Errorf("Name '%s' already used as a name for a Thing class", className)
+		}
+	}
+
+	return nil
+}
 
 // TODO extract to library.
 // Validate if a class can be added to the schema
 func validateCanAddClass(knd kind.Kind, class *models.SemanticSchemaClass, schemaState *localSchemaState) error {
 	// First check if there is a name clash.
-	fmt.Printf("schema state: %#v\n", schemaState)
-	fmt.Printf("action schema: %#v\n", schemaState.SchemaFor(kind.ACTION_KIND))
-	for _, otherClass := range schemaState.SchemaFor(kind.ACTION_KIND).Classes {
-		if class.Class == otherClass.Class {
-			return fmt.Errorf("Class name already used.")
+	err := validateClassNameUniqueness(class.Class, schemaState)
+	if err != nil {
+		return err
+	}
+
+	// TODO validate name against contextionary / keywords.
+	// TODO validate properties:
+	//  - against contextionary / keywords.
+	//  - primitive types
+	//  - relation types.
+
+	// all is fine!
+	return nil
+}
+
+func validatePropertyNameUniqueness(propertyName string, class *models.SemanticSchemaClass) error {
+	for _, otherProperty := range class.Properties {
+		if propertyName == otherProperty.Name {
+			return fmt.Errorf("Name '%s' already in use as a property name", propertyName)
 		}
 	}
-	for _, otherClass := range schemaState.SchemaFor(kind.THING_KIND).Classes {
-		if class.Class == otherClass.Class {
-			return fmt.Errorf("Class name already used.")
-		}
+
+	return nil
+}
+
+// Verify if we can add the passed property to the passed in class.
+// We need the total schema state to be able to check that references etc are valid.
+func validateCanAddProperty(property *models.SemanticSchemaClassProperty, class *models.SemanticSchemaClass, schemaState *localSchemaState) error {
+	// First check if there is a name clash.
+	err := validatePropertyNameUniqueness(property.Name, class)
+	if err != nil {
+		return err
 	}
 
 	// TODO validate name against contextionary / keywords.
@@ -42,6 +82,15 @@ func (l *LocalSchemaManager) GetSchema() database.Schema {
 	}
 }
 
+func (l *LocalSchemaManager) UpdateMeta(kind kind.Kind, atContext strfmt.URI, maintainer strfmt.Email, name string) error {
+	semanticSchema := l.schemaState.SchemaFor(kind)
+	semanticSchema.AtContext = atContext
+	semanticSchema.Maintainer = maintainer
+	semanticSchema.Name = name
+
+	return l.saveToDisk()
+}
+
 func (l *LocalSchemaManager) AddClass(kind kind.Kind, class *models.SemanticSchemaClass) error {
 	err := validateCanAddClass(kind, class, &l.schemaState)
 	if err != nil {
@@ -50,8 +99,7 @@ func (l *LocalSchemaManager) AddClass(kind kind.Kind, class *models.SemanticSche
 		// TODO keep it sorted.
 		semanticSchema := l.schemaState.SchemaFor(kind)
 		semanticSchema.Classes = append(semanticSchema.Classes, class)
-		l.saveToDisk()
-		return nil
+		return l.saveToDisk()
 	}
 }
 
@@ -75,19 +123,118 @@ func (l *LocalSchemaManager) DropClass(kind kind.Kind, className string) error {
 	semanticSchema.Classes[len(semanticSchema.Classes)-1] = nil // to prevent leaking this pointer.
 	semanticSchema.Classes = semanticSchema.Classes[:len(semanticSchema.Classes)-1]
 
-	l.saveToDisk()
+	return l.saveToDisk()
+}
+
+func (l *LocalSchemaManager) UpdateClass(kind kind.Kind, className string, newClassName *string, newKeywords *models.SemanticSchemaKeywords) error {
+	semanticSchema := l.schemaState.SchemaFor(kind)
+
+	class, err := schema.GetClassByName(semanticSchema, className)
+	if err != nil {
+		return err
+	}
+
+	// First validate the request
+	if newClassName != nil {
+		err = validateClassNameUniqueness(*newClassName, &l.schemaState)
+		if err != nil {
+			return err
+		}
+	}
+
+	if newKeywords != nil {
+		// TODO validate keywords in contextionary
+	}
+
+	// Validated! Now apply the changes.
+	if newClassName != nil {
+		class.Class = *newClassName
+	}
+
+	if newKeywords != nil {
+		class.Keywords = *newKeywords
+	}
 
 	return nil
 }
 
-func (l *LocalSchemaManager) AddProperty(kind kind.Kind, className string, prop models.SemanticSchemaClassProperty) error {
+func (l *LocalSchemaManager) AddProperty(kind kind.Kind, className string, prop *models.SemanticSchemaClassProperty) error {
+	semanticSchema := l.schemaState.SchemaFor(kind)
+	class, err := schema.GetClassByName(semanticSchema, className)
+	if err != nil {
+		return err
+	}
+
+	err = validateCanAddProperty(prop, class, &l.schemaState)
+	if err != nil {
+		return err
+	}
+
+	class.Properties = append(class.Properties, prop)
+
 	return nil
 }
 
-func (l *LocalSchemaManager) UpdateProperty(kind kind.Kind, className string, propName string, newName *string, newKeywords *models.SemanticSchemaClassKeywords) error {
+func (l *LocalSchemaManager) UpdateProperty(kind kind.Kind, className string, propName string, newName *string, newKeywords *models.SemanticSchemaKeywords) error {
+	semanticSchema := l.schemaState.SchemaFor(kind)
+	class, err := schema.GetClassByName(semanticSchema, className)
+
+	if err != nil {
+		return err
+	}
+
+	prop, err := schema.GetPropertyByName(class, propName)
+	if err != nil {
+		return err
+	}
+
+	if newName != nil {
+		// verify uniqueness
+		err = validatePropertyNameUniqueness(*newName, class)
+		if err != nil {
+			return err
+		}
+	}
+
+	if newKeywords != nil {
+		// TODO: validate keywords.
+	}
+
+	// The updates are validated. Apply them.
+
+	if newName != nil {
+		prop.Name = *newName
+	}
+
+	if newKeywords != nil {
+		prop.Keywords = *newKeywords
+	}
+
 	return nil
 }
 
 func (l *LocalSchemaManager) DropProperty(kind kind.Kind, className string, propName string) error {
-	return nil
+	semanticSchema := l.schemaState.SchemaFor(kind)
+	class, err := schema.GetClassByName(semanticSchema, className)
+	if err != nil {
+		return err
+	}
+
+	var propIdx int = -1
+	for idx, prop := range class.Properties {
+		if prop.Name == propName {
+			propIdx = idx
+			break
+		}
+	}
+
+	if propIdx == -1 {
+		return fmt.Errorf("Could not find property '%s'", propName)
+	}
+
+	class.Properties[propIdx] = class.Properties[len(class.Properties)-1]
+	class.Properties[len(class.Properties)-1] = nil // to prevent leaking this pointer.
+	class.Properties = class.Properties[:len(class.Properties)-1]
+
+	return l.saveToDisk()
 }
---
+++ database/schema_manager/local/manager_test.go
@@ -0,0 +1,427 @@
+package local
+
+import (
+	log "github.com/sirupsen/logrus"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/creativesoftwarefdn/weaviate/models"
+	"github.com/creativesoftwarefdn/weaviate/schema/kind"
+	"github.com/go-openapi/strfmt"
+)
+
+var schemaTests = []struct {
+	name string
+	fn   func(*testing.T, *LocalSchemaManager)
+}{
+	{name: "UpdateMeta", fn: testUpdateMeta},
+	{name: "AddThingClass", fn: testAddThingClass},
+	{name: "RemoveThingClass", fn: testRemoveThingClass},
+	{name: "CantAddSameClassTwice", fn: testCantAddSameClassTwice},
+	{name: "CantAddSameClassTwiceDifferentKind", fn: testCantAddSameClassTwiceDifferentKinds},
+	{name: "UpdateClassName", fn: testUpdateClassName},
+	{name: "UpdateClassNameCollision", fn: testUpdateClassNameCollision},
+	{name: "AddThingClassWithKeywords", fn: testAddThingClassWithKeywords},
+	{name: "UpdateClassKeywords", fn: testUpdateClassKeywords},
+	{name: "AddPropertyDuringCreation", fn: testAddPropertyDuringCreation},
+	{name: "AddInvalidPropertyDuringCreation", fn: testAddInvalidPropertyDuringCreation},
+	{name: "DropProperty", fn: testDropProperty},
+	{name: "UpdatePropertyName", fn: testUpdatePropertyName},
+	{name: "UpdatePropertyNameCollision", fn: testUpdatePropertyNameCollision},
+	{name: "UpdatePropertyKeywords", fn: testUpdatePropertyKeywords},
+}
+
+func testUpdateMeta(t *testing.T, lsm *LocalSchemaManager) {
+	t.Parallel()
+
+	assert.Equal(t, lsm.GetSchema().Things.AtContext, strfmt.URI(""))
+	assert.Equal(t, lsm.GetSchema().Things.Maintainer, strfmt.Email(""))
+	assert.Equal(t, lsm.GetSchema().Things.Name, "")
+
+	assert.Nil(t, lsm.UpdateMeta(kind.THING_KIND, "http://new/context", "person@example.org", "somename"))
+
+	assert.Equal(t, lsm.GetSchema().Things.AtContext, strfmt.URI("http://new/context"))
+	assert.Equal(t, lsm.GetSchema().Things.Maintainer, strfmt.Email("person@example.org"))
+	assert.Equal(t, lsm.GetSchema().Things.Name, "somename")
+}
+
+func testAddThingClass(t *testing.T, lsm *LocalSchemaManager) {
+	t.Parallel()
+
+	thingClasses := testGetClassNames(lsm, kind.THING_KIND)
+	assert.NotContains(t, thingClasses, "Car")
+
+	err := lsm.AddClass(kind.THING_KIND, &models.SemanticSchemaClass{
+		Class: "Car",
+	})
+
+	assert.Nil(t, err)
+
+	thingClasses = testGetClassNames(lsm, kind.THING_KIND)
+	assert.Contains(t, thingClasses, "Car")
+}
+
+func testRemoveThingClass(t *testing.T, lsm *LocalSchemaManager) {
+	t.Parallel()
+
+	err := lsm.AddClass(kind.THING_KIND, &models.SemanticSchemaClass{
+		Class: "Car",
+	})
+
+	assert.Nil(t, err)
+
+	thingClasses := testGetClassNames(lsm, kind.THING_KIND)
+	assert.Contains(t, thingClasses, "Car")
+
+	// Now delete the class
+	err = lsm.DropClass(kind.THING_KIND, "Car")
+	assert.Nil(t, err)
+
+	thingClasses = testGetClassNames(lsm, kind.THING_KIND)
+	assert.NotContains(t, thingClasses, "Car")
+}
+
+func testCantAddSameClassTwice(t *testing.T, lsm *LocalSchemaManager) {
+	t.Parallel()
+
+	err := lsm.AddClass(kind.THING_KIND, &models.SemanticSchemaClass{
+		Class: "Car",
+	})
+
+	assert.Nil(t, err)
+
+	// Add it again
+	err = lsm.AddClass(kind.THING_KIND, &models.SemanticSchemaClass{
+		Class: "Car",
+	})
+
+	assert.NotNil(t, err)
+}
+
+func testCantAddSameClassTwiceDifferentKinds(t *testing.T, lsm *LocalSchemaManager) {
+	t.Parallel()
+
+	err := lsm.AddClass(kind.THING_KIND, &models.SemanticSchemaClass{
+		Class: "Car",
+	})
+
+	assert.Nil(t, err)
+
+	// Add it again, but with a different kind.
+	err = lsm.AddClass(kind.ACTION_KIND, &models.SemanticSchemaClass{
+		Class: "Car",
+	})
+
+	assert.NotNil(t, err)
+}
+
+func testUpdateClassName(t *testing.T, lsm *LocalSchemaManager) {
+	t.Parallel()
+
+	// Create a simple class.
+	assert.Nil(t, lsm.AddClass(kind.THING_KIND, &models.SemanticSchemaClass{Class: "InitialName"}))
+
+	// Rename it
+	newName := "NewName"
+	assert.Nil(t, lsm.UpdateClass(kind.THING_KIND, "InitialName", &newName, nil))
+
+	thingClasses := testGetClassNames(lsm, kind.THING_KIND)
+	assert.Len(t, thingClasses, 1)
+	assert.Equal(t, thingClasses[0], "NewName")
+}
+
+func testUpdateClassNameCollision(t *testing.T, lsm *LocalSchemaManager) {
+	t.Parallel()
+
+	// Create a class to rename
+	assert.Nil(t, lsm.AddClass(kind.THING_KIND, &models.SemanticSchemaClass{Class: "InitialName"}))
+
+	// Create another class, that we'll collide names with.
+	// For some extra action, use a Action class here.
+	assert.Nil(t, lsm.AddClass(kind.ACTION_KIND, &models.SemanticSchemaClass{Class: "ExistingClass"}))
+
+	// Try to rename a class to one that already exists
+	collidingNewName := "ExistingClass"
+	err := lsm.UpdateClass(kind.THING_KIND, "InitialName", &collidingNewName, nil)
+	// Should fail
+	assert.NotNil(t, err)
+
+	// Should not change the original name
+	thingClasses := testGetClassNames(lsm, kind.THING_KIND)
+	assert.Len(t, thingClasses, 1)
+	assert.Equal(t, thingClasses[0], "InitialName")
+}
+
+func testAddThingClassWithKeywords(t *testing.T, lsm *LocalSchemaManager) {
+	t.Parallel()
+
+	keywords := models.SemanticSchemaKeywords{
+		{Keyword: "vehicle", Weight: 0.6},
+		{Keyword: "transport", Weight: 0.4},
+	}
+
+	err := lsm.AddClass(kind.THING_KIND, &models.SemanticSchemaClass{
+		Class:    "Car",
+		Keywords: keywords,
+	})
+	assert.Nil(t, err)
+
+	thingClasses := testGetClasses(lsm, kind.THING_KIND)
+	assert.Len(t, thingClasses, 1)
+	assert.Len(t, thingClasses[0].Keywords, 2)
+	assert.Equal(t, thingClasses[0].Keywords[0].Keyword, "vehicle")
+	assert.Equal(t, thingClasses[0].Keywords[0].Weight, float32(0.6))
+	assert.Equal(t, thingClasses[0].Keywords[1].Keyword, "transport")
+	assert.Equal(t, thingClasses[0].Keywords[1].Weight, float32(0.4))
+}
+
+func testUpdateClassKeywords(t *testing.T, lsm *LocalSchemaManager) {
+	t.Parallel()
+
+	// Create class with a keyword
+	keywords := models.SemanticSchemaKeywords{
+		{Keyword: "transport", Weight: 1.0},
+	}
+
+	err := lsm.AddClass(kind.THING_KIND, &models.SemanticSchemaClass{
+		Class:    "Car",
+		Keywords: keywords,
+	})
+	assert.Nil(t, err)
+
+	//Now update just the keyword
+	updatedKeywords := models.SemanticSchemaKeywords{
+		{Keyword: "vehicle", Weight: 1.0},
+	}
+
+	err = lsm.UpdateClass(kind.THING_KIND, "Car", nil, &updatedKeywords)
+
+	thingClasses := testGetClasses(lsm, kind.THING_KIND)
+	assert.Len(t, thingClasses, 1)
+	assert.Len(t, thingClasses[0].Keywords, 1)
+	assert.Equal(t, thingClasses[0].Keywords[0].Keyword, "vehicle")
+	assert.Equal(t, thingClasses[0].Keywords[0].Weight, float32(1.0))
+}
+
+func testAddPropertyDuringCreation(t *testing.T, lsm *LocalSchemaManager) {
+	t.Parallel()
+
+	var properties []*models.SemanticSchemaClassProperty = []*models.SemanticSchemaClassProperty{
+		{Name: "color", AtDataType: []string{"string"}},
+	}
+
+	err := lsm.AddClass(kind.THING_KIND, &models.SemanticSchemaClass{
+		Class:      "Car",
+		Properties: properties,
+	})
+	assert.Nil(t, err)
+
+	thingClasses := testGetClasses(lsm, kind.THING_KIND)
+	assert.Len(t, thingClasses, 1)
+	require.Len(t, thingClasses[0].Properties, 1)
+	assert.Equal(t, thingClasses[0].Properties[0].Name, "color")
+	assert.Equal(t, thingClasses[0].Properties[0].AtDataType, []string{"string"})
+}
+
+func testAddInvalidPropertyDuringCreation(t *testing.T, lsm *LocalSchemaManager) {
+	t.Skip("Validation")
+	t.Parallel()
+
+	var properties []*models.SemanticSchemaClassProperty = []*models.SemanticSchemaClassProperty{
+		{Name: "color", AtDataType: []string{"blurp"}},
+	}
+
+	err := lsm.AddClass(kind.THING_KIND, &models.SemanticSchemaClass{
+		Class:      "Car",
+		Properties: properties,
+	})
+	assert.NotNil(t, err)
+}
+
+func testDropProperty(t *testing.T, lsm *LocalSchemaManager) {
+	t.Parallel()
+
+	var properties []*models.SemanticSchemaClassProperty = []*models.SemanticSchemaClassProperty{
+		{Name: "color", AtDataType: []string{"string"}},
+	}
+
+	err := lsm.AddClass(kind.THING_KIND, &models.SemanticSchemaClass{
+		Class:      "Car",
+		Properties: properties,
+	})
+	assert.Nil(t, err)
+
+	thingClasses := testGetClasses(lsm, kind.THING_KIND)
+	require.Len(t, thingClasses, 1)
+	assert.Len(t, thingClasses[0].Properties, 1)
+
+	// Now drop the property
+	lsm.DropProperty(kind.THING_KIND, "Car", "color")
+
+	thingClasses = testGetClasses(lsm, kind.THING_KIND)
+	require.Len(t, thingClasses, 1)
+	assert.Len(t, thingClasses[0].Properties, 0)
+}
+
+func testUpdatePropertyName(t *testing.T, lsm *LocalSchemaManager) {
+	t.Parallel()
+
+	// Create a class & property
+	var properties []*models.SemanticSchemaClassProperty = []*models.SemanticSchemaClassProperty{
+		{Name: "color", AtDataType: []string{"string"}},
+	}
+
+	err := lsm.AddClass(kind.THING_KIND, &models.SemanticSchemaClass{
+		Class:      "Car",
+		Properties: properties,
+	})
+	assert.Nil(t, err)
+
+	// Update the property name
+	smell := "smell"
+	err = lsm.UpdateProperty(kind.THING_KIND, "Car", "color", &smell, nil)
+	assert.Nil(t, err)
+
+	// Check that the name is updated
+	thingClasses := testGetClasses(lsm, kind.THING_KIND)
+	assert.Len(t, thingClasses, 1)
+	require.Len(t, thingClasses[0].Properties, 1)
+	assert.Equal(t, thingClasses[0].Properties[0].Name, "smell")
+	assert.Equal(t, thingClasses[0].Properties[0].AtDataType, []string{"string"})
+}
+
+func testUpdatePropertyNameCollision(t *testing.T, lsm *LocalSchemaManager) {
+	t.Parallel()
+
+	// Create a class & property
+	var properties []*models.SemanticSchemaClassProperty = []*models.SemanticSchemaClassProperty{
+		{Name: "color", AtDataType: []string{"string"}},
+		{Name: "smell", AtDataType: []string{"string"}},
+	}
+
+	err := lsm.AddClass(kind.THING_KIND, &models.SemanticSchemaClass{
+		Class:      "Car",
+		Properties: properties,
+	})
+	assert.Nil(t, err)
+
+	// Update the property name
+	smell := "smell"
+	err = lsm.UpdateProperty(kind.THING_KIND, "Car", "color", &smell, nil)
+	assert.NotNil(t, err)
+
+	// Check that the name is updated
+	thingClasses := testGetClasses(lsm, kind.THING_KIND)
+	assert.Len(t, thingClasses, 1)
+	require.Len(t, thingClasses[0].Properties, 2)
+	assert.Equal(t, thingClasses[0].Properties[0].Name, "color")
+	assert.Equal(t, thingClasses[0].Properties[1].Name, "smell")
+}
+
+func testUpdatePropertyKeywords(t *testing.T, lsm *LocalSchemaManager) {
+	t.Parallel()
+
+	// Create a class Car with a property color.
+
+	var properties []*models.SemanticSchemaClassProperty = []*models.SemanticSchemaClassProperty{
+		{Name: "color", AtDataType: []string{"string"}},
+	}
+
+	err := lsm.AddClass(kind.THING_KIND, &models.SemanticSchemaClass{
+		Class:      "Car",
+		Properties: properties,
+	})
+	assert.Nil(t, err)
+
+	thingClasses := testGetClasses(lsm, kind.THING_KIND)
+	assert.Len(t, thingClasses, 1)
+	require.Len(t, thingClasses[0].Properties, 1)
+	assert.Equal(t, thingClasses[0].Properties[0].Name, "color")
+
+	// Assert that there are no keywords.
+	assert.Nil(t, thingClasses[0].Properties[0].Keywords)
+
+	// Now update the property, add keywords
+	newKeywords := &models.SemanticSchemaKeywords{
+		&models.SemanticSchemaKeywordsItems0{Keyword: "color", Weight: 0.9},
+		&models.SemanticSchemaKeywordsItems0{Keyword: "paint", Weight: 0.1},
+	}
+
+	err = lsm.UpdateProperty(kind.THING_KIND, "Car", "color", nil, newKeywords)
+	assert.Nil(t, err)
+
+	// Verify the content of the keywords.
+	thingClasses = testGetClasses(lsm, kind.THING_KIND)
+	assert.Len(t, thingClasses, 1)
+	require.Len(t, thingClasses[0].Properties, 1)
+	assert.Equal(t, "color", thingClasses[0].Properties[0].Keywords[0].Keyword)
+	assert.Equal(t, float32(0.9), thingClasses[0].Properties[0].Keywords[0].Weight)
+	assert.Equal(t, "paint", thingClasses[0].Properties[0].Keywords[1].Keyword)
+	assert.Equal(t, float32(0.1), thingClasses[0].Properties[0].Keywords[1].Weight)
+}
+
+// This grant parent test setups up the temporary directory needed for the tests.
+func TestSchema(t *testing.T) {
+	tempDir, err := ioutil.TempDir("", "test-schema-manager")
+	if err != nil {
+		log.Fatalf("Could not initialize temporary directory: %v\n", err)
+	}
+
+	// We need this test here to make sure that we wait until all child tests
+	// (that can be run in parallel) have finished, before cleaning up the temp directory.
+	t.Run("group", func(t *testing.T) {
+		for _, testCase := range schemaTests {
+
+			// Create a test case, and inject the local schema manager in there
+			// to reduce boilerplate in each separate test.
+			t.Run(testCase.name, func(t *testing.T) {
+				lsm := newLSM(tempDir)
+				testCase.fn(t, lsm)
+			})
+		}
+	})
+
+	os.RemoveAll(tempDir)
+}
+
+// New Local Schema Manager
+func newLSM(baseTempDir string) *LocalSchemaManager {
+	tempDir, err := ioutil.TempDir(baseTempDir, "test-schema-manager")
+	if err != nil {
+		log.Fatalf("Could not initialize temporary directory: %v\n", err)
+	}
+
+	lsm, err := New(tempDir)
+	if err != nil {
+		panic(err)
+	}
+
+	return lsm
+}
+
+func testGetClasses(l *LocalSchemaManager, k kind.Kind) []*models.SemanticSchemaClass {
+	var classes []*models.SemanticSchemaClass
+	schema := l.GetSchema()
+
+	for _, class := range schema.SemanticSchemaFor(k).Classes {
+		classes = append(classes, class)
+	}
+
+	return classes
+}
+
+func testGetClassNames(l *LocalSchemaManager, k kind.Kind) []string {
+	var names []string
+	schema := l.GetSchema()
+
+	// Extract all names
+	for _, class := range schema.SemanticSchemaFor(k).Classes {
+		names = append(names, class.Class)
+	}
+
+	return names
+}
