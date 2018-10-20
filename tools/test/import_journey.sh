118e1c68f1355e6fd1627b9f39e3e1e8b48300bc
Etienne Dilocker <etienne@dilocker.de>
2018-10-20 15:11:05 +0200

    gh-525 add script which blocks until dev-importer completes
    ... and either succeed or timeout after a set amount of seconds.
    
    As of now it's not running on CI yet, but works fine locally. Next up is
    to try it out on CI. Then we'll have an easy way to tell from our CI
    whether the development path is blocked or not. I.e. in the future we
    should no longer merge any PRs which don't pass this test.

---
+++ tools/test/import_journey.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# This scripts runs the import journey that is included in our development setup
+# The point is to fail on CI, not only if weaviate itself cannot be built,
+# but also if test data cannot be importet. It therefore tests the journey
+# of the weaviate developer.
+
+docker-compose -f docker-compose-dev.yml build
+docker-compose -f docker-compose-dev.yml up --force-recreate -d 
+
+MAX_WAIT_SECONDS=120
+END_TIME=$(( SECONDS + MAX_WAIT_SECONDS ))
+IMPORTER_EXIT_CODE=
+
+echo -n "Waiting for the importer container to finish..."
+while [ $SECONDS -lt $END_TIME ]; do
+  IMPORTER_EXIT_CODE=$(
+    docker container wait weaviate_weaviate_demo_importer_1
+  )
+
+  if [ "$IMPORTER_EXIT_CODE" -eq "0" ]; then
+    echo 
+    echo "Success!";
+    exit 0;
+  fi
+
+  echo -n "."
+done
+
+echo
+echo -------
+echo FAILURE
+echo -------
+echo "The importer never succeeded in ${MAX_WAIT_SECONDS}s."
+echo "The last exit code was $IMPORTER_EXIT_CODE."
+echo "Here are the last 30 lines of logs from the importer:"
+docker logs weaviate_weaviate_demo_importer_1 --tail 30
+
+exit $IMPORTER_EXIT_CODE
+
