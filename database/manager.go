76d0c6e218542db663bf532bce1d2ad5d34be3c0
Maarten Hoogendoorn <maarten@moretea.nl>
2018-10-23 14:59:25 +0200

    gh-507: Move Migrator interface into its own package

--- database/manager.go
+++ database/manager.go
@@ -1,33 +1,16 @@
 package database
 
 import (
-	"github.com/creativesoftwarefdn/weaviate/models"
+	"github.com/creativesoftwarefdn/weaviate/database/schema_migrator"
 	"github.com/creativesoftwarefdn/weaviate/schema/kind"
-
 	"github.com/go-openapi/strfmt"
 )
 
-type Migrator interface {
-	// Both a Connector and a SchemaManager are expected to implement this.
+type SchemaManager interface {
+	schema_migrator.Migrator
 
 	// Update the Thing or Action schema's meta data.
 	UpdateMeta(kind kind.Kind, atContext strfmt.URI, maintainer strfmt.Email, name string) error
-
-	// Add a class to the Thing or Action schema, depending on the kind parameter.
-	AddClass(kind kind.Kind, class *models.SemanticSchemaClass) error
-
-	// Drop a class from the schema.
-	DropClass(kind kind.Kind, className string) error
-
-	UpdateClass(kind kind.Kind, className string, newClassName *string, newKeywords *models.SemanticSchemaKeywords) error
-
-	AddProperty(kind kind.Kind, className string, prop *models.SemanticSchemaClassProperty) error
-	UpdateProperty(kind kind.Kind, className string, propName string, newName *string, newKeywords *models.SemanticSchemaKeywords) error
-	DropProperty(kind kind.Kind, className string, propName string) error
-}
-
-type SchemaManager interface {
-	Migrator
 
 	// Return a reference to the database schema.
 	// Note that this function can be both called from having a ConnectorLock as a SchemaLock.
---
+++ database/schema_migrator/migrator.go
@@ -0,0 +1,28 @@
+package schema_migrator
+
+import (
+	"github.com/creativesoftwarefdn/weaviate/models"
+	"github.com/creativesoftwarefdn/weaviate/schema/kind"
+)
+
+type Migrator interface {
+	// Add a class to the Thing or Action schema, depending on the kind parameter.
+	AddClass(kind kind.Kind, class *models.SemanticSchemaClass) error
+
+	// Drop a class from the schema.
+	DropClass(kind kind.Kind, className string) error
+
+	// Update a given class. If newClassName is not nil, update the class name, if newKeywords is not nil, update the keywords.
+	// If both updates are specified, either both updates succeed, or none do.
+	UpdateClass(kind kind.Kind, className string, newClassName *string, newKeywords *models.SemanticSchemaKeywords) error
+
+	// Add a property to a given class.
+	AddProperty(kind kind.Kind, className string, prop *models.SemanticSchemaClassProperty) error
+
+	// Update a given property. If newName is not nil, update the property name, if newKeywords is not nil, update the keywords.
+	// If both updates are specified, either both updates succeed, or none do.
+	UpdateProperty(kind kind.Kind, className string, propName string, newName *string, newKeywords *models.SemanticSchemaKeywords) error
+
+	// Drop the given property from the schema
+	DropProperty(kind kind.Kind, className string, propName string) error
+}
