aa9d68525029e60958c988933acd04b4b8df839a
Maarten Hoogendoorn <maarten@moretea.nl>
2018-11-04 23:59:43 +0100

    Add kind lookup functions
    This will allow us to refactor kinds to other types (e.g. ints).

--- schema/kind/kind.go
+++ schema/kind/kind.go
@@ -1,6 +1,25 @@
 package kind
 
+import (
+	"fmt"
+)
+
 type Kind string
 
 const THING_KIND Kind = "thing"
 const ACTION_KIND Kind = "action"
+
+func (k *Kind) Name() string {
+	return string(*k)
+}
+
+func KindByName(name string) Kind {
+	switch name {
+	case "thing":
+		return THING_KIND
+	case "action":
+		return ACTION_KIND
+	default:
+		panic(fmt.Sprintf("No such kind %s", name))
+	}
+}
