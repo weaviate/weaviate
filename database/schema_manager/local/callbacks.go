77ef58bddaa5e61d601156ac06453f3bf157a2f0
Maarten Hoogendoorn <maarten@moretea.nl>
2018-11-04 23:59:43 +0100

    Extend schema to also be able to store state for a connector.
    - Introduce two interfaces "database/connector_state".StateManager
      and "database/connector_state".Connector
    - Make the database initialization connect those two.
    - Convert local schema storage from struct to interface.
    
    Closes gh-552.

--- database/schema_manager/local/callbacks.go
+++ database/schema_manager/local/callbacks.go
@@ -4,11 +4,11 @@ import (
 	db_schema "github.com/creativesoftwarefdn/weaviate/database/schema"
 )
 
-func (l *LocalSchemaManager) RegisterSchemaUpdateCallback(callback func(updatedSchema db_schema.Schema)) {
+func (l *localSchemaManager) RegisterSchemaUpdateCallback(callback func(updatedSchema db_schema.Schema)) {
 	l.callbacks = append(l.callbacks, callback)
 }
 
-func (l *LocalSchemaManager) triggerCallbacks() {
+func (l *localSchemaManager) triggerCallbacks() {
 	schema := l.GetSchema()
 
 	for _, cb := range l.callbacks {
