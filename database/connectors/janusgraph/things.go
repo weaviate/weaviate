4f0f2885ce858a9993f66545accf3c36063d1966
Maarten Hoogendoorn <maarten@moretea.nl>
2018-11-06 23:59:10 +0100

    Re-use class helpers for things.

--- database/connectors/janusgraph/things.go
+++ database/connectors/janusgraph/things.go
@@ -15,161 +15,33 @@ package janusgraph
 
 import (
 	"context"
-	"errors"
-	"fmt"
-	"reflect"
-	"time"
 
 	"github.com/go-openapi/strfmt"
 
-	connutils "github.com/creativesoftwarefdn/weaviate/database/connectors/utils"
+	"fmt"
+	"github.com/creativesoftwarefdn/weaviate/database/connectors/utils"
 	"github.com/creativesoftwarefdn/weaviate/database/schema"
 	"github.com/creativesoftwarefdn/weaviate/database/schema/kind"
 	"github.com/creativesoftwarefdn/weaviate/models"
-
-	"github.com/creativesoftwarefdn/weaviate/gremlin"
-
-	"encoding/json"
 )
 
 func (j *Janusgraph) AddThing(ctx context.Context, thing *models.Thing, UUID strfmt.UUID) error {
-	k := kind.THING_KIND
-
 	sanitizedClassName := schema.AssertValidClassName(thing.AtClass)
-	vertexLabel := j.state.getMappedClassName(sanitizedClassName)
-
-	q := gremlin.G.AddV(string(vertexLabel)).
-		As("newClass").
-		StringProperty(PROP_KIND, k.Name()).
-		StringProperty(PROP_UUID, UUID.String()).
-		StringProperty(PROP_CLASS_ID, string(vertexLabel)).
-		StringProperty(PROP_AT_CONTEXT, thing.AtContext).
-		Int64Property(PROP_CREATION_TIME_UNIX, thing.CreationTimeUnix).
-		Int64Property(PROP_LAST_UPDATE_TIME_UNIX, thing.LastUpdateTimeUnix)
-
-	// map properties in thing.Schema according to the mapping.
-
-	type edgeToAdd struct {
-		PropertyName string
-		Type         string
-		Reference    string
-		Location     string
-	}
-
-	var edgesToAdd []edgeToAdd
-
-	thingSchema, schema_ok := thing.Schema.(map[string]interface{})
-	if schema_ok {
-		for propName, value := range thingSchema {
-			// TODO relation type
-			// if primitive type:
-			sanitziedPropertyName := schema.AssertValidPropertyName(propName)
-			janusPropertyName := string(j.state.getMappedPropertyName(sanitizedClassName, sanitziedPropertyName))
-
-			switch t := value.(type) {
-			case string:
-				q = q.StringProperty(janusPropertyName, t)
-			case int:
-				q = q.Int64Property(janusPropertyName, int64(t))
-			case int8:
-				q = q.Int64Property(janusPropertyName, int64(t))
-			case int16:
-				q = q.Int64Property(janusPropertyName, int64(t))
-			case int32:
-				q = q.Int64Property(janusPropertyName, int64(t))
-			case int64:
-				q = q.Int64Property(janusPropertyName, t)
-			case bool:
-				q = q.BoolProperty(janusPropertyName, t)
-			case float32:
-				q = q.Float64Property(janusPropertyName, float64(t))
-			case float64:
-				q = q.Float64Property(janusPropertyName, t)
-			case time.Time:
-				q = q.StringProperty(janusPropertyName, time.Time.String(t))
-			case *models.SingleRef:
-				panic("not supported yet")
-				// Postpone creation of edges
-				edgesToAdd = append(edgesToAdd, edgeToAdd{
-					PropertyName: janusPropertyName,
-					Reference:    t.NrDollarCref.String(),
-					Type:         t.Type,
-					Location:     *t.LocationURL,
-				})
-			default:
-				j.messaging.ExitError(78, "The type "+reflect.TypeOf(value).String()+" is not supported for Thing properties.")
-			}
-		}
-	}
-	//
-	//	// Add edges to all referened things.
-	//	for _, edge := range edgesToAdd {
-	//		q = q.AddE("thingEdge").
-	//			FromRef("newClass").
-	//			ToQuery(gremlin.G.V().HasLabel(THING_LABEL).HasString("uuid", edge.Reference)).
-	//			StringProperty(PROPERTY_EDGE_LABEL, edge.PropertyName).
-	//			StringProperty("$cref", edge.Reference).
-	//			StringProperty("type", edge.Type).
-	//			StringProperty("locationUrl", edge.Location)
-	//	}
-
-	// Link to key
-	q = q.AddE(KEY_VERTEX_LABEL).
-		StringProperty("locationUrl", *thing.Key.LocationURL).
-		FromRef("newClass").
-		ToQuery(gremlin.G.V().HasLabel(KEY_VERTEX_LABEL).HasString(PROP_UUID, thing.Key.NrDollarCref.String()))
-
-	_, err := j.client.Execute(q)
-
-	return err
+	return j.addClass(kind.THING_KIND, sanitizedClassName, UUID, thing.AtContext, thing.CreationTimeUnix, thing.LastUpdateTimeUnix, thing.Key, thing.Schema)
 }
 
-func (f *Janusgraph) GetThing(ctx context.Context, UUID strfmt.UUID, thingResponse *models.ThingGetResponse) error {
-	k := kind.THING_KIND
-
-	// Fetch the thing, it's key, and it's relations.
-	q := gremlin.G.V().
-		StringProperty(PROP_KIND, k.Name()).
-		HasString(PROP_UUID, string(UUID)).
-		As("class").
-		OutEWithLabel(KEY_VERTEX_LABEL).As("keyEdge").
-		InV().Path().FromRef("keyEdge").As("key"). // also get the path, so that we can learn about the location of the key.
-		V().
-		HasString(PROP_UUID, string(UUID)).
-		Raw(`.optional(outE("thingEdge").as("thingEdge").as("ref")).choose(select("ref"), select("class", "key", "ref"), select("class", "key"))`)
-
-	result, err := f.client.Execute(q)
-
-	if err != nil {
-		return err
-	}
-
-	if len(result.Data) == 0 {
-		return errors.New(connutils.StaticThingNotFound)
-	}
-
-	// The outputs 'thing' and 'key' will be repeated over all results. Just get them for one for now.
-	thingVertex := result.Data[0].AssertKey("class").AssertVertex()
-	keyPath := result.Data[0].AssertKey("key").AssertPath()
-
-	// However, we can get multiple refs. In that case, we'll have multiple datums,
-	// each with the same thing & key, but a different ref.
-	// Let's extract those refs.
-	var refEdges []*gremlin.Edge
-	for _, datum := range result.Data {
-		ref, err := datum.Key("ref")
-		if err == nil {
-			refEdges = append(refEdges, ref.AssertEdge())
-		}
-	}
-
-	thingResponse.Key = newKeySingleRefFromKeyPath(keyPath)
-	return f.fillThingResponseFromVertexAndEdges(thingVertex, refEdges, thingResponse)
-	return nil
+func (j *Janusgraph) GetThing(ctx context.Context, UUID strfmt.UUID, thingResponse *models.ThingGetResponse) error {
+	return j.getClass(kind.THING_KIND, UUID,
+		&thingResponse.AtClass,
+		&thingResponse.AtContext,
+		&thingResponse.ThingID,
+		&thingResponse.CreationTimeUnix,
+		&thingResponse.LastUpdateTimeUnix,
+		&thingResponse.Schema,
+		&thingResponse.Key)
 }
 
-// TODO check
-func (f *Janusgraph) GetThings(ctx context.Context, UUIDs []strfmt.UUID, response *models.ThingsListResponse) error {
+func (j *Janusgraph) GetThings(ctx context.Context, UUIDs []strfmt.UUID, response *models.ThingsListResponse) error {
 	// TODO: Optimize query to perform just _one_ JanusGraph lookup.
 
 	response.TotalResults = 0
@@ -177,7 +49,7 @@ func (f *Janusgraph) GetThings(ctx context.Context, UUIDs []strfmt.UUID, respons
 
 	for _, uuid := range UUIDs {
 		var thing_response models.ThingGetResponse
-		err := f.GetThing(ctx, uuid, &thing_response)
+		err := j.GetThing(ctx, uuid, &thing_response)
 
 		if err == nil {
 			response.TotalResults += 1
@@ -190,156 +62,36 @@ func (f *Janusgraph) GetThings(ctx context.Context, UUIDs []strfmt.UUID, respons
 	return nil
 }
 
-// TODO check
-func (f *Janusgraph) ListThings(ctx context.Context, first int, offset int, keyID strfmt.UUID, wheres []*connutils.WhereQuery, response *models.ThingsListResponse) error {
-	k := kind.THING_KIND
-
-	if len(wheres) > 0 {
-		return errors.New("Wheres are not supported in ListThings")
-	}
-
-	q := gremlin.G.V().
-		HasString(PROP_KIND, k.Name()).
-		Range(offset, first).
-		Values([]string{PROP_UUID})
-
-	result, err := f.client.Execute(q)
-
-	if err != nil {
-		return err
-	}
-
+func (j *Janusgraph) ListThings(ctx context.Context, first int, offset int, keyID strfmt.UUID, wheres []*connutils.WhereQuery, response *models.ThingsListResponse) error {
 	response.TotalResults = 0
 	response.Things = make([]*models.ThingGetResponse, 0)
 
-	// Get the UUIDs from the first query.
-	UUIDs := result.AssertStringSlice()
-
-	for _, uuid := range UUIDs {
+	return j.listClass(kind.THING_KIND, first, offset, keyID, wheres, func(uuid strfmt.UUID) {
 		var thing_response models.ThingGetResponse
-		err := f.GetThing(ctx, strfmt.UUID(uuid), &thing_response)
+		err := j.GetThing(ctx, uuid, &thing_response)
 
 		if err == nil {
 			response.TotalResults += 1
 			response.Things = append(response.Things, &thing_response)
 		} else {
-			// skip silently; it's probably deleted.
+			// Silently ignorre the potentially removed things.
 		}
-	}
-
-	return nil
+	})
 }
 
 func (j *Janusgraph) UpdateThing(ctx context.Context, thing *models.Thing, UUID strfmt.UUID) error {
-	k := kind.THING_KIND
-
 	sanitizedClassName := schema.AssertValidClassName(thing.AtClass)
-	vertexLabel := j.state.getMappedClassName(sanitizedClassName)
-
-	// Base settings
-	q := gremlin.G.V().
-		HasString(PROP_KIND, k.Name()).
-		HasString(PROP_UUID, string(UUID)).
-		As("thing").
-		StringProperty(PROP_CLASS_ID, string(vertexLabel)).
-		StringProperty(PROP_AT_CONTEXT, thing.AtContext).
-		Int64Property(PROP_CREATION_TIME_UNIX, thing.CreationTimeUnix).
-		Int64Property(PROP_LAST_UPDATE_TIME_UNIX, thing.LastUpdateTimeUnix)
-
-	type expectedEdge struct {
-		PropertyName string
-		Type         string
-		Reference    string
-		Location     string
-	}
-
-	var expectedEdges []expectedEdge
-
-	thingSchema, schema_ok := thing.Schema.(map[string]interface{})
-	if schema_ok {
-		for propName, value := range thingSchema {
-			sanitziedPropertyName := schema.AssertValidPropertyName(propName)
-			janusPropertyName := string(j.state.getMappedPropertyName(sanitizedClassName, sanitziedPropertyName))
-
-			switch t := value.(type) {
-			case string:
-				q = q.StringProperty(janusPropertyName, t)
-			case int:
-				q = q.Int64Property(janusPropertyName, int64(t))
-			case int8:
-				q = q.Int64Property(janusPropertyName, int64(t))
-			case int16:
-				q = q.Int64Property(janusPropertyName, int64(t))
-			case int32:
-				q = q.Int64Property(janusPropertyName, int64(t))
-			case int64:
-				q = q.Int64Property(janusPropertyName, t)
-			case bool:
-				q = q.BoolProperty(janusPropertyName, t)
-			case float32:
-				q = q.Float64Property(janusPropertyName, float64(t))
-			case float64:
-				q = q.Float64Property(janusPropertyName, t)
-			case time.Time:
-				q = q.StringProperty(janusPropertyName, time.Time.String(t))
-			case *models.SingleRef:
-				// Postpone creation of edges
-				expectedEdges = append(expectedEdges, expectedEdge{
-					PropertyName: janusPropertyName,
-					Reference:    t.NrDollarCref.String(),
-					Type:         t.Type,
-					Location:     *t.LocationURL,
-				})
-			default:
-				j.messaging.ExitError(78, "The type "+reflect.TypeOf(value).String()+" is not supported for Thing properties.")
-			}
-		}
-	}
-
-	// Update all edges to all referened things.
-	// TODO: verify what to if we're not mentioning some reference? how should we remove such a reference?
-	for _, edge := range expectedEdges {
-		_ = edge
-		// First drop the edge
-		//	q = q.Optional(gremlin.Current().OutEWithLabel("thingEdge").HasString(PROPERTY_EDGE_LABEL, edge.PropertyName).Drop()).
-		//		AddE("thingEdge").
-		//		FromRef("thing").
-		//		ToQuery(gremlin.G.V().HasLabel(THING_LABEL).HasString("uuid", edge.Reference)).
-		//		StringProperty(PROPERTY_EDGE_LABEL, edge.PropertyName).
-		//		StringProperty("$cref", edge.Reference).
-		//		StringProperty("type", edge.Type).
-		//		StringProperty("locationUrl", edge.Location)
-	}
-
-	// Don't update the key.
-	// TODO verify that indeed this is the desired behaviour.
-
-	_, err := j.client.Execute(q)
-
-	return err
+	return j.updateClass(kind.THING_KIND, sanitizedClassName, UUID, thing.AtContext, thing.LastUpdateTimeUnix, thing.Schema)
 }
 
-func (f *Janusgraph) DeleteThing(ctx context.Context, thing *models.Thing, UUID strfmt.UUID) error {
-	k := kind.THING_KIND
-
-	q := gremlin.G.V().HasString(PROP_KIND, k.Name()).
-		HasString(PROP_UUID, string(UUID)).
-		Drop()
-
-	_, err := f.client.Execute(q)
-
-	return err
+func (j *Janusgraph) DeleteThing(ctx context.Context, thing *models.Thing, UUID strfmt.UUID) error {
+	return j.deleteClass(kind.THING_KIND, UUID)
 }
 
-func (f *Janusgraph) HistoryThing(ctx context.Context, UUID strfmt.UUID, history *models.ThingHistory) error {
+func (j *Janusgraph) HistoryThing(ctx context.Context, UUID strfmt.UUID, history *models.ThingHistory) error {
 	return nil
 }
 
-func (f *Janusgraph) MoveToHistoryThing(ctx context.Context, thing *models.Thing, UUID strfmt.UUID, deleted bool) error {
+func (j *Janusgraph) MoveToHistoryThing(ctx context.Context, thing *models.Thing, UUID strfmt.UUID, deleted bool) error {
 	return nil
-}
-
-func debug(result interface{}) {
-	j, _ := json.MarshalIndent(result, "", " ")
-	fmt.Printf("%v\n", string(j))
 }
