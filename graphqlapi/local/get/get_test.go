7e2c2ddb2554695e1dbcb603517aa77badadcdc5
Maarten Hoogendoorn <maarten@moretea.nl>
2018-11-24 16:49:44 +0100

    Parse out nested relations from GraphQL

--- graphqlapi/local/get/get_test.go
+++ graphqlapi/local/get/get_test.go
@@ -16,7 +16,7 @@ func TestSimpleFieldParamsOK(t *testing.T) {
 	expectedParams := &LocalGetClassParams{
 		Kind:       kind.ACTION_KIND,
 		ClassName:  "SomeAction",
-		Properties: []SelectProperty{{Name: "intField"}},
+		Properties: []SelectProperty{{Name: "intField", IsPrimitive: true}},
 	}
 
 	resolver.On("LocalGetClass", expectedParams).
@@ -33,7 +33,7 @@ func TestExtractIntField(t *testing.T) {
 	expectedParams := &LocalGetClassParams{
 		Kind:       kind.ACTION_KIND,
 		ClassName:  "SomeAction",
-		Properties: []SelectProperty{{Name: "intField"}},
+		Properties: []SelectProperty{{Name: "intField", IsPrimitive: true}},
 	}
 
 	resolver.On("LocalGetClass", expectedParams).
@@ -51,7 +51,7 @@ func TestExtractPagination(t *testing.T) {
 	expectedParams := &LocalGetClassParams{
 		Kind:       kind.ACTION_KIND,
 		ClassName:  "SomeAction",
-		Properties: []SelectProperty{{Name: "intField"}},
+		Properties: []SelectProperty{{Name: "intField", IsPrimitive: true}},
 		Pagination: &common_resolver.Pagination{
 			First: 10,
 			After: 20,
@@ -62,5 +62,54 @@ func TestExtractPagination(t *testing.T) {
 		Return(test_helper.EmptyListThunk(), nil).Once()
 
 	query := "{ Get { Actions { SomeAction(first:10, after: 20) { intField } } } }"
+	resolver.AssertResolve(t, query)
+}
+
+func TestGetRelation(t *testing.T) {
+	t.Parallel()
+
+	resolver := newMockResolver()
+
+	expectedParams := &LocalGetClassParams{
+		Kind:      kind.ACTION_KIND,
+		ClassName: "SomeAction",
+		Properties: []SelectProperty{
+			{
+				Name:        "HasAction",
+				IsPrimitive: false,
+				Refs: []SelectClass{
+					{
+						ClassName: "SomeAction",
+						RefProperties: []SelectProperty{
+							{
+								Name:        "intField",
+								IsPrimitive: true,
+							},
+							{
+								Name:        "HasAction",
+								IsPrimitive: false,
+								Refs: []SelectClass{
+									{
+										ClassName: "SomeAction",
+										RefProperties: []SelectProperty{
+											{
+												Name:        "intField",
+												IsPrimitive: true,
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	resolver.On("LocalGetClass", expectedParams).
+		Return(test_helper.EmptyListThunk(), nil).Once()
+
+	query := "{ Get { Actions { SomeAction { HasAction { ... on SomeAction { intField, HasAction { ... on SomeAction { intField } } } } } } } }"
 	resolver.AssertResolve(t, query)
 }
