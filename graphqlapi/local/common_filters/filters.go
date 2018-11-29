2a52035262c44af971d66888f383491c82801aed
Maarten Hoogendoorn <maarten@moretea.nl>
2018-11-29 12:06:06 +0100

    Init filters just once & enable to creat new set of filters for tests.

--- graphqlapi/local/common_filters/filters.go
+++ graphqlapi/local/common_filters/filters.go
@@ -3,69 +3,80 @@ package common_filters
 import (
 	"github.com/graphql-go/graphql"
 	"github.com/creativesoftwarefdn/weaviate/graphqlapi/descriptions"
+	"sync"
 )
 
+var sharedCommonFilters graphql.InputObjectConfigFieldMap
+var initFilter sync.Once
+
 // The filters common to Local->Get and Local->GetMeta queries.
+func Get() graphql.InputObjectConfigFieldMap {
+	initFilter.Do(func() {
+    sharedCommonFilters = BuildNew()
+  })
 
-func Build() graphql.InputObjectConfigFieldMap {
-	commonFilters := graphql.InputObjectConfigFieldMap{
-		"operator": &graphql.InputObjectFieldConfig{
-			Type: graphql.NewEnum(graphql.EnumConfig{
-				Name: "WhereOperatorEnum",
-				Values: graphql.EnumValueConfigMap{
-					"And":              &graphql.EnumValueConfig{},
-					"Or":               &graphql.EnumValueConfig{},
-					"Equal":            &graphql.EnumValueConfig{},
-					"Not":              &graphql.EnumValueConfig{},
-					"NotEqual":         &graphql.EnumValueConfig{},
-					"GreaterThan":      &graphql.EnumValueConfig{},
-					"GreaterThanEqual": &graphql.EnumValueConfig{},
-					"LessThan":         &graphql.EnumValueConfig{},
-					"LessThanEqual":    &graphql.EnumValueConfig{},
-				},
-				Description: descriptions.WhereOperatorEnumDesc,
-			}),
-			Description: descriptions.WhereOperatorDesc,
-		},
-		"path": &graphql.InputObjectFieldConfig{
-			Type:        graphql.NewList(graphql.String),
-			Description: descriptions.WherePathDesc,
-		},
-		"valueInt": &graphql.InputObjectFieldConfig{
-			Type:        graphql.Int,
-			Description: descriptions.WhereValueIntDesc,
-		},
-		"valueNumber": &graphql.InputObjectFieldConfig{
-			Type:        graphql.Float,
-			Description: descriptions.WhereValueNumberDesc,
-		},
-		"valueBoolean": &graphql.InputObjectFieldConfig{
-			Type:        graphql.Boolean,
-			Description: descriptions.WhereValueBooleanDesc,
-		},
-		"valueString": &graphql.InputObjectFieldConfig{
-			Type:        graphql.String,
-			Description: descriptions.WhereValueStringDesc,
-		},
-		"valueDate": &graphql.InputObjectFieldConfig{
-			Type:        graphql.String,
-			Description: descriptions.WhereValueStringDesc,
-		},
-	}
+  return sharedCommonFilters
+}
 
-	// Recurse into the same time.
-	commonFilters["operands"] = &graphql.InputObjectFieldConfig{
-		Description: descriptions.WhereOperandsDesc,
-		Type: graphql.NewList(graphql.NewInputObject(
-			graphql.InputObjectConfig{
-				Name:        "WhereOperandsInpObj",
-				Description: descriptions.WhereOperandsInpObjDesc,
-				Fields: (graphql.InputObjectConfigFieldMapThunk)(func() graphql.InputObjectConfigFieldMap {
-					return commonFilters
+func BuildNew() graphql.InputObjectConfigFieldMap {
+  commonFilters := graphql.InputObjectConfigFieldMap{
+			"operator": &graphql.InputObjectFieldConfig{
+				Type: graphql.NewEnum(graphql.EnumConfig{
+					Name: "WhereOperatorEnum",
+					Values: graphql.EnumValueConfigMap{
+						"And":              &graphql.EnumValueConfig{},
+						"Or":               &graphql.EnumValueConfig{},
+						"Equal":            &graphql.EnumValueConfig{},
+						"Not":              &graphql.EnumValueConfig{},
+						"NotEqual":         &graphql.EnumValueConfig{},
+						"GreaterThan":      &graphql.EnumValueConfig{},
+						"GreaterThanEqual": &graphql.EnumValueConfig{},
+						"LessThan":         &graphql.EnumValueConfig{},
+						"LessThanEqual":    &graphql.EnumValueConfig{},
+					},
+					Description: descriptions.WhereOperatorEnumDesc,
 				}),
+				Description: "Operator in the 'where' filter field, value is one of the 'WhereOperatorEnum' object",
 			},
-		)),
-	}
+			"path": &graphql.InputObjectFieldConfig{
+				Type:        graphql.NewList(graphql.String),
+				Description: descriptions.WherePathDesc,
+			},
+			"valueInt": &graphql.InputObjectFieldConfig{
+				Type:        graphql.Int,
+				Description: descriptions.WhereValueIntDesc,
+			},
+			"valueNumber": &graphql.InputObjectFieldConfig{
+				Type:        graphql.Float,
+				Description: descriptions.WhereValueNumberDesc,
+			},
+			"valueBoolean": &graphql.InputObjectFieldConfig{
+				Type:        graphql.Boolean,
+				Description: descriptions.WhereValueBooleanDesc,
+			},
+			"valueString": &graphql.InputObjectFieldConfig{
+				Type:        graphql.String,
+				Description: descriptions.WhereValueStringDesc,
+			},
+			"valueDate": &graphql.InputObjectFieldConfig{
+				Type:        graphql.String,
+				Description: descriptions.WhereValueStringDesc,
+			},
+		}
+
+		// Recurse into the same time.
+		commonFilters["operands"] = &graphql.InputObjectFieldConfig{
+			Description: descriptions.WhereOperandsDesc,
+			Type: graphql.NewList(graphql.NewInputObject(
+				graphql.InputObjectConfig{
+					Name:        "WhereOperandsInpObj",
+					Description: descriptions.WhereOperandsInpObjDesc,
+					Fields: (graphql.InputObjectConfigFieldMapThunk)(func() graphql.InputObjectConfigFieldMap {
+						return commonFilters
+					}),
+				},
+			)),
+		}
 
 	return commonFilters
 }
--- graphqlapi/local/get/get.go
+++ graphqlapi/local/get/get.go
@@ -67,7 +67,7 @@ func Build(dbSchema *schema.Schema) (*graphql.Field, error) {
 				Type: graphql.NewInputObject(
 					graphql.InputObjectConfig{
 						Name:        "WeaviateLocalGetWhereInpObj",
-						Fields:      common_filters.Build(),
+						Fields:      common_filters.Get(),
 						Description: descriptions.LocalGetWhereInpObjDesc,
 					},
 				),
@@ -137,6 +137,7 @@ func buildGetClass(dbSchema *schema.Schema, k kind.Kind, class *models.SemanticS
 				Description: descriptions.LocalGetClassUUIDDesc,
 				Type:        graphql.String,
 				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					// TODO
 					fmt.Printf("WHOOPTYDOO uuid\n")
 					return "uuid", nil
 				},
