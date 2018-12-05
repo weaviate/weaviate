81fa1348dcc4e806214f71b5eea5fff3caa44404
Maarten Hoogendoorn <maarten@moretea.nl>
2018-12-05 11:39:10 +0100

    framework for resolving GetMeta

---
+++ database/connectors/janusgraph/resolver_get_meta.go
@@ -0,0 +1,10 @@
+package janusgraph
+
+import (
+	graphql_local_get_meta "github.com/creativesoftwarefdn/weaviate/graphqlapi/local/get_meta"
+)
+
+func (j *Janusgraph) LocalGetMeta(params *graphql_local_get_meta.LocalGetMetaParams) (func() interface{}, error) {
+  return nil, nil
+}
+
