97dd6fa876635646d51d340dc21bb63c8fa4fa0a
Etienne Dilocker <etienne@dilocker.de>
2018-12-06 09:23:17 +0100

    gh-602 get peer by name from network
    This assumes that "name" is a way to uniquely identify a peer.
    @bobvanluijt @laura-ham - Can I make this assumption?

---
+++ network/p2p_network_test.go
@@ -0,0 +1,62 @@
+package network
+
+import (
+	"testing"
+
+	"github.com/go-openapi/strfmt"
+)
+
+func TestGetExistingPeer(t *testing.T) {
+	peer := Peer{
+		Id:   strfmt.UUID("some-id"),
+		Name: "best-peer",
+		URI:  "http://best-peer.com",
+	}
+
+	subject := network{
+		peers: []Peer{peer},
+	}
+
+	actual, err := subject.GetPeerByName("best-peer")
+
+	t.Run("should not error", func(t *testing.T) {
+		if err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("should return correct peer", func(t *testing.T) {
+		if actual.Id != peer.Id {
+			t.Errorf("%s does not match, wanted %s, gut got %s", "Id", peer.Id, actual.Id)
+		}
+
+		if actual.Name != peer.Name {
+			t.Errorf("%s does not match, wanted %s, gut got %s", "Name", peer.Name, actual.Name)
+		}
+
+		if actual.URI != peer.URI {
+			t.Errorf("%s does not match, wanted %s, gut got %s", "URI", peer.URI, actual.URI)
+		}
+	})
+
+}
+
+func TestGetWrongPeer(t *testing.T) {
+	peer := Peer{
+		Id:   strfmt.UUID("some-id"),
+		Name: "best-peer",
+		URI:  "http://best-peer.com",
+	}
+
+	subject := network{
+		peers: []Peer{peer},
+	}
+
+	_, err := subject.GetPeerByName("worst-peer")
+
+	t.Run("should error with ErrPeerNotFound", func(t *testing.T) {
+		if err != ErrPeerNotFound {
+			t.Errorf("expected peer not found error, but got %s", err)
+		}
+	})
+}
