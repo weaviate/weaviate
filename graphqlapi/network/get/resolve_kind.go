cff9a0438bc8e26d30439fd0a6329cd4163d65c1
Etienne Dilocker <etienne@dilocker.de>
2018-12-10 15:30:08 +0100

    gh-602 resolve Network.Get for actions as well

--- graphqlapi/network/get/resolve_kind.go
+++ graphqlapi/network/get/resolve_kind.go
@@ -3,20 +3,42 @@ package network_get
 import (
 	"fmt"
 
+	"github.com/creativesoftwarefdn/weaviate/database/schema/kind"
 	"github.com/graphql-go/graphql"
 )
 
-func ResolveThing(p graphql.ResolveParams) (interface{}, error) {
-	firstLevel, ok := p.Source.(map[string]interface{})
-	if !ok {
-		return nil, fmt.Errorf("expected source to be map[string]interface{}, was %#v", p.Source)
-	}
+func resolveKind(k kind.Kind) func(p graphql.ResolveParams) (interface{}, error) {
+	kindField := kindNameInResult(k)
 
-	secondLevel, ok := firstLevel["Things"].(map[string]interface{})
-	if !ok {
-		return nil, fmt.Errorf("expected source to be map[string]map[string][]interface{}, was %#v", firstLevel["Things"])
-	}
+	return func(p graphql.ResolveParams) (interface{}, error) {
+		firstLevel, ok := p.Source.(map[string]interface{})
+		if !ok {
+			return nil, fmt.Errorf("expected source to be map[string]interface{}, was %#v", p.Source)
+		}
 
-	fmt.Printf("about to return %v", secondLevel[p.Info.FieldName])
-	return secondLevel[p.Info.FieldName], nil
+		secondLevel, ok := firstLevel[kindField].(map[string]interface{})
+		if !ok {
+			return nil, fmt.Errorf("expected source to be map[string]map[string][]interface{}, was %#v", firstLevel[kindField])
+		}
+
+		fmt.Printf("about to return %v", secondLevel[p.Info.FieldName])
+		return secondLevel[p.Info.FieldName], nil
+	}
+}
+
+// ResolveAction as part of a network query: Network -> Get -> Action -> <nameOfAction>
+var ResolveAction = resolveKind(kind.ACTION_KIND)
+
+// ResolveThing as part of a network query: Network -> Get -> Thing -> <nameOfThing>
+var ResolveThing = resolveKind(kind.THING_KIND)
+
+func kindNameInResult(k kind.Kind) string {
+	switch k {
+	case kind.THING_KIND:
+		return "Things"
+	case kind.ACTION_KIND:
+		return "Actions"
+	default:
+		return ""
+	}
 }
