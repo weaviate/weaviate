1e627a868fe56197df7fa5e2f2ec11c5897bdee9
Etienne Dilocker <etienne@dilocker.de>
2018-12-11 17:35:57 +0100

    gh-602 extract filters also for multiple instances and multiple filters
    Initially I made the assumption it might be easier to leave the untyped
    arguments untyped as we only care about their json represnetation in the
    end and this way we could avoid knowing about all possible filter
    fields. This has however turned rather messy and I'm most likely going
    to clean this up next.

--- graphqlapi/network/get/filters_test.go
+++ graphqlapi/network/get/filters_test.go
@@ -52,5 +52,96 @@ func TestSingleInstanceWithSingleFilter(t *testing.T) {
 	if err != nil {
 		t.Errorf("expected FiltersForNetworkInstances not to error, but got %s", err)
 	}
+}
 
+func TestTwoInstancesWithSingleFilterEach(t *testing.T) {
+	args := map[string]interface{}{
+		"where": map[string]interface{}{
+			"operator": "And",
+			"operands": []map[string]interface{}{{
+				"path":     []string{"weaviateB", "Things", "City", "population"},
+				"operator": "GreaterThan",
+				"valueInt": 1000000,
+			}, {
+				"path":     []string{"weaviateC", "Things", "Airports", "capacity"},
+				"operator": "LessThan",
+				"valueInt": 60000,
+			}},
+		},
+	}
+	expectedResult := FiltersPerInstance{
+		"weaviateB": map[string]interface{}{
+			"where": map[string]interface{}{
+				"operator": "And",
+				"operands": []map[string]interface{}{{
+					"path":     []string{"Things", "City", "population"},
+					"operator": "GreaterThan",
+					"valueInt": 1000000,
+				}},
+			},
+		},
+		"weaviateC": map[string]interface{}{
+			"where": map[string]interface{}{
+				"operator": "And",
+				"operands": []map[string]interface{}{{
+					"path":     []string{"Things", "Airports", "capacity"},
+					"operator": "LessThan",
+					"valueInt": 60000,
+				}},
+			},
+		},
+	}
+
+	result, err := FiltersForNetworkInstances(args)
+
+	if !reflect.DeepEqual(expectedResult, result) {
+		t.Errorf("expected result to be \n\n%#v\n\n, but got \n\n%#v\n\n", expectedResult, result)
+	}
+
+	if err != nil {
+		t.Errorf("expected FiltersForNetworkInstances not to error, but got %s", err)
+	}
+}
+
+func TestOneInstanceWithTwoFilters(t *testing.T) {
+	args := map[string]interface{}{
+		"where": map[string]interface{}{
+			"operator": "And",
+			"operands": []map[string]interface{}{{
+				"path":     []string{"weaviateB", "Things", "City", "population"},
+				"operator": "GreaterThan",
+				"valueInt": 1000000,
+			}, {
+				"path":        []string{"weaviateB", "Things", "City", "name"},
+				"operator":    "NotEqual",
+				"valueString": "Berlin",
+			}},
+		},
+	}
+	expectedResult := FiltersPerInstance{
+		"weaviateB": map[string]interface{}{
+			"where": map[string]interface{}{
+				"operator": "And",
+				"operands": []map[string]interface{}{{
+					"path":     []string{"Things", "City", "population"},
+					"operator": "GreaterThan",
+					"valueInt": 1000000,
+				}, {
+					"path":        []string{"Things", "City", "name"},
+					"operator":    "NotEqual",
+					"valueString": "Berlin",
+				}},
+			},
+		},
+	}
+
+	result, err := FiltersForNetworkInstances(args)
+
+	if !reflect.DeepEqual(expectedResult, result) {
+		t.Errorf("expected result to be \n\n%#v\n\n, but got \n\n%#v\n\n", expectedResult, result)
+	}
+
+	if err != nil {
+		t.Errorf("expected FiltersForNetworkInstances not to error, but got %s", err)
+	}
 }
