202999396e3b5e76da234c9ce662b441863cbadd
bobvanluijt <bob@kubrickolo.gy>
2018-12-12 15:23:54 +0100

    gh-622: Silence output

--- tools/download_latest_contextionary.sh
+++ tools/download_latest_contextionary.sh
@@ -9,10 +9,14 @@ mkdir -p ./contextionary
 
 # Download the latest files and remove old ones
 for SINGLEEXT in idx knn; do
-    rm -f ./contextionary/contextionary.$SINGLEEXT && wget -O ./contextionary/contextionary.$SINGLEEXT https://contextionary.creativesoftwarefdn.org/$(curl -sS https://contextionary.creativesoftwarefdn.org/contextionary.json | jq -r ".latestVersion")/en/contextionary.$SINGLEEXT && echo "$SINGLEEXT file = done" &
+    echo "Start Downloading $SINGLEEXT" && \
+    rm -f ./contextionary/contextionary.$SINGLEEXT && \
+    wget --quiet -O ./contextionary/contextionary.$SINGLEEXT https://contextionary.creativesoftwarefdn.org/$(curl -sS https://contextionary.creativesoftwarefdn.org/contextionary.json | jq -r ".latestVersion")/en/contextionary.$SINGLEEXT && \
+    echo "$SINGLEEXT file = done" &
 done 
 
 # Wiat to finish download
 wait
 
 echo "Done downloading the open source contextionary."
+exit 0
