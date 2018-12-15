f52df21d70fcfa470aa2e023bd025a9023056125
Etienne Dilocker <etienne@dilocker.de>
2018-12-16 00:29:10 +0100

    gh-609: compare peer list to previous and note change
    A very simple "event" model was introduce where each peer gets annotated
    with how it differed from the previous cycle. This can then be used to
    trigger the download of a new schema when necessary.

---
+++ network/peer_change_test.go
@@ -0,0 +1,52 @@
+package network
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestEmptyPeersToSinglePeer(t *testing.T) {
+	before := Peers{}
+	after := Peers{{ID: "foo"}}
+
+	actual := PeersDiff(before, after)
+	expected := Peers{{
+		ID:         "foo",
+		LastChange: NewlyAdded,
+	}}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("expected new peer list to be %#v, but was %#v", expected, actual)
+	}
+}
+
+func TestSamePeersAsBefore(t *testing.T) {
+	before := Peers{{ID: "foo"}}
+	after := Peers{{ID: "foo"}}
+
+	actual := PeersDiff(before, after)
+	expected := Peers{{
+		ID:         "foo",
+		LastChange: NoChange,
+	}}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("expected new peer list to be %#v, but was %#v", expected, actual)
+	}
+}
+
+func TestPeerUpdatedSchema(t *testing.T) {
+	before := Peers{{ID: "foo", SchemaHash: "oldhash"}}
+	after := Peers{{ID: "foo", SchemaHash: "newhash"}}
+
+	actual := PeersDiff(before, after)
+	expected := Peers{{
+		ID:         "foo",
+		LastChange: SchemaChange,
+		SchemaHash: "newhash",
+	}}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("expected new peer list to be %#v, but was %#v", expected, actual)
+	}
+}
