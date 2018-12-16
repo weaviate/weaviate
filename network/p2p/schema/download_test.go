de8af924780024ca897d1c7e1b14ddb9e0d48970
Etienne Dilocker <etienne@dilocker.de>
2018-12-16 13:29:01 +0100

    gh-609: add simple schema downloader based on peer
    it can extract all the info from a network.Peer and download the schema
    there. Currently ignores Auth.
    
    This should be used in the pinging cycle, where we can have many
    downloads concurrently.

---
+++ network/p2p/schema/download_test.go
@@ -0,0 +1,121 @@
+package schema
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/creativesoftwarefdn/weaviate/database/schema"
+	"github.com/creativesoftwarefdn/weaviate/models"
+	"github.com/creativesoftwarefdn/weaviate/network"
+	"github.com/go-openapi/strfmt"
+	"github.com/magiconair/properties/assert"
+)
+
+func TestDownloadSchemaFromPeer(t *testing.T) {
+	var (
+		result     schema.Schema
+		err        error
+		peerServer *httptest.Server
+		peer       network.Peer
+	)
+
+	arrange := func(matchers ...requestMatcher) {
+		peerServer = fakePeerSchemaEndpoint(t, matchers...)
+		peer = network.Peer{
+			Name: "bestPeer",
+			URI:  strfmt.URI(peerServer.URL),
+		}
+	}
+
+	act := func() {
+		result, err = download(peer)
+	}
+
+	cleanUp := func() {
+		peerServer.Close()
+	}
+
+	t.Run("no error should occur", func(t *testing.T) {
+		arrange()
+		act()
+		cleanUp()
+		if err != nil {
+			t.Fatalf("expected no error, but got %s", err)
+		}
+	})
+
+	t.Run("result should match the schema", func(t *testing.T) {
+		arrange()
+		act()
+		cleanUp()
+		assert.Equal(t, result, sampleSchema(), "result should match the schema")
+	})
+
+	t.Run("peer should be called", func(t *testing.T) {
+		called := false
+		matcher := func(t *testing.T, r *http.Request) {
+			called = true
+		}
+		arrange(matcher)
+		act()
+
+		if called == false {
+			t.Error("handler was never called")
+		}
+
+		cleanUp()
+	})
+
+	t.Run("request should be GET /schema", func(t *testing.T) {
+		matcher := func(t *testing.T, r *http.Request) {
+			if r.Method != "GET" {
+				t.Fatalf("expected method to be GET, but was %s", r.Method)
+			}
+
+			expectedURI := "/weaviate/v1/schema"
+			if r.RequestURI != expectedURI {
+				t.Fatalf("expected uri to be %s, but was %s",
+					expectedURI, r.RequestURI)
+			}
+		}
+		arrange(matcher)
+		act()
+
+		cleanUp()
+	})
+
+}
+
+type requestMatcher func(t *testing.T, r *http.Request)
+
+func fakePeerSchemaEndpoint(t *testing.T, matchers ...requestMatcher) *httptest.Server {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		for _, matcher := range matchers {
+			matcher(t, r)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(sampleSchema())
+	}))
+	return ts
+}
+
+func sampleSchema() schema.Schema {
+	return schema.Schema{
+		Actions: &models.SemanticSchema{
+			Classes: []*models.SemanticSchemaClass{
+				&models.SemanticSchemaClass{
+					Class: "Flight",
+				},
+			},
+		},
+		Things: &models.SemanticSchema{
+			Classes: []*models.SemanticSchemaClass{
+				&models.SemanticSchemaClass{
+					Class: "Airplane",
+				},
+			},
+		},
+	}
+}
