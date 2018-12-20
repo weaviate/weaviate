57dec8c0dca46a02470fe82fe73c349b70b56787
Etienne Dilocker <etienne@dilocker.de>
2018-12-20 17:12:15 +0100

    gh-630: sketch out unit test structure for graphql builder
    Right now we have an issue where duplicate schemas can lead to duplicate
    object, as we found out after moving the where filter.
    
    It's very hard right now to get feedback for that, as you basically have
    to run the entire acceptance suite, with actual peers. A unit test
    should be much easier to provide feedback on that. However, we first
    need the changes from PR #649 becasue they substantially change the
    method signarture with regards to what peers are. Previously there were
    just a []string and afterwards they'll be a complex object with
    individual schemas attached.

---
+++ graphqlapi/schema_test.go
@@ -0,0 +1,9 @@
+package graphqlapi
+
+// Test with only local schema
+
+// Test with one remote peer with independent schema
+
+// Test with one remote peer with same schema
+
+// Test with with two peers with different schema than local, but same on both remotes
