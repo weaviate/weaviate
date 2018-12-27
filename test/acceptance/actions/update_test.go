68a4f0b0be47110592b91994e033c89012515704
Etienne Dilocker <etienne@dilocker.de>
2018-12-27 15:41:57 +0100

    gh-658: adhere to 'localhost' convention in local ref in acc test
    As mentioned previously we are now enforcing the convention that a local
    reference must always have 'localhost' as the location URL. Previously
    it was also allowed to match the hostname under which the weaviate
    instance would start up. So this means previosly 127.0.0.1 was also
    allowed for a local reference.
    
    We still had some test that used the exact host, i.e. 127.0.0.1. This
    commit should replace all of them with localhost and fix the broken
    tests.

--- test/acceptance/actions/update_test.go
+++ test/acceptance/actions/update_test.go
@@ -15,11 +15,12 @@ package test
 // Acceptance tests for actions
 
 import (
+	"testing"
+
 	"github.com/creativesoftwarefdn/weaviate/client/actions"
 	"github.com/creativesoftwarefdn/weaviate/models"
 	"github.com/creativesoftwarefdn/weaviate/test/acceptance/helper"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func TestCanUpdateActionSetNumber(t *testing.T) {
