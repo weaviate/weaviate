16f5d132ca207206752601bbe1bdd4d4f1d1b2dd
Etienne Dilocker <etienne@dilocker.de>
2018-12-28 18:23:32 +0100

    gh-659: (refactor) split out graphql Local.Get.Kind.Class
    These are the places I will have to touch to also allow a
    Network-Cross-Ref in. The logic that we have here is relatively simple,
    but it was quite hard to understand because methods were a hundred lines
    long.
    
    This commit splits out the seperate concerns into different methods (and
    files). This will allow us to build the Network-Cross-Refs into graphql soon.

--- graphqlapi/local/get/get.go
+++ graphqlapi/local/get/get.go
@@ -10,6 +10,7 @@
  * See www.creativesoftwarefdn.org for details
  * Contact: @CreativeSofwFdn / bob@kub.design
  */
+
 package local_get
 
 import (
@@ -18,21 +19,18 @@ import (
 
 	"github.com/creativesoftwarefdn/weaviate/database/schema"
 	"github.com/creativesoftwarefdn/weaviate/database/schema/kind"
-	common "github.com/creativesoftwarefdn/weaviate/graphqlapi/common_resolver"
 	"github.com/creativesoftwarefdn/weaviate/graphqlapi/descriptions"
 	"github.com/creativesoftwarefdn/weaviate/graphqlapi/local/common_filters"
 	"github.com/creativesoftwarefdn/weaviate/models"
-	"github.com/creativesoftwarefdn/weaviate/network/crossrefs"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/graphql-go/graphql"
-	graphql_ast "github.com/graphql-go/graphql/language/ast"
 )
 
+// Build the Local.Get part of the graphql tree
 func Build(dbSchema *schema.Schema) (*graphql.Field, error) {
 	getKinds := graphql.Fields{}
 
 	if len(dbSchema.Actions.Classes) == 0 && len(dbSchema.Things.Classes) == 0 {
-		return nil, fmt.Errorf("There are no Actions or Things classes defined yet.")
+		return nil, fmt.Errorf("there are no Actions or Things classes defined yet")
 	}
 
 	knownClasses := map[string]*graphql.Object{}
@@ -103,17 +101,10 @@ func Build(dbSchema *schema.Schema) (*graphql.Field, error) {
 // Builds the classes below a Local -> Get -> (k kind.Kind)
 func buildGetClasses(dbSchema *schema.Schema, k kind.Kind, semanticSchema *models.SemanticSchema, knownClasses *map[string]*graphql.Object) (*graphql.Object, error) {
 	classFields := graphql.Fields{}
-
-	var kindName string
-	switch k {
-	case kind.THING_KIND:
-		kindName = "Thing"
-	case kind.ACTION_KIND:
-		kindName = "Action"
-	}
+	kindName := strings.Title(k.Name())
 
 	for _, class := range semanticSchema.Classes {
-		classField, err := buildGetClass(dbSchema, k, kindName, class, knownClasses)
+		classField, err := buildGetClass(dbSchema, k, class, knownClasses)
 		if err != nil {
 			return nil, fmt.Errorf("Could not build class for %s", class.Class)
 		}
@@ -127,230 +118,4 @@ func buildGetClasses(dbSchema *schema.Schema, k kind.Kind, semanticSchema *model
 	})
 
 	return classes, nil
-}
-
-// Build a single class in Local -> Get -> (k kind.Kind) -> (models.SemanticSchemaClass)
-func buildGetClass(dbSchema *schema.Schema, k kind.Kind, kindName string, class *models.SemanticSchemaClass, knownClasses *map[string]*graphql.Object) (*graphql.Field, error) {
-	classObject := graphql.NewObject(graphql.ObjectConfig{
-		Name: class.Class,
-		Fields: (graphql.FieldsThunk)(func() graphql.Fields {
-			classProperties := graphql.Fields{}
-
-			classProperties["uuid"] = &graphql.Field{
-				Description: descriptions.LocalGetClassUUIDDesc,
-				Type:        graphql.String,
-			}
-
-			for _, property := range class.Properties {
-				propertyType, err := dbSchema.FindPropertyDataType(property.AtDataType)
-				if err != nil {
-					// We can't return an error in this FieldsThunk function, so we need to panic
-					panic(fmt.Sprintf("buildGetClass: wrong propertyType for %s.%s.%s; %s", k.Name(), class.Class, property.Name, err.Error()))
-				}
-
-				var propertyField *graphql.Field
-
-				if propertyType.IsPrimitive() {
-					switch propertyType.AsPrimitive() {
-
-					case schema.DataTypeString:
-						propertyField = &graphql.Field{
-							Description: property.Description,
-							Type:        graphql.String,
-						}
-					case schema.DataTypeText:
-						propertyField = &graphql.Field{
-							Description: property.Description,
-							Type:        graphql.String,
-						}
-					case schema.DataTypeInt:
-						propertyField = &graphql.Field{
-							Description: property.Description,
-							Type:        graphql.Int,
-						}
-					case schema.DataTypeNumber:
-						propertyField = &graphql.Field{
-							Description: property.Description,
-							Type:        graphql.Float,
-						}
-					case schema.DataTypeBoolean:
-						propertyField = &graphql.Field{
-							Description: property.Description,
-							Type:        graphql.Boolean,
-						}
-					case schema.DataTypeDate:
-						propertyField = &graphql.Field{
-							Description: property.Description,
-							Type:        graphql.String, // String since no graphql date datatype exists
-						}
-					default:
-						panic(fmt.Sprintf("buildGetClass: unknown primitive type for %s.%s.%s; %s", k.Name(), class.Class, property.Name, propertyType.AsPrimitive()))
-					}
-
-					propertyField.Name = property.Name
-					classProperties[property.Name] = propertyField
-				} else {
-					// This is a reference
-					refClasses := propertyType.Classes()
-					propertyName := strings.Title(property.Name)
-					dataTypeClasses := []*graphql.Object{}
-
-					for _, refClassName := range refClasses {
-						refClass, ok := (*knownClasses)[string(refClassName)]
-
-						if crossrefs.ValidClassName(string(refClassName)) {
-							// simply ignore all network cross-refs for now
-							continue
-						}
-
-						if !ok {
-							panic(fmt.Sprintf("buildGetClass: unknown referenced class type for %s.%s.%s; %s", k.Name(), class.Class, property.Name, refClassName))
-						}
-
-						dataTypeClasses = append(dataTypeClasses, refClass)
-					}
-
-					classUnion := graphql.NewUnion(graphql.UnionConfig{
-						Name:  fmt.Sprintf("%s%s%s", class.Class, propertyName, "Obj"),
-						Types: dataTypeClasses,
-						ResolveType: func(p graphql.ResolveTypeParams) *graphql.Object {
-							// TODO: inspect type of result.
-							return (*knownClasses)["City"]
-						},
-						Description: property.Description,
-					})
-
-					// TODO: Check cardinality
-
-					classProperties[propertyName] = &graphql.Field{
-						Type:        classUnion,
-						Description: property.Description,
-					}
-				}
-			}
-
-			return classProperties
-		}),
-		Description: class.Description,
-	})
-
-	(*knownClasses)[class.Class] = classObject
-
-	classField := graphql.Field{
-		Type:        graphql.NewList(classObject),
-		Description: class.Description,
-		Args: graphql.FieldConfigArgument{
-			"first": &graphql.ArgumentConfig{
-				Description: descriptions.FirstDesc,
-				Type:        graphql.Int,
-			},
-			"after": &graphql.ArgumentConfig{
-				Description: descriptions.AfterDesc,
-				Type:        graphql.Int,
-			},
-			"where": &graphql.ArgumentConfig{
-				Description: descriptions.LocalGetWhereDesc,
-				Type: graphql.NewInputObject(
-					graphql.InputObjectConfig{
-						Name:        fmt.Sprintf("WeaviateLocalGet%ss%sWhereInpObj", kindName, class.Class),
-						Fields:      common_filters.BuildNew(fmt.Sprintf("WeaviateLocalGet%ss%s", kindName, class.Class)),
-						Description: descriptions.LocalGetWhereInpObjDesc,
-					},
-				),
-			},
-		},
-		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-			fmt.Printf("- thing class (supposed to extract pagination, now return nil)\n")
-			filtersAndResolver := p.Source.(*filtersAndResolver)
-
-			pagination, err := common.ExtractPaginationFromArgs(p.Args)
-			if err != nil {
-				return nil, err
-			}
-
-			// There can only be exactly one graphql_ast.Field; it is the class name.
-			if len(p.Info.FieldASTs) != 1 {
-				panic("Only one Field expected here")
-			}
-
-			selectionsOfClass := p.Info.FieldASTs[0].SelectionSet
-			properties, err := extractProperties(selectionsOfClass)
-			if err != nil {
-				return nil, err
-			}
-
-			fmt.Print("\n\n\n\n\n")
-			spew.Dump(properties)
-			fmt.Print("\n\n\n\n\n")
-
-			params := LocalGetClassParams{
-				Filters:    filtersAndResolver.filters,
-				Kind:       k,
-				ClassName:  class.Class,
-				Pagination: pagination,
-				Properties: properties,
-			}
-
-			promise, err := filtersAndResolver.resolver.LocalGetClass(&params)
-			return promise, err
-		},
-	}
-
-	return &classField, nil
-}
-
-func extractProperties(selections *graphql_ast.SelectionSet) ([]SelectProperty, error) {
-	// debugFieldAsts(fieldASTs)
-	var properties []SelectProperty
-
-	for _, selection := range selections.Selections {
-		field := selection.(*graphql_ast.Field)
-		name := field.Name.Value
-		property := SelectProperty{Name: name}
-
-		property.IsPrimitive = (field.SelectionSet == nil)
-
-		if !property.IsPrimitive {
-			// We can interpret this property in different ways
-			for _, subSelection := range field.SelectionSet.Selections {
-				// Is it a field with the name __typename?
-				subsectionField, ok := subSelection.(*graphql_ast.Field)
-				if ok {
-					if subsectionField.Name.Value == "__typename" {
-						property.IncludeTypeName = true
-						continue
-					} else {
-						return nil, fmt.Errorf("Expected a InlineFragment, not a '%s' field ", subsectionField.Name.Value)
-					}
-				}
-
-				// Otherwise these _must_ be inline fragments
-				fragment, ok := subSelection.(*graphql_ast.InlineFragment)
-				if !ok {
-					return nil, fmt.Errorf("Expected a InlineFragment; you need to specify as which type you want to retrieve a reference %#v", subSelection)
-				}
-
-				var className schema.ClassName
-				var err error
-				err, className = schema.ValidateClassName(fragment.TypeCondition.Name.Value)
-				if err != nil {
-					return nil, fmt.Errorf("The inline fragment type name '%s' is not a valid class name.", fragment.TypeCondition.Name.Value)
-				}
-
-				subProperties, err := extractProperties(fragment.SelectionSet)
-				if err != nil {
-					return nil, err
-				}
-
-				property.Refs = append(property.Refs, SelectClass{
-					ClassName:     string(className),
-					RefProperties: subProperties,
-				})
-			}
-		}
-
-		properties = append(properties, property)
-	}
-
-	return properties, nil
 }
---
+++ graphqlapi/local/get/get_kind_class.go
@@ -0,0 +1,274 @@
+package local_get
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/creativesoftwarefdn/weaviate/database/schema"
+	"github.com/creativesoftwarefdn/weaviate/database/schema/kind"
+	common "github.com/creativesoftwarefdn/weaviate/graphqlapi/common_resolver"
+	"github.com/creativesoftwarefdn/weaviate/graphqlapi/descriptions"
+	"github.com/creativesoftwarefdn/weaviate/graphqlapi/local/common_filters"
+	"github.com/creativesoftwarefdn/weaviate/models"
+	"github.com/creativesoftwarefdn/weaviate/network/crossrefs"
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/graphql/language/ast"
+)
+
+// Build a single class in Local -> Get -> (k kind.Kind) -> (models.SemanticSchemaClass)
+func buildGetClass(dbSchema *schema.Schema, k kind.Kind, class *models.SemanticSchemaClass, knownClasses *map[string]*graphql.Object) (*graphql.Field, error) {
+	classObject := buildGetClassObject(k.Name(), class, dbSchema, knownClasses)
+	(*knownClasses)[class.Class] = classObject
+	classField := buildGetClassField(classObject, k, class)
+	return &classField, nil
+}
+
+func buildGetClassObject(kindName string, class *models.SemanticSchemaClass, dbSchema *schema.Schema,
+	knownClasses *map[string]*graphql.Object) *graphql.Object {
+	return graphql.NewObject(graphql.ObjectConfig{
+		Name: class.Class,
+		Fields: (graphql.FieldsThunk)(func() graphql.Fields {
+			classProperties := graphql.Fields{}
+
+			classProperties["uuid"] = &graphql.Field{
+				Description: descriptions.LocalGetClassUUIDDesc,
+				Type:        graphql.String,
+			}
+
+			for _, property := range class.Properties {
+				propertyType, err := dbSchema.FindPropertyDataType(property.AtDataType)
+				if err != nil {
+					// We can't return an error in this FieldsThunk function, so we need to panic
+					panic(fmt.Sprintf("buildGetClass: wrong propertyType for %s.%s.%s; %s",
+						kindName, class.Class, property.Name, err.Error()))
+				}
+
+				var field *graphql.Field
+				var fieldName string
+				if propertyType.IsPrimitive() {
+					fieldName = property.Name
+					field = buildPrimitiveField(propertyType, property, kindName, class.Class)
+				} else {
+					// uppercase key because it's a reference
+					fieldName = strings.Title(property.Name)
+					field = buildReferenceField(propertyType, property, kindName, class.Class, knownClasses)
+				}
+				classProperties[fieldName] = field
+			}
+
+			return classProperties
+		}),
+		Description: class.Description,
+	})
+}
+
+func buildPrimitiveField(propertyType schema.PropertyDataType,
+	property *models.SemanticSchemaClassProperty, kindName, className string) *graphql.Field {
+	switch propertyType.AsPrimitive() {
+	case schema.DataTypeString:
+		return &graphql.Field{
+			Description: property.Description,
+			Name:        property.Name,
+			Type:        graphql.String,
+		}
+	case schema.DataTypeText:
+		return &graphql.Field{
+			Description: property.Description,
+			Name:        property.Name,
+			Type:        graphql.String,
+		}
+	case schema.DataTypeInt:
+		return &graphql.Field{
+			Description: property.Description,
+			Name:        property.Name,
+			Type:        graphql.Int,
+		}
+	case schema.DataTypeNumber:
+		return &graphql.Field{
+			Description: property.Description,
+			Name:        property.Name,
+			Type:        graphql.Float,
+		}
+	case schema.DataTypeBoolean:
+		return &graphql.Field{
+			Description: property.Description,
+			Name:        property.Name,
+			Type:        graphql.Boolean,
+		}
+	case schema.DataTypeDate:
+		return &graphql.Field{
+			Description: property.Description,
+			Name:        property.Name,
+			Type:        graphql.String, // String since no graphql date datatype exists
+		}
+	default:
+		panic(fmt.Sprintf("buildGetClass: unknown primitive type for %s.%s.%s; %s",
+			kindName, className, property.Name, propertyType.AsPrimitive()))
+	}
+}
+
+func buildReferenceField(propertyType schema.PropertyDataType,
+	property *models.SemanticSchemaClassProperty, kindName, className string,
+	knownClasses *map[string]*graphql.Object) *graphql.Field {
+	refClasses := propertyType.Classes()
+	propertyName := strings.Title(property.Name)
+	dataTypeClasses := []*graphql.Object{}
+
+	for _, refClassName := range refClasses {
+		if crossrefs.ValidClassName(string(refClassName)) {
+			// simply ignore all network cross-refs for now
+			// fmt.Printf("\n\n\nhave nttwork ref %s\n\n\n", string(refClassName))
+			// spew.Dump(knownClasses)
+			continue
+		}
+
+		refClass, ok := (*knownClasses)[string(refClassName)]
+		if !ok {
+			panic(fmt.Sprintf("buildGetClass: unknown referenced class type for %s.%s.%s; %s",
+				kindName, className, property.Name, refClassName))
+		}
+
+		dataTypeClasses = append(dataTypeClasses, refClass)
+	}
+
+	classUnion := graphql.NewUnion(graphql.UnionConfig{
+		Name:  fmt.Sprintf("%s%s%s", className, propertyName, "Obj"),
+		Types: dataTypeClasses,
+		ResolveType: func(p graphql.ResolveTypeParams) *graphql.Object {
+			// TODO: inspect type of result.
+			return (*knownClasses)["City"]
+		},
+		Description: property.Description,
+	})
+
+	// TODO: Check cardinality
+
+	return &graphql.Field{
+		Type:        classUnion,
+		Description: property.Description,
+	}
+}
+
+func buildGetClassField(classObject *graphql.Object, k kind.Kind,
+	class *models.SemanticSchemaClass) graphql.Field {
+	kindName := strings.Title(k.Name())
+	return graphql.Field{
+		Type:        graphql.NewList(classObject),
+		Description: class.Description,
+		Args: graphql.FieldConfigArgument{
+			"first": &graphql.ArgumentConfig{
+				Description: descriptions.FirstDesc,
+				Type:        graphql.Int,
+			},
+			"after": &graphql.ArgumentConfig{
+				Description: descriptions.AfterDesc,
+				Type:        graphql.Int,
+			},
+			"where": &graphql.ArgumentConfig{
+				Description: descriptions.LocalGetWhereDesc,
+				Type: graphql.NewInputObject(
+					graphql.InputObjectConfig{
+						Name:        fmt.Sprintf("WeaviateLocalGet%ss%sWhereInpObj", kindName, class.Class),
+						Fields:      common_filters.BuildNew(fmt.Sprintf("WeaviateLocalGet%ss%s", kindName, class.Class)),
+						Description: descriptions.LocalGetWhereInpObjDesc,
+					},
+				),
+			},
+		},
+		Resolve: makeResolveGetClass(k, class.Class),
+	}
+}
+
+func makeResolveGetClass(k kind.Kind, className string) graphql.FieldResolveFn {
+	return func(p graphql.ResolveParams) (interface{}, error) {
+		fmt.Printf("- thing class (supposed to extract pagination, now return nil)\n")
+		filtersAndResolver := p.Source.(*filtersAndResolver)
+
+		pagination, err := common.ExtractPaginationFromArgs(p.Args)
+		if err != nil {
+			return nil, err
+		}
+
+		// There can only be exactly one ast.Field; it is the class name.
+		if len(p.Info.FieldASTs) != 1 {
+			panic("Only one Field expected here")
+		}
+
+		selectionsOfClass := p.Info.FieldASTs[0].SelectionSet
+		properties, err := extractProperties(selectionsOfClass)
+		if err != nil {
+			return nil, err
+		}
+
+		// fmt.Print("\n\n\n\n\n")
+		// spew.Dump(properties)
+		// fmt.Print("\n\n\n\n\n")
+
+		params := LocalGetClassParams{
+			Filters:    filtersAndResolver.filters,
+			Kind:       k,
+			ClassName:  className,
+			Pagination: pagination,
+			Properties: properties,
+		}
+
+		promise, err := filtersAndResolver.resolver.LocalGetClass(&params)
+		return promise, err
+	}
+}
+
+func extractProperties(selections *ast.SelectionSet) ([]SelectProperty, error) {
+	// debugFieldAsts(fieldASTs)
+	var properties []SelectProperty
+
+	for _, selection := range selections.Selections {
+		field := selection.(*ast.Field)
+		name := field.Name.Value
+		property := SelectProperty{Name: name}
+
+		property.IsPrimitive = (field.SelectionSet == nil)
+
+		if !property.IsPrimitive {
+			// We can interpret this property in different ways
+			for _, subSelection := range field.SelectionSet.Selections {
+				// Is it a field with the name __typename?
+				subsectionField, ok := subSelection.(*ast.Field)
+				if ok {
+					if subsectionField.Name.Value == "__typename" {
+						property.IncludeTypeName = true
+						continue
+					} else {
+						return nil, fmt.Errorf("Expected a InlineFragment, not a '%s' field ", subsectionField.Name.Value)
+					}
+				}
+
+				// Otherwise these _must_ be inline fragments
+				fragment, ok := subSelection.(*ast.InlineFragment)
+				if !ok {
+					return nil, fmt.Errorf("Expected a InlineFragment; you need to specify as which type you want to retrieve a reference %#v", subSelection)
+				}
+
+				var className schema.ClassName
+				var err error
+				err, className = schema.ValidateClassName(fragment.TypeCondition.Name.Value)
+				if err != nil {
+					return nil, fmt.Errorf("the inline fragment type name '%s' is not a valid class name", fragment.TypeCondition.Name.Value)
+				}
+
+				subProperties, err := extractProperties(fragment.SelectionSet)
+				if err != nil {
+					return nil, err
+				}
+
+				property.Refs = append(property.Refs, SelectClass{
+					ClassName:     string(className),
+					RefProperties: subProperties,
+				})
+			}
+		}
+
+		properties = append(properties, property)
+	}
+
+	return properties, nil
+}
