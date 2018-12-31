c3984b97551cc3a7be35ffe1b4a5b5b871f46a21
Etienne Dilocker <etienne@dilocker.de>
2018-12-31 11:19:01 +0100

    gh-630 resolve Network.Get where filters at new position
    Basically this is just deleting a bunch of code. Previously we had to
    parse the filters entirely, because we had to modify the path. It looked
    somehting like
    
    WeaviateB->Things->City->name and was placed at the level of Network.Get.
    
    We had to rewrite this to just say Things->City->name (i.e. remove the
    peer name) and inject the filter at the correct place after
    Local.Get.
    
    Now that the filters are at the level of the KindClass anyway, we just
    proxy them together with everything else. So no more parsing is required
    at all.

--- graphqlapi/network/get/resolver.go
+++ graphqlapi/network/get/resolver.go
@@ -10,10 +10,10 @@
  * See www.creativesoftwarefdn.org for details
  * Contact: @CreativeSofwFdn / bob@kub.design
  */
+
 package network_get
 
 import (
-	"encoding/json"
 	"fmt"
 	"regexp"
 
@@ -35,7 +35,6 @@ func ParseSubQuery(subQuery []byte) SubQuery {
 
 // WrapInLocalQuery assumes the subquery can be sent as part of a
 // Local-Query, i.e. it should start with `Get{ ... }`
-// TODO: At the moment ignores filter params
 func (s SubQuery) WrapInLocalQuery() string {
 	return fmt.Sprintf("{ Local { %s } }", s)
 }
@@ -54,7 +53,6 @@ type Resolver interface {
 
 // FiltersAndResolver is a helper tuple to bubble data through the resolvers.
 type FiltersAndResolver struct {
-	Filters  FiltersPerInstance
 	Resolver Resolver
 }
 
@@ -66,10 +64,9 @@ func NetworkGetInstanceResolve(p graphql.ResolveParams) (interface{}, error) {
 	}
 
 	resolver := filterAndResolver.Resolver
-	filters := filterAndResolver.Filters
 	astLoc := p.Info.FieldASTs[0].GetLoc()
 	rawSubQuery := astLoc.Source.Body[astLoc.Start:astLoc.End]
-	subQueryWithoutInstance, err := replaceInstanceNameAndInjectFilter(p.Info.FieldName, rawSubQuery, filters)
+	subQueryWithoutInstance, err := replaceInstanceName(p.Info.FieldName, rawSubQuery)
 	if err != nil {
 		return nil, fmt.Errorf("could not replace instance name in sub-query: %s", err)
 	}
@@ -100,45 +97,11 @@ func NetworkGetInstanceResolve(p graphql.ResolveParams) (interface{}, error) {
 	return local["Get"], nil
 }
 
-func replaceInstanceNameAndInjectFilter(instanceName string, query []byte, filters FiltersPerInstance) ([]byte, error) {
+func replaceInstanceName(instanceName string, query []byte) ([]byte, error) {
 	r, err := regexp.Compile(fmt.Sprintf(`^%s\s*`, instanceName))
 	if err != nil {
 		return []byte{}, err
 	}
 
-	queryWithoutFilters := func() []byte { return r.ReplaceAll(query, []byte("Get ")) }
-	if filters == nil {
-		return queryWithoutFilters(), nil
-	}
-
-	instanceFilters, ok := filters[instanceName]
-	if !ok {
-		return queryWithoutFilters(), nil
-	}
-
-	where, ok := instanceFilters.(map[string]interface{})["where"]
-	if !ok {
-		return queryWithoutFilters(), nil
-	}
-
-	whereBytes, err := json.Marshal(where)
-	if err != nil {
-		return nil, fmt.Errorf("could not marshal extracted filters back to json: %s", err)
-	}
-
-	graphQLWhere := jsonToGraphQLWhere(whereBytes)
-	return r.ReplaceAll(query, []byte(fmt.Sprintf("Get(where: %s) ", graphQLWhere))), nil
-}
-
-func jsonToGraphQLWhere(whereJSON []byte) []byte {
-	// Remove Quotes on all keys, because graphql object structure
-	// is more like javascript objects, then JSON. Quotes on keys
-	// are not valid.
-	replaceKeyQuotes := regexp.MustCompile(`"(\w+)"\s*:`)
-	withoutKeyQuotes := replaceKeyQuotes.ReplaceAll(whereJSON, []byte("$1:"))
-
-	// Remove quotes on operator value, because that's an Enum in the graphQL
-	// schema, so it also doesn't expect quotes.
-	replaceOperatorEnumQuotes := regexp.MustCompile(`operator:\s*"(\w+)"`)
-	return replaceOperatorEnumQuotes.ReplaceAll(withoutKeyQuotes, []byte("operator:$1"))
+	return r.ReplaceAll(query, []byte("Get ")), nil
 }
