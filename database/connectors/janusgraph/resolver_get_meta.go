4b5e05c0478bd8913bfda84cc1bcb45af9d0590b
Etienne Dilocker <etienne@dilocker.de>
2019-01-03 14:49:28 +0100

    gh-660: no longer return thunk in connector.LocalGetMeta
    The original intend of the thunks was to enable parallelism. We want to
    keep this idea, but the thunk can always be created in the
    graphql.Resolve() methode in the graphqlapi package. The connectors do
    not have to know of the concept of thunks, their concern should simply
    be to resolve based on the given input.

--- database/connectors/janusgraph/resolver_get_meta.go
+++ database/connectors/janusgraph/resolver_get_meta.go
@@ -16,6 +16,6 @@ import (
 	graphql_local_get_meta "github.com/creativesoftwarefdn/weaviate/graphqlapi/local/get_meta"
 )
 
-func (j *Janusgraph) LocalGetMeta(params *graphql_local_get_meta.LocalGetMetaParams) (func() interface{}, error) {
+func (j *Janusgraph) LocalGetMeta(params *graphql_local_get_meta.LocalGetMetaParams) (interface{}, error) {
 	return nil, nil
 }
