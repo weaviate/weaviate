a5f61947568bc899354a3161d64aaab9d8ea319d
Travis CI User <travis@example.org>
2019-01-05 22:32:18 +0000

    ?? bleep bloop - auto updated Weaviate

--- graphqlapi/local/common_filters/ast.go
+++ graphqlapi/local/common_filters/ast.go
@@ -4,11 +4,9 @@
  * \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
  *  \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
  *
- * Copyright © 2016 - 2018 Weaviate. All rights reserved.
+ * Copyright © 2016 - 2019 Weaviate. All rights reserved.
  * LICENSE: https://github.com/creativesoftwarefdn/weaviate/blob/develop/LICENSE.md
- * AUTHOR: Bob van Luijt (bob@kub.design)
- * See www.creativesoftwarefdn.org for details
- * Contact: @CreativeSofwFdn / bob@kub.design
+ * DESIGN: Bob van Luijt (bob@k10y.co)
  */
 package common_filters
 
