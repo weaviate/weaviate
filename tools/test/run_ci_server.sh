bf966befb71b82a35c78bfb7b92a9c4ec4431948
Etienne Dilocker <etienne@dilocker.de>
2019-01-11 18:12:59 +0100

    gh-680: make acceptance test setup much more verbose
    Right now it seems the acceptance tests are failing during the
    docker-build step on CI, but locally (even with a completely clean
    docker install) they are fine.
    
    Hopefully this will shed some light onto what is going on on CI.

--- tools/test/run_ci_server.sh
+++ tools/test/run_ci_server.sh
@@ -7,7 +7,7 @@ function main() {
   echo "Pull images..."
   surpress_on_success docker pull golang:1.11-alpine
   echo "Build containers (this will take the longest)..."
-  surpress_on_success docker-compose -f docker-compose-test.yml build weaviate janus index db \
+  docker-compose -f docker-compose-test.yml build weaviate janus index db \
     genesis_fake remote_weaviate_fake remote_weaviate_fakes_with_test_schema genesis
   echo "Start up docker-compose setup..."
   surpress_on_success docker-compose -f docker-compose-test.yml up --force-recreate -d weaviate \
