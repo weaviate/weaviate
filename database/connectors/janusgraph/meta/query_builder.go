e6bd9eadf188dd121c6d7559884ef859c0a42aa2
Etienne Dilocker <etienne@dilocker.de>
2019-01-13 15:20:06 +0100

    gh-653: build query for <boolProp>.count

---
+++ database/connectors/janusgraph/meta/query_builder.go
@@ -0,0 +1,71 @@
+package meta
+
+import (
+	"fmt"
+
+	"github.com/creativesoftwarefdn/weaviate/database/connectors/janusgraph/state"
+	"github.com/creativesoftwarefdn/weaviate/database/schema"
+	"github.com/creativesoftwarefdn/weaviate/graphqlapi/local/getmeta"
+	"github.com/creativesoftwarefdn/weaviate/gremlin"
+)
+
+type Query struct {
+	params     getmeta.Params
+	nameSource nameSource
+}
+
+func NewQuery(params getmeta.Params, nameSource nameSource) *Query {
+	return &Query{params: params, nameSource: nameSource}
+}
+
+type nameSource interface {
+	GetMappedPropertyName(className schema.ClassName, propName schema.PropertyName) state.MappedPropertyName
+}
+
+func (b *Query) String() (string, error) {
+	q := gremlin.New()
+
+	// get only one prop for now
+	prop := b.params.Properties[0]
+	propQuery, err := b.booleanProp(prop)
+	if err != nil {
+		return "", err
+	}
+
+	q = q.Union(propQuery)
+
+	return fmt.Sprintf(".%s", q.String()), nil
+
+}
+
+func (b *Query) booleanProp(prop getmeta.MetaProperty) (*gremlin.Query, error) {
+	q := gremlin.New()
+
+	// get only one analysis for now
+	// analysis := prop.StatisticalAnalyses[0]
+	countQuery, err := b.booleanPropCount(prop)
+	if err != nil {
+		return nil, err
+	}
+
+	q = q.Union(countQuery).AsProjectBy(string(prop.Name))
+
+	return q, nil
+}
+
+func (b *Query) booleanPropCount(prop getmeta.MetaProperty) (*gremlin.Query, error) {
+	q := gremlin.New()
+
+	q = q.HasProperty(b.mappedPropertyName(b.params.ClassName, prop.Name)).Count()
+
+	return q, nil
+}
+
+func (b *Query) mappedPropertyName(className schema.ClassName,
+	propName schema.PropertyName) string {
+	if b.nameSource == nil {
+		return string(propName)
+	}
+
+	return string(b.nameSource.GetMappedPropertyName(className, propName))
+}
---
+++ database/connectors/janusgraph/meta/query_builder_test.go
@@ -0,0 +1,92 @@
+package meta
+
+import (
+	"testing"
+
+	"github.com/creativesoftwarefdn/weaviate/database/connectors/janusgraph/state"
+	"github.com/creativesoftwarefdn/weaviate/database/schema"
+	gm "github.com/creativesoftwarefdn/weaviate/graphqlapi/local/getmeta"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type testCase struct {
+	name          string
+	inputProps    []gm.MetaProperty
+	expectedQuery string
+}
+
+type testCases []testCase
+
+func (tests testCases) AssertQuery(t *testing.T, nameSource nameSource) {
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			params := gm.Params{
+				Properties: test.inputProps,
+			}
+			query, err := NewQuery(params, nameSource).String()
+			require.Nil(t, err, "should not error")
+			assert.Equal(t, test.expectedQuery, query, "should match the query")
+
+		})
+
+	}
+
+}
+
+func Test_QueryBuilder(t *testing.T) {
+
+	tests := testCases{
+		testCase{
+			name: "with only a boolean, with only count",
+			inputProps: []gm.MetaProperty{
+				gm.MetaProperty{
+					Name:                "isCapital",
+					StatisticalAnalyses: []gm.StatisticalAnalysis{gm.Count},
+				},
+			},
+			expectedQuery: `.union(` +
+				`union(has("isCapital").count()).as("isCapital").project("isCapital").by(select("isCapital"))` +
+				`)`,
+		},
+	}
+
+	tests.AssertQuery(t, nil)
+
+}
+
+func Test_QueryBuilderWithNamesource(t *testing.T) {
+
+	tests := testCases{
+		testCase{
+			name: "with only a boolean, with only count",
+			inputProps: []gm.MetaProperty{
+				gm.MetaProperty{
+					Name:                "isCapital",
+					StatisticalAnalyses: []gm.StatisticalAnalysis{gm.Count},
+				},
+			},
+			expectedQuery: `.union(` +
+				`union(has("prop_20").count()).as("isCapital").project("isCapital").by(select("isCapital"))` +
+				`)`,
+		},
+	}
+
+	tests.AssertQuery(t, &fakeNameSource{})
+
+}
+
+type fakeNameSource struct{}
+
+func (f *fakeNameSource) GetMappedPropertyName(className schema.ClassName,
+	propName schema.PropertyName) state.MappedPropertyName {
+	switch propName {
+	case schema.PropertyName("inCountry"):
+		return "prop_15"
+	}
+	return state.MappedPropertyName("prop_20")
+}
+
+func (f *fakeNameSource) GetMappedClassName(className schema.ClassName) state.MappedClassName {
+	return state.MappedClassName("class_18")
+}
