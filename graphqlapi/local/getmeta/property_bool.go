634f74fe7e9dc8ac0522adb1877ae83c4de73984
Etienne Dilocker <etienne@dilocker.de>
2019-01-13 09:42:13 +0100

    gh-653: make getmeta resolvers actually resolve when they get data
    Previously most would simply return an error like "not supported".
    Ironically in almost all cases the default resolver is perfect, so we
    the solution is to simply delete the erroring resolver. To verify we
    didn't miss one, this is also being validated in our tests.

--- graphqlapi/local/getmeta/property_bool.go
+++ graphqlapi/local/getmeta/property_bool.go
@@ -15,49 +15,31 @@ func booleanPropertyFields(class *models.SemanticSchemaClass,
 			Name:        fmt.Sprintf("Meta%s%sType", class.Class, property.Name),
 			Description: descriptions.GetMetaPropertyTypeDesc,
 			Type:        graphql.String,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return nil, fmt.Errorf("not supported")
-			},
 		},
 		"count": &graphql.Field{
 			Name:        fmt.Sprintf("Meta%s%sCount", class.Class, property.Name),
 			Description: descriptions.GetMetaPropertyCountDesc,
 			Type:        graphql.Int,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return nil, fmt.Errorf("not supported")
-			},
 		},
 		"totalTrue": &graphql.Field{
 			Name:        fmt.Sprintf("Meta%s%sTotalTrue", class.Class, property.Name),
 			Description: descriptions.GetMetaClassPropertyTotalTrueDesc,
 			Type:        graphql.Int,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return nil, fmt.Errorf("not supported")
-			},
 		},
 		"percentageTrue": &graphql.Field{
 			Name:        fmt.Sprintf("Meta%s%sPercentageTrue", class.Class, property.Name),
 			Description: descriptions.GetMetaClassPropertyPercentageTrueDesc,
 			Type:        graphql.Float,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return nil, fmt.Errorf("not supported")
-			},
 		},
 		"totalFalse": &graphql.Field{
 			Name:        fmt.Sprintf("Meta%s%sTotalFalse", class.Class, property.Name),
 			Description: descriptions.GetMetaClassPropertyTotalFalseDesc,
 			Type:        graphql.Int,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return nil, fmt.Errorf("not supported")
-			},
 		},
 		"percentageFalse": &graphql.Field{
 			Name:        fmt.Sprintf("Meta%s%sPercentageFalse", class.Class, property.Name),
 			Description: descriptions.GetMetaClassPropertyPercentageFalseDesc,
 			Type:        graphql.Float,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return nil, fmt.Errorf("not supported")
-			},
 		},
 	}
 
