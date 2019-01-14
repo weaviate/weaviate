1ab2c1eb14659929aa61b5df2c3e99d5901b21d1
Etienne Dilocker <etienne@dilocker.de>
2019-01-14 14:09:27 +0100

    gh-653: allow more than one class property in meta at a time
    Previously we had made a shortcut to only take the first prop, this is
    now resolved and now we iterate over every prop.

--- database/connectors/janusgraph/meta/query_builder_test.go
+++ database/connectors/janusgraph/meta/query_builder_test.go
@@ -14,8 +14,51 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// This file contains only helpers for other tests, please see the test files
-// for individual props.
+// This file contains only a single test to verify that combining multiple
+// props together works as intended and helpers for other tests. Each
+// individual property type however, is also extensively tested, please see the
+// test files for individual props with the format query_builder_<type>_test.go
+
+func Test_QueryBuilder_MultipleProps(t *testing.T) {
+	tests := testCases{
+		testCase{
+			name: "with multiple props",
+			inputProps: []gm.MetaProperty{
+				gm.MetaProperty{
+					Name: "isCapital",
+					StatisticalAnalyses: []gm.StatisticalAnalysis{
+						gm.Count, gm.TotalTrue, gm.TotalFalse, gm.PercentageTrue, gm.PercentageFalse,
+					},
+				},
+				gm.MetaProperty{
+					Name: "population",
+					StatisticalAnalyses: []gm.StatisticalAnalysis{
+						gm.Average, gm.Sum, gm.Highest, gm.Lowest, gm.Count,
+					},
+				},
+			},
+			expectedQuery: `
+				.union(
+					union(
+						has("isCapital").count()
+							.as("count").project("count").by(select("count")),
+						groupCount().by("isCapital")
+							.as("boolGroupCount").project("boolGroupCount").by(select("boolGroupCount"))
+					)
+						.as("isCapital").project("isCapital").by(select("isCapital")),
+					aggregate("aggregation").by("population").cap("aggregation").limit(1)
+						.as("average", "sum", "highest", "lowest", "count")
+						.select("average", "sum", "highest", "lowest", "count")
+						.by(mean(local)).by(sum(local)).by(max(local)).by(min(local)).by(count(local))
+						.as("population").project("population").by(select("population"))
+				)
+			`,
+		},
+	}
+
+	tests.AssertQuery(t, nil)
+
+}
 
 type fakeNameSource struct{}
 
