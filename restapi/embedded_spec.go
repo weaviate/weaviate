14c0779e0b4cba6391c5e6ac6a8f35260cec7fcb
Travis CI User <travis@example.org>
2019-01-22 10:32:01 +0000

    ?? bleep bloop - auto updated Weaviate

--- restapi/embedded_spec.go
+++ restapi/embedded_spec.go
@@ -46,7 +46,7 @@ func init() {
       "url": "https://github.com/creativesoftwarefdn",
       "email": "hello@creativesoftwarefdn.org"
     },
-    "version": "0.12.20"
+    "version": "0.12.21"
   },
   "basePath": "/weaviate/v1",
   "paths": {
@@ -2839,7 +2839,7 @@ func init() {
       "url": "https://github.com/creativesoftwarefdn",
       "email": "hello@creativesoftwarefdn.org"
     },
-    "version": "0.12.20"
+    "version": "0.12.21"
   },
   "basePath": "/weaviate/v1",
   "paths": {
