c5d2c54baaf42d5481f2a806ec7e5474f0390cea
Etienne Dilocker <etienne@dilocker.de>
2019-01-23 13:28:56 +0100

    gh-693: verify local schema building with component test
    This helps us efficiently rule out issues with edge cases, such as empty
    schemas, schemas with only one Kind of classes, classes without any
    properties, etc.

--- graphqlapi/local/local_component_test.go
+++ graphqlapi/local/local_component_test.go
@@ -1,4 +1,4 @@
-package network
+package local
 
 import (
 	"fmt"
@@ -13,24 +13,91 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// These test are component tests for the network package. However, they only
-// assert that the graphql tree can be built under certain circumstances. This
-// helps us to catch errors on edge cases like empty peer lists, peers with
-// empty schemas, etc. However, we don't get any guaruantuee of whether the
-// individual queries resolve correctly. For those cases we have unit tests in
-// die individual subpackages (i.e. get, getmeta, aggreagate, etc.).
-// Additionally we have (a few) e2e tests.
+// These tests are component tests for the local package including all its
+// subpackages, such as get, getmeta, etc.. However, they only assert that the
+// graphql tree can be built under certain circumstances. This helps us to
+// catch errors on edge cases like empty schemas, classes with empty
+// properties, empty peer lists, peers with empty schemas, etc. However, we
+// don't get any guaruantuee of whether the individual queries resolve
+// correctly. For those cases we have unit tests in die individual subpackages
+// (i.e. get, getmeta, aggreagate, etc.).  Additionally we have (a few) e2e
+// tests.
 
 func Test_GraphQLNetworkBuild(t *testing.T) {
 
 	tests := testCases{
+
+		// This tests asserts that an action-only schema doesn't lead to errors.
 		testCase{
-			name:  "without any peers",
+			name:  "with only actions locally",
 			peers: peers.Peers{},
+			localSchema: schema.Schema{
+				Actions: &models.SemanticSchema{
+					Classes: []*models.SemanticSchemaClass{
+						&models.SemanticSchemaClass{
+							Class: "BestLocalAction",
+							Properties: []*models.SemanticSchemaClassProperty{
+								&models.SemanticSchemaClassProperty{
+									AtDataType: []string{"string"},
+									Name:       "myStringProp",
+								},
+							},
+						},
+					},
+				},
+				Things: &models.SemanticSchema{},
+			},
+		},
+
+		// This tests asserts that a things-only schema doesn't lead to errors.
+		testCase{
+			name:  "with only things locally",
+			peers: peers.Peers{},
+			localSchema: schema.Schema{
+				Things: &models.SemanticSchema{
+					Classes: []*models.SemanticSchemaClass{
+						&models.SemanticSchemaClass{
+							Class: "BestLocalThing",
+							Properties: []*models.SemanticSchemaClassProperty{
+								&models.SemanticSchemaClassProperty{
+									AtDataType: []string{"string"},
+									Name:       "myStringProp",
+								},
+							},
+						},
+					},
+				},
+				Actions: &models.SemanticSchema{},
+			},
+		},
+
+		// This tests asserts that a class without any properties doesn't lead to
+		// errors.
+		testCase{
+			name:  "with things without properties locally",
+			peers: peers.Peers{},
+			localSchema: schema.Schema{
+				Things: &models.SemanticSchema{
+					Classes: []*models.SemanticSchemaClass{
+						&models.SemanticSchemaClass{
+							Class:      "BestLocalThing",
+							Properties: []*models.SemanticSchemaClassProperty{},
+						},
+					},
+				},
+				Actions: &models.SemanticSchema{},
+			},
 		},
 
 		testCase{
-			name: "one peer with empty schema",
+			name:        "without any peers",
+			peers:       peers.Peers{},
+			localSchema: validSchema(),
+		},
+
+		testCase{
+			name:        "one peer with empty schema",
+			localSchema: validSchema(),
 			peers: peers.Peers{
 				peers.Peer{
 					Name:   "SomePeer",
@@ -40,7 +107,8 @@ func Test_GraphQLNetworkBuild(t *testing.T) {
 		},
 
 		testCase{
-			name: "one peer with a thing schema without classes",
+			name:        "one peer with a thing schema without classes",
+			localSchema: validSchema(),
 			peers: peers.Peers{
 				peers.Peer{
 					Name: "SomePeer",
@@ -57,7 +125,8 @@ func Test_GraphQLNetworkBuild(t *testing.T) {
 		// could otherwise end up with empty Fields which would lead to a graphQL
 		// build error
 		testCase{
-			name: "one peer with a thing schema without properties, but no actions",
+			name:        "one peer with a thing schema without properties, but no actions",
+			localSchema: validSchema(),
 			peers: peers.Peers{
 				peers.Peer{
 					Name: "SomePeer",
@@ -80,7 +149,8 @@ func Test_GraphQLNetworkBuild(t *testing.T) {
 		// could otherwise end up with empty Fields which would lead to a graphQL
 		// build error
 		testCase{
-			name: "one peer with a thing schema with properties, but no actions",
+			name:        "one peer with a thing schema with properties, but no actions",
+			localSchema: validSchema(),
 			peers: peers.Peers{
 				peers.Peer{
 					Name: "SomePeer",
@@ -105,7 +175,8 @@ func Test_GraphQLNetworkBuild(t *testing.T) {
 		},
 
 		testCase{
-			name: "one peer with a both a thing and an action class",
+			name:        "one peer with a both a thing and an action class",
+			localSchema: validSchema(),
 			peers: peers.Peers{
 				peers.Peer{
 					Name: "SomePeer",
@@ -135,7 +206,8 @@ func Test_GraphQLNetworkBuild(t *testing.T) {
 		// test asserts that there is no naming collision, i.e. that the properties
 		// are namespaced correctly by their respective classes.
 		testCase{
-			name: "one peer with a both a thing and an action class with properties",
+			name:        "one peer with a both a thing and an action class with properties",
+			localSchema: validSchema(),
 			peers: peers.Peers{
 				peers.Peer{
 					Name: "SomePeer",
@@ -174,7 +246,8 @@ func Test_GraphQLNetworkBuild(t *testing.T) {
 		// This tests assert that there are no name collisions with multiple peers,
 		// i.e. that every peer has their objects and fields namespaced correctly.
 		testCase{
-			name: "two peers with identical schemas",
+			name:        "two peers with identical schemas",
+			localSchema: validSchema(),
 			peers: peers.Peers{
 				peers.Peer{
 					Name: "SomePeer",
@@ -246,8 +319,9 @@ func Test_GraphQLNetworkBuild(t *testing.T) {
 }
 
 type testCase struct {
-	name  string
-	peers peers.Peers
+	name        string
+	peers       peers.Peers
+	localSchema schema.Schema
 }
 
 type testCases []testCase
@@ -255,14 +329,14 @@ type testCases []testCase
 func (tests testCases) AssertNoError(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			networkSchema, err := Build(test.peers)
+			localSchema, err := Build(&test.localSchema, test.peers, nil)
 			require.Nil(t, err, test.name)
 
 			schemaObject := graphql.ObjectConfig{
 				Name:        "WeaviateObj",
 				Description: "Location of the root query",
 				Fields: graphql.Fields{
-					"Network": networkSchema,
+					"Local": localSchema,
 				},
 			}
 
@@ -280,5 +354,36 @@ func (tests testCases) AssertNoError(t *testing.T) {
 
 			assert.Nil(t, err, test.name)
 		})
+	}
+}
+
+func validSchema() schema.Schema {
+	return schema.Schema{
+		Things: &models.SemanticSchema{
+			Classes: []*models.SemanticSchemaClass{
+				&models.SemanticSchemaClass{
+					Class: "BestLocalThing",
+					Properties: []*models.SemanticSchemaClassProperty{
+						&models.SemanticSchemaClassProperty{
+							AtDataType: []string{"string"},
+							Name:       "myStringProp",
+						},
+					},
+				},
+			},
+		},
+		Actions: &models.SemanticSchema{
+			Classes: []*models.SemanticSchemaClass{
+				&models.SemanticSchemaClass{
+					Class: "BestLocalAction",
+					Properties: []*models.SemanticSchemaClassProperty{
+						&models.SemanticSchemaClassProperty{
+							AtDataType: []string{"string"},
+							Name:       "myStringProp",
+						},
+					},
+				},
+			},
+		},
 	}
 }
