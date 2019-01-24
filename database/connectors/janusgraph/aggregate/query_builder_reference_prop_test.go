cc162b661a09454f62a71cfd8dbc13efee84adc3
Etienne Dilocker <etienne@dilocker.de>
2019-01-24 11:11:03 +0100

    gh-693: aggregate (count) ref-props in Local.Aggregate

---
+++ database/connectors/janusgraph/aggregate/query_builder_reference_prop_test.go
@@ -0,0 +1,52 @@
+/*                          _       _
+ *__      _____  __ ___   ___  __ _| |_ ___
+ *\ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+ * \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+ *  \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+ *
+ * Copyright © 2016 - 2018 Weaviate. All rights reserved.
+ * LICENSE: https://github.com/creativesoftwarefdn/weaviate/blob/develop/LICENSE.md
+ * AUTHOR: Bob van Luijt (bob@kub.design)
+ * See www.creativesoftwarefdn.org for details
+ * Contact: @CreativeSofwFdn / bob@kub.design
+ */
+package aggregate
+
+import (
+	"testing"
+
+	"github.com/creativesoftwarefdn/weaviate/database/schema"
+	ag "github.com/creativesoftwarefdn/weaviate/graphqlapi/local/aggregate"
+	cf "github.com/creativesoftwarefdn/weaviate/graphqlapi/local/common_filters"
+)
+
+func Test_QueryBuilder_ReferenceProps(t *testing.T) {
+	tests := testCases{
+		testCase{
+			name: "counting a ref prop, grouped by a primitive prop",
+			inputProps: []ag.Property{
+				ag.Property{
+					Name:        "inCountry",
+					Aggregators: []ag.Aggregator{ag.Count},
+				},
+			},
+			inputGroupBy: &cf.Path{
+				Class:    schema.ClassName("City"),
+				Property: schema.PropertyName("isCapital"),
+			},
+			expectedQuery: `
+				.group().by("isCapital").by(
+					fold()
+						.match(
+							__.as("a").unfold().out("inCountry").count().as("inCountry__count")
+						)
+						.select("inCountry__count").by(project("inCountry__count")).as("inCountry")
+						.select("inCountry").by(project("inCountry"))
+					)
+				`,
+		},
+	}
+
+	tests.AssertQuery(t, nil)
+
+}
