ba1a685bbf21be40ac0e08dd91b1762345393235
Etienne Dilocker <etienne@dilocker.de>
2019-01-29 10:54:57 +0100

    gh-685: extensively document behavior of distributed RWMutex
    With a local Mutex it's very important to pass it by reference, so we
    always operate on exactly the same lock. For this distributed lock using
    etcd it's the other way round. The lifespan of a Mutex type should only
    be from Lock() to Unlock() or from RLock() to RUnlock().
    
    This commit adds extensive documentation including examples of why not
    paying attention to this is problematic.

--- database/database.go
+++ database/database.go
@@ -135,6 +135,55 @@ func New(params *Params) (Database, error) {
 		messaging.ExitError(1, fmt.Sprintf("Could not connect to backing database: %s", errConnect.Error()))
 	}
 
+	// Note that this lock is not passed to the returned &database{} type. Because
+	// of the distributed nature of the locks (currently being managed with etcd)
+	// the local types should not span the entire session. Each RWMutex holds the
+	// state of the key it uses for locking. This means exactly this type has to
+	// be reused for unlocking, but not used for anything else
+	//
+	// For example, the following is fine:
+	//	m1 := recipe.NewRWMutex(/*...*/)
+	//	m2 := recipe.NewRWMutex(/*...*/)
+	//	m1.Lock()
+	//	m1.Unlock()
+	//	// now discard m1!
+	//	m2.RLock()
+	//	m2.RUnlock()
+	//	// now discard m2!
+	//
+	// The above example might not be immediately intuitive, because when using
+	// local mutexes it is extremly important to pass them by reference,
+	// otherwise no syncing is possible. However on this remote lock it's exactly
+	// the opposite.
+	//
+	// This code however would lead to problems:
+	//	m := recipe.NewRWMutex(/*...*/)
+	//
+	//	go func() {
+	// 		m.Lock()
+	// 		// do something
+	// 		m.Unlock()
+	// 	}()
+	//
+	// 	go func() {
+	// 		m.RLock()
+	// 		// do something
+	// 		m.RUnlock()
+	// 	}()
+	//
+	// The above code segment would not work, because the first goroutine when
+	// locking would set the internal m.myKey to whatever key and lease it got.
+	// It is important that it keeps this state because it needs it to unlock the
+	// same mutex, when doing m.Unlock(). However, the second goroutine would try
+	// to do the same, it would set myKey to its own key+Lease on m.RLock(). This
+	// means whichever goroutine of the two last called either Lock() or Unlock()
+	// will have overwritten the internal key. Thus the other routine can never
+	// be unlocked again, because on Unlock() or RUnlock() it would now try to
+	// unlock with the other key.
+	//
+	// To fix the issue in Example 2, there should be no such thing as a "global"
+	// Mutex, instead there should either be one per gouroutine or even better
+	// yet, the mutexes should be created within the goroutines themselves.
 	lock := recipe.NewRWMutex(lockerSession, lockerKey)
 	// Init the database. Manually lock the schema, so that initialization happens atomically across all instances.
 	if err := lock.Lock(); err != nil {
@@ -164,6 +213,13 @@ func New(params *Params) (Database, error) {
 
 // Get a lock on the connector, allow access to the database, cannot modify schema.
 func (db *database) ConnectorLock() (ConnectorLock, error) {
+	// Note that we are creating a new lock every time that ConnectorLock() is
+	// called. This lock has a short lifespan. It's only other usage, other than
+	// calling RLock() in the next line is to pass it to the user, so that they
+	// can call RUnlock() exactly once on it. This might seem counter-intuitive,
+	// as with local locks we need to make sure there is only one global lock.
+	// Please see database.New() for extensive documentation on why that would be
+	// problematic in the case of the distributed lock.
 	lock := recipe.NewRWMutex(db.lockerSession, db.lockerKey)
 	// Init the database. Manually lock the schema, so that initialization happens atomically across all instances.
 	if err := lock.RLock(); err != nil {
@@ -180,6 +236,13 @@ func (db *database) ConnectorLock() (ConnectorLock, error) {
 // Get a lock on the schema manager. Can both modify the database and the schema.
 // We ensure that only request in one instance should hold this lock at the same time.
 func (db *database) SchemaLock() (SchemaLock, error) {
+	// Note that we are creating a new lock every time that SchemaLock() is
+	// called. This lock has a short lifespan. It's only other usage, other than
+	// calling Lock() in the next line is to pass it to the user, so that they
+	// can call Unlock() exactly once on it. This might seem counter-intuitive,
+	// as with local locks we need to make sure there is only one global lock.
+	// Please see database.New() for extensive documentation on why that would be
+	// problematic in the case of the distributed lock.
 	lock := recipe.NewRWMutex(db.lockerSession, db.lockerKey)
 	// Init the database. Manually lock the schema, so that initialization happens atomically across all instances.
 	if err := lock.Lock(); err != nil {
