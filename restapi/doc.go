4281a90e3b2b6bff7efc93b917e9c0e74faca539
Travis CI User <travis@example.org>
2019-01-30 12:08:28 +0000

    ?? bleep bloop - auto updated Weaviate

--- restapi/doc.go
+++ restapi/doc.go
@@ -19,7 +19,7 @@ Decentralised Knowledge Graph
       https
     Host: localhost
     BasePath: /weaviate/v1
-    Version: 0.12.24
+    Version: 0.12.25
     Contact: Weaviate<hello@creativesoftwarefdn.org> https://github.com/creativesoftwarefdn
 
     Consumes:
