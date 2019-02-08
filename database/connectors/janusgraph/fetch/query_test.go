5813fc31db148a8232c3806c8c85cfce7f1f0fb6
Etienne Dilocker <etienne@dilocker.de>
2019-02-08 11:28:13 +0100

    gh-645 exclude properties that have a differnet type than our match q
    I.e. if the user specified something like `name == string("foo")` and we
    do have a `name` prop, but it is not of type string, then we have to
    exclude it, otherwise we would build a gremlin query that would lead to
    errors in the db.

--- database/connectors/janusgraph/fetch/query_test.go
+++ database/connectors/janusgraph/fetch/query_test.go
@@ -29,7 +29,7 @@ func Test_QueryBuilder(t *testing.T) {
 						PossibleNames: contextionary.SearchResults{
 							Results: []contextionary.SearchResult{
 								{
-									Name:      "population",
+									Name:      "name",
 									Certainty: 1.0,
 								},
 							},
@@ -47,7 +47,51 @@ func Test_QueryBuilder(t *testing.T) {
 			expectedQuery: `
 				g.V().has("kind", "thing").and(
 					or(
-						has("classId", "class_18").has("prop_2", eq("Amsterdam"))
+						has("classId", "class_18").has("prop_1", eq("Amsterdam"))
+					)
+				).valueMap("uuid", "classId")
+			`,
+		},
+		{
+			name: "with the field type not matching the property type",
+			inputParams: fetch.Params{
+				Kind: kind.THING_KIND,
+				PossibleClassNames: contextionary.SearchResults{
+					Results: []contextionary.SearchResult{
+						{
+							Name:      "City",
+							Certainty: 1.0,
+						},
+					},
+				},
+				Properties: []fetch.Property{
+					{
+						PossibleNames: contextionary.SearchResults{
+							Results: []contextionary.SearchResult{
+								{
+									Name:      "name", // actually is a string prop, should be included
+									Certainty: 1.0,
+								},
+								{
+									Name:      "population", // is an int prop, so should not be included
+									Certainty: 1.0,
+								},
+							},
+						},
+						Match: fetch.PropertyMatch{
+							Operator: common_filters.OperatorEqual,
+							Value: &common_filters.Value{
+								Value: "Amsterdam",
+								Type:  schema.DataTypeString,
+							},
+						},
+					},
+				},
+			},
+			expectedQuery: `
+				g.V().has("kind", "thing").and(
+					or(
+						has("classId", "class_18").has("prop_1", eq("Amsterdam"))
 					)
 				).valueMap("uuid", "classId")
 			`,
@@ -73,11 +117,11 @@ func Test_QueryBuilder(t *testing.T) {
 						PossibleNames: contextionary.SearchResults{
 							Results: []contextionary.SearchResult{
 								{
-									Name:      "population",
+									Name:      "name",
 									Certainty: 1.0,
 								},
 								{
-									Name:      "inhabitants",
+									Name:      "title",
 									Certainty: 1.0,
 								},
 							},
@@ -95,8 +139,8 @@ func Test_QueryBuilder(t *testing.T) {
 			expectedQuery: `
 				g.V().has("kind", "thing").and(
 					or(
-						has("classId", "class_18").has("prop_2", eq("Amsterdam")),
-						has("classId", "class_19").has("prop_12", eq("Amsterdam"))
+						has("classId", "class_18").has("prop_1", eq("Amsterdam")),
+						has("classId", "class_19").has("prop_11", eq("Amsterdam"))
 					)
 				).valueMap("uuid", "classId")
 			`,
