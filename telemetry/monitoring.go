da677d5880354642cc1264d24131b407685c0a7d
Jeroen Stravers <jeroen.stravers@kpn.com>
2019-02-21 18:28:18 +0100

    gh-699: implemented and applied constructor funcs

--- telemetry/monitoring.go
+++ telemetry/monitoring.go
@@ -7,14 +7,22 @@ import (
 const Failed int = 0
 const Succeeded int = 1
 
-// A struct used to log the type and amount of POST and GQL requests this Weaviate receives.
+// A struct used to log the type and amount of POST and GQL Requests this Weaviate receives.
 // Contains a map and a mutex used to control access to this map.
 type RequestsLog struct { // TODO: RENAME
 	Mutex *sync.Mutex
 	Log   map[string]*RequestLog
 }
 
-/* Reset the hashmap used to log performed requests. */
+// Create a new Requestslog
+func NewLog() *RequestsLog {
+	return &RequestsLog{
+		Mutex: &sync.Mutex{},
+		Log:   make(map[string]*RequestLog),
+	}
+}
+
+// Reset the hashmap used to log performed Requests.
 func (r *RequestsLog) Reset(telemetryEnabled bool) int {
 	if telemetryEnabled {
 		r.Mutex.Lock()
@@ -25,7 +33,7 @@ func (r *RequestsLog) Reset(telemetryEnabled bool) int {
 	return Failed
 }
 
-/* Register a performed request. Either creates a new entry or updates an existing one. */
+// Register a performed Request. Either creates a new entry or updates an existing one.
 func (r *RequestsLog) Register(request *RequestLog, telemetryEnabled bool) int {
 	if telemetryEnabled {
 
@@ -41,12 +49,22 @@ func (r *RequestsLog) Register(request *RequestLog, telemetryEnabled bool) int {
 	return Failed
 }
 
-// A struct used both for logging new request type calls and for counting the total amount of calls per
-// request type in a RequestsLog. Contains all relevant details of an individual request type.
+// A struct used both for logging new Request type calls and for counting the total amount of calls per
+// Request type in a RequestsLog. Contains all relevant details of an individual Request type.
 type RequestLog struct {
 	Name       string // name of the Weaviate instance. Is `a-b-c` where a, b, and c are random words from the contextionary.
 	Type       string // "GQL" or "POST"
-	Identifier string // name of the request; "weaviate.x.y.z"
+	Identifier string // name of the Request; "weaviate.x.y.z"
 	Amount     int    // how often the function was called
 	When       int64  // timestamp in epoch
+}
+
+// Note `When` attribute is not set here; the timestamp is provided separately at the moment of POSTing
+func NewRequestTypeLog(name string, requestType string, identifier string, amount int) *RequestLog {
+	return &RequestLog{
+		Name:       name,
+		Type:       requestType,
+		Identifier: identifier,
+		Amount:     amount,
+	}
 }
