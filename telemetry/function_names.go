7b2938b734d187f8892c9aae26d97345edd37066
Jeroen Stravers <jeroen.stravers@kpn.com>
2019-02-22 09:49:57 +0100

    gh-699: added serviceid constants

---
+++ telemetry/function_names.go
@@ -0,0 +1,17 @@
+package telemetry
+
+import ()
+
+const LocalQuery string = "weaviate.local.query"
+const LocalQueryMeta string = "weaviate.local.query.meta"
+
+const NetworkQuery string = "Weaviate.network.query"
+const NetworkQueryMeta string = "weaviate.network.query.meta"
+
+const LocalAdd string = "weaviate.local.add"
+const LocalAddMeta string = "weaviate.local.add.meta"
+
+const LocalManipulate string = "weaviate.local.manipulate"
+const LocalManipulateMeta string = "weaviate.local.manipulate.meta"
+
+const NetworkToolsMap string = "weaviate.network.tools.map"
