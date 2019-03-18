e91e6cb306a79e472d01b6ac0c4cd7e82bf7bb27
Etienne Dilocker <etienne@dilocker.de>
2019-03-18 17:52:49 +0100

    gh-699: add dummy resolver for Network Aggregate

---
+++ graphqlapi/network/aggregate/resolver.go
@@ -0,0 +1,13 @@
+package aggregate
+
+import (
+	"fmt"
+
+	"github.com/graphql-go/graphql"
+)
+
+func resolveClass(params graphql.ResolveParams) (interface{}, error) {
+	// do logging here
+
+	return nil, fmt.Errorf("resolve network aggregate class field not yet supported")
+}
