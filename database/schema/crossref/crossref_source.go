9d5082371b5efda34bf70cc5b4c8612edc7bfba4
Etienne Dilocker <etienne@dilocker.de>
2019-03-19 11:27:53 +0100

    gh-788: add RefSource construct
    It's very similar to crossref.Ref{} only that it requires a source
    property to be set. We will be able to parse the "from" in the batch
    resources body into the new RefSource

---
+++ database/schema/crossref/crossref_source.go
@@ -0,0 +1,98 @@
+/*                          _       _
+ *__      _____  __ ___   ___  __ _| |_ ___
+ *\ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+ * \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+ *  \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+ *
+ * Copyright © 2016 - 2019 Weaviate. All rights reserved.
+ * LICENSE: https://github.com/creativesoftwarefdn/weaviate/blob/develop/LICENSE.md
+ * DESIGN & CONCEPT: Bob van Luijt (@bobvanluijt)
+ * CONTACT: hello@creativesoftwarefdn.org
+ */
+package crossref
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/creativesoftwarefdn/weaviate/database/schema"
+	"github.com/creativesoftwarefdn/weaviate/database/schema/kind"
+	"github.com/go-openapi/strfmt"
+)
+
+// RefSource is an abstraction of the source of a cross-ref. The opposite would
+// be Ref which represents the target insted. A RefSource is specified in a URI
+// format in the API. When this type is used it is safe to assume that a Ref is
+// semantically valid. This guarantuee would not be possible on the URI format,
+// as the URI can be well-formed, but not contain the data we expect in it.  Do
+// not use directly, such as crossref.RefSource{}, as you won't have any
+// guarantees in this case. Always use one of the parsing options or New()
+type RefSource struct {
+	Local    bool
+	PeerName string
+	Property schema.PropertyName
+	TargetID strfmt.UUID
+	Kind     kind.Kind
+}
+
+// ParseSource is a safe way to generate a RefSource, as it will error if any
+// of the input parameters are not as expected.
+func ParseSource(uriString string) (*RefSource, error) {
+	uri, err := url.Parse(uriString)
+	if err != nil {
+		return nil, fmt.Errorf("invalid cref URI: %s", err)
+	}
+
+	pathSegments := strings.Split(uri.Path, "/")
+	if len(pathSegments) != 4 {
+		return nil, fmt.Errorf(
+			"invalid cref URI: path must be of format '/{things,actions}/<uuid>/<propertyName>', but got '%s'",
+			uri.Path)
+	}
+
+	if ok := strfmt.IsUUID(pathSegments[2]); !ok {
+		return nil, fmt.Errorf("invalid cref URI: 2nd path segment must be uuid, but got '%s'",
+			pathSegments[2])
+	}
+
+	k, err := parseKind(pathSegments[1])
+	if err != nil {
+		return nil, fmt.Errorf("invalid cref URI: %s", err)
+	}
+
+	property := pathSegments[3]
+	if property == "" {
+		return nil, fmt.Errorf("property cannot be empty")
+	}
+
+	return &RefSource{
+		Local:    (uri.Host == "localhost"),
+		PeerName: uri.Host,
+		TargetID: strfmt.UUID(pathSegments[2]),
+		Kind:     k,
+		Property: schema.PropertyName(pathSegments[3]),
+	}, nil
+}
+
+func (r *RefSource) String() string {
+	uri := url.URL{
+		Host:   r.PeerName,
+		Scheme: "weaviate",
+		Path:   fmt.Sprintf("/%s/%s/%s", pluralizeKindName(r.Kind), r.TargetID, r.Property),
+	}
+
+	return uri.String()
+}
+
+// func pluralizeKindName(k kind.Kind) string {
+// 	return strings.ToLower(k.Name()) + "s"
+// }
+
+// // SingleRef converts the parsed Ref back into the API helper construct
+// // containing a stringified representation (URI format) of the Ref
+// func (r *Ref) SingleRef() *models.SingleRef {
+// 	return &models.SingleRef{
+// 		NrDollarCref: strfmt.URI(r.String()),
+// 	}
+// }
