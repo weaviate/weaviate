1a400ba146ee5330f9f2c816f15bb04da78eb1cb
Etienne Dilocker <etienne@dilocker.de>
2019-03-19 11:27:53 +0100

    gh-788: add RefSource construct
    It's very similar to crossref.Ref{} only that it requires a source
    property to be set. We will be able to parse the "from" in the batch
    resources body into the new RefSource

---
+++ database/schema/crossref/crossref_source_test.go
@@ -0,0 +1,155 @@
+/*                          _       _
+ *__      _____  __ ___   ___  __ _| |_ ___
+ *\ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+ * \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+ *  \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+ *
+ * Copyright © 2016 - 2019 Weaviate. All rights reserved.
+ * LICENSE: https://github.com/creativesoftwarefdn/weaviate/blob/develop/LICENSE.md
+ * DESIGN & CONCEPT: Bob van Luijt (@bobvanluijt)
+ * CONTACT: hello@creativesoftwarefdn.org
+ */
+package crossref
+
+import (
+	"testing"
+
+	"github.com/creativesoftwarefdn/weaviate/database/schema"
+	"github.com/creativesoftwarefdn/weaviate/database/schema/kind"
+	"github.com/go-openapi/strfmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Source_ParsingFromString(t *testing.T) {
+	t.Run("from a local thing ref that is well-formed", func(t *testing.T) {
+		uri := "weaviate://localhost/things/c2cd3f91-0160-477e-869a-8da8829e0a4d/myRefProp"
+		ref, err := ParseSource(uri)
+
+		require.Nil(t, err, "should not error")
+
+		t.Run("is a local ref", func(t *testing.T) {
+			assert.Equal(t, ref.Local, true)
+		})
+
+		t.Run("peerName points to localhost", func(t *testing.T) {
+			assert.Equal(t, ref.PeerName, "localhost")
+		})
+
+		t.Run("id points correctly", func(t *testing.T) {
+			assert.Equal(t, ref.TargetID, strfmt.UUID("c2cd3f91-0160-477e-869a-8da8829e0a4d"))
+		})
+
+		t.Run("the kind is 'thing'", func(t *testing.T) {
+			assert.Equal(t, ref.Kind, kind.THING_KIND)
+		})
+
+		t.Run("the property name is correct", func(t *testing.T) {
+			assert.Equal(t, ref.Property, schema.PropertyName("myRefProp"))
+		})
+	})
+
+	t.Run("from a local action ref that is well-formed", func(t *testing.T) {
+		uri := "weaviate://localhost/actions/c2cd3f91-0160-477e-869a-8da8829e0a4d/myRefProp"
+		ref, err := ParseSource(uri)
+
+		require.Nil(t, err, "should not error")
+
+		t.Run("is a local ref", func(t *testing.T) {
+			assert.Equal(t, ref.Local, true)
+		})
+
+		t.Run("peerName points to localhost", func(t *testing.T) {
+			assert.Equal(t, ref.PeerName, "localhost")
+		})
+
+		t.Run("id points correctly", func(t *testing.T) {
+			assert.Equal(t, ref.TargetID, strfmt.UUID("c2cd3f91-0160-477e-869a-8da8829e0a4d"))
+		})
+
+		t.Run("the kind is 'thing'", func(t *testing.T) {
+			assert.Equal(t, ref.Kind, kind.ACTION_KIND)
+		})
+
+		t.Run("the property name is correct", func(t *testing.T) {
+			assert.Equal(t, ref.Property, schema.PropertyName("myRefProp"))
+		})
+	})
+
+	t.Run("from a network action ref that is well-formed", func(t *testing.T) {
+		uri := "weaviate://another-weaviate/actions/c2cd3f91-0160-477e-869a-8da8829e0a4d/myRefProp"
+		ref, err := ParseSource(uri)
+
+		require.Nil(t, err, "should not error")
+
+		t.Run("is a local ref", func(t *testing.T) {
+			assert.Equal(t, ref.Local, false)
+		})
+
+		t.Run("peerName points to localhost", func(t *testing.T) {
+			assert.Equal(t, ref.PeerName, "another-weaviate")
+		})
+
+		t.Run("id points correctly", func(t *testing.T) {
+			assert.Equal(t, ref.TargetID, strfmt.UUID("c2cd3f91-0160-477e-869a-8da8829e0a4d"))
+		})
+
+		t.Run("the kind is 'thing'", func(t *testing.T) {
+			assert.Equal(t, ref.Kind, kind.ACTION_KIND)
+		})
+
+		t.Run("the property name is correct", func(t *testing.T) {
+			assert.Equal(t, ref.Property, schema.PropertyName("myRefProp"))
+		})
+	})
+
+	t.Run("with formatting errors", func(t *testing.T) {
+
+		type testCaseError struct {
+			name string
+			uri  string
+		}
+
+		tests := []testCaseError{
+			{
+				name: "with an invalid URL",
+				uri:  "i:am:not:a:url",
+			},
+			{
+				name: "with too few path segments",
+				uri:  "weaviate://localhost/things",
+			},
+			{
+				name: "with too many path segments",
+				uri:  "weaviate://localhost/things/c2cd3f91-0160-477e-869a-8da8829e0a4d/myRefProp/somethingElse",
+			},
+			{
+				name: "without a property",
+				uri:  "weaviate://localhost/things/c2cd3f91-0160-477e-869a-8da8829e0a4d/",
+			},
+			{
+				name: "with an invalid uuid",
+				uri:  "weaviate://localhost/things/c2cd3f91-iSneakedInHere-477e-869a-8da8829e0a4d",
+			},
+			{
+				name: "with an invalid kind",
+				uri:  "weaviate://localhost/humans/c2cd3f91-0160-477e-869a-8da8829e0a4d",
+			},
+		}
+
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				_, err := ParseSource(test.uri)
+				assert.NotNil(t, err, test.name)
+			})
+		}
+	})
+}
+
+func Test_Source_GenerateString(t *testing.T) {
+	uri := "weaviate://localhost/things/c2cd3f91-0160-477e-869a-8da8829e0a4d/myRefProp"
+	ref, err := ParseSource(uri)
+
+	require.Nil(t, err, "should not error")
+	assert.Equal(t, uri, ref.String(), "should be the same as the input string")
+}
