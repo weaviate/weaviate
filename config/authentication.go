bd820901a7e4a171830f3b418ffb98199cd14a76
Etienne Dilocker <etienne@dilocker.de>
2019-03-21 17:32:27 +0100

    gh-708: validate jwt, extract username from jwt if valid
    This involves an elaborate test setup which allows us to spin up a whole
    OIDC token server from a unit test using just a simple httptest.Server

--- config/authentication.go
+++ config/authentication.go
@@ -7,5 +7,10 @@ type Authentication struct {
 
 // OIDC configures the OIDC middleware
 type OIDC struct {
-	Enabled bool `json:"enabled" yaml:"enabled"`
+	Enabled                bool   `json:"enabled" yaml:"enabled"`
+	Issuer                 string `json:"issuer" yaml:"issuer"`
+	ClientID               string `json:"client_id" yaml:"client_id"`
+	SkipAudienceValidation bool   `json:"skip_audience_validation" yaml:"skip_audience_validation"`
+	UsernameClaim          string `yaml:"username_claim" json:"username_claim"`
+	GroupsClaim            string `yaml:"groups_claim" json:"groups_claim"`
 }
