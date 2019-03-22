fbdc50a5e086a20f5b7c93d73c5e455cf8596248
Jeroen Stravers <jeroen.stravers@kpn.com>
2019-03-22 18:20:48 +0100

    gh-803: wip. acceptance test almost done, only need to parse cbor back to map[string]interface{}

--- tools/mock_api/mock_api.go
+++ tools/mock_api/mock_api.go
@@ -29,15 +29,16 @@ func main() {
 
 	router.HandleFunc("/mock/new", AddLog).Methods("POST")
 	router.HandleFunc("/mock/last", GetMostRecentLog).Methods("GET")
-	router.HandleFunc("/mock/count", GetMostRecentLog).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8087", router))
 }
 
 var receivedLogs [][]byte
 var mostRecentLog []byte
-var counter int
 
-// AddLog stores a POSTed log.
+// EmptyRequestSize is the length of an empty log converted to a byte array (cbor)
+const EmptyRequestSize int = 3
+
+// AddLog stores the most recently received non-empty POSTed log.
 func AddLog(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("received POST")
 
@@ -45,10 +46,8 @@ func AddLog(w http.ResponseWriter, r *http.Request) {
 	buf.ReadFrom(r.Body)
 	content := buf.Bytes()
 
-	if len(content) > 0 {
+	if len(content) > EmptyRequestSize {
 		fmt.Println(content)
-		fmt.Println("Non-null requests received: %s", counter)
-		counter++
 		receivedLogs = append(receivedLogs, content)
 		mostRecentLog = content
 	}
@@ -57,5 +56,6 @@ func AddLog(w http.ResponseWriter, r *http.Request) {
 // GetMostRecentLog returns the most recently received log.
 func GetMostRecentLog(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("received GET")
+	fmt.Println(mostRecentLog)
 	w.Write(mostRecentLog)
 }
