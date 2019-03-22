3a8a67f290c12b500ac546a54ec6701cfb135b4f
Etienne Dilocker <etienne@dilocker.de>
2019-03-22 09:38:43 +0100

    gh-801: extend oidc middleware to extract all relevant claims
    A username MUST be extracted from the claim that is configured, a groups
    claim CAN be extracted if it is provided.

--- auth/authentication/oidc/middleware.go
+++ auth/authentication/oidc/middleware.go
@@ -66,7 +66,68 @@ func (c *Client) ValidateAndExtract(token string, scopes []string) (*models.Prin
 		return nil, errors.New(401, err.Error())
 	}
 
+	claims, err := c.extractClaims(parsed)
+	if err != nil {
+		return nil, errors.New(500, fmt.Sprintf("oidc: %v", err))
+	}
+
+	username, err := c.extractUsername(claims)
+	if err != nil {
+		return nil, errors.New(500, fmt.Sprintf("oidc: %v", err))
+	}
+
+	groups := c.extractGroups(claims)
+
 	return &models.Principal{
-		Username: parsed.Subject,
+		Username: username,
+		Groups:   groups,
 	}, nil
+}
+
+func (c *Client) extractClaims(token *oidc.IDToken) (map[string]interface{}, error) {
+	var claims map[string]interface{}
+	if err := token.Claims(&claims); err != nil {
+		return nil, fmt.Errorf("could not extract claims from token: %v", err)
+	}
+
+	return claims, nil
+}
+
+func (c *Client) extractUsername(claims map[string]interface{}) (string, error) {
+	usernameUntyped, ok := claims[c.config.UsernameClaim]
+	if !ok {
+		return "", fmt.Errorf("token doesn't contain required claim '%s'", c.config.UsernameClaim)
+	}
+
+	username, ok := usernameUntyped.(string)
+	if !ok {
+		return "", fmt.Errorf("claim '%s' is not a string, but %T", c.config.UsernameClaim, usernameUntyped)
+	}
+
+	return username, nil
+}
+
+// extractGroups never errors, if groups cant be parsed an empty set of groups
+// is returned. This is because groups are not a required standard in the OIDC
+// spec, so we can't error if an OIDC provider does not support them.
+func (c *Client) extractGroups(claims map[string]interface{}) []string {
+	var groups []string
+
+	groupsUntyped, ok := claims[c.config.GroupsClaim]
+	if !ok {
+		return groups
+	}
+
+	groupsSlice, ok := groupsUntyped.([]interface{})
+	if !ok {
+		return groups
+	}
+
+	for _, untyped := range groupsSlice {
+		if group, ok := untyped.(string); ok {
+			groups = append(groups, group)
+		}
+	}
+
+	return groups
 }
