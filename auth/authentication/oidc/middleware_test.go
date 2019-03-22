aaed063a8723e9d987220032631e9f81b52e9953
Etienne Dilocker <etienne@dilocker.de>
2019-03-22 09:38:43 +0100

    gh-801: extend oidc middleware to extract all relevant claims
    A username MUST be extracted from the claim that is configured, a groups
    claim CAN be extracted if it is provided.

--- auth/authentication/oidc/middleware_test.go
+++ auth/authentication/oidc/middleware_test.go
@@ -31,42 +31,117 @@ func Test_Middleware_NotConfigured(t *testing.T) {
 
 type claims struct {
 	jwt.StandardClaims
+	Email  string   `json:"email"`
+	Groups []string `json:"groups"`
 }
 
 func Test_Middleware_WithValidToken(t *testing.T) {
-	server := newOIDCServer(t)
-	defer server.Close()
+	t.Run("without groups set", func(t *testing.T) {
+		server := newOIDCServer(t)
+		defer server.Close()
 
-	cfg := config.Environment{
-		Authentication: config.Authentication{
-			OIDC: config.OIDC{
-				Enabled:                true,
-				Issuer:                 server.URL,
-				ClientID:               "best_client",
-				SkipAudienceValidation: false,
-				UsernameClaim:          "sub",
-				GroupsClaim:            "groups",
+		cfg := config.Environment{
+			Authentication: config.Authentication{
+				OIDC: config.OIDC{
+					Enabled:                true,
+					Issuer:                 server.URL,
+					ClientID:               "best_client",
+					SkipAudienceValidation: false,
+					UsernameClaim:          "sub",
+					GroupsClaim:            "groups",
+				},
 			},
-		},
-	}
+		}
 
-	token := token(t, "best-user", server.URL, "best_client")
-	client, err := New(cfg)
-	require.Nil(t, err)
+		token := token(t, "best-user", server.URL, "best_client")
+		client, err := New(cfg)
+		require.Nil(t, err)
 
-	principal, err := client.ValidateAndExtract(token, []string{})
-	require.Nil(t, err)
-	assert.Equal(t, "best-user", principal.Username)
+		principal, err := client.ValidateAndExtract(token, []string{})
+		require.Nil(t, err)
+		assert.Equal(t, "best-user", principal.Username)
+	})
+
+	t.Run("with a non-standard username claim", func(t *testing.T) {
+		server := newOIDCServer(t)
+		defer server.Close()
+
+		cfg := config.Environment{
+			Authentication: config.Authentication{
+				OIDC: config.OIDC{
+					Enabled:                true,
+					Issuer:                 server.URL,
+					ClientID:               "best_client",
+					SkipAudienceValidation: false,
+					UsernameClaim:          "email",
+					GroupsClaim:            "groups",
+				},
+			},
+		}
+
+		token := tokenWithEmail(t, "best-user", server.URL, "best_client", "foo@bar.com")
+		client, err := New(cfg)
+		require.Nil(t, err)
+
+		principal, err := client.ValidateAndExtract(token, []string{})
+		require.Nil(t, err)
+		assert.Equal(t, "foo@bar.com", principal.Username)
+	})
+
+	t.Run("with groups claim", func(t *testing.T) {
+		server := newOIDCServer(t)
+		defer server.Close()
+
+		cfg := config.Environment{
+			Authentication: config.Authentication{
+				OIDC: config.OIDC{
+					Enabled:                true,
+					Issuer:                 server.URL,
+					ClientID:               "best_client",
+					SkipAudienceValidation: false,
+					UsernameClaim:          "sub",
+					GroupsClaim:            "groups",
+				},
+			},
+		}
+
+		token := tokenWithGroups(t, "best-user", server.URL, "best_client", []string{"group1", "group2"})
+		client, err := New(cfg)
+		require.Nil(t, err)
+
+		principal, err := client.ValidateAndExtract(token, []string{})
+		require.Nil(t, err)
+		assert.Equal(t, "best-user", principal.Username)
+		assert.Equal(t, []string{"group1", "group2"}, principal.Groups)
+	})
 }
 
 func token(t *testing.T, subject string, issuer string, aud string) string {
+	return tokenWithEmail(t, subject, issuer, aud, "")
+}
+
+func tokenWithEmail(t *testing.T, subject string, issuer string, aud string, email string) string {
 	claims := claims{
-		jwt.StandardClaims{
-			Subject:   subject,
-			Issuer:    issuer,
-			Audience:  aud,
-			ExpiresAt: time.Now().Add(10 * time.Second).Unix(),
-		},
+		Email: email,
+	}
+
+	return tokenWithClaims(t, subject, issuer, aud, claims)
+}
+
+func tokenWithGroups(t *testing.T, subject string, issuer string, aud string, groups []string) string {
+	claims := claims{
+		Groups: groups,
+	}
+
+	return tokenWithClaims(t, subject, issuer, aud, claims)
+}
+
+func tokenWithClaims(t *testing.T, subject string, issuer string, aud string, claims claims) string {
+	claims.StandardClaims = jwt.StandardClaims{
+		Subject:   subject,
+		Issuer:    issuer,
+		Audience:  aud,
+		ExpiresAt: time.Now().Add(10 * time.Second).Unix(),
 	}
 
 	token, err := signToken(claims)
