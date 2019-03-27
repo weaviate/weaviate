a0d50459b4ca2c19864b198e17e42da4cf6a414c
Etienne Dilocker <etienne@dilocker.de>
2019-03-27 17:44:04 +0100

    gh-709: proxy fetch query to all peers concurrently
    There is currently no timeout in place.

--- network/p2p/graphql_resolver_test.go
+++ network/p2p/graphql_resolver_test.go
@@ -16,11 +16,14 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"sync"
 	"testing"
 
 	"github.com/creativesoftwarefdn/weaviate/graphqlapi/network/common"
+	"github.com/creativesoftwarefdn/weaviate/models"
 	"github.com/creativesoftwarefdn/weaviate/network/common/peers"
 	"github.com/go-openapi/strfmt"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestProxyGetInstance(t *testing.T) {
@@ -336,6 +339,143 @@ func TestProxyAggregateInstance(t *testing.T) {
 		}
 		arrange(matcher)
 		act()
+		cleanUp()
+	})
+
+	t.Run("should form a local query from the subquery in the request body", func(t *testing.T) {
+		matcher := func(t *testing.T, r *http.Request) {
+			expectedBody := fmt.Sprintf("%s\n", `{"query":"{ Local { Aggregate { WeaviateB { Things { City { population { count } } } } } }"}`)
+			defer r.Body.Close()
+			bodyBytes, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				t.Errorf("reading the request body returned an error: %s", err)
+			}
+
+			actualBody := string(bodyBytes)
+			if actualBody != expectedBody {
+				t.Errorf("expected body to be \n%#v\n, but was \n%#v\n", expectedBody, actualBody)
+			}
+		}
+		arrange(matcher)
+		act()
+		cleanUp()
+	})
+
+	// re-enable when we have auth again
+	// t.Run("should proxy along the key and token headers", func(t *testing.T) {
+	// 	matcher := func(t *testing.T, r *http.Request) {
+	// 		key := r.Header.Get("X-API-KEY")
+	// 		token := r.Header.Get("X-API-TOKEN")
+
+	// 		expectedKey := "stand-in-for-key-id-uuid"
+	// 		if key != expectedKey {
+	// 			t.Errorf("expected key to be \n%#v\n, but was \n%#v\n", expectedKey, key)
+	// 		}
+
+	// 		expectedToken := "stand-in-for-token-uuid"
+	// 		if token != expectedToken {
+	// 			t.Errorf("expected token to be \n%#v\n, but was \n%#v\n", expectedToken, token)
+	// 		}
+	// 	}
+	// 	arrange(matcher)
+	// 	act()
+	// 	cleanUp()
+	// })
+}
+
+func TestProxyFetch(t *testing.T) {
+	var (
+		subject *network
+		remote1 *httptest.Server
+		remote2 *httptest.Server
+		err     error
+		results []*models.GraphQLResponse
+	)
+
+	arrange := func(matchers ...requestMatcher) {
+		remote1 = fakeRemoteInstanceWithGraphQL(t, matchers...)
+		remote2 = fakeRemoteInstanceWithGraphQL(t, matchers...)
+		subject = &network{
+			peers: []peers.Peer{{
+				Name: "best-instance",
+				URI:  strfmt.URI(remote1.URL),
+				ID:   strfmt.UUID("some-id"),
+			}, {
+				Name: "worst-instance",
+				URI:  strfmt.URI(remote2.URL),
+				ID:   strfmt.UUID("some-other-id"),
+			}},
+		}
+	}
+
+	act := func() {
+		results, err = subject.ProxyFetch(
+			common.SubQuery(`Aggregate { WeaviateB { Things { City { population { count } } } }`),
+		)
+	}
+
+	cleanUp := func() {
+		remote1.Close()
+		remote2.Close()
+	}
+
+	t.Run("should not error", func(t *testing.T) {
+		arrange()
+		act()
+
+		if err != nil {
+			t.Errorf("should not error, but got %s", err)
+		}
+
+		cleanUp()
+	})
+
+	t.Run("handler should be called", func(t *testing.T) {
+		called := false
+		m := &sync.Mutex{}
+		matcher := func(t *testing.T, r *http.Request) {
+			m.Lock()
+			defer m.Unlock()
+			called = true
+		}
+		arrange(matcher)
+		act()
+
+		if called == false {
+			t.Error("handler was never called")
+		}
+
+		cleanUp()
+	})
+
+	t.Run("should be post request", func(t *testing.T) {
+		matcher := func(t *testing.T, r *http.Request) {
+			if r.Method != "POST" {
+				t.Errorf("expected method to be POST, but got %s", r.Method)
+			}
+		}
+		arrange(matcher)
+		act()
+		cleanUp()
+	})
+
+	t.Run("should call correct url path", func(t *testing.T) {
+		matcher := func(t *testing.T, r *http.Request) {
+			expectedPath := "/weaviate/v1/graphql"
+			if r.URL.Path != expectedPath {
+				t.Errorf("expected path to be %s, but was %s", expectedPath, r.URL.Path)
+			}
+		}
+		arrange(matcher)
+		act()
+		cleanUp()
+	})
+
+	t.Run("should return the correct results", func(t *testing.T) {
+		arrange()
+		act()
+		_ = err
+		assert.Len(t, results, 2)
 		cleanUp()
 	})
 
