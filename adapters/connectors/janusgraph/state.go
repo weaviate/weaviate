ad60cd23e5f6b41c3bc32b03b3781e2edfe856e3
Etienne Dilocker <etienne@dilocker.de>
2019-04-20 19:04:46 +0200

    gh-837 add connector state manager (and its repo)

--- adapters/connectors/janusgraph/state.go
+++ adapters/connectors/janusgraph/state.go
@@ -14,27 +14,26 @@ package janusgraph
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 )
 
-// Called by a connector when it has updated it's internal state that needs to be shared across all connectors in other Weaviate instances.
-func (j *Janusgraph) SetState(ctx context.Context, state json.RawMessage) {
-	err := json.Unmarshal(state, &j.state)
+// // Called by a connector when it has updated it's internal state that needs to be shared across all connectors in other Weaviate instances.
+// func (j *Janusgraph) SetState(ctx context.Context, state json.RawMessage) {
+// 	err := json.Unmarshal(state, &j.state)
 
-	// Extra assertions if the connector has been initialized already.
-	if j.initialized {
-		if err != nil {
-			panic(fmt.Sprintf("Could not deserialize a schema update, after Weaviate was initialized. Are you running multiple versions of Weaviate in the same cluster? Reason: %s", err.Error()))
-		}
-		if j.state.Version != SCHEMA_VERSION {
-			panic(fmt.Sprintf("Received a schema update of version %v. We can only handle schema version %v. Are you running multiple versions of Weaviate in the same cluster?", j.state.Version, SCHEMA_VERSION))
-		}
-	} else {
-		if err != nil {
-			panic(fmt.Sprintf("Received an illegal JSON document as the connector state during initialization. Cannot recover from this. Error: %v", err))
-		}
-	}
-}
+// 	// Extra assertions if the connector has been initialized already.
+// 	if j.initialized {
+// 		if err != nil {
+// 			panic(fmt.Sprintf("Could not deserialize a schema update, after Weaviate was initialized. Are you running multiple versions of Weaviate in the same cluster? Reason: %s", err.Error()))
+// 		}
+// 		if j.state.Version != SCHEMA_VERSION {
+// 			panic(fmt.Sprintf("Received a schema update of version %v. We can only handle schema version %v. Are you running multiple versions of Weaviate in the same cluster?", j.state.Version, SCHEMA_VERSION))
+// 		}
+// 	} else {
+// 		if err != nil {
+// 			panic(fmt.Sprintf("Received an illegal JSON document as the connector state during initialization. Cannot recover from this. Error: %v", err))
+// 		}
+// 	}
+// }
 
 func (j *Janusgraph) UpdateStateInStateManager(ctx context.Context) {
 	rawState, err := json.Marshal(j.state)
--- database/connector_state/connector_state.go
+++ database/connector_state/connector_state.go
@@ -22,18 +22,15 @@ type StateManager interface {
 	SetState(ctx context.Context, state json.RawMessage) error
 
 	// Used by a connector to get the initial state.
-	GetInitialConnectorState() json.RawMessage
+	GetInitialState() json.RawMessage
 
 	// Link a connector to this state manager.
 	// When the internal state of some connector is updated, this state connector will call SetState on the provided conn.
-	SetStateConnector(conn Connector)
+	// SetStateConnector(conn Connector)
 }
 
 // Implemented by a connector
 type Connector interface {
-	// Called by the state manager, when an external state update is happening.
-	SetState(ctx context.Context, state json.RawMessage)
-
 	// Link a StateManager to this connector, so that when a connector is updating it's own state, it can propagate these changes to othr Weaviate instances.
 	SetStateManager(manager StateManager)
 }
