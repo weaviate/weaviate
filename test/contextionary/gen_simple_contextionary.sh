7a14e589adfc46454877f62b042c8960b84bf13a
Etienne Dilocker <etienne@dilocker.de>
2019-05-15 18:28:12 +0200

    gh-849: add simple way to fake a dev c11y without stopwords
    should be replaced with a new release of the dev c11y once we have it

--- test/contextionary/gen_simple_contextionary.sh
+++ test/contextionary/gen_simple_contextionary.sh
@@ -8,6 +8,16 @@ else
   bunzip2 -k test/contextionary/en_test-vectors-small.txt.bz2
 fi
 
+# Fake stopword removal by removing the first 10 words. This will become
+# obsolete once we have released a new minimal c11y
+
+# build stopword.json
+cat test/contextionary/en_test-vectors-small.txt | head | \
+  while read -r word _; do echo "$word"; done | jq -nR '[inputs | select(length>0)] | { language: "en", words: . }'  > test/contextionary/stopwords.json
+
+# remove stop words
+sed -i.bak 1,10d test/contextionary/en_test-vectors-small.txt && rm test/contextionary/en_test-vectors-small.txt.bak
+
 if [ -f test/contextionary/example.knn ]; then
   echo "Fixture contextionary already generated"
 else
