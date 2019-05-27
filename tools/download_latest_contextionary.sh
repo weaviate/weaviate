3c7453f040726b027b3b3ce19663a24a99f6b2d1
Etienne Dilocker <etienne@dilocker.de>
2019-05-27 09:11:15 +0200

    gh-849: include stopwords file in production image

--- tools/download_latest_contextionary.sh
+++ tools/download_latest_contextionary.sh
@@ -4,19 +4,21 @@
 # Run in $WEAVIATE/ root folder
 ##
 
+VERSION=0.6.0
+
 # Create contextionary dir if not available
 mkdir -p ./contextionary
 
 # Download the latest files and remove old ones
-for SINGLEEXT in idx knn; do
-    echo "Start Downloading $SINGLEEXT" && \
-    rm -f ./contextionary/contextionary.$SINGLEEXT && \
-    wget --quiet -O ./contextionary/contextionary.$SINGLEEXT https://c11y.semi.technology/0.4.0/en/contextionary.$SINGLEEXT && \
-    echo "$SINGLEEXT file = done" &
+for FILE in stopwords.json contextionary.idx contextionary.knn; do
+    echo "Start Downloading $FILE" && \
+    rm -f ./contextionary/$FILE && \
+    wget --quiet -O ./contextionary/$FILE https://c11y.semi.technology/$VERSION/en/$FILE && \
+    echo "$FILE = done" &
 done 
 
-# Wiat to finish download
+# Wait to finish download
 wait
 
-echo "Done downloading the open source contextionary."
+echo "Done downloading open source contextionary v$VERSION."
 exit 0
