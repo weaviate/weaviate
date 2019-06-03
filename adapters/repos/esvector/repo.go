127d5571d8385861d97b836c2f97bfde74129ae1
Etienne Dilocker <etienne@dilocker.de>
2019-06-03 10:53:55 +0200

    gh-888 make es settings idempotent
    and verify with integration tests

--- adapters/repos/esvector/repo.go
+++ adapters/repos/esvector/repo.go
@@ -11,8 +11,7 @@ import (
 )
 
 const (
-	index      = "weaviate"
-	doctype    = "concept"
+	doctype    = "doc"
 	vectorProp = "embedding_vector"
 )
 
@@ -27,8 +26,71 @@ func NewRepo(client *elasticsearch.Client) *Repo {
 	return &Repo{client}
 }
 
+// PutIndex idempotently creates an index
+func (r *Repo) PutIndex(ctx context.Context, index string) error {
+
+	ok, err := r.indexExists(ctx, index)
+	if err != nil {
+		return fmt.Errorf("create index: %v", err)
+	}
+
+	if ok {
+		return nil
+	}
+
+	body := map[string]interface{}{
+		"settings": map[string]interface{}{
+			"index.mapping.single_type": true,
+		},
+	}
+
+	var buf bytes.Buffer
+	err = json.NewEncoder(&buf).Encode(body)
+	if err != nil {
+		return fmt.Errorf("create index: %v", err)
+	}
+
+	req := esapi.IndicesCreateRequest{
+		Index: index,
+		Body:  &buf,
+	}
+
+	res, err := req.Do(ctx, r.client)
+	if err != nil {
+		return fmt.Errorf("create index: %v", err)
+	}
+
+	if err := errorResToErr(res); err != nil {
+		return fmt.Errorf("create index: %v", err)
+	}
+
+	return nil
+}
+
+func (r *Repo) indexExists(ctx context.Context, index string) (bool, error) {
+	req := esapi.IndicesExistsRequest{
+		Index: []string{index},
+	}
+	req.Do(ctx, r.client)
+
+	res, err := req.Do(ctx, r.client)
+	if err != nil {
+		return false, fmt.Errorf("check index exists: %v", err)
+	}
+
+	s := res.Status()
+	switch s {
+	case "200 OK":
+		return true, nil
+	case "404 Not Found":
+		return false, nil
+	default:
+		return false, fmt.Errorf("check index exists: status: %s", s)
+	}
+}
+
 // SetMappings for overall concept doctype of weaviate index
-func (r *Repo) SetMappings(ctx context.Context) error {
+func (r *Repo) SetMappings(ctx context.Context, index string) error {
 	var buf bytes.Buffer
 	body := map[string]interface{}{
 		"properties": map[string]interface{}{
@@ -40,9 +102,14 @@ func (r *Repo) SetMappings(ctx context.Context) error {
 	}
 
 	err := json.NewEncoder(&buf).Encode(body)
+	if err != nil {
+		return err
+	}
+
 	req := esapi.IndicesPutMappingRequest{
 		Index:        []string{index},
 		DocumentType: doctype,
+		Body:         &buf,
 	}
 	res, err := req.Do(ctx, r.client)
 	if err != nil {
@@ -63,7 +130,7 @@ func errorResToErr(res *esapi.Response) error {
 
 	var e map[string]interface{}
 	if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
-		return fmt.Errorf("request is error: error parsing the response body: %s", err)
+		return fmt.Errorf("request is error: status: %s", res.Status())
 	}
 
 	return fmt.Errorf("request is error: status: %v, type: %v, reason: %v",
