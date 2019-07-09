0dc1b61ec707bbcbf82dc827474a727227fb0167
Etienne Dilocker <etienne@dilocker.de>
2019-07-09 15:55:01 +0200

    gh-903 prepare release 0.16.0

--- adapters/handlers/rest/doc.go
+++ adapters/handlers/rest/doc.go
@@ -19,7 +19,7 @@ Decentralised Knowledge Graph
       https
     Host: localhost
     BasePath: /weaviate/v1
-    Version: 0.15.0
+    Version: 0.16.0
     Contact: Weaviate<hello@semi.technology> https://github.com/semi-technologies
 
     Consumes:
