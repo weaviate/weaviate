e7b029c41eff61936edaba8724b176ff70cb26fe
Etienne Dilocker <etienne@dilocker.de>
2019-07-19 10:54:43 +0200

    gh-909: don't remove build tags, add headers to new files

--- tools/license_headers/main.go
+++ tools/license_headers/main.go
@@ -1,3 +1,15 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2019 Weaviate. All rights reserved.
+//  LICENSE: https://github.com/semi-technologies/weaviate/blob/develop/LICENSE.md
+//  DESIGN & CONCEPT: Bob van Luijt (@bobvanluijt)
+//  CONTACT: hello@semi.technology
+//
+
 package main
 
 import (
@@ -12,11 +24,13 @@ import (
 
 var (
 	headerSectionRe *regexp.Regexp
+	buildTagRe      *regexp.Regexp
 	targetHeader    []byte
 )
 
 func init() {
 	headerSectionRe = regexp.MustCompile(`^(//.*\n)*\n`)
+	buildTagRe = regexp.MustCompile(`^//\s\+build`)
 	h, err := ioutil.ReadFile("tools/license_headers/header.txt")
 	fatal(err)
 	targetHeader = bytes.TrimSpace(h)
@@ -36,6 +50,15 @@ func processSingleFile(name string) error {
 	bytes, err := ioutil.ReadFile(name)
 	if err != nil {
 		return fmt.Errorf("%s: %v", name, err)
+	}
+
+	if hasNoHeader(bytes, name) {
+		err := extendWithHeader(name, bytes)
+		if err != nil {
+			return fmt.Errorf("%s: %v", name, err)
+		}
+		fmt.Printf("🖋️  succesfully created header: %s\n", name)
+		return nil
 	}
 
 	if headerNeedsUpdate(bytes) {
@@ -60,12 +83,33 @@ func headerNeedsUpdate(content []byte) bool {
 	return true
 }
 
+func extendWithHeader(name string, content []byte) error {
+	target := append(targetHeader, []byte("\n\n")...)
+	updated := append(target, content...)
+	return ioutil.WriteFile(name, updated, 0)
+}
+
 func updateHeader(name string, content []byte) error {
+	if startsWithBuildTag(content) {
+		// the document starts with a build tag, this means we don't replace, but
+		// insert the header at pos 0
+		return extendWithHeader(name, content)
+	}
+
 	// append a newline so we consistently have one newline regardless of the
 	// input doc
-	target := append(targetHeader, []byte("\n")...)
+	target := append(targetHeader, []byte("\n\n")...)
 	replaced := headerSectionRe.ReplaceAll(content, target)
 	return ioutil.WriteFile(name, replaced, 0)
+}
+
+func hasNoHeader(content []byte, name string) bool {
+	h := headerSectionRe.Find(content)
+	return h == nil
+}
+
+func startsWithBuildTag(content []byte) bool {
+	return buildTagRe.Match(content)
 }
 
 func fatal(err error) {
