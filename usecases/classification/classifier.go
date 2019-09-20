de54118ab258c926a272e7c26625551eff85573f
Etienne Dilocker <etienne@dilocker.de>
2019-09-20 17:10:42 +0200

    gh-976 flesh out classifier (uc)

---
+++ usecases/classification/classifier.go
@@ -0,0 +1,86 @@
+package classification
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-openapi/strfmt"
+	uuid "github.com/satori/go.uuid"
+	"github.com/semi-technologies/weaviate/entities/models"
+	"github.com/semi-technologies/weaviate/entities/search"
+	schemaUC "github.com/semi-technologies/weaviate/usecases/schema"
+)
+
+type Classifier struct {
+	schemaGetter schemaUC.SchemaGetter
+	repo         Repo
+}
+
+func New(sg schemaUC.SchemaGetter, cr Repo) *Classifier {
+	return &Classifier{
+		schemaGetter: sg,
+		repo:         cr,
+	}
+}
+
+// Repo to manage classification state, should be consistent, not used to store
+// acutal data object vectors, see VectorRepo
+type Repo interface {
+	Put(models.Classification) error
+	Get(id strfmt.UUID) (*models.Classification, error)
+}
+
+type VectorRepo interface {
+	GetUnclassified(ctx context.Context, class string, properites []string) (*search.Result, error)
+	AggregateNeighbors(ctx context.Context, vector []float32, properties []string, k int) ([]NeighborRef, error)
+	PutThing(ctx context.Context, thing *models.Thing, vector []float32) error
+	PutAction(ctx context.Context, action *models.Action, vector []float32) error
+}
+
+// NeighborRef is the result of an aggregation of the ref properties of k neighbors
+type NeighborRef struct {
+	// Property indicates which property was aggregated
+	Property string
+
+	// The beacon of the most common (kNN) reference
+	Beacon strfmt.URI
+
+	// Count (n<=k) of number of the winning Beacon
+	Count int
+}
+
+func (c *Classifier) Schedule(params models.Classification) (*models.Classification, error) {
+	err := NewValidator(c.schemaGetter, params).Do()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := c.assignNewID(&params); err != nil {
+		return nil, fmt.Errorf("classification: assign id: %v", err)
+	}
+
+	params.Status = models.ClassificationStatusRunning
+
+	if err := c.repo.Put(params); err != nil {
+		return nil, fmt.Errorf("classification: put: %v", err)
+	}
+
+	// asynchronously trigger the classification
+	go c.run(params)
+
+	return &params, nil
+}
+
+func (c *Classifier) assignNewID(params *models.Classification) error {
+	id, err := uuid.NewV4()
+	if err != nil {
+		return err
+	}
+
+	params.ID = strfmt.UUID(id.String())
+	return nil
+}
+
+func (c *Classifier) Get(id strfmt.UUID) (*models.Classification, error) {
+	return c.repo.Get(id)
+}
--- usecases/classification/fakes_for_test.go
+++ usecases/classification/fakes_for_test.go
@@ -1,6 +1,15 @@
 package classification
 
-import "github.com/semi-technologies/weaviate/entities/schema"
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"github.com/go-openapi/strfmt"
+	"github.com/semi-technologies/weaviate/entities/models"
+	"github.com/semi-technologies/weaviate/entities/schema"
+	"github.com/semi-technologies/weaviate/entities/search"
+)
 
 type fakeSchemaGetter struct {
 	schema schema.Schema
@@ -8,4 +17,54 @@ type fakeSchemaGetter struct {
 
 func (f *fakeSchemaGetter) GetSchemaSkipAuth() schema.Schema {
 	return f.schema
+}
+
+type fakeClassificationRepo struct {
+	sync.Mutex
+	db map[strfmt.UUID]models.Classification
+}
+
+func newFakeClassificationRepo() *fakeClassificationRepo {
+	return &fakeClassificationRepo{
+		db: map[strfmt.UUID]models.Classification{},
+	}
+}
+
+func (f *fakeClassificationRepo) Put(class models.Classification) error {
+	f.Lock()
+	defer f.Unlock()
+
+	f.db[class.ID] = class
+	return nil
+}
+
+func (f *fakeClassificationRepo) Get(id strfmt.UUID) (*models.Classification, error) {
+	class, ok := f.db[id]
+	if !ok {
+		return nil, nil
+	}
+
+	return &class, nil
+}
+
+// read requests are specified throuh unclassified and classified,
+// write requests (Put[Kind]) are stored in the db map
+type fakeVectorRepo struct {
+	unclassified search.Result
+	classified   search.Result
+	dp           map[strfmt.UUID]*models.Thing
+}
+
+func (f *fakeVectorRepo) GetUnclassified(ctx context.Context, class string,
+	properties []string) (*search.Result, error) {
+	return &f.unclassified, nil
+}
+
+func (f *fakeVectorRepo) AggregateNeighbors(ctx context.Context, vector []float32,
+	properties []string, k int) ([]NeighborRef, error) {
+	if k != 1 {
+		return nil, fmt.Errorf("fake vector repo only supports k=1")
+	}
+
+	return nil, nil
 }
