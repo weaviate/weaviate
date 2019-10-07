055c3e077de84743be4f03db0bf3c621911df72e
Etienne Dilocker <etienne@dilocker.de>
2019-10-07 15:00:16 +0200

    gh-979 extend models.SingleRef with optional meta field

--- entities/models/single_ref.go
+++ entities/models/single_ref.go
@@ -38,6 +38,9 @@ type SingleRef struct {
 	// Format: uri
 	Class strfmt.URI `json:"class,omitempty"`
 
+	// Additional Meta information about this particular reference. Only shown if meta==true.
+	Meta *ReferenceMeta `json:"meta,omitempty"`
+
 	// If using a concept reference (rather than a direct reference), specify the desired properties here
 	Schema PropertySchema `json:"schema,omitempty"`
 }
@@ -51,6 +54,10 @@ func (m *SingleRef) Validate(formats strfmt.Registry) error {
 	}
 
 	if err := m.validateClass(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateMeta(formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -81,6 +88,24 @@ func (m *SingleRef) validateClass(formats strfmt.Registry) error {
 
 	if err := validate.FormatOf("class", "body", "uri", m.Class.String(), formats); err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func (m *SingleRef) validateMeta(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.Meta) { // not required
+		return nil
+	}
+
+	if m.Meta != nil {
+		if err := m.Meta.Validate(formats); err != nil {
+			if ve, ok := err.(*errors.Validation); ok {
+				return ve.ValidateName("meta")
+			}
+			return err
+		}
 	}
 
 	return nil
