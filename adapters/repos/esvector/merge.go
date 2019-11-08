db4a411301ac5e29e9f2b8e593d7d54b937766d2
Etienne Dilocker <etienne@dilocker.de>
2019-11-08 15:14:53 +0100

    gh-1021 stub out esvector.Merge() method

---
+++ adapters/repos/esvector/merge.go
@@ -0,0 +1,11 @@
+package esvector
+
+import (
+	"context"
+
+	"github.com/semi-technologies/weaviate/usecases/kinds"
+)
+
+func (r *Repo) Merge(ctx context.Context, merge kinds.MergeDocument) error {
+	return nil
+}
