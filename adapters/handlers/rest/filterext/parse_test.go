06ae999b044c3af6abae32b02f6e14b96f519bb2
Etienne Dilocker <etienne@dilocker.de>
2019-12-04 11:07:49 +0100

    gh-985 refactor filterext test to table-driven

--- adapters/handlers/rest/filterext/parse_test.go
+++ adapters/handlers/rest/filterext/parse_test.go
@@ -7,33 +7,47 @@ import (
 	"github.com/semi-technologies/weaviate/entities/models"
 	"github.com/semi-technologies/weaviate/entities/schema"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func Test_ExtractFlatFilters(t *testing.T) {
 	t.Parallel()
 
-	input := &models.WhereFilter{
-		Operator: "Equal",
-		ValueInt: ptInt(42),
-		Path:     []string{"intField"},
+	type test struct {
+		name           string
+		input          *models.WhereFilter
+		expectedFilter *filters.LocalFilter
+		expectedErr    error
 	}
 
-	expected := &filters.LocalFilter{Root: &filters.Clause{
-		Operator: filters.OperatorEqual,
-		On: &filters.Path{
-			Class:    schema.AssertValidClassName("Todo"),
-			Property: schema.AssertValidPropertyName("intField"),
+	tests := []test{
+		test{
+			name: "valid int filter",
+			input: &models.WhereFilter{
+				Operator: "Equal",
+				ValueInt: ptInt(42),
+				Path:     []string{"intField"},
+			},
+			expectedFilter: &filters.LocalFilter{Root: &filters.Clause{
+				Operator: filters.OperatorEqual,
+				On: &filters.Path{
+					Class:    schema.AssertValidClassName("Todo"),
+					Property: schema.AssertValidPropertyName("intField"),
+				},
+				Value: &filters.Value{
+					Value: 42,
+					Type:  schema.DataTypeInt,
+				},
+			}},
 		},
-		Value: &filters.Value{
-			Value: 42,
-			Type:  schema.DataTypeInt,
-		},
-	}}
+	}
 
-	actual, err := Parse(input)
-	require.Nil(t, err)
-	assert.Equal(t, expected, actual)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			filter, err := Parse(test.input)
+			assert.Equal(t, test.expectedErr, err)
+			assert.Equal(t, test.expectedFilter, filter)
+		})
+	}
 }
 
 func ptInt(in int) *int64 {
