cd332d656d65b2c0f41c2b45d81a99dce2eac5b3
Etienne Dilocker <etienne@dilocker.de>
2019-12-11 17:40:00 +0100

    gh-975 merge string and numerical props
    The merging is extremly primitive at the moment, it takes the first
    element and appends the others in parantheses.

--- usecases/traverser/grouper/grouper_test.go
+++ usecases/traverser/grouper/grouper_test.go
@@ -9,8 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGrouper(t *testing.T) {
-
+func TestGrouper_ModeClosest(t *testing.T) {
 	in := []search.Result{
 		search.Result{
 			Vector: []float32{0.1, 0.1, 0.98},
@@ -67,6 +66,71 @@ func TestGrouper(t *testing.T) {
 
 	log, _ := test.NewNullLogger()
 	res, err := New(log).Group(in, "closest", 0.2)
+	require.Nil(t, err)
+	assert.Equal(t, expectedOut, res)
+}
+
+func TestGrouper_ModeMerge(t *testing.T) {
+	in := []search.Result{
+		search.Result{
+			Vector: []float32{0.1, 0.1, 0.98},
+			Schema: map[string]interface{}{
+				"name":  "A1",
+				"count": 10.0,
+			},
+		},
+		search.Result{
+			Vector: []float32{0.1, 0.1, 0.96},
+			Schema: map[string]interface{}{
+				"name":  "A2",
+				"count": 11.0,
+			},
+		},
+		search.Result{
+			Vector: []float32{0.1, 0.1, 0.93},
+			Schema: map[string]interface{}{
+				"name":  "A3",
+				"count": 12.0,
+			},
+		},
+		// search.Result{
+		// 	Vector: []float32{0.1, 0.98, 0.1},
+		// 	Schema: map[string]interface{}{
+		// 		"name": "B1",
+		// 	},
+		// },
+		// search.Result{
+		// 	Vector: []float32{0.1, 0.93, 0.1},
+		// 	Schema: map[string]interface{}{
+		// 		"name": "B2",
+		// 	},
+		// },
+		// search.Result{
+		// 	Vector: []float32{0.1, 0.92, 0.1},
+		// 	Schema: map[string]interface{}{
+		// 		"name": "B3",
+		// 	},
+		// },
+	}
+
+	expectedOut := []search.Result{
+		search.Result{
+			// Vector: []float32{0.1, 0.1, 0.98},
+			Schema: map[string]interface{}{
+				"name":  "A1 (A2, A3)",
+				"count": 11.0,
+			},
+		},
+		// search.Result{
+		// 	Vector: []float32{0.1, 0.98, 0.1},
+		// 	Schema: map[string]interface{}{
+		// 		"name": "B1",
+		// 	},
+		// },
+	}
+
+	log, _ := test.NewNullLogger()
+	res, err := New(log).Group(in, "merge", 0.2)
 	require.Nil(t, err)
 	assert.Equal(t, expectedOut, res)
 }
---
+++ usecases/traverser/grouper/merge_group.go
@@ -0,0 +1,136 @@
+package grouper
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/semi-technologies/weaviate/entities/search"
+)
+
+type valueType int
+
+const (
+	numerical valueType = iota
+	textual
+	boolean
+	reference
+	geo
+	unknown
+)
+
+type valueGroup struct {
+	values    []interface{}
+	valueType valueType
+	name      string
+}
+
+func (g group) flattenMerge() (search.Result, error) {
+	values := g.makeValueGroups()
+	merged, err := mergeValueGroups(values)
+	if err != nil {
+		return search.Result{}, fmt.Errorf("merge values: %v", err)
+	}
+
+	return search.Result{
+		Schema: merged,
+	}, nil
+}
+
+func (g group) makeValueGroups() map[string]valueGroup {
+	values := map[string]valueGroup{}
+
+	for _, elem := range g.Elements {
+		if elem.Schema == nil {
+			continue
+		}
+
+		for propName, propValue := range elem.Schema.(map[string]interface{}) {
+			current, ok := values[propName]
+			if !ok {
+				current = valueGroup{
+					values:    []interface{}{propValue},
+					valueType: valueTypeOf(propValue),
+					name:      propName,
+				}
+				values[propName] = current
+				continue
+			}
+
+			current.values = append(current.values, propValue)
+			values[propName] = current
+		}
+	}
+
+	return values
+}
+
+func mergeValueGroups(props map[string]valueGroup) (map[string]interface{}, error) {
+	mergedProps := map[string]interface{}{}
+
+	for propName, group := range props {
+		var (
+			res interface{}
+			err error
+		)
+		switch group.valueType {
+		case textual:
+			res, err = mergeTextualProps(group.values)
+		case numerical:
+			res, err = mergeNumericalProps(group.values)
+		default:
+			err = fmt.Errorf("unrecognized value type")
+		}
+		if err != nil {
+			return nil, fmt.Errorf("prop '%s': %v", propName, err)
+		}
+
+		mergedProps[propName] = res
+	}
+
+	return mergedProps, nil
+}
+
+func valueTypeOf(in interface{}) valueType {
+	switch in.(type) {
+	case string:
+		return textual
+	case float64:
+		return numerical
+	case bool:
+		return boolean
+	default:
+		return unknown
+	}
+}
+
+func mergeTextualProps(in []interface{}) (string, error) {
+	var values []string
+	for i, elem := range in {
+		asString, ok := elem.(string)
+		if !ok {
+			return "", fmt.Errorf("element %d: expected numerical element to be float64, but got %T", i, elem)
+		}
+
+		values = append(values, asString)
+	}
+
+	if len(values) == 1 {
+		return values[0], nil
+	}
+
+	return fmt.Sprintf("%s (%s)", values[0], strings.Join(values[1:], ", ")), nil
+}
+
+func mergeNumericalProps(in []interface{}) (float64, error) {
+	var sum float64
+	for i, elem := range in {
+		asFloat, ok := elem.(float64)
+		if !ok {
+			return 0, fmt.Errorf("element %d: expected numerical element to be float64, but got %T", i, elem)
+		}
+
+		sum += asFloat
+	}
+
+	return sum / float64(len(in)), nil
+}
