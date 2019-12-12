5e7007fbe5fd4c874d71cef049ea18c53bd724e8
Etienne Dilocker <etienne@dilocker.de>
2019-12-12 10:53:07 +0100

    gh-975 improve property merging
    - string duplicates are now removed
    - boolean values included (most common)

--- usecases/traverser/grouper/grouper_test.go
+++ usecases/traverser/grouper/grouper_test.go
@@ -75,58 +75,70 @@ func TestGrouper_ModeMerge(t *testing.T) {
 		search.Result{
 			Vector: []float32{0.1, 0.1, 0.98},
 			Schema: map[string]interface{}{
-				"name":  "A1",
-				"count": 10.0,
+				"name":    "A1",
+				"count":   10.0,
+				"illegal": true,
 			},
 		},
 		search.Result{
 			Vector: []float32{0.1, 0.1, 0.96},
 			Schema: map[string]interface{}{
-				"name":  "A2",
-				"count": 11.0,
+				"name":    "A2",
+				"count":   11.0,
+				"illegal": true,
+			},
+		},
+		search.Result{
+			Vector: []float32{0.1, 0.1, 0.96},
+			Schema: map[string]interface{}{
+				"name":    "A2",
+				"count":   11.0,
+				"illegal": true,
 			},
 		},
 		search.Result{
 			Vector: []float32{0.1, 0.1, 0.93},
 			Schema: map[string]interface{}{
-				"name":  "A3",
-				"count": 12.0,
+				"name":    "A3",
+				"count":   12.0,
+				"illegal": false,
 			},
 		},
-		// search.Result{
-		// 	Vector: []float32{0.1, 0.98, 0.1},
-		// 	Schema: map[string]interface{}{
-		// 		"name": "B1",
-		// 	},
-		// },
-		// search.Result{
-		// 	Vector: []float32{0.1, 0.93, 0.1},
-		// 	Schema: map[string]interface{}{
-		// 		"name": "B2",
-		// 	},
-		// },
-		// search.Result{
-		// 	Vector: []float32{0.1, 0.92, 0.1},
-		// 	Schema: map[string]interface{}{
-		// 		"name": "B3",
-		// 	},
-		// },
+		search.Result{
+			Vector: []float32{0.1, 0.98, 0.1},
+			Schema: map[string]interface{}{
+				"name": "B1",
+			},
+		},
+		search.Result{
+			Vector: []float32{0.1, 0.93, 0.1},
+			Schema: map[string]interface{}{
+				"name": "B2",
+			},
+		},
+		search.Result{
+			Vector: []float32{0.1, 0.92, 0.1},
+			Schema: map[string]interface{}{
+				"name": "B3",
+			},
+		},
 	}
 
 	expectedOut := []search.Result{
 		search.Result{
 			// Vector: []float32{0.1, 0.1, 0.98},
 			Schema: map[string]interface{}{
-				"name":  "A1 (A2, A3)",
-				"count": 11.0,
+				"name":    "A1 (A2, A3)", // note that A2 is only contained once, even though its twice in the input set
+				"count":   11.0,          // mean of all inputs
+				"illegal": true,          // the most common input value, with a bias towards true on equal count
 			},
 		},
-		// search.Result{
-		// 	Vector: []float32{0.1, 0.98, 0.1},
-		// 	Schema: map[string]interface{}{
-		// 		"name": "B1",
-		// 	},
-		// },
+		search.Result{
+			// Vector: []float32{0.1, 0.98, 0.1},
+			Schema: map[string]interface{}{
+				"name": "B1 (B2, B3)",
+			},
+		},
 	}
 
 	log, _ := test.NewNullLogger()
