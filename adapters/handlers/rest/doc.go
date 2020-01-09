74485a797ec2bab1195a0b45983dd96b45a942d0
Etienne Dilocker <etienne@dilocker.de>
2020-01-09 16:30:12 +0100

    prepare release 0.21.9

--- adapters/handlers/rest/doc.go
+++ adapters/handlers/rest/doc.go
@@ -22,7 +22,7 @@ Open Source Knowledge Graph (GraphQL/RESTful/P2P)
       https
     Host: localhost
     BasePath: /v1
-    Version: 0.21.8
+    Version: 0.21.9
     Contact: Weaviate<hello@semi.technology> https://github.com/semi-technologies
 
     Consumes:
