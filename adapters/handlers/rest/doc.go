f097745ee7292522ab8ef6bd530c9d7eb2c3df56
Etienne Dilocker <etienne@dilocker.de>
2020-01-16 16:56:36 +0100

    gh-975 update version and license headers

--- adapters/handlers/rest/doc.go
+++ adapters/handlers/rest/doc.go
@@ -22,7 +22,7 @@ Open Source Knowledge Graph (GraphQL/RESTful/P2P)
       https
     Host: localhost
     BasePath: /v1
-    Version: 0.21.10
+    Version: 0.21.11
     Contact: Weaviate<hello@semi.technology> https://github.com/semi-technologies
 
     Consumes:
--- adapters/handlers/rest/embedded_spec.go
+++ adapters/handlers/rest/embedded_spec.go
@@ -50,7 +50,7 @@ func init() {
       "url": "https://github.com/semi-technologies",
       "email": "hello@semi.technology"
     },
-    "version": "0.21.10"
+    "version": "0.21.11"
   },
   "basePath": "/v1",
   "paths": {
@@ -3795,7 +3795,7 @@ func init() {
       "url": "https://github.com/semi-technologies",
       "email": "hello@semi.technology"
     },
-    "version": "0.21.10"
+    "version": "0.21.11"
   },
   "basePath": "/v1",
   "paths": {
