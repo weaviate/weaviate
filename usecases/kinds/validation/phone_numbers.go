86e1ae114257ff84a729d79111b673df67fe152b
Etienne Dilocker <etienne@dilocker.de>
2020-02-26 10:42:23 +0100

    gh-1088 improve error handling around missing defaultCountry
    test other possible edge cases which were missing before

--- usecases/kinds/validation/phone_numbers.go
+++ usecases/kinds/validation/phone_numbers.go
@@ -12,7 +12,12 @@ func parsePhoneNumber(input, defaultCountry string) (*models.PhoneNumber, error)
 	defaultCountry = strings.ToUpper(defaultCountry)
 	num, err := phonenumbers.Parse(input, defaultCountry)
 	if err != nil {
-		return nil, fmt.Errorf("invalid phone number: %v", err)
+		switch err {
+		case phonenumbers.ErrInvalidCountryCode:
+			return nil, fmt.Errorf("invalid phone number: invalid or missing defaultCountry - this field is optional if the specified number is in the international format, but required if the number is in national format, use ISO 3166-1 alpha-2")
+		default:
+			return nil, fmt.Errorf("invalid phone number: %v", err)
+		}
 	}
 
 	return &models.PhoneNumber{
