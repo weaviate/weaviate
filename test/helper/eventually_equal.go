523c13a14c529efe51dba923eabb557d980ac9e2
Etienne Dilocker <etienne@dilocker.de>
2020-04-30 15:12:23 +0200

    give classification test more time
    it timed out on CI

--- test/helper/eventually_equal.go
+++ test/helper/eventually_equal.go
@@ -57,3 +57,24 @@ func AssertEventuallyEqual(t *testing.T, expected interface{}, actualThunk func(
 
 	t.Errorf("waiting for %s, but never succeeded:\n\n%s", elapsed, fakeT.lastError)
 }
+
+func AssertEventuallyEqualWithFrequencyAndTimeout(t *testing.T, expected interface{}, actualThunk func() interface{}, msg ...interface{},
+	interval time.Duration, timeout time.Duration) {
+	elapsed := 0 * time.Millisecond
+	fakeT := &fakeT{}
+
+	for elapsed < timeout {
+		fakeT.Reset()
+		actual := actualThunk()
+		assert.Equal(fakeT, expected, actual, msg...)
+
+		if fakeT.lastError == nil {
+			return
+		}
+
+		time.Sleep(interval)
+		elapsed += interval
+	}
+
+	t.Errorf("waiting for %s, but never succeeded:\n\n%s", elapsed, fakeT.lastError)
+}
