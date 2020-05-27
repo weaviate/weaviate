27fa61622f20dd3e18890d7ae9cb3e594cf7c4ca
Etienne Dilocker <etienne@dilocker.de>
2020-05-27 17:27:03 +0200

    gh-1147 build simple text analyzer

---
+++ adapters/repos/db/inverted/analyzer_test.go
@@ -0,0 +1,66 @@
+package inverted
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAnalyzer(t *testing.T) {
+	a := NewAnalyzer()
+
+	t.Run("with text", func(t *testing.T) {
+		t.Run("only unique words", func(t *testing.T) {
+			res := a.Text("Hello, my name is John Doe")
+			assert.ElementsMatch(t, res, []Countable{
+				{
+					Data:          []byte("hello"),
+					TermFrequency: float32(1) / 6,
+				},
+				{
+					Data:          []byte("my"),
+					TermFrequency: float32(1) / 6,
+				},
+				{
+					Data:          []byte("name"),
+					TermFrequency: float32(1) / 6,
+				},
+				{
+					Data:          []byte("is"),
+					TermFrequency: float32(1) / 6,
+				},
+				{
+					Data:          []byte("john"),
+					TermFrequency: float32(1) / 6,
+				},
+				{
+					Data:          []byte("doe"),
+					TermFrequency: float32(1) / 6,
+				},
+			})
+		})
+
+		t.Run("with repeated words", func(t *testing.T) {
+			res := a.Text("Du. Du hast. Du hast. Du hast mich gefragt.")
+			assert.ElementsMatch(t, res, []Countable{
+				{
+					Data:          []byte("du"),
+					TermFrequency: float32(4) / 9,
+				},
+				{
+					Data:          []byte("hast"),
+					TermFrequency: float32(3) / 9,
+				},
+				{
+					Data:          []byte("mich"),
+					TermFrequency: float32(1) / 9,
+				},
+				{
+					Data:          []byte("gefragt"),
+					TermFrequency: float32(1) / 9,
+				},
+			})
+		})
+
+	})
+}
