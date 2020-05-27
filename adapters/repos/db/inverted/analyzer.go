25ceaff2f886c03a632316cbae9156610d1d7ecb
Etienne Dilocker <etienne@dilocker.de>
2020-05-27 17:38:52 +0200

    gh-1147 analyze string properties

--- adapters/repos/db/inverted/analyzer.go
+++ adapters/repos/db/inverted/analyzer.go
@@ -44,6 +44,37 @@ func (a *Analyzer) Text(in string) []Countable {
 	return out
 }
 
+// String splits only on spaces and does not lowercase, then aggregates
+// duplicates
+func (a *Analyzer) String(in string) []Countable {
+	parts := strings.FieldsFunc(in, func(c rune) bool {
+		return unicode.IsSpace(c)
+	})
+
+	terms := map[string]uint32{}
+	total := 0
+	for _, word := range parts {
+		count, ok := terms[word]
+		if !ok {
+			terms[word] = 0
+		}
+		terms[word] = count + 1
+		total++
+	}
+
+	out := make([]Countable, len(terms))
+	i := 0
+	for term, count := range terms {
+		out[i] = Countable{
+			Data:          []byte(term),
+			TermFrequency: float32(count) / float32(total),
+		}
+		i++
+	}
+
+	return out
+}
+
 func NewAnalyzer() *Analyzer {
 	return &Analyzer{}
 }
