ecfb0777de0ff32a0f698d368f4d63a8b542bc2f
Etienne Dilocker <etienne@dilocker.de>
2020-05-29 12:21:11 +0200

    gh-1147 serialize objectKind to binary and back

---
+++ adapters/repos/db/storage_object_test.go
@@ -0,0 +1,39 @@
+package db
+
+import (
+	"testing"
+
+	"github.com/go-openapi/strfmt"
+	"github.com/semi-technologies/weaviate/entities/models"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStorageObjectMarshalling(t *testing.T) {
+	before := NewKindObjectFromThing(
+		&models.Thing{
+			Class:              "MyFavoriteClass",
+			CreationTimeUnix:   123456,
+			LastUpdateTimeUnix: 56789,
+			ID:                 strfmt.UUID("73f2eb5f-5abf-447a-81ca-74b1dd168247"),
+			Meta: &models.ObjectMeta{
+				Classification: &models.ObjectMetaClassification{
+					BasedOn: []string{"some", "fields"},
+				},
+			},
+			Schema: map[string]interface{}{
+				"name": "MyName",
+				"foo":  float64(17),
+			},
+		},
+		[]float32{1, 2, 0.7},
+	)
+
+	asBinary, err := before.MarshalBinary()
+	require.Nil(t, err)
+
+	after, err := NewKindObjectFromBinary(asBinary)
+	require.Nil(t, err)
+
+	assert.Equal(t, before, after)
+}
