cd3455348f68858df68d6b09a6c4da4b2d78fdb7
Etienne Dilocker <etienne@dilocker.de>
2020-06-08 22:00:48 +0200

    gh-1147 support lessThan(Equal) op on int/float

--- adapters/repos/db/filtersearcher/compare_operators.go
+++ adapters/repos/db/filtersearcher/compare_operators.go
@@ -18,6 +18,10 @@ func (fs *FilterSearcher) docPointers(operator filters.Operator, b *bolt.Bucket,
 		return fs.docPointersGreaterThan(b, value, limit, hasFrequency, false)
 	case filters.OperatorGreaterThanEqual:
 		return fs.docPointersGreaterThan(b, value, limit, hasFrequency, true)
+	case filters.OperatorLessThan:
+		return fs.docPointersLessThan(b, value, limit, hasFrequency, false)
+	case filters.OperatorLessThanEqual:
+		return fs.docPointersLessThan(b, value, limit, hasFrequency, true)
 	default:
 		return docPointers{}, fmt.Errorf("operator not supported (yet)")
 	}
@@ -27,13 +31,23 @@ func (fs *FilterSearcher) docPointersEqual(b *bolt.Bucket, value []byte,
 	limit int, hasFrequency bool) (docPointers, error) {
 	return fs.parseInvertedIndexRow(b.Get(value), limit, hasFrequency)
 }
-
 func (fs *FilterSearcher) docPointersGreaterThan(b *bolt.Bucket, value []byte,
 	limit int, hasFrequency bool, allowEqual bool) (docPointers, error) {
+	c := b.Cursor()
+	return fs.docPointersCompare(b, c, c.Next, value, limit, hasFrequency, allowEqual)
+}
+
+func (fs *FilterSearcher) docPointersLessThan(b *bolt.Bucket, value []byte,
+	limit int, hasFrequency bool, allowEqual bool) (docPointers, error) {
+	c := b.Cursor()
+	return fs.docPointersCompare(b, c, c.Prev, value, limit, hasFrequency, allowEqual)
+}
+
+func (fs *FilterSearcher) docPointersCompare(b *bolt.Bucket, c *bolt.Cursor, cursorStep func() ([]byte, []byte),
+	value []byte, limit int, hasFrequency bool, allowEqual bool) (docPointers, error) {
 
 	var pointers docPointers
-	c := b.Cursor()
-	for k, v := c.Seek(value); k != nil; k, v = c.Next() {
+	for k, v := c.Seek(value); k != nil; k, v = cursorStep() {
 		if bytes.Equal(k, value) && !allowEqual {
 			continue
 		}
