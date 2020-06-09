415144754195372c5b5e4aa9a64ff75e35b44243
Etienne Dilocker <etienne@dilocker.de>
2020-06-09 10:17:41 +0200

    gh-1147 make int/float properly lexicographically sortable

---
+++ adapters/repos/db/inverted/serialization.go
@@ -0,0 +1,57 @@
+package inverted
+
+import (
+	"bytes"
+	"encoding/binary"
+	"math"
+
+	"github.com/pkg/errors"
+)
+
+// LexicographicallySortableFloat64 transforms a conversion to a
+// lexicographically sortable byte slice. In general, for lexicographical
+// sorting big endian notatino is required. Additionally  the sign needs to be
+// flipped in any case, but additionally each remaining byte also needs to be
+// flipped if the number is negative
+func LexicographicallySortableFloat64(in float64) ([]byte, error) {
+	buf := bytes.NewBuffer(nil)
+
+	err := binary.Write(buf, binary.BigEndian, in)
+	if err != nil {
+		return nil, errors.Wrap(err, "serialize float64 value as big endian")
+	}
+
+	var out []byte
+	if in >= 0 {
+		// on positive numbers only flip the sign
+		out = buf.Bytes()
+		firstByte := out[0] ^ 0x80
+		out = append([]byte{firstByte}, out[1:]...)
+	} else {
+		// on negative numbers flip every bit
+		out = make([]byte, 8)
+		for i, b := range buf.Bytes() {
+			out[i] = b ^ 0xFF
+		}
+	}
+
+	return out, nil
+}
+
+// LexicographicallySortableInt64 performs a conversion to a lexicographically
+// sortable byte slice. Fro this, big endian notation is required and the sign
+// must be flipped
+func LexicographicallySortableInt64(in int64) ([]byte, error) {
+	buf := bytes.NewBuffer(nil)
+	asInt64 := int64(in)
+
+	// flip the sign
+	asInt64 = asInt64 ^ math.MinInt64
+
+	err := binary.Write(buf, binary.BigEndian, asInt64)
+	if err != nil {
+		return nil, errors.Wrap(err, "serialize int value as big endian")
+	}
+
+	return buf.Bytes(), nil
+}
