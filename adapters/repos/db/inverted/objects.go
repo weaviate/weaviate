ea316160522f1a6dff0b24c17450751fb3288f23
Etienne Dilocker <etienne@dilocker.de>
2020-07-31 11:57:05 +0200

    gh-1218 analyze ref count in reference props
    So far it ignores the ref-content, but prepares us to allow for
    filtering for ref length. For classifciations we need a subset thereof:
    Filtering for all unset refs (i.e. ref-count == 0)

--- adapters/repos/db/inverted/objects.go
+++ adapters/repos/db/inverted/objects.go
@@ -15,6 +15,7 @@ import (
 	"fmt"
 
 	"github.com/pkg/errors"
+	"github.com/semi-technologies/weaviate/adapters/repos/db/helpers"
 	"github.com/semi-technologies/weaviate/entities/models"
 	"github.com/semi-technologies/weaviate/entities/schema"
 )
@@ -25,86 +26,126 @@ func (a *Analyzer) Object(input map[string]interface{}, props []*models.Property
 		propsMap[prop.Name] = prop
 	}
 
-	out := make([]Property, len(input))
-	i := 0
-	for key, value := range input {
-		prop, ok := propsMap[key]
-		if !ok {
-			// not a registered property, skip
+	var out []Property
+	for key, prop := range propsMap {
+		if len(prop.DataType) < 1 {
+			return nil, fmt.Errorf("prop %q has no datatype", prop.Name)
+		}
+
+		var property *Property
+		var err error
+		if schema.IsRefDataType(prop.DataType) {
+			value, ok := input[key]
+			if !ok {
+				// explicitly set zero-value, so we can index for "ref not set"
+				value = make(models.MultipleRef, 0)
+			}
+			property, err = a.analyzeRefProp(prop, value)
+		} else {
+			value, ok := input[key]
+			if !ok {
+				// skip any primitive prop that's not set
+				continue
+			}
+			property, err = a.analyzePrimitiveProp(prop, value)
+		}
+		if err != nil {
+			return nil, errors.Wrap(err, "analyze primitive prop")
+		}
+		if property == nil {
 			continue
 		}
 
-		if len(prop.DataType) != 1 {
-			// must be a ref prop or something else is wrong, skip
-			continue
-		}
-		var hasFrequency bool
-
-		var items []Countable
-		switch schema.DataType(prop.DataType[0]) {
-		case schema.DataTypeText:
-			hasFrequency = true
-			asString, ok := value.(string)
-			if !ok {
-				return nil, fmt.Errorf("expected property %s to be of type string, but got %T", key, value)
-			}
-			items = a.Text(asString)
-		case schema.DataTypeString:
-			hasFrequency = true
-			asString, ok := value.(string)
-			if !ok {
-				return nil, fmt.Errorf("expected property %s to be of type string, but got %T", key, value)
-			}
-			items = a.String(asString)
-		case schema.DataTypeInt:
-			hasFrequency = false
-			asInt, ok := value.(int64)
-			if !ok {
-				return nil, fmt.Errorf("expected property %s to be of type int64, but got %T", key, value)
-			}
-
-			var err error
-			items, err = a.Int(int(asInt))
-			if err != nil {
-				return nil, errors.Wrapf(err, "analyze property %s", key)
-			}
-		case schema.DataTypeNumber:
-			hasFrequency = false
-			asFloat, ok := value.(float64)
-			if !ok {
-				return nil, fmt.Errorf("expected property %s to be of type float64, but got %T", key, value)
-			}
-
-			var err error
-			items, err = a.Float(asFloat) // convert to int before analyzing
-			if err != nil {
-				return nil, errors.Wrapf(err, "analyze property %s", key)
-			}
-		case schema.DataTypeBoolean:
-			hasFrequency = false
-			asBool, ok := value.(bool)
-			if !ok {
-				return nil, fmt.Errorf("expected property %s to be of type bool, but got %T", key, value)
-			}
-
-			var err error
-			items, err = a.Bool(asBool) // convert to int before analyzing
-			if err != nil {
-				return nil, errors.Wrapf(err, "analyze property %s", key)
-			}
-
-		default:
-			// ignore unsupported prop type
-			continue
-		}
-
-		out[i] = Property{
-			Name:         key,
-			Items:        items,
-			HasFrequency: hasFrequency,
-		}
-		i++
+		out = append(out, *property)
 	}
 
-	return out[:i], nil
+	return out, nil
+}
+
+func (a *Analyzer) analyzePrimitiveProp(prop *models.Property, value interface{}) (*Property, error) {
+	var hasFrequency bool
+	var items []Countable
+	switch schema.DataType(prop.DataType[0]) {
+	case schema.DataTypeText:
+		hasFrequency = true
+		asString, ok := value.(string)
+		if !ok {
+			return nil, fmt.Errorf("expected property %s to be of type string, but got %T", prop.Name, value)
+		}
+		items = a.Text(asString)
+	case schema.DataTypeString:
+		hasFrequency = true
+		asString, ok := value.(string)
+		if !ok {
+			return nil, fmt.Errorf("expected property %s to be of type string, but got %T", prop.Name, value)
+		}
+		items = a.String(asString)
+	case schema.DataTypeInt:
+		hasFrequency = false
+		asInt, ok := value.(int64)
+		if !ok {
+			return nil, fmt.Errorf("expected property %s to be of type int64, but got %T", prop.Name, value)
+		}
+
+		var err error
+		items, err = a.Int(int(asInt))
+		if err != nil {
+			return nil, errors.Wrapf(err, "analyze property %s", prop.Name)
+		}
+	case schema.DataTypeNumber:
+		hasFrequency = false
+		asFloat, ok := value.(float64)
+		if !ok {
+			return nil, fmt.Errorf("expected property %s to be of type float64, but got %T", prop.Name, value)
+		}
+
+		var err error
+		items, err = a.Float(asFloat) // convert to int before analyzing
+		if err != nil {
+			return nil, errors.Wrapf(err, "analyze property %s", prop.Name)
+		}
+	case schema.DataTypeBoolean:
+		hasFrequency = false
+		asBool, ok := value.(bool)
+		if !ok {
+			return nil, fmt.Errorf("expected property %s to be of type bool, but got %T", prop.Name, value)
+		}
+
+		var err error
+		items, err = a.Bool(asBool) // convert to int before analyzing
+		if err != nil {
+			return nil, errors.Wrapf(err, "analyze property %s", prop.Name)
+		}
+
+	default:
+		// ignore unsupported prop type
+		return nil, nil
+	}
+
+	return &Property{
+		Name:         prop.Name,
+		Items:        items,
+		HasFrequency: hasFrequency,
+	}, nil
+}
+
+func (a *Analyzer) analyzeRefProp(prop *models.Property, value interface{}) (*Property, error) {
+	// TODO: return mutliple properties when support ref-indexing. For now we
+	// only support counting the refs
+
+	asRefs, ok := value.(models.MultipleRef)
+	if !ok {
+		return nil, fmt.Errorf("expected property %q to be of type models.MutlipleRef, but got %T", prop.Name, value)
+	}
+
+	items, err := a.RefCount(asRefs)
+	if err != nil {
+		return nil, errors.Wrapf(err, "analyze ref-property %q", prop.Name)
+	}
+
+	return &Property{
+		Name:         helpers.MetaCountProp(prop.Name),
+		Items:        items,
+		HasFrequency: false,
+	}, nil
 }
