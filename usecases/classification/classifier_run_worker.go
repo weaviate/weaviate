c3e5b0ca952cab30e23401f1359a6cbc3d878e06
Etienne Dilocker <etienne@dilocker.de>
2020-08-17 15:40:04 +0200

    gh-1218 add license headers

--- usecases/classification/classifier_run_worker.go
+++ usecases/classification/classifier_run_worker.go
@@ -1,3 +1,14 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2020 SeMI Technologies B.V. All rights reserved.
+//
+//  CONTACT: hello@semi.technology
+//
+
 package classification
 
 import (
