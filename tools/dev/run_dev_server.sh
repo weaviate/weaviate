76797980c7b1f905af1d48d598f784292bbfe3ba
Etienne Dilocker <etienne@dilocker.de>
2020-09-08 10:47:25 +0200

    gh-1230 allow parsing config from env

--- tools/dev/run_dev_server.sh
+++ tools/dev/run_dev_server.sh
@@ -10,8 +10,14 @@ export DEVELOPMENT_UI=on
 export LOG_LEVEL=debug
 export LOG_FORMAT=text
 
-go run ./cmd/weaviate-server \
-  --scheme http \
-  --host "127.0.0.1" \
-  --port 8080 \
-  --config-file="./tools/dev/config.${CONFIG}.yaml"
+CONFIGURATION_STORAGE_TYPE=etcd \
+  CONFIGURATION_STORAGE_URL=http://localhost:2379 \
+  CONTEXTIONARY_URL=localhost:9999 \
+  QUERY_DEFAULTS_LIMIT=20 \
+  ORIGIN=http://localhost:8080 \
+  AUTHENTICATION_ANONYMOUS_ACCESS_ENABLED=true \
+  ESVECTOR_URL=http://localhost:9201 \
+  go run ./cmd/weaviate-server \
+    --scheme http \
+    --host "127.0.0.1" \
+    --port 8080
