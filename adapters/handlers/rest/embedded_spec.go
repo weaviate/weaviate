97dc2ab673060758c8ff7c0011a80f20b7951acf
Travis CI User <travis@example.org>
2020-09-15 09:58:14 +0000

    bleep bloop => auto updated Weaviate

--- adapters/handlers/rest/embedded_spec.go
+++ adapters/handlers/rest/embedded_spec.go
@@ -48,7 +48,7 @@ func init() {
       "url": "https://github.com/semi-technologies",
       "email": "hello@semi.technology"
     },
-    "version": "0.22.15"
+    "version": "0.22.16"
   },
   "basePath": "/v1",
   "paths": {
@@ -3852,7 +3852,7 @@ func init() {
       "url": "https://github.com/semi-technologies",
       "email": "hello@semi.technology"
     },
-    "version": "0.22.15"
+    "version": "0.22.16"
   },
   "basePath": "/v1",
   "paths": {
