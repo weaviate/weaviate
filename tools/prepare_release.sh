894422849a1248c4ac3ca9fcef54ec3fd149bbf9
Etienne Dilocker <etienne@dilocker.de>
2020-09-15 14:43:56 +0200

    gh-1236 update release script to use configuration api

--- tools/prepare_release.sh
+++ tools/prepare_release.sh
@@ -6,7 +6,7 @@ VERSION=$(jq -r '.info.version' openapi-specs/schema.json)
 LANGUAGES="en nl de cs it"
 IMAGE_BASE="semitechnologies/weaviate:"
 MSG=${1:""}
-REQUIRED_TOOLS="jq yaml2json json2yaml git"
+REQUIRED_TOOLS="jq git"
 
 for tool in $REQUIRED_TOOLS; do
   if ! hash "$tool" 2>/dev/null; then
@@ -20,30 +20,13 @@ if git rev-parse "$VERSION" >/dev/null 2>&1; then
   exit 1
 fi
 
-for lang in $LANGUAGES; do
-  yaml2json "docker-compose/runtime/$lang/docker-compose.yml" | \
-    jq ".services.weaviate.image = \"$IMAGE_BASE$VERSION\"" | json2yaml > "docker-compose/runtime/$lang/tmp.yml"
-  mv "docker-compose/runtime/$lang/tmp.yml" "docker-compose/runtime/$lang/docker-compose.yml"
-  echo "Successfully updated docker compose file for $lang"
-done
-
 git commit -a -m "prepare release $VERSION"
 
 git tag -a "$VERSION" -m "release $VERSION - $MSG"
 
 echo "You can use the following template for the release notes, copy/paste below the line"
 echo "----------------------------"
-cat <<EOM
-Docker image/tag: \`semitechnologies/weaviate:$VERSION\`
-See also: example docker compose files in [English](https://github.com/semi-technologies/weaviate/tree/$VERSION/docker-compose/runtime/en), [German](https://github.com/semi-technologies/weaviate/tree/$VERSION/docker-compose/runtime/de), [Dutch](https://github.com/semi-technologies/weaviate/tree/$VERSION/docker-compose/runtime/nl), [Italian](https://github.com/semi-technologies/weaviate/tree/$VERSION/docker-compose/runtime/it) and [Czech](https://github.com/semi-technologies/weaviate/tree/$VERSION/docker-compose/runtime/cs).
-
-## Breaking Changes
-
-## New Features
-
-## Fixes
-
-EOM
+VERSION="$VERSION" LANGUAGES="$LANGUAGES" go run ./tools/release_template
 
 
 
