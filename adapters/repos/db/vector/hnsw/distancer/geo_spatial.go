ce656e1530c977ee18891209972b33814349e562
Etienne Dilocker <etienne@dilocker.de>
2020-09-18 17:20:06 +0200

    gh-1248 test recall for hnsw geo-spatial search

---
+++ adapters/repos/db/vector/hnsw/distancer/geo_spatial.go
@@ -0,0 +1,43 @@
+package distancer
+
+import (
+	"fmt"
+	"math"
+)
+
+func geoDist(a, b []float32) (float32, error) {
+	if len(a) != 2 || len(b) != 2 {
+		return 0, fmt.Errorf("distance vectors must have len 2")
+	}
+
+	latARadian := float64(a[0] * math.Pi / 180)
+	latBRadian := float64(b[0] * math.Pi / 180)
+	const R = float64(6371e3)
+	deltaLatRadian := float64(b[0]-a[0]) * math.Pi / 180
+	deltaLonRadian := float64(b[1]-b[1]) * math.Pi / 100
+
+	A := math.Sin(deltaLatRadian/2)*math.Sin(deltaLatRadian/2) +
+		math.Cos(latARadian)*math.Cos(latBRadian)*math.Sin(deltaLonRadian/2)*math.Sin(deltaLonRadian/2)
+
+	C := 2 * math.Atan2(math.Sqrt(A), math.Sqrt(1-A))
+
+	return float32(R * C), nil
+}
+
+type GeoDistancer struct {
+	a []float32
+}
+
+func (g GeoDistancer) Distance(b []float32) (float32, error) {
+	return geoDist(g.a, b)
+}
+
+type GeoProvider struct{}
+
+func (p GeoProvider) New(vec []float32) Distancer {
+	return GeoDistancer{a: vec}
+}
+
+func NewGeoProvider() Provider {
+	return GeoProvider{}
+}
---
+++ adapters/repos/db/vector/hnsw/recall_geo_spatial_test.go
@@ -0,0 +1,193 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2020 SeMI Technologies B.V. All rights reserved.
+//
+//  CONTACT: hello@semi.technology
+//
+
+// +build benchmarkRecallGeo
+
+package hnsw
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"runtime"
+	"sort"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/semi-technologies/weaviate/adapters/repos/db/vector/hnsw/distancer"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRecallGeo(t *testing.T) {
+	var size = 10000
+	var queries = 100
+	var efConstruction = 128
+	var maxNeighbors = 64
+
+	var vectors = make([][]float32, size)
+	var queryVectors = make([][]float32, queries)
+	var vectorIndex *hnsw
+
+	t.Run("generate random vectors", func(t *testing.T) {
+		fmt.Printf("generating %d vectors", size)
+		for i := 0; i < size; i++ {
+			lat, lon := randLatLon()
+			vectors[i] = []float32{lat, lon}
+		}
+		fmt.Printf("done\n")
+
+		fmt.Printf("generating %d search queries", queries)
+		for i := 0; i < queries; i++ {
+			lat, lon := randLatLon()
+			queryVectors[i] = []float32{lat, lon}
+		}
+		fmt.Printf("done\n")
+
+	})
+
+	t.Run("importing into hnsw", func(t *testing.T) {
+		fmt.Printf("importing into hnsw\n")
+		cl := &noopCommitLogger{}
+		makeCL := func() CommitLogger {
+			return cl
+		}
+
+		index, err := New(Config{
+			RootPath:              "doesnt-matter-as-committlogger-is-mocked-out",
+			ID:                    "recallbenchmark",
+			MakeCommitLoggerThunk: makeCL,
+			MaximumConnections:    maxNeighbors,
+			EFConstruction:        efConstruction,
+			VectorForIDThunk: func(ctx context.Context, id int32) ([]float32, error) {
+				return vectors[int(id)], nil
+			},
+		})
+
+		index.distancerProvider = distancer.NewGeoProvider()
+		require.Nil(t, err)
+		vectorIndex = index
+
+		workerCount := runtime.GOMAXPROCS(0)
+		jobsForWorker := make([][][]float32, workerCount)
+
+		for i, vec := range vectors {
+			workerID := i % workerCount
+			jobsForWorker[workerID] = append(jobsForWorker[workerID], vec)
+		}
+
+		beforeImport := time.Now()
+		wg := &sync.WaitGroup{}
+		for workerID, jobs := range jobsForWorker {
+			wg.Add(1)
+			go func(workerID int, myJobs [][]float32) {
+				defer wg.Done()
+				for i, vec := range myJobs {
+					originalIndex := (i * workerCount) + workerID
+					err := vectorIndex.Add(originalIndex, vec)
+					require.Nil(t, err)
+				}
+			}(workerID, jobs)
+		}
+
+		wg.Wait()
+		fmt.Printf("import took %s\n", time.Since(beforeImport))
+	})
+
+	t.Run("with k=10", func(t *testing.T) {
+		var k = 10
+
+		var relevant int
+		var retrieved int
+
+		var times time.Duration
+
+		for i := 0; i < queries; i++ {
+			controlList := bruteForce(vectors, queryVectors[i], k)
+			before := time.Now()
+			results, err := vectorIndex.knnSearchByVector(queryVectors[i], k, 800, nil)
+			times += time.Since(before)
+
+			require.Nil(t, err)
+
+			retrieved += k
+			relevant += matchesInLists(controlList, results)
+		}
+
+		recall := float32(relevant) / float32(retrieved)
+		fmt.Printf("recall is %f\n", recall)
+		fmt.Printf("avg search time for k=%d is %s\n", k, times/time.Duration(queries))
+		assert.True(t, recall >= 0.99)
+	})
+
+}
+
+func matchesInLists(control []int, results []int) int {
+	desired := map[int]struct{}{}
+	for _, relevant := range control {
+		desired[relevant] = struct{}{}
+	}
+
+	var matches int
+	for _, candidate := range results {
+		_, ok := desired[candidate]
+		if ok {
+			matches++
+		}
+	}
+
+	return matches
+}
+
+func bruteForce(vectors [][]float32, query []float32, k int) []int {
+	type distanceAndIndex struct {
+		distance float32
+		index    int
+	}
+
+	var distances = make([]distanceAndIndex, len(vectors))
+
+	distancer := distancer.NewGeoProvider().New(query)
+	for i, vec := range vectors {
+		dist, _ := distancer.Distance(vec)
+		distances[i] = distanceAndIndex{
+			index:    i,
+			distance: dist,
+		}
+	}
+
+	sort.Slice(distances, func(a, b int) bool {
+		return distances[a].distance < distances[b].distance
+	})
+
+	if len(distances) < k {
+		k = len(distances)
+	}
+
+	out := make([]int, k)
+	for i := 0; i < k; i++ {
+		out[i] = distances[i].index
+	}
+
+	return out
+}
+
+func randLatLon() (float32, float32) {
+	maxLat := float32(90.0)
+	minLat := float32(-90.0)
+	maxLon := float32(180)
+	minLon := float32(-180)
+
+	lat := minLat + (maxLat-minLat)*rand.Float32()
+	lon := minLon + (maxLon-minLon)*rand.Float32()
+	return lat, lon
+}
