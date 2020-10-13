f358fc646489f454cb5e7e62f0351fb7884d26cc
Etienne Dilocker <etienne@dilocker.de>
2020-10-13 13:17:42 +0200

    gh-1265 support int props

--- adapters/repos/db/aggregations_numerical.go
+++ adapters/repos/db/aggregations_numerical.go
@@ -15,7 +15,7 @@ import (
 	"github.com/semi-technologies/weaviate/usecases/traverser"
 )
 
-func (a *Aggregator) numericalProperty(ctx context.Context,
+func (a *Aggregator) floatProperty(ctx context.Context,
 	prop traverser.AggregateProperty) (aggregation.Property, error) {
 	out := aggregation.Property{
 		Type:                  aggregation.PropertyTypeNumerical,
@@ -28,8 +28,7 @@ func (a *Aggregator) numericalProperty(ctx context.Context,
 			return fmt.Errorf("could not find bucket for prop %s", prop.Name)
 		}
 
-		// TODO: support int props, for now it just assumes all props are float
-		agg := newFloatAggregator()
+		agg := newNumericalAggregator()
 
 		if err := b.ForEach(func(k, v []byte) error {
 			return a.parseAndAddFloatRow(agg, k, v)
@@ -47,7 +46,38 @@ func (a *Aggregator) numericalProperty(ctx context.Context,
 	return out, nil
 }
 
-func (a *Aggregator) parseAndAddFloatRow(agg *floatAggregator, k, v []byte) error {
+func (a *Aggregator) intProperty(ctx context.Context,
+	prop traverser.AggregateProperty) (aggregation.Property, error) {
+	out := aggregation.Property{
+		Type:                  aggregation.PropertyTypeNumerical,
+		NumericalAggregations: map[string]float64{},
+	}
+
+	if err := a.shard.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(helpers.BucketFromPropName(prop.Name.String()))
+		if b == nil {
+			return fmt.Errorf("could not find bucket for prop %s", prop.Name)
+		}
+
+		agg := newNumericalAggregator()
+
+		if err := b.ForEach(func(k, v []byte) error {
+			return a.parseAndAddIntRow(agg, k, v)
+		}); err != nil {
+			return err
+		}
+
+		a.addNumericalAggregations(&out, prop.Aggregators, agg)
+
+		return nil
+	}); err != nil {
+		return out, err
+	}
+
+	return out, nil
+}
+
+func (a *Aggregator) parseAndAddFloatRow(agg *numericalAggregator, k, v []byte) error {
 	if len(k) != 8 {
 		// we expect to see either an int64 or a float64, so any non-8 length
 		// is unexpected
@@ -69,8 +99,30 @@ func (a *Aggregator) parseAndAddFloatRow(agg *floatAggregator, k, v []byte) erro
 	return nil
 }
 
+func (a *Aggregator) parseAndAddIntRow(agg *numericalAggregator, k, v []byte) error {
+	if len(k) != 8 {
+		// we expect to see either an int64 or a float64, so any non-8 length
+		// is unexpected
+		return fmt.Errorf("unexpected key length on inverted index, "+
+			"expected 8: got %d", len(k))
+	}
+
+	if len(v) < 8 {
+		// we expect to see a at least a checksum (4 bytes) and a count
+		// (uint32), if that's not the case, then the row is corrupt
+		return fmt.Errorf("unexpected value length on inverted index, "+
+			"expected at least 8: got %d", len(k))
+	}
+
+	if err := agg.AddInt64(k, v[4:8]); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (a *Aggregator) addNumericalAggregations(prop *aggregation.Property,
-	aggs []traverser.Aggregator, agg *floatAggregator) {
+	aggs []traverser.Aggregator, agg *numericalAggregator) {
 	for _, aProp := range aggs {
 		switch aProp {
 		case traverser.MeanAggregator:
@@ -94,14 +146,14 @@ func (a *Aggregator) addNumericalAggregations(prop *aggregation.Property,
 	}
 }
 
-func newFloatAggregator() *floatAggregator {
-	return &floatAggregator{
+func newNumericalAggregator() *numericalAggregator {
+	return &numericalAggregator{
 		min: math.MaxFloat64,
 		max: math.SmallestNonzeroFloat64,
 	}
 }
 
-type floatAggregator struct {
+type numericalAggregator struct {
 	count    uint32
 	min      float64
 	max      float64
@@ -116,7 +168,7 @@ type floatCountPair struct {
 	count uint32
 }
 
-func (a *floatAggregator) AddFloat64(number, count []byte) error {
+func (a *numericalAggregator) AddFloat64(number, count []byte) error {
 	var countParsed uint32
 
 	numberParsed, err := inverted.ParseLexicographicallySortableFloat64(number)
@@ -152,35 +204,78 @@ func (a *floatAggregator) AddFloat64(number, count []byte) error {
 	return nil
 }
 
-func (a *floatAggregator) Mean() float64 {
+func (a *numericalAggregator) AddInt64(number, count []byte) error {
+	var countParsed uint32
+
+	numberParsed, err := inverted.ParseLexicographicallySortableInt64(number)
+	if err != nil {
+		return errors.Wrap(err, "read float64")
+	}
+
+	asFloat := float64(numberParsed)
+
+	if err := binary.Read(bytes.NewReader(count), binary.LittleEndian,
+		&countParsed); err != nil {
+		return errors.Wrap(err, "read doc count")
+	}
+
+	if countParsed == 0 {
+		// skip
+		return nil
+	}
+
+	a.count += countParsed
+	a.sum += asFloat * float64(countParsed)
+	if asFloat < a.min {
+		a.min = asFloat
+	} else if asFloat > a.max {
+		a.max = asFloat
+	}
+
+	if countParsed > a.maxCount {
+		a.maxCount = countParsed
+		a.mode = asFloat
+	}
+
+	a.pairs = append(a.pairs, floatCountPair{value: asFloat, count: countParsed})
+
+	return nil
+}
+
+func (a *numericalAggregator) Mean() float64 {
 	if a.count == 0 {
 		return 0
 	}
 	return a.sum / float64(a.count)
 }
 
-func (a *floatAggregator) Max() float64 {
+func (a *numericalAggregator) Max() float64 {
 	return a.max
 }
 
-func (a *floatAggregator) Min() float64 {
+func (a *numericalAggregator) Min() float64 {
 	return a.min
 }
 
-func (a *floatAggregator) Sum() float64 {
+func (a *numericalAggregator) Sum() float64 {
 	return a.sum
 }
 
-func (a *floatAggregator) Count() float64 {
+func (a *numericalAggregator) Count() float64 {
 	return float64(a.count)
 }
 
-func (a *floatAggregator) Mode() float64 {
+func (a *numericalAggregator) Mode() float64 {
 	return a.mode
 }
 
-func (a *floatAggregator) Median() float64 {
-	index := a.count / 2
+func (a *numericalAggregator) Median() float64 {
+	var index uint32
+	if a.count%2 == 0 {
+		index = a.count / 2
+	} else {
+		index = a.count/2 + 1
+	}
 
 	// since the pairs are read from an inverted index, which is in turn
 	// lexicographically sorted, we know that our pairs must also be sorted
