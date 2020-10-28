cdfde721f11ce5a1f933d7ae407c3aba1359b271
Etienne Dilocker <etienne@dilocker.de>
2020-10-28 10:54:13 +0100

    gh-1249 improve integration test runner script
    By default it will skip slow tests (such as recall tests), but on CI we
    provide the --include-slow flag to make sure all tests are run.

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -2,12 +2,35 @@
 
 set -e 
 
-if [[ $1 == --no-restart ]]; then
+norestart=false
+includeslow=false
+
+for arg in "$@"; do
+  if [[ $arg == --no-restart ]]; then
+    norestart=true
+    shift
+  fi
+
+  if [[ $arg == --include-slow ]]; then
+    includeslow=true
+    shift
+  fi
+done
+
+
+if [ $norestart = true ]; then
   echo "Found --no-restart flag, reusing running dependencies from previous run..."
-  shift
 else
   docker-compose down --remove-orphans
   docker-compose up -d esvector
 fi
 
-go test -count 1 -coverpkg=./adapters/repos/... -coverprofile=coverage-integration.txt -tags=integrationTest "$@" ./adapters/repos/...
+tags=integrationTest
+if [ $includeslow = true ]; then
+  echo "Found --include-slow flag, running all tests, including the slow ones"
+  tags="$tags,integrationTestSlow"
+else 
+  echo "Found no --include-slow flag, skipping the slow ones"
+fi
+
+go test -count 1 -coverpkg=./adapters/repos/... -coverprofile=coverage-integration.txt -tags=$tags "$@" ./adapters/repos/...
