c111db598c06a26819e2b9d11ab646be06479276
Etienne Dilocker <etienne@dilocker.de>
2020-11-20 16:11:54 +0100

    gh-1286 switch journey tests to standalone
    Not all are green yet, they will be before this issues is considered
    completed.

--- tools/test/run_ci_server.sh
+++ tools/test/run_ci_server.sh
@@ -7,7 +7,7 @@ function main() {
   echo "Pull images..."
   surpress_on_success docker pull golang:1.11-alpine
   echo "Build containers (this will take the longest)..."
-  docker-compose -f docker-compose-test.yml build weaviate esvector
+  docker-compose -f docker-compose-test.yml build weaviate
   echo "Start up docker-compose setup..."
   surpress_on_success docker-compose -f docker-compose-test.yml up --force-recreate -d weaviate \
     contextionary 
