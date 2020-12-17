431b89fd1055c5b2aa1f15db90c92307969832b6
Etienne Dilocker <etienne@dilocker.de>
2020-12-17 15:30:43 +0100

    gh-1324 build layered cache prefiller with limit

---
+++ adapters/repos/db/vector/hnsw/vector_cache_prefiller.go
@@ -0,0 +1,112 @@
+package hnsw
+
+import (
+	"context"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+type vectorCachePrefiller struct {
+	cache  cache
+	index  *hnsw
+	logger logrus.FieldLogger
+}
+
+type cache interface {
+	get(ctx context.Context, id uint64) ([]float32, error)
+	len() int32
+}
+
+func newVectorCachePrefiller(cache cache, index *hnsw,
+	logger logrus.FieldLogger) *vectorCachePrefiller {
+	return &vectorCachePrefiller{
+		cache:  cache,
+		index:  index,
+		logger: logger,
+	}
+}
+
+func (pf *vectorCachePrefiller) Prefill(ctx context.Context, limit int) error {
+	before := time.Now()
+	for level := pf.maxLevel(); level >= 0; level-- {
+		ok, err := pf.prefillLevel(ctx, level, limit)
+		if err != nil {
+			return err
+		}
+
+		if !ok {
+			break
+		}
+	}
+
+	pf.logTotal(int(pf.cache.len()), limit, before)
+	return nil
+}
+
+// returns false if the max has been reached, true otherwise
+func (pf *vectorCachePrefiller) prefillLevel(ctx context.Context,
+	level, limit int) (bool, error) {
+	pf.index.RLock()
+	defer pf.index.RUnlock()
+
+	before := time.Now()
+	layerCount := 0
+	for i, node := range pf.index.nodes {
+		if int(pf.cache.len()) >= limit {
+			break
+		}
+
+		if err := ctx.Err(); err != nil {
+			return false, err
+		}
+
+		if node == nil {
+			continue
+		}
+
+		if levelOfNode(node) != level {
+			continue
+		}
+
+		// we are not really interested in the result, we just want to populate the
+		// cache
+		pf.cache.get(ctx, uint64(i))
+		layerCount++
+	}
+
+	pf.logLevel(level, layerCount, before)
+	return true, nil
+}
+
+func (pf *vectorCachePrefiller) logLevel(level, count int, before time.Time) {
+	pf.logger.WithFields(logrus.Fields{
+		"action": "vector_cache_prefill_level",
+		"level":  level,
+		"count":  count,
+		"took":   time.Since(before),
+	}).Debug("prefilled level in vector cache")
+}
+
+func (pf *vectorCachePrefiller) logTotal(count, limit int, before time.Time) {
+	pf.logger.WithFields(logrus.Fields{
+		"action": "vector_cache_prefill",
+		"limit":  limit,
+		"count":  count,
+		"took":   time.Since(before),
+	}).Info("prefilled vector cache")
+}
+
+func levelOfNode(node *vertex) int {
+	node.Lock()
+	defer node.Unlock()
+
+	return node.level
+}
+
+func (pf *vectorCachePrefiller) maxLevel() int {
+	pf.index.RLock()
+	defer pf.index.RUnlock()
+
+	return pf.index.currentMaximumLayer
+}
