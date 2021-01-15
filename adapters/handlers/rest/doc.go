bb056a180671f5eb526682ee3608aeb02773274f
Etienne Dilocker <etienne@dilocker.de>
2021-01-15 12:55:23 +0100

    prepare release v1.0.3

--- adapters/handlers/rest/doc.go
+++ adapters/handlers/rest/doc.go
@@ -18,7 +18,7 @@
 //    https
 //  Host: localhost
 //  BasePath: /v1
-//  Version: 1.0.2
+//  Version: 1.0.3
 //  Contact: Weaviate<hello@semi.technology> https://github.com/semi-technologies
 //
 //  Consumes:
--- adapters/handlers/rest/embedded_spec.go
+++ adapters/handlers/rest/embedded_spec.go
@@ -48,7 +48,7 @@ func init() {
       "url": "https://github.com/semi-technologies",
       "email": "hello@semi.technology"
     },
-    "version": "1.0.2"
+    "version": "1.0.3"
   },
   "basePath": "/v1",
   "paths": {
@@ -2623,7 +2623,7 @@ func init() {
       "url": "https://github.com/semi-technologies",
       "email": "hello@semi.technology"
     },
-    "version": "1.0.2"
+    "version": "1.0.3"
   },
   "basePath": "/v1",
   "paths": {
