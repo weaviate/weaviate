92bdbd51fc78546ca09afbf8e18e0dd50f5b0185
Etienne Dilocker <etienne@dilocker.de>
2021-02-16 16:57:42 +0100

    gh-1459 add a simple vectorizer to the module
    Inspired by the text2vec-contextionary module with some features
    (config, discovery) hard-coded at the moment

---
+++ modules/text2vec-transformers/vectorizer/vectorizer.go
@@ -0,0 +1,102 @@
+package vectorizer
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/fatih/camelcase"
+	"github.com/semi-technologies/weaviate/entities/models"
+	"github.com/semi-technologies/weaviate/modules/text2vec-transformers/ent"
+)
+
+type Vectorizer struct {
+	client Client
+}
+
+func New(client Client) *Vectorizer {
+	return &Vectorizer{
+		client: client,
+	}
+}
+
+type Client interface {
+	Vectorize(ctx context.Context, input string) (*ent.VectorizationResult, error)
+}
+
+// IndexCheck returns whether a property of a class should be indexed
+type ClassIndexCheck interface {
+	PropertyIndexed(property string) bool
+	VectorizeClassName() bool
+	VectorizePropertyName(propertyName string) bool
+}
+
+func (v *Vectorizer) Object(ctx context.Context, object *models.Object,
+	icheck ClassIndexCheck) error {
+	vec, err := v.object(ctx, object.Class, object.Properties, icheck)
+	if err != nil {
+		return err
+	}
+
+	object.Vector = vec
+	return nil
+}
+
+func (v *Vectorizer) object(ctx context.Context, className string,
+	schema interface{}, icheck ClassIndexCheck) ([]float32, error) {
+	var corpi []string
+
+	if icheck.VectorizeClassName() {
+		corpi = append(corpi, camelCaseToLower(className))
+	}
+
+	if schema != nil {
+		for prop, value := range schema.(map[string]interface{}) {
+			if !icheck.PropertyIndexed(prop) {
+				continue
+			}
+
+			valueString, ok := value.(string)
+			if ok {
+				if icheck.VectorizePropertyName(prop) {
+					// use prop and value
+					corpi = append(corpi, strings.ToLower(
+						fmt.Sprintf("%s %s", camelCaseToLower(prop), valueString)))
+				} else {
+					corpi = append(corpi, strings.ToLower(valueString))
+				}
+			}
+		}
+	}
+
+	if len(corpi) == 0 {
+		// fall back to using the class name
+		corpi = append(corpi, camelCaseToLower(className))
+	}
+
+	text := strings.Join(corpi, " ")
+	res, err := v.client.Vectorize(ctx, text)
+	if err != nil {
+		return nil, err
+	}
+
+	return res.Vector, nil
+}
+
+func camelCaseToLower(in string) string {
+	parts := camelcase.Split(in)
+	var sb strings.Builder
+	for i, part := range parts {
+		if part == " " {
+			continue
+		}
+
+		if i > 0 {
+			sb.WriteString(" ")
+		}
+
+		sb.WriteString(strings.ToLower(part))
+	}
+
+	return sb.String()
+}
