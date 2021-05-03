b452092ce15efdcdfb773fbb238540f036b04af2
Etienne Dilocker <etienne@dilocker.de>
2021-05-03 20:48:16 +0200

    gh-1523 support range queries for 'replace', memtable only atm

---
+++ adapters/repos/db/lsmkv/cursor_memtable.go
@@ -0,0 +1,58 @@
+package lsmkv
+
+import (
+	"bytes"
+)
+
+type memtableCursor struct {
+	data    []*binarySearchNode
+	current int
+}
+
+func (l *Memtable) newCursor() *memtableCursor {
+	// This cursor is a really primitive approach, it actually requires
+	// flattening the entire memtable - even if the cursor were to point to the
+	// very last element. However, given that the memtable will on average be
+	// only half it's max capacity and even that is relatively small, we might
+	// get away with the full-flattening and a linear search. Let's not optimize
+	// prematurely.
+
+	l.RLock()
+	defer l.RUnlock()
+
+	// TODO: support strategies other than replace
+	data := l.key.flattenInOrder()
+
+	return &memtableCursor{
+		data: data,
+	}
+}
+
+func (c *memtableCursor) seek(key []byte) ([]byte, []byte) {
+	pos := c.posLargerThanEqual(key)
+	if pos == -1 {
+		return nil, nil
+	}
+
+	c.current = pos
+	return c.data[pos].key, c.data[pos].value
+}
+
+func (c *memtableCursor) posLargerThanEqual(key []byte) int {
+	for i, node := range c.data {
+		if bytes.Compare(node.key, key) >= 0 {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func (c *memtableCursor) next() ([]byte, []byte) {
+	c.current++
+	if c.current >= len(c.data) {
+		return nil, nil
+	}
+
+	return c.data[c.current].key, c.data[c.current].value
+}
