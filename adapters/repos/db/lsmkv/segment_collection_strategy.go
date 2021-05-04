9229d30a9ea05a12c9928fdd4e296f00a805c5e2
Etienne Dilocker <etienne@dilocker.de>
2021-05-04 16:52:32 +0200

    gh-1523 store key alongside data when strat==collection

---
+++ adapters/repos/db/lsmkv/segment_collection_strategy.go
@@ -0,0 +1,128 @@
+package lsmkv
+
+import (
+	"bytes"
+	"encoding/binary"
+
+	"github.com/pkg/errors"
+)
+
+func (i *segment) getCollection(key []byte) ([]value, error) {
+	if i.strategy != SegmentStrategySetCollection &&
+		i.strategy != SegmentStrategyMapCollection {
+		return nil, errors.Errorf("get only possible for strategies %q, %q",
+			StrategySetCollection, StrategyMapCollection)
+	}
+
+	if !i.bloomFilter.Test(key) {
+		return nil, NotFound
+	}
+
+	node, err := i.index.Get(key)
+	if err != nil {
+		return nil, err
+	}
+
+	return i.collectionStratParseData(i.contents[node.Start:node.End])
+}
+
+func (i *segment) collectionStratParseData(in []byte) ([]value, error) {
+	if len(in) == 0 {
+		return nil, NotFound
+	}
+
+	r := bytes.NewReader(in)
+
+	readSoFar := 0
+
+	var valuesLen uint64
+	if err := binary.Read(r, binary.LittleEndian, &valuesLen); err != nil {
+		return nil, errors.Wrap(err, "read values len")
+	}
+	readSoFar += 8
+
+	values := make([]value, valuesLen)
+	for i := range values {
+		if err := binary.Read(r, binary.LittleEndian, &values[i].tombstone); err != nil {
+			return nil, errors.Wrap(err, "read value tombstone")
+		}
+		readSoFar += 1
+
+		var valueLen uint64
+		if err := binary.Read(r, binary.LittleEndian, &valueLen); err != nil {
+			return nil, errors.Wrap(err, "read value len")
+		}
+		readSoFar += 8
+
+		values[i].value = make([]byte, valueLen)
+		n, err := r.Read(values[i].value)
+		if err != nil {
+			return nil, errors.Wrap(err, "read value")
+		}
+		readSoFar += n
+	}
+
+	return values, nil
+}
+
+// TODO
+// nolint:unused
+type segmentCollectionParseResult struct {
+	key    []byte
+	values []value
+	read   int // so that the cursor and calculate its offset for the next round
+}
+
+// TODO
+// nolint:unused
+func (i *segment) collectionStratParseDataWithKey(in []byte) (segmentCollectionParseResult, error) {
+	out := segmentCollectionParseResult{}
+
+	if len(in) == 0 {
+		return out, NotFound
+	}
+
+	r := bytes.NewReader(in)
+
+	var valuesLen uint64
+	if err := binary.Read(r, binary.LittleEndian, &valuesLen); err != nil {
+		return out, errors.Wrap(err, "read values len")
+	}
+	out.read += 8
+
+	out.values = make([]value, valuesLen)
+	for i := range out.values {
+		if err := binary.Read(r, binary.LittleEndian, &out.values[i].tombstone); err != nil {
+			return out, errors.Wrap(err, "read value tombstone")
+		}
+		out.read += 1
+
+		var valueLen uint64
+		if err := binary.Read(r, binary.LittleEndian, &valueLen); err != nil {
+			return out, errors.Wrap(err, "read value len")
+		}
+		out.read += 8
+
+		out.values[i].value = make([]byte, valueLen)
+		n, err := r.Read(out.values[i].value)
+		if err != nil {
+			return out, errors.Wrap(err, "read value")
+		}
+		out.read += n
+	}
+
+	var keyLen uint32
+	if err := binary.Read(r, binary.LittleEndian, &keyLen); err != nil {
+		return out, errors.Wrap(err, "read key len")
+	}
+	out.read += 4
+
+	out.key = make([]byte, keyLen)
+	n, err := r.Read(out.key)
+	if err != nil {
+		return out, errors.Wrap(err, "read key")
+	}
+	out.read += n
+
+	return out, nil
+}
---
+++ adapters/repos/db/lsmkv/segment_replace_strategy.go
@@ -0,0 +1,103 @@
+package lsmkv
+
+import (
+	"bytes"
+	"encoding/binary"
+
+	"github.com/pkg/errors"
+)
+
+func (i *segment) get(key []byte) ([]byte, error) {
+	if i.strategy != SegmentStrategyReplace {
+		return nil, errors.Errorf("get only possible for strategy %q", StrategyReplace)
+	}
+
+	if !i.bloomFilter.Test(key) {
+		return nil, NotFound
+	}
+
+	node, err := i.index.Get(key)
+	if err != nil {
+		return nil, err
+	}
+
+	return i.replaceStratParseData(i.contents[node.Start:node.End])
+}
+
+func (i *segment) replaceStratParseData(in []byte) ([]byte, error) {
+	if len(in) == 0 {
+		return nil, NotFound
+	}
+
+	// check the tombstone byte
+	if in[0] == 0x01 {
+		return nil, Deleted
+	}
+
+	r := bytes.NewReader(in[1:])
+	var valueLength uint64
+	if err := binary.Read(r, binary.LittleEndian, &valueLength); err != nil {
+		return nil, errors.Wrap(err, "read value length encoding")
+	}
+
+	data := make([]byte, valueLength)
+	if _, err := r.Read(data); err != nil {
+		return nil, errors.Wrap(err, "read value")
+	}
+
+	return data, nil
+}
+
+type segmentParseResult struct {
+	deleted bool
+	key     []byte
+	value   []byte
+	read    int // so that the cursor and calculate its offset for the next round
+}
+
+func (i *segment) replaceStratParseDataWithKey(in []byte) (segmentParseResult, error) {
+	out := segmentParseResult{}
+
+	if len(in) == 0 {
+		return out, NotFound
+	}
+
+	// check the tombstone byte
+	if in[0] == 0x01 {
+		out.deleted = true
+	}
+	out.read += 1
+
+	r := bytes.NewReader(in[1:])
+	var valueLength uint64
+	if err := binary.Read(r, binary.LittleEndian, &valueLength); err != nil {
+		return out, errors.Wrap(err, "read value length encoding")
+	}
+	out.read += 8
+
+	out.value = make([]byte, valueLength)
+	if n, err := r.Read(out.value); err != nil {
+		return out, errors.Wrap(err, "read value")
+	} else {
+		out.read += n
+	}
+
+	var keyLength uint32
+	if err := binary.Read(r, binary.LittleEndian, &keyLength); err != nil {
+		return out, errors.Wrap(err, "read key length encoding")
+	}
+	out.read += 4
+
+	out.key = make([]byte, keyLength)
+	if n, err := r.Read(out.key); err != nil {
+		return out, errors.Wrap(err, "read key")
+	} else {
+		out.read += n
+	}
+
+	if out.deleted {
+		return out, Deleted
+	}
+
+	return out, nil
+}
