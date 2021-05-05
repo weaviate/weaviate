fc18a6c41ce7fe5a5aaa310b71d7d9f34dd02b1f
Etienne Dilocker <etienne@dilocker.de>
2021-05-05 13:40:32 +0200

    gh-1523 support deletes and disk flushes in 'Set' cursor

--- adapters/repos/db/lsmkv/strategies_set_integration_test.go
+++ adapters/repos/db/lsmkv/strategies_set_integration_test.go
@@ -801,212 +801,251 @@ func TestSetCollectionStrategy_Cursors(t *testing.T) {
 		})
 	})
 
-	// t.Run("with a single flush between updates", func(t *testing.T) {
-	// 	b, err := NewBucketWithStrategy(dirName, StrategySetCollection)
-	// 	require.Nil(t, err)
+	t.Run("with flushes", func(t *testing.T) {
+		rand.Seed(time.Now().UnixNano())
+		dirName := fmt.Sprintf("./testdata/%d", rand.Intn(10000000))
+		os.MkdirAll(dirName, 0o777)
+		defer func() {
+			err := os.RemoveAll(dirName)
+			fmt.Println(err)
+		}()
 
-	// 	// so big it effectively never triggers as part of this test
-	// 	b.SetMemtableThreshold(1e9)
+		b, err := NewBucketWithStrategy(dirName, StrategySetCollection)
+		require.Nil(t, err)
 
-	// 	key1 := []byte("test2-key-1")
-	// 	key2 := []byte("test2-key-2")
-	// 	key3 := []byte("test2-key-3")
+		// so big it effectively never triggers as part of this test
+		b.SetMemtableThreshold(1e9)
 
-	// 	t.Run("set original values and verify", func(t *testing.T) {
-	// 		orig1 := [][]byte{[]byte("value 1.1"), []byte("value 1.2")}
-	// 		orig2 := [][]byte{[]byte("value 2.1"), []byte("value 2.2")}
-	// 		orig3 := [][]byte{[]byte("value 3.1"), []byte("value 3.2")}
+		t.Run("first third (%3==0)", func(t *testing.T) {
+			pairs := 20
+			valuesPerPair := 3
+			var keys [][]byte
+			var values [][][]byte
 
-	// 		err = b.SetAdd(key1, orig1)
-	// 		require.Nil(t, err)
-	// 		err = b.SetAdd(key2, orig2)
-	// 		require.Nil(t, err)
-	// 		err = b.SetAdd(key3, orig3)
-	// 		require.Nil(t, err)
+			for i := 0; i < pairs; i++ {
+				if i%3 != 0 {
+					continue
+				}
+				keys = append(keys, []byte(fmt.Sprintf("key-%03d", i)))
+				curValues := make([][]byte, valuesPerPair)
+				for j := range curValues {
+					curValues[j] = []byte(fmt.Sprintf("value-%03d.%d", i, j))
+				}
+				values = append(values, curValues)
+			}
 
-	// 		res, err := b.SetList(key1)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, res, orig1)
-	// 		res, err = b.SetList(key2)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, res, orig2)
-	// 		res, err = b.SetList(key3)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, res, orig3)
-	// 	})
+			// shuffle to make sure the BST isn't accidentally in order
+			rand.Seed(time.Now().UnixNano())
+			rand.Shuffle(len(keys), func(i, j int) {
+				keys[i], keys[j] = keys[j], keys[i]
+				values[i], values[j] = values[j], values[i]
+			})
 
-	// 	t.Run("flush to disk", func(t *testing.T) {
-	// 		require.Nil(t, b.FlushAndSwitch())
-	// 	})
+			for i := range keys {
+				err = b.SetAdd(keys[i], values[i])
+				require.Nil(t, err)
+			}
+		})
 
-	// 	t.Run("replace some, keep one", func(t *testing.T) {
-	// 		orig1 := [][]byte{[]byte("value 1.1"), []byte("value 1.2")}
-	// 		orig2 := [][]byte{[]byte("value 2.1"), []byte("value 2.2")}
-	// 		orig3 := [][]byte{[]byte("value 3.1"), []byte("value 3.2")}
-	// 		append2 := [][]byte{[]byte("value 2.3")}
-	// 		append3 := [][]byte{[]byte("value 3.3")}
+		t.Run("flush to disk", func(t *testing.T) {
+			require.Nil(t, b.FlushAndSwitch())
+		})
 
-	// 		err = b.SetAdd(key2, append2)
-	// 		require.Nil(t, err)
-	// 		err = b.SetAdd(key3, append3)
-	// 		require.Nil(t, err)
+		t.Run("second third (%3==1)", func(t *testing.T) {
+			pairs := 20
+			valuesPerPair := 3
+			var keys [][]byte
+			var values [][][]byte
 
-	// 		res, err := b.SetList(key1)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, orig1, res)
-	// 		res, err = b.SetList(key2)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, append(orig2, append2...), res)
-	// 		res, err = b.SetList(key3)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, append(orig3, append3...), res)
-	// 	})
-	// })
+			for i := 0; i < pairs; i++ {
+				if i%3 != 1 {
+					continue
+				}
+				keys = append(keys, []byte(fmt.Sprintf("key-%03d", i)))
+				curValues := make([][]byte, valuesPerPair)
+				for j := range curValues {
+					curValues[j] = []byte(fmt.Sprintf("value-%03d.%d", i, j))
+				}
+				values = append(values, curValues)
+			}
 
-	// t.Run("with flushes after initial and update", func(t *testing.T) {
-	// 	b, err := NewBucketWithStrategy(dirName, StrategySetCollection)
-	// 	require.Nil(t, err)
+			// shuffle to make sure the BST isn't accidentally in order
+			rand.Seed(time.Now().UnixNano())
+			rand.Shuffle(len(keys), func(i, j int) {
+				keys[i], keys[j] = keys[j], keys[i]
+				values[i], values[j] = values[j], values[i]
+			})
 
-	// 	// so big it effectively never triggers as part of this test
-	// 	b.SetMemtableThreshold(1e9)
-	// 	key1 := []byte("test-3-key-1")
-	// 	key2 := []byte("test-3-key-2")
-	// 	key3 := []byte("test-3-key-3")
+			for i := range keys {
+				err = b.SetAdd(keys[i], values[i])
+				require.Nil(t, err)
+			}
+		})
 
-	// 	t.Run("set original values and verify", func(t *testing.T) {
-	// 		orig1 := [][]byte{[]byte("value 1.1"), []byte("value 1.2")}
-	// 		orig2 := [][]byte{[]byte("value 2.1"), []byte("value 2.2")}
-	// 		orig3 := [][]byte{[]byte("value 3.1"), []byte("value 3.2")}
+		t.Run("flush to disk", func(t *testing.T) {
+			require.Nil(t, b.FlushAndSwitch())
+		})
 
-	// 		err = b.SetAdd(key1, orig1)
-	// 		require.Nil(t, err)
-	// 		err = b.SetAdd(key2, orig2)
-	// 		require.Nil(t, err)
-	// 		err = b.SetAdd(key3, orig3)
-	// 		require.Nil(t, err)
+		t.Run("thrid third (%3==2) memtable-only", func(t *testing.T) {
+			pairs := 20
+			valuesPerPair := 3
+			var keys [][]byte
+			var values [][][]byte
 
-	// 		res, err := b.SetList(key1)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, res, orig1)
-	// 		res, err = b.SetList(key2)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, res, orig2)
-	// 		res, err = b.SetList(key3)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, res, orig3)
-	// 	})
+			for i := 0; i < pairs; i++ {
+				if i%3 != 2 {
+					continue
+				}
+				keys = append(keys, []byte(fmt.Sprintf("key-%03d", i)))
+				curValues := make([][]byte, valuesPerPair)
+				for j := range curValues {
+					curValues[j] = []byte(fmt.Sprintf("value-%03d.%d", i, j))
+				}
+				values = append(values, curValues)
+			}
 
-	// 	t.Run("flush to disk", func(t *testing.T) {
-	// 		require.Nil(t, b.FlushAndSwitch())
-	// 	})
+			// shuffle to make sure the BST isn't accidentally in order
+			rand.Seed(time.Now().UnixNano())
+			rand.Shuffle(len(keys), func(i, j int) {
+				keys[i], keys[j] = keys[j], keys[i]
+				values[i], values[j] = values[j], values[i]
+			})
 
-	// 	t.Run("replace some, keep one", func(t *testing.T) {
-	// 		orig1 := [][]byte{[]byte("value 1.1"), []byte("value 1.2")}
-	// 		orig2 := [][]byte{[]byte("value 2.1"), []byte("value 2.2")}
-	// 		orig3 := [][]byte{[]byte("value 3.1"), []byte("value 3.2")}
-	// 		append2 := [][]byte{[]byte("value 2.3")}
-	// 		append3 := [][]byte{[]byte("value 3.3")}
+			for i := range keys {
+				err = b.SetAdd(keys[i], values[i])
+				require.Nil(t, err)
+			}
 
-	// 		err = b.SetAdd(key2, append2)
-	// 		require.Nil(t, err)
-	// 		err = b.SetAdd(key3, append3)
-	// 		require.Nil(t, err)
+			// no flush for this one, so this segment stays in the memtable
+		})
 
-	// 		// Flush again!
-	// 		require.Nil(t, b.FlushAndSwitch())
+		t.Run("seek from somewhere in the middle", func(t *testing.T) {
+			expectedKeys := [][]byte{
+				[]byte("key-016"),
+				[]byte("key-017"),
+				[]byte("key-018"),
+				[]byte("key-019"),
+			}
+			expectedValues := [][][]byte{
+				[][]byte{[]byte("value-016.0"), []byte("value-016.1"), []byte("value-016.2")},
+				[][]byte{[]byte("value-017.0"), []byte("value-017.1"), []byte("value-017.2")},
+				[][]byte{[]byte("value-018.0"), []byte("value-018.1"), []byte("value-018.2")},
+				[][]byte{[]byte("value-019.0"), []byte("value-019.1"), []byte("value-019.2")},
+			}
 
-	// 		res, err := b.SetList(key1)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, orig1, res)
-	// 		res, err = b.SetList(key2)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, append(orig2, append2...), res)
-	// 		res, err = b.SetList(key3)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, append(orig3, append3...), res)
-	// 	})
-	// })
+			var retrievedKeys [][]byte
+			var retrievedValues [][][]byte
+			c := b.SetCursor()
+			for k, v := c.Seek([]byte("key-016")); k != nil; k, v = c.Next() {
+				retrievedKeys = append(retrievedKeys, k)
+				retrievedValues = append(retrievedValues, v)
+			}
 
-	// t.Run("update in memtable, then do an orderly shutdown, and re-init", func(t *testing.T) {
-	// 	b, err := NewBucketWithStrategy(dirName, StrategySetCollection)
-	// 	require.Nil(t, err)
+			assert.Equal(t, expectedKeys, retrievedKeys)
+			assert.Equal(t, expectedValues, retrievedValues)
+		})
 
-	// 	// so big it effectively never triggers as part of this test
-	// 	b.SetMemtableThreshold(1e9)
+		t.Run("start from the beginning", func(t *testing.T) {
+			expectedKeys := [][]byte{
+				[]byte("key-000"),
+				[]byte("key-001"),
+				[]byte("key-002"),
+			}
+			expectedValues := [][][]byte{
+				[][]byte{[]byte("value-000.0"), []byte("value-000.1"), []byte("value-000.2")},
+				[][]byte{[]byte("value-001.0"), []byte("value-001.1"), []byte("value-001.2")},
+				[][]byte{[]byte("value-002.0"), []byte("value-002.1"), []byte("value-002.2")},
+			}
 
-	// 	key1 := []byte("test4-key-1")
-	// 	key2 := []byte("test4-key-2")
-	// 	key3 := []byte("test4-key-3")
+			var retrievedKeys [][]byte
+			var retrievedValues [][][]byte
+			c := b.SetCursor()
+			retrieved := 0
+			for k, v := c.First(); k != nil && retrieved < 3; k, v = c.Next() {
+				retrieved++
+				retrievedKeys = append(retrievedKeys, k)
+				retrievedValues = append(retrievedValues, v)
+			}
 
-	// 	t.Run("set original values and verify", func(t *testing.T) {
-	// 		orig1 := [][]byte{[]byte("value 1.1"), []byte("value 1.2")}
-	// 		orig2 := [][]byte{[]byte("value 2.1"), []byte("value 2.2")}
-	// 		orig3 := [][]byte{[]byte("value 3.1"), []byte("value 3.2")}
+			assert.Equal(t, expectedKeys, retrievedKeys)
+			assert.Equal(t, expectedValues, retrievedValues)
+		})
 
-	// 		err = b.SetAdd(key1, orig1)
-	// 		require.Nil(t, err)
-	// 		err = b.SetAdd(key2, orig2)
-	// 		require.Nil(t, err)
-	// 		err = b.SetAdd(key3, orig3)
-	// 		require.Nil(t, err)
+		t.Run("delete & extend an existing key", func(t *testing.T) {
+			key := []byte("key-002")
+			extend := [][]byte{[]byte("value-002.3")}
 
-	// 		res, err := b.SetList(key1)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, res, orig1)
-	// 		res, err = b.SetList(key2)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, res, orig2)
-	// 		res, err = b.SetList(key3)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, res, orig3)
-	// 	})
+			require.Nil(t, b.SetAdd(key, extend))
 
-	// 	t.Run("replace some, keep one", func(t *testing.T) {
-	// 		orig1 := [][]byte{[]byte("value 1.1"), []byte("value 1.2")}
-	// 		orig2 := [][]byte{[]byte("value 2.1"), []byte("value 2.2")}
-	// 		orig3 := [][]byte{[]byte("value 3.1"), []byte("value 3.2")}
-	// 		append2 := [][]byte{[]byte("value 2.3")}
-	// 		append3 := [][]byte{[]byte("value 3.3")}
+			key = []byte("key-001")
+			deleteValue := []byte("value-001.1")
+			require.Nil(t, b.SetDeleteSingle(key, deleteValue))
+		})
 
-	// 		err = b.SetAdd(key2, append2)
-	// 		require.Nil(t, err)
-	// 		err = b.SetAdd(key3, append3)
-	// 		require.Nil(t, err)
+		t.Run("verify the extension is contained", func(t *testing.T) {
+			expectedKeys := [][]byte{
+				[]byte("key-001"),
+				[]byte("key-002"),
+			}
+			expectedValues := [][][]byte{
+				[][]byte{
+					[]byte("value-001.0"),
+					// "value-001.1" deleted
+					[]byte("value-001.2"),
+				},
+				[][]byte{
+					[]byte("value-002.0"), []byte("value-002.1"), []byte("value-002.2"),
+					[]byte("value-002.3"),
+				},
+			}
 
-	// 		res, err := b.SetList(key1)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, orig1, res)
-	// 		res, err = b.SetList(key2)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, append(orig2, append2...), res)
-	// 		res, err = b.SetList(key3)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, append(orig3, append3...), res)
-	// 	})
+			var retrievedKeys [][]byte
+			var retrievedValues [][][]byte
+			c := b.SetCursor()
+			retrieved := 0
+			for k, v := c.Seek([]byte("key-001")); k != nil && retrieved < 2; k, v = c.Next() {
+				retrieved++
+				retrievedKeys = append(retrievedKeys, k)
+				retrievedValues = append(retrievedValues, v)
+			}
 
-	// 	t.Run("orderly shutdown", func(t *testing.T) {
-	// 		b.Shutdown(context.Background())
-	// 	})
+			assert.Equal(t, expectedKeys, retrievedKeys)
+			assert.Equal(t, expectedValues, retrievedValues)
+		})
 
-	// 	t.Run("init another bucket on the same files", func(t *testing.T) {
-	// 		b2, err := NewBucketWithStrategy(dirName, StrategySetCollection)
-	// 		require.Nil(t, err)
+		t.Run("flush to disk", func(t *testing.T) {
+			require.Nil(t, b.FlushAndSwitch())
+		})
 
-	// 		orig1 := [][]byte{[]byte("value 1.1"), []byte("value 1.2")}
-	// 		orig2 := [][]byte{[]byte("value 2.1"), []byte("value 2.2")}
-	// 		orig3 := [][]byte{[]byte("value 3.1"), []byte("value 3.2")}
-	// 		append2 := [][]byte{[]byte("value 2.3")}
-	// 		append3 := [][]byte{[]byte("value 3.3")}
+		t.Run("verify again after flush", func(t *testing.T) {
+			expectedKeys := [][]byte{
+				[]byte("key-001"),
+				[]byte("key-002"),
+			}
+			expectedValues := [][][]byte{
+				[][]byte{
+					[]byte("value-001.0"),
+					// "value-001.1" deleted
+					[]byte("value-001.2"),
+				},
+				[][]byte{
+					[]byte("value-002.0"), []byte("value-002.1"), []byte("value-002.2"),
+					[]byte("value-002.3"),
+				},
+			}
 
-	// 		res, err := b2.SetList(key1)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, orig1, res)
-	// 		res, err = b2.SetList(key2)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, append(orig2, append2...), res)
-	// 		res, err = b2.SetList(key3)
-	// 		require.Nil(t, err)
-	// 		assert.Equal(t, append(orig3, append3...), res)
-	// 	})
-	// })
+			var retrievedKeys [][]byte
+			var retrievedValues [][][]byte
+			c := b.SetCursor()
+			retrieved := 0
+			for k, v := c.Seek([]byte("key-001")); k != nil && retrieved < 2; k, v = c.Next() {
+				retrieved++
+				retrievedKeys = append(retrievedKeys, k)
+				retrievedValues = append(retrievedValues, v)
+			}
+
+			assert.Equal(t, expectedKeys, retrievedKeys)
+			assert.Equal(t, expectedValues, retrievedValues)
+		})
+	})
 }
