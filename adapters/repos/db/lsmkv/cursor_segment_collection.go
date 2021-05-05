cacacc320d4c3a7c659915206aaa66557568cfb4
Etienne Dilocker <etienne@dilocker.de>
2021-05-05 13:15:19 +0200

    gh-1523 support disksegment cursors in 'Map' strategy

--- adapters/repos/db/lsmkv/cursor_segment_collection.go
+++ adapters/repos/db/lsmkv/cursor_segment_collection.go
@@ -1,6 +1,8 @@
 //nolint // TODO
 package lsmkv
 
+import "github.com/semi-technologies/weaviate/adapters/repos/db/lsmkv/segmentindex"
+
 type segmentCursorCollection struct {
 	segment    *segment
 	nextOffset uint64
@@ -23,57 +25,54 @@ func (s *SegmentGroup) newCollectionCursors() []innerCursorCollection {
 }
 
 func (s *segmentCursorCollection) seek(key []byte) ([]byte, []value, error) {
-	panic("not implemented")
-	// node, err := s.segment.index.Seek(key)
-	// if err != nil {
-	// 	if err == segmentindex.NotFound {
-	// 		return nil, nil, NotFound
-	// 	}
+	node, err := s.segment.index.Seek(key)
+	if err != nil {
+		if err == segmentindex.NotFound {
+			return nil, nil, NotFound
+		}
 
-	// 	return nil, nil, err
-	// }
+		return nil, nil, err
+	}
 
-	// parsed, err := s.segment.collectionStratParseDataWithKey(
-	// 	s.segment.contents[node.Start:node.End])
-	// if err != nil {
-	// 	return parsed.key, nil, err
-	// }
+	parsed, err := s.segment.collectionStratParseDataWithKey(
+		s.segment.contents[node.Start:node.End])
+	if err != nil {
+		return parsed.key, nil, err
+	}
 
-	// s.nextOffset = node.End
+	s.nextOffset = node.End
 
-	// return parsed.key, parsed.value, nil
+	return parsed.key, parsed.values, nil
 }
 
 func (s *segmentCursorCollection) next() ([]byte, []value, error) {
-	panic("not implemented")
-	// if s.nextOffset >= s.segment.dataEndPos {
-	// 	return nil, nil, NotFound
-	// }
+	if s.nextOffset >= s.segment.dataEndPos {
+		return nil, nil, NotFound
+	}
 
-	// parsed, err := s.segment.collectionStratParseDataWithKey(
-	// 	s.segment.contents[s.nextOffset:])
+	parsed, err := s.segment.collectionStratParseDataWithKey(
+		s.segment.contents[s.nextOffset:])
 
-	// // make sure to set the next offset before checking the error. The error
-	// // could be 'Deleted' which would require that the offset is still advanced
-	// // for the next cycle
-	// s.nextOffset = s.nextOffset + uint64(parsed.read)
-	// if err != nil {
-	// 	return parsed.key, nil, err
-	// }
+	// make sure to set the next offset before checking the error. The error
+	// could be 'Deleted' which would require that the offset is still advanced
+	// for the next cycle
+	s.nextOffset = s.nextOffset + uint64(parsed.read)
+	if err != nil {
+		return parsed.key, nil, err
+	}
 
-	// return parsed.key, parsed.value, nil
+	return parsed.key, parsed.values, nil
 }
 
 func (s *segmentCursorCollection) first() ([]byte, []value, error) {
-	panic("not implemented")
-	// s.nextOffset = s.segment.dataStartPos
-	// parsed, err := s.segment.collectionStratParseDataWithKey(
-	// 	s.segment.contents[s.nextOffset:])
-	// if err != nil {
-	// 	return nil, nil, err
-	// }
+	s.nextOffset = s.segment.dataStartPos
+	parsed, err := s.segment.collectionStratParseDataWithKey(
+		s.segment.contents[s.nextOffset:])
+	if err != nil {
+		return nil, nil, err
+	}
 
-	// s.nextOffset = s.nextOffset + uint64(parsed.read)
+	s.nextOffset = s.nextOffset + uint64(parsed.read)
 
-	// return parsed.key, parsed.value, nil
+	return parsed.key, parsed.values, nil
 }
