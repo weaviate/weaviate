bbddf7bc3754f4304c858dd4e43e2cb7dbef90eb
Etienne Dilocker <etienne@dilocker.de>
2021-06-08 16:56:26 +0200

    fix push docker script to always extract version
    (including on non-tagged commits)

--- ci/push_docker.sh
+++ ci/push_docker.sh
@@ -17,10 +17,10 @@ function release() {
   tag_latest="${DOCKER_REPO}:latest"
   tag_exact=
 
+  weaviate_version="$(jq -r '.info.version' < openapi-specs/schema.json)"
   if [ "$TRAVIS_BRANCH" = "master" ]; then
     tag_exact="${DOCKER_REPO}:${weaviate_version}-$(echo "$TRAVIS_COMMIT" | cut -c1-7)"
   elif [ -n "$TRAVIS_TAG" ]; then
-    weaviate_version="$(jq -r '.info.version' < openapi-specs/schema.json)"
         if [ "$TRAVIS_TAG" != "v$weaviate_version" ]; then
             echo "The release tag ($TRAVIS_TAG) and Weaviate version (v$weaviate_version) are not equal! Can't release."
             return 1
