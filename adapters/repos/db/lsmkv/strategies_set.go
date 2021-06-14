c630a848ebcaeb2d5cabf3614f8f046f9edee0ae
Etienne Dilocker <etienne@dilocker.de>
2021-06-14 14:37:58 +0200

    gh-1570 in "set" deal with old tombstones

--- adapters/repos/db/lsmkv/strategies_set.go
+++ adapters/repos/db/lsmkv/strategies_set.go
@@ -43,6 +43,30 @@ func (s *setDecoder) Do(in []value) [][]byte {
 	return out[:i]
 }
 
+// DoPartial keeps any extra tombstones, but does not keep tombstones which
+// were "consumed"
+func (s *setDecoder) DoPartial(in []value) []value {
+	count := map[string]uint{}
+	for _, value := range in {
+		count[string(value.value)] = count[string(value.value)] + 1
+	}
+
+	out := make([]value, len(in))
+
+	i := 0
+	for _, value := range in {
+		if count[string(value.value)] != 1 {
+			count[string(value.value)] = count[string(value.value)] - 1
+			continue
+		}
+
+		out[i] = value
+		i++
+	}
+
+	return out[:i]
+}
+
 type setEncoder struct{}
 
 func newSetEncoder() *setEncoder {
