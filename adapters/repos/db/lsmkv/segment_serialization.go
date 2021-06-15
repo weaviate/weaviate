67b8ab23db7c43d0666fe6f78debf1336e31db1e
Etienne Dilocker <etienne@dilocker.de>
2021-06-15 15:07:38 +0200

    gh-1570 prepare writing and reading of secondary indices
    At this point no secondary indices are being built or written yet, but
    the dynamic writing and reading already works.

--- adapters/repos/db/lsmkv/segment_serialization.go
+++ adapters/repos/db/lsmkv/segment_serialization.go
@@ -1,7 +1,9 @@
 package lsmkv
 
 import (
+	"bytes"
 	"encoding/binary"
+	"fmt"
 	"io"
 
 	"github.com/pkg/errors"
@@ -15,7 +17,7 @@ type segmentHeader struct {
 	indexStart       uint64
 }
 
-func (h *segmentHeader) WriteTo(w io.Writer) (int, error) {
+func (h *segmentHeader) WriteTo(w io.Writer) (int64, error) {
 	if err := binary.Write(w, binary.LittleEndian, &h.level); err != nil {
 		return -1, err
 	}
@@ -32,7 +34,63 @@ func (h *segmentHeader) WriteTo(w io.Writer) (int, error) {
 		return -1, err
 	}
 
-	return SegmentHeaderSize, nil
+	return int64(SegmentHeaderSize), nil
+}
+
+func (h *segmentHeader) PrimaryIndex(source []byte) ([]byte, error) {
+	if h.secondaryIndices == 0 {
+		fmt.Printf("read segment has no secondaries\n")
+		return source[h.indexStart:], nil
+	}
+
+	offsets, err := h.parseSecondaryIndexOffsets(
+		source[h.indexStart:h.secondaryIndexOffsetsEnd()])
+	if err != nil {
+		return nil, err
+	}
+
+	// the beginning of the first secondary is also the end of the primary
+	end := offsets[0]
+	fmt.Printf("primary index from to %d-%d\n", h.secondaryIndexOffsetsEnd(), end)
+	fmt.Printf("theoretical end would be %d\n", len(source))
+	return source[h.secondaryIndexOffsetsEnd():end], nil
+}
+
+func (h *segmentHeader) secondaryIndexOffsetsEnd() uint64 {
+	return h.indexStart + (uint64(h.secondaryIndices) * 8)
+}
+
+func (h *segmentHeader) parseSecondaryIndexOffsets(source []byte) ([]uint64, error) {
+	r := bytes.NewReader(source)
+
+	offsets := make([]uint64, h.secondaryIndices)
+	if err := binary.Read(r, binary.LittleEndian, &offsets); err != nil {
+		return nil, err
+	}
+
+	return offsets, nil
+}
+
+func (h *segmentHeader) SecondaryIndex(source []byte, indexID uint16) ([]byte, error) {
+	if indexID >= h.secondaryIndices {
+		return nil, errors.Errorf("retrieve index %d with len %d",
+			indexID, h.secondaryIndices)
+	}
+
+	offsets, err := h.parseSecondaryIndexOffsets(
+		source[h.indexStart:h.secondaryIndexOffsetsEnd()])
+	if err != nil {
+		return nil, err
+	}
+
+	start := offsets[indexID]
+	if indexID == h.secondaryIndices-1 {
+		// this is the last index, return until EOF
+		return source[start:], nil
+	}
+
+	end := offsets[indexID+1]
+	return source[start:end], nil
 }
 
 func parseSegmentHeader(r io.Reader) (*segmentHeader, error) {
