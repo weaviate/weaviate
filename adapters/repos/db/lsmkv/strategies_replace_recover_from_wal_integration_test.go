3669163103aeaf82371224c3af281fdb623098ee
Etienne Dilocker <etienne@dilocker.de>
2021-07-06 11:07:50 +0200

    gh-1614 don't error on wal EOF
    so that all items prior to the corrupt one can still be loaded at the
    startup process can continue.

--- adapters/repos/db/lsmkv/strategies_replace_recover_from_wal_integration_test.go
+++ adapters/repos/db/lsmkv/strategies_replace_recover_from_wal_integration_test.go
@@ -16,9 +16,11 @@ package lsmkv
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"math/rand"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"testing"
 	"time"
 
@@ -136,6 +138,159 @@ func TestReplaceStrategy_RecoverFromWAL(t *testing.T) {
 			res, err = bRec.Get(key3)
 			require.Nil(t, err)
 			assert.Equal(t, res, updated3)
+		})
+	})
+}
+
+func TestReplaceStrategy_RecoverFromWALWithCorruptLastElement(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	dirNameOriginal := fmt.Sprintf("./testdata/%d-original", rand.Intn(10000000))
+	dirNameRecovered := fmt.Sprintf("./testdata/%d-recovered", rand.Intn(10000000))
+	os.MkdirAll(dirNameOriginal, 0o777)
+	os.MkdirAll(dirNameRecovered, 0o777)
+	defer func() {
+		err := os.RemoveAll(dirNameOriginal)
+		fmt.Println(err)
+		err = os.RemoveAll(dirNameRecovered)
+		fmt.Println(err)
+	}()
+
+	t.Run("without previous state", func(t *testing.T) {
+		b, err := NewBucket(testCtx(), dirNameOriginal, nullLogger(), WithStrategy(StrategyReplace))
+		require.Nil(t, err)
+
+		// so big it effectively never triggers as part of this test
+		b.SetMemtableThreshold(1e9)
+
+		t.Run("set original values", func(t *testing.T) {
+			key1 := []byte("key-1")
+			key2 := []byte("key-2")
+			key3 := []byte("key-3")
+			orig1 := []byte("original value for key1")
+			orig2 := []byte("original value for key2")
+			orig3 := []byte("original value for key3")
+
+			err = b.Put(key1, orig1)
+			require.Nil(t, err)
+			err = b.Put(key2, orig2)
+			require.Nil(t, err)
+			err = b.Put(key3, orig3)
+			require.Nil(t, err)
+		})
+
+		t.Run("delete one, update one", func(t *testing.T) {
+			key2 := []byte("key-2")
+			key3 := []byte("key-3")
+			updated3 := []byte("updated value for key 3")
+
+			err = b.Delete(key2)
+			require.Nil(t, err)
+
+			err = b.Put(key3, updated3)
+			require.Nil(t, err)
+		})
+
+		t.Run("verify control", func(t *testing.T) {
+			key1 := []byte("key-1")
+			key2 := []byte("key-2")
+			key3 := []byte("key-3")
+			orig1 := []byte("original value for key1")
+			updated3 := []byte("updated value for key 3")
+			res, err := b.Get(key1)
+			require.Nil(t, err)
+			assert.Equal(t, res, orig1)
+			res, err = b.Get(key2)
+			require.Nil(t, err)
+			assert.Nil(t, res)
+			res, err = b.Get(key3)
+			require.Nil(t, err)
+			assert.Equal(t, res, updated3)
+		})
+
+		t.Run("make sure the WAL is flushed", func(t *testing.T) {
+			require.Nil(t, b.WriteWAL())
+		})
+
+		t.Run("copy state into recovery folder and destroy original", func(t *testing.T) {
+			cmd := exec.Command("/bin/bash", "-c", fmt.Sprintf("cp -r %s/*.wal %s",
+				dirNameOriginal, dirNameRecovered))
+			var out bytes.Buffer
+			cmd.Stderr = &out
+			err := cmd.Run()
+			if err != nil {
+				fmt.Println(out.String())
+				t.Fatal(err)
+			}
+			b = nil
+			require.Nil(t, os.RemoveAll(dirNameOriginal))
+		})
+
+		t.Run("corrupt WAL by removing some bytes at the very end", func(t *testing.T) {
+			entries, err := os.ReadDir(dirNameRecovered)
+			require.Nil(t, err)
+			require.Len(t, entries, 1, "there should be exactly one .wal file")
+
+			oldFileName := filepath.Join(dirNameRecovered, entries[0].Name())
+			tmpFileName := oldFileName + ".tmp"
+
+			err = os.Rename(oldFileName, tmpFileName)
+			require.Nil(t, err)
+
+			orig, err := os.Open(tmpFileName)
+			require.Nil(t, err)
+
+			correctLog, err := io.ReadAll(orig)
+			require.Nil(t, err)
+			err = orig.Close()
+			require.Nil(t, err)
+
+			corruptLog := correctLog[:len(correctLog)-6]
+
+			err = os.Remove(tmpFileName)
+			require.Nil(t, err)
+
+			corrupt, err := os.Create(oldFileName)
+			require.Nil(t, err)
+
+			_, err = corrupt.Write(corruptLog)
+			require.Nil(t, err)
+
+			err = corrupt.Close()
+			require.Nil(t, err)
+		})
+
+		var bRec *Bucket
+
+		t.Run("create new bucket from existing state", func(t *testing.T) {
+			b, err := NewBucket(testCtx(), dirNameRecovered, nullLogger(), WithStrategy(StrategyReplace))
+			require.Nil(t, err)
+
+			// so big it effectively never triggers as part of this test
+			b.SetMemtableThreshold(1e9)
+
+			bRec = b
+		})
+
+		t.Run("verify all data prior to the corruption is present", func(t *testing.T) {
+			key1 := []byte("key-1")
+			key2 := []byte("key-2")
+			key3 := []byte("key-3")
+			orig1 := []byte("original value for key1")
+			notUpdated3 := []byte("original value for key3")
+
+			// the last operation we performed (that now got corrupted) was an update
+			// on key3. So now that we're expecting all state prior to the corruption
+			// to be present, we would expect the original value for key3
+
+			res, err := bRec.Get(key1)
+			require.Nil(t, err)
+			assert.Equal(t, res, orig1)
+			res, err = bRec.Get(key2)
+			require.Nil(t, err)
+			assert.Nil(t, res)
+			res, err = bRec.Get(key3)
+			require.Nil(t, err)
+			assert.Equal(t, res, notUpdated3)
 		})
 	})
 }
