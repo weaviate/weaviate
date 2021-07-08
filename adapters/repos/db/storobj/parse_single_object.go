10cc7a71aa3c835899a71ee8030d2aaec84c3e30
Etienne Dilocker <etienne@dilocker.de>
2021-07-08 13:21:36 +0200

    gh-1616 exchange json parser and remove unnecessary allocations
    A lot of reading was done using binary.LittleEndian.Read() which led to
    allocations we didn't need. By not using a reader, but accessing the
    slice directly, we can keep a lot of short-term variables on the stack.
    This removes a lot of unnecessary allocs.

--- adapters/repos/db/storobj/parse_single_object.go
+++ adapters/repos/db/storobj/parse_single_object.go
@@ -1,73 +1,45 @@
 package storobj
 
 import (
-	"bytes"
 	"encoding/binary"
-	"encoding/json"
-	"io"
 
+	"github.com/buger/jsonparser"
 	"github.com/pkg/errors"
 )
 
 func ParseAndExtractTextProp(data []byte, propName string) (string, bool, error) {
-	var version uint8
-	r := bytes.NewReader(data)
-	le := binary.LittleEndian
-	if err := binary.Read(r, le, &version); err != nil {
+	propsBytes, err := extractPropsBytes(data)
+	if err != nil {
 		return "", false, err
 	}
 
-	if version != 1 {
-		return "", false, errors.Errorf("unsupported binary marshaller version %d", version)
-	}
-
-	io.CopyN(io.Discard, r, discardBytesPreVector)
-
-	var vecLen uint16
-	if err := binary.Read(r, le, &vecLen); err != nil {
+	val, _, _, err := jsonparser.Get(propsBytes, propName)
+	if err != nil {
 		return "", false, err
 	}
 
-	io.CopyN(io.Discard, r, int64(vecLen*4))
-
-	var classNameLen uint16
-	if err := binary.Read(r, le, &classNameLen); err != nil {
-		return "", false, err
-	}
-
-	io.CopyN(io.Discard, r, int64(classNameLen))
-
-	var propsLen uint32
-	if err := binary.Read(r, le, &propsLen); err != nil {
-		return "", false, err
-	}
-
-	start := int64(1 + discardBytesPreVector + 2 + 4*vecLen + 2 + classNameLen + 4)
-	end := start + int64(propsLen)
-
-	// propsBytes := make([]byte, propsLen)
-	// if _, err := r.Read(propsBytes); err != nil {
-	// 	return "", false, err
-	// }
-	propsBytes := data[start:end]
-	// _ = propsBytes
-	// return "", false, nil
-
-	var propsMap map[string]interface{}
-	// if err := json.Unmarshal(propsBytes, &propsMap); err != nil {
-	// 	return "", false, err
-	// }
-
-	if err := json.NewDecoder(bytes.NewReader(propsBytes)).Decode(&propsMap); err != nil {
-		return "", false, err
-	}
-
-	prop, ok := propsMap[propName]
-	if !ok {
-		return "", false, nil
-	}
-
-	return prop.(string), true, nil
+	return string(val), len(val) > 0, err
 }
 
-const discardBytesPreVector = 8 + 1 + 16 + 8 + 8
+func extractPropsBytes(data []byte) ([]byte, error) {
+	version := uint8(data[0])
+	if version != 1 {
+		return nil, errors.Errorf("unsupported binary marshaller version %d", version)
+	}
+
+	vecLen := binary.LittleEndian.Uint16(data[discardBytesPreVector : discardBytesPreVector+2])
+
+	classNameStart := discardBytesPreVector + 2 + vecLen*4
+
+	classNameLen := binary.LittleEndian.Uint16(data[classNameStart : classNameStart+2])
+
+	propsLenStart := classNameStart + 2 + classNameLen
+	propsLen := binary.LittleEndian.Uint32(data[propsLenStart : propsLenStart+4])
+
+	start := int64(propsLenStart + 4)
+	end := start + int64(propsLen)
+
+	return data[start:end], nil
+}
+
+const discardBytesPreVector = 1 + 8 + 1 + 16 + 8 + 8
