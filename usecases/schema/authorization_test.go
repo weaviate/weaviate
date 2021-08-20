1ed52dc0fec2fea6a4d990e65c10c8db8a7f355c
Etienne Dilocker <etienne@dilocker.de>
2021-08-20 10:36:12 +0200

    gh-1654 remove unused code from schema manager

--- usecases/schema/authorization_test.go
+++ usecases/schema/authorization_test.go
@@ -79,12 +79,6 @@ func Test_Schema_Authorization(t *testing.T) {
 			expectedResource: "schema/objects",
 		},
 		testCase{
-			methodName:       "UpdateClassProperty",
-			additionalArgs:   []interface{}{"somename", "someprop", &models.Property{}},
-			expectedVerb:     "update",
-			expectedResource: "schema/objects",
-		},
-		testCase{
 			methodName:       "UpdatePropertyAddDataType",
 			additionalArgs:   []interface{}{"somename", "someprop", "datatype"},
 			expectedVerb:     "update",
