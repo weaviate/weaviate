fe2de9d7739c90ee96f3f610a4ccefc9ab0df87f
Etienne Dilocker <etienne@dilocker.de>
2021-09-30 12:30:33 +0200

    prepare release v1.8.0-rc.0

--- adapters/handlers/rest/doc.go
+++ adapters/handlers/rest/doc.go
@@ -18,7 +18,7 @@
 //    https
 //  Host: localhost
 //  BasePath: /v1
-//  Version: 1.7.2
+//  Version: 1.8.0-rc.0
 //  Contact: Weaviate<hello@semi.technology> https://github.com/semi-technologies
 //
 //  Consumes:
