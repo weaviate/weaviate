3cbedabffe70a39749cdc4871cbd7ece71a141b7
Etienne Dilocker <etienne@dilocker.de>
2021-12-10 12:33:13 +0100

    gh-1754 add error message to multi2vec, add client tests

---
+++ modules/multi2vec-clip/clients/vectorizer_test.go
@@ -0,0 +1,95 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2021 SeMI Technologies B.V. All rights reserved.
+//
+//  CONTACT: hello@semi.technology
+//
+
+package clients
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/semi-technologies/weaviate/modules/multi2vec-clip/ent"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestVectorize(t *testing.T) {
+	t.Run("when the response is successful", func(t *testing.T) {
+		server := httptest.NewServer(&testVectorizeHandler{
+			t: t,
+			res: vecResponse{
+				TextVectors: [][]float32{
+					{0, 1, 2},
+				},
+				ImageVectors: [][]float32{
+					{1, 2, 3},
+				},
+			},
+		})
+		defer server.Close()
+		c := New(server.URL, nullLogger())
+		res, err := c.Vectorize(context.Background(), []string{"hello"},
+			[]string{"image-encoding"})
+
+		assert.Nil(t, err)
+		assert.Equal(t, &ent.VectorizationResult{
+			TextVectors: [][]float32{
+				{0, 1, 2},
+			},
+			ImageVectors: [][]float32{
+				{1, 2, 3},
+			},
+		}, res)
+	})
+
+	t.Run("when the server has a an error", func(t *testing.T) {
+		server := httptest.NewServer(&testVectorizeHandler{
+			t: t,
+			res: vecResponse{
+				Error: "some error from the server",
+			},
+		})
+		defer server.Close()
+		c := New(server.URL, nullLogger())
+		_, err := c.Vectorize(context.Background(), []string{"hello"},
+			[]string{"image-encoding"})
+
+		require.NotNil(t, err)
+		assert.Contains(t, err.Error(), "some error from the server")
+	})
+}
+
+type testVectorizeHandler struct {
+	t *testing.T
+	// the test handler will report as not ready before the time has passed
+	res vecResponse
+}
+
+func (f *testVectorizeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	assert.Equal(f.t, "/vectorize", r.URL.String())
+	assert.Equal(f.t, http.MethodPost, r.Method)
+
+	if f.res.Error != "" {
+		w.WriteHeader(500)
+	}
+	jsonBytes, _ := json.Marshal(f.res)
+	w.Write(jsonBytes)
+}
+
+func ptFloat(in float64) *float64 {
+	return &in
+}
+
+func ptString(in string) *string {
+	return &in
+}
