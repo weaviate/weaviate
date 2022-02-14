4d0ac319dab18aad68412050decd42874ab443e1
Etienne Dilocker <etienne@dilocker.de>
2022-02-14 15:19:37 +0100

    gh-1798 build minimal bm25 implementation for single word query
    Lots of limitations as of now:
    
    - It currently only supports searching across a single property
    - It currently only supports a single word in the query. So you can’t
      have spaces yet :smile: That’s the biggest limitation to get this
      usable and is the next big challenge
    - It isn’t real BM25 yet, BM25 weights matches also by the length which
      I’m currently just faking by giving each result the same hard-coded
      length, so right now the results probably aren’t entirely correct yet,
      but they do make sense in general.
    - It is not optimized in any way and will probably break at scale for
      now :slightly_smiling_face:
    - It cannot yet be combined with filters

--- adapters/repos/db/inverted/bm25_searcher.go
+++ adapters/repos/db/inverted/bm25_searcher.go
@@ -1,9 +1,17 @@
 package inverted
 
 import (
+	"bytes"
 	"context"
+	"encoding/binary"
+	"fmt"
+	"math"
+	"runtime/debug"
+	"sort"
+	"strings"
 
 	"github.com/pkg/errors"
+	"github.com/semi-technologies/weaviate/adapters/repos/db/helpers"
 	"github.com/semi-technologies/weaviate/adapters/repos/db/lsmkv"
 	"github.com/semi-technologies/weaviate/adapters/repos/db/propertyspecific"
 	"github.com/semi-technologies/weaviate/entities/additional"
@@ -40,5 +48,157 @@ func (b *BM25Searcher) Object(ctx context.Context, limit int,
 	keywordRanking *traverser.KeywordRankingParams,
 	filter *filters.LocalFilter, additional additional.Properties,
 	className schema.ClassName) ([]*storobj.Object, error) {
-	return nil, errors.Errorf("bm25 not implemented yet")
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Println(err)
+			debug.PrintStack()
+		}
+	}()
+
+	// TODO: more complex pre-processing
+	terms := strings.Split(keywordRanking.Query, " ")
+
+	if len(terms) != 1 {
+		return nil, errors.Errorf("only single term supported so far")
+	}
+
+	// TODO: support multi term queries
+	term := terms[0]
+
+	ids, err := b.getIdsWithFrequenciesForTerm(ctx, keywordRanking.Properties[0], term)
+	if err != nil {
+		return nil, errors.Wrap(err, "read doc ids and their frequencies from inverted index")
+	}
+
+	b.score(ids)
+
+	// TODO we only need to pick the top k, not sort the entire list
+	sort.Slice(ids.docIDs, func(a, b int) bool {
+		return ids.docIDs[a].score > ids.docIDs[b].score
+	})
+
+	res, err := b.objectsByDocID(ids.IDs(), additional)
+	if err != nil {
+		return nil, errors.Wrap(err, "resolve doc ids to objects")
+	}
+
+	return res, nil
+}
+
+func (bm *BM25Searcher) score(ids docPointersWithScore) {
+	averageDocLen := float64(1) // TODO: use real value
+	docLen := float64(1)        // TODO: use real value
+	k1 := 1.2                   // TODO: make configurable
+	b := 0.75                   // TODO: make configurable
+	N := float64(bm.store.Bucket(helpers.ObjectsBucketLSM).Count())
+	n := float64(len(ids.docIDs))
+	idf := math.Log(float64(1) + (N-n+0.5)/(n+0.5))
+	for i, id := range ids.docIDs {
+		tf := id.frequency / (id.frequency + k1*(1-b+b*docLen/averageDocLen))
+		ids.docIDs[i].score = tf * idf
+	}
+}
+
+func (b *BM25Searcher) getIdsWithFrequenciesForTerm(ctx context.Context,
+	prop, term string) (docPointersWithScore, error) {
+	bucketName := helpers.BucketFromPropNameLSM(prop)
+	bucket := b.store.Bucket(bucketName)
+
+	return b.docPointersInvertedFrequency(prop, bucket, 0, &propValuePair{
+		operator: filters.OperatorEqual,
+		value:    []byte(term),
+		prop:     prop,
+	}, true)
+}
+
+func (b *BM25Searcher) docPointersInvertedFrequency(prop string, bucket *lsmkv.Bucket,
+	limit int, pv *propValuePair, tolerateDuplicates bool) (docPointersWithScore, error) {
+	rr := NewRowReaderFrequency(bucket, pv.value, pv.operator, false)
+
+	var pointers docPointersWithScore
+	var hashes [][]byte
+
+	if err := rr.Read(context.TODO(), func(k []byte, pairs []lsmkv.MapPair) (bool, error) {
+		currentDocIDs := make([]docPointerWithScore, len(pairs))
+		// beforePairs := time.Now()
+		for i, pair := range pairs {
+			currentDocIDs[i].id = binary.LittleEndian.Uint64(pair.Key)
+			freqBits := binary.LittleEndian.Uint64(pair.Value)
+			currentDocIDs[i].frequency = math.Float64frombits(freqBits)
+		}
+		// fmt.Printf("loop through pairs took %s\n", time.Since(beforePairs))
+
+		pointers.count += uint64(len(pairs))
+		if len(pointers.docIDs) > 0 {
+			pointers.docIDs = append(pointers.docIDs, currentDocIDs...)
+		} else {
+			pointers.docIDs = currentDocIDs
+		}
+
+		hashBucket := b.store.Bucket(helpers.HashBucketFromPropNameLSM(pv.prop))
+		if b == nil {
+			return false, errors.Errorf("no hash bucket for prop '%s' found", pv.prop)
+		}
+
+		// use retrieved k instead of pv.value - they are typically the same, but
+		// not on a like operator with wildcard where we only had a partial match
+		currHash, err := hashBucket.Get(k)
+		if err != nil {
+			return false, errors.Wrap(err, "get hash")
+		}
+
+		hashes = append(hashes, currHash)
+		if limit > 0 && pointers.count >= uint64(limit) {
+			return false, nil
+		}
+
+		return true, nil
+	}); err != nil {
+		return pointers, errors.Wrap(err, "read row")
+	}
+
+	pointers.checksum = combineChecksums(hashes, pv.operator)
+
+	// TODO
+	// if !tolerateDuplicates {
+	// 	pointers.removeDuplicates()
+	// }
+	return pointers, nil
+}
+
+func (bm *BM25Searcher) objectsByDocID(ids []uint64,
+	additional additional.Properties) ([]*storobj.Object, error) {
+	out := make([]*storobj.Object, len(ids))
+
+	bucket := bm.store.Bucket(helpers.ObjectsBucketLSM)
+	if bucket == nil {
+		return nil, errors.Errorf("objects bucket not found")
+	}
+
+	i := 0
+
+	for _, id := range ids {
+		keyBuf := bytes.NewBuffer(nil)
+		binary.Write(keyBuf, binary.LittleEndian, &id)
+		docIDBytes := keyBuf.Bytes()
+		res, err := bucket.GetBySecondary(0, docIDBytes)
+		if err != nil {
+			return nil, err
+		}
+
+		if res == nil {
+			continue
+		}
+
+		unmarshalled, err := storobj.FromBinaryOptional(res, additional)
+		if err != nil {
+			return nil, errors.Wrapf(err, "unmarshal data object at position %d", i)
+		}
+
+		out[i] = unmarshalled
+		i++
+	}
+
+	return out[:i], nil
 }
