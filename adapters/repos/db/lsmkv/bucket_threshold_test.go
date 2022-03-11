2551472f1bcb3a70ba6fcd9f1814ae21f7935163
Travis CI User <travis@example.org>
2022-03-11 16:54:55 +0000

    bleep bloop => auto updated Weaviate

--- adapters/repos/db/lsmkv/bucket_threshold_test.go
+++ adapters/repos/db/lsmkv/bucket_threshold_test.go
@@ -1,5 +1,13 @@
-//go:build integrationTest
-// +build integrationTest
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2022 SeMI Technologies B.V. All rights reserved.
+//
+//  CONTACT: hello@semi.technology
+//
 
 package lsmkv
 
