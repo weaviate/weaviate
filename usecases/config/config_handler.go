3a44b1f61c59f620489a346c3fbc4f4eeefdc467
Parker Duckworth <parkerduckworth@gmail.com>
2022-03-17 13:45:23 -0500

    add bm25 env config handler

--- usecases/config/config_handler.go
+++ usecases/config/config_handler.go
@@ -52,6 +52,7 @@ type Config struct {
 	ModulesPath             string         `json:"modules_path" yaml:"modules_path"`
 	AutoSchema              AutoSchema     `json:"auto_schema" yaml:"auto_schema"`
 	Cluster                 cluster.Config `json:"cluster" yaml:"cluster"`
+	BM25                    BM25           `json:"bm25" yaml:"bm25"`
 }
 
 type moduleProvider interface {
@@ -118,6 +119,21 @@ func (p Persistence) Validate() error {
 	return nil
 }
 
+type BM25 struct {
+	K1 float64 `json:"k1" yaml:"k1"`
+	B  float64 `json:"b" yaml:"b"`
+}
+
+func (bm25 BM25) Validate() error {
+	if bm25.K1 < 0 {
+		return fmt.Errorf("BM25.k1 must be >= 0")
+	}
+	if bm25.B < 0 || bm25.B > 1 {
+		return fmt.Errorf("BM25.b must be <= 0 and <= 1")
+	}
+	return nil
+}
+
 // GetConfigOptionGroup creates a option group for swagger
 func GetConfigOptionGroup() *swag.CommandLineOptionsGroup {
 	commandLineOptionsGroup := swag.CommandLineOptionsGroup{
@@ -160,7 +176,7 @@ func (f *WeaviateConfig) LoadConfig(flags *swag.CommandLineOptionsGroup, logger 
 	if len(file) > 0 {
 		config, err := f.parseConfigFile(file, configFileName)
 		if err != nil {
-			return err
+			return configErr(err)
 		}
 		f.Config = config
 
@@ -168,23 +184,27 @@ func (f *WeaviateConfig) LoadConfig(flags *swag.CommandLineOptionsGroup, logger 
 	}
 
 	if err := FromEnv(&f.Config); err != nil {
-		return err
+		return configErr(err)
 	}
 
 	if err := f.Config.Authentication.Validate(); err != nil {
-		return fmt.Errorf("invalid config: %v", err)
+		return configErr(err)
 	}
 
 	if err := f.Config.Authorization.Validate(); err != nil {
-		return fmt.Errorf("invalid config: %v", err)
+		return configErr(err)
 	}
 
 	if err := f.Config.Persistence.Validate(); err != nil {
-		return fmt.Errorf("invalid config: %v", err)
+		return configErr(err)
 	}
 
 	if err := f.Config.AutoSchema.Validate(); err != nil {
-		return fmt.Errorf("invalid config: %v", err)
+		return configErr(err)
+	}
+
+	if err := f.Config.BM25.Validate(); err != nil {
+		return configErr(err)
 	}
 
 	return nil
@@ -214,4 +234,8 @@ func (f *WeaviateConfig) parseConfigFile(file []byte, name string) (Config, erro
 	}
 
 	return config, nil
+}
+
+func configErr(err error) error {
+	return fmt.Errorf("invalid config: %v", err)
 }
