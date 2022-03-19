6c0d439ea73e090d9885bfe6dfee526d168ea085
Etienne Dilocker <etienne@dilocker.de>
2022-03-19 13:32:20 +0100

    gh-1867 in thread-local cache store index in existing structures
    This completely eliminates the need for a linear or binary search
    through the thread-local cache, since we just kept the lookup
    information of the index in which we retrieved the items.

--- adapters/repos/db/vector/hnsw/heuristic.go
+++ adapters/repos/db/vector/hnsw/heuristic.go
@@ -14,7 +14,6 @@ package hnsw
 import (
 	"context"
 
-	"github.com/pkg/errors"
 	"github.com/semi-technologies/weaviate/adapters/repos/db/helpers"
 	"github.com/semi-technologies/weaviate/adapters/repos/db/vector/hnsw/priorityqueue"
 )
@@ -25,76 +24,17 @@ func (h *hnsw) selectNeighborsHeuristic(input *priorityqueue.Queue,
 		return nil
 	}
 
-	closestFirst := h.pools.pqHeuristic.GetMin(input.Len())
-	for input.Len() > 0 {
-		elem := input.Pop()
-		closestFirst.Insert(elem.ID, elem.Dist)
-	}
-
-	returnList := h.pools.pqItemSlice.Get().([]priorityqueue.Item)
-
-	for closestFirst.Len() > 0 && len(returnList) < max {
-		curr := closestFirst.Pop()
-		if denyList != nil && denyList.Contains(curr.ID) {
-			continue
-		}
-		distToQuery := curr.Dist
-
-		good := true
-		for _, item := range returnList {
-
-			peerDist, ok, err := h.distBetweenNodes(curr.ID, item.ID)
-			if err != nil {
-				return errors.Wrapf(err, "distance between %d and %d", curr.ID, item.ID)
-			}
-
-			if !ok {
-				continue
-			}
-
-			if peerDist < distToQuery {
-				good = false
-				break
-			}
-		}
-
-		if good {
-			returnList = append(returnList, curr)
-		}
-
-	}
-
-	h.pools.pqHeuristic.Put(closestFirst)
-
-	for _, retElem := range returnList {
-		input.Insert(retElem.ID, retElem.Dist)
-	}
-
-	// rewind and return to pool
-	returnList = returnList[:0]
-
-	// nolint:staticcheck
-	h.pools.pqItemSlice.Put(returnList)
-
-	return nil
-}
-
-func (h *hnsw) selectNeighborsHeuristicWithStatistics(input *priorityqueue.Queue,
-	max int, denyList helpers.AllowList, statistics map[uint64]int) error {
-	if input.Len() < max {
-		return nil
-	}
-
-	localVectorCache := map[uint64][]float32{}
-
-	// TODO, if this solution stays we need somethign with fewer allocs
-	ids := make([]uint64, 0, input.Len())
+	// TODO, if this solution stays we might need something with fewer allocs
+	ids := make([]uint64, input.Len())
+	var vecs [][]float32
 
 	closestFirst := h.pools.pqHeuristic.GetMin(input.Len())
+	i := uint64(0)
 	for input.Len() > 0 {
 		elem := input.Pop()
-		closestFirst.Insert(elem.ID, elem.Dist)
-		ids = append(ids, elem.ID)
+		closestFirst.Insert(elem.ID, i, elem.Dist)
+		ids[i] = elem.ID
+		i++
 	}
 
 	vecs, err := h.multiVectorForID(context.TODO(), ids)
@@ -102,11 +42,7 @@ func (h *hnsw) selectNeighborsHeuristicWithStatistics(input *priorityqueue.Queue
 		return err
 	}
 
-	for i, id := range ids {
-		localVectorCache[id] = vecs[i]
-	}
-
-	returnList := h.pools.pqItemSlice.Get().([]priorityqueue.Item)
+	returnList := h.pools.pqItemSlice.Get().([]priorityqueue.ItemWithIndex)
 
 	for closestFirst.Len() > 0 && len(returnList) < max {
 		curr := closestFirst.Pop()
@@ -115,23 +51,11 @@ func (h *hnsw) selectNeighborsHeuristicWithStatistics(input *priorityqueue.Queue
 		}
 		distToQuery := curr.Dist
 
+		currVec := vecs[curr.Index]
 		good := true
 		for _, item := range returnList {
-
-			// statistics[curr.ID]++
-			// statistics[item.ID]++
-			// peerDist, ok, err := h.distBetweenNodes(curr.ID, item.ID)
-			// if err != nil {
-			// 	return errors.Wrapf(err, "distance between %d and %d", curr.ID, item.ID)
-			// }
-
-			// current distance provider implementations cannot fail
-			peerDist, _, _ := h.distancerProvider.SingleDist(localVectorCache[curr.ID],
-				localVectorCache[item.ID])
-
-			// if !ok {
-			// 	continue
-			// }
+			peerDist, _, _ := h.distancerProvider.SingleDist(currVec,
+				vecs[item.Index])
 
 			if peerDist < distToQuery {
 				good = false
