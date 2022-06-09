bd8f5407e9c4b8526fc33c00dfaf3001003b2742
Parker Duckworth <31421773+parkerduckworth@users.noreply.github.com>
2022-06-09 09:52:43 -0500

    Fix aggregate groupBy/nearMedia bug: certainty was ignored (#1975)

---
+++ adapters/repos/db/aggregator/vector_search.go
@@ -0,0 +1,53 @@
+package aggregator
+
+import (
+	"github.com/pkg/errors"
+	"github.com/semi-technologies/weaviate/adapters/repos/db/helpers"
+)
+
+func (a *Aggregator) vectorSearch(allow helpers.AllowList) (ids []uint64, err error) {
+	if a.params.ObjectLimit != nil {
+		ids, err = a.searchByVector(a.params.SearchVector, a.params.ObjectLimit, allow)
+		return
+	}
+
+	ids, err = a.searchByVectorDistance(a.params.SearchVector, allow)
+	return
+}
+
+func (a *Aggregator) searchByVector(searchVector []float32, limit *int, ids helpers.AllowList) ([]uint64, error) {
+	idsFound, dists, err := a.vectorIndex.SearchByVector(searchVector, *limit, ids)
+	if err != nil {
+		return idsFound, err
+	}
+
+	if a.params.Certainty > 0 {
+		targetDist := float32(1-a.params.Certainty) * 2
+
+		i := 0
+		for _, dist := range dists {
+			if dist > targetDist {
+				break
+			}
+			i++
+		}
+
+		return idsFound[:i], nil
+
+	}
+	return idsFound, nil
+}
+
+func (a *Aggregator) searchByVectorDistance(searchVector []float32, ids helpers.AllowList) ([]uint64, error) {
+	if a.params.Certainty <= 0 {
+		return nil, errors.New("must provide certainty or objectLimit with vector search")
+	}
+
+	targetDist := float32(1-a.params.Certainty) * 2
+	idsFound, _, err := a.vectorIndex.SearchByVectorDistance(searchVector, targetDist, -1, ids)
+	if err != nil {
+		return nil, errors.Wrap(err, "aggregate search by vector")
+	}
+
+	return idsFound, nil
+}
