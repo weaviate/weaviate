93d032250d61dd9ad679ad44c3c4b52846033391
etiennedi <etienne@dilocker.de>
2022-07-02 18:27:41 +0200

    add limit by distance cases for l2 distance

--- test/acceptance/vector_distances/l2_test.go
+++ test/acceptance/vector_distances/l2_test.go
@@ -39,22 +39,143 @@ func addTestDataL2(t *testing.T) {
 }
 
 func testL2(t *testing.T) {
-	res := AssertGraphQL(t, nil, `
-	{
-	  Get{
-			L2Squared_Class(nearVector:{vector: [10,11,12]}){
-		  	name 
-		  	_additional{distance certainty}
-		  }
+	t.Run("without any limiting parameters", func(t *testing.T) {
+		res := AssertGraphQL(t, nil, `
+		{
+			Get{
+				L2Squared_Class(nearVector:{vector: [10,11,12]}){
+					name 
+					_additional{distance certainty}
+				}
+			}
 		}
-	}
-	`)
-	results := res.Get("Get", "L2Squared_Class").AsSlice()
-	expectedDistances := []float32{
-		0,   // the same vector as the query
-		50,  // distance to the second vector
-		365, // l2 squared distance to the root
-	}
+		`)
+		results := res.Get("Get", "L2Squared_Class").AsSlice()
+		expectedDistances := []float32{
+			0,   // the same vector as the query
+			50,  // distance to the second vector
+			365, // l2 squared distance to the root
+		}
 
-	compareDistances(t, expectedDistances, results)
+		compareDistances(t, expectedDistances, results)
+	})
+
+	t.Run("with a certainty", func(t *testing.T) {
+		// not supported for non-cosine distances
+		ErrorGraphQL(t, nil, `
+		{
+			Get{
+				L2Squared_Class(nearVector:{vector: [10,11,12], certainty:0.3}){
+					name 
+					_additional{distance certainty}
+				}
+			}
+		}
+		`)
+	})
+
+	t.Run("a high distance that includes all elements", func(t *testing.T) {
+		res := AssertGraphQL(t, nil, `
+		{
+			Get{
+				L2Squared_Class(nearVector:{vector: [10,11,12], distance: 365}){
+					name 
+					_additional{distance certainty}
+				}
+			}
+		}
+		`)
+		results := res.Get("Get", "L2Squared_Class").AsSlice()
+		expectedDistances := []float32{
+			0,   // the same vector as the query
+			50,  // distance to the second vector
+			365, // l2 squared distance to the root
+		}
+
+		compareDistances(t, expectedDistances, results)
+	})
+
+	t.Run("a distance that is too low for the last element", func(t *testing.T) {
+		res := AssertGraphQL(t, nil, `
+		{
+			Get{
+				L2Squared_Class(nearVector:{vector: [10,11,12], distance: 364}){
+					name 
+					_additional{distance certainty}
+				}
+			}
+		}
+		`)
+		results := res.Get("Get", "L2Squared_Class").AsSlice()
+		expectedDistances := []float32{
+			0,  // the same vector as the query
+			50, // distance to the second vector
+			// last eleme skipped, because 365 > 364
+		}
+
+		compareDistances(t, expectedDistances, results)
+	})
+
+	t.Run("a distance that is too low for the second element", func(t *testing.T) {
+		res := AssertGraphQL(t, nil, `
+		{
+			Get{
+				L2Squared_Class(nearVector:{vector: [10,11,12], distance: 49}){
+					name 
+					_additional{distance certainty}
+				}
+			}
+		}
+		`)
+		results := res.Get("Get", "L2Squared_Class").AsSlice()
+		expectedDistances := []float32{
+			0, // the same vector as the query
+			// second elem skipped, because 50 > 49
+			// last eleme skipped, because 365 > 364
+		}
+
+		compareDistances(t, expectedDistances, results)
+	})
+
+	t.Run("a really low distance that only matches one elem", func(t *testing.T) {
+		res := AssertGraphQL(t, nil, `
+		{
+			Get{
+				L2Squared_Class(nearVector:{vector: [10,11,12], distance: 0.001}){
+					name 
+					_additional{distance certainty}
+				}
+			}
+		}
+		`)
+		results := res.Get("Get", "L2Squared_Class").AsSlice()
+		expectedDistances := []float32{
+			0, // the same vector as the query
+			// second elem skipped, because 50 > 0.001
+			// last eleme skipped, because 365 > 0.001
+		}
+
+		compareDistances(t, expectedDistances, results)
+	})
+
+	t.Run("a distance of 0 only matches exact elements", func(t *testing.T) {
+		res := AssertGraphQL(t, nil, `
+		{
+			Get{
+				L2Squared_Class(nearVector:{vector: [10,11,12], distance: 0}){
+					name 
+					_additional{distance certainty}
+				}
+			}
+		}
+		`)
+		results := res.Get("Get", "L2Squared_Class").AsSlice()
+		expectedDistances := []float32{
+			0, // the same vector as the query
+			// second elem skipped, because 50 > 0
+			// last eleme skipped, because 365 > 0
+		}
+
+		compareDistances(t, expectedDistances, results)
+	})
 }
