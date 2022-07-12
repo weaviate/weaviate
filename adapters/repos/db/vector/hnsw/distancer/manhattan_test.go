5c6a7d8245fd5218e3252e42d8ce8b8011717400
Aakash Thatte <aathatte2002@gmail.com>
2022-07-12 16:22:40 +0530

    Add unit test for manhattan distance

---
+++ adapters/repos/db/vector/hnsw/distancer/manhattan_test.go
@@ -0,0 +1,68 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2022 SeMI Technologies B.V. All rights reserved.
+//
+//  CONTACT: hello@semi.technology
+//
+
+package distancer
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestManhattanDistancer(t *testing.T) {
+	t.Run("identical vectors", func(t *testing.T) {
+		vec1 := []float32{3, 4, 5}
+		vec2 := []float32{3, 4, 5}
+		expectedDistance := float32(0)
+
+		dist, ok, err := NewManhattanProvider().New(vec1).Distance(vec2)
+		require.Nil(t, err)
+		require.True(t, ok)
+		control, ok, err := NewManhattanProvider().SingleDist(vec1, vec2)
+		require.True(t, ok)
+		require.Nil(t, err)
+		assert.Equal(t, control, dist)
+		assert.Equal(t, expectedDistance, dist)
+	})
+
+	t.Run("same angle, different euclidean position", func(t *testing.T) {
+		vec1 := []float32{3, 4, 5}
+		vec2 := []float32{1.5, 2, 2.5}
+		// distance will be abs(3-1.5) + abs(4-2) + abs(5-2.5) = 1.5 + 2 + 2.5 = 6
+		expectedDistance := float32(6)
+
+		dist, ok, err := NewManhattanProvider().New(vec1).Distance(vec2)
+		require.Nil(t, err)
+		require.True(t, ok)
+		control, ok, err := NewManhattanProvider().SingleDist(vec1, vec2)
+		require.True(t, ok)
+		require.Nil(t, err)
+		assert.Equal(t, control, dist)
+		assert.Equal(t, expectedDistance, dist)
+	})
+
+	t.Run("different vectors", func(t *testing.T) {
+		vec1 := []float32{10, 11}
+		vec2 := []float32{13, 15}
+		// distance will be calculated as abs(10-13) + abs(11-15) = 3 + 4 = 7
+		expectedDistance := float32(7)
+
+		dist, ok, err := NewManhattanProvider().New(vec1).Distance(vec2)
+		require.Nil(t, err)
+		require.True(t, ok)
+		control, ok, err := NewManhattanProvider().SingleDist(vec1, vec2)
+		require.True(t, ok)
+		require.Nil(t, err)
+		assert.Equal(t, control, dist)
+		assert.Equal(t, expectedDistance, dist)
+	})
+}
