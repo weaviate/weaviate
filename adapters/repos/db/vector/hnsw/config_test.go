b5b0ab730f472ded4c2453bf24624c17ba6c00d0
Aakash Thatte <aathatte2002@gmail.com>
2022-07-12 14:43:15 +0530

    Fix conflicts in config_test.go

--- adapters/repos/db/vector/hnsw/config_test.go
+++ adapters/repos/db/vector/hnsw/config_test.go
@@ -174,7 +174,7 @@ func Test_UserConfig(t *testing.T) {
 				"dynamicEfMax":           json.Number("18"),
 				"dynamicEfFactor":        json.Number("19"),
 				"skip":                   true,
-				"distance":               "l2-squared",
+				"distance":               "manhattan",
 			},
 			expected: UserConfig{
 				CleanupIntervalSeconds: 11,
