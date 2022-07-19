fa299ecfdbeccb3b5b9f61d62286e8e0ff485262
Parker Duckworth <parkerduckworth@gmail.com>
2022-07-19 11:52:19 -0500

    add test which cancels blocking CycleManager.Stop with ctx

--- entities/cyclemanager/cyclemanager_test.go
+++ entities/cyclemanager/cyclemanager_test.go
@@ -2,6 +2,7 @@ package cyclemanager
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
@@ -51,6 +52,61 @@ func TestCycleManager(t *testing.T) {
 	})
 }
 
+func TestCycleManager_CancelContext(t *testing.T) {
+	sleeper := sleeper{
+		dreams: make(chan string, 1),
+	}
+
+	cycleInterval := 100 * time.Millisecond
+
+	t.Run("create new", func(t *testing.T) {
+		description := "test cycle"
+		sleeper.sleepCycle = New(sleeper.sleepDelayedWakeup, description)
+
+		assert.False(t, sleeper.sleepCycle.running)
+		assert.Equal(t, sleeper.sleepCycle.description, description)
+		assert.NotNil(t, sleeper.sleepCycle.cycleFunc)
+		assert.NotNil(t, sleeper.sleepCycle.Stopped)
+	})
+
+	t.Run("start", func(t *testing.T) {
+		sleeper.sleepCycle.Start(cycleInterval)
+		assert.True(t, sleeper.sleepCycle.running)
+		assert.Equal(t, "something wonderful...", <-sleeper.dreams)
+	})
+
+	t.Run("cancel early", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Nanosecond)
+		defer cancel()
+
+		awake := make(chan struct{})
+
+		go func() {
+			sleeper.sleepCycle.Stop(ctx)
+			awake <- struct{}{}
+		}()
+
+		select {
+		case <-ctx.Done():
+			done := make(chan struct{})
+
+			// if it takes longer than a second to restart
+			// the cycle, that means that `Stop` still has
+			// the lock obtained, and Start must wait.
+			//
+			// failure here will be obvious, because `Stop`
+			// here is configured to block for well beyond
+			// one second.
+			go failIfTimeout(done, time.Second)
+
+			sleeper.sleepCycle.Start(cycleInterval)
+			done <- struct{}{}
+		case <-awake:
+			t.Fatal("context should have been cancelled")
+		}
+	})
+}
+
 type sleeper struct {
 	sleepCycle *CycleManager
 	dreams     chan string
@@ -69,4 +125,31 @@ func (s *sleeper) sleep(interval time.Duration) {
 			}
 		}
 	}()
+}
+
+func (s *sleeper) sleepDelayedWakeup(interval time.Duration) {
+	go func() {
+		t := time.Tick(interval)
+		for {
+			select {
+			case <-s.sleepCycle.Stopped:
+				// simulate a blocking channel receive
+				fmt.Println("about to sleep for 24 hours")
+				time.Sleep(24 * time.Hour)
+				fmt.Println("done sleeping")
+				return
+			case <-t:
+				s.dreams <- "something wonderful..."
+			}
+		}
+	}()
+}
+
+func failIfTimeout(done chan struct{}, d time.Duration) {
+	select {
+	case <-done:
+		return
+	case <-time.After(d):
+		panic("test timed out")
+	}
 }
