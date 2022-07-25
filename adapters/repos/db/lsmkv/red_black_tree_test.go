a2eb0198917d1ae09c0a9460fa8126ac110a729a
Dirk Kulawiak <dirk@semi.technology>
2022-07-25 08:30:22 +0200

    Add randomized red-black tree test and extend validation of trees

--- adapters/repos/db/lsmkv/red_black_tree_test.go
+++ adapters/repos/db/lsmkv/red_black_tree_test.go
@@ -12,7 +12,9 @@
 package lsmkv
 
 import (
+	"fmt"
 	"math"
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -107,8 +109,8 @@ func TestRbTrees(t *testing.T) {
 				iByte := []byte{uint8(key)}
 				tree.insert(iByte, iByte, nil)
 				require.Empty(t, tree.root.parent)
-				require.True(t, ValidateRBTree(t, tree))
 			}
+			ValidateRBTree(t, tree)
 
 			flattenTree := tree.flattenInOrder()
 			require.Equal(t, len(tt.keys), len(flattenTree)) // no entries got lost
@@ -132,7 +134,7 @@ func TestRbTrees(t *testing.T) {
 }
 
 // add keys as a) normal keys b) tombstone keys and c) half tombstone, half normal.
-// The resulting trees must have the same order and colors
+// The resulting (reblalanced) trees must have the same order and colors
 var tombstoneTests = []struct {
 	name string
 	keys []uint
@@ -140,8 +142,9 @@ var tombstoneTests = []struct {
 	{"Rotate left around root", []uint{61, 83, 99}},
 	{"Rotate right around root", []uint{61, 30, 10}},
 	{"Multiple rotations along the tree and colour changes", []uint{166, 92, 33, 133, 227, 236, 71, 183, 18, 139, 245, 161}},
-	{"Ordered nodes increasing",[]uint{1, 2, 3, 4, 5, 6, 7, 8}},
-	{"Ordered nodes decreasing", []uint{8, 7, 6, 5, 4, 3, 2, 1}}}
+	{"Ordered nodes increasing", []uint{1, 2, 3, 4, 5, 6, 7, 8}},
+	{"Ordered nodes decreasing", []uint{8, 7, 6, 5, 4, 3, 2, 1}},
+}
 
 func TestTombstones(t *testing.T) {
 	for _, tt := range tombstoneTests {
@@ -159,9 +162,9 @@ func TestTombstones(t *testing.T) {
 					treeHalfHalf.setTombstone(iByte, nil)
 				}
 			}
-			require.True(t, ValidateRBTree(t, treeNormal))
-			require.True(t, ValidateRBTree(t, treeTombstone))
-			require.True(t, ValidateRBTree(t, treeHalfHalf))
+			ValidateRBTree(t, treeNormal)
+			ValidateRBTree(t, treeTombstone)
+			ValidateRBTree(t, treeHalfHalf)
 
 			treeNormalFlatten := treeNormal.flattenInOrder()
 			treeTombstoneFlatten := treeTombstone.flattenInOrder()
@@ -180,6 +183,37 @@ func TestTombstones(t *testing.T) {
 	}
 }
 
+type void struct{}
+
+var member void
+
+func TestRandomTrees(t *testing.T) {
+	setSeed(t)
+	tree := &binarySearchTree{}
+	amount := rand.Intn(100000)
+	keySize := rand.Intn(100)
+	uniqueKeys := make(map[string]void)
+	for i := 0; i < amount; i++ {
+		key := make([]byte, keySize)
+		rand.Read(key)
+		uniqueKeys[fmt.Sprint(key)] = member
+		if rand.Intn(5) == 1 { // add 20% of all entries as tombstone
+			tree.setTombstone(key, nil)
+		} else {
+			tree.insert(key, key, nil)
+		}
+	}
+
+	// all added keys are still part of the tree
+	treeFlattened := tree.flattenInOrder()
+	require.Equal(t, len(uniqueKeys), len(treeFlattened))
+	for _, entry := range treeFlattened {
+		_, ok := uniqueKeys[fmt.Sprint(entry.key)]
+		require.True(t, ok)
+	}
+	ValidateRBTree(t, tree)
+}
+
 func getIndexInSlice(reorderedKeys []uint, key []byte) int {
 	for i, v := range reorderedKeys {
 		if v == uint(key[0]) {
@@ -189,20 +223,30 @@ func getIndexInSlice(reorderedKeys []uint, key []byte) int {
 	return -1
 }
 
-// Checks if a tree could be an RB tree
-func ValidateRBTree(t *testing.T, tree *binarySearchTree) bool {
-	if tree.root.colourIsred {
-		return false
-	}
+// Checks if a tree is a RB tree
+//
+// There are several properties that valid RB trees follow:
+// 1) The root node is always black
+// 2) The max depth of a tree is 2* Log2(N+1), where N is the number of nodes
+// 3) Every path from root to leave has the same number of _black_ nodes
+// 4) Red nodes only have black (or nil) children
+//
+// In addition this also validates some general tree properties:
+//  - root has no parent
+//  - if node A is a child of B, B must be the parent of A)
+func ValidateRBTree(t *testing.T, tree *binarySearchTree) {
+	require.False(t, tree.root.colourIsred)
+	require.True(t, tree.root.parent == nil)
 
-	treeDepth, nodeCount := MaxTreeDepthAndNodeCount(t, tree.root)
+	treeDepth, nodeCount, _ := WalkTree(t, tree.root)
 	maxDepth := 2 * math.Log2(float64(nodeCount)+1)
-	return treeDepth <= int(maxDepth)
+	require.True(t, treeDepth <= int(maxDepth))
 }
 
-func MaxTreeDepthAndNodeCount(t *testing.T, node *binarySearchNode) (int, int) {
+// Walks through the tree and counts the depth, number of nodes and number of black nodes
+func WalkTree(t *testing.T, node *binarySearchNode) (int, int, int) {
 	if node == nil {
-		return 0, 0
+		return 0, 0, 0
 	}
 
 	// validate parent/child connections
@@ -213,17 +257,29 @@ func MaxTreeDepthAndNodeCount(t *testing.T, node *binarySearchNode) (int, int) {
 		require.Equal(t, node.left.parent, node)
 	}
 
-	if node.right == nil && node.left == nil {
-		return 1, 1
+	// red nodes need black (or nil) children
+	if node.colourIsred {
+		require.True(t, node.left == nil || !node.left.colourIsred)
+		require.True(t, node.right == nil || !node.right.colourIsred)
 	}
 
-	depthRight, nodeCountRight := MaxTreeDepthAndNodeCount(t, node.right)
-	depthLeft, nodeCountLeft := MaxTreeDepthAndNodeCount(t, node.left)
+	blackNode := int(1)
+	if node.colourIsred {
+		blackNode = 0
+	}
+
+	if node.right == nil && node.left == nil {
+		return 1, 1, blackNode
+	}
+
+	depthRight, nodeCountRight, blackNodesDepthRight := WalkTree(t, node.right)
+	depthLeft, nodeCountLeft, blackNodesDepthLeft := WalkTree(t, node.left)
+	require.Equal(t, blackNodesDepthRight, blackNodesDepthLeft)
 
 	nodeCount := nodeCountLeft + nodeCountRight + 1
 	if depthRight > depthLeft {
-		return depthRight + 1, nodeCount
+		return depthRight + 1, nodeCount, blackNodesDepthRight + blackNode
 	} else {
-		return depthLeft + 1, nodeCount
+		return depthLeft + 1, nodeCount, blackNodesDepthRight + blackNode
 	}
 }
