cba6ec65f4ebcf308624c4a08e711d1ab7c40f46
Andrzej Liszka <andrzej.liszka@gmail.com>
2022-09-06 09:46:14 +0200

    storage modules additional tests (cleanup)

--- test/modules/storage-aws-s3/backup_create_test.go
+++ test/modules/storage-aws-s3/backup_create_test.go
@@ -30,9 +30,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// TODO: test remaining methods
-
-func Test_S3Storage_SnapshotCreate(t *testing.T) {
+func Test_S3Storage_BackupCreate(t *testing.T) {
 	ctx := context.Background()
 	compose, err := docker.New().WithMinIO().Start(ctx)
 	if err != nil {
@@ -41,8 +39,8 @@ func Test_S3Storage_SnapshotCreate(t *testing.T) {
 
 	require.Nil(t, os.Setenv(envMinioEndpoint, compose.GetMinIO().URI()))
 
-	t.Run("store snapshot", moduleLevelStoreSnapshot)
-	t.Run("get meta status", moduleLevelGetMetaStatus)
+	t.Run("store backup meta", moduleLevelStoreBackupMeta)
+	t.Run("copy objects", moduleLevelCopyObjects)
 	t.Run("copy files", moduleLevelCopyFiles)
 
 	if err := compose.Terminate(ctx); err != nil {
@@ -50,20 +48,17 @@ func Test_S3Storage_SnapshotCreate(t *testing.T) {
 	}
 }
 
-func moduleLevelStoreSnapshot(t *testing.T) {
+func moduleLevelStoreBackupMeta(t *testing.T) {
+	testDir := t.TempDir()
 	testCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
-	testdataMainDir := "./testData"
-	// testDir := moduleshelper.MakeTestDir(t, testdataMainDir)
-	defer moduleshelper.RemoveDir(t, testdataMainDir)
-
-	className := "SnapshotClass"
+	className := "BackupClass"
 	backupID := "backup_id"
 	bucketName := "bucket"
 	region := "eu-west-1"
 	endpoint := os.Getenv(envMinioEndpoint)
-	metadataFilename := "snapshot.json"
+	metadataFilename := "backup.json"
 
 	t.Run("setup env", func(t *testing.T) {
 		require.Nil(t, os.Setenv(envAwsRegion, region))
@@ -74,45 +69,49 @@ func moduleLevelStoreSnapshot(t *testing.T) {
 		createBucket(testCtx, t, endpoint, region, bucketName)
 	})
 
-	t.Run("store snapshot in s3", func(t *testing.T) {
-		logger, _ := test.NewNullLogger()
-
+	t.Run("store backup meta in s3", func(t *testing.T) {
 		s3Config := s3.NewConfig(endpoint, bucketName, "", false)
-		path, _ := os.Getwd()
-		s3, err := s3.New(s3Config, logger, path)
+		logger, _ := test.NewNullLogger()
+		s3, err := s3.New(s3Config, logger, testDir)
 		require.Nil(t, err)
 
-		meta, err := s3.GetObject(testCtx, backupID, metadataFilename)
-		assert.Nil(t, meta)
-		assert.NotNil(t, err)
-		assert.IsType(t, backup.ErrNotFound{}, err)
+		t.Run("access permissions", func(t *testing.T) {
+			err = s3.Initialize(testCtx, backupID)
+			assert.Nil(t, err)
+		})
 
-		err = s3.Initialize(testCtx, backupID)
-		assert.Nil(t, err)
+		t.Run("backup meta does not exist yet", func(t *testing.T) {
+			meta, err := s3.GetObject(testCtx, backupID, metadataFilename)
+			assert.Nil(t, meta)
+			assert.NotNil(t, err)
+			assert.IsType(t, backup.ErrNotFound{}, err)
+		})
 
-		desc := &backup.BackupDescriptor{
-			StartedAt:   time.Now(),
-			CompletedAt: time.Time{},
-			ID:          backupID,
-			Classes: []backup.ClassDescriptor{
-				{
-					Name: className,
+		t.Run("put backup meta on storage", func(t *testing.T) {
+			desc := &backup.BackupDescriptor{
+				StartedAt:   time.Now(),
+				CompletedAt: time.Time{},
+				ID:          backupID,
+				Classes: []backup.ClassDescriptor{
+					{
+						Name: className,
+					},
 				},
-			},
-			Status: string(backup.Started),
-		}
+				Status: string(backup.Started),
+			}
 
-		b, err := json.Marshal(desc)
-		require.Nil(t, err)
+			b, err := json.Marshal(desc)
+			require.Nil(t, err)
 
-		err = s3.PutObject(testCtx, backupID, metadataFilename, b)
-		require.Nil(t, err)
+			err = s3.PutObject(testCtx, backupID, metadataFilename, b)
+			require.Nil(t, err)
 
-		dest := s3.DestinationPath(backupID)
-		expected := fmt.Sprintf("s3://%s/%s/snapshot.json", bucketName, backupID)
-		assert.Equal(t, expected, dest)
+			dest := s3.DestinationPath(backupID)
+			expected := fmt.Sprintf("s3://%s/%s/snapshot.json", bucketName, backupID)
+			assert.Equal(t, expected, dest)
+		})
 
-		t.Run("assert snapshot meta contents", func(t *testing.T) {
+		t.Run("assert backup meta contents", func(t *testing.T) {
 			obj, err := s3.GetObject(testCtx, backupID, metadataFilename)
 			require.Nil(t, err)
 
@@ -130,18 +129,13 @@ func moduleLevelStoreSnapshot(t *testing.T) {
 	})
 }
 
-func moduleLevelGetMetaStatus(t *testing.T) {
+func moduleLevelCopyObjects(t *testing.T) {
+	testDir := t.TempDir()
 	testCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
-	testdataMainDir := "./testData"
-	testDir := moduleshelper.MakeTestDir(t, testdataMainDir)
-	defer moduleshelper.RemoveDir(t, testdataMainDir)
-
-	moduleshelper.CreateTestFiles(t, testDir)
-
-	key := "moduleLevelGetMetaStatus"
-	snapshotID := "snapshot_id"
+	key := "moduleLevelCopyObjects"
+	backupID := "backup_id"
 	bucketName := "bucket"
 	region := "eu-west-1"
 	endpoint := os.Getenv(envMinioEndpoint)
@@ -155,43 +149,32 @@ func moduleLevelGetMetaStatus(t *testing.T) {
 		createBucket(testCtx, t, endpoint, region, bucketName)
 	})
 
-	s3Config := s3.NewConfig(endpoint, bucketName, "", false)
-	logger, _ := test.NewNullLogger()
-
-	t.Run("set snapshot status", func(t *testing.T) {
+	t.Run("copy objects", func(t *testing.T) {
+		s3Config := s3.NewConfig(endpoint, bucketName, "", false)
+		logger, _ := test.NewNullLogger()
 		s3, err := s3.New(s3Config, logger, testDir)
 		require.Nil(t, err)
 
-		err = s3.PutObject(testCtx, snapshotID, key, []byte("hello"))
-		assert.Nil(t, err)
-	})
+		t.Run("put object to backet", func(t *testing.T) {
+			err = s3.PutObject(testCtx, backupID, key, []byte("hello"))
+			assert.Nil(t, err)
+		})
 
-	t.Run("get snapshot status", func(t *testing.T) {
-		s3, err := s3.New(s3Config, logger, testDir)
-		require.Nil(t, err)
-
-		meta, err := s3.GetObject(testCtx, snapshotID, key)
-		assert.Nil(t, err)
-		assert.Equal(t, []byte("hello"), meta)
+		t.Run("get object from backet", func(t *testing.T) {
+			meta, err := s3.GetObject(testCtx, backupID, key)
+			assert.Nil(t, err)
+			assert.Equal(t, []byte("hello"), meta)
+		})
 	})
 }
 
 func moduleLevelCopyFiles(t *testing.T) {
+	testDir := t.TempDir()
 	testCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
-	testdataMainDir := "./testData"
-	testDir := moduleshelper.MakeTestDir(t, testdataMainDir)
-	defer moduleshelper.RemoveDir(t, testdataMainDir)
-
-	fpaths := moduleshelper.CreateTestFiles(t, testDir)
-	fpath := fpaths[0]
-	expectedContents, err := os.ReadFile(fpath)
-	require.Nil(t, err)
-	require.NotNil(t, expectedContents)
-
 	key := "moduleLevelCopyFiles"
-	snapshotID := "snapshot_id"
+	backupID := "backup_id"
 	bucketName := "bucket"
 	region := "eu-west-1"
 	endpoint := os.Getenv(envMinioEndpoint)
@@ -205,33 +188,41 @@ func moduleLevelCopyFiles(t *testing.T) {
 		createBucket(testCtx, t, endpoint, region, bucketName)
 	})
 
-	s3Config := s3.NewConfig(endpoint, bucketName, "", false)
-	logger, _ := test.NewNullLogger()
-	s3, err := s3.New(s3Config, logger, testDir)
-	require.Nil(t, err)
+	t.Run("copy files", func(t *testing.T) {
+		fpaths := moduleshelper.CreateTestFiles(t, testDir)
+		fpath := fpaths[0]
+		expectedContents, err := os.ReadFile(fpath)
+		require.Nil(t, err)
+		require.NotNil(t, expectedContents)
 
-	t.Run("verify source data path", func(t *testing.T) {
-		assert.Equal(t, testDir, s3.SourceDataPath())
-	})
-
-	t.Run("copy file to storage", func(t *testing.T) {
-		srcPath, _ := filepath.Rel(testDir, fpath)
-		err = s3.PutFile(testCtx, snapshotID, key, srcPath)
+		s3Config := s3.NewConfig(endpoint, bucketName, "", false)
+		logger, _ := test.NewNullLogger()
+		s3, err := s3.New(s3Config, logger, testDir)
 		require.Nil(t, err)
 
-		contents, err := s3.GetObject(testCtx, snapshotID, key)
-		require.Nil(t, err)
-		assert.Equal(t, expectedContents, contents)
-	})
+		t.Run("verify source data path", func(t *testing.T) {
+			assert.Equal(t, testDir, s3.SourceDataPath())
+		})
 
-	t.Run("fetch file from storage", func(t *testing.T) {
-		destPath := testDir + "/file_0.copy.db"
+		t.Run("copy file to storage", func(t *testing.T) {
+			srcPath, _ := filepath.Rel(testDir, fpath)
+			err = s3.PutFile(testCtx, backupID, key, srcPath)
+			require.Nil(t, err)
 
-		err := s3.WriteToFile(testCtx, snapshotID, key, destPath)
-		require.Nil(t, err)
+			contents, err := s3.GetObject(testCtx, backupID, key)
+			require.Nil(t, err)
+			assert.Equal(t, expectedContents, contents)
+		})
 
-		contents, err := os.ReadFile(destPath)
-		require.Nil(t, err)
-		assert.Equal(t, expectedContents, contents)
+		t.Run("fetch file from storage", func(t *testing.T) {
+			destPath := testDir + "/file_0.copy.db"
+
+			err := s3.WriteToFile(testCtx, backupID, key, destPath)
+			require.Nil(t, err)
+
+			contents, err := os.ReadFile(destPath)
+			require.Nil(t, err)
+			assert.Equal(t, expectedContents, contents)
+		})
 	})
 }
