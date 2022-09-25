9b62915cdd5799e46371e36fdb16d961daaff1e6
Dasith U Edirisinghe <udasith@gmail.com>
2022-09-25 16:12:32 +0530

    Initialize the cohere embedding

---
+++ modules/text2vec-cohere/config.go
@@ -0,0 +1,46 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2022 SeMI Technologies B.V. All rights reserved.
+//
+//  CONTACT: hello@semi.technology
+//
+
+package modcohere
+
+import (
+	"context"
+
+	"github.com/semi-technologies/weaviate/entities/models"
+	"github.com/semi-technologies/weaviate/entities/modulecapabilities"
+	"github.com/semi-technologies/weaviate/entities/moduletools"
+	"github.com/semi-technologies/weaviate/entities/schema"
+	"github.com/semi-technologies/weaviate/modules/text2vec-huggingface/vectorizer"
+)
+
+func (m *CohereModule) ClassConfigDefaults() map[string]interface{} {
+	return map[string]interface{}{
+		"vectorizeClassName": vectorizer.DefaultVectorizeClassName,
+	}
+}
+
+func (m *CohereModule) PropertyConfigDefaults(
+	dt *schema.DataType,
+) map[string]interface{} {
+	return map[string]interface{}{
+		"skip":                  !vectorizer.DefaultPropertyIndexed,
+		"vectorizePropertyName": vectorizer.DefaultVectorizePropertyName,
+	}
+}
+
+func (m *CohereModule) ValidateClass(ctx context.Context,
+	class *models.Class, cfg moduletools.ClassConfig,
+) error {
+	settings := vectorizer.NewClassSettings(cfg)
+	return settings.Validate(class)
+}
+
+var _ = modulecapabilities.ClassConfigurator(New())
