ed3f541284447ac22a49c3bfeaee32e23433dee4
Dasith U Edirisinghe <udasith@gmail.com>
2022-09-25 16:12:32 +0530

    Initialize the cohere embedding

---
+++ modules/text2vec-cohere/nearText.go
@@ -0,0 +1,36 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2022 SeMI Technologies B.V. All rights reserved.
+//
+//  CONTACT: hello@semi.technology
+//
+
+package modcohere
+
+import (
+	"github.com/semi-technologies/weaviate/entities/modulecapabilities"
+	"github.com/semi-technologies/weaviate/modules/text2vec-huggingface/neartext"
+)
+
+func (m *CohereModule) initNearText() error {
+	m.searcher = neartext.NewSearcher(m.vectorizer)
+	m.graphqlProvider = neartext.New(m.nearTextTransformer)
+	return nil
+}
+
+func (m *CohereModule) Arguments() map[string]modulecapabilities.GraphQLArgument {
+	return m.graphqlProvider.Arguments()
+}
+
+func (m *CohereModule) VectorSearches() map[string]modulecapabilities.VectorForParams {
+	return m.searcher.VectorSearches()
+}
+
+var (
+	_ = modulecapabilities.GraphQLArguments(New())
+	_ = modulecapabilities.Searcher(New())
+)
