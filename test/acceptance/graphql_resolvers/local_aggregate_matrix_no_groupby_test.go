0e41c73abe80d3dc865c9db8e39ed8789da0dba1
Andrzej Liszka <andrzej.liszka@gmail.com>
2022-09-27 15:06:15 +0200

    WVT-328: testmatrix for aggregation - array data types

--- test/acceptance/graphql_resolvers/local_aggregate_matrix_no_groupby_test.go
+++ test/acceptance/graphql_resolvers/local_aggregate_matrix_no_groupby_test.go
@@ -21,7 +21,7 @@ import (
 
 func aggregateArrayClassWithoutGroupByTest(t *testing.T) {
 	t.Run("aggregate ArrayClass without group by", func(t *testing.T) {
-		h := &gqlAggregateHelper{}
+		h := &gqlAggregateResponseHelper{}
 		testCasesGen := &aggregateArrayClassTestCases{}
 
 		allResults := map[string]interface{}{
@@ -99,7 +99,7 @@ func aggregateArrayClassWithoutGroupByTest(t *testing.T) {
 
 func aggregateNoPropsClassWithoutGroupByTest(t *testing.T) {
 	t.Run("aggregate NoPropsClass without group by", func(t *testing.T) {
-		h := &gqlAggregateHelper{}
+		h := &gqlAggregateResponseHelper{}
 		testCasesGen := &aggregateNoPropsClassTestCases{}
 
 		allResults := map[string]interface{}{
@@ -140,10 +140,100 @@ func aggregateNoPropsClassWithoutGroupByTest(t *testing.T) {
 	})
 }
 
+func aggregateCityClassWithoutGroupByTest(t *testing.T) {
+	t.Run("aggregate City without group by", func(t *testing.T) {
+		h := &gqlAggregateResponseHelper{}
+		testCasesGen := &aggregateCityTestCases{}
+
+		allResults := map[string]interface{}{
+			"meta":       h.meta(6),
+			"cityArea":   h.number(4, 891.96, 217.22, 217.22, 2320.48, 605.6500000000001, 580.12),
+			"cityRights": h.date(4),
+			"history":    h.text(4, []string{historyAmsterdam, historyRotterdam, historyBerlin, historyDusseldorf}, []int64{1, 1, 1, 1}),
+			"isCapital":  h.boolean(5, 3, 2, 0.6, 0.4),
+			"museums":    h.texts(9, []string{"German Historical Museum", "Museum Boijmans Van Beuningen", "Onomato", "Rijksmuseum", "Schiffahrt Museum"}, []int64{1, 1, 1, 1, 1}),
+			"name":       h.string(5, []string{"Amsterdam", "Berlin", "Dusseldorf", "Missing Island", "Rotterdam"}, []int64{1, 1, 1, 1, 1}),
+			"population": h.int(5, 3470000, 0, 600000, 6470000, 600000, 1294000),
+			"timezones":  h.strings(8, []string{"CEST", "CET"}, []int64{4, 4}),
+			"inCountry":  h.pointingTo("Country"),
+		}
+		resultsWithData := map[string]interface{}{
+			"meta":       h.meta(2),
+			"cityArea":   h.number(2, 891.96, 891.95, 891.95, 1783.91, 891.955, 891.955),
+			"cityRights": h.date(2),
+			"history":    h.text(2, []string{historyAmsterdam, historyBerlin}, []int64{1, 1}),
+			"isCapital":  h.boolean(2, 0, 2, 0, 1),
+			"museums":    h.texts(3, []string{"German Historical Museum", "Rijksmuseum", "Stedelijk Museum"}, []int64{1, 1, 1}),
+			"name":       h.string(2, []string{"Amsterdam", "Berlin"}, []int64{1, 1}),
+			"population": h.int(2, 3470000, 1800000, 1800000, 5270000, 2635000, 2635000),
+			"timezones":  h.strings(4, []string{"CEST", "CET"}, []int64{2, 2}),
+			"inCountry":  h.pointingTo("Country"),
+		}
+		resultsWithoutData := map[string]interface{}{
+			"meta":       h.meta(1),
+			"cityArea":   h.number0(),
+			"cityRights": h.date0(),
+			"history":    h.text0(),
+			"isCapital":  h.boolean0(),
+			"museums":    h.texts0(),
+			"name":       h.string0(),
+			"population": h.int0(),
+			"timezones":  h.strings0(),
+			"inCountry":  h.pointingTo("Country"),
+		}
+		noResults := map[string]interface{}{
+			"meta":       h.meta(0),
+			"cityArea":   h.number0(),
+			"cityRights": h.date0(),
+			"history":    h.text0(),
+			"isCapital":  h.boolean0(),
+			"museums":    h.texts0(),
+			"name":       h.string0(),
+			"population": h.int0(),
+			"timezones":  h.strings0(),
+			"inCountry":  h.pointingTo("Country"),
+		}
+
+		testCases := []aggregateTestCase{
+			testCasesGen.WithoutFilters(allResults),
+
+			testCasesGen.WithWhereFilter_AllResults(allResults),
+			testCasesGen.WithWhereFilter_ResultsWithData(resultsWithData),
+			testCasesGen.WithWhereFilter_ResultsWithoutData(resultsWithoutData),
+			testCasesGen.WithWhereFilter_NoResults(noResults),
+
+			testCasesGen.WithNearObjectFilter_AllResults(allResults),
+			testCasesGen.WithNearObjectFilter_ResultsWithData(resultsWithData),
+			testCasesGen.WithNearObjectFilter_ResultsWithoutData(resultsWithoutData),
+
+			testCasesGen.WithWhereAndNearObjectFilters_AllResults(allResults),
+			testCasesGen.WithWhereAndNearObjectFilters_ResultsWithData(resultsWithData),
+			testCasesGen.WithWhereAndNearObjectFilters_ResultsWithoutData(resultsWithoutData),
+			testCasesGen.WithWhereAndNearObjectFilters_NoResults(noResults),
+		}
+
+		for _, tc := range testCases {
+			query := aggregateCityQuery(tc.filters, "")
+
+			t.Run(tc.name, func(t *testing.T) {
+				result := graphqlhelper.AssertGraphQL(t, helper.RootAuth, query)
+				extracted := extractCityNoGroupByResult(result)
+				expected := tc.expected.(map[string]interface{})
+
+				assert.Equal(t, expected, extracted)
+			})
+		}
+	})
+}
+
 func extractArrayClassNoGroupByResult(result *graphqlhelper.GraphQLResult) map[string]interface{} {
 	return extractAggregateResult(result, arrayClassName)[0].(map[string]interface{})
 }
 
 func extractNoPropsClassNoGroupByResult(result *graphqlhelper.GraphQLResult) map[string]interface{} {
 	return extractAggregateResult(result, noPropsClassName)[0].(map[string]interface{})
+}
+
+func extractCityNoGroupByResult(result *graphqlhelper.GraphQLResult) map[string]interface{} {
+	return extractAggregateResult(result, cityClassName)[0].(map[string]interface{})
 }
