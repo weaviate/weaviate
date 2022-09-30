f3df389adda21357e71b300b8b077cf78a78e645
Travis CI User <travis@example.org>
2022-09-30 06:06:18 +0000

    bleep bloop => auto updated Weaviate

--- usecases/backup/shard.go
+++ usecases/backup/shard.go
@@ -1,14 +1,13 @@
-//	_       _
-//
+//                           _       _
 // __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
 //
-//	\ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
-//	 \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
-//	  \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//  Copyright © 2016 - 2022 SeMI Technologies B.V. All rights reserved.
 //
-//	 Copyright © 2016 - 2022 SeMI Technologies B.V. All rights reserved.
+//  CONTACT: hello@semi.technology
 //
-//	 CONTACT: hello@semi.technology
 
 package backup
 
