91e7f716cbab6bceb49471e688e1f7566227a9cf
Jeremy Price <donomii@gmail.com>
2022-10-06 11:41:51 +0200

    merge

--- test/run.sh
+++ test/run.sh
@@ -97,7 +97,7 @@ function run_unit_tests() {
     echo "Skipping unit test"
     return
   fi
-  go test -race -coverprofile=coverage-unit.txt -covermode=atomic -count 1 $(go list ./... | grep -v 'test/acceptance' | grep -v 'test/modules') | grep -v '\[no test files\]'
+  go test -v -race -coverprofile=coverage-unit.txt -covermode=atomic -count 1 $(go list ./... | grep -v 'test/acceptance' | grep -v 'test/modules') | grep -v '\[no test files\]'
 }
 
 function run_integration_tests() {
