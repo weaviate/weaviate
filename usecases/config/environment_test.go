96f6ba2151e1664a9a06e61b53ea5016a59425cc
Parker Duckworth <parkerduckworth@gmail.com>
2022-10-13 13:58:11 -0500

    move cluster cfg logic to func, add cfg parse tests

--- usecases/config/environment_test.go
+++ usecases/config/environment_test.go
@@ -12,9 +12,12 @@
 package config
 
 import (
+	"errors"
 	"os"
 	"testing"
 
+	"github.com/semi-technologies/weaviate/usecases/cluster"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -78,6 +81,77 @@ func TestEnvironmentSetFlushAfter(t *testing.T) {
 				require.NotNil(t, err)
 			} else {
 				require.Equal(t, tt.expected, conf.Persistence.FlushIdleMemtablesAfter)
+			}
+		})
+	}
+}
+
+func TestEnvironmentParseClusterConfig(t *testing.T) {
+	tests := []struct {
+		name           string
+		envVars        map[string]string
+		expectedResult cluster.Config
+		expectedErr    error
+	}{
+		{
+			name: "valid cluster config - both ports provided",
+			envVars: map[string]string{
+				"CLUSTER_GOSSIP_BIND_PORT": "7100",
+				"CLUSTER_DATA_BIND_PORT":   "7101",
+			},
+			expectedResult: cluster.Config{
+				GossipBindPort: 7100,
+				DataBindPort:   7101,
+			},
+		},
+		{
+			name: "valid cluster config - no ports provided",
+			expectedResult: cluster.Config{
+				GossipBindPort: DefaultGossipBindPort,
+				DataBindPort:   DefaultGossipBindPort + 1,
+			},
+		},
+		{
+			name: "valid cluster config - only gossip bind port provided",
+			envVars: map[string]string{
+				"CLUSTER_GOSSIP_BIND_PORT": "7777",
+			},
+			expectedResult: cluster.Config{
+				GossipBindPort: 7777,
+				DataBindPort:   7778,
+			},
+		},
+		{
+			name: "invalid cluster config - both ports provided",
+			envVars: map[string]string{
+				"CLUSTER_GOSSIP_BIND_PORT": "7100",
+				"CLUSTER_DATA_BIND_PORT":   "7111",
+			},
+			expectedErr: errors.New("CLUSTER_DATA_BIND_PORT must be one port " +
+				"number greater than CLUSTER_GOSSIP_BIND_PORT"),
+		},
+		{
+			name: "invalid config - only data bind port provided",
+			envVars: map[string]string{
+				"CLUSTER_DATA_BIND_PORT": "7101",
+			},
+			expectedErr: errors.New("CLUSTER_DATA_BIND_PORT must be one port " +
+				"number greater than CLUSTER_GOSSIP_BIND_PORT"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			for k, v := range test.envVars {
+				t.Setenv(k, v)
+			}
+			cfg, err := parseClusterConfig()
+			if test.expectedErr != nil {
+				assert.EqualError(t, err, test.expectedErr.Error(),
+					"expected err: %v, got: %v", test.expectedErr, err)
+			} else {
+				assert.Nil(t, err, "expected nil, got: %v", err)
+				assert.EqualValues(t, test.expectedResult, cfg)
 			}
 		})
 	}
