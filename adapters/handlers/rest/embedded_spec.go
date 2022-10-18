01b6cd4a542ad535014d4d22596ca6f26eed11b8
Marcin Antas <antas.marcin@gmail.com>
2022-10-18 13:04:00 +0200

    prepare release v1.15.5

--- adapters/handlers/rest/embedded_spec.go
+++ adapters/handlers/rest/embedded_spec.go
@@ -48,7 +48,7 @@ func init() {
       "url": "https://github.com/semi-technologies",
       "email": "hello@semi.technology"
     },
-    "version": "1.15.4"
+    "version": "1.15.5"
   },
   "basePath": "/v1",
   "paths": {
@@ -4042,7 +4042,7 @@ func init() {
       "url": "https://github.com/semi-technologies",
       "email": "hello@semi.technology"
     },
-    "version": "1.15.4"
+    "version": "1.15.5"
   },
   "basePath": "/v1",
   "paths": {
