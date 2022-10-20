ec37412dcdbebe59e5fa01606b0b888dc2787676
Dasith U Edirisinghe <udasith@gmail.com>
2022-10-20 21:21:36 +0530

    vectorizer dir updated with tests

---
+++ modules/text2vec-cohere/vectorizer/class_settings.go
@@ -0,0 +1,192 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2022 SeMI Technologies B.V. All rights reserved.
+//
+//  CONTACT: hello@semi.technology
+//
+
+package vectorizer
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/pkg/errors"
+
+	"github.com/semi-technologies/weaviate/entities/models"
+	"github.com/semi-technologies/weaviate/entities/moduletools"
+	"github.com/semi-technologies/weaviate/entities/schema"
+)
+
+const (
+	DefaultCohereModel           = "large"
+	DefaultTruncate              = "NONE"
+	DefaultVectorizeClassName    = true
+	DefaultPropertyIndexed       = true
+	DefaultVectorizePropertyName = false
+)
+
+var availableCohereModels = []string{"small", "medium", "large"}
+var availableTruncates = []string{"NONE", "LEFT", "RIGHT"}
+
+type classSettings struct {
+	cfg moduletools.ClassConfig
+}
+
+func NewClassSettings(cfg moduletools.ClassConfig) *classSettings {
+	return &classSettings{cfg: cfg}
+}
+
+func (ic *classSettings) PropertyIndexed(propName string) bool {
+	if ic.cfg == nil {
+		// we would receive a nil-config on cross-class requests, such as Explore{}
+		return DefaultPropertyIndexed
+	}
+
+	vcn, ok := ic.cfg.Property(propName)["skip"]
+	if !ok {
+		return DefaultPropertyIndexed
+	}
+
+	asBool, ok := vcn.(bool)
+	if !ok {
+		return DefaultPropertyIndexed
+	}
+
+	return !asBool
+}
+
+func (ic *classSettings) VectorizePropertyName(propName string) bool {
+	if ic.cfg == nil {
+		// we would receive a nil-config on cross-class requests, such as Explore{}
+		return DefaultVectorizePropertyName
+	}
+	vcn, ok := ic.cfg.Property(propName)["vectorizePropertyName"]
+	if !ok {
+		return DefaultVectorizePropertyName
+	}
+
+	asBool, ok := vcn.(bool)
+	if !ok {
+		return DefaultVectorizePropertyName
+	}
+
+	return asBool
+}
+
+func (ic *classSettings) Model() string {
+	return ic.getProperty("model", DefaultCohereModel)
+}
+
+func (ic *classSettings) Truncate() string {
+	return ic.getProperty("truncate", DefaultTruncate)
+}
+
+func (ic *classSettings) VectorizeClassName() bool {
+	if ic.cfg == nil {
+		// we would receive a nil-config on cross-class requests, such as Explore{}
+		return DefaultVectorizeClassName
+	}
+
+	vcn, ok := ic.cfg.Class()["vectorizeClassName"]
+	if !ok {
+		return DefaultVectorizeClassName
+	}
+
+	asBool, ok := vcn.(bool)
+	if !ok {
+		return DefaultVectorizeClassName
+	}
+
+	return asBool
+}
+
+func (ic *classSettings) Validate(class *models.Class) error {
+	if ic.cfg == nil {
+		// we would receive a nil-config on cross-class requests, such as Explore{}
+		return errors.New("empty config")
+	}
+
+	model := ic.Model()
+	if !ic.validateCohereSetting(model, availableCohereModels) {
+		return errors.Errorf("wrong Cohere model name, available model names are: %v", availableCohereModels)
+	}
+	truncate := ic.Truncate()
+	if !ic.validateCohereSetting(truncate, availableTruncates) {
+		return errors.Errorf("wrong truncate type, available types are: %v", availableTruncates)
+	}
+
+	err := ic.validateIndexState(class, ic)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ic *classSettings) validateCohereSetting(value string, availableValues []string) bool {
+	for i := range availableValues {
+		if value == availableValues[i] {
+			return true
+		}
+	}
+	return false
+}
+
+func (ic *classSettings) getProperty(name, defaultValue string) string {
+	if ic.cfg == nil {
+		// we would receive a nil-config on cross-class requests, such as Explore{}
+		return defaultValue
+	}
+
+	model, ok := ic.cfg.Class()[name]
+	if ok {
+		asString, ok := model.(string)
+		if ok {
+			return strings.ToLower(asString)
+		}
+	}
+
+	return defaultValue
+}
+
+func (cv *classSettings) validateIndexState(class *models.Class, settings ClassSettings) error {
+	if settings.VectorizeClassName() {
+		// if the user chooses to vectorize the classname, vector-building will
+		// always be possible, no need to investigate further
+
+		return nil
+	}
+
+	// search if there is at least one indexed, string/text prop. If found pass
+	// validation
+	for _, prop := range class.Properties {
+		if len(prop.DataType) < 1 {
+			return errors.Errorf("property %s must have at least one datatype: "+
+				"got %v", prop.Name, prop.DataType)
+		}
+
+		if prop.DataType[0] != string(schema.DataTypeString) &&
+			prop.DataType[0] != string(schema.DataTypeText) {
+			// we can only vectorize text-like props
+			continue
+		}
+
+		if settings.PropertyIndexed(prop.Name) {
+			// found at least one, this is a valid schema
+			return nil
+		}
+	}
+
+	return fmt.Errorf("invalid properties: didn't find a single property which is " +
+		"of type string or text and is not excluded from indexing. In addition the " +
+		"class name is excluded from vectorization as well, meaning that it cannot be " +
+		"used to determine the vector position. To fix this, set 'vectorizeClassName' " +
+		"to true if the class name is contextionary-valid. Alternatively add at least " +
+		"contextionary-valid text/string property which is not excluded from " +
+		"indexing")
+}
