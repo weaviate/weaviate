f7d48033fc2788f1472f40319bb40df2447a7b01
Jeremy Price <donomii@gmail.com>
2022-10-21 11:31:12 +0200

    Add tests

---
+++ adapters/repos/db/bm25f_test.go
@@ -0,0 +1,95 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/go-openapi/strfmt"
+	"github.com/google/uuid"
+	"github.com/semi-technologies/weaviate/entities/additional"
+	"github.com/semi-technologies/weaviate/entities/models"
+	"github.com/semi-technologies/weaviate/entities/schema"
+	"github.com/semi-technologies/weaviate/entities/searchparams"
+	enthnsw "github.com/semi-technologies/weaviate/entities/vectorindex/hnsw"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBM25FJourney(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	dirName := t.TempDir()
+
+	logger := logrus.New()
+	schemaGetter := &fakeSchemaGetter{shardState: singleShardState()}
+	repo := New(logger, Config{
+		FlushIdleAfter:            60,
+		RootPath:                  dirName,
+		QueryMaximumResults:       10000,
+		MaxImportGoroutinesFactor: 1,
+	}, &fakeRemoteClient{}, &fakeNodeResolver{}, &fakeRemoteNodeClient{}, nil)
+	repo.SetSchemaGetter(schemaGetter)
+	err := repo.WaitForStartup(context.TODO())
+	require.Nil(t, err)
+	defer repo.Shutdown(context.Background())
+
+	class := &models.Class{
+		VectorIndexConfig:   enthnsw.NewDefaultUserConfig(),
+		InvertedIndexConfig: invertedConfig(),
+		Class:               "MyClass",
+		Properties: []*models.Property{
+			{
+				Name:         "title",
+				DataType:     []string{string(schema.DataTypeString)},
+				Tokenization: "word",
+			},
+			{
+				Name:         "description",
+				DataType:     []string{string(schema.DataTypeString)},
+				Tokenization: "word",
+			},
+		},
+	}
+
+	schema := schema.Schema{
+		Objects: &models.Schema{
+			Classes: []*models.Class{class},
+		},
+	}
+
+	schemaGetter.schema = schema
+
+	migrator := NewMigrator(repo, logger)
+	migrator.AddClass(context.Background(), class, schemaGetter.shardState)
+
+	testData := []map[string]interface{}{}
+	testData = append(testData, map[string]interface{}{"title": "Our journey to BM25F", "description": "This is how we get to BM25F"})
+
+	testData = append(testData, map[string]interface{}{"title": "Why I dont like journey", "description": "This is about how we get somewhere"})
+	testData = append(testData, map[string]interface{}{"title": "My journeys in Journey", "description": "A journey story about journeying"})
+	testData = append(testData, map[string]interface{}{"title": "An unrelated title", "description": "Actually all about journey"})
+	testData = append(testData, map[string]interface{}{"title": "journey journey", "description": "journey journey journey"})
+	testData = append(testData, map[string]interface{}{"title": "journey", "description": "journey journey"})
+
+	for i, data := range testData {
+		id := strfmt.UUID(uuid.MustParse(fmt.Sprintf("%032d", i)).String())
+
+		obj := &models.Object{Class: "MyClass", ID: id, Properties: data, CreationTimeUnix: 1565612833955, LastUpdateTimeUnix: 10000020}
+		vector := []float32{1, 3, 5, 0.4}
+		//{title: "Our journey to BM25F", description: " This is how we get to BM25F"}}
+		err = repo.PutObject(context.Background(), obj, vector)
+		require.Nil(t, err)
+	}
+
+	idx := repo.GetIndex("MyClass")
+	require.NotNil(t, idx)
+
+	kwr := &searchparams.KeywordRanking{Properties: []string{"title", "description"}, Query: "journey"}
+	addit := additional.Properties{}
+	res, err := idx.objectSearch(context.TODO(), 1000, nil, kwr, nil, addit)
+	fmt.Printf("res: %+v, err: %+v", res, err)
+	require.Nil(t, err)
+
+}
