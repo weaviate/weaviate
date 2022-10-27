89b7d3f0c06d62dbb76f143d7da8c41ba61dabcc
Jeremy Price <donomii@gmail.com>
2022-10-27 15:13:31 +0200

    Test params change

--- adapters/repos/db/bm25f_test.go
+++ adapters/repos/db/bm25f_test.go
@@ -32,26 +32,25 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestBM25FJourney(t *testing.T) {
-	rand.Seed(time.Now().UnixNano())
-	dirName := t.TempDir()
+func BM25FinvertedConfig(k1,b float32) *models.InvertedIndexConfig {
+	return &models.InvertedIndexConfig{
+		Bm25: &models.BM25Config{
+			K1: k1,
+			B:  b,
+		},
+		CleanupIntervalSeconds: 60,
+		Stopwords: &models.StopwordConfig{
+			Preset: "none",
+		},
+		IndexNullState:      true,
+		IndexPropertyLength: true,
+	}
+}
 
-	logger := logrus.New()
-	schemaGetter := &fakeSchemaGetter{shardState: singleShardState()}
-	repo := New(logger, Config{
-		FlushIdleAfter:            60,
-		RootPath:                  dirName,
-		QueryMaximumResults:       10000,
-		MaxImportGoroutinesFactor: 1,
-	}, &fakeRemoteClient{}, &fakeNodeResolver{}, &fakeRemoteNodeClient{}, nil)
-	repo.SetSchemaGetter(schemaGetter)
-	err := repo.WaitForStartup(context.TODO())
-	require.Nil(t, err)
-	defer repo.Shutdown(context.Background())
-
+func SetupClass (t require.TestingT, repo *DB, schemaGetter *fakeSchemaGetter, logger logrus.FieldLogger, k1,b float32) {
 	class := &models.Class{
 		VectorIndexConfig:   enthnsw.NewDefaultUserConfig(),
-		InvertedIndexConfig: invertedConfig(),
+		InvertedIndexConfig: BM25FinvertedConfig(k1,b),
 		Class:               "MyClass",
 		Properties: []*models.Property{
 			{
@@ -93,12 +92,34 @@ func TestBM25FJourney(t *testing.T) {
 		obj := &models.Object{Class: "MyClass", ID: id, Properties: data, CreationTimeUnix: 1565612833955, LastUpdateTimeUnix: 10000020}
 		vector := []float32{1, 3, 5, 0.4}
 		//{title: "Our journey to BM25F", description: " This is how we get to BM25F"}}
-		err = repo.PutObject(context.Background(), obj, vector)
+		err := repo.PutObject(context.Background(), obj, vector)
 		require.Nil(t, err)
 	}
+}
+
+func TestBM25FJourney(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	dirName := t.TempDir()
+
+	logger := logrus.New()
+	schemaGetter := &fakeSchemaGetter{shardState: singleShardState()}
+	repo := New(logger, Config{
+		FlushIdleAfter:            60,
+		RootPath:                  dirName,
+		QueryMaximumResults:       10000,
+		MaxImportGoroutinesFactor: 1,
+	}, &fakeRemoteClient{}, &fakeNodeResolver{}, &fakeRemoteNodeClient{}, nil)
+	repo.SetSchemaGetter(schemaGetter)
+	err := repo.WaitForStartup(context.TODO())
+	require.Nil(t, err)
+	defer repo.Shutdown(context.Background())
+
+
+	SetupClass(t, repo, schemaGetter, logger, 1.2, 0.75)
 
 	idx := repo.GetIndex("MyClass")
 	require.NotNil(t, idx)
+
 
 	//Check basic search with one property
 	kwr := &searchparams.KeywordRanking{Type: "bm25", Properties: []string{"title", "description"}, Query: "journey"}
@@ -176,4 +197,53 @@ func TestBM25FJourney(t *testing.T) {
 	require.Equal(t, uint64(5), res[2].DocID())
 	require.Equal(t, uint64(6), res[3].DocID())
 	require.Equal(t, uint64(2), res[4].DocID())
+}
+
+func TestBM25FDifferentParamsJourney(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	dirName := t.TempDir()
+
+	logger := logrus.New()
+	schemaGetter := &fakeSchemaGetter{shardState: singleShardState()}
+	repo := New(logger, Config{
+		FlushIdleAfter:            60,
+		RootPath:                  dirName,
+		QueryMaximumResults:       10000,
+		MaxImportGoroutinesFactor: 1,
+	}, &fakeRemoteClient{}, &fakeNodeResolver{}, &fakeRemoteNodeClient{}, nil)
+	repo.SetSchemaGetter(schemaGetter)
+	err := repo.WaitForStartup(context.TODO())
+	require.Nil(t, err)
+	defer repo.Shutdown(context.Background())
+
+
+	SetupClass(t, repo, schemaGetter, logger, 1.2, 0.75)
+
+	idx := repo.GetIndex("MyClass")
+	require.NotNil(t, idx)
+
+		//Check scores change when k1 and b are changed
+			//Check boosted
+	kwr := &searchparams.KeywordRanking{Type: "bm25", Properties: []string{"title", "description"}, Query: "journey^3"}
+	addit := additional.Properties{}
+	res, err := idx.objectSearch(context.TODO(), 1000, nil, kwr, nil, addit)
+
+	require.Nil(t, err)
+
+	//Check results in correct order
+	require.Equal(t, uint64(4), res[0].DocID())
+	require.Equal(t, uint64(5), res[1].DocID())
+	require.Equal(t, uint64(6), res[2].DocID())
+	require.Equal(t, uint64(2), res[3].DocID())
+	//Print results
+	fmt.Println("--- Start results for boosted search ---")
+	for _, r := range res {
+		fmt.Printf("Result id: %v, score: %v, title: %v, description: %v, additional %+v\n", r.DocID(), r.Score(), r.Object.Properties.(map[string]interface{})["title"], r.Object.Properties.(map[string]interface{})["description"], r.Object.Additional)
+	}
+
+	//Check scores
+	require.Equal(t, float32(0.061129723), res[0].Score())
+	require.Equal(t, float32(0.059050433), res[1].Score())
+	require.Equal(t, float32(0.05252086), res[2].Score())
+	require.Equal(t, float32(0.044437673), res[3].Score())
 }
