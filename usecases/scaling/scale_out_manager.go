dc8d6089d5869713e9ee7d62be8158dbd7ca9753
etiennedi <etienne@dilocker.de>
2022-10-28 15:16:02 +0200

    [ci skip] Explain in detail how scale-out works

--- usecases/scaling/scale_out_manager.go
+++ usecases/scaling/scale_out_manager.go
@@ -90,61 +90,99 @@ func (som *ScaleOutManager) Scale(ctx context.Context, className string,
 	return nil, nil
 }
 
+// scaleOut is a relatively primitive implelemation that takes a shard and
+// copies it onto another node. Then it returns the new and updated sharding
+// state where the node-association is updated to point to all nodes where the
+// shard can be found.
+//
+// This implementation is meant as a temporary one and should probably be
+// replaced with something more sophisticated. The main issues are:
+//
+//   - Everything is synchrounous. This blocks the users request until all data
+//     is copied which is not great.
+//   - Everything is sequential. A lot of the things could probably happen in
+//     parallel
+//
+// Follow the in-line comments to see how this implementation achieves scalign
+// out
 func (som *ScaleOutManager) scaleOut(ctx context.Context, className string,
 	old, updated sharding.Config,
 ) (*sharding.State, error) {
+	// First identify what the sharding state was before this change. This is
+	// mainly to be able to compare the diff later, so we know where we need to
+	// make changes
 	ssBefore := som.schemaManager.ShardingState(className)
 	if ssBefore == nil {
 		return nil, errors.Errorf("no sharding state for class %q", className)
 	}
 
+	// Create a deep copy of the old sharding state, so we can start building the
+	// updated state. Because this is a deep copy we don't risk leaking our
+	// changes to anyone else. We can return the changes in the end where the
+	// caller can then make sure to broadcast the new state to the cluster.
 	ssAfter := ssBefore.DeepCopy()
 
+	// Identify all shards of the class and adjust the replicas. After this is
+	// done, the affected shards now belong to more nodes than they did before.
 	for name, shard := range ssAfter.Physical {
 		shard.AdjustReplicas(updated.Replicas, som.clusterState)
 		ssAfter.Physical[name] = shard
 	}
+	// However, so far we have only updated config, now we also need to actually
+	// copy files.
 
+	// This is a temporary limitation that kills UX, this should be fixed soon.
+	// Right now the user would have to know which node to send the request to
+	// because there is no forwarding yet.
 	for name := range ssBefore.Physical {
 		if !ssBefore.IsShardLocal(name) {
-			// TODO
 			return nil, errors.Errorf("scaling remote shards not supported yet, send request to node that has the shard to be scaled out")
 		}
 	}
 
-	// for each shard
-	//
-	// - find existing local copy on current or remote node
-	// TODO: what if existing shard is not on current node?
-	//       in first iteration just fail, it's OK to support only 1->n for now
-	//       which makes sure that the existing shard is local
-	//
-	// - create a snapshot
-
+	// Next, we will iterate over every shard that this class has. This is the
+	// meat&bones of this implemenation. For each shard, we're roughly doing the
+	// following:
+	// - Create a single shard backup, so the shard is safe to copy
+	// - Figure out the copy targets (i.e. each node that is part of the after
+	//   state, but wasn't part of the before state yet)
+	// - Create an empty shard on the target node
+	// - Copy over all files from the backup
+	// - Reinit the shard to recognize the copied files
+	// - Release the single-shard backup
 	for shardName := range ssBefore.Physical {
+		// Create backup of the single shard
 		bakID := fmt.Sprintf("_internal_scaleout_%s", uuid.New().String())
 		bak, err := som.backerUpper.SingleShardBackup(ctx, bakID, className, shardName)
 		if err != nil {
 			return nil, errors.Wrap(err, "create snapshot")
 		}
 
+		// Figure out which nodes are new by diffing the before and after state
 		// TODO: This manual diffing is ugly, refactor!
 		newNodes := ssAfter.Physical[shardName].BelongsToNodes
 		previousNodes := ssBefore.Physical[shardName].BelongsToNodes
-
+		// This relies on the convention that new nodes are always appended at the end
 		additions := newNodes[len(previousNodes):]
+
+		// Iterate over the new target nodes and copy files
 		for _, targetNode := range additions {
 			bakShard := bak.Shards[0]
 			if bakShard.Name != shardName {
-				// this sanity check is only needed because of the [0] above. If this
+				// this sanity check is only needed because of the Shards[0] above. If this
 				// supports multi-shard, we need a better logic anyway
 				return nil, fmt.Errorf("shard name mismatch in backup: %q vs %q", bakShard.Name,
 					shardName)
 			}
+
+			// Create an empty shard on the remote node. This is a requirement to
+			// copy files in the next step. If the empty shard didn't exist, we'd
+			// have no copy target which could receive the files.
 			if err := som.CreateShard(ctx, targetNode, className, shardName); err != nil {
 				return nil, fmt.Errorf("create new shard on remote node: %w", err)
 			}
 
+			// Transfer each file that's part of the backup.
 			for _, file := range bakShard.Files {
 				err := som.PutFile(ctx, file, targetNode, className, shardName)
 				if err != nil {
@@ -152,29 +190,26 @@ func (som *ScaleOutManager) scaleOut(ctx context.Context, className string,
 				}
 			}
 
+			// Now that all files are on the remote node's new shard, the shard needs
+			// to be reinitialized. Otherwise, it would not recognize the files when
+			// serving traffic later.
 			if err := som.ReinitShard(ctx, targetNode, className, shardName); err != nil {
 				return nil, fmt.Errorf("create new shard on remote node: %w", err)
 			}
-
 		}
 
+		// Clean up after ourselves and prevent blocking future backups.
 		if err := som.backerUpper.ReleaseBackup(ctx, bakID, className); err != nil {
 			return nil, fmt.Errorf("release shard backup: %w", err)
 		}
 	}
 
-	//
-	// - identify target nodes and tell them to create (empty) local shards
-	//
-	// - transfer all files from source shard to new shard
-	//
-	// - release snapshots to restart compation cycles, etc
-	//
-
-	// finally, commit sharding state back to schema manager so that everyone is
-	// aware of the new associations. The schema Manager itself must make sure
-	// that the updated association is replicated to the entire cluster
-
+	// Finally, return sharding state back to schema manager. The schema manager
+	// will then broadcast this updated state to the cluster. This is essentially
+	// what will take the new replication shards live: On the new nodes, if
+	// traffic is incoming, IsShardLocal() would have returned false before. But
+	// now that a copy of the local shard is present it will return true and
+	// serve the traffic.
 	return &ssAfter, nil
 }
 
