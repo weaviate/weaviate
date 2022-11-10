0b80309c67bad19f5cc108e1587bfb660cc8f5e0
etiennedi <etienne@dilocker.de>
2022-11-10 19:10:01 +0100

    prepare release v1.16.1

--- adapters/handlers/rest/embedded_spec.go
+++ adapters/handlers/rest/embedded_spec.go
@@ -48,7 +48,7 @@ func init() {
       "url": "https://github.com/semi-technologies",
       "email": "hello@semi.technology"
     },
-    "version": "1.16.0"
+    "version": "1.16.1"
   },
   "basePath": "/v1",
   "paths": {
@@ -4195,7 +4195,7 @@ func init() {
       "url": "https://github.com/semi-technologies",
       "email": "hello@semi.technology"
     },
-    "version": "1.16.0"
+    "version": "1.16.1"
   },
   "basePath": "/v1",
   "paths": {
