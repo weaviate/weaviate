287ae2642bac3a166538764ab1ccf2292fd4cb27
Jeremy Price <donomii@gmail.com>
2022-11-14 10:30:36 +0100

    Add hybrid parameters

---
+++ adapters/handlers/graphql/local/common_filters/hybrid.go
@@ -0,0 +1,33 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2022 SeMI Technologies B.V. All rights reserved.
+//
+//  CONTACT: hello@semi.technology
+//
+
+package common_filters
+
+import "github.com/semi-technologies/weaviate/entities/searchparams"
+
+// ExtractHybrid
+func ExtractHybrid(source map[string]interface{}) searchparams.HybridSearch {
+	var args searchparams.HybridSearch
+
+	alpha, ok := source["alpha"]
+	if ok {
+		args.Alpha = alpha.(float64)
+	}
+
+	query, ok := source["query"]
+	if ok {
+		args.Query = query.(string)
+	}
+
+	args.Type = "hybrid"
+
+	return args
+}
