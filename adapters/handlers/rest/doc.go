4d80beee01262357f5f432a839bb8505b1aae536
Marcin Antas <antas.marcin@gmail.com>
2022-11-18 17:34:28 +0100

    prepare release v1.16.4

--- adapters/handlers/rest/doc.go
+++ adapters/handlers/rest/doc.go
@@ -18,7 +18,7 @@
 //	  https
 //	Host: localhost
 //	BasePath: /v1
-//	Version: 1.16.3
+//	Version: 1.16.4
 //	Contact: Weaviate<hello@semi.technology> https://github.com/semi-technologies
 //
 //	Consumes:
