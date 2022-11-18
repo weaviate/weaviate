efef64791af4d7ea591040ff9ae6df54b1c77269
Marcin Antas <antas.marcin@gmail.com>
2022-11-18 17:31:33 +0100

    Revert "prepare release v1.16.4"
    This reverts commit 6c8368f169eac8ee63a1056892d5765809d6271d.

--- adapters/handlers/rest/doc.go
+++ adapters/handlers/rest/doc.go
@@ -18,7 +18,7 @@
 //	  https
 //	Host: localhost
 //	BasePath: /v1
-//	Version: 1.16.4
+//	Version: 1.16.3
 //	Contact: Weaviate<hello@semi.technology> https://github.com/semi-technologies
 //
 //	Consumes:
