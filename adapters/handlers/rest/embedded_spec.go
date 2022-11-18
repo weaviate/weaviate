e6e3b9d219a54ef581f93b986a0e0760d198b826
Marcin Antas <antas.marcin@gmail.com>
2022-11-18 17:31:33 +0100

    Revert "prepare release v1.16.4"
    This reverts commit 7065ec0a0a05bdd348e51b388e7682aab69fc66c.

--- adapters/handlers/rest/embedded_spec.go
+++ adapters/handlers/rest/embedded_spec.go
@@ -48,7 +48,7 @@ func init() {
       "url": "https://github.com/semi-technologies",
       "email": "hello@semi.technology"
     },
-    "version": "1.16.4"
+    "version": "1.16.3"
   },
   "basePath": "/v1",
   "paths": {
@@ -4195,7 +4195,7 @@ func init() {
       "url": "https://github.com/semi-technologies",
       "email": "hello@semi.technology"
     },
-    "version": "1.16.4"
+    "version": "1.16.3"
   },
   "basePath": "/v1",
   "paths": {
