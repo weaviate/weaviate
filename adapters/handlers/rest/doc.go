808a87b37130cec1444f8c846351056e5f5b833e
etiennedi <etienne@dilocker.de>
2022-12-12 17:45:46 +0100

    prepare release v1.16.7

--- adapters/handlers/rest/doc.go
+++ adapters/handlers/rest/doc.go
@@ -18,7 +18,7 @@
 //	  https
 //	Host: localhost
 //	BasePath: /v1
-//	Version: 1.16.6
+//	Version: 1.16.7
 //	Contact: Weaviate<hello@semi.technology> https://github.com/semi-technologies
 //
 //	Consumes:
