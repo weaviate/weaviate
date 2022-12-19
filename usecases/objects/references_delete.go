468f27b16927d2459efff5dd8f10488d576f2a82
Dirk Kulawiak <dirk@semi.technology>
2022-12-19 16:02:38 +0100

    Pass class to Update vector

--- usecases/objects/references_delete.go
+++ usecases/objects/references_delete.go
@@ -85,7 +85,7 @@ func (m *Manager) DeleteObjectReference(
 		return &Error{"repo.putobject", StatusInternalServerError, err}
 	}
 
-	if err := m.updateRefVector(ctx, input.Class, input.ID); err != nil {
+	if err := m.updateRefVector(ctx, principal, input.Class, input.ID); err != nil {
 		return &Error{"update ref vector", StatusInternalServerError, err}
 	}
 
