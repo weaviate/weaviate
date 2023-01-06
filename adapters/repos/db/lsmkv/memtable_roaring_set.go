bf957a2f66fbf1088783295d14ddaea0a8e94527
etiennedi <etienne@dilocker.de>
2023-01-06 20:54:45 +0100

    make sure memtable size is increased for each write

--- adapters/repos/db/lsmkv/memtable_roaring_set.go
+++ adapters/repos/db/lsmkv/memtable_roaring_set.go
@@ -1,6 +1,8 @@
 package lsmkv
 
 import (
+	"time"
+
 	"github.com/dgraph-io/sroar"
 )
 
@@ -9,9 +11,13 @@ func (m *Memtable) roaringSetAddOne(key []byte, value uint64) error {
 		return err
 	}
 
+	// TODO: write into commit log
+
 	set := newRoaringSet()
 	set.additions.Set(value)
 	m.roaringSet.insert(key, set)
+
+	m.roaringSetAdjustMeta(1)
 	return nil
 }
 
@@ -20,9 +26,13 @@ func (m *Memtable) roaringSetRemoveOne(key []byte, value uint64) error {
 		return err
 	}
 
+	// TODO: write into commit log
+
 	set := newRoaringSet()
 	set.deletions.Set(value)
 	m.roaringSet.insert(key, set)
+
+	m.roaringSetAdjustMeta(1)
 	return nil
 }
 
@@ -31,9 +41,13 @@ func (m *Memtable) roaringSetAddList(key []byte, values []uint64) error {
 		return err
 	}
 
+	// TODO: write into commit log
+
 	set := newRoaringSet()
 	set.additions.SetMany(values)
 	m.roaringSet.insert(key, set)
+
+	m.roaringSetAdjustMeta(len(values))
 	return nil
 }
 
@@ -42,9 +56,13 @@ func (m *Memtable) roaringSetAddBitmap(key []byte, bm *sroar.Bitmap) error {
 		return err
 	}
 
+	// TODO: write into commit log
+
 	set := newRoaringSet()
 	set.additions = bm
 	m.roaringSet.insert(key, set)
+
+	m.roaringSetAdjustMeta(bm.GetCardinality())
 	return nil
 }
 
@@ -54,4 +72,13 @@ func (m *Memtable) roaringSetGet(key []byte) (*roaringSet, error) {
 	}
 
 	return m.roaringSet.get(key)
+}
+
+func (m *Memtable) roaringSetAdjustMeta(entriesChanged int) {
+	// in the worst case roaring bitmaps take 2 bytes per entry. A reasonable
+	// estimation is therefore to take the changed entries and multiply them by
+	// 2.
+	m.size += uint64(entriesChanged * 2)
+	m.metrics.size(m.size)
+	m.lastWrite = time.Now()
 }
