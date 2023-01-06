dfc6dc6cbdfb36fbfd6fbdb29705c78ac7d5b5aa
etiennedi <etienne@dilocker.de>
2023-01-06 20:54:45 +0100

    make sure memtable size is increased for each write

--- adapters/repos/db/lsmkv/memtable_roaring_set_test.go
+++ adapters/repos/db/lsmkv/memtable_roaring_set_test.go
@@ -19,6 +19,7 @@ func TestMemtableRoaringSet(t *testing.T) {
 		assert.Nil(t, m.roaringSetAddOne(key1, 2))
 		assert.Nil(t, m.roaringSetAddOne(key2, 3))
 		assert.Nil(t, m.roaringSetAddOne(key2, 4))
+		assert.Greater(t, m.Size(), uint64(0))
 
 		setKey1, err := m.roaringSetGet(key1)
 		require.Nil(t, err)
@@ -43,6 +44,7 @@ func TestMemtableRoaringSet(t *testing.T) {
 
 		assert.Nil(t, m.roaringSetAddList(key1, []uint64{1, 2}))
 		assert.Nil(t, m.roaringSetAddList(key2, []uint64{3, 4}))
+		assert.Greater(t, m.Size(), uint64(0))
 
 		setKey1, err := m.roaringSetGet(key1)
 		require.Nil(t, err)
@@ -71,6 +73,7 @@ func TestMemtableRoaringSet(t *testing.T) {
 		bm2 := sroar.NewBitmap()
 		bm2.SetMany([]uint64{3, 4})
 		assert.Nil(t, m.roaringSetAddBitmap(key2, bm2))
+		assert.Greater(t, m.Size(), uint64(0))
 
 		setKey1, err := m.roaringSetGet(key1)
 		require.Nil(t, err)
@@ -95,6 +98,7 @@ func TestMemtableRoaringSet(t *testing.T) {
 
 		assert.Nil(t, m.roaringSetRemoveOne(key1, 7))
 		assert.Nil(t, m.roaringSetRemoveOne(key2, 8))
+		assert.Greater(t, m.Size(), uint64(0))
 
 		setKey1, err := m.roaringSetGet(key1)
 		require.Nil(t, err)
