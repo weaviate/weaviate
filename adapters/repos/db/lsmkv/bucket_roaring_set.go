dd99415c5f65f385b92d172532762325d2250a60
etiennedi <etienne@dilocker.de>
2023-01-06 21:28:16 +0100

    flesh out bucket level API
    currently only supports reading from memtable

--- adapters/repos/db/lsmkv/bucket_roaring_set.go
+++ adapters/repos/db/lsmkv/bucket_roaring_set.go
@@ -50,7 +50,7 @@ func (b *Bucket) RoaringSetAddBitmap(key []byte, bm *sroar.Bitmap) error {
 	return b.active.roaringSetAddBitmap(key, bm)
 }
 
-func (b *Bucket) RoaringSetGet(key []byte) (*roaringSet, error) {
+func (b *Bucket) RoaringSetGet(key []byte) (*sroar.Bitmap, error) {
 	if err := checkStrategyRoaringSet(b.strategy); err != nil {
 		return nil, err
 	}
@@ -58,7 +58,35 @@ func (b *Bucket) RoaringSetGet(key []byte) (*roaringSet, error) {
 	b.flushLock.RLock()
 	defer b.flushLock.RUnlock()
 
-	return b.active.roaringSetGet(key)
+	var segments []*roaringSet
+
+	// TODO: check segments other than memtable
+
+	memtable, err := b.active.roaringSetGet(key)
+	if err != nil {
+		if err != NotFound {
+			return nil, err
+		}
+	} else {
+		segments = append(segments, memtable)
+	}
+
+	return flattenRoaringSegments(segments)
+}
+
+func flattenRoaringSegments(segments []*roaringSet) (*sroar.Bitmap, error) {
+	if len(segments) == 0 {
+		return sroar.NewBitmap(), nil
+	}
+
+	if len(segments) > 1 {
+		return nil, fmt.Errorf("merging multiple segments not implemented yet")
+	}
+
+	cur := segments[0]
+	merged := cur.additions.Clone()
+
+	return merged, nil
 }
 
 func checkStrategyRoaringSet(bucketStrat string) error {
