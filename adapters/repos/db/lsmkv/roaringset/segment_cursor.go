823d5c31786b4cf5017e6b3de6d503f628adfb0a
etiennedi <etienne@dilocker.de>
2023-01-08 22:51:18 +0100

    add segment cursor in prep for compaction

---
+++ adapters/repos/db/lsmkv/roaringset/segment_cursor.go
@@ -0,0 +1,58 @@
+package roaringset
+
+import (
+	"github.com/semi-technologies/weaviate/adapters/repos/db/lsmkv/segmentindex"
+)
+
+type Seeker interface {
+	Seek(key []byte) (segmentindex.Node, error)
+}
+
+// A SegmentCursor iterates over all key-value pairs in a single disk segment.
+// You can either start at the beginning using [*SegmentCursor.First] or start
+// at an arbitrary key that you may find using [*SegmentCursor.Seek]
+type SegmentCursor struct {
+	index      Seeker
+	data       []byte
+	nextOffset uint64
+}
+
+// NewSegmentCursor creates a cursor for a single disk segment. Make sure that
+// the data buf is already sliced correctly to start at the payload, as calling
+// [*SegmentCursor.First] will start reading at offset 0 relative to the passed
+// in buffer. Similary, the buffer may only contain payloads, as the buffer end
+// is used to determine if more keys can be found.
+//
+// Therefore if the payload is part of a longer continous buffer, the cursor
+// should be initialized with data[payloadStartPos:payloadEndPos]
+func NewSegmentCursor(data []byte, index Seeker) *SegmentCursor {
+	return &SegmentCursor{index: index, data: data, nextOffset: 0}
+}
+
+func (c *SegmentCursor) Next() ([]byte, BitmapLayer, error) {
+	if c.nextOffset >= uint64(len(c.data)) {
+		return nil, BitmapLayer{}, nil
+	}
+
+	sn := NewSegmentNodeFromBuffer(c.data[c.nextOffset:])
+	c.nextOffset += sn.Len()
+	layer := BitmapLayer{
+		Additions: sn.Additions(),
+		Deletions: sn.Deletions(),
+	}
+	return sn.PrimaryKey(), layer, nil
+}
+
+func (c *SegmentCursor) First() ([]byte, BitmapLayer, error) {
+	c.nextOffset = 0
+	return c.Next()
+}
+
+func (c *SegmentCursor) Seek(key []byte) ([]byte, BitmapLayer, error) {
+	node, err := c.index.Seek(key)
+	if err != nil {
+		return nil, BitmapLayer{}, err
+	}
+	c.nextOffset = node.Start
+	return c.Next()
+}
