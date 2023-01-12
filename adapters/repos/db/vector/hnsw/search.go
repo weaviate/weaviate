0b319bd59bfd0f14ca6672b59bd551ea05d3a44e
Abdel Rodríguez <abdelr1982@gmail.com>
2023-01-12 14:10:02 +0100

    Sync when compressing

--- adapters/repos/db/vector/hnsw/search.go
+++ adapters/repos/db/vector/hnsw/search.go
@@ -68,11 +68,20 @@ func (h *hnsw) SearchByVector(vector []float32, k int, allowList helpers.AllowLi
 		vector = distancer.Normalize(vector)
 	}
 
+	var ids []uint64
+	var distances []float32
+	var err error
 	flatSearchCutoff := int(atomic.LoadInt64(&h.flatSearchCutoff))
 	if allowList != nil && !h.forbidFlat && len(allowList) < flatSearchCutoff {
-		return h.flatSearch(vector, k, allowList)
+		h.compressActionLock.InvokeConcurrentTask(func() {
+			ids, distances, err = h.flatSearch(vector, k, allowList)
+		})
+		return ids, distances, err
 	}
-	return h.knnSearchByVector(vector, k, h.searchTimeEF(k), allowList)
+	h.compressActionLock.InvokeConcurrentTask(func() {
+		ids, distances, err = h.knnSearchByVector(vector, k, h.searchTimeEF(k), allowList)
+	})
+	return ids, distances, err
 }
 
 // SearchByVectorDistance wraps SearchByVector, and calls it recursively until
