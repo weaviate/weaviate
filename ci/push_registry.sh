5d3cc9f9d481e231bd46b7116f1310c6a7e60fc9
Dirk Kulawiak <dirk@semi.technology>
2023-01-13 12:11:42 +0100

    Better label and tag

--- ci/push_registry.sh
+++ ci/push_registry.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-DOCKER_REPO="semitechnologies/weaviate"
 IMAGE_NAME="weaviate"
 function release() {
   # for multi-platform build
-  docker build . --file Dockerfile --tag $IMAGE_NAME
 
   IMAGE_ID=ghcr.io/$REPO_OWNER/$IMAGE_NAME
   TAG=
@@ -12,24 +10,28 @@ function release() {
   git_hash=$(echo "$GITHUB_SHA" | cut -c1-7)
 
   weaviate_version="$(jq -r '.info.version' < openapi-specs/schema.json)"
+  echo weaviate_version=$weaviate_version
+
   if [ "$GITHUB_REF_NAME" = "master" ]; then
-    TAG="${DOCKER_REPO}:${weaviate_version}-${git_hash}"
+    TAG="$${weaviate_version}-${git_hash}"
   elif [  "$GITHUB_REF_TYPE" == "tag" ]; then
         if [ "$GITHUB_REF_NAME" != "v$weaviate_version" ]; then
             echo "The release tag ($GITHUB_REF_NAME) and Weaviate version (v$weaviate_version) are not equal! Can't release."
             return 1
         fi
-        TAG="${DOCKER_REPO}:${weaviate_version}"
+        TAG="${weaviate_version}"
   else
     pr_title="$(echo -n "$PR_TITLE" | tr '[:upper:]' '[:lower:]' | tr -c -s '[:alnum:]' '-' | sed 's/-$//g')"
-    TAG="${DOCKER_REPO}:preview-${pr_title}-${git_hash}"
+    TAG="preview-${pr_title}-${git_hash}"
   fi
 
   echo IMAGE_ID=$IMAGE_ID
-  echo TAG=TAG
+  echo TAG=$TAG
+  echo IMAGE_NAME=$IMAGE_NAME
 
-  docker tag $IMAGE_NAME $IMAGE_ID:TAG
-  docker push $IMAGE_ID:TAG
+  docker build . --file Dockerfile --tag $IMAGE_NAME  --label "tag=${TAG}"
+  docker tag $IMAGE_NAME $IMAGE_ID:$TAG
+  docker push $IMAGE_ID:$TAG
 }
 
 release
