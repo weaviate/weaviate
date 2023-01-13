3abdc039ef1d9208af3ffd9a1ad0297060dde5f4
Jeremy Price <donomii@gmail.com>
2023-01-13 17:59:15 +0900

    Fix return

--- adapters/repos/db/index.go
+++ adapters/repos/db/index.go
@@ -807,7 +807,7 @@ func (i *Index) objectSearch(ctx context.Context, limit int, filters *filters.Lo
 
 				// WEAVIATE-471 - error if we can't find a property to search
 				if len(keywordRanking.Properties) == 0 {
-					return nil, errors.New("No properties provided, and no indexed properties found in class")
+					return nil, []float32{}, errors.New("No properties provided, and no indexed properties found in class")
 				}
 
 
