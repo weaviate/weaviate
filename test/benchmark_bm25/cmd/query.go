231146bfb0530ffe4f8717ffe9b4c2b9427fcde5
etiennedi <etienne@dilocker.de>
2023-01-19 14:55:38 +0100

    query query list, analyze latencies

---
+++ test/benchmark_bm25/cmd/query.go
@@ -0,0 +1,88 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/weaviate/weaviate-go-client/v4/weaviate/graphql"
+	"github.com/weaviate/weaviate/test/benchmark_bm25/lib"
+)
+
+func init() {
+	rootCmd.AddCommand(queryCmd)
+	queryCmd.PersistentFlags().IntVarP(&BatchSize, "batch-size", "b", DefaultBatchSize, "number of objects in a single import batch")
+}
+
+var queryCmd = &cobra.Command{
+	Use:   "query",
+	Short: "Send queries for a dataset",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		client, err := lib.ClientFromOrigin(Origin)
+		if err != nil {
+			return err
+		}
+
+		ok, err := client.Misc().LiveChecker().Do(context.Background())
+		if err != nil {
+			return fmt.Errorf("weaviate is not ready: %w", err)
+		}
+
+		if !ok {
+			return fmt.Errorf("weaviate is not ready")
+		}
+		log.Print("weaviate is ready")
+
+		datasets, err := lib.ParseDatasetConfig(DatasetConfigPath)
+		if err != nil {
+			return fmt.Errorf("parse dataset cfg file: %w", err)
+		}
+
+		ds := datasets.Datasets[0]
+
+		log.Print("parse queries")
+		q, err := lib.ParseQueries(ds)
+		if err != nil {
+			return err
+		}
+		log.Print("queries parsed")
+
+		times := []time.Duration{}
+
+		log.Print("start querying")
+
+		for i, query := range q {
+			before := time.Now()
+			bm25 := &graphql.BM25ArgumentBuilder{}
+			bm25.WithQuery(query)
+
+			_, err := client.GraphQL().Get().WithClassName(lib.ClassNameFromDatasetID(ds.ID)).
+				WithLimit(10).WithBM25(bm25).WithFields(graphql.Field{Name: "_additional { id }"}).Do(context.Background())
+			if err != nil {
+				return err
+			}
+
+			times = append(times, time.Since(before))
+
+			if i%1000 == 0 {
+				log.Printf("completed %d/%d queries", i, len(q))
+			}
+		}
+
+		meta, err := client.GraphQL().Aggregate().WithClassName(lib.ClassNameFromDatasetID(ds.ID)).
+			WithFields(graphql.Field{Name: "meta", Fields: []graphql.Field{{Name: "count"}}}).
+			Do(context.Background())
+		if err != nil {
+			return err
+		}
+
+		objCount := int(meta.Data["Aggregate"].(map[string]interface{})[lib.ClassNameFromDatasetID(ds.ID)].([]interface{})[0].(map[string]interface{})["meta"].(map[string]interface{})["count"].(float64))
+
+		fmt.Printf("\nObjects imported: %d\n", objCount)
+		stat := lib.AnalyzeLatencies(times)
+		stat.PrettyPrint()
+		return nil
+	},
+}
