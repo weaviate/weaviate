55f49a0cbbad8361c12de81e9bb46429b3114e6c
Abdel Rodríguez <abdelr1982@gmail.com>
2023-01-26 21:50:30 +0100

    Adding normal distribution for tile encoders

--- adapters/repos/db/vector/ssdhelpers/tileEncoder_test.go
+++ adapters/repos/db/vector/ssdhelpers/tileEncoder_test.go
@@ -21,21 +21,45 @@ import (
 )
 
 func TestTileEncoderEncode(t *testing.T) {
-	encoder := ssdhelpers.NewTileEncoder(4, 0)
+	encoder := ssdhelpers.NewTileEncoder(4, 0, ssdhelpers.LogNormalEncoderDistribution)
 	for i := 0; i < 1000000; i++ {
 		encoder.Add([]float32{float32(rand.NormFloat64() + 100)})
 	}
+	encoder.Fit([][]float32{})
 	assert.Equal(t, encoder.Encode([]float32{0.1}), byte(0))
 	assert.Equal(t, encoder.Encode([]float32{100}), byte(8))
 	assert.Equal(t, encoder.Encode([]float32{1000}), byte(16))
 }
 
 func TestTileEncoderCentroids(t *testing.T) {
-	encoder := ssdhelpers.NewTileEncoder(4, 0)
+	encoder := ssdhelpers.NewTileEncoder(4, 0, ssdhelpers.LogNormalEncoderDistribution)
 	for i := 0; i < 1000000; i++ {
 		encoder.Add([]float32{float32(rand.NormFloat64() + 100)})
 	}
+	encoder.Fit([][]float32{})
 	assert.Equal(t, math.Round(float64(encoder.Centroid(0)[0])), 98.0)
 	assert.Equal(t, math.Round(float64(encoder.Centroid(2)[0])), 99.0)
 	assert.Equal(t, math.Round(float64(encoder.Centroid(14)[0])), 101.0)
+}
+
+func TestNormalTileEncoderEncode(t *testing.T) {
+	encoder := ssdhelpers.NewTileEncoder(4, 0, ssdhelpers.NormalEncoderDistribution)
+	for i := 0; i < 1000000; i++ {
+		encoder.Add([]float32{float32(rand.NormFloat64())})
+	}
+	encoder.Fit([][]float32{})
+	assert.Equal(t, encoder.Encode([]float32{0.1}), byte(8))
+	assert.Equal(t, encoder.Encode([]float32{100}), byte(16))
+	assert.Equal(t, encoder.Encode([]float32{1000}), byte(16))
+}
+
+func TestNormalTileEncoderCentroids(t *testing.T) {
+	encoder := ssdhelpers.NewTileEncoder(4, 0, ssdhelpers.NormalEncoderDistribution)
+	for i := 0; i < 1000000; i++ {
+		encoder.Add([]float32{float32(rand.NormFloat64())})
+	}
+	encoder.Fit([][]float32{})
+	assert.Equal(t, math.Round(float64(encoder.Centroid(0)[0])), -2.0)
+	assert.Equal(t, math.Round(float64(encoder.Centroid(8)[0])), 0.0)
+	assert.Equal(t, math.Round(float64(encoder.Centroid(16)[0])), 2.0)
 }
