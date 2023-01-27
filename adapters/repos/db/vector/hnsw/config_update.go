c6f93d906f9d104c7d371e7071d7422d007f17c9
Abdel Rodríguez <abdelr1982@gmail.com>
2023-01-27 10:10:39 +0100

    Temporal params for encoder type and distribution
    This will be changed with the final API later

--- adapters/repos/db/vector/hnsw/config_update.go
+++ adapters/repos/db/vector/hnsw/config_update.go
@@ -98,7 +98,7 @@ func (h *hnsw) UpdateUserConfig(updated schema.VectorIndexConfig) error {
 	// ToDo: check atomic operation
 	if !h.compressed.Load() && parsed.Compressed {
 		h.logger.WithField("action", "compress").Info("switching to compressed vectors")
-		if err := h.Compress(parsed.PQSegments); err != nil {
+		if err := h.Compress(parsed.PQSegments, parsed.PQEncoderType, parsed.PQEncoderDistribution); err != nil {
 			h.logger.Error(err)
 			return err
 		}
