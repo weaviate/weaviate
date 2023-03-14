0abe7fceb67c6b9b3d4695681feea105971d1b6b
Redouan El Rhazouani <81578195+redouan-rhazouani@users.noreply.github.com>
2023-03-14 17:39:27 +0100

    unit test delegate

---
+++ usecases/cluster/delegate_test.go
@@ -0,0 +1,59 @@
+package cluster
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDiskSpace(t *testing.T) {
+	want := DiskSpace{
+		Total:     256,
+		Available: 3,
+	}
+	bytes, err := want.MarshalBinary()
+	assert.Nil(t, err)
+	got := DiskSpace{}
+	err = got.UnmarshalBinary(bytes)
+	assert.Nil(t, err)
+	assert.Equal(t, want, got)
+}
+
+func TestDelegate(t *testing.T) {
+	st := State{
+		delegate: delegate{
+			dataPath:  ".",
+			DiskUsage: make(map[string]DiskSpace, 32),
+		},
+	}
+	diskSpaces := []DiskSpace{{1, 1}, {3, 2}, {4, 5}, {4, 2}, {4, 1}, {5, 2}}
+	done := make(chan struct{})
+	go func() {
+		for i, x := range diskSpaces {
+			node := fmt.Sprintf("N-%d", i+1)
+			st.delegate.Set(node, x)
+		}
+		done <- struct{}{}
+	}()
+
+	_, ok := st.delegate.Get("X")
+	assert.False(t, ok)
+
+	for i, x := range diskSpaces {
+		space, ok := st.delegate.Get(fmt.Sprintf("N-%d", i+1))
+		if ok {
+			assert.Equal(t, x, space)
+		}
+	}
+	<-done
+	for i, x := range diskSpaces {
+		node := fmt.Sprintf("N-%d", i+1)
+		space, ok := st.delegate.Get(node)
+		assert.Equal(t, x, space)
+		assert.True(t, ok)
+		st.delegate.Delete(node)
+
+	}
+	assert.Empty(t, st.delegate.DiskUsage)
+}
--- usecases/cluster/state.go
+++ usecases/cluster/state.go
@@ -15,8 +15,6 @@ import (
 	"fmt"
 	"net"
 	"strings"
-	"sync"
-	"syscall"
 
 	"github.com/hashicorp/memberlist"
 	"github.com/pkg/errors"
@@ -170,63 +168,4 @@ func (s *State) SchemaSyncIgnored() bool {
 
 func (s *State) DiskSpace(node string) (DiskSpace, bool) {
 	return s.delegate.Get(node)
-}
-
-type DiskSpace struct {
-	Total     uint64
-	Available uint64
-}
-type delegate struct {
-	dataPath string
-	sync.Mutex
-	DiskUsage map[string]DiskSpace
-}
-
-func (*delegate) NodeMeta(limit int) (meta []byte) {
-	return nil
-}
-
-func (d *delegate) LocalState(join bool) []byte {
-	return nil
-}
-
-func (d *delegate) MergeRemoteState(buf []byte, join bool) {
-}
-
-func (d *delegate) NotifyMsg(data []byte) {}
-
-func (d *delegate) GetBroadcasts(overhead, limit int) [][]byte {
-	return nil
-}
-
-func (d *delegate) Get(node string) (DiskSpace, bool) {
-	d.Lock()
-	defer d.Unlock()
-	x, ok := d.DiskUsage[node]
-	return x, ok
-}
-
-func (d *delegate) Set(node string, x DiskSpace) {
-	d.Lock()
-	defer d.Unlock()
-	d.DiskUsage[node] = x
-}
-
-func (d *delegate) Delete(node string) {
-	// TODO clean up entries for node leaving the cluster
-	d.Lock()
-	defer d.Unlock()
-	delete(d.DiskUsage, node)
-}
-
-func diskSpace(path string) (DiskSpace, error) {
-	fs := syscall.Statfs_t{}
-	err := syscall.Statfs(path, &fs)
-	if err != nil {
-		return DiskSpace{}, err
-	}
-	return DiskSpace{
-		Total:     fs.Blocks * uint64(fs.Bsize),
-		Available: fs.Bavail * uint64(fs.Bsize),
-	}, nil
 }
