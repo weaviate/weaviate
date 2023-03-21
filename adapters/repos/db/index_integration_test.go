2cb119238d1666a15b1ff44fa773eb17241027d9
Parker Duckworth <parkerduckworth@gmail.com>
2023-03-21 09:15:47 -0500

    add additional tests

--- adapters/repos/db/index_integration_test.go
+++ adapters/repos/db/index_integration_test.go
@@ -51,18 +51,6 @@ func TestIndex_DropIndex(t *testing.T) {
 	assert.Equal(t, 0, len(indexFilesAfterDelete))
 }
 
-func TestIndex_DropIndexReadOnly(t *testing.T) {
-	ctx := testCtx()
-	class := &models.Class{Class: "deletetest"}
-	shard, index := testShard(t, ctx, class.Class)
-
-	err := index.updateShardStatus(ctx, shard.name, storagestate.StatusReadOnly.String())
-	require.Nil(t, err)
-
-	err = index.drop()
-	require.Nil(t, err)
-}
-
 func TestIndex_DropEmptyAndRecreateEmptyIndex(t *testing.T) {
 	dirName := t.TempDir()
 	class := &models.Class{Class: "deletetest"}
@@ -232,6 +220,91 @@ func TestIndex_DropWithDataAndRecreateWithDataIndex(t *testing.T) {
 	assert.Empty(t, afterRecreateObj2)
 	assert.NotNil(t, afterRecreateAndInsertObj1)
 	assert.NotNil(t, afterRecreateAndInsertObj2)
+}
+
+func TestIndex_DropReadOnlyEmptyIndex(t *testing.T) {
+	ctx := testCtx()
+	class := &models.Class{Class: "deletetest"}
+	shard, index := testShard(t, ctx, class.Class)
+
+	err := index.updateShardStatus(ctx, shard.name, storagestate.StatusReadOnly.String())
+	require.Nil(t, err)
+
+	err = index.drop()
+	require.Nil(t, err)
+}
+
+func TestIndex_DropReadOnlyIndexWithData(t *testing.T) {
+	ctx := testCtx()
+	dirName := t.TempDir()
+	logger, _ := test.NewNullLogger()
+	class := &models.Class{
+		Class: "deletetest",
+		Properties: []*models.Property{
+			{
+				Name:     "name",
+				DataType: []string{"string"},
+			},
+		},
+		InvertedIndexConfig: &models.InvertedIndexConfig{},
+	}
+	fakeSchema := schema.Schema{
+		Objects: &models.Schema{
+			Classes: []*models.Class{
+				class,
+			},
+		},
+	}
+
+	shardState := singleShardState()
+	index, err := NewIndex(ctx, IndexConfig{
+		RootPath:  dirName,
+		ClassName: schema.ClassName(class.Class),
+	}, shardState, inverted.ConfigFromModel(class.InvertedIndexConfig),
+		hnsw.NewDefaultUserConfig(), &fakeSchemaGetter{
+			schema: fakeSchema, shardState: shardState,
+		}, nil, logger, nil, nil, nil, nil, nil, nil)
+	require.Nil(t, err)
+
+	productsIds := []strfmt.UUID{
+		"1295c052-263d-4aae-99dd-920c5a370d06",
+		"1295c052-263d-4aae-99dd-920c5a370d07",
+	}
+
+	products := []map[string]interface{}{
+		{"name": "one"},
+		{"name": "two"},
+	}
+
+	err = index.addUUIDProperty(ctx)
+	require.Nil(t, err)
+
+	err = index.addProperty(ctx, &models.Property{
+		Name:     "name",
+		DataType: []string{"string"},
+	})
+	require.Nil(t, err)
+
+	for i, p := range products {
+		product := models.Object{
+			Class:      class.Class,
+			ID:         productsIds[i],
+			Properties: p,
+		}
+
+		err := index.putObject(ctx, storobj.FromObject(
+			&product, []float32{0.1, 0.2, 0.01, 0.2}), nil)
+		require.Nil(t, err)
+	}
+
+	// set all shards to readonly
+	for _, shard := range index.Shards {
+		err = shard.updateStatus(storagestate.StatusReadOnly.String())
+		require.Nil(t, err)
+	}
+
+	err = index.drop()
+	require.Nil(t, err)
 }
 
 func emptyIdx(t *testing.T, rootDir string, class *models.Class) *Index {
