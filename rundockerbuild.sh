17dc35ff157d9b1da71d6db65e366c04d5618f45
George Williams <george.williams@gmail.com>
2023-03-22 22:10:15 +0000

    added dislaimer

--- rundockerbuild.sh
+++ rundockerbuild.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# This script is for running the docker build for GSI's weaivate container
+# Note:  We may end up removing this and re-factoring the docker build if/when we merge with main weaviate
+
 DOCKER_BUILDKIT=0 docker build -f Dockerfile -t gsi/weaviate .
