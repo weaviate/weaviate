5920e9cfaedd81ce2d7e99b270eee1d75ec65d24
Jeremy Price <donomii@gmail.com>
2023-03-23 12:28:14 +0100

    Add heap library

---
+++ adapters/repos/db/inverted/heap.go
@@ -0,0 +1,72 @@
+package inverted
+
+type Heap[T any] struct {
+	 Data []T
+	comp func(a, b T) bool
+}
+
+func NewHeap[T any](comp func(a, b T) bool) *Heap[T] {
+	return &Heap[T]{comp: comp}
+}
+
+func (h *Heap[T]) Len() int { return len(h.Data) }
+
+func (h *Heap[T]) Push(v T) {
+	h. Data = append(h. Data, v)
+	h.up(h.Len() - 1)
+}
+
+func (h *Heap[T]) Pop() T {
+	n := h.Len() - 1
+	if n > 0 {
+		h.swap(0, n)
+		h.down()
+	}
+	v := h. Data[n]
+	h. Data = h. Data[0:n]
+	return v
+}
+
+func (h *Heap[T]) Peek() T {
+	return h. Data[0]
+}
+
+func (h *Heap[T]) swap(i, j int) {
+	h. Data[i], h. Data[j] = h. Data[j], h. Data[i]
+}
+
+func (h *Heap[T]) up(jj int) {
+	for {
+		i := parent(jj)
+		if i == jj || !h.comp(h. Data[jj], h. Data[i]) {
+			break
+		}
+		h.swap(i, jj)
+		jj = i
+	}
+}
+
+func (h *Heap[T]) down() {
+	n := h.Len() - 1
+	i1 := 0
+	for {
+		j1 := left(i1)
+		if j1 >= n || j1 < 0 {
+			break
+		}
+		j := j1
+		j2 := right(i1)
+		if j2 < n && h.comp(h. Data[j2], h. Data[j1]) {
+			j = j2
+		}
+		if !h.comp(h. Data[j], h. Data[i1]) {
+			break
+		}
+		h.swap(i1, j)
+		i1 = j
+	}
+}
+
+func parent(i int) int { return (i - 1) / 2 }
+func left(i int) int   { return (i * 2) + 1 }
+func right(i int) int  { return left(i) + 1 }
