539171d243eb67439d35efadbe83f04d2fde8d2d
Parker Duckworth <31421773+parkerduckworth@users.noreply.github.com>
2023-03-24 10:20:34 -0500

    Update ref2vec tests to reflect all centroid calc scenarios (#2807)

--- test/modules/ref2vec-centroid/centroid_test.go
+++ test/modules/ref2vec-centroid/centroid_test.go
@@ -160,6 +160,51 @@ func TestCentroid(t *testing.T) {
 		res = helper.AssertGetObject(t, article.Class, article.ID, "vector")
 		assert.Nil(t, res.Vector)
 	})
+
+	t.Run("batch create objects", func(t *testing.T) {
+		ref1 := &models.SingleRef{Beacon: newBeacon(para1.Class, para1.ID)}
+		ref2 := &models.SingleRef{Beacon: newBeacon(para2.Class, para2.ID)}
+
+		article1 := articles.NewArticle().
+			WithTitle("Popularity of the Nissan RB26DETT").
+			WithReferences(ref1, ref2)
+		defer helper.DeleteObject(t, article1.Object())
+
+		article2 := articles.NewArticle().
+			WithTitle("A Nissan RB Origin Story").
+			WithReferences(ref1, ref2)
+		defer helper.DeleteObject(t, article2.Object())
+
+		batch := []*models.Object{article1.Object(), article2.Object()}
+		helper.CreateObjectsBatch(t, batch)
+
+		res := helper.AssertGetObject(t, article1.Class, article1.ID, "vector")
+		assert.EqualValues(t, []float32{3, 5, 7}, res.Vector)
+		res = helper.AssertGetObject(t, article2.Class, article2.ID, "vector")
+		assert.EqualValues(t, []float32{3, 5, 7}, res.Vector)
+	})
+
+	// TODO: Uncomment when batch refs supports centroid re-calc
+	//t.Run("batch create references", func(t *testing.T) {
+	//	article := articles.NewArticle().
+	//		WithTitle("Popularity of the Nissan RB26DETT")
+	//	defer helper.DeleteObject(t, article.Object())
+	//
+	//	refs := []*models.BatchReference{
+	//		{
+	//			From: strfmt.URI(crossref.NewSource("Article", "hasParagraphs", article.ID).String()),
+	//			To:   strfmt.URI(crossref.NewLocalhost("Paragraph", para1.ID).String()),
+	//		},
+	//		{
+	//			From: strfmt.URI(crossref.NewSource("Article", "hasParagraphs", article.ID).String()),
+	//			To:   strfmt.URI(crossref.NewLocalhost("Paragraph", para2.ID).String()),
+	//		},
+	//	}
+	//
+	//	helper.AddReferences(t, refs)
+	//	res := helper.AssertGetObject(t, article.Class, article.ID, "vector")
+	//	assert.EqualValues(t, []float32{3, 5, 7}, res.Vector)
+	//})
 }
 
 func newBeacon(className string, id strfmt.UUID) strfmt.URI {
