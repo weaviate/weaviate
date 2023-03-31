09ed717d5094e4ca43047356b13347298c1ff59f
George Williams <george.williams@gmail.com>
2023-03-31 10:56:56 -0700

    fixed comment license header

--- gsi/tests/weaviate-gemini-sanity.py
+++ gsi/tests/weaviate-gemini-sanity.py
@@ -1,13 +1,13 @@
-//                           _       _
-// __      _____  __ ___   ___  __ _| |_ ___
-// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
-//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
-//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
-//
-//  Copyright © 2016 - 2023 Weaviate B.V. All rights reserved.
-//
-//  CONTACT: hello@weaviate.io
-//
+#                           _       _
+# __      _____  __ ___   ___  __ _| |_ ___
+# \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+#  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+#   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+#
+#  Copyright © 2016 - 2023 Weaviate B.V. All rights reserved.
+#
+#  CONTACT: hello@weaviate.io
+#
 
 import json
 import sys
