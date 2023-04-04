562020ea7af573547def21ed502191f7cbb90f27
jacobenson <jbenson116@gmail.com>
2023-04-04 23:23:35 +0000

    adding gemini_vision.py for full upload/query removing batch... .py files updated docker-compose.yml

--- gsi/examples/vision/object_count.py
+++
@@ -1,4 +0,0 @@
-import weaviate
-client = weaviate.Client('http://localhost:8080')
-print(client.query.aggregate('Fashion').with_meta_count().do())
-print(client.query.aggregate('FashionBatchless').with_meta_count().do())
