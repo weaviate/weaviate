d36b9dd0aeb23d1a6a7b9c647508d0faa920563c
Jacob Benson <jbenson116@gmail.com>
2023-04-05 17:31:14 +0000

    adding back dockerfile and rundockerbuild script

---
+++ gsi/rundockerbuild.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This script is for running the docker build for GSI's weaivate container
+
+set -e
+set -x
+
+cd .. && DOCKER_BUILDKIT=0 docker build -f Dockerfile -t gsi/weaviate . 
