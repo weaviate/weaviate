7b03c9109c46427c359dec47c9fe081ebf0366fb
deegiimurun <deegiimurun@gmail.com>
2023-04-19 15:59:14 +0800

    multiple nodes available

--- tools/dev/dev.go
+++ tools/dev/dev.go
@@ -14,6 +14,7 @@ package main
 import (
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 
 	"github.com/jessevdk/go-flags"
@@ -21,22 +22,31 @@ import (
 
 func main() {
 	var opts struct {
-		Node           int    `long:"node" default:"1"`
-		Delete         bool   `long:"delete"`
-		Restart        bool   `long:"restart"`
-		Keycloak       bool   `long:"keycloak"`
-		Transformers   string `long:"transformers" choice:"t2v-pq" choice:"t2v" choice:"ctx"`
-		Contextionary  bool   `long:"contextionary"`
-		QNA            bool   `long:"qna"`
-		Sum            bool   `long:"sum"`
-		Image          bool   `long:"image"`
-		Ner            bool   `long:"ner"`
-		TextSpellcheck bool   `long:"spellcheck"`
-		Multi2vecClip  bool   `long:"clip"`
-		Prometheus     bool   `long:"prometheus"`
-		S3             bool   `long:"s3" `
-		GCS            bool   `long:"gcs"`
-		Azure          bool   `long:"azure"`
+		Node                   int    `long:"node" default:"1"`
+		Delete                 bool   `long:"delete"`
+		Restart                bool   `long:"restart"`
+		Keycloak               bool   `long:"keycloak"`
+		Transformers           string `long:"transformers" choice:"t2v-pq" choice:"t2v" choice:"ctx"`
+		Contextionary          bool   `long:"contextionary"`
+		QNA                    bool   `long:"qna"`
+		Sum                    bool   `long:"sum"`
+		Image                  bool   `long:"image"`
+		Ner                    bool   `long:"ner"`
+		TextSpellcheck         bool   `long:"spellcheck"`
+		Multi2vecClip          bool   `long:"clip"`
+		Prometheus             bool   `long:"prometheus"`
+		S3                     bool   `long:"s3" `
+		GCS                    bool   `long:"gcs"`
+		Azure                  bool   `long:"azure"`
+		LogLevel               string `long:"log-level" default:"debug"`
+		LogFormat              string `long:"log-format" default:"text"`
+		PrometheusMonitoring   string `long:"prometheus-monitor" default:"false"`
+		GoBlockProfileRate     int    `long:"block-rate" default:"20"`
+		GoMutexProfileFraction int    `long:"mutex-fraction" default:"20"`
+		PersistentDataPath     string `long:"persistent-path" default:"data"`
+		Origin                 string `long:"origin" default:"localhost:8080"`
+		QueryDefaultLimit      string `long:"query-limit" default:"20"`
+		QueryMaxResults        string `long:"query-max" default:"10000"`
 	}
 
 	_, err := flags.Parse(&opts)
@@ -104,32 +114,43 @@ func main() {
 
 	defaultEnvArgs := []string{
 		"GO111MODULE=on",
-		"LOG_LEVEL=debug",
-		"LOG_FORMAT=text",
-		"PERSISTENCE_DATA_PATH=./data",
+		"LOG_LEVEL=" + opts.LogLevel,
+		"LOG_FORMAT=" + opts.LogFormat,
+		"PROMETHEUS_MONITORING_ENABLED=" + opts.PrometheusMonitoring,
+		"GO_BLOCK_PROFILE_RATE=" + strconv.Itoa(opts.GoBlockProfileRate),
+		"GO_MUTEX_PROFILE_FRACTION=" + strconv.Itoa(opts.GoMutexProfileFraction),
+		"PERSISTENCE_DATA_PATH=./" + opts.PersistentDataPath,
+		"ORIGIN=" + opts.Origin,
+		"QUERY_DEFAULTS_LIMIT=" + opts.QueryDefaultLimit,
+		"QUERY_MAXIMUM_RESULTS=" + opts.QueryMaxResults,
+		"TRACK_VECTOR_DIMENSIONS=true",
 	}
 
-	if opts.Node == 1 {
-		envArgs := []string{
-			"CONTEXTIONARY_URL=localhost:9999",
-			"AUTHENTICATION_ANONYMOUS_ACCESS_ENABLED=true",
-			"DEFAULT_VECTORIZER_MODULE=text2vec-openai",
-			"BACKUP_FILESYSTEM_PATH=" + path + "/backups",
-			"ENABLE_MODULES=text2vec-backup-filesystem,text2vec-openai",
-			"CLUSTER_GOSSIP_BIND_PORT=7100",
-			"CLUSTER_DATA_BIND_PORT=7101",
-		}
-		execCommand(append(defaultEnvArgs, envArgs...), "go", "run",
-			"-ldflags", "-X github.com/weaviate/weaviate/usecases/config.GitHash="+string(strings.Trim(string(gitHash), "\n")),
-			"./cmd/weaviate-server",
-			"--scheme", "http",
-			"--host", "127.0.0.1",
-			"--port", "8080",
-			"--read-timeout", "600s",
-			"--write-timeout", "600s",
-		)
-	} else {
+	weaviatePort := 8080 + opts.Node - 1
+	gossipPort := 7100 + (opts.Node-1)*2
+	clusterDataBindPort := gossipPort + 1
+
+	envArgs := []string{
+		"CONTEXTIONARY_URL=localhost:9999",
+		"AUTHENTICATION_ANONYMOUS_ACCESS_ENABLED=true",
+		"DEFAULT_VECTORIZER_MODULE=text2vec-contextionary",
+		"BACKUP_FILESYSTEM_PATH=" + path + "/backups" + "/node" + strconv.Itoa(opts.Node),
+		"ENABLE_MODULES=backup-filesystem,text2vec-contextionary",
+		"CLUSTER_GOSSIP_BIND_PORT=" + strconv.Itoa(gossipPort),
+		"CLUSTER_DATA_BIND_PORT=" + strconv.Itoa(clusterDataBindPort),
+		"CLUSTER_HOSTNAME=node" + strconv.Itoa(opts.Node),
+		"CLUSTER_JOIN=localhost:7100",
 	}
+
+	execCommand(append(defaultEnvArgs, envArgs...), "go", "run",
+		"-ldflags", "-X github.com/weaviate/weaviate/usecases/config.GitHash="+string(strings.Trim(string(gitHash), "\n")),
+		"./cmd/weaviate-server",
+		"--scheme", "http",
+		"--host", "127.0.0.1",
+		"--port", strconv.Itoa(weaviatePort),
+		"--read-timeout", "600s",
+		"--write-timeout", "600s",
+	)
 }
 
 func execCommand(envVars []string, name string, arg ...string) {
