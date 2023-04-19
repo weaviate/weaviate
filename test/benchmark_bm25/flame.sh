28c3ba32b12f1ed7e54c9bcc65389160596314ee
Jeremy Price <donomii@gmail.com>
2023-04-19 12:09:31 +0200

    .

--- test/benchmark_bm25/flame.sh
+++ test/benchmark_bm25/flame.sh
@@ -13,10 +13,10 @@ NAME=$1
 #go tool pprof -png http://localhost:6060/debug/pprof/allocs > "${NAME}_allocs.png" &
 
 #Write profiles to file instead of pngs
-curl localhost:6060/debug/pprof/trace\?seconds=30 > "${NAME}_trace.out" &
-curl http://127.0.0.1:6060/debug/pprof/allocs\?seconds=30 > "${NAME}_allocs.prof" &
-curl http://127.0.0.1:6060/debug/pprof/heap\?seconds=30 > "${NAME}_heap.prof" &
-curl http://127.0.0.1:6060/debug/pprof/profile\?seconds=30 > "${NAME}_profile.prof" &
+#curl localhost:6060/debug/pprof/trace\?seconds=30 > "${NAME}_trace.out" 
+curl http://127.0.0.1:6060/debug/pprof/allocs\?seconds=30 > "${NAME}_allocs.prof" 
+curl http://127.0.0.1:6060/debug/pprof/heap\?seconds=30 > "${NAME}_heap.prof" 
+curl http://127.0.0.1:6060/debug/pprof/profile\?seconds=30 > "${NAME}_profile.prof" 
 
 #go tool pprof -raw -output="${NAME}_cpu.txt" 'http://localhost:6060/debug/pprof/profile?seconds=30'
 
