c5eb3818f99fe4595e881edcd737e92f339c4b5e
Dirk Kulawiak <dirk@semi.technology>
2023-04-27 12:44:52 +0200

    Add autocut to BM25

--- test/acceptance_with_go_client/bm25_test.go
+++ test/acceptance_with_go_client/bm25_test.go
@@ -13,6 +13,7 @@ package acceptance_with_go_client
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -29,11 +30,53 @@ var paragraphs = []string{
 	"this has nothing to do with the rest",
 }
 
+var (
+	TRUE = true
+	ctx  = context.Background()
+)
+
+func AddClassAndObjects(t *testing.T, className string, datatype string, c *client.Client) {
+	class := &models.Class{
+		Class: className,
+		Properties: []*models.Property{
+			{Name: "contents", DataType: []string{datatype}, Tokenization: "word", IndexInverted: &TRUE},
+			{Name: "num", DataType: []string{"int"}},
+		},
+		InvertedIndexConfig: &models.InvertedIndexConfig{Bm25: &models.BM25Config{K1: 1.2, B: 0.75}},
+		Vectorizer:          "none",
+	}
+	require.Nil(t, c.Schema().ClassCreator().WithClass(class).Do(ctx))
+
+	creator := c.Data().Creator()
+	_, err := creator.WithClassName(className).WithProperties(
+		map[string]interface{}{"contents": []string{"nice", "what a rain day"}, "num": 0}).Do(ctx)
+	require.Nil(t, err)
+	_, err = creator.WithClassName(className).WithProperties(
+		map[string]interface{}{"contents": []string{"rain", "snow and sun at once? nice"}, "num": 1}).Do(ctx)
+	require.Nil(t, err)
+	_, err = creator.WithClassName(className).WithProperties(
+		map[string]interface{}{"contents": []string{
+			"super long text to get the score down",
+			"snow and sun at the same time? How nice",
+			"long text without any meaning",
+			"just ignore this",
+			"this too, it doesn't matter",
+		}, "num": 2}).Do(ctx)
+	_, err = creator.WithClassName(className).WithProperties(
+		map[string]interface{}{"contents": []string{
+			"super long text to get the score down",
+			"rain is necessary",
+			"long text without any meaning",
+			"just ignore this",
+			"this too, it doesn't matter",
+		}, "num": 3}).Do(ctx)
+	require.Nil(t, err)
+}
+
 func TestBm25(t *testing.T) {
-	ctx := context.Background()
 	c := client.New(client.Config{Scheme: "http", Host: "localhost:8080"})
 	c.Schema().AllDeleter().Do(ctx)
-	truePointer := true
+	className := "Paragraph15845"
 
 	cases := []struct{ datatype string }{
 		{datatype: string(schema.DataTypeTextArray)},
@@ -42,32 +85,41 @@ func TestBm25(t *testing.T) {
 	}
 	for _, tt := range cases {
 		t.Run("arrays "+tt.datatype, func(t *testing.T) {
-			className := "Paragraph15845"
-			class := &models.Class{
-				Class: className,
-				Properties: []*models.Property{
-					{Name: "contents", DataType: []string{tt.datatype}, Tokenization: "word", IndexInverted: &truePointer},
-					{Name: "num", DataType: []string{"int"}},
-				},
-				InvertedIndexConfig: &models.InvertedIndexConfig{Bm25: &models.BM25Config{K1: 1.2, B: 0.75}},
-				Vectorizer:          "none",
-			}
-			require.Nil(t, c.Schema().ClassCreator().WithClass(class).Do(ctx))
+			AddClassAndObjects(t, className, tt.datatype, c)
 			defer c.Schema().ClassDeleter().WithClassName(className).Do(ctx)
 
-			creator := c.Data().Creator()
-			_, err := creator.WithClassName(className).WithProperties(
-				map[string]interface{}{"contents": []string{"what a nice day", "what a rainy day"}, "num": 0}).Do(ctx)
-			require.Nil(t, err)
-			_, err = creator.WithClassName(className).WithProperties(
-				map[string]interface{}{"contents": []string{"rain all day", "snow and sun at the same time? How nice"}, "num": 1}).Do(ctx)
-			require.Nil(t, err)
-
-			builder := c.GraphQL().Bm25ArgBuilder().WithQuery("nice").WithProperties("contents")
+			builder := c.GraphQL().Bm25ArgBuilder().WithQuery("nice rain").WithProperties("contents")
 			results, err := c.GraphQL().Get().WithClassName(className).WithBM25(builder).WithFields(graphql.Field{Name: "num"}).Do(ctx)
 			require.Nil(t, err)
 			result := results.Data["Get"].(map[string]interface{})[className].([]interface{})
-			require.Len(t, result, 2)
+			require.Len(t, result, 4)
+			require.Equal(t, 0., result[0].(map[string]interface{})["num"])
+			require.Equal(t, 1., result[1].(map[string]interface{})["num"])
+		})
+	}
+}
+
+func TestBm25Autocut(t *testing.T) {
+	ctx := context.Background()
+	c := client.New(client.Config{Scheme: "http", Host: "localhost:8080"})
+	c.Schema().AllDeleter().Do(ctx)
+	className := "Paragraph453745"
+
+	AddClassAndObjects(t, className, string(schema.DataTypeTextArray), c)
+	defer c.Schema().ClassDeleter().WithClassName(className).Do(ctx)
+
+	cases := []struct {
+		autocut    int
+		numResults int
+	}{
+		{autocut: 1, numResults: 2}, {autocut: 2, numResults: 4}, {autocut: -1, numResults: 4 /*disabled*/},
+	}
+	for _, tt := range cases {
+		t.Run("autocut "+fmt.Sprint(tt.autocut), func(t *testing.T) {
+			results, err := c.GraphQL().Raw().WithQuery(fmt.Sprintf("{Get{%s(bm25:{query:\"rain nice\", autocut: %d, properties: [\"contents\"]}){num}}}", className, tt.autocut)).Do(ctx)
+			require.Nil(t, err)
+			result := results.Data["Get"].(map[string]interface{})[className].([]interface{})
+			require.Len(t, result, tt.numResults)
 			require.Equal(t, 0., result[0].(map[string]interface{})["num"])
 			require.Equal(t, 1., result[1].(map[string]interface{})["num"])
 		})
