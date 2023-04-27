669b55de7528b6909aed3404cec598d1a71bef1e
Jeremy Price <donomii@gmail.com>
2023-04-27 18:40:12 +0200

    Just strip spaces and punctuation

--- adapters/repos/db/helpers/tokenizer.go
+++ adapters/repos/db/helpers/tokenizer.go
@@ -89,8 +89,10 @@ func tokenizeWord(in string) []string {
 
 // tokenizetrigram splits on any non-alphanumerical and lowercases the words, joins them together, then groups them into trigrams
 func tokenizetrigram(in string) []string {
-	terms := tokenizeWord(in)
-	inputString := strings.Join(terms, "")
+	//Strip whitespace and punctuation from the input string
+	inputString := strings.Join(strings.FieldsFunc(in, func(r rune) bool {
+		return !unicode.IsLetter(r) && !unicode.IsNumber(r)
+	}), "")
 	var trigrams []string
 	for i := 0; i < len(inputString)-2; i++ {
 		trigrams = append(trigrams, inputString[i:i+3])
