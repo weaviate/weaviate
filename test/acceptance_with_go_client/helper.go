53751dc1b28e9856b448a9a989587ab7f8cb10c6
Dirk Kulawiak <dirk@semi.technology>
2023-04-28 15:23:27 +0200

    Add test for correct error message

---
+++ test/acceptance_with_go_client/helper.go
@@ -0,0 +1,22 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2023 Weaviate B.V. All rights reserved.
+//
+//  CONTACT: hello@weaviate.io
+//
+
+package acceptance_with_go_client
+
+func truePointer() *bool {
+	t := true
+	return &t
+}
+
+func falsePointer() *bool {
+	t := false
+	return &t
+}
