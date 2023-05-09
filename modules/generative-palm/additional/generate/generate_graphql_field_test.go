ead470c758d12993dbdd2bee0daf9269d8c9c305
Marcin Antas <antas.marcin@gmail.com>
2023-05-09 14:23:10 +0200

    Google PaLM new modules: text2vec-palm and generative-palm

---
+++ modules/generative-palm/additional/generate/generate_graphql_field_test.go
@@ -0,0 +1,42 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2023 Weaviate B.V. All rights reserved.
+//
+//  CONTACT: hello@weaviate.io
+//
+
+package generate
+
+import (
+	"testing"
+)
+
+func TestGenerateField(t *testing.T) {
+	//t.Run("should generate generate argument properly", func(t *testing.T) {
+	//	// given
+	//	answerProvider := &GenerateProvider{}
+	//	classname := "Class"
+	//
+	//	// when
+	//	answer := answerProvider.additionalGenerateField(classname)
+	//
+	//	// then
+	//	// the built graphQL field needs to support this structure:
+	//	// Type: {
+	//	//   generate: {
+	//	//     result: "result",
+	//	//   }
+	//	// }
+	//	assert.NotNil(t, answer)
+	//	assert.Equal(t, "ClassAdditionalGenerate", answer.Type.Name())
+	//	assert.NotNil(t, answer.Type)
+	//	answerObject, answerObjectOK := answer.Type.(*graphql.Object)
+	//	assert.True(t, answerObjectOK)
+	//	assert.Equal(t, 1, len(answerObject.Fields()))
+	//	assert.NotNil(t, answerObject.Fields()["result"])
+	//})
+}
