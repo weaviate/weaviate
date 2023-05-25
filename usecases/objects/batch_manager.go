f6d795526a77106eef88ea6114d5b7b930620a3f
Parker Duckworth <parkerduckworth@gmail.com>
2023-05-25 13:24:02 -0500

    working batch delete tenant objects

--- usecases/objects/batch_manager.go
+++ usecases/objects/batch_manager.go
@@ -42,7 +42,8 @@ type BatchVectorRepo interface {
 type batchRepoNew interface {
 	BatchPutObjects(ctx context.Context, objects BatchObjects,
 		repl *additional.ReplicationProperties) (BatchObjects, error)
-	BatchDeleteObjects(ctx context.Context, params BatchDeleteParams, repl *additional.ReplicationProperties) (BatchDeleteResult, error)
+	BatchDeleteObjects(ctx context.Context, params BatchDeleteParams,
+		repl *additional.ReplicationProperties) (BatchDeleteResult, error)
 	AddBatchReferences(ctx context.Context, references BatchReferences,
 		repl *additional.ReplicationProperties) (BatchReferences, error)
 }
