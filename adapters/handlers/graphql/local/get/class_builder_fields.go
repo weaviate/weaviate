bcdef56f0a0b5a98dbee087d7913a6fb4c6a8f93
Parker Duckworth <31421773+parkerduckworth@users.noreply.github.com>
2023-06-07 15:25:32 -0500

    Prevent hybrid and bm25 search from being used with sort (#2921)

--- adapters/handlers/graphql/local/get/class_builder_fields.go
+++ adapters/handlers/graphql/local/get/class_builder_fields.go
@@ -380,6 +380,9 @@ func (r *resolver) makeResolveGetClass(className string) graphql.FieldResolveFn 
 		// extracts bm25 (sparseSearch) from the query
 		var keywordRankingParams *searchparams.KeywordRanking
 		if bm25, ok := p.Args["bm25"]; ok {
+			if len(sort) > 0 {
+				return nil, fmt.Errorf("bm25 search is not compatible with sort")
+			}
 			p := common_filters.ExtractBM25(bm25.(map[string]interface{}), addlProps.ExplainScore)
 			keywordRankingParams = &p
 		}
@@ -389,6 +392,9 @@ func (r *resolver) makeResolveGetClass(className string) graphql.FieldResolveFn 
 		// refactored
 		var hybridParams *searchparams.HybridSearch
 		if hybrid, ok := p.Args["hybrid"]; ok {
+			if len(sort) > 0 {
+				return nil, fmt.Errorf("hybrid search is not compatible with sort")
+			}
 			p, err := common_filters.ExtractHybridSearch(hybrid.(map[string]interface{}), addlProps.ExplainScore)
 			if err != nil {
 				return nil, fmt.Errorf("failed to extract hybrid params: %w", err)
