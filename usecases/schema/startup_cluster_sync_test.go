fe0dd96b4f2e03e6abdb02ad76b94f5afb427c62
Redouan El Rhazouani <81578195+redouan-rhazouani@users.noreply.github.com>
2023-06-12 13:47:52 +0200

    schema: enhance schema persistence capability to selectively persist schema changes

--- usecases/schema/startup_cluster_sync_test.go
+++ usecases/schema/startup_cluster_sync_test.go
@@ -385,7 +385,10 @@ func newManagerWithClusterAndTx(t *testing.T, clusterState clusterState,
 ) (*Manager, error) {
 	logger, _ := test.NewNullLogger()
 	repo := newFakeRepo()
-	repo.schema = initialSchema
+	if initialSchema == nil {
+		initialSchema = &State{}
+	}
+	repo.schema = *initialSchema
 	sm, err := NewManager(&NilMigrator{}, repo, logger, &fakeAuthorizer{},
 		config.Config{DefaultVectorizerModule: config.VectorizerModuleNone},
 		dummyParseVectorConfig, // only option for now
