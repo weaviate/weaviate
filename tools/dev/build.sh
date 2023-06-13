3260a1ce97aa75387c6e735e93be98ee09ad558f
Dirk Kulawiak <dirk@semi.technology>
2023-06-13 13:04:11 +0200

    Fail script on errors

--- tools/dev/build.sh
+++ tools/dev/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+set -eou pipefail
+
 # run from Weaviate Root
 
 export GIT_HASH=$(git rev-parse --short HEAD)
-goreleaser build --clean
+goreleaser build --clean # add --snapshot to this commandline to build from non-tagged commit or with unclean directory
 
 codesign -f -o runtime --timestamp -s "Developer ID Application: Weaviate B.V. (QUZ8SKLS6R)" dist/weaviate_darwin_all/weaviate
 
