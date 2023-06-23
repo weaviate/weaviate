d900a75f70ba08db26ea871f1bd051bbdb247d93
Parker Duckworth <parkerduckworth@gmail.com>
2023-06-23 18:48:15 -0500

    more efficient conditional

--- usecases/objects/batch_references_add.go
+++ usecases/objects/batch_references_add.go
@@ -113,7 +113,7 @@ func (b *BatchManager) validateReference(ctx context.Context, principal *models.
 		err = joinErrors(errors)
 	}
 
-	if shouldValidateMultiTenantRef(tenantKey, source, target) && err == nil {
+	if err == nil && shouldValidateMultiTenantRef(tenantKey, source, target) {
 		// can only validate multi-tenancy when everything above succeeds
 		err = validateReferenceMultiTenancy(ctx, principal,
 			b.schemaManager, b.vectorRepo, source, target, tenantKey)
