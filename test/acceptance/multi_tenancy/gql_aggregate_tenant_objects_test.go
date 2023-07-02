7f91d7c84857edd49c17e37898e1282ac80355ed
Dirk Kulawiak <dirk@semi.technology>
2023-07-02 09:15:08 +0200

    Fix aggregate with nearObject

--- test/acceptance/multi_tenancy/gql_aggregate_tenant_objects_test.go
+++ test/acceptance/multi_tenancy/gql_aggregate_tenant_objects_test.go
@@ -16,6 +16,9 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/go-openapi/strfmt"
+	"github.com/google/uuid"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/weaviate/weaviate/client/nodes"
@@ -46,40 +49,36 @@ func TestGQLAggregateTenantObjects(t *testing.T) {
 	defer func() {
 		helper.DeleteClass(t, testClass.Class)
 	}()
+	helper.CreateClass(t, &testClass)
 
-	t.Run("setup test data", func(t *testing.T) {
-		t.Run("create class with multi-tenancy enabled", func(t *testing.T) {
-			helper.CreateClass(t, &testClass)
-		})
+	tenants := []*models.Tenant{
+		{Name: tenantName1},
+		{Name: tenantName2},
+	}
+	helper.CreateTenants(t, testClass.Class, tenants)
 
-		t.Run("create tenants", func(t *testing.T) {
-			tenants := []*models.Tenant{
-				{Name: tenantName1},
-				{Name: tenantName2},
-			}
-			helper.CreateTenants(t, testClass.Class, tenants)
-		})
-
-		t.Run("add tenant objects", func(t *testing.T) {
-			batch1 := makeTenantBatch(batchParams{
-				className:  testClass.Class,
-				tenantName: tenantName1,
-				batchSize:  numTenantObjs1,
-			})
-			batch2 := makeTenantBatch(batchParams{
-				className:  testClass.Class,
-				tenantName: tenantName2,
-				batchSize:  numTenantObjs2,
-			})
-
-			helper.CreateObjectsBatch(t, batch1)
-			helper.CreateObjectsBatch(t, batch2)
-		})
+	batch1 := makeTenantBatch(batchParams{
+		className:  testClass.Class,
+		tenantName: tenantName1,
+		batchSize:  numTenantObjs1,
+	})
+	batch2 := makeTenantBatch(batchParams{
+		className:  testClass.Class,
+		tenantName: tenantName2,
+		batchSize:  numTenantObjs2,
 	})
 
+	helper.CreateObjectsBatch(t, batch1)
+	helper.CreateObjectsBatch(t, batch2)
+
 	t.Run("GQL Aggregate tenant objects", func(t *testing.T) {
-		testAggregateTenantSuccess(t, testClass.Class, tenantName1, numTenantObjs1)
-		testAggregateTenantSuccess(t, testClass.Class, tenantName2, numTenantObjs2)
+		testAggregateTenantSuccess(t, testClass.Class, tenantName1, numTenantObjs1, "")
+		testAggregateTenantSuccess(t, testClass.Class, tenantName2, numTenantObjs2, "")
+	})
+
+	t.Run("GQL Aggregate tenant objects near object", func(t *testing.T) {
+		testAggregateTenantSuccess(t, testClass.Class, tenantName1, numTenantObjs1, string(batch1[0].ID))
+		testAggregateTenantSuccess(t, testClass.Class, tenantName2, numTenantObjs2, string(batch2[0].ID))
 	})
 
 	t.Run("Get global tenant objects count", func(t *testing.T) {
@@ -178,6 +177,7 @@ func makeTenantBatch(params batchParams) []*models.Object {
 	batch := make([]*models.Object, params.batchSize)
 	for i := range batch {
 		batch[i] = &models.Object{
+			ID:    strfmt.UUID(uuid.New().String()),
 			Class: params.className,
 			Properties: map[string]interface{}{
 				"name": params.tenantName,
@@ -188,8 +188,13 @@ func makeTenantBatch(params batchParams) []*models.Object {
 	return batch
 }
 
-func testAggregateTenantSuccess(t *testing.T, className, tenantName string, expectedCount int) {
-	query := fmt.Sprintf(`{Aggregate{%s(tenant:%q){meta{count}}}}`, className, tenantName)
+func testAggregateTenantSuccess(t *testing.T, className, tenantName string, expectedCount int, nearObjectId string) {
+	nearObject := ""
+	if nearObjectId != "" {
+		nearObject = fmt.Sprintf(`nearObject: {id: "%s", certainty: 0.4},`, nearObjectId)
+	}
+
+	query := fmt.Sprintf(`{Aggregate{%s(%s,tenant:%q){meta{count}}}}`, className, nearObject, tenantName)
 	resp := graphqlhelper.AssertGraphQL(t, helper.RootAuth, query)
 	result := resp.Get("Aggregate", className).AsSlice()
 	require.Len(t, result, 1)
