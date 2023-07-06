7865a4ad5d52bb742969b907b75df5e34c39f436
etiennedi <etienne@dilocker.de>
2023-07-06 19:57:39 +0200

    prepare release v1.20.0

--- adapters/handlers/rest/embedded_spec.go
+++ adapters/handlers/rest/embedded_spec.go
@@ -48,7 +48,7 @@ func init() {
       "url": "https://github.com/weaviate",
       "email": "hello@weaviate.io"
     },
-    "version": "1.20.0-prealpha"
+    "version": "1.20.0"
   },
   "basePath": "/v1",
   "paths": {
@@ -4681,7 +4681,7 @@ func init() {
       "url": "https://github.com/weaviate",
       "email": "hello@weaviate.io"
     },
-    "version": "1.20.0-prealpha"
+    "version": "1.20.0"
   },
   "basePath": "/v1",
   "paths": {
