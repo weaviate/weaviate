628d4f68f83147ef93df3c9e416fa63a5eb04279
Marcin Antas <antas.marcin@gmail.com>
2023-07-18 16:09:25 +0200

    Fix module's unit tests

--- modules/multi2vec-bind/clients/startup.go
+++ modules/multi2vec-bind/clients/startup.go
@@ -35,7 +35,7 @@ func (v *vectorizer) WaitForStartup(initCtx context.Context,
 			}
 			v.logger.
 				WithField("action", "multi2vec_remote_wait_for_startup").
-				WithError(lastErr).Warnf("multi2vec-clip inference service not ready")
+				WithError(lastErr).Warnf("multi2vec-bind inference service not ready")
 		case <-expired:
 			return errors.Wrapf(lastErr, "init context expired before remote was ready")
 		}
