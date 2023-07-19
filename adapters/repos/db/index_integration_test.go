075484b35fc400c710dd6ff217af44b12425622d
Jeremy Price <donomii@gmail.com>
2023-07-19 17:16:19 +0900

    Correct file count on delete/create shard test

--- adapters/repos/db/index_integration_test.go
+++ adapters/repos/db/index_integration_test.go
@@ -71,9 +71,9 @@ func TestIndex_DropEmptyAndRecreateEmptyIndex(t *testing.T) {
 	indexFilesAfterRecreate, err := getIndexFilenames(dirName, class.Class)
 	require.Nil(t, err)
 
-	assert.Equal(t, 6, len(indexFilesBeforeDelete))
+	assert.Equal(t, 8, len(indexFilesBeforeDelete))
 	assert.Equal(t, 0, len(indexFilesAfterDelete))
-	assert.Equal(t, 6, len(indexFilesAfterRecreate))
+	assert.Equal(t, 8, len(indexFilesAfterRecreate))
 
 	err = index.drop()
 	require.Nil(t, err)
