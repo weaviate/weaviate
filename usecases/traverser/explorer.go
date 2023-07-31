bd3c71122b9225afcaea6576d9b2857ee81a6b1a
Jeremy Price <donomii@gmail.com>
2023-07-31 15:55:56 +0900

    Hybrid oversearch

--- usecases/traverser/explorer.go
+++ usecases/traverser/explorer.go
@@ -32,6 +32,7 @@ import (
 	"github.com/weaviate/weaviate/entities/searchparams"
 	"github.com/weaviate/weaviate/entities/storobj"
 	"github.com/weaviate/weaviate/entities/vectorindex/hnsw"
+	"github.com/weaviate/weaviate/usecases/config"
 	"github.com/weaviate/weaviate/usecases/floatcomp"
 	uc "github.com/weaviate/weaviate/usecases/schema"
 	"github.com/weaviate/weaviate/usecases/traverser/grouper"
@@ -48,6 +49,7 @@ type Explorer struct {
 	schemaGetter     uc.SchemaGetter
 	nearParamsVector *nearParamsVector
 	metrics          explorerMetrics
+	config           config.Config
 }
 
 type explorerMetrics interface {
@@ -97,9 +99,7 @@ type hybridSearcher interface {
 }
 
 // NewExplorer with search and connector repo
-func NewExplorer(searcher objectsSearcher, logger logrus.FieldLogger,
-	modulesProvider ModulesProvider, metrics explorerMetrics,
-) *Explorer {
+func NewExplorer(searcher objectsSearcher, logger logrus.FieldLogger, modulesProvider ModulesProvider, metrics explorerMetrics, conf config.Config) *Explorer {
 	return &Explorer{
 		searcher:         searcher,
 		logger:           logger,
@@ -107,6 +107,7 @@ func NewExplorer(searcher objectsSearcher, logger logrus.FieldLogger,
 		metrics:          metrics,
 		schemaGetter:     nil, // schemaGetter is set later
 		nearParamsVector: newNearParamsVector(modulesProvider, searcher),
+		config:           conf,
 	}
 }
 
@@ -247,6 +248,40 @@ func (e *Explorer) getClassVectorSearch(ctx context.Context,
 	return e.searchResultsToGetResponse(ctx, res, searchVector, params)
 }
 
+func MinInt(ints ...int) int {
+	min := ints[0]
+	for _, i := range ints {
+		if i < min {
+			min = i
+		}
+	}
+	return min
+}
+
+func MaxInt(ints ...int) int {
+	max := ints[0]
+	for _, i := range ints {
+		if i > max {
+			max = i
+		}
+	}
+	return max
+}
+
+func (e *Explorer) CalculateTotalLimit(pagination *filters.Pagination) (int, error) {
+	if pagination == nil {
+		return 0, fmt.Errorf("invalid params, pagination object is nil")
+	}
+
+	if pagination.Limit == -1 {
+		return int(e.config.QueryDefaults.Limit + int64(pagination.Offset)), nil
+	}
+
+	TotalLimit := pagination.Offset + pagination.Limit
+
+	return MinInt(TotalLimit, int(e.config.QueryMaximumResults)), nil
+}
+
 func (e *Explorer) Hybrid(ctx context.Context, params dto.GetParams) ([]search.Result, error) {
 	sparseSearch := func() ([]*storobj.Object, []float32, error) {
 		params.KeywordRanking = &searchparams.KeywordRanking{
@@ -255,9 +290,29 @@ func (e *Explorer) Hybrid(ctx context.Context, params dto.GetParams) ([]search.R
 			Properties: params.HybridSearch.Properties,
 		}
 
+		if params.Pagination == nil {
+			return nil, nil, fmt.Errorf("invalid params, pagination object is nil")
+		}
+
+		TotalLimit, err := e.CalculateTotalLimit(params.Pagination)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		enforcedMin := MaxInt(params.Pagination.Offset+100, TotalLimit)
+
+		oldLimit := params.Pagination.Limit
+		params.Pagination.Limit = enforcedMin - params.Pagination.Offset
+
 		res, dists, err := e.searcher.SparseObjectSearch(ctx, params)
 		if err != nil {
 			return nil, nil, err
+		}
+		params.Pagination.Limit = oldLimit
+
+		if len(res) > TotalLimit {
+			res = res[:TotalLimit]
+			dists = dists[:TotalLimit]
 		}
 
 		return res, dists, nil
@@ -287,15 +342,12 @@ func (e *Explorer) Hybrid(ctx context.Context, params dto.GetParams) ([]search.R
 		return res, nil
 	}
 
-	h := hybrid.NewSearcher(&hybrid.Params{
+	res, err := hybrid.Search(ctx, &hybrid.Params{
 		HybridSearch: params.HybridSearch,
 		Keyword:      params.KeywordRanking,
 		Class:        params.ClassName,
 		Autocut:      params.Pagination.Autocut,
-	}, e.logger, sparseSearch, denseSearch,
-		postProcess, e.modulesProvider)
-
-	res, err := h.Search(ctx)
+	}, e.logger, sparseSearch, denseSearch, postProcess, e.modulesProvider)
 	if err != nil {
 		return nil, err
 	}
