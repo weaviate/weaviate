2828e3b2c4eb328623459557ad420ad10ce1fa55
Julia Kowalik <priv.julia.kowalik@gmail.com>
2023-08-03 15:37:23 +0200

    Add copyright notices

--- modules/text2vec-kserve/utils/join_error.go
+++ modules/text2vec-kserve/utils/join_error.go
@@ -1,3 +1,14 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2023 Weaviate B.V. All rights reserved.
+//
+//  CONTACT: hello@weaviate.io
+//
+
 // Copyright 2022 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
