b0d006379a1a9e5e2cf027d6e6c3905f90d5e498
etiennedi <etienne@dilocker.de>
2023-08-14 11:53:52 +0200

    prepare release v1.21.0-rc.1

--- adapters/handlers/rest/doc.go
+++ adapters/handlers/rest/doc.go
@@ -18,7 +18,7 @@
 //	  https
 //	Host: localhost
 //	BasePath: /v1
-//	Version: 1.21.0-rc.0
+//	Version: 1.21.0-rc.1
 //	Contact: Weaviate<hello@weaviate.io> https://github.com/weaviate
 //
 //	Consumes:
