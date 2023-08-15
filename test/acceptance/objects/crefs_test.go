0502fb3e429fc3a0dc6d8d9504528b651d08a8fa
Dirk Kulawiak <dirk@semi.technology>
2023-08-15 14:20:06 +0200

    Disable autodetection for multi-target

--- test/acceptance/objects/crefs_test.go
+++ test/acceptance/objects/crefs_test.go
@@ -131,6 +131,117 @@ func TestRefsWithoutToClass(t *testing.T) {
 	}, objWithoutRef)
 }
 
+func TestBatchRefsMultiTarget(t *testing.T) {
+	refToClassName := "ReferenceTo"
+	refFromClassName := "ReferenceFrom"
+	defer deleteObjectClass(t, refToClassName)
+	defer deleteObjectClass(t, refFromClassName)
+
+	params := clschema.NewSchemaObjectsCreateParams().WithObjectClass(&models.Class{Class: refToClassName})
+	resp, err := helper.Client(t).Schema.SchemaObjectsCreate(params, nil)
+	helper.AssertRequestOk(t, resp, err, nil)
+
+	refFromClass := &models.Class{
+		Class: refFromClassName,
+		Properties: []*models.Property{
+			{
+				DataType: []string{refToClassName, refFromClassName},
+				Name:     "ref",
+			},
+		},
+	}
+	params2 := clschema.NewSchemaObjectsCreateParams().WithObjectClass(refFromClass)
+	resp2, err := helper.Client(t).Schema.SchemaObjectsCreate(params2, nil)
+	helper.AssertRequestOk(t, resp2, err, nil)
+
+	uuidsTo := make([]strfmt.UUID, 10)
+	uuidsFrom := make([]strfmt.UUID, 10)
+	for i := 0; i < 10; i++ {
+		uuidsTo[i] = assertCreateObject(t, refToClassName, map[string]interface{}{})
+		assertGetObjectEventually(t, uuidsTo[i])
+		uuidsFrom[i] = assertCreateObject(t, refFromClassName, map[string]interface{}{})
+		assertGetObjectEventually(t, uuidsFrom[i])
+	}
+
+	// add refs without toClass
+	var batchRefs []*models.BatchReference
+	for i := range uuidsFrom[:2] {
+		from := beaconStart + "ReferenceFrom/" + uuidsFrom[i] + "/ref"
+		to := beaconStart + uuidsTo[i]
+		batchRefs = append(batchRefs, &models.BatchReference{From: strfmt.URI(from), To: strfmt.URI(to)})
+	}
+
+	// add refs with toClass target 1
+	for i := range uuidsFrom[2:5] {
+		j := i + 2
+		from := beaconStart + "ReferenceFrom/" + uuidsFrom[j] + "/ref"
+		to := beaconStart + "ReferenceTo/" + uuidsTo[j]
+		batchRefs = append(batchRefs, &models.BatchReference{From: strfmt.URI(from), To: strfmt.URI(to)})
+	}
+
+	// add refs with toClass target 2
+	for i := range uuidsFrom[5:] {
+		j := i + 5
+		from := beaconStart + "ReferenceFrom/" + uuidsFrom[j] + "/ref"
+		to := beaconStart + "ReferenceFrom/" + uuidsTo[j]
+		batchRefs = append(batchRefs, &models.BatchReference{From: strfmt.URI(from), To: strfmt.URI(to)})
+	}
+
+	postRefParams := batch.NewBatchReferencesCreateParams().WithBody(batchRefs)
+	postRefResponse, err := helper.Client(t).Batch.BatchReferencesCreate(postRefParams, nil)
+	helper.AssertRequestOk(t, postRefResponse, err, nil)
+
+	// no autodetect for multi-target
+	for i := range uuidsFrom[:2] {
+		objWithRef := func() interface{} {
+			obj := assertGetObjectWithClass(t, uuidsFrom[i], refFromClassName)
+			return obj.Properties
+		}
+		testhelper.AssertEventuallyEqual(t, map[string]interface{}{
+			"ref": []interface{}{
+				map[string]interface{}{
+					"beacon": fmt.Sprintf(beaconStart+"%s", uuidsTo[i].String()),
+					"href":   fmt.Sprintf(pathStart+"%s", uuidsTo[i].String()),
+				},
+			},
+		}, objWithRef)
+	}
+
+	// refs for target 1
+	for i := range uuidsFrom[2:5] {
+		j := i + 2
+		objWithRef := func() interface{} {
+			obj := assertGetObjectWithClass(t, uuidsFrom[j], refFromClassName)
+			return obj.Properties
+		}
+		testhelper.AssertEventuallyEqual(t, map[string]interface{}{
+			"ref": []interface{}{
+				map[string]interface{}{
+					"beacon": fmt.Sprintf(beaconStart+"%s/%s", refToClassName, uuidsTo[j].String()),
+					"href":   fmt.Sprintf(pathStart+"%s/%s", refToClassName, uuidsTo[j].String()),
+				},
+			},
+		}, objWithRef)
+	}
+
+	// refs for target 2
+	for i := range uuidsFrom[5:] {
+		j := i + 5
+		objWithRef := func() interface{} {
+			obj := assertGetObjectWithClass(t, uuidsFrom[j], refFromClassName)
+			return obj.Properties
+		}
+		testhelper.AssertEventuallyEqual(t, map[string]interface{}{
+			"ref": []interface{}{
+				map[string]interface{}{
+					"beacon": fmt.Sprintf(beaconStart+"%s/%s", refFromClassName, uuidsTo[j].String()),
+					"href":   fmt.Sprintf(pathStart+"%s/%s", refFromClassName, uuidsTo[j].String()),
+				},
+			},
+		}, objWithRef)
+	}
+}
+
 func TestBatchRefsWithoutToClass(t *testing.T) {
 	refToClassName := "ReferenceTo"
 	refFromClassName := "ReferenceFrom"
@@ -164,8 +275,7 @@ func TestBatchRefsWithoutToClass(t *testing.T) {
 		assertGetObjectEventually(t, uuidsFrom[i])
 	}
 
-	batchRefs := []*models.BatchReference{}
-
+	var batchRefs []*models.BatchReference
 	for i := range uuidsFrom {
 		from := beaconStart + "ReferenceFrom/" + uuidsFrom[i] + "/ref"
 		to := beaconStart + uuidsTo[i]
