9ae2a93eb12bcc7d7f2816f9a5195ff66d27c495
Parker Duckworth <31421773+parkerduckworth@users.noreply.github.com>
2023-08-16 11:47:46 -0400

    Stable 1.20 into master (#3406)
    Co-authored-by: Marcin Antas <antas.marcin@gmail.com>
    Co-authored-by: Etienne Dilocker <etienne@dilocker.de>

--- test/acceptance/multi_tenancy/batch_add_tenant_objects_test.go
+++ test/acceptance/multi_tenancy/batch_add_tenant_objects_test.go
@@ -30,7 +30,8 @@ func TestBatchAddTenantObjects(t *testing.T) {
 	testClass := models.Class{
 		Class: "MultiTenantClass",
 		MultiTenancyConfig: &models.MultiTenancyConfig{
-			Enabled: true,
+			Enabled:            true,
+			AutoTenantCreation: true,
 		},
 		Properties: []*models.Property{
 			{
@@ -72,8 +73,6 @@ func TestBatchAddTenantObjects(t *testing.T) {
 		helper.DeleteClass(t, testClass.Class)
 	}()
 
-	helper.CreateTenants(t, testClass.Class, []*models.Tenant{{Name: tenantName}})
-
 	t.Run("add and get tenant objects", func(t *testing.T) {
 		helper.CreateObjectsBatch(t, tenantObjects)
 
@@ -93,12 +92,14 @@ func TestBatchWithMixedTenants(t *testing.T) {
 		{
 			Class: className + "1",
 			MultiTenancyConfig: &models.MultiTenancyConfig{
-				Enabled: true,
+				Enabled:            true,
+				AutoTenantCreation: true,
 			},
 		}, {
 			Class: className + "2",
 			MultiTenancyConfig: &models.MultiTenancyConfig{
-				Enabled: true,
+				Enabled:            true,
+				AutoTenantCreation: true,
 			},
 		},
 	}
@@ -140,7 +141,8 @@ func TestAddNonTenantBatchToMultiClass(t *testing.T) {
 	testClass := models.Class{
 		Class: className,
 		MultiTenancyConfig: &models.MultiTenancyConfig{
-			Enabled: true,
+			Enabled:            true,
+			AutoTenantCreation: true,
 		},
 	}
 	nonTenantObjects := []*models.Object{
@@ -179,7 +181,8 @@ func TestAddBatchToNonMultiClass(t *testing.T) {
 	testClass := models.Class{
 		Class: className,
 		MultiTenancyConfig: &models.MultiTenancyConfig{
-			Enabled: false,
+			Enabled:            false,
+			AutoTenantCreation: true,
 		},
 	}
 	tenantObjects := []*models.Object{
