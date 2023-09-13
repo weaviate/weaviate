abf9e9f9bd4043b4f933dd1f34c066f55a055acb
Parker Duckworth <31421773+parkerduckworth@users.noreply.github.com>
2023-09-13 12:16:42 -0400

    Restructure internal file structure to align with the class structure (#3522)

--- adapters/repos/db/resource_use.go
+++ adapters/repos/db/resource_use.go
@@ -49,13 +49,13 @@ func (d *DB) scanResourceUsage() {
 		defer t.Stop()
 		for {
 			select {
-			case <-d.shutdown:
+			case <-db.shutdown:
 				return
 			case <-t.C:
-				if !d.resourceScanState.isReadOnly {
-					du := d.getDiskUse(d.config.RootPath)
-					d.resourceUseWarn(d.memMonitor, du)
-					d.resourceUseReadonly(d.memMonitor, du)
+				if !db.resourceScanState.isReadOnly {
+					du := db.getDiskUse(db.config.RootPath)
+					db.resourceUseWarn(db.memMonitor, du)
+					db.resourceUseReadonly(db.memMonitor, du)
 				}
 			}
 		}
