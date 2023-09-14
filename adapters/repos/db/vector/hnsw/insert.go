f669a7184441683d220630b37bea34d19907849c
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2023-09-14 18:17:42 +0800

    fix un-unlocked mutex

--- adapters/repos/db/vector/hnsw/insert.go
+++ adapters/repos/db/vector/hnsw/insert.go
@@ -98,112 +98,123 @@ func (h *hnsw) AddBatch(ids []uint64, vectors [][]float32) error {
 			vector = distancer.Normalize(vector)
 		}
 
-		h.compressActionLock.RLock()
-		h.deleteVsInsertLock.RLock()
-
-		before := time.Now()
-
-		wasFirst := false
-		var firstInsertError error
-		h.initialInsertOnce.Do(func() {
-			if h.isEmpty() {
-				wasFirst = true
-				firstInsertError = h.insertInitialElement(node, vector)
-			}
-		})
-		if wasFirst {
-			if firstInsertError != nil {
-				return firstInsertError
-			}
-			continue
-		}
-
-		node.markAsMaintenance()
-
-		h.RLock()
-		// initially use the "global" entrypoint which is guaranteed to be on the
-		// currently highest layer
-		entryPointID := h.entryPointID
-		// initially use the level of the entrypoint which is the highest level of
-		// the h-graph in the first iteration
-		currentMaximumLayer := h.currentMaximumLayer
-		h.RUnlock()
-
-		targetLevel := node.level
-		node.connections = make([][]uint64, targetLevel+1)
-
-		for i := targetLevel; i >= 0; i-- {
-			capacity := h.maximumConnections
-			if i == 0 {
-				capacity = h.maximumConnectionsLayerZero
-			}
-
-			node.connections[i] = make([]uint64, 0, capacity)
-		}
-
-		nodeId := node.id
-
-		h.shardedNodeLocks[nodeId%NodeLockStripe].Lock()
-		h.nodes[nodeId] = node
-		h.shardedNodeLocks[nodeId%NodeLockStripe].Unlock()
-
-		if h.compressed.Load() {
-			compressed := h.pq.Encode(vector)
-			h.storeCompressedVector(node.id, compressed)
-			h.compressedVectorsCache.preload(node.id, compressed)
-		} else {
-			h.cache.preload(node.id, vector)
-		}
-
-		h.insertMetrics.prepareAndInsertNode(before)
-		before = time.Now()
-
-		var err error
-		entryPointID, err = h.findBestEntrypointForNode(currentMaximumLayer, targetLevel,
-			entryPointID, vector)
+		err := h.addOne(vector, node)
 		if err != nil {
-			return errors.Wrap(err, "find best entrypoint")
+			return err
 		}
 
-		h.insertMetrics.findEntrypoint(before)
-		before = time.Now()
+		h.insertMetrics.total(globalBefore)
+	}
+	return nil
+}
 
-		// TODO: check findAndConnectNeighbors...
-		if err := h.findAndConnectNeighbors(node, entryPointID, vector,
-			targetLevel, currentMaximumLayer, helpers.NewAllowList()); err != nil {
-			return errors.Wrap(err, "find and connect neighbors")
-		}
+func (h *hnsw) addOne(vector []float32, node *vertex) error {
+	h.compressActionLock.RLock()
+	h.deleteVsInsertLock.RLock()
 
-		h.insertMetrics.findAndConnectTotal(before)
-		before = time.Now()
+	before := time.Now()
 
-		// go h.insertHook(nodeId, targetLevel, neighborsAtLevel)
-		node.unmarkAsMaintenance()
-
-		h.RLock()
-		if targetLevel > h.currentMaximumLayer {
-			h.RUnlock()
-			h.Lock()
-			// check again to avoid changes from RUnlock to Lock again
-			if targetLevel > h.currentMaximumLayer {
-				if err := h.commitLog.SetEntryPointWithMaxLayer(nodeId, targetLevel); err != nil {
-					h.Unlock()
-					return err
-				}
-
-				h.entryPointID = nodeId
-				h.currentMaximumLayer = targetLevel
-			}
-			h.Unlock()
-		} else {
-			h.RUnlock()
-		}
-
+	defer func() {
 		h.deleteVsInsertLock.RUnlock()
 		h.compressActionLock.RUnlock()
 		h.insertMetrics.updateGlobalEntrypoint(before)
-		h.insertMetrics.total(globalBefore)
+	}()
+
+	wasFirst := false
+	var firstInsertError error
+	h.initialInsertOnce.Do(func() {
+		if h.isEmpty() {
+			wasFirst = true
+			firstInsertError = h.insertInitialElement(node, vector)
+		}
+	})
+	if wasFirst {
+		if firstInsertError != nil {
+			return firstInsertError
+		}
+		return nil
 	}
+
+	node.markAsMaintenance()
+
+	h.RLock()
+	// initially use the "global" entrypoint which is guaranteed to be on the
+	// currently highest layer
+	entryPointID := h.entryPointID
+	// initially use the level of the entrypoint which is the highest level of
+	// the h-graph in the first iteration
+	currentMaximumLayer := h.currentMaximumLayer
+	h.RUnlock()
+
+	targetLevel := node.level
+	node.connections = make([][]uint64, targetLevel+1)
+
+	for i := targetLevel; i >= 0; i-- {
+		capacity := h.maximumConnections
+		if i == 0 {
+			capacity = h.maximumConnectionsLayerZero
+		}
+
+		node.connections[i] = make([]uint64, 0, capacity)
+	}
+
+	nodeId := node.id
+
+	h.shardedNodeLocks[nodeId%NodeLockStripe].Lock()
+	h.nodes[nodeId] = node
+	h.shardedNodeLocks[nodeId%NodeLockStripe].Unlock()
+
+	if h.compressed.Load() {
+		compressed := h.pq.Encode(vector)
+		h.storeCompressedVector(node.id, compressed)
+		h.compressedVectorsCache.preload(node.id, compressed)
+	} else {
+		h.cache.preload(node.id, vector)
+	}
+
+	h.insertMetrics.prepareAndInsertNode(before)
+	before = time.Now()
+
+	var err error
+	entryPointID, err = h.findBestEntrypointForNode(currentMaximumLayer, targetLevel,
+		entryPointID, vector)
+	if err != nil {
+		return errors.Wrap(err, "find best entrypoint")
+	}
+
+	h.insertMetrics.findEntrypoint(before)
+	before = time.Now()
+
+	// TODO: check findAndConnectNeighbors...
+	if err := h.findAndConnectNeighbors(node, entryPointID, vector,
+		targetLevel, currentMaximumLayer, helpers.NewAllowList()); err != nil {
+		return errors.Wrap(err, "find and connect neighbors")
+	}
+
+	h.insertMetrics.findAndConnectTotal(before)
+	before = time.Now()
+
+	node.unmarkAsMaintenance()
+
+	h.RLock()
+	if targetLevel > h.currentMaximumLayer {
+		h.RUnlock()
+		h.Lock()
+		// check again to avoid changes from RUnlock to Lock again
+		if targetLevel > h.currentMaximumLayer {
+			if err := h.commitLog.SetEntryPointWithMaxLayer(nodeId, targetLevel); err != nil {
+				h.Unlock()
+				return err
+			}
+
+			h.entryPointID = nodeId
+			h.currentMaximumLayer = targetLevel
+		}
+		h.Unlock()
+	} else {
+		h.RUnlock()
+	}
+
 	return nil
 }
 
