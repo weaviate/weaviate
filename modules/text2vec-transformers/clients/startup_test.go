69a68963313ad93ef9d9e6a3cd2285b48c218fb9
Parker Duckworth <31421773+parkerduckworth@users.noreply.github.com>
2023-09-19 08:37:37 -0400

    handle lsm cleanup error case (#3549)

--- modules/text2vec-transformers/clients/startup_test.go
+++ modules/text2vec-transformers/clients/startup_test.go
@@ -52,7 +52,7 @@ func TestWaitForStartup(t *testing.T) {
 		v := New(url, url, 0, nullLogger())
 		ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
 		defer cancel()
-		err := v.WaitForStartup(ctx, 150*time.Millisecond)
+		err := v.WaitForStartup(ctx, 50*time.Millisecond)
 
 		require.NotNil(t, err, nullLogger())
 		assert.Contains(t, err.Error(), "init context expired before remote was ready: send check ready request")
