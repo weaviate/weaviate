f0beedb7ce9d673b8c4b9a4fcc3811fefa9cc177
Jeremy Price <donomii@gmail.com>
2023-09-20 20:32:22 +0200

    Revert "Delete indexer"
    This reverts commit e91214442c03ebec4aead721b1ff76a908215d6f.

---
+++ adapters/repos/db/inverted_reindexer_set_to_roaringset.go
@@ -0,0 +1,76 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2023 Weaviate B.V. All rights reserved.
+//
+//  CONTACT: hello@weaviate.io
+//
+
+package db
+
+import (
+	"context"
+	"time"
+
+	"github.com/weaviate/weaviate/adapters/repos/db/lsmkv"
+)
+
+type ShardInvertedReindexTaskSetToRoaringSet struct{}
+
+func (t *ShardInvertedReindexTaskSetToRoaringSet) GetPropertiesToReindex(ctx context.Context,
+	shard *Shard,
+) ([]ReindexableProperty, error) {
+	reindexableProperties := []ReindexableProperty{}
+
+	bucketOptions := []lsmkv.BucketOption{
+		lsmkv.WithIdleThreshold(time.Duration(shard.index.Config.MemtablesFlushIdleAfter) * time.Second),
+	}
+
+	for name, bucket := range shard.store.GetBucketsByName() {
+		if bucket.Strategy() == lsmkv.StrategySetCollection &&
+			bucket.DesiredStrategy() == lsmkv.StrategyRoaringSet {
+
+			propName, indexType := GetPropNameAndIndexTypeFromBucketName(name)
+			switch indexType {
+			case IndexTypePropValue:
+				reindexableProperties = append(reindexableProperties,
+					ReindexableProperty{
+						PropertyName:    propName,
+						IndexType:       IndexTypePropValue,
+						DesiredStrategy: lsmkv.StrategyRoaringSet,
+						BucketOptions:   bucketOptions,
+					},
+				)
+			case IndexTypePropLength:
+				reindexableProperties = append(reindexableProperties,
+					ReindexableProperty{
+						PropertyName:    propName,
+						IndexType:       IndexTypePropLength,
+						DesiredStrategy: lsmkv.StrategyRoaringSet,
+						BucketOptions:   bucketOptions,
+					},
+				)
+			case IndexTypePropNull:
+				reindexableProperties = append(reindexableProperties,
+					ReindexableProperty{
+						PropertyName:    propName,
+						IndexType:       IndexTypePropNull,
+						DesiredStrategy: lsmkv.StrategyRoaringSet,
+						BucketOptions:   bucketOptions,
+					},
+				)
+			default:
+				// skip remaining
+			}
+		}
+	}
+
+	return reindexableProperties, nil
+}
+
+func (t *ShardInvertedReindexTaskSetToRoaringSet) OnPostResumeStore(ctx context.Context, shard *Shard) error {
+	return nil
+}
