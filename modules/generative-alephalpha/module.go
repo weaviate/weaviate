80d9de1243a0524b3416efec28e3de83a9a01c66
hsm207 <hsm207@users.noreply.github.com>
2023-10-02 18:07:41 +0000

    stub module.go

---
+++ modules/generative-alephalpha/module.go
@@ -0,0 +1,77 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2023 Weaviate B.V. All rights reserved.
+//
+//  CONTACT: hello@weaviate.io
+//
+
+package modgenerativealephalpha
+
+import (
+	"context"
+	"net/http"
+	// "os"
+
+	// "github.com/pkg/errors"
+	// "github.com/sirupsen/logrus"
+	"github.com/weaviate/weaviate/entities/modulecapabilities"
+	"github.com/weaviate/weaviate/entities/moduletools"
+	// "github.com/weaviate/weaviate/modules/generative-alephalpha/clients"
+	// additionalprovider "github.com/weaviate/weaviate/usecases/modulecomponents/additional"
+	generativemodels "github.com/weaviate/weaviate/usecases/modulecomponents/additional/models"
+	
+)
+
+const Name = "generative-alephalpha"
+
+
+type GenerativeAlephAlphaModule struct {
+	generative                     generativeClient
+	additionalPropertiesProvider modulecapabilities.AdditionalProperties
+}
+
+type generativeClient interface {
+	GenerateSingleResult(ctx context.Context, textProperties map[string]string, prompt string, cfg moduletools.ClassConfig) (*generativemodels.GenerateResponse, error)
+	GenerateAllResults(ctx context.Context, textProperties []map[string]string, task string, cfg moduletools.ClassConfig) (*generativemodels.GenerateResponse, error)
+	Generate(ctx context.Context, cfg moduletools.ClassConfig, prompt string) (*generativemodels.GenerateResponse, error)
+	MetaInfo() (map[string]interface{}, error)
+}
+
+func New() *GenerativeAlephAlphaModule {
+	return &GenerativeAlephAlphaModule{}
+}
+
+func (m *GenerativeAlephAlphaModule) Name() string {
+	return Name
+}
+
+func (m *GenerativeAlephAlphaModule) Type() modulecapabilities.ModuleType {
+	return modulecapabilities.Text2TextGenerative
+}
+
+func (m *GenerativeAlephAlphaModule) Init(ctx context.Context, params moduletools.ModuleInitParams) error {
+	return nil
+}
+
+func (m *GenerativeAlephAlphaModule) MetaInfo() (map[string]interface{}, error) {
+	return nil, nil
+}
+
+func (m *GenerativeAlephAlphaModule) RootHandler() http.Handler {
+	// TODO: remove once this is a capability interface
+	return nil
+}
+
+func (m *GenerativeAlephAlphaModule) AdditionalProperties() map[string]modulecapabilities.AdditionalProperty {
+	return m.additionalPropertiesProvider.AdditionalProperties()
+}
+
+var (
+	_ = modulecapabilities.Module(New())
+	_ = modulecapabilities.AdditionalProperties(New())
+	_ = modulecapabilities.MetaProvider(New())
+)
