acf0a2e4615fc6b1e3133fca4f928271ac77947e
Jeremy Price <donomii@gmail.com>
2023-10-10 00:40:31 +0900

    Refactor core object to allow scripting (#3612)
    * Refactor core object to allow scripting
    
    * Add some examples in cmd/

--- usecases/backup/scheduler.go
+++ usecases/backup/scheduler.go
@@ -261,7 +261,7 @@ func (s *Scheduler) validateRestoreRequest(ctx context.Context, store coordStore
 	if err != nil {
 		notFoundErr := backup.ErrNotFound{}
 		if errors.As(err, &notFoundErr) {
-			return nil, fmt.Errorf("%w: %q", errMetaNotFound, destPath)
+			return nil, fmt.Errorf("scheduler cannot validate restore request: %w: '%v' (%w)", errMetaNotFound, destPath, err)
 		}
 		return nil, fmt.Errorf("find backup %s: %w", destPath, err)
 	}
