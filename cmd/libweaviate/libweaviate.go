783b3bb5a7d3f054a37944ea9937def5fa6b776e
Jeremy Price <donomii@gmail.com>
2023-10-09 18:15:31 +0200

    .

--- cmd/libweaviate/libweaviate.go
+++ cmd/libweaviate/libweaviate.go
@@ -15,8 +15,15 @@ package main
 
 // The function signature must not include neither Go struct nor Go interface nor Go array nor variadic argument.
 
+/*
+#include<stdbool.h>
+typedef const char cchar_t;
+*/
+import "C"
+
 import (
-	"C"
+	"unsafe"
+	"github.com/vitaminwater/cgo.wchar"
 	"context"
 	"fmt"
 	"os"
@@ -28,13 +35,18 @@ import (
 	"github.com/weaviate/weaviate/entities/cyclemanager"
 	"github.com/weaviate/weaviate/usecases/config"
 )
+import "github.com/weaviate/weaviate/adapters/handlers/rest/state"
 
 var started = false
+var appState *state.State
 
 //export dumpBucket
-func dumpBucket(storeDir, propName string) {
-	if !started {
-		fmt.Println("Weaviate has not been started!  Call startWeaviate() before any other function")
+func dumpBucket(storeDir_c, propName_c *C.cchar_t) {
+	storeDir := wstr2go(storeDir_c)
+	propName := wstr2go(propName_c)
+
+	if started {
+		fmt.Println("Dump bucket must only be run while weaviate is shut down.  Do not start weaviate when doing a direct bucket dump")
 		os.Exit(1)
 	}
 
@@ -62,16 +74,65 @@ func dumpBucket(storeDir, propName string) {
 		return nil
 	})
 	fmt.Printf("Dir: %v, bucket %v\n", storeDir, bucketName)
+	
+}
+
+func wstr2go(cstr *C.cchar_t) string {
+	val, err := wchar.WcharStringPtrToGoString(unsafe.Pointer(cstr))
+	if err != nil {
+		panic(err)
+	}
+	return val
 }
 
 //export startWeaviate
-func startWeaviate() {
-	config := config.GetConfigOptionGroup()
-	rest.MakeAppState(context.Background(), config)
-	if len(os.Args) != 3 {
-		fmt.Printf("Usage: %s <storeDir> <propName>\n", os.Args[0])
-		os.Exit(1)
-	}
+func startWeaviate(dataPath_c , origin_c, clusterHostname_c,defaultVectoriser_c *C.cchar_t) {
+	dataPath := wstr2go(dataPath_c)
+
+	    if dataPath == "" {
+			dataPath = "./data"
+		}
+
+		origin := wstr2go(origin_c)
+		if origin == "" {
+			origin = "http://localhost:8080"
+		}
+
+		clusterHostname := wstr2go(clusterHostname_c)
+		if clusterHostname == "" {
+			clusterHostname = "node1"
+		}
+
+		defaultVectoriser :=wstr2go(defaultVectoriser_c)
+		if defaultVectoriser == "" {
+			defaultVectoriser = "none"
+		}
+		
+		// set environment variables                            
+        os.Setenv("LOG_LEVEL", "debug")                 
+        os.Setenv("LOG_FORMAT", "text")                 
+        os.Setenv("PROMETHEUS_MONITORING_ENABLED", "true")
+        os.Setenv("GO_BLOCK_PROFILE_RATE", "20")
+        os.Setenv("GO_MUTEX_PROFILE_FRACTION", "20")
+        os.Setenv("PERSISTENCE_DATA_PATH", dataPath)
+        os.Setenv("ORIGIN", origin)
+        os.Setenv("QUERY_DEFAULTS_LIMIT", "20")
+        os.Setenv("QUERY_MAXIMUM_RESULTS", "10000")
+        os.Setenv("CLUSTER_HOSTNAME", clusterHostname)
+        os.Setenv("TRACK_VECTOR_DIMENSIONS", "true")    
+        os.Setenv("CLUSTER_GOSSIP_BIND_PORT", "7100")   
+        os.Setenv("CLUSTER_DATA_BIND_PORT", "7101")     
+        os.Setenv("AUTHENTICATION_ANONYMOUS_ACCESS_ENABLED", "true")
+        os.Setenv("DEFAULT_VECTORIZER_MODULE", defaultVectoriser) 
+                                                                
+        config := config.GetConfigOptionGroup()    
+		
+		
+		fmt.Printf("Starting Weaviate with dataPath: %v, origin: %v, clusterHostname: %v, defaultVectoriser: %v\n", dataPath, origin, clusterHostname, defaultVectoriser)
+        appState = rest.MakeAppState(context.Background(), config)
+
+	
+
 	started = true
 }
 
