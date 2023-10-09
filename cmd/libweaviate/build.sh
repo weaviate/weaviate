591a72618c54ede53e209e8c529546e276007383
Jeremy Price <donomii@gmail.com>
2023-10-09 16:59:14 +0200

    Libraries missing for current macosx on my machine?

--- cmd/libweaviate/build.sh
+++ cmd/libweaviate/build.sh
@@ -1,6 +1,7 @@
 go build -buildmode c-shared -o libweaviate.so .
-cp libweaviate.so libweaviate.dylib
-gcc -L . -I . -lweaviate -ltcl -shared -o libweaviatetcl.dylib bindings/tcl.c
-#Now load tclsh and run load [file join [pwd] libweaviate[info sharedlibextension]]
+go build -buildmode c-shared -o libweaviate.dylib .
+gcc -dynamiclib -DUSE_TCL_STUBS bindings/tcl.c -L/Library/Developer/CommandLineTools/SDKs/MacOSX12.1.sdk/System/Library/Frameworks/Tcl.framework -L/Library/Developer/CommandLineTools/SDKs/MacOSX13.3.sdk/System/Library/Frameworks/Tcl.framework/Versions/8.5/ -L/Library/Developer/CommandLineTools/SDKs/MacOSX12.1.sdk/System/Library/Frameworks/Tcl.framework/Versions/8.5/ -L/Library/Developer/CommandLineTools/SDKs/MacOSX12.3.sdk/System/Library/Frameworks/Tcl.framework/Versions/8.5/  -L. -I. -lweaviate -ltclstub -o libweaviateTCL.dylib
+
+#Now load tclsh and run load [file join [pwd] libweaviateTCL[info sharedlibextension]]
 echo start tclsh and run
-echo load [file join [pwd] libweaviate[info sharedlibextension]]
+echo load [file join [pwd] libweaviateTCL[info sharedlibextension]]
