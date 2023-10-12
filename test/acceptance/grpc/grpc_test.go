9b7a93d45799661ef34f194d3dca7730da6e8092
Tommy Smith <tommy@weaviate.io>
2023-10-12 17:14:12 +0200

    dedup test

--- test/acceptance/grpc/grpc_test.go
+++ test/acceptance/grpc/grpc_test.go
@@ -51,120 +51,85 @@ func TestGRPC(t *testing.T) {
 		require.NotNil(t, resp)
 	})
 
-	t.Run("Search with props", func(t *testing.T) {
-		scifi := "sci-fi"
-		resp, err := grpcClient.SearchV1(context.TODO(), &pb.SearchRequestV1{
-			Collection: booksClass.Class,
-			Properties: &pb.PropertiesRequest{
-				NonRefProperties: []string{"title"},
-				ObjectProperties: []*pb.ObjectPropertiesRequest{
-					{PropName: "meta", PrimitiveProperties: []string{"isbn"}},
-					{PropName: "reviews", PrimitiveProperties: []string{"tags"}},
+	tests := []struct {
+		name string
+		req  *pb.SearchRequestV1
+	}{
+		{
+			name: "Search with props",
+			req: &pb.SearchRequestV1{
+				Collection: booksClass.Class,
+				Properties: &pb.PropertiesRequest{
+					NonRefProperties: []string{"title"},
+					ObjectProperties: []*pb.ObjectPropertiesRequest{
+						{PropName: "meta", PrimitiveProperties: []string{"isbn"}},
+						{PropName: "reviews", PrimitiveProperties: []string{"tags"}},
+					},
+				},
+				Metadata: &pb.MetadataRequest{
+					Uuid: true,
 				},
 			},
-			Metadata: &pb.MetadataRequest{
-				Uuid: true,
+		},
+		{
+			name: "Search without props",
+			req: &pb.SearchRequestV1{
+				Collection: booksClass.Class,
 			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			scifi := "sci-fi"
+			resp, err := grpcClient.SearchV1(context.TODO(), tt.req)
+			require.NoError(t, err)
+			require.NotNil(t, resp)
+			require.NotNil(t, resp.Results)
+			assert.Equal(t, len(books.BatchObjects()), len(resp.Results))
+			for i := range resp.Results {
+				res := resp.Results[i]
+				id := res.Metadata.Id
+
+				assert.True(t, id == books.Dune.String() || id == books.ProjectHailMary.String() || id == books.TheLordOfTheIceGarden.String())
+				title, ok := res.Properties.NonRefProperties.AsMap()["title"]
+				require.True(t, ok)
+
+				objProps := res.Properties.ObjectProperties
+				require.Len(t, objProps, 1)
+
+				objArrayProps := res.Properties.ObjectArrayProperties
+				require.Len(t, objArrayProps, 1)
+
+				isbn, ok := objProps[0].Value.NonRefProperties.AsMap()["isbn"]
+				require.True(t, ok)
+
+				tags := objArrayProps[0].Values[0].TextArrayProperties[0].Values
+				require.True(t, ok)
+
+				expectedTitle := ""
+				expectedIsbn := ""
+				expectedTags := []string{}
+				if id == books.Dune.String() {
+					expectedTitle = "Dune"
+					expectedIsbn = "978-0593099322"
+					expectedTags = []string{scifi, "epic"}
+				}
+				if id == books.ProjectHailMary.String() {
+					expectedTitle = "Project Hail Mary"
+					expectedIsbn = "978-0593135204"
+					expectedTags = []string{scifi}
+				}
+				if id == books.TheLordOfTheIceGarden.String() {
+					expectedTitle = "The Lord of the Ice Garden"
+					expectedIsbn = "978-8374812962"
+					expectedTags = []string{scifi, "fantasy"}
+				}
+				assert.Equal(t, expectedTitle, title)
+				assert.Equal(t, expectedIsbn, isbn)
+				assert.Equal(t, expectedTags, tags)
+			}
 		})
-		require.NoError(t, err)
-		require.NotNil(t, resp)
-		require.NotNil(t, resp.Results)
-		assert.Equal(t, len(books.BatchObjects()), len(resp.Results))
-		for i := range resp.Results {
-			res := resp.Results[i]
-			id := res.Metadata.Id
-
-			assert.True(t, id == books.Dune.String() || id == books.ProjectHailMary.String() || id == books.TheLordOfTheIceGarden.String())
-			title, ok := res.Properties.NonRefProperties.AsMap()["title"]
-			require.True(t, ok)
-
-			objProps := res.Properties.ObjectProperties
-			require.Len(t, objProps, 1)
-
-			objArrayProps := res.Properties.ObjectArrayProperties
-			require.Len(t, objArrayProps, 1)
-
-			isbn, ok := objProps[0].Value.NonRefProperties.AsMap()["isbn"]
-			require.True(t, ok)
-
-			tags := objArrayProps[0].Values[0].TextArrayProperties[0].Values
-			require.True(t, ok)
-
-			expectedTitle := ""
-			expectedIsbn := ""
-			expectedTags := []string{}
-			if id == books.Dune.String() {
-				expectedTitle = "Dune"
-				expectedIsbn = "978-0593099322"
-				expectedTags = []string{scifi, "epic"}
-			}
-			if id == books.ProjectHailMary.String() {
-				expectedTitle = "Project Hail Mary"
-				expectedIsbn = "978-0593135204"
-				expectedTags = []string{scifi}
-			}
-			if id == books.TheLordOfTheIceGarden.String() {
-				expectedTitle = "The Lord of the Ice Garden"
-				expectedIsbn = "978-8374812962"
-				expectedTags = []string{scifi, "fantasy"}
-			}
-			assert.Equal(t, expectedTitle, title)
-			assert.Equal(t, expectedIsbn, isbn)
-			assert.Equal(t, expectedTags, tags)
-		}
-	})
-
-	t.Run("Search without props", func(t *testing.T) {
-		resp, err := grpcClient.SearchV1(context.TODO(), &pb.SearchRequestV1{
-			Collection: booksClass.Class,
-		})
-		require.NoError(t, err)
-		require.NotNil(t, resp)
-		require.NotNil(t, resp.Results)
-		assert.Equal(t, len(books.BatchObjects()), len(resp.Results))
-		for i := range resp.Results {
-			res := resp.Results[i]
-			id := res.Metadata.Id
-
-			assert.True(t, id == books.Dune.String() || id == books.ProjectHailMary.String() || id == books.TheLordOfTheIceGarden.String())
-			title, ok := res.Properties.NonRefProperties.AsMap()["title"]
-			require.True(t, ok)
-
-			objProps := res.Properties.ObjectProperties
-			require.Len(t, objProps, 1)
-
-			objArrayProps := res.Properties.ObjectArrayProperties
-			require.Len(t, objArrayProps, 1)
-
-			isbn, ok := objProps[0].Value.NonRefProperties.AsMap()["isbn"]
-			require.True(t, ok)
-
-			tags := objArrayProps[0].Values[0].TextArrayProperties[0].Values
-			require.True(t, ok)
-
-			expectedTitle := ""
-			expectedIsbn := ""
-			expectedTags := []string{}
-			if id == books.Dune.String() {
-				expectedTitle = "Dune"
-				expectedIsbn = "978-0593099322"
-				expectedTags = []string{"sci-fi", "epic"}
-			}
-			if id == books.ProjectHailMary.String() {
-				expectedTitle = "Project Hail Mary"
-				expectedIsbn = "978-0593135204"
-				expectedTags = []string{"sci-fi"}
-			}
-			if id == books.TheLordOfTheIceGarden.String() {
-				expectedTitle = "The Lord of the Ice Garden"
-				expectedIsbn = "978-8374812962"
-				expectedTags = []string{"sci-fi", "fantasy"}
-			}
-			assert.Equal(t, expectedTitle, title)
-			assert.Equal(t, expectedIsbn, isbn)
-			assert.Equal(t, expectedTags, tags)
-		}
-	})
+	}
 
 	t.Run("gRPC Search removed", func(t *testing.T) {
 		_, err := grpcClient.Search(context.TODO(), &pb.SearchRequest{})
