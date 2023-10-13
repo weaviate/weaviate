9d03e51dea2f39bf1646c88fd9e538d1c615bf89
Redouan El Rhazouani <81578195+redouan-rhazouani@users.noreply.github.com>
2023-10-13 11:45:14 +0200

    Initial minimal end-to-end schema processing with associated TODOs

---
+++ usecases/schema/property_test.go
@@ -0,0 +1,10 @@
+package schema
+
+/// TODO-RAFT START
+// Fix Unit Tests
+// With property-related transactions refactored into property.go, we need to re-implement the following test cases:
+// - Adding new property (previously in add_property_test.go)
+// - Updating existing property 
+// - Validation (previously in remove_duplicate_props_test.go)
+// Please consult the previous implementation's test files for reference.
+/// TODO-RAFT END
