5d239b680ca2b3e4d3b4889a0e893610872e273e
Redouan El Rhazouani <81578195+redouan-rhazouani@users.noreply.github.com>
2023-10-13 11:45:14 +0200

    Initial minimal end-to-end schema processing with associated TODOs

--- usecases/schema/property_test.go
+++ usecases/schema/property_test.go
@@ -1,21 +1,10 @@
-//                           _       _
-// __      _____  __ ___   ___  __ _| |_ ___
-// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
-//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
-//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
-//
-//  Copyright © 2016 - 2023 Weaviate B.V. All rights reserved.
-//
-//  CONTACT: hello@weaviate.io
-//
-
 package schema
 
 /// TODO-RAFT START
 // Fix Unit Tests
 // With property-related transactions refactored into property.go, we need to re-implement the following test cases:
 // - Adding new property (previously in add_property_test.go)
-// - Updating existing property
+// - Updating existing property 
 // - Validation (previously in remove_duplicate_props_test.go)
 // Please consult the previous implementation's test files for reference.
 /// TODO-RAFT END
