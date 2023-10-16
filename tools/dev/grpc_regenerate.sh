00e78d7ee35a7c173dd7bd94651e03eb58e59324
Tommy Smith <tommy@weaviate.io>
2023-10-16 10:57:21 +0100

    correctly add license to grpc generation to maintain auto-gen comment

--- tools/dev/grpc_regenerate.sh
+++ tools/dev/grpc_regenerate.sh
@@ -16,7 +16,9 @@ rm -fr $OUT_DIR && mkdir -p $OUT_DIR && cd $GEN_DIR && protoc \
     --go-grpc_out=paths=source_relative:protocol \
     ../proto/v1/*.proto
 
-cd - && go run ./tools/license_headers/main.go
+cd - && sed -i '' '/versions:/, /source: .*/d' ./grpc/generated/protocol/**/*.go
+
+go run ./tools/license_headers/main.go
 
 goimports -w $OUT_DIR
 
