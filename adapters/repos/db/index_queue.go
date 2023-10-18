00c9caf68b3d1545632ce13aa7ad4c7d7b588856
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2023-10-18 13:16:25 +0700

    use correct env var

--- adapters/repos/db/index_queue.go
+++ adapters/repos/db/index_queue.go
@@ -191,7 +191,6 @@ func (q *IndexQueue) Close() error {
 
 // Push adds a list of vectors to the queue.
 func (q *IndexQueue) Push(ctx context.Context, vectors ...vectorDescriptor) error {
-	panic("PUSH CALLED! IT'S WORKING!")
 	if ctx.Err() != nil {
 		return ctx.Err()
 	}
--- test/acceptance/run.sh
+++ test/acceptance/run.sh
@@ -5,6 +5,7 @@ set -eou pipefail
 function main() {
   # needed for test/docker package during replication tests
   export TEST_WEAVIATE_IMAGE=weaviate/test-server
+  echo "-+- WEAVIATE_ASYNC_INDEXING=" $WEAVIATE_ASYNC_INDEXING
   # for now we need to run the tests sequentially, there seems to be some sort of issues with running them in parallel
     for pkg in $(go list ./... | grep 'test/acceptance' | grep -v 'test/acceptance/stress_tests' ); do
       if ! go test -count 1 -race "$pkg"; then
