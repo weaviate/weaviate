cadc1fa0b2c280e3ca5183ce862f33fbb4d60bb1
Loic Reyreaud <loic@weaviate.io>
2023-10-18 11:21:15 +0200

    add inverted index config to and from function with tests
    Signed-off-by: Loic Reyreaud <loic@weaviate.io>

--- entities/schema/inverted_index_config.go
+++ entities/schema/inverted_index_config.go
@@ -11,17 +11,56 @@
 
 package schema
 
-import "github.com/weaviate/weaviate/entities/models"
+import (
+	"github.com/weaviate/weaviate/entities/models"
+)
 
 type InvertedIndexConfig struct {
-	BM25                BM25Config
-	Stopwords           models.StopwordConfig
-	IndexTimestamps     bool
-	IndexNullState      bool
-	IndexPropertyLength bool
+	BM25                   BM25Config
+	Stopwords              models.StopwordConfig
+	CleanupIntervalSeconds uint64
+	IndexTimestamps        bool
+	IndexNullState         bool
+	IndexPropertyLength    bool
 }
 
 type BM25Config struct {
 	K1 float64
 	B  float64
+}
+
+func (i *InvertedIndexConfig) FromModel(m *models.InvertedIndexConfig) {
+	if m.Bm25 != nil {
+		i.BM25.K1 = float64(m.Bm25.K1)
+		i.BM25.B = float64(m.Bm25.B)
+	}
+	if m.Stopwords != nil {
+		i.Stopwords = *m.Stopwords
+	}
+	i.CleanupIntervalSeconds = uint64(m.CleanupIntervalSeconds)
+	i.IndexTimestamps = m.IndexTimestamps
+	i.IndexNullState = m.IndexNullState
+	i.IndexPropertyLength = m.IndexPropertyLength
+}
+
+func (i *InvertedIndexConfig) ToModel() *models.InvertedIndexConfig {
+	m := &models.InvertedIndexConfig{}
+
+	if i.BM25.K1 != 0 || i.BM25.B != 0 {
+		m.Bm25 = &models.BM25Config{}
+		m.Bm25.K1 = float32(i.BM25.K1)
+		m.Bm25.B = float32(i.BM25.B)
+	}
+	if len(i.Stopwords.Additions) > 0 || len(i.Stopwords.Removals) > 0 || len(i.Stopwords.Preset) > 0 {
+		m.Stopwords = &models.StopwordConfig{}
+		// Force a copy to avoid references
+		*m.Stopwords = i.Stopwords
+	}
+
+	m.CleanupIntervalSeconds = int64(i.CleanupIntervalSeconds)
+	m.IndexTimestamps = i.IndexTimestamps
+	m.IndexNullState = i.IndexNullState
+	m.IndexPropertyLength = i.IndexPropertyLength
+
+	return m
 }
