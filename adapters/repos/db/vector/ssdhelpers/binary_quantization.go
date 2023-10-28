4fe76abc5fe32840e8e089de96d47ad2644fad45
Abdel Rodríguez <abdelr1982@gmail.com>
2023-10-28 14:43:46 +0200

    fix swagger

--- adapters/repos/db/vector/ssdhelpers/binary_quantization.go
+++ adapters/repos/db/vector/ssdhelpers/binary_quantization.go
@@ -1,14 +1,14 @@
-//	_       _
-//
+//                           _       _
 // __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
 //
-//	\ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
-//	 \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
-//	  \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//  Copyright © 2016 - 2023 Weaviate B.V. All rights reserved.
 //
-//	 Copyright © 2016 - 2023 Weaviate B.V. All rights reserved.
+//  CONTACT: hello@weaviate.io
 //
-//	 CONTACT: hello@weaviate.io
+
 package ssdhelpers
 
 import (
