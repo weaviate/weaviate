78925774a8f5b43d729c7c65bab03ba8e21078f7
Joan Fontanals Martinez <joan.martinez@jina.ai>
2023-10-30 16:42:00 +0100

    fix: fix default jinaAI model

--- modules/text2vec-jinaai/vectorizer/class_settings.go
+++ modules/text2vec-jinaai/vectorizer/class_settings.go
@@ -24,7 +24,7 @@ import (
 
 const (
 	DefaultJinaAIDocumentType    = "text"
-	DefaultJinaAIModel           = "jina-embedding-v2"
+	DefaultJinaAIModel           = "jina-embeddings-v2-base-en"
 	DefaultVectorizeClassName    = true
 	DefaultPropertyIndexed       = true
 	DefaultVectorizePropertyName = false
