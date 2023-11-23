d10fa65a09147c948531898a7b6f7a86fb8c111e
Jeremy Price <donomii@gmail.com>
2023-11-23 13:55:37 +0100

    .

--- adapters/repos/db/shard_lazyloader.go
+++ adapters/repos/db/shard_lazyloader.go
@@ -35,7 +35,6 @@ import (
 	"github.com/weaviate/weaviate/entities/searchparams"
 	"github.com/weaviate/weaviate/entities/storagestate"
 	"github.com/weaviate/weaviate/entities/storobj"
-	"github.com/weaviate/weaviate/usecases/config"
 	"github.com/weaviate/weaviate/usecases/monitoring"
 	"github.com/weaviate/weaviate/usecases/objects"
 	"github.com/weaviate/weaviate/usecases/replica"
