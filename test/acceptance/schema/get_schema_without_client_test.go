2642a99106c8b37d629eb61bfb64b793adace474
Abdel Rodríguez <abdelr1982@gmail.com>
2023-12-06 00:08:54 +0100

    fixing acceptance tests

--- test/acceptance/schema/get_schema_without_client_test.go
+++ test/acceptance/schema/get_schema_without_client_test.go
@@ -49,6 +49,9 @@ func testGetSchemaWithoutClient(t *testing.T) {
 					"dynamicEfMax":           float64(500),
 					"dynamicEfFactor":        float64(8),
 					"distance":               "cosine",
+					"bq": map[string]interface{}{
+						"enabled": false,
+					},
 					"pq": map[string]interface{}{
 						"bitCompression": false,
 						"centroids":      float64(256),
