b84b18d0086871dc9e64b78dd86a7d816c55c3f0
Parker Duckworth <parkerduckworth@gmail.com>
2023-12-13 12:38:24 -0500

    implicit tenant creation + tests

--- adapters/handlers/rest/embedded_spec.go
+++ adapters/handlers/rest/embedded_spec.go
@@ -3960,6 +3960,11 @@ func init() {
     "MultiTenancyConfig": {
       "description": "Configuration related to multi-tenancy within a class",
       "properties": {
+        "autoTenantCreation": {
+          "description": "Nonexistent tenants should (not) be created implicitly",
+          "type": "boolean",
+          "x-omitempty": false
+        },
         "enabled": {
           "description": "Whether or not multi-tenancy is enabled for this class",
           "type": "boolean",
@@ -9201,6 +9206,11 @@ func init() {
     "MultiTenancyConfig": {
       "description": "Configuration related to multi-tenancy within a class",
       "properties": {
+        "autoTenantCreation": {
+          "description": "Nonexistent tenants should (not) be created implicitly",
+          "type": "boolean",
+          "x-omitempty": false
+        },
         "enabled": {
           "description": "Whether or not multi-tenancy is enabled for this class",
           "type": "boolean",
