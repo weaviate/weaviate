e498c8fb50e6bf2a3bbd360cc9acf1bda44d8a51
Jeronimo <jeronimo.irazabal@gmail.com>
2023-12-14 13:31:50 +0100

    chore: initial implementation of difference readers
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>

---
+++ usecases/replica/hashtree/hashtree_diff_reader.go
@@ -0,0 +1,49 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2023 Weaviate B.V. All rights reserved.
+//
+//  CONTACT: hello@weaviate.io
+//
+
+package hashtree
+
+import "errors"
+
+var ErrNoMoreDifferences = errors.New("no more differences")
+
+type HashTreeDiffReader struct {
+	diff         *Bitset
+	firstLeafPos int
+	pos          int
+}
+
+func (ht *HashTree) NewDiffReader(diff *Bitset) *HashTreeDiffReader {
+	if diff == nil || diff.Size() != NodesCount(ht.Height()) {
+		panic("illegal diff")
+	}
+
+	firstLeafPos := NodesCount(ht.Height() - 1)
+
+	return &HashTreeDiffReader{
+		diff:         diff,
+		firstLeafPos: firstLeafPos,
+		pos:          firstLeafPos,
+	}
+}
+
+func (r *HashTreeDiffReader) Next() (int, error) {
+	for ; r.pos < r.diff.Size() && !r.diff.IsSet(r.pos); r.pos++ {
+	}
+
+	if r.pos == r.diff.Size() {
+		return 0, ErrNoMoreDifferences
+	}
+
+	r.pos++
+
+	return r.pos - 1 - r.firstLeafPos, nil
+}
