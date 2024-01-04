93241669bc6283bfb5a4620ea664d1d920326b43
Loic Reyreaud <loic@weaviate.io>
2024-01-04 12:58:25 +0100

    re-add class property merge in RAFT using update class
    Signed-off-by: Loic Reyreaud <loic@weaviate.io>

--- usecases/schema/property.go
+++ usecases/schema/property.go
@@ -16,9 +16,12 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/pkg/errors"
 	"github.com/weaviate/weaviate/entities/models"
 	"github.com/weaviate/weaviate/entities/schema"
 )
+
+var errPropertyNotFound = errors.New("property not found")
 
 // AddClassProperty to an existing Class
 func (h *Handler) AddClassProperty(ctx context.Context, principal *models.Principal,
@@ -135,120 +138,64 @@ func (m *Handler) validatePropModuleConfig(class *models.Class, prop *models.Pro
 	return nil
 }
 
+func mergeObjectProperty(c *models.Class, p *models.Property) error {
+	var prop *models.Property
+	for i := range c.Properties {
+		if c.Properties[i].Name == p.Name {
+			prop = c.Properties[i]
+			break
+		}
+	}
+	if prop == nil {
+		return errPropertyNotFound
+	}
+
+	prop.NestedProperties, _ = schema.MergeRecursivelyNestedProperties(prop.NestedProperties, p.NestedProperties)
+	return nil
+}
+
 // MergeClassObjectProperty of an existing Class
 // Merges NestedProperties of incoming object/object[] property into existing one
-func (h *Handler) MergeClassObjectProperty(ctx context.Context, principal *models.Principal,
-	class string, property *models.Property,
+func (h *Handler) MergeClassObjectProperty(
+	ctx context.Context,
+	principal *models.Principal,
+	className string,
+	prop *models.Property,
 ) error {
 	err := h.Authorizer.Authorize(principal, "update", "schema/objects")
 	if err != nil {
 		return err
 	}
 
-	// TODO-RAFT
-	return nil
-	// return h.mergeClassObjectProperty(ctx, class, property)
-}
+	class := h.metaReader.ReadOnlyClass(className)
+	prop.Name = schema.LowercaseFirstLetter(prop.Name)
 
-// func (h *Handler) mergeClassObjectProperty(ctx context.Context,
-// 	className string, prop *models.Property,
-// ) error {
-// 	return nil
-// h.Lock()
-// defer h.Unlock()
-
-// class, err := h.schemaCache.readOnlyClass(className)
-// if err != nil {
-// 	return err
-// }
-// prop.Name = schema.LowercaseFirstLetter(prop.Name)
-
-	// // reuse setDefaults/validation/migrate methods coming from add property
-	// // (empty existing names map, to validate existing updated property)
-	// // TODO nested - refactor / cleanup setDefaults/validation/migrate methods
-	// m.setNewPropDefaults(class, prop)
-	// if err := m.validateProperty(prop, class, map[string]bool{}, false); err != nil {
-	// 	return err
-	// }
-	// // migrate only after validation in completed
-	// migratePropertySettings(prop)
-
-	// tx, err := m.cluster.BeginTransaction(ctx, mergeObjectProperty,
-	// 	MergeObjectPropertyPayload{className, prop}, DefaultTxTTL)
-	// if err != nil {
-	// 	// possible causes for errors could be nodes down (we expect every node to
-	// 	// the up for a schema transaction) or concurrent transactions from other
-	// 	// nodes
-	// 	return errors.Wrap(err, "open cluster-wide transaction")
-	// }
-
-	// if err := m.cluster.CommitWriteTransaction(ctx, tx); err != nil {
-	// 	// Only log the commit error, but do not abort the changes locally. Once
-	// 	// we've told others to commit, we also need to commit ourselves!
-	// 	//
-	// 	// The idea is that if we abort our changes we are guaranteed to create an
-	// 	// inconsistency as soon as any other node honored the commit. This would
-	// 	// for example be the case in a 3-node cluster where node 1 is the
-	// 	// coordinator, node 2 honored the commit and node 3 died during the commit
-	// 	// phase.
-	// 	//
-	// 	// In this scenario it is far more desirable to make sure that node 1 and
-	// 	// node 2 stay in sync, as node 3 - who may or may not have missed the
-	// 	// update - can use a local WAL from the first TX phase to replay any
-	// 	// missing changes once it's back.
-	// 	m.logger.WithError(err).Errorf("not every node was able to commit")
-	// }
-
-	// return m.mergeClassObjectPropertyApplyChanges(ctx, className, prop)
-}
-
-func (h *Handler) mergeClassObjectPropertyApplyChanges(ctx context.Context,
-	className string, prop *models.Property,
-) error {
-	// class, err := m.schemaCache.mergeObjectProperty(className, prop)
-	// if err != nil {
-	// 	return err
-	// }
-	// metadata, err := json.Marshal(&class)
-	// if err != nil {
-	// 	return fmt.Errorf("marshal class %s: %w", className, err)
-	// }
-	// h.logger.
-	// 	WithField("action", "schema.update_object_property").
-	// 	Debug("saving updated schema to configuration store")
-	// err = h.repo.UpdateClass(ctx, ClassPayload{Name: className, Metadata: metadata})
-	// if err != nil {
-	// 	return err
-	// }
-	// h.triggerSchemaUpdateCallbacks()
-
-	// // TODO nested - implement MergeObjectProperty (needed for indexing/filtering)
-	// // will result in a mismatch between schema and index if function below fails
-	// // return m.migrator.MergeObjectProperty(ctx, className, prop)
-	return nil
-}
-
-func (h *Handler) deduplicateProps(props []*models.Property,
-	className string,
-) []*models.Property {
-	seen := map[string]struct{}{}
-	i := 0
-	for j, prop := range props {
-		name := strings.ToLower(prop.Name)
-		if _, ok := seen[name]; ok {
-			h.logger.WithFields(logrus.Fields{
-				"action": "startup_repair_schema",
-				"prop":   prop.Name,
-				"class":  className,
-			}).Warningf("removing duplicate property %s", prop.Name)
-			continue
-		}
-		if i != j {
-			props[i] = prop
-		}
-		seen[name] = struct{}{}
-		i++
+	// reuse setDefaults/validation/migrate methods coming from add property
+	// (empty existing names map, to validate existing updated property)
+	// TODO nested - refactor / cleanup setDefaults/validation/migrate methods
+	if err := h.setNewPropDefaults(class, prop); err != nil {
+		return err
+	}
+	if err := h.validateProperty(prop, className, map[string]bool{}, false); err != nil {
+		return err
 	}
 
-	return props[:i]
+	// migrate only after validation in completed
+	migratePropertySettings(prop)
+
+	err = mergeObjectProperty(class, prop)
+	if err != nil {
+		return err
+	}
+
+	h.logger.WithField("action", "schema.update_object_property").Debugf("updating class %s after property merge", className)
+	err = h.metaWriter.UpdateClass(class, nil)
+	if err != nil {
+		return err
+	}
+
+	// TODO: implement MergeObjectProperty (needed for indexing/filtering)
+	// will result in a mismatch between schema and index if function below fails
+	// return h.migrator.MergeObjectProperty(ctx, className, prop)
+	return nil
 }
