a01063151811e4fed0a3bf2665e0385a28013d9f
Mohamed Badawi <contact@m-mooga.com>
2024-01-30 10:25:34 +0100

    feat: raft cluster recovery support (#4087)
    * feat: raft cluster recovery support

--- usecases/config/config_handler.go
+++ usecases/config/config_handler.go
@@ -299,6 +299,7 @@ type Raft struct {
 	Join              []string
 	SnapshotThreshold uint64
 	HeartbeatTimeout  time.Duration
+	RecoveryTimeout   time.Duration
 	ElectionTimeout   time.Duration
 	SnapshotInterval  time.Duration
 
