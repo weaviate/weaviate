3c72f07b8610959155d2c9c0e0b5f6258b3f30e3
Connor Shorten <connorshorten300@gmail.com>
2024-02-02 09:12:28 -0500

    Starting class settings

---
+++ modules/generative-ollama/config/class_settings.go
@@ -0,0 +1,87 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2024 Weaviate B.V. All rights reserved.
+//
+//  CONTACT: hello@weaviate.io
+//
+
+package config
+
+import (
+	"github.com/pkg/errors"
+	"github.com/weaviate/weaviate/entities/models"
+	"github.com/weaviate/weaviate/entities/moduletools"
+)
+
+const (
+	modelProperty = "model"
+)
+
+var availableOllamaModels = []string{
+	"llama2",
+}
+
+// note we might want to separate the baseURL and completions URL in the future. Fine-tuned models also use this URL. 12/3/23
+var (
+	DefaultOllamaModel = "llama2"
+)
+
+type classSettings struct {
+	cfg moduletools.ClassConfig
+}
+
+func NewClassSettings(cfg moduletools.ClassConfig) *classSettings {
+	return &classSettings{cfg: cfg}
+}
+
+func (ic *classSettings) Validate(class *models.Class) error {
+	if ic.cfg == nil {
+		// we would receive a nil-config on cross-class requests, such as Explore{}
+		return errors.New("empty config")
+	}
+	model := ic.getStringProperty(modelProperty, DefaultOllamaModel)
+	if model == nil || !ic.validateModel(*model) {
+		return errors.Errorf("wrong Ollama model name, available model names are: %v", availableOllamaModels)
+	}
+
+	return nil
+}
+
+func (ic *classSettings) getStringProperty(name, defaultValue string) *string {
+	if ic.cfg == nil {
+		// we would receive a nil-config on cross-class requests, such as Explore{}
+		return &defaultValue
+	}
+
+	model, ok := ic.cfg.ClassByModuleName("generative-ollama")[name]
+	if ok {
+		asString, ok := model.(string)
+		if ok {
+			return &asString
+		}
+		var empty string
+		return &empty
+	}
+	return &defaultValue
+}
+
+func (ic *classSettings) validateModel(model string) bool {
+	return contains(availableOllamaModels, model)
+}
+
+func (ic *classSettings) Model() string {
+	return *ic.getStringProperty(modelProperty, DefaultOllamaModel)
+}
+
+func contains[T comparable](s []T, e T) bool {
+	for _, v := range s {
+		if v == e {
+			return true
+		}
+	}
+	return false
+}
