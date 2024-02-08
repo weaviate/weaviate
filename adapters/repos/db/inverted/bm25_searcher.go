a52a592495639a6778c864b0d7e857f06abe1e0a
Jeremy Price <donomii@gmail.com>
2024-02-08 23:33:14 +0900

    Don't prealloc results

--- adapters/repos/db/inverted/bm25_searcher.go
+++ adapters/repos/db/inverted/bm25_searcher.go
@@ -161,8 +161,8 @@ func (b *BM25Searcher) wand(
 
 	averagePropLength = averagePropLength / float64(len(params.Properties))
 
-	results := make(terms, 100)
-	indices := make([]map[uint64]int, 100)
+	results := terms{}
+	indices := []map[uint64]int{}
 
 	var eg errgroup.Group
 	eg.SetLimit(_NUMCPU)
