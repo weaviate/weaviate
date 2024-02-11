038f5f73aa369a0dfb7a126f1cc79afe9f29ed10
Marcin Antas <antas.marcin@gmail.com>
2024-02-11 12:19:27 +0100

    Add support for generic nearImage argument

--- usecases/modulecomponents/nearImage/graphql_argument.go
+++ usecases/modulecomponents/nearImage/graphql_argument.go
@@ -57,5 +57,9 @@ func nearImageFields(prefix string) graphql.InputObjectConfigFieldMap {
 			Description: descriptions.Distance,
 			Type:        graphql.Float,
 		},
+		"targetVectors": &graphql.InputObjectFieldConfig{
+			Description: "Target vectors",
+			Type:        graphql.NewList(graphql.String),
+		},
 	}
 }
