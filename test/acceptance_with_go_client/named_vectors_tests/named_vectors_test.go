80777cd77832ab42fdf758ee8fd24a697b94e026
Marcin Antas <antas.marcin@gmail.com>
2024-02-13 21:03:17 +0100

    Add support for none vectorizer

--- test/acceptance_with_go_client/named_vectors_tests/named_vectors_test.go
+++ test/acceptance_with_go_client/named_vectors_tests/named_vectors_test.go
@@ -30,6 +30,8 @@ func TestNamedVectors_SingleNode(t *testing.T) {
 	t.Run("schema", testCreateSchema(t, endpoint))
 	t.Run("objects", testCreateObject(t, endpoint))
 	t.Run("batch", testBatchObject(t, endpoint))
+	t.Run("none vectorizer", testCreateSchemaWithNoneVectorizer(t, endpoint))
+	t.Run("mixed objects with none and vectorizer", testCreateSchemaWithMixedVectorizers(t, endpoint))
 }
 
 func TestNamedVectors_Cluster(t *testing.T) {
@@ -40,9 +42,11 @@ func TestNamedVectors_Cluster(t *testing.T) {
 		require.NoError(t, compose.Terminate(ctx))
 	}()
 	endpoint := compose.GetWeaviate().URI()
-	t.Run("schema", testCreateSchema(t, endpoint))
+	t.Run("schema", testCreateSchemaWithNoneVectorizer(t, endpoint))
 	t.Run("object", testCreateObject(t, endpoint))
 	t.Run("batch", testBatchObject(t, endpoint))
+	t.Run("none vectorizer", testCreateSchemaWithNoneVectorizer(t, endpoint))
+	t.Run("mixed objects with none and vectorizer", testCreateSchemaWithMixedVectorizers(t, endpoint))
 }
 
 func createSingleNodeEnvironment(ctx context.Context) (compose *docker.DockerCompose, err error) {
