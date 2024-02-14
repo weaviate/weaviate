976ce033d2451a79a130cc168fbfbaea8f1dd24f
Marcin Antas <antas.marcin@gmail.com>
2024-02-14 08:27:41 +0100

    Add all generic arguments

--- usecases/modulecomponents/generic_module.go
+++ usecases/modulecomponents/generic_module.go
@@ -16,8 +16,14 @@ import (
 
 	"github.com/tailor-inc/graphql"
 	"github.com/weaviate/weaviate/entities/modulecapabilities"
+	"github.com/weaviate/weaviate/usecases/modulecomponents/additional"
+	"github.com/weaviate/weaviate/usecases/modulecomponents/arguments/nearAudio"
+	"github.com/weaviate/weaviate/usecases/modulecomponents/arguments/nearDepth"
 	"github.com/weaviate/weaviate/usecases/modulecomponents/arguments/nearImage"
+	"github.com/weaviate/weaviate/usecases/modulecomponents/arguments/nearImu"
 	"github.com/weaviate/weaviate/usecases/modulecomponents/arguments/nearText"
+	"github.com/weaviate/weaviate/usecases/modulecomponents/arguments/nearThermal"
+	"github.com/weaviate/weaviate/usecases/modulecomponents/arguments/nearVideo"
 )
 
 type ArgumentType int
@@ -28,12 +34,24 @@ const (
 	Explore
 )
 
-func GetGenericArgument(name, className string, argumentType ArgumentType) *graphql.ArgumentConfig {
+func GetGenericArgument(name, className string, argumentType ArgumentType,
+	nearTextTransformer modulecapabilities.TextTransform,
+) *graphql.ArgumentConfig {
 	switch name {
-	case "nearText":
-		return getGenericArgument(nearText.New(nil).Arguments()[name], className, argumentType)
-	case "nearImage":
+	case nearText.NAME:
+		return getGenericArgument(nearText.New(nearTextTransformer).Arguments()[name], className, argumentType)
+	case nearImage.NAME:
 		return getGenericArgument(nearImage.New().Arguments()[name], className, argumentType)
+	case nearAudio.NAME:
+		return getGenericArgument(nearAudio.New().Arguments()[name], className, argumentType)
+	case nearDepth.NAME:
+		return getGenericArgument(nearDepth.New().Arguments()[name], className, argumentType)
+	case nearImu.NAME:
+		return getGenericArgument(nearImu.New().Arguments()[name], className, argumentType)
+	case nearThermal.NAME:
+		return getGenericArgument(nearThermal.New().Arguments()[name], className, argumentType)
+	case nearVideo.NAME:
+		return getGenericArgument(nearVideo.New().Arguments()[name], className, argumentType)
 	default:
 		panic(fmt.Sprintf("Unknown generic argument: %s", name))
 	}
@@ -51,5 +69,15 @@ func getGenericArgument(arg modulecapabilities.GraphQLArgument,
 		return arg.ExploreArgumentsFunction()
 	default:
 		panic(fmt.Sprintf("Unknown argument type: %v", argumentType))
+	}
+}
+
+func GetGenericAdditionalProperty(name, className string) *modulecapabilities.AdditionalProperty {
+	switch name {
+	case "featureProjection":
+		fp := additional.NewText2VecProvider().AdditionalProperties()["featureProjection"]
+		return &fp
+	default:
+		return nil
 	}
 }
