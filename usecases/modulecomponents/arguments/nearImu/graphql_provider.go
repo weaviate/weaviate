c283867ef6fb390abc27fe1364802057b9c5efc2
Marcin Antas <antas.marcin@gmail.com>
2024-02-14 08:27:41 +0100

    Add all generic arguments

--- usecases/modulecomponents/arguments/nearImu/graphql_provider.go
+++ usecases/modulecomponents/arguments/nearImu/graphql_provider.go
@@ -15,6 +15,8 @@ import (
 	"github.com/weaviate/weaviate/entities/modulecapabilities"
 )
 
+const NAME = "nearIMU"
+
 type GraphQLArgumentsProvider struct{}
 
 func New() *GraphQLArgumentsProvider {
@@ -23,7 +25,7 @@ func New() *GraphQLArgumentsProvider {
 
 func (g *GraphQLArgumentsProvider) Arguments() map[string]modulecapabilities.GraphQLArgument {
 	arguments := map[string]modulecapabilities.GraphQLArgument{}
-	arguments["nearIMU"] = g.getNearIMU()
+	arguments[NAME] = g.getNearIMU()
 	return arguments
 }
 
