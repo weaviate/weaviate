d0a206bd6d2872c49ab3e148d862a4117a439892
Dirk Kulawiak <dirk@semi.technology>
2024-03-08 10:05:38 +0100

    Add custom linter for direct error group usage

---
+++ tools/linter_error_groups.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Get all files with 'errgroup' in them. The only place where direct usage is allowed is in error_group_wrapper.go
+files=$(git ls-files | xargs grep -l 'errgroup')
+
+for file in $files; do
+    # Check if the file is not one of the permitted usages
+    if [ "$file" != "entities/errors/error_group_wrapper.go" ] && [ "$file" != "tools/linter_error_groups.sh" ]; then
+        echo "Error: $file directly uses error groups. Please use entities/errors/error_group_wrapper.go instead."
+        exit 1
+    fi
+done
