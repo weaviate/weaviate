a7b9030bc39eca311c9da33de761a71f6d539d6a
moogacs <contact@m-mooga.com>
2024-03-14 22:02:40 +0100

    add more

--- test/acceptance/replication/crud_test.go
+++ test/acceptance/replication/crud_test.go
@@ -23,6 +23,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/weaviate/weaviate/client/objects"
+	enterrors "github.com/weaviate/weaviate/entities/errors"
 	"github.com/weaviate/weaviate/entities/models"
 	"github.com/weaviate/weaviate/entities/schema/crossref"
 	"github.com/weaviate/weaviate/test/docker"
