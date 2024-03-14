1df3a823f776110f7c7ce65d2e4e41b4418abd8c
Abdel Rodríguez <abdelr1982@gmail.com>
2024-03-14 10:39:56 +0100

    Supporting restarts

--- adapters/repos/db/vector/composer/restore_integration_test.go
+++ adapters/repos/db/vector/composer/restore_integration_test.go
@@ -13,13 +13,13 @@ package composer_test
 
 import (
 	"context"
-	"fmt"
+	"math"
 	"sync"
 	"testing"
-	"time"
 
+	"github.com/sirupsen/logrus/hooks/test"
 	"github.com/stretchr/testify/assert"
-	"github.com/weaviate/weaviate/adapters/repos/db/vector/common"
+	"github.com/stretchr/testify/require"
 	"github.com/weaviate/weaviate/adapters/repos/db/vector/composer"
 	"github.com/weaviate/weaviate/adapters/repos/db/vector/compressionhelpers"
 	"github.com/weaviate/weaviate/adapters/repos/db/vector/hnsw"
@@ -32,19 +32,22 @@ import (
 	hnswent "github.com/weaviate/weaviate/entities/vectorindex/hnsw"
 )
 
-func TestComposer(t *testing.T) {
+func TestBackup_Integration(t *testing.T) {
 	dimensions := 20
 	vectors_size := 10_000
 	queries_size := 10
 	k := 10
 
 	vectors, queries := testinghelpers.RandomVecs(vectors_size, queries_size, dimensions)
-	rootPath := t.TempDir()
-	distancer := distancer.NewL2SquaredProvider()
 	truths := make([][]uint64, queries_size)
+	distancer := distancer.NewL2SquaredProvider()
 	compressionhelpers.Concurrently(uint64(len(queries)), func(i uint64) {
 		truths[i], _ = testinghelpers.BruteForce(vectors, queries[i], k, distanceWrapper(distancer))
 	})
+	logger, _ := test.NewNullLogger()
+
+	dirName := t.TempDir()
+	indexID := "restore-integration-test"
 	noopCallback := cyclemanager.NewCallbackGroupNoop()
 	fuc := flatent.UserConfig{}
 	fuc.SetDefaults()
@@ -54,11 +57,15 @@ func TestComposer(t *testing.T) {
 		EF:                    32,
 		VectorCacheMaxObjects: 1_000_000,
 	}
-	composer, err := composer.New(composer.Config{
-		RootPath:              rootPath,
-		ID:                    "nil-vector-test",
-		MakeCommitLoggerThunk: hnsw.MakeNoopCommitLogger,
-		DistanceProvider:      distancer,
+
+	config := composer.Config{
+		RootPath:         dirName,
+		ID:               indexID,
+		Logger:           logger,
+		DistanceProvider: distancer,
+		MakeCommitLoggerThunk: func() (hnsw.CommitLogger, error) {
+			return hnsw.NewCommitLogger(dirName, indexID, logger, noopCallback)
+		},
 		VectorForIDThunk: func(ctx context.Context, id uint64) ([]float32, error) {
 			vec := vectors[int(id)]
 			if vec == nil {
@@ -70,66 +77,39 @@ func TestComposer(t *testing.T) {
 		TombstoneCallbacks:       noopCallback,
 		ShardCompactionCallbacks: noopCallback,
 		ShardFlushCallbacks:      noopCallback,
-	}, ent.UserConfig{
+	}
+
+	uc := ent.UserConfig{
 		Threeshold: uint64(vectors_size),
 		Distance:   distancer.Type(),
 		HnswUC:     hnswuc,
 		FlatUC:     fuc,
-	}, testinghelpers.NewDummyStore(t))
-	assert.Nil(t, err)
+	}
+
+	store := testinghelpers.NewDummyStore(t)
+
+	idx, err := composer.New(config, uc, store)
+	require.Nil(t, err)
+	idx.PostStartup()
 
 	compressionhelpers.Concurrently(uint64(vectors_size), func(i uint64) {
-		composer.Add(i, vectors[i])
+		idx.Add(i, vectors[i])
 	})
-	shouldUpgrade, at := composer.ShouldUpgrade()
-	assert.True(t, shouldUpgrade)
-	assert.Equal(t, vectors_size, at)
-	assert.False(t, composer.Upgraded())
-	recall1, latency1 := recallAndLatency(queries, k, composer, truths)
-	fmt.Println(recall1, latency1)
-	assert.True(t, recall1 > 0.99)
+
 	wg := sync.WaitGroup{}
 	wg.Add(1)
-	composer.Upgrade(func() {
+	idx.Upgrade(func() {
 		wg.Done()
 	})
 	wg.Wait()
-	shouldUpgrade, _ = composer.ShouldUpgrade()
-	assert.False(t, shouldUpgrade)
-	recall2, latency2 := recallAndLatency(queries, k, composer, truths)
-	fmt.Println(recall2, latency2)
-	assert.True(t, recall2 > 0.9)
-	assert.True(t, latency1 > latency2)
-}
+	recall1, _ := recallAndLatency(queries, k, idx, truths)
+	assert.True(t, recall1 > 0.9)
 
-func recallAndLatency(queries [][]float32, k int, index composer.VectorIndex, truths [][]uint64) (float32, float32) {
-	var relevant uint64
-	var retrieved int
+	assert.Nil(t, idx.Shutdown(context.Background()))
+	idx, err = composer.New(config, uc, store)
+	require.Nil(t, err)
+	idx.PostStartup()
 
-	var querying time.Duration = 0
-	compressionhelpers.Concurrently(uint64(len(queries)), func(i uint64) {
-		before := time.Now()
-		results, _, _ := index.SearchByVector(queries[i], k, nil)
-		querying += time.Since(before)
-		retrieved += k
-		relevant += testinghelpers.MatchesInLists(truths[i], results)
-	})
-
-	recall := float32(relevant) / float32(retrieved)
-	latency := float32(querying.Microseconds()) / float32(len(queries))
-	return recall, latency
-}
-
-func TempVectorForIDThunk(vectors [][]float32) func(context.Context, uint64, *common.VectorSlice) ([]float32, error) {
-	return func(ctx context.Context, id uint64, container *common.VectorSlice) ([]float32, error) {
-		copy(container.Slice, vectors[int(id)])
-		return vectors[int(id)], nil
-	}
-}
-
-func distanceWrapper(provider distancer.Provider) func(x, y []float32) float32 {
-	return func(x, y []float32) float32 {
-		dist, _, _ := provider.SingleDist(x, y)
-		return dist
-	}
+	recall2, _ := recallAndLatency(queries, k, idx, truths)
+	assert.True(t, math.Abs(float64(recall1-recall2)) <= 0.1)
 }
