47c667f3b7aa4a335d370dfba345a7017f8435e1
Dirk Kulawiak <dirk@semi.technology>
2024-03-14 05:51:26 +0100

    Fix loop vars

--- adapters/handlers/rest/handlers_graphql.go
+++ adapters/handlers/rest/handlers_graphql.go
@@ -175,6 +175,7 @@ func setupGraphQLHandlers(
 
 		// Generate a goroutine for each separate request
 		for requestIndex, unbatchedRequest := range params.Body {
+			requestIndex, unbatchedRequest := requestIndex, unbatchedRequest
 			wg.Add(1)
 			enterrors.GoWrapper(func() {
 				handleUnbatchedGraphQLRequest(ctx, wg, graphQL, unbatchedRequest, requestIndex, &requestResults, metricRequestsTotal)
