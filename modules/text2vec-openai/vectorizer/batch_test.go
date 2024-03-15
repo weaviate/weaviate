3e312f2747b1337af103709b878066041fc17717
Dirk Kulawiak <dirk@semi.technology>
2024-03-15 15:21:47 +0100

    Add tests for zero remaining requests

--- modules/text2vec-openai/vectorizer/batch_test.go
+++ modules/text2vec-openai/vectorizer/batch_test.go
@@ -39,7 +39,7 @@ func TestBatch(t *testing.T) {
 		{name: "first object errors", objects: []*models.Object{{Class: "Car", Properties: map[string]interface{}{"test": "error something"}}, {Class: "Car", Properties: map[string]interface{}{"test": "test"}}}, skip: []bool{false, false}, wantErrors: map[int]error{0: fmt.Errorf("something")}},
 		{name: "vectorize all", objects: []*models.Object{{Class: "Car", Properties: map[string]interface{}{"test": "test"}}, {Class: "Car", Properties: map[string]interface{}{"test": "something"}}}, skip: []bool{false, false}},
 		{name: "multiple vectorizer batches", objects: []*models.Object{
-			{Class: "Car", Properties: map[string]interface{}{"test": "rate 25"}}, // set limit so next 3 objects are one batch
+			{Class: "Car", Properties: map[string]interface{}{"test": "tokens 25"}}, // set limit so next 3 objects are one batch
 			{Class: "Car", Properties: map[string]interface{}{"test": "first object first batch"}},
 			{Class: "Car", Properties: map[string]interface{}{"test": "second object first batch"}},
 			{Class: "Car", Properties: map[string]interface{}{"test": "third object first batch"}},
@@ -49,7 +49,7 @@ func TestBatch(t *testing.T) {
 			{Class: "Car", Properties: map[string]interface{}{"test": "fourth object second batch"}},
 		}, skip: []bool{false, false, false, false, false, false, false, false}},
 		{name: "multiple vectorizer batches with skips and errors", objects: []*models.Object{
-			{Class: "Car", Properties: map[string]interface{}{"test": "rate 25"}}, // set limit so next 3 objects are one batch
+			{Class: "Car", Properties: map[string]interface{}{"test": "tokens 25"}}, // set limit so next 3 objects are one batch
 			{Class: "Car", Properties: map[string]interface{}{"test": "first object first batch"}},
 			{Class: "Car", Properties: map[string]interface{}{"test": "second object first batch"}},
 			{Class: "Car", Properties: map[string]interface{}{"test": "error something"}},
@@ -59,12 +59,12 @@ func TestBatch(t *testing.T) {
 			{Class: "Car", Properties: map[string]interface{}{"test": "fourth object second batch"}},
 		}, skip: []bool{false, true, false, false, false, true, false, false}, wantErrors: map[int]error{3: fmt.Errorf("something")}},
 		{name: "token too long", objects: []*models.Object{
-			{Class: "Car", Properties: map[string]interface{}{"test": "rate 5"}}, // set limit
+			{Class: "Car", Properties: map[string]interface{}{"test": "tokens 5"}}, // set limit
 			{Class: "Car", Properties: map[string]interface{}{"test": "long long long long, long, long, long, long"}},
 			{Class: "Car", Properties: map[string]interface{}{"test": "short"}},
 		}, skip: []bool{false, false, false}, wantErrors: map[int]error{1: fmt.Errorf("text too long for vectorization")}},
 		{name: "token too long, last item in batch", objects: []*models.Object{
-			{Class: "Car", Properties: map[string]interface{}{"test": "rate 5"}}, // set limit
+			{Class: "Car", Properties: map[string]interface{}{"test": "tokens 5"}}, // set limit
 			{Class: "Car", Properties: map[string]interface{}{"test": "short"}},
 			{Class: "Car", Properties: map[string]interface{}{"test": "long long long long, long, long, long, long"}},
 		}, skip: []bool{false, false, false}, wantErrors: map[int]error{2: fmt.Errorf("text too long for vectorization")}},
@@ -74,7 +74,7 @@ func TestBatch(t *testing.T) {
 			{Class: "Car", Properties: map[string]interface{}{"test": "second object first batch"}},
 		}, skip: []bool{false, false, true}},
 		{name: "deadline", deadline: 200 * time.Millisecond, objects: []*models.Object{
-			{Class: "Car", Properties: map[string]interface{}{"test": "rate 15"}}, // set limit so next two items are in a batch
+			{Class: "Car", Properties: map[string]interface{}{"test": "tokens 15"}}, // set limit so next two items are in a batch
 			{Class: "Car", Properties: map[string]interface{}{"test": "wait 200"}},
 			{Class: "Car", Properties: map[string]interface{}{"test": "long long long long"}},
 			{Class: "Car", Properties: map[string]interface{}{"test": "next batch, will be aborted due to context deadline"}},
@@ -162,12 +162,37 @@ func TestBatchTimeouts(t *testing.T) {
 			v := New(client, tt.batchTime)
 
 			_, errs := v.ObjectBatch(context.Background(), []*models.Object{
-				{Class: "Car", Properties: map[string]interface{}{"test": "rate 11"}}, // first request, set rate down so the next two items can be sent
-				{Class: "Car", Properties: map[string]interface{}{"test": "wait 90"}}, // second batch, use up batch time to trigger waiting for refresh
-				{Class: "Car", Properties: map[string]interface{}{"test": "rate 11"}}, // set next rate so the next object is too long. Depending on the total batch time it either sleeps or not
+				{Class: "Car", Properties: map[string]interface{}{"test": "tokens 13"}}, // first request, set rate down so the next two items can be sent
+				{Class: "Car", Properties: map[string]interface{}{"test": "wait 90"}},   // second batch, use up batch time to trigger waiting for refresh
+				{Class: "Car", Properties: map[string]interface{}{"test": "tokens 11"}}, // set next rate so the next object is too long. Depending on the total batch time it either sleeps or not
 				{Class: "Car", Properties: map[string]interface{}{"test": "next batch long long long long long long"}},
 			}, []bool{false, false, false, false}, cfg)
 
+			require.Len(t, errs, tt.expectedErrors)
+		})
+	}
+}
+
+func TestBatchRequestLimit(t *testing.T) {
+	client := &fakeBatchClient{defaultResetRate: 1}
+	cfg := &fakeClassConfig{vectorizePropertyName: false, classConfig: map[string]interface{}{"vectorizeClassName": false}}
+	thirtyTokens := "ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab ab"
+
+	cases := []struct {
+		batchTime      time.Duration
+		expectedErrors int
+	}{
+		{batchTime: 100 * time.Millisecond, expectedErrors: 1},
+		{batchTime: 2 * time.Second, expectedErrors: 0},
+	}
+	for _, tt := range cases {
+		t.Run("", func(t *testing.T) {
+			v := New(client, tt.batchTime)
+
+			_, errs := v.ObjectBatch(context.Background(), []*models.Object{
+				{Class: "Car", Properties: map[string]interface{}{"test": "requests 0"}},                               // wait for the rate limit to reset
+				{Class: "Car", Properties: map[string]interface{}{"test": "requests 0" + thirtyTokens + thirtyTokens}}, // fill up default limit of 100 tokens
+			}, []bool{false, false, false, false}, cfg)
 			require.Len(t, errs, tt.expectedErrors)
 		})
 	}
--- modules/text2vec-openai/vectorizer/fakes_for_test.go
+++ modules/text2vec-openai/vectorizer/fakes_for_test.go
@@ -15,6 +15,7 @@ import (
 	"context"
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/weaviate/weaviate/modules/text2vec-openai/ent"
@@ -38,16 +39,27 @@ func (c *fakeBatchClient) Vectorize(ctx context.Context,
 
 	vectors := make([][]float32, len(text))
 	errors := make([]error, len(text))
-	rateLimit := &ent.RateLimits{RemainingTokens: 100, RemainingRequests: 100, LimitTokens: 200, ResetTokens: c.defaultResetRate}
+	rateLimit := &ent.RateLimits{RemainingTokens: 100, RemainingRequests: 100, LimitTokens: 200, ResetTokens: c.defaultResetRate, ResetRequests: 1}
 	for i := range text {
 		if len(text[i]) >= len("error ") && text[i][:6] == "error " {
 			errors[i] = fmt.Errorf(text[i][6:])
 			continue
 		}
-		if len(text[i]) >= len("rate ") && text[i][:5] == "rate " {
-			rate, _ := strconv.Atoi(text[i][5:])
-			rateLimit = &ent.RateLimits{RemainingTokens: rate, RemainingRequests: rate, LimitTokens: 2 * rate, ResetTokens: c.defaultResetRate}
+
+		tok := len("tokens ")
+		if len(text[i]) >= tok && text[i][:tok] == "tokens " {
+			rate, _ := strconv.Atoi(text[i][tok:])
+			rateLimit.RemainingTokens = rate
+			rateLimit.LimitTokens = 2 * rate
 		}
+
+		req := len("requests ")
+		if len(text[i]) >= req && text[i][:req] == "requests " {
+			reqs, _ := strconv.Atoi(strings.Split(text[i][req:], " ")[0])
+			rateLimit.RemainingRequests = reqs
+			rateLimit.LimitRequests = 2 * reqs
+		}
+
 		if len(text[i]) >= len("wait ") && text[i][:5] == "wait " {
 			wait, _ := strconv.Atoi(text[i][5:])
 			time.Sleep(time.Duration(wait) * time.Millisecond)
