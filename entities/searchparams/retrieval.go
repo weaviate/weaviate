a70f0acf1e70bd9f74e6a155f69d94a491067fe5
Jeremy Price <donomii@gmail.com>
2024-03-20 17:11:50 +0900

    Regenerate reformat

--- entities/searchparams/retrieval.go
+++ entities/searchparams/retrieval.go
@@ -43,7 +43,6 @@ type HybridSearch struct {
 	FusionAlgorithm  int         `json:"fusionalgorithm"`
 	NearTextParams   *NearTextParams
 	NearVectorParams *NearVector
-
 }
 
 type NearObject struct {
