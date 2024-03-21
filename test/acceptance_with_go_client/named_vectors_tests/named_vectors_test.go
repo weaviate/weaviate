864c978a47a043a8d0ed44aed687a22b555f6ed4
Marcin Antas <antas.marcin@gmail.com>
2024-03-21 13:58:04 +0100

    Fix getting a default target vector in hybrid when only 1 vectorizer is defined

--- test/acceptance_with_go_client/named_vectors_tests/named_vectors_test.go
+++ test/acceptance_with_go_client/named_vectors_tests/named_vectors_test.go
@@ -79,6 +79,7 @@ func allTests(endpoint string) func(t *testing.T) {
 		t.Run("validation", testSchemaValidation(endpoint))
 		t.Run("cross references", testReferenceProperties(endpoint))
 		t.Run("objects with vectorizer and objects", testCreateSchemaWithVectorizerAndBYOV(endpoint))
+		t.Run("hybrid", testHybrid(endpoint))
 	}
 }
 
