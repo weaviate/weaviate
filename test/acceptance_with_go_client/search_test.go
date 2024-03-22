970cb2467850b9eab10a20091a50e040a935dabb
Jeremy Price <donomii@gmail.com>
2024-03-23 00:19:43 +0900

    Fix tests

--- test/acceptance_with_go_client/search_test.go
+++ test/acceptance_with_go_client/search_test.go
@@ -289,7 +289,7 @@ func TestHybridWithOnlyVectorSearch(t *testing.T) {
 		map[string]interface{}{"text": "how much wood can a woodchuck chuck?"}).Do(ctx)
 	require.Nil(t, err)
 
-	results, err := c.GraphQL().Raw().WithQuery(fmt.Sprintf("{Get{%s(hybrid:{searches: nearVector: {vector:%v}}){text}}}", className, model.Object.Vector)).Do(ctx)
+	results, err := c.GraphQL().Raw().WithQuery(fmt.Sprintf("{Get{%s(hybrid:{searches: { nearVector: {vector:%v}}}){text}}}", className, model.Object.Vector)).Do(ctx)
 	require.Nil(t, err)
 	result := results.Data["Get"].(map[string]interface{})[className].([]interface{})
 	require.Len(t, result, 1)
@@ -315,7 +315,7 @@ func TestHybridWithVectorSubsearch(t *testing.T) {
 		map[string]interface{}{"text": "how much wood can a woodchuck chuck?"}).Do(ctx)
 	require.Nil(t, err)
 
-	results, err := c.GraphQL().Raw().WithQuery(fmt.Sprintf("{Get{%s(hybrid:{nearVector: { vector:%v}}){text}}}", className, model.Object.Vector)).Do(ctx)
+	results, err := c.GraphQL().Raw().WithQuery(fmt.Sprintf("{Get{%s(hybrid:{searches: { nearVector: { vector:%v}}}){text}}}", className, model.Object.Vector)).Do(ctx)
 	require.Nil(t, err)
 	result := results.Data["Get"].(map[string]interface{})[className].([]interface{})
 	require.Len(t, result, 1)
