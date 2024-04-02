cf876a0755e9c096235fd3f800ab69362e52e48c
Jeremy Price <donomii@gmail.com>
2024-04-03 01:16:25 +0900

    Missing bracket

--- test/acceptance/graphql_resolvers/setup_test.go
+++ test/acceptance/graphql_resolvers/setup_test.go
@@ -50,7 +50,7 @@ func TestGraphQL_AsyncIndexing(t *testing.T) {
 
 func TestGraphQL_SyncIndexing(t *testing.T) {
 	testGraphQL(t)
-	testGroupBy(t)
+
 }
 
 func testGraphQL(t *testing.T) {
@@ -144,7 +144,7 @@ func testGraphQL(t *testing.T) {
 	deleteObjectClass(t, "CustomVectorClass")
 }
 
-func testGroupBy(t *testing.T) {
+func TestGroupBy(t *testing.T) {
 	t.Run("setup test schema", addTestSchema)
 
 	t.Run("import test data (company groups)", addTestDataCompanyGroups)
