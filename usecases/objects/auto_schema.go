2cd67258ea6e5768aaf4c2df08cafd62bec6a784
Mohamed Badawi <contact@m-mooga.com>
2024-04-05 12:14:14 +0200

    autoSchema: remove retry and some tests sleeps (#4600)
    * autoSchema: remove retry and some tests sleeps

--- usecases/objects/auto_schema.go
+++ usecases/objects/auto_schema.go
@@ -19,10 +19,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/cenkalti/backoff/v4"
 	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
-	"github.com/weaviate/weaviate/cluster/utils"
 	"github.com/weaviate/weaviate/entities/additional"
 	"github.com/weaviate/weaviate/entities/models"
 	"github.com/weaviate/weaviate/entities/schema"
@@ -73,26 +71,22 @@ func (m *autoSchemaManager) autoSchema(ctx context.Context, principal *models.Pr
 
 		object.Class = schema.UppercaseClassName(object.Class)
 
-		// Batch together the GetClass and subsequent createClass or updateClass to ensure we will retry if the schema changes
-		// have not yet propagated back to the follower node.
-		err := backoff.Retry(func() error {
-			schemaClass, err := m.schemaManager.GetClass(ctx, principal, object.Class)
-			if err != nil {
-				return err
-			}
-			if schemaClass == nil && !allowCreateClass {
-				return fmt.Errorf("given class does not exist")
-			}
-			properties, err := m.getProperties(object)
-			if err != nil {
-				return err
-			}
-			if schemaClass == nil {
-				return m.createClass(ctx, principal, object.Class, properties)
-			}
-			return m.schemaManager.AddClassProperty(ctx, principal, schemaClass, true, properties...)
-		}, utils.NewBackoff())
+		schemaClass, err := m.schemaManager.GetClass(ctx, principal, object.Class)
 		if err != nil {
+			return err
+		}
+		if schemaClass == nil && !allowCreateClass {
+			return fmt.Errorf("given class does not exist")
+		}
+		properties, err := m.getProperties(object)
+		if err != nil {
+			return err
+		}
+		if schemaClass == nil {
+			return m.createClass(ctx, principal, object.Class, properties)
+		}
+
+		if err := m.schemaManager.AddClassProperty(ctx, principal, schemaClass, true, properties...); err != nil {
 			return err
 		}
 	}
