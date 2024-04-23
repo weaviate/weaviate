856280f9a5236709d4a495c5fd4629e4ac8d73ca
Tommy Smith <tommy@weaviate.io>
2024-04-23 13:44:48 +0100

    Change []string{} to nil

--- adapters/handlers/rest/handlers_schema.go
+++ adapters/handlers/rest/handlers_schema.go
@@ -290,7 +290,7 @@ func (s *schemaHandlers) deleteTenants(params schema.TenantsDeleteParams,
 func (s *schemaHandlers) getTenants(params schema.TenantsGetParams,
 	principal *models.Principal,
 ) middleware.Responder {
-	tenants, err := s.manager.GetConsistentTenants(params.HTTPRequest.Context(), principal, params.ClassName, *params.Consistency, []string{})
+	tenants, err := s.manager.GetConsistentTenants(params.HTTPRequest.Context(), principal, params.ClassName, *params.Consistency, nil)
 	if err != nil {
 		s.metricRequestsTotal.logError(params.ClassName, err)
 		switch err.(type) {
