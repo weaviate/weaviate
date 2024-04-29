74272e349256c45344acc04f17205c2cf497329c
etiennedi <etienne@dilocker.de>
2024-04-29 12:21:38 +0200

    only involve leader for tenant status when necessary
    remove requirements for push step
    
    fix poc implementation
    
    it used the wrong status before
    
    only involve leader for tenant status if necessary
    
    Use an optimistic logic that can rely on the local state when it seems
    compatible.
    
    Revert "remove requirements for push step"
    
    This reverts commit dfb31b11565235485c60e6df8bf01eb4f5ac347e.

--- adapters/repos/db/clusterintegrationtest/fakes_for_test.go
+++ adapters/repos/db/clusterintegrationtest/fakes_for_test.go
@@ -182,6 +182,12 @@ func (f *fakeSchemaManager) TenantsShards(class string, tenants ...string) (map[
 	return res, nil
 }
 
+func (f *fakeSchemaManager) OptimisticTenantStatus(class string, tenant string) (map[string]string, error) {
+	res := map[string]string{}
+	res[tenant] = models.TenantActivityStatusHOT
+	return res, nil
+}
+
 func (f *fakeSchemaManager) ShardFromUUID(class string, uuid []byte) string {
 	ss := f.shardState
 	return ss.Shard("", string(uuid))
