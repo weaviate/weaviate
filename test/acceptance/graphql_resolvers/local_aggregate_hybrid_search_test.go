4bbf3c0319ad599780fb8fa35afbd7af1ef12578
Jeremy Price <donomii@gmail.com>
2024-05-01 19:34:47 +0900

    Add more hybrid tests

--- test/acceptance/graphql_resolvers/local_aggregate_hybrid_search_test.go
+++ test/acceptance/graphql_resolvers/local_aggregate_hybrid_search_test.go
@@ -103,7 +103,7 @@ func aggregationWithHybridSearch(t *testing.T) {
 		})
 	})
 
-	t.Run("with grouping, sparse search and nearText", func(t *testing.T) {
+	t.Run("with grouping, nearText", func(t *testing.T) {
 		query := `
 		{
 			Aggregate {
@@ -135,7 +135,7 @@ func aggregationWithHybridSearch(t *testing.T) {
 		require.Len(t, result, 9)
 	})
 
-	t.Run("with grouping, sparse search and nearText, moveTo", func(t *testing.T) {
+	t.Run("with grouping, and nearText, moveTo", func(t *testing.T) {
 		query := `
 	{
 		Aggregate {
@@ -174,5 +174,49 @@ func aggregationWithHybridSearch(t *testing.T) {
 
 		result := graphqlhelper.AssertGraphQL(t, helper.RootAuth, query).Get("Aggregate", "Company").AsSlice()
 		require.Len(t, result, 9)
+	})
+
+	t.Run("with grouping, nearVector and nearText conflict", func(t *testing.T) {
+		query := `
+	{
+		Aggregate {
+			Company
+			(
+				objectLimit: 30
+				  groupBy: "name"
+				  hybrid: {
+					alpha: 0.5
+					query: ""
+					searches: {
+						nearText: {
+							concepts: ["Google"]
+							certainty: 0.4,
+								moveTo: {
+									concepts:["positive"],
+									force: 0.5
+								},
+								moveAwayFrom: {
+									concepts:["epic"],
+									force: 0.25
+								}
+						}
+						nearVector: {
+							vector: [0.1, 0.2, 0.3]
+						}
+					}
+				}
+			)
+			{
+				name {
+					topOccurrences {
+						  value
+					}
+				  }
+			}
+		}
+	}`
+
+		 graphqlhelper.ErrorGraphQL(t, helper.RootAuth, query)
+
 	})
 }
