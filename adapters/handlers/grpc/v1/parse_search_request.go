17f2bb212f3777e4943c94606ca6fba3cdcd817a
Jeremy Price <donomii@gmail.com>
2024-05-02 13:59:07 +0200

    Add tests, improve parameter extraction in hybrid aggregate (#4809)

--- adapters/handlers/grpc/v1/parse_search_request.go
+++ adapters/handlers/grpc/v1/parse_search_request.go
@@ -331,6 +331,16 @@ func searchParamsFromProto(req *pb.SearchRequest, getClass func(string) *models.
 
 		out.GroupBy = groupBy
 	}
+
+	if out.HybridSearch != nil && out.HybridSearch.NearTextParams != nil && out.HybridSearch.NearVectorParams != nil {
+		return dto.GetParams{}, errors.New("cannot combine nearText and nearVector in hybrid search")
+	}
+	if out.HybridSearch != nil && out.HybridSearch.NearTextParams != nil && out.HybridSearch.Vector != nil {
+		return dto.GetParams{}, errors.New("cannot combine nearText and query in hybrid search")
+	}
+	if out.HybridSearch != nil && out.HybridSearch.NearVectorParams != nil && out.HybridSearch.Vector != nil {
+		return dto.GetParams{}, errors.New("cannot combine nearVector and vector in hybrid search")
+	}
 	return out, nil
 }
 
