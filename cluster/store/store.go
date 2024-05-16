8701a0cf6122631c9a41b2ea0c6fa0d801b30c3a
moogacs <contact@m-mooga.com>
2024-05-16 18:24:14 +0200

    more

--- cluster/store/store.go
+++ cluster/store/store.go
@@ -616,10 +616,10 @@ func (st *Store) Execute(req *api.ApplyRequest) (uint64, error) {
 	return resp.Version, resp.Error
 }
 
-// Apply log is invoked once a log entry is committed.
-// It returns a value which will be made available in the
-// ApplyFuture returned by Raft.Apply method if that
-// method was called on the same Raft node as the FSM.
+// Apply is called once a log entry is committed by a majority of the cluster.
+// Apply should apply the log to the FSM. Apply must be deterministic and
+// produce the same result on all peers in the cluster.
+// The returned value is returned to the client as the ApplyFuture.Response.
 func (st *Store) Apply(l *raft.Log) interface{} {
 	ret := Response{Version: l.Index}
 	st.log.WithFields(logrus.Fields{
@@ -727,6 +727,18 @@ func (st *Store) Apply(l *raft.Log) interface{} {
 	return ret
 }
 
+// Snapshot returns an FSMSnapshot used to: support log compaction, to
+// restore the FSM to a previous state, or to bring out-of-date followers up
+// to a recent log index.
+//
+// The Snapshot implementation should return quickly, because Apply can not
+// be called while Snapshot is running. Generally this means Snapshot should
+// only capture a pointer to the state, and any expensive IO should happen
+// as part of FSMSnapshot.Persist.
+//
+// Apply and Snapshot are always called from the same thread, but Apply will
+// be called concurrently with FSMSnapshot.Persist. This means the FSM should
+// be implemented to allow for concurrent updates while a snapshot is happening.
 func (st *Store) Snapshot() (raft.FSMSnapshot, error) {
 	st.log.Info("persisting snapshot")
 	return st.db.Schema, nil
@@ -920,6 +932,9 @@ func (st *Store) reloadDBFromSnapshot() bool {
 func (st *Store) reloadDBFromSchema() {
 	st.dbLock.Lock()
 	defer st.dbLock.Unlock()
+	if st.lastAppliedIndexOnStart.Load() == 0 {
+		return
+	}
 
 	classes := st.db.Schema.MetaClasses()
 
