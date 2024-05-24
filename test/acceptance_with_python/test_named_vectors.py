3f2c777d04a15327204c60421620f2bc6cf162f8
Dirk Kulawiak <dirk@semi.technology>
2024-05-24 11:41:13 -0700

    Fix test

--- test/acceptance_with_python/test_named_vectors.py
+++ test/acceptance_with_python/test_named_vectors.py
@@ -49,15 +49,27 @@ def named_collection(
 
 
 def test_create_named_vectors_with_and_without_vectorizer(
-    named_collection: NamedCollection,
+    collection_factory: CollectionFactory,
 ) -> None:
-    collection = named_collection()
+    collection = collection_factory(
+        properties=[
+            wvc.config.Property(name="title", data_type=wvc.config.DataType.TEXT),
+            wvc.config.Property(name="content", data_type=wvc.config.DataType.TEXT),
+        ],
+        vectorizer_config=[
+            wvc.config.Configure.NamedVectors.text2vec_contextionary(
+                name="AllExplicit",
+                source_properties=["title", "content"],
+                vectorize_collection_name=False,
+            ),
+            wvc.config.Configure.NamedVectors.none(name="bringYourOwn"),
+        ],
+    )
 
     uuid = collection.data.insert(
         properties={"title": "Hello", "content": "World"},
         vector={"bringYourOwn": [0.5, 0.25, 0.75]},
     )
-
     obj = collection.query.fetch_object_by_id(uuid, include_vector=True)
     assert obj.vector["AllExplicit"] is not None
     assert obj.vector["bringYourOwn"] is not None
