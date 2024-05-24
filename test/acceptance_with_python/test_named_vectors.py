6ddb7ecdb7b9039c89bcc36e547790bbe9531615
Dirk Kulawiak <dirk@semi.technology>
2024-05-24 11:23:48 -0700

    Add test for near_vector

--- test/acceptance_with_python/test_named_vectors.py
+++ test/acceptance_with_python/test_named_vectors.py
@@ -1,25 +1,57 @@
+import pytest
 import weaviate.classes as wvc
+from typing_extensions import Protocol, Generator
+from weaviate.collections import Collection
 
 from conftest import CollectionFactory
 
 
-def test_create_named_vectors_with_and_without_vectorizer(
+class NamedCollection(Protocol):
+    """Typing for fixture."""
+
+    def __call__(self, name: str = "", multi_tenancy: bool = False) -> Collection:
+        """Typing for fixture."""
+        ...
+
+
+@pytest.fixture
+def named_collection(
     collection_factory: CollectionFactory,
+) -> Generator[NamedCollection, None, None]:
+    def _factory(name: str = "") -> Collection:
+        collection = collection_factory(
+            name,
+            properties=[
+                wvc.config.Property(name="title1", data_type=wvc.config.DataType.TEXT),
+                wvc.config.Property(name="title2", data_type=wvc.config.DataType.TEXT),
+            ],
+            vectorizer_config=[
+                wvc.config.Configure.NamedVectors.text2vec_contextionary(
+                    name="All",
+                    vectorize_collection_name=False,
+                ),
+                wvc.config.Configure.NamedVectors.text2vec_contextionary(
+                    name="title1",
+                    source_properties=["title1"],
+                    vectorize_collection_name=False,
+                ),
+                wvc.config.Configure.NamedVectors.text2vec_contextionary(
+                    name="title2",
+                    source_properties=["title2"],
+                    vectorize_collection_name=False,
+                ),
+            ],
+        )
+
+        return collection
+
+    yield _factory
+
+
+def test_create_named_vectors_with_and_without_vectorizer(
+    named_collection: NamedCollection,
 ) -> None:
-    collection = collection_factory(
-        properties=[
-            wvc.config.Property(name="title", data_type=wvc.config.DataType.TEXT),
-            wvc.config.Property(name="content", data_type=wvc.config.DataType.TEXT),
-        ],
-        vectorizer_config=[
-            wvc.config.Configure.NamedVectors.text2vec_contextionary(
-                name="AllExplicit",
-                source_properties=["title", "content"],
-                vectorize_collection_name=False,
-            ),
-            wvc.config.Configure.NamedVectors.none(name="bringYourOwn"),
-        ],
-    )
+    collection = named_collection()
 
     uuid = collection.data.insert(
         properties={"title": "Hello", "content": "World"},
@@ -31,29 +63,10 @@ def test_create_named_vectors_with_and_without_vectorizer(
     assert obj.vector["bringYourOwn"] is not None
 
 
-def test_hybrid_search_with_multiple_target_vectors(collection_factory: CollectionFactory) -> None:
-    collection = collection_factory(
-        properties=[
-            wvc.config.Property(name="title1", data_type=wvc.config.DataType.TEXT),
-            wvc.config.Property(name="title2", data_type=wvc.config.DataType.TEXT),
-        ],
-        vectorizer_config=[
-            wvc.config.Configure.NamedVectors.text2vec_contextionary(
-                name="All",
-                vectorize_collection_name=False,
-            ),
-            wvc.config.Configure.NamedVectors.text2vec_contextionary(
-                name="title1",
-                source_properties=["title1"],
-                vectorize_collection_name=False,
-            ),
-            wvc.config.Configure.NamedVectors.text2vec_contextionary(
-                name="title2",
-                source_properties=["title2"],
-                vectorize_collection_name=False,
-            ),
-        ],
-    )
+def test_hybrid_search_with_multiple_target_vectors(
+    named_collection: NamedCollection,
+) -> None:
+    collection = named_collection()
 
     uuid1 = collection.data.insert(
         properties={"title1": "apple", "title2": "cocoa"},
@@ -118,25 +131,10 @@ def test_hybrid_search_with_multiple_target_vectors(collection_factory: Collecti
     assert near_vector_sub_search.objects[2].metadata.score == 0
 
 
-def test_near_object(collection_factory: CollectionFactory) -> None:
-    collection = collection_factory(
-        properties=[
-            wvc.config.Property(name="title1", data_type=wvc.config.DataType.TEXT),
-            wvc.config.Property(name="title2", data_type=wvc.config.DataType.TEXT),
-        ],
-        vectorizer_config=[
-            wvc.config.Configure.NamedVectors.text2vec_contextionary(
-                name="title1",
-                source_properties=["title1"],
-                vectorize_collection_name=False,
-            ),
-            wvc.config.Configure.NamedVectors.text2vec_contextionary(
-                name="title2",
-                source_properties=["title2"],
-                vectorize_collection_name=False,
-            ),
-        ],
-    )
+def test_near_object(
+    named_collection: NamedCollection,
+) -> None:
+    collection = named_collection()
 
     uuid1 = collection.data.insert(
         properties={"title1": "apple", "title2": "cocoa"},
@@ -170,25 +168,10 @@ def test_near_object(collection_factory: CollectionFactory) -> None:
     assert sorted([obj.uuid for obj in near_obj2.objects]) == sorted([uuid1, uuid2])
 
 
-def test_near_text(collection_factory: CollectionFactory) -> None:
-    collection = collection_factory(
-        properties=[
-            wvc.config.Property(name="title1", data_type=wvc.config.DataType.TEXT),
-            wvc.config.Property(name="title2", data_type=wvc.config.DataType.TEXT),
-        ],
-        vectorizer_config=[
-            wvc.config.Configure.NamedVectors.text2vec_contextionary(
-                name="title1",
-                source_properties=["title1"],
-                vectorize_collection_name=False,
-            ),
-            wvc.config.Configure.NamedVectors.text2vec_contextionary(
-                name="title2",
-                source_properties=["title2"],
-                vectorize_collection_name=False,
-            ),
-        ],
-    )
+def test_near_text(
+    named_collection: NamedCollection,
+) -> None:
+    collection = named_collection()
 
     uuid1 = collection.data.insert(
         properties={"title1": "apple", "title2": "cocoa"},
@@ -219,3 +202,41 @@ def test_near_text(collection_factory: CollectionFactory) -> None:
     assert len(near_text2.objects) == 2
     # order is not guaranteed
     assert sorted([obj.uuid for obj in near_text2.objects]) == sorted([uuid1, uuid2])
+
+
+def test_near_vector(
+    named_collection: NamedCollection,
+) -> None:
+    collection = named_collection()
+
+    uuid1 = collection.data.insert(
+        properties={"title1": "apple", "title2": "cocoa"},
+    )
+    uuid2 = collection.data.insert(
+        properties={"title1": "cocoa", "title2": "apple"},
+    )
+    uuid3 = collection.data.insert(
+        properties={"title1": "mountain", "title2": "ridge line"},
+    )
+
+    obj1 = collection.query.fetch_object_by_id(uuid1, include_vector=True)
+    near_vector1 = collection.query.near_vector(
+        obj1.vector["title1"],
+        target_vector="title1",
+        distance=0.1,
+        return_metadata=wvc.query.MetadataQuery.full(),
+    )
+
+    assert len(near_vector1.objects) == 1
+    assert near_vector1.objects[0].uuid == uuid1
+
+    # finds both objects, but the second target vector has a larger distance
+    near_vector2 = collection.query.near_vector(
+        obj1.vector["title1"],
+        target_vector=["title1", "title2"],
+        distance=0.9,
+        return_metadata=wvc.query.MetadataQuery.full(),
+    )
+    assert len(near_vector2.objects) == 2
+    # order is not guaranteed
+    assert sorted([obj.uuid for obj in near_vector2.objects]) == sorted([uuid1, uuid2])
