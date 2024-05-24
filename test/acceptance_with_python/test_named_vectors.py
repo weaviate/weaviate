cb8cce5a042accf57787691dde46189223bba3cb
Dirk Kulawiak <dirk@semi.technology>
2024-05-24 11:08:42 -0700

    Add tests for nearObject and nearText

--- test/acceptance_with_python/test_named_vectors.py
+++ test/acceptance_with_python/test_named_vectors.py
@@ -82,31 +82,140 @@ def test_hybrid_search_with_multiple_target_vectors(collection_factory: Collecti
     assert direct.objects[1].metadata.score == 1
     assert direct.objects[2].metadata.score == 0
 
-    nearTextSubSearch = collection.query.hybrid(
+    near_text_sub_search = collection.query.hybrid(
         "something else",
         vector=wvc.query.HybridVector.near_text("apple sandwich"),
         target_vector=["title1", "title2"],
         return_metadata=wvc.query.MetadataQuery.full(),
     )
-    assert len(nearTextSubSearch.objects) == 3
+    assert len(near_text_sub_search.objects) == 3
 
     # first two objects are a perfect fit for vector search, but their order is not guaranteed
-    assert sorted([obj.uuid for obj in nearTextSubSearch.objects[:2]]) == sorted([uuid1, uuid2])
-    assert nearTextSubSearch.objects[0].metadata.score > 0.5  # only vector search part has result
-    assert nearTextSubSearch.objects[1].metadata.score > 0.5
-    assert nearTextSubSearch.objects[2].metadata.score == 0
+    assert sorted([obj.uuid for obj in near_text_sub_search.objects[:2]]) == sorted([uuid1, uuid2])
+    assert (
+        near_text_sub_search.objects[0].metadata.score > 0.5
+    )  # only vector search part has result
+    assert near_text_sub_search.objects[1].metadata.score > 0.5
+    assert near_text_sub_search.objects[2].metadata.score == 0
 
     obj1 = collection.query.fetch_object_by_id(uuid1, include_vector=True)
-    nearVectorSubSearch = collection.query.hybrid(
+    near_vector_sub_search = collection.query.hybrid(
         "something else",
         vector=wvc.query.HybridVector.near_vector(obj1.vector["title1"]),
         target_vector=["title1", "title2"],
         return_metadata=wvc.query.MetadataQuery.full(),
     )
-    assert len(nearVectorSubSearch.objects) == 3
+    assert len(near_vector_sub_search.objects) == 3
 
     # first two objects are a perfect fit for vector search, but their order is not guaranteed
-    assert sorted([obj.uuid for obj in nearVectorSubSearch.objects[:2]]) == sorted([uuid1, uuid2])
-    assert nearVectorSubSearch.objects[0].metadata.score > 0.5  # only vector search part has result
-    assert nearVectorSubSearch.objects[1].metadata.score > 0.5
-    assert nearVectorSubSearch.objects[2].metadata.score == 0
+    assert sorted([obj.uuid for obj in near_vector_sub_search.objects[:2]]) == sorted(
+        [uuid1, uuid2]
+    )
+    assert (
+        near_vector_sub_search.objects[0].metadata.score > 0.5
+    )  # only vector search part has result
+    assert near_vector_sub_search.objects[1].metadata.score > 0.5
+    assert near_vector_sub_search.objects[2].metadata.score == 0
+
+
+def test_near_object(collection_factory: CollectionFactory) -> None:
+    collection = collection_factory(
+        properties=[
+            wvc.config.Property(name="title1", data_type=wvc.config.DataType.TEXT),
+            wvc.config.Property(name="title2", data_type=wvc.config.DataType.TEXT),
+        ],
+        vectorizer_config=[
+            wvc.config.Configure.NamedVectors.text2vec_contextionary(
+                name="title1",
+                source_properties=["title1"],
+                vectorize_collection_name=False,
+            ),
+            wvc.config.Configure.NamedVectors.text2vec_contextionary(
+                name="title2",
+                source_properties=["title2"],
+                vectorize_collection_name=False,
+            ),
+        ],
+    )
+
+    uuid1 = collection.data.insert(
+        properties={"title1": "apple", "title2": "cocoa"},
+    )
+    uuid2 = collection.data.insert(
+        properties={"title1": "cocoa", "title2": "apple"},
+    )
+    uuid3 = collection.data.insert(
+        properties={"title1": "mountain", "title2": "ridge line"},
+    )
+
+    # only finds first object with minimal distance
+    near_obj1 = collection.query.near_object(
+        uuid1,
+        target_vector="title1",
+        distance=0.1,
+        return_metadata=wvc.query.MetadataQuery.full(),
+    )
+    assert len(near_obj1.objects) == 1
+    assert near_obj1.objects[0].uuid == uuid1
+
+    # finds both objects, but the second target vector has a larger distance
+    near_obj2 = collection.query.near_object(
+        uuid1,
+        target_vector=["title1", "title2"],
+        distance=0.9,
+        return_metadata=wvc.query.MetadataQuery.full(),
+    )
+    assert len(near_obj2.objects) == 2
+    # order is not guaranteed
+    assert sorted([obj.uuid for obj in near_obj2.objects]) == sorted([uuid1, uuid2])
+
+
+def test_near_text(collection_factory: CollectionFactory) -> None:
+    collection = collection_factory(
+        properties=[
+            wvc.config.Property(name="title1", data_type=wvc.config.DataType.TEXT),
+            wvc.config.Property(name="title2", data_type=wvc.config.DataType.TEXT),
+        ],
+        vectorizer_config=[
+            wvc.config.Configure.NamedVectors.text2vec_contextionary(
+                name="title1",
+                source_properties=["title1"],
+                vectorize_collection_name=False,
+            ),
+            wvc.config.Configure.NamedVectors.text2vec_contextionary(
+                name="title2",
+                source_properties=["title2"],
+                vectorize_collection_name=False,
+            ),
+        ],
+    )
+
+    uuid1 = collection.data.insert(
+        properties={"title1": "apple", "title2": "cocoa"},
+    )
+    uuid2 = collection.data.insert(
+        properties={"title1": "cocoa", "title2": "apple"},
+    )
+    uuid3 = collection.data.insert(
+        properties={"title1": "mountain", "title2": "ridge line"},
+    )
+
+    near_text1 = collection.query.near_text(
+        "apple",
+        target_vector="title1",
+        distance=0.1,
+        return_metadata=wvc.query.MetadataQuery.full(),
+    )
+    assert len(near_text1.objects) == 1
+    assert near_text1.objects[0].uuid == uuid1
+
+    # finds both objects, but the second target vector has a larger distance
+    near_text2 = collection.query.near_text(
+        "apple",
+        target_vector=["title1", "title2"],
+        distance=0.9,
+        return_metadata=wvc.query.MetadataQuery.full(),
+    )
+    assert len(near_text2.objects) == 2
+    # order is not guaranteed
+    assert sorted([obj.uuid for obj in near_text2.objects]) == sorted([uuid1, uuid2])
