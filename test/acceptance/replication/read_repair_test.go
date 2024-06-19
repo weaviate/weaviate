765c9d3284e4407cd1b0c96ce2124f948bfd052d
Dirk Kulawiak <dirk@semi.technology>
2024-06-19 08:30:51 +0200

    Add extra test with named vectors

--- test/acceptance/replication/read_repair_test.go
+++ test/acceptance/replication/read_repair_test.go
@@ -17,6 +17,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/weaviate/weaviate/test/helper/sample-schema/books"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/weaviate/weaviate/entities/models"
@@ -118,6 +120,108 @@ func readRepair(t *testing.T) {
 	replaceObj := repairObj
 	replaceObj.Properties = map[string]interface{}{
 		"contents": "this paragraph was replaced",
+	}
+
+	t.Run("ReplaceObjectOneOnNode2", func(t *testing.T) {
+		updateObjectCL(t, compose.ContainerURI(2), &replaceObj, replica.One)
+	})
+
+	t.Run("RestartNode-3", func(t *testing.T) {
+		startNodeAt(ctx, t, compose, 3)
+	})
+
+	t.Run("TriggerRepairAllOnNode1", func(t *testing.T) {
+		exists, err := objectExistsCL(t, compose.ContainerURI(1),
+			replaceObj.Class, replaceObj.ID, replica.All)
+		require.Nil(t, err)
+		require.True(t, exists)
+	})
+
+	t.Run("UpdatedObjectRepairedOnNode-3", func(t *testing.T) {
+		exists, err := objectExistsCL(t, compose.ContainerURI(3),
+			replaceObj.Class, replaceObj.ID, replica.One)
+		require.Nil(t, err)
+		require.True(t, exists)
+
+		resp, err := getObjectCL(t, compose.ContainerURI(1),
+			repairObj.Class, repairObj.ID, replica.One)
+		require.Nil(t, err)
+		assert.Equal(t, replaceObj.ID, resp.ID)
+		assert.Equal(t, replaceObj.Class, resp.Class)
+		assert.EqualValues(t, replaceObj.Properties, resp.Properties)
+		assert.EqualValues(t, replaceObj.Vector, resp.Vector)
+	})
+}
+
+func readRepairNamedVectors(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+
+	compose, err := docker.New().
+		With3NodeCluster().
+		WithText2VecContextionary().
+		Start(ctx)
+	require.Nil(t, err)
+	defer func() {
+		if err := compose.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate test containers: %s", err.Error())
+		}
+	}()
+
+	helper.SetupClient(compose.ContainerURI(1))
+	bookClass := books.ClassNamedContextionaryVectorizer()
+	booksObjs := books.Objects()
+
+	t.Run("CreateSchema", func(t *testing.T) {
+		bookClass.ReplicationConfig = &models.ReplicationConfig{
+			Factor: 3,
+		}
+		helper.CreateClass(t, bookClass)
+	})
+
+	time.Sleep(time.Second) // remove once eventual consistency has been addressed
+
+	t.Run("InsertBooks/Node-1", func(t *testing.T) {
+		createObjects(t, compose.ContainerURI(1), booksObjs)
+	})
+
+	t.Run("StopNode-3", func(t *testing.T) {
+		stopNodeAt(ctx, t, compose, 3)
+	})
+
+	repairObj := models.Object{
+		ID:    booksObjs[0].ID,
+		Class: bookClass.Class,
+		Properties: map[string]interface{}{
+			"title": "a new title",
+		},
+	}
+	t.Run("AddObjectToNode-1", func(t *testing.T) {
+		createObjectCL(t, compose.ContainerURI(1), &repairObj, replica.One)
+	})
+
+	t.Run("RestartNode-3", func(t *testing.T) {
+		startNodeAt(ctx, t, compose, 3)
+		time.Sleep(time.Second)
+	})
+
+	t.Run("TriggerRepairQuorumOnNode-3", func(t *testing.T) {
+		resp, err := getObjectCL(t, compose.ContainerURI(3),
+			repairObj.Class, repairObj.ID, replica.Quorum)
+		require.Nil(t, err)
+		assert.Equal(t, repairObj.ID, resp.ID)
+		assert.Equal(t, repairObj.Class, resp.Class)
+		assert.EqualValues(t, repairObj.Properties, resp.Properties)
+		assert.EqualValues(t, repairObj.Vector, resp.Vector)
+	})
+
+	t.Run("StopNode-3", func(t *testing.T) {
+		stopNodeAt(ctx, t, compose, 3)
+	})
+
+	replaceObj := repairObj
+	replaceObj.Properties = map[string]interface{}{
+		"title": "this title was replaced",
 	}
 
 	t.Run("ReplaceObjectOneOnNode2", func(t *testing.T) {
