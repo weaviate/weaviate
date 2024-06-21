c2d3471720dd495bfc7145790572e37a23c43db8
Naman Mudgal <naman.mudgal2002@gmail.com>
2024-06-22 04:10:37 +0530

    adding config

---
+++ modules/reranker-jinaai/config/class_settings.go
@@ -0,0 +1,76 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2024 Weaviate B.V. All rights reserved.
+//
+//  CONTACT: hello@weaviate.io
+//
+
+package config
+
+import (
+	"github.com/pkg/errors"
+	"github.com/weaviate/weaviate/entities/models"
+	"github.com/weaviate/weaviate/entities/moduletools"
+	basesettings "github.com/weaviate/weaviate/usecases/modulecomponents/settings"
+)
+
+const (
+	modelProperty = "model"
+)
+
+const (
+	DefaultjinaaiModel = "jina-embeddings-v2-base-en"
+)
+
+var availablejinaaiModels = []string{
+	"jina-embeddings-v2-base-en",
+	
+}
+
+type classSettings struct {
+	cfg                  moduletools.ClassConfig
+	propertyValuesHelper basesettings.PropertyValuesHelper
+}
+
+func NewClassSettings(cfg moduletools.ClassConfig) *classSettings {
+	return &classSettings{cfg: cfg, propertyValuesHelper: basesettings.NewPropertyValuesHelper("reranker-jinaai")}
+}
+
+func (ic *classSettings) Validate(class *models.Class) error {
+	if ic.cfg == nil {
+		// we would receive a nil-config on cross-class requests, such as Explore{}
+		return errors.New("empty config")
+	}
+	model := ic.getStringProperty(modelProperty, DefaultjinaaiModel)
+	if model == nil || !ic.validateModel(*model) {
+		return errors.Errorf("wrong jinaai model name, available model names are: %v", availablejinaaiModels)
+	}
+
+	return nil
+}
+
+func (ic *classSettings) getStringProperty(name string, defaultValue string) *string {
+	asString := ic.propertyValuesHelper.GetPropertyAsStringWithNotExists(ic.cfg, name, "", defaultValue)
+	return &asString
+}
+
+func (ic *classSettings) validateModel(model string) bool {
+	return contains(availablejinaaiModels, model)
+}
+
+func (ic *classSettings) Model() string {
+	return *ic.getStringProperty(modelProperty, DefaultjinaaiModel)
+}
+
+func contains[T comparable](s []T, e T) bool {
+	for _, v := range s {
+		if v == e {
+			return true
+		}
+	}
+	return false
+}
