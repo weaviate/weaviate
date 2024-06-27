4a69d4cc61b2f2e078231e881c7fa319b668e3e0
etiennedi <etienne@dilocker.de>
2024-06-27 13:03:08 +0200

    recover panic in NewShard(), return as error

--- adapters/repos/db/shard.go
+++ adapters/repos/db/shard.go
@@ -17,6 +17,7 @@ import (
 	"io"
 	"os"
 	"path"
+	"runtime/debug"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -250,6 +251,16 @@ func NewShard(ctx context.Context, promMetrics *monitoring.PrometheusMetrics,
 	}
 
 	defer func() {
+		p := recover()
+		if p != nil {
+			err = fmt.Errorf("unexpected error initializing shard %q of index %q: %v", shardName, index.ID(), p)
+			index.logger.WithError(err).WithFields(logrus.Fields{
+				"index": index.ID(),
+				"shard": shardName,
+			}).Error("panic during shard initialization")
+			debug.PrintStack()
+		}
+
 		if err != nil {
 			// spawn a new context as we cannot guarantee that the init context is
 			// still valid, but we want to make sure that we have enough time to clean
