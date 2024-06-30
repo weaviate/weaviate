6c73c3fe1f3ad39fad11abaab036d351aa4477d2
Dirk Kulawiak <dirk@semi.technology>
2024-06-30 09:12:37 +0200

    Fix property extraction with GRPC

--- usecases/modulecomponents/additional/generate/generate_params_extractor.go
+++ usecases/modulecomponents/additional/generate/generate_params_extractor.go
@@ -16,8 +16,6 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/pkg/errors"
-
 	"github.com/tailor-inc/graphql/language/ast"
 )
 
@@ -33,13 +31,8 @@ func (p *GenerateProvider) parseGenerateArguments(args []*ast.Argument) *Params 
 		case "singleResult":
 			obj := arg.Value.(*ast.ObjectValue).Fields
 			out.Prompt = &obj[0].Value.(*ast.StringValue).Value
-			all := compile.FindAll([]byte(*out.Prompt), -1)
-			for entry := range all {
-				propName := string(all[entry])
-				propName = strings.Trim(propName, "{")
-				propName = strings.Trim(propName, "}")
-				propertiesToExtract = append(propertiesToExtract, propName)
-			}
+			singlePropPrompts := ExtractPropsFromPrompt(out.Prompt)
+			propertiesToExtract = append(propertiesToExtract, singlePropPrompts...)
 		case "groupedResult":
 			obj := arg.Value.(*ast.ObjectValue).Fields
 			for _, field := range obj {
@@ -69,17 +62,14 @@ func (p *GenerateProvider) parseGenerateArguments(args []*ast.Argument) *Params 
 	return out
 }
 
-func GenerateForPrompt(textProperties map[string]string, prompt string) (string, error) {
-	all := compile.FindAll([]byte(prompt), -1)
-	for _, match := range all {
-		originalProperty := string(match)
-		replacedProperty := compile.FindStringSubmatch(originalProperty)[1]
-		replacedProperty = strings.TrimSpace(replacedProperty)
-		value := textProperties[replacedProperty]
-		if value == "" {
-			return "", errors.Errorf("Following property has empty value: '%v'. Make sure you spell the property name correctly, verify that the property exists and has a value", replacedProperty)
-		}
-		prompt = strings.ReplaceAll(prompt, originalProperty, value)
+func ExtractPropsFromPrompt(prompt *string) []string {
+	propertiesToExtract := make([]string, 0)
+	all := compile.FindAll([]byte(*prompt), -1)
+	for entry := range all {
+		propName := string(all[entry])
+		propName = strings.Trim(propName, "{")
+		propName = strings.Trim(propName, "}")
+		propertiesToExtract = append(propertiesToExtract, propName)
 	}
-	return prompt, nil
+	return propertiesToExtract
 }
