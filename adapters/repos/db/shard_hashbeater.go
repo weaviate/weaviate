d09dae81c5cbb545ef71b18b4b17a64a11d73fa2
Jeronimo <jeronimo.irazabal@gmail.com>
2024-07-16 18:32:09 -0300

    chore: improved logging and reduced hashbeat frequency
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>

--- adapters/repos/db/shard_hashbeater.go
+++ adapters/repos/db/shard_hashbeater.go
@@ -47,7 +47,7 @@ func (s *Shard) initHashBeater() {
 				Info("hashbeater stopped")
 		}()
 
-		t := time.NewTicker(500 * time.Millisecond)
+		t := time.NewTicker(1 * time.Second)
 		defer t.Stop()
 
 		backoffs := []time.Duration{
@@ -97,15 +97,13 @@ func (s *Shard) initHashBeater() {
 					continue
 				}
 
-				hosts := make([]string, len(stats.hostStats))
 				localObjects := 0
 				remoteObjects := 0
 				objectsPropagated := 0
 				var objectProgationTook time.Duration
 				var propagationErr error
 
-				for i, stat := range stats.hostStats {
-					hosts[i] = stat.host
+				for _, stat := range stats.hostStats {
 					localObjects += stat.localObjects
 					remoteObjects += stat.remoteObjects
 					objectsPropagated += stat.objectsPropagated
@@ -121,7 +119,7 @@ func (s *Shard) initHashBeater() {
 					WithField("class_name", s.class.Class).
 					WithField("shard_name", s.name).
 					WithField("hashbeat_iteration", it).
-					WithField("hosts", hosts).
+					WithField("hosts", s.getLastComparedHosts()).
 					WithField("diff_calculation_took", stats.diffCalculationTook.String()).
 					WithField("local_objects", localObjects).
 					WithField("remote_objects", remoteObjects).
@@ -149,12 +147,12 @@ func (s *Shard) initHashBeater() {
 	}, s.index.logger)
 
 	enterrors.GoWrapper(func() {
-		t := time.NewTicker(100 * time.Millisecond)
+		t := time.NewTicker(1 * time.Second)
 		defer t.Stop()
 
 		// just in case host comparison is not enough
 		// this way we ensure hashbeat will be always triggered
-		jict := time.NewTicker(5 * time.Second)
+		jict := time.NewTicker(10 * time.Second)
 		defer jict.Stop()
 
 		for {
@@ -243,7 +241,7 @@ func (s *Shard) hashBeat() (stats hashBeatStats, err error) {
 
 	diffCalculationStart := time.Now()
 
-	replyCh, hosts, err := s.index.replicator.CollectShardDifferences(s.hashBeaterCtx, s.name, s.hashtree)
+	replyCh, _, err := s.index.replicator.CollectShardDifferences(s.hashBeaterCtx, s.name, s.hashtree)
 	if err != nil {
 		return stats, fmt.Errorf("collecting differences: %w", err)
 	}
@@ -324,7 +322,7 @@ func (s *Shard) hashBeat() (stats hashBeatStats, err error) {
 		diffCollectionErr = nil
 	}
 
-	s.setLastComparedNodes(hosts)
+	s.setLastComparedNodes(s.allAliveHostnames())
 
 	return stats, diffCollectionErr
 }
