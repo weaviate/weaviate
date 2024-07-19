d9dbdb737d25dceed5738420875941d9b6e0c8f8
André Mourão <andre.mourao@weaviate.io>
2024-07-19 12:32:23 +0100

    Extra check to log and avoid recovering from empty file.

--- adapters/repos/db/lsmkv/bucket_recover_from_wal.go
+++ adapters/repos/db/lsmkv/bucket_recover_from_wal.go
@@ -65,6 +65,18 @@ func (b *Bucket) mayRecoverFromCommitLogs(ctx context.Context) error {
 		cl.pause()
 		defer cl.unpause()
 
+		stat, err := cl.file.Stat()
+		if err != nil {
+			return errors.Wrap(err, "stat commit log")
+		}
+
+		if stat.Size() == 0 {
+			b.logger.WithField("action", "lsm_recover_from_active_wal").
+				WithField("path", path).
+				Warning("empty write-ahead-log found. Did weaviate crash prior to this or the tenant on/loaded from the cloud? Nothing to recover from this file.")
+			continue
+		}
+
 		mt, err := newMemtable(path, b.strategy, b.secondaryIndices, cl, b.metrics, b.logger)
 		if err != nil {
 			return err
