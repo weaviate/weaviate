09bb6ec9d018b0dfcff7fb9de0b1bdb6c1c2fbca
Loic Reyreaud <loic@weaviate.io>
2024-07-22 10:09:55 +0200

    add more parameters to configure sentry
    Signed-off-by: Loic Reyreaud <loic@weaviate.io>

--- ci/push_docker.sh
+++ ci/push_docker.sh
@@ -14,7 +14,7 @@ function release() {
   git_hash=$(echo "$GITHUB_SHA" | cut -c1-7)
 
   weaviate_version="$(jq -r '.info.version' < openapi-specs/schema.json)"
-  if [ "$GITHUB_REF_NAME" = "master" ]; then
+  if [ "$GITHUB_REF_NAME" = "main" ]; then
     tag_exact="${DOCKER_REPO}:${weaviate_version}-${git_hash}"
   elif [  "$GITHUB_REF_TYPE" == "tag" ]; then
         if [ "$GITHUB_REF_NAME" != "v$weaviate_version" ]; then
@@ -25,9 +25,10 @@ function release() {
   else
     pr_title="$(echo -n "$PR_TITLE" | tr '[:upper:]' '[:lower:]' | tr -c -s '[:alnum:]' '-' | sed 's/-$//g')"
     tag_preview="${DOCKER_REPO}:preview-${pr_title}-${git_hash}"
+    weaviate_version="preview-${pr_title}-${git_hash}"
   fi
 
-  args=("--build-arg=GITHASH=$git_hash" "--platform=linux/amd64,linux/arm64" "--target=weaviate" "--push")
+  args=("--build-arg=GITHASH=$git_hash" "--build-arg=DOCKER_IMAGE_TAG=$weaviate_version" --platform=linux/amd64,linux/arm64" "--target=weaviate" "--push")
   if [ -n "$tag_exact" ]; then
     # exact tag on master
     args+=("-t=$tag_exact")
