df8e35a85be419a8cab45f608fde9f48f3acce44
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2024-08-12 16:32:44 +0400

    add test for target vectors

--- adapters/repos/db/index.go
+++ adapters/repos/db/index.go
@@ -2292,7 +2292,7 @@ func (i *Index) DebugResetVectorIndex(ctx context.Context, shardName, targetVect
 
 	// Reindex in the background
 	enterrors.GoWrapper(func() {
-		err = shard.PreloadQueue("")
+		err = shard.PreloadQueue(targetVector)
 		if err != nil {
 			i.logger.WithField("shard", shardName).WithError(err).Error("failed to reindex vector index")
 			return
