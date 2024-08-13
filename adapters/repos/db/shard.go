dd643b06166dfaba87a67e5ce292064696a4b8c9
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2024-08-13 15:37:30 +0400

    add RepairIndex to the ShardLike interface

--- adapters/repos/db/shard.go
+++ adapters/repos/db/shard.go
@@ -161,6 +161,7 @@ type ShardLike interface {
 
 	// Debug methods
 	DebugResetVectorIndex(ctx context.Context, targetVector string) error
+	RepairIndex(ctx context.Context, targetVector string) error
 }
 
 // Shard is the smallest completely-contained index unit. A shard manages
