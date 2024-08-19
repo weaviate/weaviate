fd43382961f0ac1b73c786e71fdf97ce713ef234
Dirk Kulawiak <dirk@semi.technology>
2024-08-19 14:25:13 +0200

    Fix more test interface things

--- modules/text2vec-mistral/vectorizer/fakes_for_test.go
+++ modules/text2vec-mistral/vectorizer/fakes_for_test.go
@@ -74,7 +74,7 @@ func (c *fakeBatchClient) VectorizeQuery(ctx context.Context,
 	}, nil
 }
 
-func (c *fakeBatchClient) GetVectorizerRateLimit(ctx context.Context) *modulecomponents.RateLimits {
+func (c *fakeBatchClient) GetVectorizerRateLimit(ctx context.Context, cfg moduletools.ClassConfig) *modulecomponents.RateLimits {
 	return &modulecomponents.RateLimits{RemainingTokens: 100, RemainingRequests: 100, LimitTokens: 200, ResetTokens: time.Now().Add(time.Duration(c.defaultResetRate) * time.Second), ResetRequests: time.Now().Add(time.Duration(c.defaultResetRate) * time.Second)}
 }
 
