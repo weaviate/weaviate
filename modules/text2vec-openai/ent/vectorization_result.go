e79ffa3aec258d910616f978a4c6890549781c48
Andre Wisplinghoff <andre.wisplinghoff@gmail.com>
2024-08-27 14:18:49 +0200

    Do not keep limitTokens & limitRequests at zero if token limit reached
    This gets rid of the wrong error "text too long for vectorization". limitTokens is overall limit that can be refilled over time. Having it set to zero is not correct.

--- modules/text2vec-openai/ent/vectorization_result.go
+++ modules/text2vec-openai/ent/vectorization_result.go
@@ -36,10 +36,10 @@ func GetRateLimitsFromHeader(header http.Header) *modulecomponents.RateLimits {
 	remainingTokens := getHeaderInt(header, "x-ratelimit-remaining-tokens")
 
 	// azure returns 0 as limit, make sure this does not block anything by setting a high value
-	if limitTokens == 0 && remainingTokens > 0 {
+	if limitTokens == 0 {
 		limitTokens = dummyLimit
 	}
-	if limitRequests == 0 && remainingRequests > 0 {
+	if limitRequests == 0 {
 		limitRequests = dummyLimit
 	}
 	return &modulecomponents.RateLimits{
