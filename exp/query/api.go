ce92fc9e182cf43185aa2a3dc3cea2e2648681f2
Kaviraj <kavirajkanagaraj@gmail.com>
2024-08-30 16:40:55 +0200

    small cleanups
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

--- exp/query/api.go
+++ exp/query/api.go
@@ -12,12 +12,16 @@
 package query
 
 import (
+	"context"
+
 	"github.com/sirupsen/logrus"
 	v1 "github.com/weaviate/weaviate/adapters/handlers/grpc/v1"
-	"github.com/weaviate/weaviate/adapters/handlers/rest/state"
 	"github.com/weaviate/weaviate/grpc/generated/protocol/v1"
 	"github.com/weaviate/weaviate/usecases/auth/authentication/composer"
 	"github.com/weaviate/weaviate/usecases/config"
+	"github.com/weaviate/weaviate/usecases/objects"
+	"github.com/weaviate/weaviate/usecases/schema"
+	"github.com/weaviate/weaviate/usecases/traverser"
 )
 
 // API is the core query API that is transport agnostic (http, grpc, etc).
@@ -27,20 +31,29 @@ type API struct {
 
 	// svc provides the underlying search API via v1.WeaviateServer
 	// TODO(kavi): Split `v1.WeaviateServer` into composable `v1.Searcher` and everything else.
-	svc      protocol.WeaviateServer
-	appState *state.State
+	svc    protocol.WeaviateServer
+	schema *schema.Manager
+	batch  *objects.BatchManager
 }
 
 func NewAPI(
-	appState *state.State,
+	traverser *traverser.Traverser,
 	authComposer composer.TokenFunc,
+	allowAnonymousAccess bool,
+	schemaManager *schema.Manager,
+	batchManager *objects.BatchManager,
 	config *config.Config,
 	log logrus.FieldLogger,
 ) *API {
 	return &API{
-		log:      log,
-		config:   config,
-		svc:      v1.NewService(appState.Traverser, authComposer, true, appState.SchemaManager, appState.BatchManager, config, log),
-		appState: appState,
+		log:    log,
+		config: config,
+		svc:    v1.NewService(traverser, authComposer, true, schemaManager, batchManager, config, log),
+		schema: schemaManager,
+		batch:  batchManager,
 	}
+}
+
+// Search serves vector search over the offloaded tenant on object storage.
+func (a *API) Search(ctx context.Context, req SearchRequest) {
 }
