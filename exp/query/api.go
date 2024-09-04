a32003a55262d4f1bbb849243298b52fbd381f4c
Kaviraj <kavirajkanagaraj@gmail.com>
2024-09-04 16:30:34 +0200

    feat: Search api for `exp/query`
    Currently it exposes gRPC endpoint `weaviate.v1.Weaviate.Search`
    
    that does couple of things.
    1. Make sure the tenant searching for is offloaded tenants by talking to schema manager (`/v1/schema/<collection>/tenants`)
    2. It downloads the offloaded tenant data from object storage to local disk
    
    It doesn't load and search the index yet. will do that in follow up PR
    
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

--- exp/query/api.go
+++ exp/query/api.go
@@ -13,51 +13,79 @@ package query
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/sirupsen/logrus"
-	v1 "github.com/weaviate/weaviate/adapters/handlers/grpc/v1"
-	"github.com/weaviate/weaviate/entities/models"
-	"github.com/weaviate/weaviate/grpc/generated/protocol/v1"
-	"github.com/weaviate/weaviate/usecases/auth/authentication/composer"
-	"github.com/weaviate/weaviate/usecases/config"
-	"github.com/weaviate/weaviate/usecases/objects"
-	"github.com/weaviate/weaviate/usecases/schema"
-	"github.com/weaviate/weaviate/usecases/traverser"
+	modsloads3 "github.com/weaviate/weaviate/modules/offload-s3"
 )
+
+const (
+	TenantOffLoadingStatus = "FROZEN"
+
+	// NOTE(kavi): using fixed nodeName that offloaded tenant under that prefix on object storage.
+	// TODO(kavi): Make it dynamic.
+	nodeName = "weaviate-0"
+)
+
+var ErrInvalidTenant = errors.New("invalid tenant status")
 
 // API is the core query API that is transport agnostic (http, grpc, etc).
 type API struct {
 	log    logrus.FieldLogger
-	config *config.Config
+	config *Config
 
+	tenant TenantInfo
 	// svc provides the underlying search API via v1.WeaviateServer
 	// TODO(kavi): Split `v1.WeaviateServer` into composable `v1.Searcher` and everything else.
-	svc    protocol.WeaviateServer
-	schema *schema.Manager
-	batch  *objects.BatchManager
+	// svc    protocol.WeaviateServer
+	// schema *schema.Manager
+	// batch  *objects.BatchManager
+	offload *modsloads3.Module
+}
+
+type TenantInfo interface {
+	TenantStatus(ctx context.Context, collection, tenant string) (string, error)
 }
 
 func NewAPI(
-	traverser *traverser.Traverser,
-	authComposer composer.TokenFunc,
-	allowAnonymousAccess bool,
-	schemaManager *schema.Manager,
-	batchManager *objects.BatchManager,
-	config *config.Config,
+	tenant TenantInfo,
+	offload *modsloads3.Module,
+	config *Config,
 	log logrus.FieldLogger,
 ) *API {
 	return &API{
-		log:    log,
-		config: config,
-		svc:    v1.NewService(traverser, authComposer, true, schemaManager, batchManager, config, log),
-		schema: schemaManager,
-		batch:  batchManager,
+		log:     log,
+		config:  config,
+		tenant:  tenant,
+		offload: offload,
 	}
 }
 
 // Search serves vector search over the offloaded tenant on object storage.
 func (a *API) Search(ctx context.Context, req *SearchRequest) (*SearchResponse, error) {
-	// This will fetch tenants/objects from the underlying object store
+	info, err := a.tenant.TenantStatus(ctx, req.Collection, req.Tenant)
+	if err != nil {
+		return nil, err
+	}
+
+	if info != TenantOffLoadingStatus {
+		return nil, fmt.Errorf("tenant %q is not offloaded, %w", req.Tenant, ErrInvalidTenant)
+	}
+
+	// src - s3://<collection>/<tenant>/<node>/
+	// dst (local) - <data-path/<collection>/<tenant>
+	if err := a.offload.Download(ctx, req.Collection, req.Tenant, nodeName); err != nil {
+		return nil, err
+	}
+
+	// Expectations
+	// 0. Check if tenant status == FROZEN
+	// 1. Build local path `/<datapath>/<collection>/<tenant>` from search request
+	// 2. If it doesn't exist, fetch from object store to same path as above
+	// 3. If it doesn't exist on object store, return 404
+	// 4. Once in the local disk, load the index
+	// 5. Searve the search.
 	return &SearchResponse{}, nil
 }
 
@@ -67,5 +95,5 @@ type SearchRequest struct {
 }
 
 type SearchResponse struct {
-	Objects []models.Object
+	IDs []string
 }
---
+++ exp/querytenant/info.go
@@ -0,0 +1,72 @@
+package querytenant
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+)
+
+var ErrTenantNotFound = errors.New("tenant not found")
+
+type TenantInfo struct {
+	addr   string
+	path   string
+	client *http.Client
+}
+
+func NewTenantInfo(addr, path string) *TenantInfo {
+	c := http.DefaultClient
+	c.Timeout = 2 * time.Second
+
+	return &TenantInfo{
+		addr:   addr,
+		path:   path,
+		client: c,
+	}
+}
+
+func (t *TenantInfo) TenantStatus(ctx context.Context, collection, tenant string) (string, error) {
+	respPayload := []struct {
+		Error  []ErrorResponse `json:"error,omitempty"`
+		Status string          `json:"activityStatus"`
+		Name   string          `json:"name"`
+	}{}
+
+	path := fmt.Sprintf(t.path, collection)
+	u := fmt.Sprintf("%s/%s", t.addr, path)
+
+	resp, err := t.client.Get(u)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if err := json.NewDecoder(resp.Body).Decode(&respPayload); err != nil {
+		return "", err
+	}
+
+	var rerr error
+	for _, v := range respPayload {
+		for _, e := range v.Error {
+			rerr = errors.Join(rerr, errors.New(e.Message))
+		}
+		if strings.ToLower(v.Name) == strings.ToLower(tenant) {
+			return v.Status, nil
+		}
+
+	}
+
+	if rerr != nil {
+		return "", rerr
+	}
+
+	return "", ErrTenantNotFound
+}
+
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
