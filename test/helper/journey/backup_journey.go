2581be638496e881b66b7f01495e75abb5d7ab1a
moogacs <contact@m-mooga.com>
2024-09-10 16:49:32 +0200

    backport : add backup list endpoint

--- test/helper/journey/backup_journey.go
+++ test/helper/journey/backup_journey.go
@@ -64,6 +64,14 @@ func backupJourney(t *testing.T, className, backend, backupID string,
 	t.Run("create backup", func(t *testing.T) {
 		resp, err := helper.CreateBackup(t, helper.DefaultBackupConfig(), className, backend, backupID)
 		helper.AssertRequestOk(t, resp, err, nil)
+
+		listresp, err := helper.ListBackup(t, helper.DefaultBackupConfig(), className, backend)
+		helper.AssertRequestOk(t, listresp, err, nil)
+
+		require.Equal(t, backupID, listresp.Payload[0].ID)
+		require.Equal(t, models.BackupCreateResponseStatusSTARTED, listresp.Payload[0].Status)
+		require.Equal(t, []string{className}, listresp.Payload[0].Classes)
+
 		// wait for create success
 		createTime := time.Now()
 		for {
@@ -93,6 +101,13 @@ func backupJourney(t *testing.T, className, backend, backupID string,
 
 		require.Equal(t, *statusResp.Payload.Status,
 			string(backup.Success), statusResp.Payload.Error)
+
+		listresp, err = helper.ListBackup(t, helper.DefaultBackupConfig(), className, backend)
+		helper.AssertRequestOk(t, listresp, err, nil)
+
+		require.Equal(t, backupID, listresp.Payload[0].ID)
+		require.Equal(t, models.BackupCreateResponseStatusSUCCESS, listresp.Payload[0].Status)
+		require.Equal(t, []string{className}, listresp.Payload[0].Classes)
 	})
 
 	t.Run("delete class for restoration", func(t *testing.T) {
