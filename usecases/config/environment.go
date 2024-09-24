e99a8e10a07680dcc471895fb01d4204db0f81ce
moogacs <contact@m-mooga.com>
2024-09-24 15:18:48 +0200

    make rbac middleware opt-in

--- usecases/config/environment.go
+++ usecases/config/environment.go
@@ -458,7 +458,7 @@ func FromEnv(config *Config) error {
 		config.HNSWStartupWaitForVectorCache = true
 	}
 
-	if entcfg.Enabled(os.Getenv("AUTHORIZATION_ENABLE_RBAC")) {
+	if entcfg.Enabled(os.Getenv("ENABLE_RBAC_ENFORCER")) {
 		config.EnableRBACEnforcer = true
 	}
 	// explicitly reset sentry config
