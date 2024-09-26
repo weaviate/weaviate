781d2ad2e4eead2e4be382a62931e58580183d48
Nate Wilkinson <nathanwilk7@gmail.com>
2024-09-26 04:34:32 -0600

    Add MAINTENANCE_NODES env var for maintenance mode (#5867)
    * Add support for maintenance mode
    
    * Working on making maintenance nodes configurable via hostname
    
    * update tests to work with maintenance nodes/mode
    
    * updating tests
    
    * unit/acceptance tests for maintenance mode
    
    * comments
    
    * clean up
    
    * close resp body to make ci/cd happy
    
    * check for nil before closing body
    
    * Add timeouts to maintenance mode test and only-acceptance option to run
    
    * simpler env var check, remove maintenance mode test options in favor of generic acceptance package option
    
    ---------
    
    Co-authored-by: Marcin Antas <antas.marcin@gmail.com>
    Co-authored-by: Nate Wilkinson <nathan@weaviate.io>

--- usecases/config/environment.go
+++ usecases/config/environment.go
@@ -769,5 +769,19 @@ func parseClusterConfig() (cluster.Config, error) {
 
 	cfg.FastFailureDetection = entcfg.Enabled(os.Getenv("FAST_FAILURE_DETECTION"))
 
+	// MAINTENANCE_NODES is experimental and subject to removal/change. It is an optional, comma
+	// separated list of hostnames that are in maintenance mode. In maintenance mode, the node will
+	// return an error for all data requests, but will still participate in the raft cluster and
+	// schema operations. This can be helpful is a node is too overwhelmed by startup tasks to handle
+	// data requests and you need to start up the node to give it time to "catch up".
+
+	// avoid the case where strings.Split creates a slice with only the empty string as I think
+	// that will be confusing for future code. eg ([]string{""}) instead of an empty slice ([]string{}).
+	// https://go.dev/play/p/3BDp1vhbkYV shows len(1) when m = "".
+	cfg.MaintenanceNodes = []string{}
+	if m := os.Getenv("MAINTENANCE_NODES"); m != "" {
+		cfg.MaintenanceNodes = strings.Split(m, ",")
+	}
+
 	return cfg, nil
 }
