28c847d8c866f2d7df4712d36be9dad6b8bd7eb5
Ajit Mistry <ajit@weaviate.io>
2024-10-01 10:56:19 +0000

    works

--- adapters/repos/db/vector/hnsw/distancer/c/hamming_bitwise_avx256_amd64.c
+++ adapters/repos/db/vector/hnsw/distancer/c/hamming_bitwise_avx256_amd64.c
@@ -5,23 +5,9 @@
 #include <string.h>
 #include <sys/types.h>
 
-static void init_lookup64bit(uint64_t *lut) {
-  for (int i = 0; i < 256; i++) {
-    uint64_t count = 0;
-    int n = i;
-    while (n) {
-      count += n & 1;
-      n >>= 1;
-    }
-    lut[i] = count;
-  }
-}
-
-static uint64_t popcnt_lookup_64bit(uint8_t *data, uint64_t *n,
-                                    uint64_t *lookup64bit) {
-  if (data == NULL || n == NULL || lookup64bit == NULL) {
-    return 0; // Error handling: invalid input
-  }
+uint64_t popcnt_lookup_64bit(uint8_t *data, uint64_t *n,
+                             uint64_t *lookup64bit) {
+  // return data[3];
 
   size_t result = 0;
   size_t i = 0;
@@ -45,52 +31,62 @@ static uint64_t popcnt_lookup_64bit(uint8_t *data, uint64_t *n,
   return result;
 }
 
-void hamming_bitwise_256(uint64_t *a, uint64_t *b, uint64_t *res, long *len) {
-  // Initialize the lookup table
+uint64_t popcount(uint64_t *x, uint64_t *lookup64bit) {
+  uint8_t *data = (uint8_t *)x;
+  uint64_t result = 0;
+  result += lookup64bit[data[0]];
 
-  uint64_t *lookup64bit = (uint64_t *)malloc(256 * sizeof(uint64_t));
+  result += lookup64bit[data[1]];
 
-  if (lookup64bit == NULL) {
-    return;
-  }
+  result += lookup64bit[data[2]];
 
-  // if (!is_initialized) {
-  init_lookup64bit(lookup64bit);
-  // is_initialized = 1;
-  // }
+  result += lookup64bit[data[3]];
+
+  result += lookup64bit[data[4]];
+
+  result += lookup64bit[data[5]];
+
+  result += lookup64bit[data[6]];
+
+  result += lookup64bit[data[7]];
+
+  return result;
+}
+
+void hamming_bitwise_256(uint64_t *a, uint64_t *b, uint64_t *res, long *len,
+                         uint64_t *lookup64bit) {
 
   int n = *len;
   uint64_t sum = 0;
 
   // fast path for small dimensions
-  if (n < 8) {
-    do {
-      sum += __builtin_popcountll(a[0] ^ b[0]);
-      //   sum += a[0] != b[0] ? 1 : 0;
-      n--;
-      a++;
-      b++;
-    } while (n);
+  // if (n < 8) {
+  //   do {
+  //     sum += __builtin_popcountll(a[0] ^ b[0]);
 
-    *res = sum;
-    return;
-  }
+  //     n--;
+  //     a++;
+  //     b++;
+  //   } while (n);
 
-  __m256i ones_256 = _mm256_set1_epi32(1);
+  //   *res = sum;
+  //   return;
+  // }
+
   __m256i zeros_256 = _mm256_setzero_si256();
 
   size_t size = 256 / 8;
 
   while (n >= 16) {
     __m256i a_vec0 = _mm256_loadu_si256((__m256i const *)a);
-    __m256i a_vec1 = _mm256_loadu_si256((__m256i const *)a + 4);
-    __m256i a_vec2 = _mm256_loadu_si256((__m256i const *)a + 8);
-    __m256i a_vec3 = _mm256_loadu_si256((__m256i const *)a + 12);
+    __m256i a_vec1 = _mm256_loadu_si256((__m256i const *)(a + 4));
+    __m256i a_vec2 = _mm256_loadu_si256((__m256i const *)(a + 8));
+    __m256i a_vec3 = _mm256_loadu_si256((__m256i const *)(a + 12));
 
     __m256i b_vec0 = _mm256_loadu_si256((__m256i const *)b);
-    __m256i b_vec1 = _mm256_loadu_si256((__m256i const *)b + 1);
-    __m256i b_vec2 = _mm256_loadu_si256((__m256i const *)b + 2);
-    __m256i b_vec3 = _mm256_loadu_si256((__m256i const *)b + 3);
+    __m256i b_vec1 = _mm256_loadu_si256((__m256i const *)(b + 4));
+    __m256i b_vec2 = _mm256_loadu_si256((__m256i const *)(b + 8));
+    __m256i b_vec3 = _mm256_loadu_si256((__m256i const *)(b + 12));
 
     __m256i cmp_result_1 = _mm256_xor_si256(a_vec0, b_vec0);
     __m256i cmp_result_2 = _mm256_xor_si256(a_vec1, b_vec1);
@@ -114,9 +110,13 @@ void hamming_bitwise_256(uint64_t *a, uint64_t *b, uint64_t *res, long *len) {
 
   while (n >= 4) {
     __m256i a_vec0 = _mm256_loadu_si256((__m256i const *)a);
+
     __m256i b_vec0 = _mm256_loadu_si256((__m256i const *)b);
+
     __m256i cmp_result_1 = _mm256_xor_si256(a_vec0, b_vec0);
+
     uint8_t *p1 = (uint8_t *)&cmp_result_1;
+
     sum += popcnt_lookup_64bit(p1, &size, lookup64bit);
 
     n -= 4;
@@ -125,7 +125,8 @@ void hamming_bitwise_256(uint64_t *a, uint64_t *b, uint64_t *res, long *len) {
   }
 
   while (n) {
-    sum += __builtin_popcountll(a[0] ^ b[0]);
+    uint64_t xor = a[0] ^ b[0];
+    sum += popcount(&xor, lookup64bit);
     n--;
     a++;
     b++;
