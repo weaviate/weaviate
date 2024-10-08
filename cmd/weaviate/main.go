78644c53e55a4a9492e7f5ccb5f0e516de11b39f
Kaviraj <kavirajkanagaraj@gmail.com>
2024-10-08 09:31:17 +0200

    put grpc options into functions
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

--- cmd/weaviate/main.go
+++ cmd/weaviate/main.go
@@ -101,25 +101,7 @@ func main() {
 		}
 		svrMetrics := monitoring.NewServerMetrics(opts.Monitoring, prometheus.DefaultRegisterer)
 		listener = monitoring.CountingListener(listener, svrMetrics.TCPActiveConnections.WithLabelValues("grpc"))
-		grpcOptions := []grpc.ServerOption{
-			grpc.StatsHandler(monitoring.NewGrpcStatsHandler(
-				svrMetrics.InflightRequests,
-				svrMetrics.RequestBodySize,
-				svrMetrics.ResponseBodySize,
-			)),
-		}
-
-		grpcInterceptUnary := grpc.ChainUnaryInterceptor(
-			monitoring.UnaryServerInstrument(svrMetrics.RequestDuration),
-		)
-		grpcOptions = append(grpcOptions, grpcInterceptUnary)
-
-		grpcInterceptStream := grpc.ChainStreamInterceptor(
-			monitoring.StreamServerInstrument(svrMetrics.RequestDuration),
-		)
-		grpcOptions = append(grpcOptions, grpcInterceptStream)
-
-		grpcServer := grpc.NewServer(grpcOptions...)
+		grpcServer := grpc.NewServer(GrpcOptions(*svrMetrics)...)
 		reflection.Register(grpcServer)
 		protocol.RegisterWeaviateServer(grpcServer, grpcQuerier)
 
@@ -146,4 +128,26 @@ type Options struct {
 	Target     string            `long:"target" description:"how should weaviate-server be running as e.g: querier, ingester, etc"`
 	Query      query.Config      `group:"query" namespace:"query"`
 	Monitoring monitoring.Config `group:"monitoring" namespace:"monitoring"`
+}
+
+func GrpcOptions(svrMetrics monitoring.ServerMetrics) []grpc.ServerOption {
+	grpcOptions := []grpc.ServerOption{
+		grpc.StatsHandler(monitoring.NewGrpcStatsHandler(
+			svrMetrics.InflightRequests,
+			svrMetrics.RequestBodySize,
+			svrMetrics.ResponseBodySize,
+		)),
+	}
+
+	grpcInterceptUnary := grpc.ChainUnaryInterceptor(
+		monitoring.UnaryServerInstrument(svrMetrics.RequestDuration),
+	)
+	grpcOptions = append(grpcOptions, grpcInterceptUnary)
+
+	grpcInterceptStream := grpc.ChainStreamInterceptor(
+		monitoring.StreamServerInstrument(svrMetrics.RequestDuration),
+	)
+	grpcOptions = append(grpcOptions, grpcInterceptStream)
+
+	return grpcOptions
 }
