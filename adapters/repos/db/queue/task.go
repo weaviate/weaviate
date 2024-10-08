0b7f54c339e9376a7106b04647359c1cafe3f4fc
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2024-10-08 14:09:44 +0400

    add task and queueDecoder types

---
+++ adapters/repos/db/queue/task.go
@@ -0,0 +1,15 @@
+package queue
+
+import "context"
+
+type Task interface {
+	Key() uint64
+	Op() uint64
+	Execute(context.Context) error
+}
+
+type TaskGroup interface {
+	Task
+
+	AddTask(op uint64, task Task) bool
+}
