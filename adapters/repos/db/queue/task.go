540c101669bdb0518703d436c59475d36edf809c
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2024-10-09 16:55:02 +0400

    prepare for stale chunks handling

--- adapters/repos/db/queue/task.go
+++ adapters/repos/db/queue/task.go
@@ -2,9 +2,14 @@ package queue
 
 import "context"
 
+type TaskRecord struct {
+	Op  uint8
+	Key uint64
+}
+
 type Task interface {
 	Key() uint64
-	Op() uint64
+	Op() uint8
 	Execute(context.Context) error
 }
 
