7d742f9b6e423447770c961caa06396e49ffe3fd
Charles <charlespierse@gmail.com>
2024-10-09 17:12:56 +0100

    Weaviate hosted embeddings Module (#4885)
    * Add initial groundwork for WCS embed service
    
    * Add rest handler for wcs embed
    
    * Fix pathMask
    
    * Update tests
    
    * Add temp host for testing
    
    * Refactor text2vec-wcs -> text2vec-weaviate
    
    * Further refactoring of wcs -> weaviate embed
    
    * Fix tests
    
    * Swagger formatting changes
    
    * Fix linting
    
    * Resolve conflicts
    
    * Add dimensions, add model to header, general cleanup
    
    * Formatting
    
    * Fix tests
    
    * Whitelist API key name
    
    * Remove validation
    
    * Fix tests
    
    * Change API key name
    
    * Update whitelisted header
    
    * Remove unused code

---
+++ modules/text2vec-weaviate/ent/class_settings.go
@@ -0,0 +1,75 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2024 Weaviate B.V. All rights reserved.
+//
+//  CONTACT: hello@weaviate.io
+//
+
+package ent
+
+import (
+	"github.com/weaviate/weaviate/entities/models"
+	"github.com/weaviate/weaviate/entities/moduletools"
+	basesettings "github.com/weaviate/weaviate/usecases/modulecomponents/settings"
+)
+
+const (
+	// TODO: replace docker internal host with actual host
+	DefaultBaseURL               = "https://embedding.labs.weaviate.io"
+	DefaultWeaviateModel         = "Snowflake/snowflake-arctic-embed-m-v1.5"
+	DefaultTruncate              = "right"
+	DefaultVectorizeClassName    = true
+	DefaultPropertyIndexed       = true
+	DefaultVectorizePropertyName = false
+)
+
+const (
+	SnowflakeArcticEmbedM = "Snowflake/snowflake-arctic-embed-m-v1.5"
+)
+
+var SnowflakeArcticEmbedMDefaultDimensions int64 = 768
+
+type classSettings struct {
+	basesettings.BaseClassSettings
+	cfg moduletools.ClassConfig
+}
+
+func NewClassSettings(cfg moduletools.ClassConfig) *classSettings {
+	return &classSettings{cfg: cfg, BaseClassSettings: *basesettings.NewBaseClassSettings(cfg, false)}
+}
+
+func (cs *classSettings) Model() string {
+	return cs.BaseClassSettings.GetPropertyAsString("model", DefaultWeaviateModel)
+}
+
+func (cs *classSettings) Truncate() string {
+	return cs.BaseClassSettings.GetPropertyAsString("truncate", DefaultTruncate)
+}
+
+func (cs *classSettings) BaseURL() string {
+	return cs.BaseClassSettings.GetPropertyAsString("baseURL", DefaultBaseURL)
+}
+
+func (cs *classSettings) Dimensions() *int64 {
+	defaultValue := PickDefaultDimensions(cs.Model())
+	return cs.BaseClassSettings.GetPropertyAsInt64("dimensions", defaultValue)
+}
+
+func (cs *classSettings) Validate(class *models.Class) error {
+	if err := cs.BaseClassSettings.Validate(class); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func PickDefaultDimensions(model string) *int64 {
+	if model == SnowflakeArcticEmbedM {
+		return &SnowflakeArcticEmbedMDefaultDimensions
+	}
+	return nil
+}
