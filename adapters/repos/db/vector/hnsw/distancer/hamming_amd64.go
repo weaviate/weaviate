77b41f75a87b16292887ba4db4db7e5256b9a82d
John Trengrove <john@weaviate.io>
2024-10-11 12:03:35 +1000

    Only use AVX256 hamming implementation

--- adapters/repos/db/vector/hnsw/distancer/hamming_amd64.go
+++ adapters/repos/db/vector/hnsw/distancer/hamming_amd64.go
@@ -19,9 +19,10 @@ import (
 func init() {
 	if cpu.X86.HasAMXBF16 && cpu.X86.HasAVX512 {
 		hammingImpl = asm.HammingAVX512
-		hammingBitwiseImpl = asm.HammingBitwiseAVX512
 	} else if cpu.X86.HasAVX2 {
 		hammingImpl = asm.HammingAVX256
+	}
+	if cpu.X86.HasAVX2 {
 		hammingBitwiseImpl = asm.HammingBitwiseAVX256
 	}
 }
