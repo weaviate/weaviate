ba5bfb5adf8ec2706d49619126ad48d794a7d691
Marcin Antas <antas.marcin@gmail.com>
2024-10-12 13:13:16 +0200

    [gcp] backport: adjust google modules

--- test/modules/many-modules/many_modules_test.go
+++ test/modules/many-modules/many_modules_test.go
@@ -70,7 +70,7 @@ func composeModules() (composeModules *docker.Compose) {
 		WithText2VecAWS(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), os.Getenv("AWS_SESSION_TOKEN")).
 		WithGenerativeOpenAI().
 		WithGenerativeCohere().
-		WithGenerativeGoogle(os.Getenv("PALM_APIKEY")).
+		WithGenerativeGoogle(os.Getenv("GOOGLE_APIKEY")).
 		WithGenerativeAWS(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), os.Getenv("AWS_SESSION_TOKEN")).
 		WithGenerativeAnyscale().
 		WithQnAOpenAI().
