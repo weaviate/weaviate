505844129c82f3c298d827bd1e76bf08e2d54ce8
Abdel Rodríguez <abdelr1982@gmail.com>
2024-10-14 12:03:24 +0200

    allocating memory outside the loop to free the memory pool a bit

--- adapters/repos/db/vector/hnsw/search.go
+++ adapters/repos/db/vector/hnsw/search.go
@@ -221,6 +221,16 @@ func (h *hnsw) searchLayerByVectorWithDistancer(queryVector []float32,
 	}
 	var connectionsReusable []uint64
 
+	var sliceConnectionsReusable *common.VectorUint64Slice
+	var slicePendingNextRound *common.VectorUint64Slice
+	var slicePendingThisRound *common.VectorUint64Slice
+
+	if allowList != nil && h.acornSearch.Load() {
+		sliceConnectionsReusable = h.pools.tempVectorsUint64.Get(M * h.maximumConnectionsLayerZero)
+		slicePendingNextRound = h.pools.tempVectorsUint64.Get(h.maximumConnectionsLayerZero)
+		slicePendingThisRound = h.pools.tempVectorsUint64.Get(h.maximumConnectionsLayerZero)
+	}
+
 	for candidates.Len() > 0 {
 		var dist float32
 		candidate := candidates.Pop()
@@ -250,17 +260,11 @@ func (h *hnsw) searchLayerByVectorWithDistancer(queryVector []float32,
 			continue
 		}
 
-		var sliceConnectionsReusable *common.VectorUint64Slice
-		var slicePendingNextRound *common.VectorUint64Slice
-		var slicePendingThisRound *common.VectorUint64Slice
 		if allowList == nil || !useAcorn {
 			connectionsReusable = make([]uint64, len(candidateNode.connections[level]))
 			copy(connectionsReusable, candidateNode.connections[level])
 		} else {
-			sliceConnectionsReusable = h.pools.tempVectorsUint64.Get(M * h.maximumConnectionsLayerZero)
 			connectionsReusable = sliceConnectionsReusable.Slice
-			slicePendingNextRound = h.pools.tempVectorsUint64.Get(h.maximumConnectionsLayerZero)
-			slicePendingThisRound = h.pools.tempVectorsUint64.Get(h.maximumConnectionsLayerZero)
 			pendingNextRound := slicePendingNextRound.Slice
 			pendingThisRound := slicePendingThisRound.Slice
 
@@ -417,11 +421,20 @@ func (h *hnsw) searchLayerByVectorWithDistancer(queryVector []float32,
 				}
 			}
 		}
+<<<<<<< HEAD
 		if allowList != nil && useAcorn {
 			h.pools.tempVectorsUint64.Put(sliceConnectionsReusable)
 			h.pools.tempVectorsUint64.Put(slicePendingNextRound)
 			h.pools.tempVectorsUint64.Put(slicePendingThisRound)
 		}
+=======
+	}
+
+	if allowList != nil && h.acornSearch.Load() {
+		h.pools.tempVectorsUint64.Put(sliceConnectionsReusable)
+		h.pools.tempVectorsUint64.Put(slicePendingNextRound)
+		h.pools.tempVectorsUint64.Put(slicePendingThisRound)
+>>>>>>> 310987d38 (allocating memory outside the loop to free the memory pool a bit)
 	}
 
 	h.pools.pqCandidates.Put(candidates)
