6dbab2af06ba61a9efdd0dd5e4b9ff2d11662177
Marcin Antas <antas.marcin@gmail.com>
2024-10-16 12:20:31 +0200

    fix TestAddAndRemoveObjectClass set default deletionStrategy to DeleteOnConflict

--- test/acceptance/schema/get_schema_without_client_test.go
+++ test/acceptance/schema/get_schema_without_client_test.go
@@ -77,7 +77,7 @@ func testGetSchemaWithoutClient(t *testing.T) {
 				},
 				"replicationConfig": map[string]interface{}{
 					"factor":           float64(1),
-					"deletionStrategy": "NoAutomatedResolution",
+					"deletionStrategy": "DeleteOnConflict",
 				},
 				"vectorizer": "text2vec-contextionary", // global default from env var, see docker-compose-test.yml
 				"invertedIndexConfig": map[string]interface{}{
