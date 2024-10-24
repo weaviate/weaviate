c2a38a1f360372cb94b411c854b29ef9600df5cd
moogacs <contact@m-mooga.com>
2024-10-24 13:58:37 +0200

    refact: unify state interface to enhance usage and testing

--- usecases/schema/parser.go
+++ usecases/schema/parser.go
@@ -22,17 +22,18 @@ import (
 	"github.com/weaviate/weaviate/entities/schema"
 	schemaConfig "github.com/weaviate/weaviate/entities/schema/config"
 	"github.com/weaviate/weaviate/entities/vectorindex"
+	"github.com/weaviate/weaviate/usecases/cluster"
 	"github.com/weaviate/weaviate/usecases/config"
 	shardingConfig "github.com/weaviate/weaviate/usecases/sharding/config"
 )
 
 type Parser struct {
-	clusterState clusterState
+	clusterState cluster.Selector
 	configParser VectorConfigParser
 	validator    validator
 }
 
-func NewParser(cs clusterState, vCfg VectorConfigParser, v validator) *Parser {
+func NewParser(cs cluster.Selector, vCfg VectorConfigParser, v validator) *Parser {
 	return &Parser{
 		clusterState: cs,
 		configParser: vCfg,
