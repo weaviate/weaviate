aa14a1dc9592e1d80f5b648ef37219a1c653c1ae
Parker Duckworth <parkerduckworth@gmail.com>
2024-11-07 10:38:58 -0500

    segregate objects and vectors_compressed segment counts

--- adapters/repos/db/lsmkv/segment_group_compaction.go
+++ adapters/repos/db/lsmkv/segment_group_compaction.go
@@ -487,11 +487,30 @@ func (sg *SegmentGroup) stripTmpExtension(oldPath, left, right string) (string, 
 }
 
 func (sg *SegmentGroup) monitorSegments() {
-	if sg.metrics == nil || sg.metrics.groupClasses ||
-		// Keeping metering to only the critical buckets helps
-		// cut down on noise when monitoring
-		!strings.HasSuffix(sg.dir, helpers.ObjectsBucketLSM) ||
-		!strings.HasSuffix(sg.dir, helpers.VectorsCompressedBucketLSM) {
+	if sg.metrics == nil || sg.metrics.groupClasses {
+		return
+	}
+
+	// Keeping metering to only the critical buckets helps
+	// cut down on noise when monitoring
+	if sg.metrics.criticalBucketsOnly {
+		if !strings.HasSuffix(sg.dir, helpers.ObjectsBucketLSM) ||
+			!strings.HasSuffix(sg.dir, helpers.VectorsCompressedBucketLSM) {
+			return
+		}
+		if strings.HasSuffix(sg.dir, helpers.ObjectsBucketLSM) {
+			sg.metrics.ObjectsBucketSegments.With(prometheus.Labels{
+				"strategy": sg.strategy,
+				"path":     sg.dir,
+			}).Set(float64(sg.Len()))
+		}
+		if strings.HasSuffix(sg.dir, helpers.VectorsCompressedBucketLSM) {
+			sg.metrics.CompressedVecsBucketSegments.With(prometheus.Labels{
+				"strategy": sg.strategy,
+				"path":     sg.dir,
+			}).Set(float64(sg.Len()))
+		}
+		sg.reportSegmentStats()
 		return
 	}
 
@@ -499,7 +518,10 @@ func (sg *SegmentGroup) monitorSegments() {
 		"strategy": sg.strategy,
 		"path":     sg.dir,
 	}).Set(float64(sg.Len()))
+	sg.reportSegmentStats()
+}
 
+func (sg *SegmentGroup) reportSegmentStats() {
 	stats := sg.segmentLevelStats()
 	stats.fillMissingLevels()
 	stats.report(sg.metrics, sg.strategy, sg.dir)
