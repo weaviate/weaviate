6d19a5005315f280b59fdb7eec5dad0d67c1865d
Kaviraj <kavirajkanagaraj@gmail.com>
2024-11-08 13:11:51 +0100

    tests for fetcher
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

--- exp/query/lsm_fetcher.go
+++ exp/query/lsm_fetcher.go
@@ -22,7 +22,7 @@ const (
 
 // LSMFetcher fetches the tenant's LSM data using given `upstream` without or without local cache.
 type LSMFetcher struct {
-	cache    *DiskCache
+	cache    LSMCache
 	upstream LSMDownloader
 	log      logrus.FieldLogger
 
@@ -34,6 +34,12 @@ type LSMDownloader interface {
 	DownloadToPath(ctx context.Context, collection, tenant, nodeName, path string) error
 }
 
+// LSMCache provides caching of tenant's LSM data.
+type LSMCache interface {
+	Tenant(collection, tenantID string) (*TenantCache, error)
+	AddTenant(collection, tenantID string, version uint64) error
+}
+
 // NewLSMFetcher creates LSMFetcher without cache.
 // Every call is forwarded to given `upstream` to fetch the LSM data for the tenant.
 func NewLSMFetcher(basePath string, upstream LSMDownloader, log logrus.FieldLogger) *LSMFetcher {
@@ -41,7 +47,7 @@ func NewLSMFetcher(basePath string, upstream LSMDownloader, log logrus.FieldLogg
 }
 
 // NewLSMFetcherWithCache creates LSMFetcher with cache.
-func NewLSMFetcherWithCache(basePath string, upstream LSMDownloader, cache *DiskCache, log logrus.FieldLogger) *LSMFetcher {
+func NewLSMFetcherWithCache(basePath string, upstream LSMDownloader, cache LSMCache, log logrus.FieldLogger) *LSMFetcher {
 	return &LSMFetcher{
 		basePath: basePath,
 		cache:    cache,
@@ -58,14 +64,14 @@ func NewLSMFetcherWithCache(basePath string, upstream LSMDownloader, cache *Disk
 // 4. if cache is enabled, found in the cache and local version is up to date, return from the cache.
 func (l *LSMFetcher) Fetch(ctx context.Context, collection, tenant string, version uint64) (*lsmkv.Store, string, error) {
 	var (
-		lsmPath  string
-		download bool = true
+		tenantPath string
+		download   bool = true
 	)
 
 	if l.cache != nil {
 		c, err := l.cache.Tenant(collection, tenant)
-		if err == nil && c.Version >= version {
-			lsmPath = c.AbsolutePath()
+		if err == nil && version != 0 && c.Version >= version {
+			tenantPath = c.AbsolutePath()
 			download = false
 		}
 		if err != nil && !errors.Is(err, ErrTenantNotFound) {
@@ -74,7 +80,7 @@ func (l *LSMFetcher) Fetch(ctx context.Context, collection, tenant string, versi
 	}
 
 	if download {
-		dst := path.Join(
+		tenantPath = path.Join(
 			l.basePath,
 			strings.ToLower(collection),
 			strings.ToLower(tenant),
@@ -86,15 +92,16 @@ func (l *LSMFetcher) Fetch(ctx context.Context, collection, tenant string, versi
 				"collection": collection,
 				"tenant":     tenant,
 				"nodeName":   nodeName,
-				"dst":        dst,
+				"dst":        tenantPath,
 			}).Debug("starting download to path")
-			if err := l.upstream.DownloadToPath(ctx, collection, tenant, nodeName, dst); err != nil {
+			if err := l.upstream.DownloadToPath(ctx, collection, tenant, nodeName, tenantPath); err != nil {
 				return nil, "", err
 			}
 		}
 
-		lsmPath = path.Join(dst, defaultLSMRoot)
 	}
+
+	lsmPath := path.Join(tenantPath, defaultLSMRoot)
 
 	store, err := lsmkv.New(lsmPath, lsmPath, l.log, nil, cyclemanager.NewCallbackGroupNoop(), cyclemanager.NewCallbackGroupNoop())
 	if err != nil {
--- exp/query/lsm_fetcher_test.go
+++ exp/query/lsm_fetcher_test.go
@@ -1,6 +1,153 @@
 package query
 
-import "testing"
+import (
+	"context"
+	"os"
+	"path"
+	"testing"
 
-func TestLSMFetcher(t *testing.T) {
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	fixedSize = 1 << 10 // 1KB
+)
+
+func TestLSMFetcher_withoutCache(t *testing.T) {
+	root := path.Join(os.TempDir(), t.Name())
+	defer func() {
+		os.RemoveAll(root)
+	}()
+
+	ctx := context.Background()
+	downloader := newMockDownloader(t)
+	f := NewLSMFetcher(root, downloader, testLogger())
+
+	// used without cache, so every `Fetch()` should download tenant data from upstream
+	kv, gotFullpath, err := f.Fetch(ctx, "test-collection", "test-tenant", 0)
+	expectedFullpath := path.Join(root, "test-collection", "test-tenant", defaultLSMRoot)
+	require.NoError(t, err)
+	assert.NotNil(t, kv)
+	assert.Equal(t, expectedFullpath, gotFullpath)
+	assert.Equal(t, 1, downloader.count)
+
+	// Any version number is irrelavent if cache is disabled and still should download from upstream
+	kv, gotFullpath, err = f.Fetch(ctx, "test-collection", "test-tenant", 111) // non-zero version number
+	expectedFullpath = path.Join(root, "test-collection", "test-tenant", defaultLSMRoot)
+	require.NoError(t, err)
+	assert.NotNil(t, kv)
+	assert.Equal(t, expectedFullpath, gotFullpath)
+	assert.Equal(t, 2, downloader.count)
+
+	// Fetching different tenant should also download from upstream
+	kv, gotFullpath, err = f.Fetch(ctx, "test-collection2", "test-tenant2", 111) // non-zero version number
+	expectedFullpath = path.Join(root, "test-collection2", "test-tenant2", defaultLSMRoot)
+	require.NoError(t, err)
+	assert.NotNil(t, kv)
+	assert.Equal(t, expectedFullpath, gotFullpath)
+	assert.Equal(t, 3, downloader.count)
+}
+
+func TestLSMFetcher_withCache(t *testing.T) {
+	root := path.Join(os.TempDir(), t.Name())
+	defer func() {
+		os.RemoveAll(root)
+	}()
+
+	ctx := context.Background()
+	upstream := newMockDownloader(t)
+	cache := newMockCache(t, root)
+	f := NewLSMFetcherWithCache(root, upstream, cache, testLogger())
+
+	// passing version 0, should always download from upstream
+	kv, gotFullpath, err := f.Fetch(ctx, "test-collection", "test-tenant", 0)
+	expectedFullpath := path.Join(root, "test-collection", "test-tenant", defaultLSMRoot)
+	require.NoError(t, err)
+	assert.NotNil(t, kv)
+	assert.Equal(t, expectedFullpath, gotFullpath)
+	assert.Equal(t, 1, upstream.count) // upstream called.
+
+	// if cache miss, download from upstream
+	kv, gotFullpath, err = f.Fetch(ctx, "test-collection", "test-tenant", 127)
+	expectedFullpath = path.Join(root, "test-collection", "test-tenant", defaultLSMRoot)
+	require.NoError(t, err)
+	assert.NotNil(t, kv)
+	assert.Equal(t, expectedFullpath, gotFullpath)
+	assert.Equal(t, 2, upstream.count)      // upstream called.
+	assert.Equal(t, 0, cache.cacheHitCount) // shouldn't hit the cache
+
+	// if cache hit and version is not matched, download from upstream
+	err = cache.AddTenant("test-collection", "test-tenant", 1)
+	require.NoError(t, err)
+	kv, gotFullpath, err = f.Fetch(ctx, "test-collection", "test-tenant", 2) // version mismatch
+	expectedFullpath = path.Join(root, "test-collection", "test-tenant", defaultLSMRoot)
+	require.NoError(t, err)
+	assert.NotNil(t, kv)
+	assert.Equal(t, expectedFullpath, gotFullpath)
+	assert.Equal(t, 3, upstream.count)      // upstream called.
+	assert.Equal(t, 1, cache.cacheHitCount) // hit the cache, but still called upstream because of version mismatch
+
+	// if cache hit and version is matched, return from the cache
+	kv, gotFullpath, err = f.Fetch(ctx, "test-collection", "test-tenant", 1) // version match
+	expectedFullpath = path.Join(root, "test-collection", "test-tenant", defaultLSMRoot)
+	require.NoError(t, err)
+	assert.NotNil(t, kv)
+	assert.Equal(t, expectedFullpath, gotFullpath)
+	assert.Equal(t, 3, upstream.count)      // upstream not called.
+	assert.Equal(t, 2, cache.cacheHitCount) // hit the cache,
+}
+
+type mockDownloader struct {
+	t     *testing.T
+	count int
+}
+
+func newMockDownloader(t *testing.T) *mockDownloader {
+	return &mockDownloader{
+		t: t,
+	}
+}
+
+func (m *mockDownloader) DownloadToPath(ctx context.Context, collection, tenant, nodeName, path string) error {
+	createTempFileWithSize(m.t, path, "mock-lsm-downloader", fixedSize)
+	m.count++
+	return nil
+}
+
+type mockCache struct {
+	t             *testing.T
+	tenants       map[string]*TenantCache
+	basePath      string
+	cacheHitCount int
+	addCount      int
+}
+
+func newMockCache(t *testing.T, basePath string) *mockCache {
+	return &mockCache{
+		t:        t,
+		tenants:  make(map[string]*TenantCache),
+		basePath: basePath,
+	}
+}
+
+func (m *mockCache) Tenant(collection, tenantID string) (*TenantCache, error) {
+	tc, ok := m.tenants[collection+tenantID]
+	if !ok {
+		return nil, ErrTenantNotFound
+	}
+	m.cacheHitCount++
+	return tc, nil
+}
+
+func (m *mockCache) AddTenant(collection, tenantID string, version uint64) error {
+	tc := &TenantCache{
+		Collection: collection,
+		TenantID:   tenantID,
+		Version:    version,
+		basePath:   m.basePath,
+	}
+	m.tenants[collection+tenantID] = tc
+	m.addCount++
+	return nil
 }
