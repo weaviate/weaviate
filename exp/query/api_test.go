e786afe441d96c8c7cde58abfd9e8c54eb3f3050
Kaviraj <kavirajkanagaraj@gmail.com>
2024-11-08 10:22:15 +0100

    add tests for disk cache
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

--- exp/query/api_test.go
+++ exp/query/api_test.go
@@ -14,7 +14,6 @@ package query
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"io"
 	"os"
 	"sync"
@@ -54,6 +53,7 @@ func TestAPI_propertyFilters(t *testing.T) {
 	offmod := modsloads3.Module{}
 	config := Config{}
 	logger := testLogger()
+	lsm := NewLSMFetcher("", &offmod, logger)
 	ctx := context.Background()
 	st, err := stopwords.NewDetectorFromPreset(stopwords.EnglishPreset)
 	require.NoError(t, err)
@@ -62,7 +62,7 @@ func TestAPI_propertyFilters(t *testing.T) {
 	require.NoError(t, err)
 	defer store.Shutdown(ctx)
 
-	api := NewAPI(testSchemaInfo, &offmod, vectorize, st, &config, logger)
+	api := NewAPI(testSchemaInfo, lsm, vectorize, st, &config, logger)
 
 	cases := []struct {
 		name     string
@@ -139,9 +139,6 @@ func TestAPI_propertyFilters(t *testing.T) {
 			res, err := api.propertyFilters(ctx, store, testCollection, nil, testTenant, tc.filters, tc.limit)
 			require.NoError(t, err)
 			assert.Equal(t, tc.expCount, len(res))
-			for _, r := range res {
-				fmt.Printf("%#v\n", r)
-			}
 		})
 	}
 }
@@ -154,6 +151,7 @@ func TestAPI_vectorSearch(t *testing.T) {
 	offmod := modsloads3.Module{}
 	config := Config{}
 	logger := testLogger()
+	lsm := NewLSMFetcher("", &offmod, logger)
 	ctx := context.Background()
 	st, err := stopwords.NewDetectorFromPreset(stopwords.EnglishPreset)
 	require.NoError(t, err)
@@ -162,7 +160,7 @@ func TestAPI_vectorSearch(t *testing.T) {
 	require.NoError(t, err)
 	defer store.Shutdown(ctx)
 
-	api := NewAPI(testSchemaInfo, &offmod, vectorize, st, &config, logger)
+	api := NewAPI(testSchemaInfo, lsm, vectorize, st, &config, logger)
 
 	nearText := []string{"biology"}
 
