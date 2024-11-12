17bd0983b95d9eedffe9c62da0ccad058c074e4f
André Mourão <andre.mourao@weaviate.io>
2024-11-12 14:03:29 +0000

    test(varenc): :zap: Add benchmarks to varenc codec code

--- adapters/repos/db/lsmkv/varenc/variable_encoding_test.go
+++ adapters/repos/db/lsmkv/varenc/variable_encoding_test.go
@@ -13,6 +13,7 @@ package varenc
 
 import (
 	"fmt"
+	"math"
 	"math/rand/v2"
 	"testing"
 
@@ -141,5 +142,107 @@ func TestCompareDeltaEncoders(t *testing.T) {
 
 	for i := range encs {
 		fmt.Printf("Encoder %d: %d %f\n", i, sizes[i], float64(sizes[0])/float64(sizes[i]))
+	}
+}
+
+func BenchmarkDeltaEncoders(b *testing.B) {
+	encs := []VarEncEncoder[uint64]{
+		&SimpleEncoder[uint64]{},
+		&VarIntEncoder{},
+		&VarIntDeltaEncoder{},
+	}
+
+	valueCount := terms.BLOCK_SIZE
+
+	for _, enc := range encs {
+		enc.Init(valueCount)
+	}
+
+	values := make([]uint64, valueCount)
+	values[0] = 100
+	for i := 1; i < valueCount; i++ {
+		values[i] = values[i-1] + rand.Uint64N(10)
+	}
+
+	for _, enc := range encs {
+		b.Run(fmt.Sprintf("%T", enc), func(b *testing.B) {
+			b.ReportAllocs()
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				packed := enc.Encode(values)
+				decoded := enc.Decode(packed)
+				assert.Equal(b, values, decoded)
+			}
+		})
+	}
+}
+
+func BenchmarkDecoder(b *testing.B) {
+	encs := []VarEncEncoder[uint64]{
+		&SimpleEncoder[uint64]{},
+		&VarIntEncoder{},
+		&VarIntDeltaEncoder{},
+	}
+	valueCount := terms.BLOCK_SIZE
+
+	// Example input values
+	values := make([]uint64, valueCount)
+
+	values[0] = 100
+	for i := range values[1:] {
+		// values[i+1] = values[i] + rand.Uint64N(10)
+		values[i+1] = values[i] + 1
+		// values[i] = uint64(math.Round(rand.Float64()*10)) + 1
+	}
+
+	for _, enc := range encs {
+		enc.Init(valueCount)
+		packed := enc.Encode(values)
+		b.Run(fmt.Sprintf("%T", enc), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				enc.Decode(packed)
+			}
+			b.ReportMetric(float64(((b.N*valueCount)/1000000))/b.Elapsed().Seconds(), "Mvalues/s")
+		})
+
+	}
+}
+
+func BenchmarkDecoderMulti(b *testing.B) {
+	encs := [][]VarEncEncoder[uint64]{
+		{&SimpleEncoder[uint64]{}, &SimpleEncoder[uint64]{}},
+		{&VarIntDeltaEncoder{}, &VarIntEncoder{}},
+	}
+
+	valueCount := terms.BLOCK_SIZE
+
+	// Example input values
+	docIds := make([]uint64, valueCount)
+
+	docIds[0] = 100
+	tfs := make([]uint64, valueCount)
+	tfs[0] = 1
+	for i := range docIds[1:] {
+		docIds[i+1] = docIds[i] + rand.Uint64N(10)
+		// docIds[i+1] = docIds[i] + 1
+		tfs[i] = uint64(math.Round(rand.Float64()*10)) + 1
+	}
+
+	for _, enc := range encs {
+		enc[0].Init(valueCount)
+		enc[1].Init(valueCount)
+
+		packedDocIds := enc[0].Encode(docIds)
+		packedTfs := enc[1].Encode(tfs)
+
+		b.Run(fmt.Sprintf("%T", enc), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				enc[0].Decode(packedDocIds)
+				enc[1].Decode(packedTfs)
+			}
+			b.ReportMetric(float64(((b.N*valueCount)/1000000))/b.Elapsed().Seconds(), "Mvalues/s")
+		})
+
 	}
 }
