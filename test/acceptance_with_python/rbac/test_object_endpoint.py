be3f7f1052547f30e323212564f432459b4faea0
Dirk Kulawiak <dirk@semi.technology>
2024-11-19 08:26:00 +0100

    Add validation for other objects endpoints

--- test/acceptance_with_python/rbac/test_object_endpoint.py
+++ test/acceptance_with_python/rbac/test_object_endpoint.py
@@ -160,3 +160,246 @@ def test_obj_replace(request: SubRequest):
 
             client.roles.revoke(user="custom-user", roles=both_write.name)
             client.roles.delete(both_write.name)
+
+
+def test_obj_update(request: SubRequest):
+    with weaviate.connect_to_local(
+        port=8081, grpc_port=50052, auth_credentials=wvc.init.Auth.api_key("admin-key")
+    ) as client:
+        name = _sanitize_role_name(request.node.name)
+        client.collections.delete(name)
+        client.roles.delete(name)
+        col = client.collections.create(name=name)
+
+        uuid_to_replace = col.data.insert({})
+
+        with weaviate.connect_to_local(
+            port=8081, grpc_port=50052, auth_credentials=wvc.init.Auth.api_key("custom-key")
+        ) as client_no_rights:
+            client.roles.create(
+                name=name,
+                permissions=RBAC.permissions.collections_data(
+                    collection=col.name, actions=RBAC.actions.collection_data.UPDATE
+                )
+                + RBAC.permissions.collections(
+                    collection=col.name, actions=RBAC.actions.collection.READ
+                ),
+            )
+            client.roles.assign(user="custom-user", roles=name)
+
+            source_no_rights = client_no_rights.collections.get(
+                name
+            )  # no network call => no RBAC check
+
+            source_no_rights.data.update(uuid=uuid_to_replace, properties={})
+
+            client.roles.revoke(user="custom-user", roles=name)
+            client.roles.delete(name)
+
+        # no metadata read
+        with weaviate.connect_to_local(
+            port=8081, grpc_port=50052, auth_credentials=wvc.init.Auth.api_key("custom-key")
+        ) as client_no_rights:
+            both_write = client.roles.create(
+                name=name,
+                permissions=RBAC.permissions.collections_data(
+                    collection=col.name, actions=RBAC.actions.collection_data.UPDATE
+                ),
+            )
+            client.roles.assign(user="custom-user", roles=both_write.name)
+
+            source_no_rights = client_no_rights.collections.get(
+                name
+            )  # no network call => no RBAC check
+            with pytest.raises(weaviate.exceptions.UnexpectedStatusCodeException) as e:
+                source_no_rights.data.update(uuid=uuid_to_replace, properties={})
+            assert e.value.status_code == 403
+
+            client.roles.revoke(user="custom-user", roles=both_write.name)
+            client.roles.delete(both_write.name)
+
+        # no data create
+        with weaviate.connect_to_local(
+            port=8081, grpc_port=50052, auth_credentials=wvc.init.Auth.api_key("custom-key")
+        ) as client_no_rights:
+            both_write = client.roles.create(
+                name=name,
+                permissions=RBAC.permissions.collections(
+                    collection=col.name, actions=RBAC.actions.collection.READ
+                ),
+            )
+            client.roles.assign(user="custom-user", roles=both_write.name)
+
+            source_no_rights = client_no_rights.collections.get(
+                name
+            )  # no network call => no RBAC check
+            with pytest.raises(weaviate.exceptions.UnexpectedStatusCodeException) as e:
+                source_no_rights.data.replace(uuid=uuid_to_replace, properties={})
+            assert e.value.status_code == 403
+
+            client.roles.revoke(user="custom-user", roles=both_write.name)
+            client.roles.delete(both_write.name)
+
+
+def test_obj_delete(request: SubRequest):
+    with weaviate.connect_to_local(
+        port=8081, grpc_port=50052, auth_credentials=wvc.init.Auth.api_key("admin-key")
+    ) as client:
+        name = _sanitize_role_name(request.node.name)
+        client.collections.delete(name)
+        client.roles.delete(name)
+        col = client.collections.create(name=name)
+
+        with weaviate.connect_to_local(
+            port=8081, grpc_port=50052, auth_credentials=wvc.init.Auth.api_key("custom-key")
+        ) as client_no_rights:
+            uuid_to_delete = col.data.insert({})
+
+            client.roles.create(
+                name=name,
+                permissions=RBAC.permissions.collections_data(
+                    collection=col.name, actions=RBAC.actions.collection_data.DELETE
+                )
+                + RBAC.permissions.collections(
+                    collection=col.name, actions=RBAC.actions.collection.READ
+                ),
+            )
+            client.roles.assign(user="custom-user", roles=name)
+
+            col_no_rights = client_no_rights.collections.get(
+                name
+            )  # no network call => no RBAC check
+
+            assert len(col) == 1
+            col_no_rights.data.delete_by_id(uuid=uuid_to_delete)
+            assert len(col) == 0
+            client.roles.revoke(user="custom-user", roles=name)
+            client.roles.delete(name)
+
+        # no metadata read
+        with weaviate.connect_to_local(
+            port=8081, grpc_port=50052, auth_credentials=wvc.init.Auth.api_key("custom-key")
+        ) as client_no_rights:
+            uuid_to_delete = col.data.insert({})
+
+            client.roles.create(
+                name=name,
+                permissions=RBAC.permissions.collections_data(
+                    collection=col.name, actions=RBAC.actions.collection_data.DELETE
+                ),
+            )
+            client.roles.assign(user="custom-user", roles=name)
+
+            col_no_rights = client_no_rights.collections.get(
+                name
+            )  # no network call => no RBAC check
+
+            assert len(col) == 1
+            with pytest.raises(weaviate.exceptions.UnexpectedStatusCodeException) as e:
+                col_no_rights.data.delete_by_id(uuid=uuid_to_delete)
+            assert e.value.status_code == 403
+            assert len(col) == 1
+
+            client.roles.revoke(user="custom-user", roles=name)
+            client.roles.delete(name)
+
+        # no data create
+        with weaviate.connect_to_local(
+            port=8081, grpc_port=50052, auth_credentials=wvc.init.Auth.api_key("custom-key")
+        ) as client_no_rights:
+            both_write = client.roles.create(
+                name=name,
+                permissions=RBAC.permissions.collections(
+                    collection=col.name, actions=RBAC.actions.collection.READ
+                ),
+            )
+            client.roles.assign(user="custom-user", roles=both_write.name)
+
+            col_no_rights = client_no_rights.collections.get(
+                name
+            )  # no network call => no RBAC check
+            with pytest.raises(weaviate.exceptions.UnexpectedStatusCodeException) as e:
+                col_no_rights.data.delete_by_id(uuid=uuid_to_delete)
+            assert e.value.status_code == 403
+
+            client.roles.revoke(user="custom-user", roles=both_write.name)
+            client.roles.delete(both_write.name)
+
+
+def test_obj_exists(request: SubRequest):
+    with weaviate.connect_to_local(
+        port=8081, grpc_port=50052, auth_credentials=wvc.init.Auth.api_key("admin-key")
+    ) as client:
+        name = _sanitize_role_name(request.node.name)
+        client.collections.delete(name)
+        client.roles.delete(name)
+        col = client.collections.create(name=name)
+
+        uuid_to_check = col.data.insert({})
+
+        with weaviate.connect_to_local(
+            port=8081, grpc_port=50052, auth_credentials=wvc.init.Auth.api_key("custom-key")
+        ) as client_no_rights:
+            client.roles.create(
+                name=name,
+                permissions=RBAC.permissions.collections_data(
+                    collection=col.name, actions=RBAC.actions.collection_data.READ
+                )
+                + RBAC.permissions.collections(
+                    collection=col.name, actions=RBAC.actions.collection.READ
+                ),
+            )
+            client.roles.assign(user="custom-user", roles=name)
+
+            col_no_rights = client_no_rights.collections.get(
+                name
+            )  # no network call => no RBAC check
+
+            assert col_no_rights.data.exists(uuid=uuid_to_check)
+            client.roles.revoke(user="custom-user", roles=name)
+            client.roles.delete(name)
+
+        # no metadata read
+        with weaviate.connect_to_local(
+            port=8081, grpc_port=50052, auth_credentials=wvc.init.Auth.api_key("custom-key")
+        ) as client_no_rights:
+            client.roles.create(
+                name=name,
+                permissions=RBAC.permissions.collections_data(
+                    collection=col.name, actions=RBAC.actions.collection_data.READ
+                ),
+            )
+            client.roles.assign(user="custom-user", roles=name)
+
+            col_no_rights = client_no_rights.collections.get(
+                name
+            )  # no network call => no RBAC check
+
+            with pytest.raises(weaviate.exceptions.UnexpectedStatusCodeException) as e:
+                col_no_rights.data.exists(uuid=uuid_to_check)
+            assert e.value.status_code == 403
+
+            client.roles.revoke(user="custom-user", roles=name)
+            client.roles.delete(name)
+
+        # no data read
+        with weaviate.connect_to_local(
+            port=8081, grpc_port=50052, auth_credentials=wvc.init.Auth.api_key("custom-key")
+        ) as client_no_rights:
+            both_write = client.roles.create(
+                name=name,
+                permissions=RBAC.permissions.collections(
+                    collection=col.name, actions=RBAC.actions.collection.READ
+                ),
+            )
+            client.roles.assign(user="custom-user", roles=both_write.name)
+
+            col_no_rights = client_no_rights.collections.get(
+                name
+            )  # no network call => no RBAC check
+            with pytest.raises(weaviate.exceptions.UnexpectedStatusCodeException) as e:
+                col_no_rights.data.exists(uuid=uuid_to_check)
+            assert e.value.status_code == 403
+
+            client.roles.revoke(user="custom-user", roles=both_write.name)
+            client.roles.delete(both_write.name)
