f3a8891273b9e422723eb9af3893a136c2c61a06
Dirk Kulawiak <dirk@semi.technology>
2024-11-20 10:23:35 +0100

    Add auth to batch GQL and add tests

--- test/acceptance/authz/gql_batch_test.go
+++ test/acceptance/authz/gql_batch_test.go
@@ -1,33 +1,18 @@
-//                           _       _
-// __      _____  __ ___   ___  __ _| |_ ___
-// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
-//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
-//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
-//
-//  Copyright © 2016 - 2024 Weaviate B.V. All rights reserved.
-//
-//  CONTACT: hello@weaviate.io
-//
-
 package test
 
 import (
 	"testing"
 
-	"github.com/go-openapi/strfmt"
-	"github.com/google/uuid"
-
-	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 	"github.com/weaviate/weaviate/client/authz"
-	"github.com/weaviate/weaviate/client/objects"
+	"github.com/weaviate/weaviate/client/graphql"
 	"github.com/weaviate/weaviate/entities/models"
 	"github.com/weaviate/weaviate/entities/schema"
 	"github.com/weaviate/weaviate/test/helper"
 	"github.com/weaviate/weaviate/usecases/auth/authorization"
 )
 
-func TestAuthZObjectValidate(t *testing.T) {
+func TestAuthZGQLBatchValidate(t *testing.T) {
 	adminKey := "admin-key"
 	adminAuth := helper.CreateAuth(adminKey)
 	customUser := "custom-user"
@@ -39,8 +24,8 @@ func TestAuthZObjectValidate(t *testing.T) {
 	helper.SetupClient("127.0.0.1:8081")
 	defer helper.ResetClient()
 
-	roleName := "AuthZObjectValidateTestRole"
-	className := "AuthZObjectValidateTest"
+	roleName := "AuthZGQLBatchTestRole"
+	className := "AuthZGQLBatchTestClass"
 	deleteObjectClass(t, className, adminAuth)
 	require.NoError(t, createClass(t, &models.Class{
 		Class: className,
@@ -52,37 +37,33 @@ func TestAuthZObjectValidate(t *testing.T) {
 		},
 	}, adminAuth))
 
+	all := "*"
+
 	t.Run("All rights", func(t *testing.T) {
-		deleteRole := &models.Role{
+		role := &models.Role{
 			Name: &roleName,
 			Permissions: []*models.Permission{
 				{
 					Action:     &readDataAction,
-					Collection: &className,
+					Collection: &all,
 				},
 				{
 					Action:     &readSchemaAction,
-					Collection: &className,
+					Collection: &all,
 				},
 			},
 		}
-		helper.DeleteRole(t, adminKey, *deleteRole.Name)
-		helper.CreateRole(t, adminKey, deleteRole)
+		helper.DeleteRole(t, adminKey, *role.Name)
+		helper.CreateRole(t, adminKey, role)
 		_, err := helper.Client(t).Authz.AssignRole(
 			authz.NewAssignRoleParams().WithID(customUser).WithBody(authz.AssignRoleBody{Roles: []string{roleName}}),
 			adminAuth,
 		)
 		require.Nil(t, err)
 
-		paramsObj := objects.NewObjectsValidateParams().WithBody(
-			&models.Object{
-				ID:    strfmt.UUID(uuid.New().String()),
-				Class: className,
-				Properties: map[string]interface{}{
-					"prop": "test",
-				},
-			})
-		_, err = helper.Client(t).Objects.ObjectsValidate(paramsObj, customAuth)
+		paramsObj := graphql.NewGraphqlBatchParams().WithBody(
+			models.GraphQLQueries{{Query: "mutation assign role $role: AssignRoleInput!", OperationName: "POST"}})
+		_, err = helper.Client(t).Graphql.GraphqlBatch(paramsObj, customAuth)
 		require.Nil(t, err)
 
 		_, err = helper.Client(t).Authz.RevokeRole(
@@ -93,37 +74,29 @@ func TestAuthZObjectValidate(t *testing.T) {
 		helper.DeleteRole(t, adminKey, roleName)
 	})
 
-	actions := []string{readDataAction, readSchemaAction}
-	for _, action := range actions {
-		t.Run("Only rights for "+action, func(t *testing.T) {
-			deleteRole := &models.Role{
-				Name: &roleName,
-				Permissions: []*models.Permission{{
-					Action:     &action,
-					Collection: &className,
-				}},
+	permissionsAll := [][]*models.Permission{
+		{{Action: &readDataAction, Collection: &all}, {Action: &readSchemaAction, Collection: &className}},
+		{{Action: &readDataAction, Collection: &className}, {Action: &readSchemaAction, Collection: &all}},
+	}
+	for _, permissions := range permissionsAll {
+		t.Run("Only read data action for a single class", func(t *testing.T) {
+			role := &models.Role{
+				Name:        &roleName,
+				Permissions: permissions,
 			}
-			helper.DeleteRole(t, adminKey, *deleteRole.Name)
-			helper.CreateRole(t, adminKey, deleteRole)
+			helper.DeleteRole(t, adminKey, roleName)
+			helper.CreateRole(t, adminKey, role)
 			_, err := helper.Client(t).Authz.AssignRole(
 				authz.NewAssignRoleParams().WithID(customUser).WithBody(authz.AssignRoleBody{Roles: []string{roleName}}),
 				adminAuth,
 			)
 			require.Nil(t, err)
 
-			paramsObj := objects.NewObjectsValidateParams().WithBody(
-				&models.Object{
-					ID:    strfmt.UUID(uuid.New().String()),
-					Class: className,
-					Properties: map[string]interface{}{
-						"prop": "test",
-					},
-				})
-			_, err = helper.Client(t).Objects.ObjectsValidate(paramsObj, customAuth)
+			paramsObj := graphql.NewGraphqlBatchParams().WithBody(
+				models.GraphQLQueries{{Query: "mutation assign role $role: AssignRoleInput!", OperationName: "POST"}})
+			resp, err := helper.Client(t).Graphql.GraphqlBatch(paramsObj, customAuth)
 			require.NotNil(t, err)
-			var errNoAuth *objects.ObjectsValidateForbidden
-			require.True(t, errors.As(err, &errNoAuth))
-
+			require.Nil(t, resp)
 			_, err = helper.Client(t).Authz.RevokeRole(
 				authz.NewRevokeRoleParams().WithID(customUser).WithBody(authz.RevokeRoleBody{Roles: []string{roleName}}),
 				adminAuth,
