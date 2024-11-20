e4597ef75b6898291a9b11336935ccb0d5cf6d8b
Tommy Smith <tommy@weaviate.io>
2024-11-20 10:33:53 +0000

    Move ShardsData back to reduce diff

--- usecases/auth/authorization/types.go
+++ usecases/auth/authorization/types.go
@@ -228,14 +228,6 @@ func CollectionsData(classes ...string) []string {
 	return paths
 }
 
-func ShardsData(class string, shards ...string) []string {
-	var paths []string
-	for _, shard := range shards {
-		paths = append(paths, Objects(class, shard, "*"))
-	}
-	return paths
-}
-
 func Collections(classes ...string) []string {
 	return append(CollectionsData(classes...), CollectionsMetadata(classes...)...)
 }
@@ -271,6 +263,14 @@ func ShardsMetadata(class string, shards ...string) []string {
 	}
 
 	return resources
+}
+
+func ShardsData(class string, shards ...string) []string {
+	var paths []string
+	for _, shard := range shards {
+		paths = append(paths, Objects(class, shard, "*"))
+	}
+	return paths
 }
 
 func Shards(class string, shards ...string) []string {
