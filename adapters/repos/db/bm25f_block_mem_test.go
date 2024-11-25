bdd80770a6a60bca347d7782bc47dac1b552ff33
André Mourão <andre.mourao@weaviate.io>
2024-11-25 10:04:35 +0000

    fix(bm25): :white_check_mark: Replace require.EqualValues with EqualFloats for improved floating-point comparison in tests

--- adapters/repos/db/bm25f_block_mem_test.go
+++ adapters/repos/db/bm25f_block_mem_test.go
@@ -271,15 +271,15 @@ func TestBM25FJourneyBlockMem(t *testing.T) {
 
 		require.Less(t, len(resAutoCut), len(resNoAutoCut))
 
-		require.EqualValues(t, float32(0.51602507), noautocutscores[0])
-		require.EqualValues(t, float32(0.49750623), noautocutscores[1]) // <= autocut last element
-		require.EqualValues(t, float32(0.34149727), noautocutscores[2])
-		require.EqualValues(t, float32(0.3049518), noautocutscores[3])
-		require.EqualValues(t, float32(0.27547202), noautocutscores[4])
+		EqualFloats(t, float32(0.51602507), noautocutscores[0], 5)
+		EqualFloats(t, float32(0.4975062), noautocutscores[1], 5) // <= autocut last element
+		EqualFloats(t, float32(0.34149727), noautocutscores[2], 5)
+		EqualFloats(t, float32(0.3049518), noautocutscores[3], 5)
+		EqualFloats(t, float32(0.27547202), noautocutscores[4], 5)
 
 		require.Len(t, resAutoCut, 2)
-		require.EqualValues(t, float32(0.51602507), autocutscores[0])
-		require.EqualValues(t, float32(0.49750623), autocutscores[1])
+		EqualFloats(t, float32(0.51602507), autocutscores[0], 5)
+		EqualFloats(t, float32(0.4975062), autocutscores[1], 5)
 	})
 }
 
