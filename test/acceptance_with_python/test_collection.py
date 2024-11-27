51623e3b7c8009ec2a83a913c43ff94816f495fe
Kaviraj <kavirajkanagaraj@gmail.com>
2024-11-27 13:25:39 +0100

    remove unwanted test
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

--- test/acceptance_with_python/test_collection.py
+++ test/acceptance_with_python/test_collection.py
@@ -35,36 +35,3 @@ def test_collection_casing() -> None:
         client.collections.delete("test_collection_case")
         col = client.collections.get("testcollectioncase")
         assert col.exists() is False
-
-
-
-# @pytest.mark.parametrize("to_upper", [True, False])
-# def test_rbac_refs(request: SubRequest, to_upper: bool):
-#     with weaviate.connect_to_local(
-#         port=8081, grpc_port=50052, auth_credentials=wvc.init.Auth.api_key("admin-key")
-#     ) as client:
-#         name = _sanitize_role_name(request.node.name)
-#         if to_upper:
-#             name = name[0].upper() + name[1:]
-#         client.collections.delete(name)
-#         client.roles.delete(name)
-#         collection = client.collections.create(name=name)
-
-#         with weaviate.connect_to_local(
-#             port=8081, grpc_port=50052, auth_credentials=wvc.init.Auth.api_key("custom-key")
-#         ) as client_no_rights:
-#             client.roles.create(
-#                 name=name,
-#                 permissions=[
-#                     RBAC.permissions.collections.read(collection=name),
-#                     RBAC.permissions.collections.objects.read(collection=name),
-#                 ],
-#             )
-#             client.roles.assign(user="custom-user", roles=name)
-#             collection_no_rights = client_no_rights.collections.get(collection.name)
-#             collection_no_rights.query.fetch_objects()
-
-#             client.roles.revoke(user="custom-user", roles=name)
-#             client.roles.delete(name)
-
-#         client.collections.delete(name)
