48580f422623e1de0fdad5f381dd4dce3104f19b
Abdel Rodríguez <abdelr1982@gmail.com>
2024-11-27 09:32:18 +0100

    MVP

---
+++ adapters/repos/db/vector/cache/paged_cache.go
@@ -0,0 +1,427 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2024 Weaviate B.V. All rights reserved.
+//
+//  CONTACT: hello@weaviate.io
+//
+
+package cache
+
+import (
+	"context"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"os"
+	"path"
+	"sync"
+
+	"github.com/pkg/errors"
+	"github.com/weaviate/weaviate/adapters/repos/db/vector/hnsw/distancer"
+)
+
+const maxCallerId = 10
+
+type genericDistancer[T float32 | uint64 | byte] interface {
+	SingleDist(x, y []T) (float32, error)
+}
+
+type page[T float32 | byte | uint64] struct {
+	sync.RWMutex
+	ids            map[uint64]int
+	rawVectors     []byte
+	dimensions     int
+	typeSize       int
+	extractElement func([]byte) T
+	putElement     func([]byte, T)
+}
+
+func newFloat32Page() *page[float32] {
+	return &page[float32]{
+		typeSize: 4,
+		ids:      make(map[uint64]int),
+		extractElement: func(raw []byte) float32 {
+			return math.Float32frombits(binary.LittleEndian.Uint32(raw))
+		},
+		putElement: func(raw []byte, value float32) {
+			binary.LittleEndian.PutUint32(raw, math.Float32bits(value))
+		},
+	}
+}
+
+func newUint64Page() *page[uint64] {
+	return &page[uint64]{
+		typeSize: 8,
+		ids:      make(map[uint64]int),
+		extractElement: func(raw []byte) uint64 {
+			return binary.LittleEndian.Uint64(raw)
+		},
+		putElement: func(raw []byte, value uint64) {
+			binary.LittleEndian.PutUint64(raw, value)
+		},
+	}
+}
+
+func newBytePage() *page[byte] {
+	return &page[byte]{
+		typeSize: 1,
+		ids:      make(map[uint64]int),
+		extractElement: func(raw []byte) byte {
+			return raw[0]
+		},
+		putElement: func(raw []byte, value byte) {
+			raw[0] = value
+		},
+	}
+}
+
+func (p *page[T]) extractVector(id uint64) ([]T, error) {
+	p.RLock()
+	pos, found := p.ids[id]
+	p.RUnlock()
+	if !found {
+		return nil, fmt.Errorf("no vector found on this page with id: %d", id)
+	}
+	rawPos := pos * p.dimensions * p.typeSize
+	result := make([]T, p.dimensions)
+	for i := range result {
+		result[i] = p.extractElement(p.rawVectors[rawPos+i*p.typeSize:])
+	}
+	return result, nil
+}
+
+func (p *page[T]) add(id uint64, vector []T) {
+	p.Lock()
+	defer p.Unlock()
+	if p.dimensions == 0 {
+		p.dimensions = len(vector)
+	}
+	p.ids[id] = len(p.ids)
+	raw := make([]byte, p.dimensions*p.typeSize)
+	for i, x := range vector {
+		p.putElement(raw[i*p.typeSize:], x)
+	}
+	p.rawVectors = append(p.rawVectors, raw...)
+}
+
+func (p *page[T]) len() int {
+	return len(p.ids)
+}
+
+func (p *page[T]) load(path string, pageSize int) error {
+	f, err := os.OpenFile(path, os.O_RDONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	defer f.Close()
+	buff := make([]byte, pageSize*(p.typeSize*p.dimensions+8))
+	n, err := f.Read(buff)
+	if err != io.EOF && err != nil {
+		return err
+	}
+	total := n / (p.typeSize*p.dimensions + 8)
+	for i := 0; i < total; i++ {
+		p.ids[binary.LittleEndian.Uint64(buff[i*8:])] = i
+	}
+	p.rawVectors = buff[total*8 : n]
+	return nil
+}
+
+func (p *page[T]) store(path string) error {
+	p.Lock()
+	defer p.Unlock()
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	total := p.len()
+	buff := make([]byte, total*(p.typeSize*p.dimensions+8))
+	for key, pos := range p.ids {
+		binary.LittleEndian.PutUint64(buff[pos*8:], key)
+	}
+	copy(buff[total*8:], p.rawVectors)
+
+	_, err = f.Write(buff)
+	if err != io.EOF && err != nil {
+		return err
+	}
+	return nil
+}
+
+type PagedCache[T float32 | byte | uint64] struct {
+	sync.RWMutex
+	pageForId         map[uint64]int
+	pages             []*page[T]
+	createPage        func() *page[T]
+	temporalVectors   map[uint64][]T
+	pageSize          int
+	distancer         genericDistancer[T]
+	path              string
+	dimensions        int
+	callerIds         []bool
+	pagesUsedByCaller []map[int]bool
+}
+
+func NewFloat32PagedCache(path string, pageSize int, distancer distancer.Provider) *PagedCache[float32] {
+	return &PagedCache[float32]{
+		pageForId:         make(map[uint64]int),
+		createPage:        newFloat32Page,
+		temporalVectors:   make(map[uint64][]float32),
+		pageSize:          pageSize,
+		distancer:         distancer,
+		path:              path,
+		callerIds:         make([]bool, maxCallerId),
+		pagesUsedByCaller: make([]map[int]bool, maxCallerId),
+	}
+}
+
+func NewUint64PagedCache(path string, pageSize int) *PagedCache[uint64] {
+	return &PagedCache[uint64]{
+		pageForId:         make(map[uint64]int),
+		createPage:        newUint64Page,
+		temporalVectors:   make(map[uint64][]uint64),
+		pageSize:          pageSize,
+		callerIds:         make([]bool, maxCallerId),
+		pagesUsedByCaller: make([]map[int]bool, maxCallerId),
+	}
+}
+
+func NewBytePagedCache(path string, pageSize int) *PagedCache[byte] {
+	return &PagedCache[byte]{
+		pageForId:         make(map[uint64]int),
+		createPage:        newBytePage,
+		temporalVectors:   make(map[uint64][]byte),
+		pageSize:          pageSize,
+		callerIds:         make([]bool, maxCallerId),
+		pagesUsedByCaller: make([]map[int]bool, maxCallerId),
+	}
+}
+
+func (pc *PagedCache[T]) GetFreeCallerId() int {
+	pc.Lock()
+	defer pc.Unlock()
+	for i, bussy := range pc.callerIds {
+		if !bussy {
+			pc.callerIds[i] = true
+			pc.pagesUsedByCaller[i] = make(map[int]bool)
+			return i
+		}
+	}
+	return -1
+}
+
+func (pc *PagedCache[T]) ReturnCallerId(id int) {
+	pc.Lock()
+	defer pc.Unlock()
+	pc.callerIds[id] = false
+	pc.pagesUsedByCaller[id] = nil
+}
+
+func (pc *PagedCache[T]) CollectMemory() {
+	pc.Lock()
+	defer pc.Unlock()
+	bussy := make([]bool, len(pc.pages))
+	for _, m := range pc.pagesUsedByCaller {
+		for i := range m {
+			bussy[i] = true
+		}
+	}
+	for i, page := range pc.pages {
+		if page == nil || bussy[i] {
+			continue
+		}
+		pc.pages[i].store(pc.fileNameForId(i))
+		pc.pages[i] = nil
+	}
+}
+
+func (pc *PagedCache[T]) allocatePageAndRecordCaller(ctx context.Context, id uint64, callerId int, asyncLoad bool) (*page[T], int, error) {
+	pageId, found := pc.pageForId[id]
+	if !found {
+		return nil, 0, fmt.Errorf("unknown vector for id: %d", id)
+	}
+
+	if err := pc.recordCallerId(callerId, pageId); err != nil {
+		return nil, 0, errors.Wrapf(err, "getting vector %d", id)
+	}
+
+	if pageId >= len(pc.pages) {
+		return nil, 0, fmt.Errorf("unknown page for id: %d", id)
+	}
+
+	page := pc.pages[pageId]
+
+	if page == nil {
+		page = pc.createPage()
+		page.Lock()
+		pc.pages[pageId] = page
+		if asyncLoad {
+			go func() {
+				pc.loadPage(ctx, pageId)
+			}()
+		} else {
+			if err := pc.loadPage(ctx, pageId); err != nil {
+				return nil, 0, errors.Wrap(err, "loading page from Get method")
+			}
+		}
+	}
+
+	return page, pageId, nil
+}
+
+func (pc *PagedCache[T]) Get(ctx context.Context, callerId int, id uint64) ([]T, error) {
+	pc.Lock()
+	defer pc.Unlock()
+	if vec, found := pc.temporalVectors[id]; found {
+		return vec, nil
+	}
+
+	page, _, err := pc.allocatePageAndRecordCaller(ctx, id, callerId, false)
+	if err != nil {
+		return nil, err
+	}
+
+	return page.extractVector(id)
+}
+
+func (pc *PagedCache[T]) Connect(callerId int, id, closest uint64) {
+	pc.Lock()
+	defer pc.Unlock()
+	page, pageId, err := pc.allocatePageAndRecordCaller(context.Background(), closest, callerId, false)
+	if err != nil {
+		page = pc.createPage()
+		pc.pages = append(pc.pages, page)
+		pageId = len(pc.pages) - 1
+		if err := pc.recordCallerId(callerId, pageId); err != nil {
+			return
+		}
+	}
+
+	page.add(id, pc.temporalVectors[id])
+	if page.len() > pc.pageSize {
+		pc.splitPageNoLock(pageId, page)
+	}
+	delete(pc.temporalVectors, id)
+	pc.pageForId[id] = pageId
+}
+
+func (pc *PagedCache[T]) MultiGet(ctx context.Context, callerId int, ids []uint64) ([][]T, []error) {
+	results := make([][]T, len(ids))
+	errs := make([]error, len(ids))
+	for i, id := range ids {
+		if err := ctx.Err(); err != nil {
+			return nil, []error{err}
+		}
+		results[i], errs[i] = pc.Get(ctx, callerId, id)
+	}
+	return results, errs
+}
+
+func (pc *PagedCache[T]) Len() int32 {
+	return 0
+}
+
+func (pc *PagedCache[T]) CountVectors() int64 {
+	return 0
+}
+
+func (pc *PagedCache[T]) Delete(ctx context.Context, id uint64) {}
+
+func (pc *PagedCache[T]) Preload(id uint64, vec []T) {
+	pc.Lock()
+	defer pc.Unlock()
+	if pc.dimensions == 0 {
+		pc.dimensions = len(vec)
+	}
+	pc.temporalVectors[id] = vec
+}
+
+func (pc *PagedCache[T]) PreloadNoLock(id uint64, vec []T) {
+	pc.temporalVectors[id] = vec
+}
+
+func (pc *PagedCache[T]) SetSizeAndGrowNoLock(id uint64) {}
+
+func (pc *PagedCache[T]) Prefetch(callerId int, id uint64) {
+	pc.allocatePageAndRecordCaller(context.Background(), id, callerId, true)
+}
+
+func (pc *PagedCache[T]) Grow(size uint64) {}
+
+func (pc *PagedCache[T]) Drop() {}
+
+func (pc *PagedCache[T]) UpdateMaxSize(size int64) {}
+
+func (pc *PagedCache[T]) CopyMaxSize() int64 {
+	return 0
+}
+
+func (pc *PagedCache[T]) All() [][]T {
+	return nil
+}
+
+func (pc *PagedCache[T]) LockAll() {
+	pc.Lock()
+}
+
+func (pc *PagedCache[T]) UnlockAll() {
+	pc.Unlock()
+}
+
+func (pc *PagedCache[T]) loadPage(ctx context.Context, pageId int) error {
+	err := ctx.Err()
+	if err != nil {
+		return err
+	}
+	pc.pages[pageId].dimensions = pc.dimensions
+	pc.pages[pageId].load(pc.fileNameForId(pageId), pc.pageSize)
+	pc.pages[pageId].Unlock()
+	return nil
+}
+
+func (pc *PagedCache[T]) recordCallerId(callerId, pageId int) error {
+	if len(pc.pagesUsedByCaller) <= callerId {
+		return errors.Errorf("invalid callerId: %d", callerId)
+	}
+	pc.pagesUsedByCaller[callerId][pageId] = true
+	return nil
+}
+
+func (pc *PagedCache[T]) splitPageNoLock(id int, page *page[T]) {
+	v := make([][]T, 2)
+	i := 0
+	for id := range page.ids {
+		v[i], _ = page.extractVector(id)
+		i++
+		if i == 2 {
+			break
+		}
+	}
+	page0 := pc.createPage()
+	page1 := pc.createPage()
+	for id := range page.ids {
+		vec, _ := page.extractVector(id)
+		d0, _ := pc.distancer.SingleDist(vec, v[0])
+		d1, _ := pc.distancer.SingleDist(vec, v[1])
+		if d0 < d1 {
+			page0.add(id, vec)
+		} else {
+			page1.add(id, vec)
+			pc.pageForId[id] = len(pc.pages)
+		}
+	}
+	pc.pages[id] = page0
+	pc.pages = append(pc.pages, page1)
+}
+
+func (pc *PagedCache[T]) fileNameForId(id int) string {
+	return path.Join(pc.path, fmt.Sprint(id, ".bin"))
+}
---
+++ adapters/repos/db/vector/cache/paged_cache_test.go
@@ -0,0 +1,264 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2024 Weaviate B.V. All rights reserved.
+//
+//  CONTACT: hello@weaviate.io
+//
+
+package cache
+
+import (
+	"context"
+	"math/rand"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/weaviate/weaviate/adapters/repos/db/vector/hnsw/distancer"
+)
+
+func TestFloat32Page(t *testing.T) {
+	t.Run("extract vector", func(t *testing.T) {
+		page := newFloat32Page()
+		page.add(0, []float32{3, 1, 5})
+		page.add(1, []float32{2, 3, 1})
+		page.add(2, []float32{3, 4, 6})
+
+		_, err := page.extractVector(10)
+		assert.NotNil(t, err)
+
+		v, err := page.extractVector(1)
+		assert.Nil(t, err)
+		assert.Equal(t, float32(2), v[0])
+		assert.Equal(t, float32(3), v[1])
+		assert.Equal(t, float32(1), v[2])
+	})
+
+	t.Run("store/load", func(t *testing.T) {
+		page := newFloat32Page()
+		page.add(0, []float32{3, 1, 5})
+		page.add(1, []float32{2, 3, 1})
+		page.add(2, []float32{3, 4, 6})
+		path := path.Join(t.TempDir(), "0.bin")
+		page.store(path)
+
+		page = newFloat32Page()
+		page.dimensions = 3
+		page.load(path, 3)
+
+		_, err := page.extractVector(10)
+		assert.NotNil(t, err)
+
+		v, err := page.extractVector(1)
+		assert.Nil(t, err)
+		assert.Equal(t, float32(2), v[0])
+		assert.Equal(t, float32(3), v[1])
+		assert.Equal(t, float32(1), v[2])
+	})
+}
+
+func TestBytePage(t *testing.T) {
+	t.Run("extract vector", func(t *testing.T) {
+		page := newBytePage()
+		page.add(0, []byte{3, 1, 5})
+		page.add(1, []byte{2, 3, 1})
+		page.add(2, []byte{3, 4, 6})
+
+		_, err := page.extractVector(10)
+		assert.NotNil(t, err)
+
+		v, err := page.extractVector(1)
+		assert.Nil(t, err)
+		assert.Equal(t, byte(2), v[0])
+		assert.Equal(t, byte(3), v[1])
+		assert.Equal(t, byte(1), v[2])
+	})
+}
+
+func TestUint64Page(t *testing.T) {
+	t.Run("extract vector", func(t *testing.T) {
+		page := newUint64Page()
+		page.add(0, []uint64{3, 1, 5})
+		page.add(1, []uint64{2, 3, 1})
+		page.add(2, []uint64{3, 4, 6})
+
+		_, err := page.extractVector(10)
+		assert.NotNil(t, err)
+
+		v, err := page.extractVector(1)
+		assert.Nil(t, err)
+		assert.Equal(t, uint64(2), v[0])
+		assert.Equal(t, uint64(3), v[1])
+		assert.Equal(t, uint64(1), v[2])
+	})
+}
+
+func TestFloat32PagedCache(t *testing.T) {
+	t.Run("preload and get", func(t *testing.T) {
+		cache := NewFloat32PagedCache(t.TempDir(), 100, distancer.NewCosineDistanceProvider())
+		cache.Preload(0, []float32{0, 2, 16})
+		cache.Preload(1, []float32{1, 12, 6})
+
+		vec, err := cache.Get(context.Background(), 0, 0)
+		assert.Nil(t, err)
+		assert.Equal(t, []float32{0, 2, 16}, vec)
+
+		vec, err = cache.Get(context.Background(), 0, 1)
+		assert.Nil(t, err)
+		assert.Equal(t, []float32{1, 12, 6}, vec)
+
+		_, err = cache.Get(context.Background(), 0, 2)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("preload and multiget", func(t *testing.T) {
+		cache := NewFloat32PagedCache(t.TempDir(), 100, distancer.NewCosineDistanceProvider())
+		cache.Preload(0, []float32{0, 2, 16})
+		cache.Preload(1, []float32{1, 12, 6})
+
+		vecs, errs := cache.MultiGet(context.Background(), 0, []uint64{0, 1})
+		assert.Equal(t, int(2), len(errs))
+		for _, err := range errs {
+			assert.Nil(t, err)
+		}
+		assert.Equal(t, []float32{0, 2, 16}, vecs[0])
+		assert.Equal(t, []float32{1, 12, 6}, vecs[1])
+	})
+
+	t.Run("connect", func(t *testing.T) {
+		cache := NewFloat32PagedCache(t.TempDir(), 100, distancer.NewCosineDistanceProvider())
+		cache.Preload(0, []float32{0, 2, 16})
+		cache.Preload(1, []float32{1, 12, 6})
+
+		callerId := cache.GetFreeCallerId()
+
+		cache.Connect(callerId, 0, 0)
+		cacheId, found := cache.pageForId[0]
+		assert.True(t, found)
+		assert.Equal(t, int(0), cacheId)
+		cache.Connect(callerId, 1, 0)
+		cacheId, found = cache.pageForId[1]
+		assert.True(t, found)
+		assert.Equal(t, int(0), cacheId)
+
+		vec, err := cache.Get(context.Background(), callerId, 0)
+		assert.Nil(t, err)
+		assert.Equal(t, []float32{0, 2, 16}, vec)
+
+		vec, err = cache.Get(context.Background(), callerId, 1)
+		assert.Nil(t, err)
+		assert.Equal(t, []float32{1, 12, 6}, vec)
+
+		_, err = cache.Get(context.Background(), callerId, 2)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("splits", func(t *testing.T) {
+		cache := NewFloat32PagedCache(t.TempDir(), 100, distancer.NewCosineDistanceProvider())
+		callerId := cache.GetFreeCallerId()
+		for i := 0; i < 110; i++ {
+			cache.Preload(uint64(i), []float32{rand.Float32(), rand.Float32(), rand.Float32()})
+			cache.Connect(callerId, uint64(i), 0)
+		}
+		total := 0
+		for i := 0; i < 110; i++ {
+			pageId, found := cache.pageForId[uint64(i)]
+			assert.True(t, found)
+			assert.True(t, pageId < 10)
+			total += pageId
+		}
+		assert.True(t, total > 0)
+	})
+
+	t.Run("prefetch", func(t *testing.T) {
+		cache := NewFloat32PagedCache(t.TempDir(), 100, distancer.NewCosineDistanceProvider())
+		cache.Preload(0, []float32{0, 2, 16})
+		cache.Preload(1, []float32{1, 12, 6})
+		callerId := cache.GetFreeCallerId()
+		cache.Connect(callerId, 0, 0)
+		cache.Connect(callerId, 1, 0)
+		cache.pages[0].store(cache.fileNameForId(0))
+		cache.pages[0] = nil
+		cache.Prefetch(callerId, 0)
+		assert.NotNil(t, cache.pages[0])
+		vec, err := cache.Get(context.Background(), callerId, 0)
+		assert.Nil(t, err)
+		assert.Equal(t, []float32{0, 2, 16}, vec)
+	})
+
+	t.Run("clean memory", func(t *testing.T) {
+		cache := NewFloat32PagedCache(t.TempDir(), 100, distancer.NewCosineDistanceProvider())
+		callerId := cache.GetFreeCallerId()
+		cache.Preload(0, []float32{0, 2, 16})
+		cache.Preload(1, []float32{1, 12, 6})
+		cache.Connect(callerId, 0, 0)
+		cache.Connect(callerId, 1, 0)
+		cache.ReturnCallerId(callerId)
+		cache.CollectMemory()
+		assert.Nil(t, cache.pages[0])
+		callerId = cache.GetFreeCallerId()
+		cache.Prefetch(callerId, 0)
+		assert.NotNil(t, cache.pages[0])
+		vec, err := cache.Get(context.Background(), callerId, 0)
+		assert.Nil(t, err)
+		assert.Equal(t, []float32{0, 2, 16}, vec)
+	})
+
+	t.Run("clean memory keeps what is in use", func(t *testing.T) {
+		cache := NewFloat32PagedCache(t.TempDir(), 100, distancer.NewCosineDistanceProvider())
+		callerId := cache.GetFreeCallerId()
+		cache.Preload(0, []float32{0, 2, 16})
+		cache.Preload(1, []float32{1, 12, 6})
+		cache.Connect(callerId, 0, 0)
+		cache.Connect(callerId, 1, 0)
+		cache.ReturnCallerId(callerId)
+		cache.CollectMemory()
+		assert.Nil(t, cache.pages[0])
+		callerId = cache.GetFreeCallerId()
+		cache.Prefetch(callerId, 0)
+		assert.NotNil(t, cache.pages[0])
+		vec, err := cache.Get(context.Background(), callerId, 0)
+		assert.Nil(t, err)
+		assert.Equal(t, []float32{0, 2, 16}, vec)
+		cache.CollectMemory()
+		assert.NotNil(t, cache.pages[0])
+	})
+}
+
+func TestUint64PagedCache(t *testing.T) {
+	cache := NewUint64PagedCache(t.TempDir(), 100)
+	cache.Preload(0, []uint64{0, 2, 16})
+	cache.Preload(1, []uint64{1, 12, 6})
+
+	vec, err := cache.Get(context.Background(), 0, 0)
+	assert.Nil(t, err)
+	assert.Equal(t, []uint64{0, 2, 16}, vec)
+
+	vec, err = cache.Get(context.Background(), 0, 1)
+	assert.Nil(t, err)
+	assert.Equal(t, []uint64{1, 12, 6}, vec)
+
+	_, err = cache.Get(context.Background(), 0, 2)
+	assert.NotNil(t, err)
+}
+
+func TestBytePagedCache(t *testing.T) {
+	cache := NewBytePagedCache(t.TempDir(), 100)
+	cache.Preload(0, []byte{0, 2, 16})
+	cache.Preload(1, []byte{1, 12, 6})
+
+	vec, err := cache.Get(context.Background(), 0, 0)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte{0, 2, 16}, vec)
+
+	vec, err = cache.Get(context.Background(), 0, 1)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte{1, 12, 6}, vec)
+
+	_, err = cache.Get(context.Background(), 0, 2)
+	assert.NotNil(t, err)
+}
