a27ce7fdb89d5109a15c9f21ba4e043cac452e7a
André Mourão <andre.mourao@weaviate.io>
2024-12-03 17:16:09 +0000

    Revert "refactor(varenc): :art: Simplify bit length manipulation logic in varint encoding and decoding for better readability"
    This reverts commit 5a1837ad0d20ac4a9b645eea9253dc3a1f99170b.

--- adapters/repos/db/lsmkv/compaction_integration_test.go
+++ adapters/repos/db/lsmkv/compaction_integration_test.go
@@ -395,7 +395,7 @@ func TestCompaction(t *testing.T) {
 		{
 			name: "compactionInvertedStrategy",
 			f: func(ctx context.Context, t *testing.T, opts []BucketOption) {
-				compactionInvertedStrategy(ctx, t, opts, 8629, 8629)
+				compactionInvertedStrategy(ctx, t, opts, 8627, 8627)
 			},
 			opts: []BucketOption{
 				WithStrategy(StrategyInverted),
@@ -404,7 +404,7 @@ func TestCompaction(t *testing.T) {
 		{
 			name: "compactionInvertedStrategy_KeepTombstones",
 			f: func(ctx context.Context, t *testing.T, opts []BucketOption) {
-				compactionInvertedStrategy(ctx, t, opts, 8933, 8933)
+				compactionInvertedStrategy(ctx, t, opts, 8931, 8931)
 			},
 			opts: []BucketOption{
 				WithStrategy(StrategyInverted),
