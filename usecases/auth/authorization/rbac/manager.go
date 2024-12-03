f92dbd00b121b80f3a1e95cc5aaf723499300623
moogacs <contact@m-mooga.com>
2024-12-03 10:07:02 +0100

    add comment

--- usecases/auth/authorization/rbac/manager.go
+++ usecases/auth/authorization/rbac/manager.go
@@ -98,11 +98,12 @@ func (m *manager) RemovePermissions(roleName string, permissions []*authorizatio
 
 func (m *manager) DeleteRoles(roles ...string) error {
 	for _, roleName := range roles {
+		// remove role
 		roleRemoved, err := m.casbin.RemoveFilteredNamedPolicy("p", 0, prefixRoleName(roleName))
 		if err != nil {
 			return err
 		}
-
+		// remove role assignment
 		roleAssignmentsRemoved, err := m.casbin.RemoveFilteredGroupingPolicy(1, prefixRoleName(roleName))
 		if err != nil {
 			return err
