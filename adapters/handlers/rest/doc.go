14a654c44973ffa64f0044bc22dc70880b400d4e
Marcin Antas <antas.marcin@gmail.com>
2024-12-04 22:10:58 +0100

    Update main version to v1.29.0-dev

--- adapters/handlers/rest/doc.go
+++ adapters/handlers/rest/doc.go
@@ -27,7 +27,7 @@
 //	  https
 //	Host: localhost
 //	BasePath: /v1
-//	Version: 1.28.0-rc.0
+//	Version: 1.29.0-dev
 //	Contact: Weaviate<hello@weaviate.io> https://github.com/weaviate
 //
 //	Consumes:
