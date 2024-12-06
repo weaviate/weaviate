9598ca760da447c27060f3472df002216b542677
Dirk Kulawiak <dirk@semi.technology>
2024-12-06 13:43:25 +0100

    Add authZ for gql group by reference

--- test/acceptance/authz/gql_refs_test.go
+++ test/acceptance/authz/gql_refs_test.go
@@ -9,7 +9,7 @@
 //  CONTACT: hello@weaviate.io
 //
 
-package gql
+package test
 
 import (
 	"context"
@@ -17,6 +17,9 @@ import (
 	"fmt"
 	"testing"
 	"time"
+
+	"github.com/pkg/errors"
+	"github.com/weaviate/weaviate/client/graphql"
 
 	"github.com/go-openapi/strfmt"
 	"github.com/stretchr/testify/require"
@@ -57,9 +60,6 @@ func TestAuthZGraphQLRefs(t *testing.T) {
 	articlesCls := articles.ArticlesClass()
 	paragraphsCls := articles.ParagraphsClass()
 
-	paragraph1UUID := strfmt.UUID("00000000-0000-0000-0000-000000000001")
-	paragraph2UUID := strfmt.UUID("00000000-0000-0000-0000-000000000002")
-
 	roleName := "can-query-articles"
 
 	t.Run("create classes", func(t *testing.T) {
@@ -69,14 +69,14 @@ func TestAuthZGraphQLRefs(t *testing.T) {
 
 	t.Run("import objects", func(t *testing.T) {
 		paragraphObjs := make([]*models.Object, 0)
-		paragraphObjs = append(paragraphObjs, articles.NewParagraph().WithID(paragraph1UUID).Object())
-		paragraphObjs = append(paragraphObjs, articles.NewParagraph().WithID(paragraph2UUID).Object())
+		paragraphObjs = append(paragraphObjs, articles.NewParagraph().WithID(UUID1).Object())
+		paragraphObjs = append(paragraphObjs, articles.NewParagraph().WithID(UUID2).Object())
 		helper.CreateObjectsBatchAuth(t, paragraphObjs, adminKey)
 
 		articleObjs := make([]*models.Object, 0)
 		articleObjs = append(articleObjs, articles.NewArticle().WithTitle("Article 1").WithReferences(
-			&models.SingleRef{Beacon: strfmt.URI("weaviate://localhost/" + paragraph1UUID.String())},
-			&models.SingleRef{Beacon: strfmt.URI("weaviate://localhost/" + paragraph2UUID.String())},
+			&models.SingleRef{Beacon: strfmt.URI("weaviate://localhost/" + UUID1.String())},
+			&models.SingleRef{Beacon: strfmt.URI("weaviate://localhost/" + UUID2.String())},
 		).Object())
 		articleObjs = append(articleObjs, articles.NewArticle().WithTitle("Article 2").Object())
 		helper.CreateObjectsBatchAuth(t, articleObjs, adminKey)
@@ -107,7 +107,7 @@ func TestAuthZGraphQLRefs(t *testing.T) {
 	})
 
 	t.Run("fail to query with Get for articles when filtering on paragraphs", func(t *testing.T) {
-		query := fmt.Sprintf(`{ Get { Article(where: {operator: Equal, path: ["hasParagraphs", "Paragraph", "_id"], valueText: "%s"}) { title } } }`, paragraph1UUID)
+		query := fmt.Sprintf(`{ Get { Article(where: {operator: Equal, path: ["hasParagraphs", "Paragraph", "_id"], valueText: "%s"}) { title } } }`, UUID1)
 		resp, err := queryGQL(t, query, customKey)
 		require.Nil(t, err)
 		require.Equal(t, 1, len(resp.Payload.Errors))
@@ -115,7 +115,7 @@ func TestAuthZGraphQLRefs(t *testing.T) {
 	})
 
 	t.Run("fail to query with Aggregate for articles when filtering on paragraphs", func(t *testing.T) {
-		query := fmt.Sprintf(`{ Aggregate { Article(where: {operator: Equal, path: ["hasParagraphs", "Paragraph", "_id"], valueText: "%s"}) { meta { count } } } }`, paragraph1UUID)
+		query := fmt.Sprintf(`{ Aggregate { Article(where: {operator: Equal, path: ["hasParagraphs", "Paragraph", "_id"], valueText: "%s"}) { meta { count } } } }`, UUID1)
 		resp, err := queryGQL(t, query, customKey)
 		require.Nil(t, err)
 		require.Equal(t, 1, len(resp.Payload.Errors))
@@ -142,7 +142,7 @@ func TestAuthZGraphQLRefs(t *testing.T) {
 	})
 
 	t.Run("successfully query for articles with Get when filtering on paragraphs returning paragraphs", func(t *testing.T) {
-		query := fmt.Sprintf(`{ Get { Article(where: {operator: Equal, path: ["hasParagraphs", "Paragraph", "_id"], valueText: "%s"}) { title hasParagraphs { ... on Paragraph { _additional { id } } } } } }`, paragraph1UUID)
+		query := fmt.Sprintf(`{ Get { Article(where: {operator: Equal, path: ["hasParagraphs", "Paragraph", "_id"], valueText: "%s"}) { title hasParagraphs { ... on Paragraph { _additional { id } } } } } }`, UUID1)
 		res := assertGQL(t, query, customKey)
 		data, ok := res.Data["Get"].(map[string]any)
 		require.True(t, ok)
@@ -153,7 +153,7 @@ func TestAuthZGraphQLRefs(t *testing.T) {
 	})
 
 	t.Run("successfully query for articles with Aggregate when filtering on paragraphs", func(t *testing.T) {
-		query := fmt.Sprintf(`{ Aggregate { Article(where: {operator: Equal, path: ["hasParagraphs", "Paragraph", "_id"], valueText: "%s"}) { meta { count } } } }`, paragraph1UUID)
+		query := fmt.Sprintf(`{ Aggregate { Article(where: {operator: Equal, path: ["hasParagraphs", "Paragraph", "_id"], valueText: "%s"}) { meta { count } } } }`, UUID1)
 		res := assertGQL(t, query, customKey)
 		data, ok := res.Data["Aggregate"].(map[string]any)
 		require.True(t, ok)
@@ -161,4 +161,114 @@ func TestAuthZGraphQLRefs(t *testing.T) {
 		require.True(t, ok)
 		require.Equal(t, json.Number("1"), dataL[0].(map[string]any)["meta"].(map[string]any)["count"])
 	})
+}
+
+func TestAuthZGraphQLRefsGroupBy(t *testing.T) {
+	adminUser := "admin-user"
+	adminKey := "admin-key"
+	adminAuth := helper.CreateAuth(adminKey)
+
+	customUser := "custom-user"
+	customKey := "custom-key"
+
+	_, down := composeUp(t, map[string]string{adminUser: adminKey}, map[string]string{customUser: customKey}, nil)
+	defer down()
+
+	articlesCls := articles.ArticlesClass()
+	paragraphsCls := articles.ParagraphsClass()
+
+	helper.DeleteClassWithAuthz(t, paragraphsCls.Class, adminAuth)
+	helper.DeleteClassWithAuthz(t, articlesCls.Class, adminAuth)
+	helper.CreateClassAuth(t, paragraphsCls, adminKey)
+	helper.CreateClassAuth(t, articlesCls, adminKey)
+	defer helper.DeleteClassWithAuthz(t, paragraphsCls.Class, adminAuth)
+	defer helper.DeleteClassWithAuthz(t, articlesCls.Class, adminAuth)
+
+	paragraphObjs := []*models.Object{articles.NewParagraph().WithID(UUID1).Object(), articles.NewParagraph().WithID(UUID2).Object()}
+	helper.CreateObjectsBatchAuth(t, paragraphObjs, adminKey)
+
+	articleObjs := make([]*models.Object, 0)
+	articleObjs = append(articleObjs, articles.NewArticle().WithTitle("Article 1").WithReferences(
+		&models.SingleRef{Beacon: strfmt.URI("weaviate://localhost/" + UUID1.String())},
+		&models.SingleRef{Beacon: strfmt.URI("weaviate://localhost/" + UUID2.String())},
+	).WithVector([]float32{1, 0}).Object())
+	articleObjs = append(articleObjs, articles.NewArticle().WithTitle("Article 2").WithVector([]float32{1, 0}).Object())
+	helper.CreateObjectsBatchAuth(t, articleObjs, adminKey)
+
+	roleName := t.Name() + "_role"
+
+	readCollectionsAction := authorization.ReadCollections
+	readDataAction := authorization.ReadData
+
+	all := "*"
+	requiredPermissions := []*models.Permission{
+		{
+			Action:      &readCollectionsAction,
+			Collections: &models.PermissionCollections{Collection: &all},
+		},
+		{
+			Action: &readDataAction,
+			Data:   &models.PermissionData{Collection: &paragraphsCls.Class},
+		},
+		{
+			Action: &readDataAction,
+			Data:   &models.PermissionData{Collection: &articlesCls.Class},
+		},
+	}
+
+	groupByQuery := `{
+				  Get {
+					Article(
+					  groupBy: {groups: 10, objectsPerGroup: 10, path: "hasParagraphs"}
+					  nearVector: {vector: [1, 0]}
+					) {
+					  _additional {
+						group {
+						  hits {
+							_additional {
+							  id
+							}
+							hasParagraphs {
+							  ... on Paragraph {
+								contents
+							  }
+							}
+						  }
+						}
+					  }
+					}
+				  }
+				}
+				`
+
+	t.Run("create and assign a role that can query for articles", func(t *testing.T) {
+		role := &models.Role{Name: &roleName, Permissions: requiredPermissions}
+		helper.DeleteRole(t, adminKey, *role.Name)
+		helper.CreateRole(t, adminKey, role)
+		helper.AssignRoleToUser(t, adminKey, roleName, customUser)
+		defer helper.DeleteRole(t, adminKey, *role.Name)
+
+		res, err := queryGQL(t, groupByQuery, customKey)
+		require.NoError(t, err)
+		require.NotNil(t, res)
+		require.Nil(t, res.Payload.Errors)
+		data, ok := res.Payload.Data["Get"].(map[string]any)
+		require.True(t, ok)
+		require.Contains(t, data, "Article")
+	})
+
+	for _, permissions := range generateMissingLists(requiredPermissions) {
+		t.Run("One permission is missing", func(t *testing.T) {
+			role := &models.Role{Name: &roleName, Permissions: permissions}
+			helper.DeleteRole(t, adminKey, *role.Name)
+			helper.CreateRole(t, adminKey, role)
+			helper.AssignRoleToUser(t, adminKey, roleName, customUser)
+			defer helper.DeleteRole(t, adminKey, *role.Name)
+			res, err := queryGQL(t, groupByQuery, customKey)
+			require.Error(t, err)
+			require.Nil(t, res)
+			var errForbidden *graphql.GraphqlPostForbidden
+			require.True(t, errors.As(err, &errForbidden))
+		})
+	}
 }
