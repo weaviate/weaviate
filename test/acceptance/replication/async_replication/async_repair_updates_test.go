84ee6a86110e8e4b5db680187d3822a2b0026e66
Jeronimo <jeronimo.irazabal@gmail.com>
2024-12-13 16:54:06 -0300

    test: increase deadline for obj propagation
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>
    
    test: increase deadline for async replication
    
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>
    
    test: simplify deletion scenario
    
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>

--- test/acceptance/replication/async_replication/async_repair_updates_test.go
+++ test/acceptance/replication/async_replication/async_repair_updates_test.go
@@ -117,7 +117,7 @@ func (suite *AsyncReplicationTestSuite) TestAsyncRepairObjectUpdateScenario() {
 					props := resp.Properties.(map[string]interface{})
 					props["contents"] = fmt.Sprintf("paragraph#%d_%d", itCount, i)
 				}
-			}, 10*time.Second, 500*time.Millisecond, "not all the objects have been asynchronously replicated")
+			}, 30*time.Second, 500*time.Millisecond, "not all the objects have been asynchronously replicated")
 		})
 	}
 }
