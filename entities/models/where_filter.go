452c7ac2607e12af0625350e0262105b94844b22
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-12-13 17:45:36 +0000

    run  ./tools/gen-code-from-swagger.sh

--- entities/models/where_filter.go
+++ entities/models/where_filter.go
@@ -37,7 +37,7 @@ type WhereFilter struct {
 
 	// operator to use
 	// Example: GreaterThanEqual
-	// Enum: [And Or Equal Like  NotLike NotEqual GreaterThan GreaterThanEqual LessThan LessThanEqual WithinGeoRange IsNull ContainsAny ContainsAll]
+	// Enum: [And Or Equal Like NotLike NotEqual GreaterThan GreaterThanEqual LessThan LessThanEqual WithinGeoRange IsNull ContainsAny ContainsAll]
 	Operator string `json:"operator,omitempty"`
 
 	// path to the property currently being filtered
@@ -148,7 +148,7 @@ var whereFilterTypeOperatorPropEnum []interface{}
 
 func init() {
 	var res []string
-	if err := json.Unmarshal([]byte(`["And","Or","Equal","Like","NotLike"NotEqual","GreaterThan","GreaterThanEqual","LessThan","LessThanEqual","WithinGeoRange","IsNull","ContainsAny","ContainsAll"]`), &res); err != nil {
+	if err := json.Unmarshal([]byte(`["And","Or","Equal","Like","NotLike","NotEqual","GreaterThan","GreaterThanEqual","LessThan","LessThanEqual","WithinGeoRange","IsNull","ContainsAny","ContainsAll"]`), &res); err != nil {
 		panic(err)
 	}
 	for _, v := range res {
