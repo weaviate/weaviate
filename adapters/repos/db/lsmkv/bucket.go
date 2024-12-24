48942900add985d2ca3474974e62bf43475f8f4e
Kaviraj <kavirajkanagaraj@gmail.com>
2024-12-24 11:20:55 +0100

    bug: When flush is failed increase retry interval
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

--- adapters/repos/db/lsmkv/bucket.go
+++ adapters/repos/db/lsmkv/bucket.go
@@ -1038,6 +1038,7 @@ func (b *Bucket) flushAndSwitchIfThresholdsMet(shouldAbort cyclemanager.ShouldAb
 				WithField("path", b.dir).
 				WithError(err).
 				Errorf("flush and switch failed")
+			return false
 		}
 
 		if b.memtableResizer != nil {
