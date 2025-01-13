94bc3e1bfab30ed36d37ef5a6d5ece22b30cc322
Loic Reyreaud <loic@weaviate.io>
2025-01-13 14:07:29 +0100

    fix renaming
    Signed-off-by: Loic Reyreaud <loic@weaviate.io>

--- usecases/schema/get_class.go
+++ usecases/schema/get_class.go
@@ -28,9 +28,9 @@ func NewClassGetter(schemaRetrievalStrategy config.SchemaRetrievalStrategy, sche
 	case config.LeaderOnly:
 		return newClassGetterLeaderOnly(schemaParser, schemaManager, logger), nil
 	case config.LocalOnly:
-		return newClassGetterLocalOnly(schemaParser, schemaManager, schemaReader, logger), nil
+		return newClassGetterLocalOnly(schemaParser, schemaReader, logger), nil
 	case config.LeaderOnMismatch:
-		return newClassGetterLeaderOnMismatch(schemaReader, logger), nil
+		return newClassGetterLeaderOnMismatch(schemaParser, schemaReader, schemaManager, logger), nil
 	default:
 		return nil, fmt.Errorf("unknown class getter method: %s", config.SchemaRetrievalStrategyToString[schemaRetrievalStrategy])
 	}
@@ -51,30 +51,32 @@ func newClassGetterLeaderOnly(parser *Parser, schemaManager SchemaManager, logge
 }
 
 type classGetterLocalOnly struct {
-	parser        *Parser
-	schemaManager SchemaManager
-	schemaReader  SchemaReader
-	logger        logrus.FieldLogger
-}
-
-func newClassGetterLocalOnly(parser *Parser, schemaManager SchemaManager, schemaReader SchemaReader, logger logrus.FieldLogger) *classGetterLocalOnly {
-	return &classGetterLocalOnly{
-		parser:        parser,
-		schemaManager: schemaManager,
-		schemaReader:  schemaReader,
-		logger:        logger,
-	}
-}
-
-type classGetterLeaderOnMismatch struct {
+	parser       *Parser
 	schemaReader SchemaReader
 	logger       logrus.FieldLogger
 }
 
-func newClassGetterLeaderOnMismatch(schemaReader SchemaReader, logger logrus.FieldLogger) *classGetterLeaderOnMismatch {
-	return &classGetterLeaderOnMismatch{
+func newClassGetterLocalOnly(parser *Parser, schemaReader SchemaReader, logger logrus.FieldLogger) *classGetterLocalOnly {
+	return &classGetterLocalOnly{
+		parser:       parser,
 		schemaReader: schemaReader,
 		logger:       logger,
+	}
+}
+
+type classGetterLeaderOnMismatch struct {
+	parser        *Parser
+	schemaReader  SchemaReader
+	schemaManager SchemaManager
+	logger        logrus.FieldLogger
+}
+
+func newClassGetterLeaderOnMismatch(parser *Parser, schemaReader SchemaReader, schemaManager SchemaManager, logger logrus.FieldLogger) *classGetterLeaderOnMismatch {
+	return &classGetterLeaderOnMismatch{
+		parser:        parser,
+		schemaReader:  schemaReader,
+		schemaManager: schemaManager,
+		logger:        logger,
 	}
 }
 
@@ -104,6 +106,36 @@ func (cg *classGetterLeaderOnly) getClasses(names []string) (map[string]versione
 }
 
 func (cg *classGetterLocalOnly) getClasses(names []string) (map[string]versioned.Class, error) {
+	vclasses := map[string]versioned.Class{}
+	for _, name := range names {
+		vc, err := cg.schemaReader.ReadOnlyVersionedClass(name)
+		if err != nil {
+			cg.logger.WithFields(logrus.Fields{
+				"Class": vc.Class.Class,
+				"Error": err,
+			}).Warn("parsing local class error")
+			continue
+		}
+		vclasses[name] = vc
+	}
+
+	// Check if we have all the classes from the local schema
+	if len(vclasses) < len(names) {
+		missingClasses := []string{}
+		for _, name := range names {
+			if _, ok := vclasses[name]; !ok {
+				missingClasses = append(missingClasses, name)
+			}
+		}
+		cg.logger.WithFields(logrus.Fields{
+			"missing":    missingClasses,
+			"suggestion": "This node received a data request for a class that is not present on the local schema on the node. If the class was just updated in the schema and you want to be able to query it immediately consider changing the " + config.SchemaRetrievalStrategyEnvVariable + " config to \"" + config.SchemaRetrievalStrategyToString[config.LeaderOnly] + "\".",
+		}).Warn("not all classes found locally")
+	}
+	return vclasses, nil
+}
+
+func (cg *classGetterLeaderOnMismatch) getClasses(names []string) (map[string]versioned.Class, error) {
 	classVersions, err := cg.schemaManager.QueryClassVersions(names...)
 	if err != nil {
 		return nil, err
@@ -152,33 +184,4 @@ func (cg *classGetterLocalOnly) getClasses(names []string) (map[string]versioned
 	}
 
 	return versionedClassesToReturn, nil
-}
-
-func (cg *classGetterLeaderOnMismatch) getClasses(names []string) (map[string]versioned.Class, error) {
-	vclasses := map[string]versioned.Class{}
-	for _, name := range names {
-		vc, err := cg.schemaReader.ReadOnlyVersionedClass(name)
-		if err != nil {
-			cg.logger.WithFields(logrus.Fields{
-				"Class": vc.Class.Class,
-				"Error": err,
-			}).Warn("parsing local class error")
-			continue
-		}
-		vclasses[name] = vc
-	}
-	if len(vclasses) < len(names) {
-		missingClasses := []string{}
-		for _, name := range names {
-			if _, ok := vclasses[name]; !ok {
-				missingClasses = append(missingClasses, name)
-			}
-		}
-		cg.logger.WithFields(logrus.Fields{
-			"missing": missingClasses,
-			"suggestion": "if this node is too slow to pick up new classes from the leader, " +
-				"consider changing the GET_CLASS_METHOD config to `always_leader`",
-		}).Warn("not all classes found locally")
-	}
-	return vclasses, nil
 }
