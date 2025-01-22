473a19d04cd44e3a55adbcc676c294678f653933
Marcin Antas <antas.marcin@gmail.com>
2025-01-22 09:00:05 +0100

    prepare release v1.27.10

--- adapters/handlers/rest/doc.go
+++ adapters/handlers/rest/doc.go
@@ -27,7 +27,7 @@
 //	  https
 //	Host: localhost
 //	BasePath: /v1
-//	Version: 1.27.9
+//	Version: 1.27.10
 //	Contact: Weaviate<hello@weaviate.io> https://github.com/weaviate
 //
 //	Consumes:
--- adapters/handlers/rest/embedded_spec.go
+++ adapters/handlers/rest/embedded_spec.go
@@ -48,7 +48,7 @@ func init() {
       "url": "https://github.com/weaviate",
       "email": "hello@weaviate.io"
     },
-    "version": "1.27.9"
+    "version": "1.27.10"
   },
   "basePath": "/v1",
   "paths": {
@@ -5575,7 +5575,7 @@ func init() {
       "url": "https://github.com/weaviate",
       "email": "hello@weaviate.io"
     },
-    "version": "1.27.9"
+    "version": "1.27.10"
   },
   "basePath": "/v1",
   "paths": {
