ee806ddecc976e21111f8b5096d1f15498b6583f
Dirk Kulawiak <dirk@semi.technology>
2025-01-30 17:25:50 +0100

    remove unneeded auth test

--- test/acceptance_with_python/rbac/test_roles.py
+++ test/acceptance_with_python/rbac/test_roles.py
@@ -8,30 +8,6 @@ from .conftest import _sanitize_role_name, role_wrapper, RoleWrapperProtocol
 pytestmark = pytest.mark.xdist_group(name="rbac")
 
 
-def test_rbac_viewer_assign(
-    request: SubRequest, admin_client, viewer_client, role_wrapper: RoleWrapperProtocol
-):
-    name = _sanitize_role_name(request.node.name)
-
-    admin_client.collections.delete(name)
-    admin_client.collections.create(name=name)
-
-    # cannot delete with viewer permissions
-    with pytest.raises(weaviate.exceptions.InsufficientPermissionsError) as e:
-        viewer_client.collections.delete(name)
-
-    # with extra role that has those permissions it works
-    with role_wrapper(
-        admin_client,
-        request,
-        Permissions.collections(collection=name, delete_collection=True),
-        "viewer-user",
-    ):
-        viewer_client.collections.delete(name)
-
-    admin_client.collections.delete(name)
-
-
 def test_rbac_with_regexp(
     request: SubRequest, admin_client, custom_client, role_wrapper: RoleWrapperProtocol
 ):
