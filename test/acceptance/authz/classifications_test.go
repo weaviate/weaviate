6c8cd60c4160a3c9459c32c1a589ab0afe0247ed
Tommy Smith <tommy@weaviate.io>
2025-01-30 13:31:58 +0000

    Split roles between post/get within test

--- test/acceptance/authz/classifications_test.go
+++ test/acceptance/authz/classifications_test.go
@@ -30,7 +30,9 @@ func TestAuthzClassification(t *testing.T) {
 
 	customUser := "custom-user"
 	customKey := "custom-key"
-	customRole := "custom"
+
+	postRole := "post"
+	getRole := "get"
 
 	_, down := composeUp(t, map[string]string{adminUser: adminKey}, map[string]string{customUser: customKey}, nil)
 	defer down()
@@ -81,14 +83,14 @@ func TestAuthzClassification(t *testing.T) {
 
 	t.Run("add the permission to update the collection", func(t *testing.T) {
 		_, err := helper.Client(t).Authz.CreateRole(authz.NewCreateRoleParams().WithBody(&models.Role{
-			Name: &customRole,
+			Name: &postRole,
 			Permissions: []*models.Permission{
 				helper.NewCollectionsPermission().WithAction(authorization.UpdateCollections).WithCollection(clsA.Class).Permission(),
 			},
 		}), helper.CreateAuth(adminKey))
 		require.NoError(t, err)
 		_, err = helper.Client(t).Authz.AssignRoleToUser(
-			authz.NewAssignRoleToUserParams().WithID(customUser).WithBody(authz.AssignRoleToUserBody{Roles: []string{customRole}}),
+			authz.NewAssignRoleToUserParams().WithID(customUser).WithBody(authz.AssignRoleToUserBody{Roles: []string{postRole}}),
 			helper.CreateAuth(adminKey),
 		)
 		require.NoError(t, err)
@@ -105,7 +107,7 @@ func TestAuthzClassification(t *testing.T) {
 
 	t.Run("add the permission to read data in the collection", func(t *testing.T) {
 		_, err := helper.Client(t).Authz.AddPermissions(
-			authz.NewAddPermissionsParams().WithID(customRole).WithBody(authz.AddPermissionsBody{
+			authz.NewAddPermissionsParams().WithID(postRole).WithBody(authz.AddPermissionsBody{
 				Permissions: []*models.Permission{
 					helper.NewDataPermission().WithAction(authorization.ReadData).WithCollection(clsA.Class).Permission(),
 				},
@@ -126,7 +128,7 @@ func TestAuthzClassification(t *testing.T) {
 
 	t.Run("add the permission to read the collection", func(t *testing.T) {
 		_, err := helper.Client(t).Authz.AddPermissions(
-			authz.NewAddPermissionsParams().WithID(customRole).WithBody(authz.AddPermissionsBody{
+			authz.NewAddPermissionsParams().WithID(postRole).WithBody(authz.AddPermissionsBody{
 				Permissions: []*models.Permission{
 					helper.NewCollectionsPermission().WithAction(authorization.ReadCollections).WithCollection(clsA.Class).Permission(),
 				},
@@ -147,7 +149,7 @@ func TestAuthzClassification(t *testing.T) {
 
 	t.Run("add the permission to read data in the reference collection", func(t *testing.T) {
 		_, err := helper.Client(t).Authz.AddPermissions(
-			authz.NewAddPermissionsParams().WithID(customRole).WithBody(authz.AddPermissionsBody{
+			authz.NewAddPermissionsParams().WithID(postRole).WithBody(authz.AddPermissionsBody{
 				Permissions: []*models.Permission{
 					helper.NewDataPermission().WithAction(authorization.ReadData).WithCollection(clsB.Class).Permission(),
 				},
@@ -168,7 +170,7 @@ func TestAuthzClassification(t *testing.T) {
 
 	t.Run("add the permission to read the reference collection", func(t *testing.T) {
 		_, err := helper.Client(t).Authz.AddPermissions(
-			authz.NewAddPermissionsParams().WithID(customRole).WithBody(authz.AddPermissionsBody{
+			authz.NewAddPermissionsParams().WithID(postRole).WithBody(authz.AddPermissionsBody{
 				Permissions: []*models.Permission{
 					helper.NewCollectionsPermission().WithAction(authorization.ReadCollections).WithCollection(clsB.Class).Permission(),
 				},
@@ -184,6 +186,14 @@ func TestAuthzClassification(t *testing.T) {
 		id = res.Payload.ID.String()
 	})
 
+	t.Run("revoke postRole from user", func(t *testing.T) {
+		_, err := helper.Client(t).Authz.RevokeRoleFromUser(
+			authz.NewRevokeRoleFromUserParams().WithID(customUser).WithBody(authz.RevokeRoleFromUserBody{Roles: []string{postRole}}),
+			helper.CreateAuth(adminKey),
+		)
+		require.NoError(t, err)
+	})
+
 	t.Run("fail to get a classification without the correct permissions", func(t *testing.T) {
 		_, err := get(id)
 		require.Error(t, err)
@@ -193,12 +203,18 @@ func TestAuthzClassification(t *testing.T) {
 	})
 
 	t.Run("add the permission to read the collection", func(t *testing.T) {
-		_, err := helper.Client(t).Authz.AddPermissions(
-			authz.NewAddPermissionsParams().WithID(customRole).WithBody(authz.AddPermissionsBody{
+		_, err := helper.Client(t).Authz.CreateRole(
+			authz.NewCreateRoleParams().WithBody(&models.Role{
+				Name: &getRole,
 				Permissions: []*models.Permission{
 					helper.NewCollectionsPermission().WithAction(authorization.ReadCollections).WithCollection(clsA.Class).Permission(),
 				},
 			}),
+			helper.CreateAuth(adminKey),
+		)
+		require.NoError(t, err)
+		_, err = helper.Client(t).Authz.AssignRoleToUser(
+			authz.NewAssignRoleToUserParams().WithID(customUser).WithBody(authz.AssignRoleToUserBody{Roles: []string{getRole}}),
 			helper.CreateAuth(adminKey),
 		)
 		require.NoError(t, err)
