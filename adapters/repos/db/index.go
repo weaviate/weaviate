b8c7ad0110a2779e9fa39e2bf2d6211b9aa97350
Jeremy Price <donomii@gmail.com>
2025-01-31 15:29:08 +0900

    Revert "Only scope shardName"
    This reverts commit ae442bbaff097841e6d9f04f2a4559f48fe2d426.

--- adapters/repos/db/index.go
+++ adapters/repos/db/index.go
@@ -1686,7 +1686,11 @@ func (i *Index) objectVectorSearch(ctx context.Context, searchVectors []models.V
 		}
 
 		if shard != nil {
-			func( shardName string) {
+			func(ctx context.Context, searchVectors []models.Vector,
+				targetVectors []string, dist float32, limit int, filters *filters.LocalFilter,
+				sort []filters.Sort, groupBy *searchparams.GroupBy, additional additional.Properties,
+				shard ShardLike, targetCombination *dto.TargetCombination, properties []string, shardName string,
+			) {
 				eg.Go(func() error {
 					defer release()
 
@@ -1702,11 +1706,15 @@ func (i *Index) objectVectorSearch(ctx context.Context, searchVectors []models.V
 					m.Unlock()
 					return nil
 				})
-			}(shardName)
+			}(ctx, searchVectors, targetVectors, dist, limit, localFilters, sort, groupBy, additionalProps, shard, targetCombination, properties, shardName)
 		}
 
 		if shard == nil || i.Config.ForceFullReplicasSearch {
-			func( shardName string) {
+			func(ctx context.Context, searchVectors []models.Vector,
+				targetVectors []string, dist float32, limit int, filters *filters.LocalFilter,
+				sort []filters.Sort, groupBy *searchparams.GroupBy, additional additional.Properties,
+				shard ShardLike, targetCombination *dto.TargetCombination, properties []string, shardName string,
+			) {
 				eg.Go(func() error {
 					// If we have no local shard or if we force the query to reach all replicas
 					remoteShardObject, remoteShardScores, err := i.remoteShardSearch(ctx, searchVectors, targetVectors, dist, limit, localFilters, sort, groupBy, additionalProps, shard, targetCombination, properties, shardName)
@@ -1720,7 +1728,7 @@ func (i *Index) objectVectorSearch(ctx context.Context, searchVectors []models.V
 					m.Unlock()
 					return nil
 				})
-			}(shardName)
+			}(ctx, searchVectors, targetVectors, dist, limit, localFilters, sort, groupBy, additionalProps, shard, targetCombination, properties, shardName)
 		}
 	}
 
