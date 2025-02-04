256985074e645509846fcd494b88192d2e243388
moogacs <contact@m-mooga.com>
2025-02-04 13:59:16 +0100

    run call backs in 1 go routine and use _NUMCPU as var

--- cluster/schema/manager.go
+++ cluster/schema/manager.go
@@ -365,7 +365,7 @@ func (s *SchemaManager) apply(op applyOp) error {
 	}
 
 	if op.enableSchemaCallback {
-		// TriggerSchemaUpdateCallbacks it concurrent and at
+		// TriggerSchemaUpdateCallbacks is concurrent and at
 		// this point of time schema shall be up to date.
 		s.db.TriggerSchemaUpdateCallbacks()
 	}
