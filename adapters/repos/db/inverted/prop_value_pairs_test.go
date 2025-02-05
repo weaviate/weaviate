a0905db87b6a9b8defd3686a1c644e0707233bf7
Andrzej Liszka <andrzej.liszka@gmail.com>
2025-02-05 12:42:03 +0100

    fix: close allowlist after using; pv:mergeDocIds if single child

--- adapters/repos/db/inverted/prop_value_pairs_test.go
+++ adapters/repos/db/inverted/prop_value_pairs_test.go
@@ -22,67 +22,67 @@ import (
 )
 
 func TestPropValuePairs_Merging(t *testing.T) {
+	type testCase struct {
+		name string
+
+		bitmaps  []*sroar.Bitmap
+		operator filters.Operator
+
+		expectedIds []uint64
+	}
+
+	testCases := []testCase{
+		{
+			name: "AND; different sets",
+
+			bitmaps: []*sroar.Bitmap{
+				roaringset.NewBitmap(7, 8, 9, 10, 11),
+				roaringset.NewBitmap(1, 3, 5, 7, 9, 11),
+				roaringset.NewBitmap(1, 3, 5, 7, 9),
+			},
+			operator: filters.OperatorAnd,
+
+			expectedIds: []uint64{7, 9},
+		},
+		{
+			name: "OR; different sets",
+
+			bitmaps: []*sroar.Bitmap{
+				roaringset.NewBitmap(7, 8, 9, 10, 11),
+				roaringset.NewBitmap(1, 3, 5, 7, 9, 11),
+				roaringset.NewBitmap(1, 3, 5, 7, 9),
+			},
+			operator: filters.OperatorOr,
+
+			expectedIds: []uint64{1, 3, 5, 7, 8, 9, 10, 11},
+		},
+		{
+			name: "AND; same sets",
+
+			bitmaps: []*sroar.Bitmap{
+				roaringset.NewBitmap(7, 8, 9, 10, 11),
+				roaringset.NewBitmap(7, 8, 9, 10, 11),
+				roaringset.NewBitmap(7, 8, 9, 10, 11),
+			},
+			operator: filters.OperatorAnd,
+
+			expectedIds: []uint64{7, 8, 9, 10, 11},
+		},
+		{
+			name: "OR; same sets",
+
+			bitmaps: []*sroar.Bitmap{
+				roaringset.NewBitmap(7, 8, 9, 10, 11),
+				roaringset.NewBitmap(7, 8, 9, 10, 11),
+				roaringset.NewBitmap(7, 8, 9, 10, 11),
+			},
+			operator: filters.OperatorOr,
+
+			expectedIds: []uint64{7, 8, 9, 10, 11},
+		},
+	}
+
 	t.Run("reuses one of underlying bitmaps", func(t *testing.T) {
-		type testCase struct {
-			name string
-
-			bitmaps  []*sroar.Bitmap
-			operator filters.Operator
-
-			expectedIds []uint64
-		}
-
-		testCases := []testCase{
-			{
-				name: "AND; different sets",
-
-				bitmaps: []*sroar.Bitmap{
-					roaringset.NewBitmap(7, 8, 9, 10, 11),
-					roaringset.NewBitmap(1, 3, 5, 7, 9, 11),
-					roaringset.NewBitmap(1, 3, 5, 7, 9),
-				},
-				operator: filters.OperatorAnd,
-
-				expectedIds: []uint64{7, 9},
-			},
-			{
-				name: "OR; different sets",
-
-				bitmaps: []*sroar.Bitmap{
-					roaringset.NewBitmap(7, 8, 9, 10, 11),
-					roaringset.NewBitmap(1, 3, 5, 7, 9, 11),
-					roaringset.NewBitmap(1, 3, 5, 7, 9),
-				},
-				operator: filters.OperatorOr,
-
-				expectedIds: []uint64{1, 3, 5, 7, 8, 9, 10, 11},
-			},
-			{
-				name: "AND; same sets",
-
-				bitmaps: []*sroar.Bitmap{
-					roaringset.NewBitmap(7, 8, 9, 10, 11),
-					roaringset.NewBitmap(7, 8, 9, 10, 11),
-					roaringset.NewBitmap(7, 8, 9, 10, 11),
-				},
-				operator: filters.OperatorAnd,
-
-				expectedIds: []uint64{7, 8, 9, 10, 11},
-			},
-			{
-				name: "OR; same sets",
-
-				bitmaps: []*sroar.Bitmap{
-					roaringset.NewBitmap(7, 8, 9, 10, 11),
-					roaringset.NewBitmap(7, 8, 9, 10, 11),
-					roaringset.NewBitmap(7, 8, 9, 10, 11),
-				},
-				operator: filters.OperatorOr,
-
-				expectedIds: []uint64{7, 8, 9, 10, 11},
-			},
-		}
-
 		for _, tc := range testCases {
 			t.Run(tc.name, func(t *testing.T) {
 				pv := &propValuePair{
@@ -111,6 +111,44 @@ func TestPropValuePairs_Merging(t *testing.T) {
 					}
 				}
 				assert.Equal(t, 1, sameCounter)
+			})
+		}
+	})
+
+	t.Run("merges multilevel pairs with single child", func(t *testing.T) {
+		for _, tc := range testCases {
+			t.Run(tc.name, func(t *testing.T) {
+				operatorOpposite := filters.OperatorAnd
+				if tc.operator == filters.OperatorAnd {
+					operatorOpposite = filters.OperatorOr
+				}
+
+				pv3 := &propValuePair{
+					operator: tc.operator,
+					children: make([]*propValuePair, len(tc.bitmaps)),
+				}
+				for i := range tc.bitmaps {
+					pv3.children[i] = &propValuePair{
+						operator: filters.OperatorEqual,
+						docIDs: docBitmap{
+							docIDs:  tc.bitmaps[i],
+							release: noopRelease,
+						},
+					}
+				}
+				pv2 := &propValuePair{
+					operator: tc.operator,
+					children: []*propValuePair{pv3},
+				}
+				pv1 := &propValuePair{
+					operator: operatorOpposite,
+					children: []*propValuePair{pv2},
+				}
+
+				dbm, err := pv1.mergeDocIDs()
+
+				require.Nil(t, err)
+				assert.ElementsMatch(t, tc.expectedIds, dbm.IDs())
 			})
 		}
 	})
