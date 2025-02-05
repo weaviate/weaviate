7766cf5e0c91a9c720a501bbc0c5f4879d7aac48
moogacs <contact@m-mooga.com>
2025-02-05 12:53:45 +0100

    fix mocks patch 1

--- usecases/auth/authorization/mocks/AuthorizerMock.go
+++ usecases/auth/authorization/mocks/AuthorizerMock.go
@@ -13,6 +13,7 @@ package mocks
 
 import (
 	mock "github.com/stretchr/testify/mock"
+
 	models "github.com/weaviate/weaviate/entities/models"
 )
 
@@ -46,13 +47,37 @@ func (_m *Authorizer) Authorize(principal *models.Principal, verb string, resour
 	return r0
 }
 
+// AuthorizeSilent provides a mock function with given fields: principal, verb, resources
+func (_m *Authorizer) AuthorizeSilent(principal *models.Principal, verb string, resources ...string) error {
+	_va := make([]interface{}, len(resources))
+	for _i := range resources {
+		_va[_i] = resources[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, principal, verb)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	if len(ret) == 0 {
+		panic("no return value specified for AuthorizeSilent")
+	}
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(*models.Principal, string, ...string) error); ok {
+		r0 = rf(principal, verb, resources...)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // NewAuthorizer creates a new instance of Authorizer. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
 // The first argument is typically a *testing.T value.
 func NewAuthorizer(t interface {
 	mock.TestingT
 	Cleanup(func())
-},
-) *Authorizer {
+}) *Authorizer {
 	mock := &Authorizer{}
 	mock.Mock.Test(t)
 
