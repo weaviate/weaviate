0887992831ade16443d825d729c04d6c5295fbe2
Kaviraj <kavirajkanagaraj@gmail.com>
2025-02-10 10:37:28 +0100

    add small doc on `replaceClasses`
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

--- cluster/schema/schema.go
+++ cluster/schema/schema.go
@@ -339,6 +339,8 @@ func (s *schema) deleteClass(name string) bool {
 	return true
 }
 
+// replaceClasses replaces the exising `schema.Classes` with given `classes`
+// mainly used in cases like restoring the whole schema from backup or something.
 func (s *schema) replaceClasses(classes map[string]*metaClass) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
