659a696b32814cbc88e9de8fbdcb38aec6cd527a
Kaviraj <kavirajkanagaraj@gmail.com>
2025-02-12 14:59:05 +0100

    add small godoc
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

--- adapters/repos/db/lsmkv/bucket.go
+++ adapters/repos/db/lsmkv/bucket.go
@@ -1024,6 +1024,7 @@ func (b *Bucket) Shutdown(ctx context.Context) error {
 	}
 }
 
+// flushAndSwitchIfThresholdsMet is part of flush callbacks of the bucket.
 func (b *Bucket) flushAndSwitchIfThresholdsMet(shouldAbort cyclemanager.ShouldAbortCallback) bool {
 	b.flushLock.RLock()
 	commitLogSize := b.active.commitlog.Size()
