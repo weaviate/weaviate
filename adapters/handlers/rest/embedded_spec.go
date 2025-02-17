2c2f50c083578fa31e4a922f363856f4387c8710
Marcin Antas <antas.marcin@gmail.com>
2025-02-17 15:07:52 +0100

    Revert "Merge pull request #7279 from weaviate/build-prepare-release-v1.28.6"
    This reverts commit cfaa18d4096e87dfca69db601c3853284876dbc0, reversing
    changes made to 497779c71bca1a53d198a9e810deefaa12cb5a72.

--- adapters/handlers/rest/embedded_spec.go
+++ adapters/handlers/rest/embedded_spec.go
@@ -48,7 +48,7 @@ func init() {
       "url": "https://github.com/weaviate",
       "email": "hello@weaviate.io"
     },
-    "version": "1.28.6"
+    "version": "1.28.5"
   },
   "basePath": "/v1",
   "paths": {
@@ -6684,7 +6684,7 @@ func init() {
       "url": "https://github.com/weaviate",
       "email": "hello@weaviate.io"
     },
-    "version": "1.28.6"
+    "version": "1.28.5"
   },
   "basePath": "/v1",
   "paths": {
