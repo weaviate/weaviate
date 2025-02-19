e39a281a4b92dac6bde3285c696cdb877d43ec1a
André Mourão <andre.mourao@weaviate.io>
2025-02-19 21:34:28 +0000

    feat(bm25_block): :sparkles: Use proper multi prop IDF to match BM25F expectation

--- adapters/repos/db/inverted/bm25_searcher_block.go
+++ adapters/repos/db/inverted/bm25_searcher_block.go
@@ -31,7 +31,7 @@ import (
 
 // var metrics = lsmkv.BlockMetrics{}
 
-func (b *BM25Searcher) createBlockTerm(N float64, filterDocIds helpers.AllowList, query []string, propName string, propertyBoost float32, duplicateTextBoosts []int, averagePropLength float64, config schema.BM25Config, ctx context.Context) ([][]*lsmkv.SegmentBlockMax, func(), error) {
+func (b *BM25Searcher) createBlockTerm(N float64, filterDocIds helpers.AllowList, query []string, propName string, propertyBoost float32, duplicateTextBoosts []int, averagePropLength float64, config schema.BM25Config, ctx context.Context) ([][]*lsmkv.SegmentBlockMax, []uint64, func(), error) {
 	bucket := b.store.Bucket(helpers.BucketSearchableFromPropNameLSM(propName))
 	return bucket.CreateDiskTerm(N, filterDocIds, query, propName, propertyBoost, duplicateTextBoosts, averagePropLength, config, ctx)
 }
@@ -67,9 +67,11 @@ func (b *BM25Searcher) wandBlock(
 	for _, tokenization := range helpers.Tokenizations {
 		propNames := propNamesByTokenization[tokenization]
 		if len(propNames) > 0 {
+			lenAllResults := len(allResults)
 			queryTerms, duplicateBoosts := queryTermsByTokenization[tokenization], duplicateBoostsByTokenization[tokenization]
+			globalIdfCounts := make([]uint64, len(queryTerms))
 			for _, propName := range propNames {
-				results, release, err := b.createBlockTerm(N, filterDocIds, queryTerms, propName, propertyBoosts[propName], duplicateBoosts, averagePropLength, b.config, ctx)
+				results, idfCounts, release, err := b.createBlockTerm(N, filterDocIds, queryTerms, propName, propertyBoosts[propName], duplicateBoosts, averagePropLength, b.config, ctx)
 				if err != nil {
 					return nil, nil, err
 				}
@@ -80,6 +82,29 @@ func (b *BM25Searcher) wandBlock(
 
 				allResults = append(allResults, results)
 				termCounts = append(termCounts, queryTerms)
+				for i := range queryTerms {
+					globalIdfCounts[i] += idfCounts[i]
+				}
+			}
+			globalIdfs := make([]float64, len(queryTerms))
+			for i := range globalIdfCounts {
+				n := globalIdfCounts[i]
+				globalIdfs[i] = math.Log(float64(1)+(N-float64(n)+0.5)/(float64(n)+0.5)) * float64(duplicateBoosts[i])
+			}
+			for _, result := range allResults[lenAllResults:] {
+				if len(result) == 0 {
+					continue
+				}
+				for j := range result {
+					if len(result[j]) == 0 {
+						continue
+					}
+					for k := range result[j] {
+						if result[j][k] != nil {
+							result[j][k].SetIdf(globalIdfs[k])
+						}
+					}
+				}
 			}
 
 		}
