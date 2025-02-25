876941964217f865a2650e945c8b1e526554d1b0
Jeremy Price <donomii@gmail.com>
2025-02-25 23:08:35 +0900

    format

--- adapters/repos/db/helpers/tokenizer.go
+++ adapters/repos/db/helpers/tokenizer.go
@@ -217,14 +217,11 @@ func tokenizeGSE(in string) []string {
 
 	terms = removeEmptyStrings(terms)
 
-
-
 	ret := append(terms, alpha...)
 	monitoring.GetMetrics().TokenizerDuration.WithLabelValues("gse").Observe(float64(time.Since(startTime).Microseconds()))
 	monitoring.GetMetrics().TokenCount.WithLabelValues("gse").Add(float64(len(ret)))
 	monitoring.GetMetrics().TokenCountPerRequest.WithLabelValues("gse").Observe(float64(len(ret)))
 	return ret
-
 }
 
 type KagomeTokenizers struct {
