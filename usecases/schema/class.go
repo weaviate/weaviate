a2b38e2026fc6f9bb0a2245b959536ba257b161d
moogacs <contact@m-mooga.com>
2025-02-28 12:41:40 +0100

    typo: splling

--- usecases/schema/class.go
+++ usecases/schema/class.go
@@ -145,12 +145,12 @@ func (h *Handler) AddClass(ctx context.Context, principal *models.Principal,
 		return nil, 0, err
 	}
 
-	existedCollectionsCount, err := h.schemaManager.QueryCollectionsCount()
+	existingCollectionsCount, err := h.schemaManager.QueryCollectionsCount()
 	if err != nil {
 		h.logger.WithField("error", err).Error("could not query the collections count")
 	}
 
-	if h.config.MaximumAllowedCollectionsCount != -1 && existedCollectionsCount >= h.config.MaximumAllowedCollectionsCount {
+	if h.config.MaximumAllowedCollectionsCount != -1 && existingCollectionsCount >= h.config.MaximumAllowedCollectionsCount {
 		return nil, 0, fmt.Errorf(
 			"cannot create collection: maximum number of collections (%d) reached - "+
 				"please consider switching to multi-tenancy or increasing the collection count limit - "+
