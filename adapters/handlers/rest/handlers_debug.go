9ad0a780bbf5a83a20847d9159a741ab0d82190b
André Mourão <andre.mourao@weaviate.io>
2025-03-14 11:05:44 +0000

    feat(bm25_block): :stethoscope: Debug endpoint to show progress on migration

--- adapters/handlers/rest/handlers_debug.go
+++ adapters/handlers/rest/handlers_debug.go
@@ -14,6 +14,7 @@ package rest
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 	"strings"
@@ -71,6 +72,214 @@ func setupDebugHandlers(appState *state.State) {
 	http.HandleFunc("/debug/index/rebuild/inverted/abort", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		appState.ReindexCtxCancel()
 		w.WriteHeader(http.StatusAccepted)
+	}))
+
+	http.HandleFunc("/debug/index/rebuild/inverted/status", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		colName := r.URL.Query().Get("collection")
+		rootPath := appState.DB.GetConfig().RootPath
+
+		if colName == "" {
+			http.Error(w, "collection and shard are required", http.StatusBadRequest)
+			return
+		}
+		className := schema.ClassName(colName)
+
+		classNameString := strings.ToLower(className.String())
+		idx := appState.DB.GetIndex(className)
+		if idx == nil {
+			logger.WithField("collection", colName).Error("collection not found or not ready")
+			http.Error(w, "collection not found or not ready", http.StatusNotFound)
+			return
+		}
+
+		// get all shards
+
+		paths := []string{}
+		err := idx.ForEachLoadedShard(
+			func(shardName string, shard db.ShardLike) error {
+				shardPath := rootPath + "/" + classNameString + "/" + shardName + "/lsm/"
+				paths = append(paths, shardPath)
+				return nil
+			},
+		)
+		if err != nil {
+			logger.WithField("collection", colName).Error("failed to get shard names")
+			http.Error(w, "failed to get shard names", http.StatusInternalServerError)
+			return
+		}
+
+		if len(paths) == 0 {
+			http.Error(w, "no shards found", http.StatusNotFound)
+			return
+		}
+
+		output := make([]map[string]string, len(paths))
+
+		for i, path := range paths {
+			output[i] = map[string]string{
+				"shard":  path,
+				"status": "unknown",
+			}
+			// check if a .migrations/searchable_map_to_blockmax exists
+			_, err := os.Stat(path + ".migrations/searchable_map_to_blockmax")
+			if err != nil {
+				output[i]["status"] = "not_started"
+				output[i]["message"] = "no searchable_map_to_blockmax found"
+				continue
+			}
+
+			path += ".migrations/searchable_map_to_blockmax/"
+			// check if started.mig exists
+			_, err = os.Stat(path + "started.mig")
+			if err != nil {
+				output[i]["status"] = "not_started"
+				output[i]["message"] = "no started.mig found"
+				continue
+			}
+			// load the started.mig file and add it's value to the output
+			startedFile, err := os.ReadFile(path + "started.mig")
+			if err != nil {
+				output[i]["status"] = "error"
+				output[i]["message"] = "failed to read started.mig"
+				continue
+			}
+			output[i]["status"] = "started"
+			output[i]["start_time"] = string(startedFile)
+
+			// check if the properties.mig file exists
+			_, err = os.Stat(path + "properties.mig")
+			if err != nil {
+				output[i]["message"] = "computing properties to reindex"
+				continue
+			}
+
+			// load the properties.mig file and add it's value to the output
+			propertiesFile, err := os.ReadFile(path + "properties.mig")
+			if err != nil {
+				output[i]["status"] = "error"
+				output[i]["message"] = "failed to read properties.mig"
+				continue
+			}
+
+			output[i]["properties"] = string(propertiesFile)
+
+			// count the progress.mig.* files in the shard directory
+			files, err := os.ReadDir(path)
+			if err != nil {
+				output[i]["status"] = "error"
+				output[i]["message"] = "failed to read shard directory"
+				continue
+			}
+			progressCount := 0
+
+			// files sorted by filename
+
+			for _, file := range files {
+				if strings.HasPrefix(file.Name(), "progress.mig.") {
+					progressCount++
+				}
+			}
+			if progressCount == 0 {
+				output[i]["message"] = "no progress.mig.* files found, no snapshots created yet"
+				continue
+			}
+
+			// load latest progress.mig.* file and add it's value to the output
+			progressFile, err := os.ReadFile(path + fmt.Sprintf("progress.mig.%09d", progressCount))
+			if err != nil {
+				output[i]["status"] = "error"
+				output[i]["message"] = fmt.Sprintf("failed to read progress.mig.%09d", progressCount)
+				continue
+			}
+
+			output[i]["latest_snapshot"] = strings.Join(strings.Split(string(progressFile), "\n"), ", ")
+
+			output[i]["status"] = "in progress"
+			output[i]["snapshot_count"] = fmt.Sprintf("%d", progressCount)
+
+			// check if reindexed.mig exists
+			_, err = os.Stat(path + "reindexed.mig")
+			if err == nil {
+				output[i]["status"] = "reindexed"
+				output[i]["message"] = "reindexing done"
+			}
+
+			// load the reindexed.mig file and add it's value to the output
+			reindexedFile, err := os.ReadFile(path + "reindexed.mig")
+			if err != nil {
+				output[i]["status"] = "error"
+				output[i]["message"] = "failed to read reindexed.mig"
+				continue
+			}
+			output[i]["reindexed"] = strings.Join(strings.Split(string(reindexedFile), "\n"), ", ")
+
+			// check if merged.mig exists
+			_, err = os.Stat(path + "merged.mig")
+			if err != nil {
+				output[i]["message"] = "reindexing done, merging buckets"
+				continue
+			}
+
+			output[i]["status"] = "merged"
+			output[i]["message"] = "merged reindex and ingest buckets"
+
+			// load the merged.mig file and add it's value to the output
+			mergedFile, err := os.ReadFile(path + "merged.mig")
+			if err != nil {
+				output[i]["status"] = "error"
+				output[i]["message"] = "failed to read merged.mig"
+				continue
+			}
+			output[i]["merged"] = string(mergedFile)
+
+			// check if swapped.mig.* exists
+			swappedFiles, err := os.ReadDir(path)
+			if err != nil {
+				output[i]["status"] = "error"
+				output[i]["message"] = "failed to read shard directory"
+				continue
+			}
+			swappedCount := 0
+			for _, file := range swappedFiles {
+				if strings.HasPrefix(file.Name(), "swapped.mig.") {
+					swappedCount++
+				}
+			}
+
+			if swappedCount > 0 {
+				output[i]["status"] = "swapped"
+				output[i]["message"] = fmt.Sprintf("swapped %d files", swappedCount)
+			}
+
+			// check if swapped.mig exists
+			_, err = os.Stat(path + "swapped.mig")
+			if err != nil {
+				output[i]["message"] = "reindexing done, buckets not swapped yet"
+				continue
+			}
+
+			output[i]["status"] = "swapped"
+			output[i]["message"] = "swapped buckets"
+
+			// load the swapped.mig file and add it's value to the output
+			swappedFile, err := os.ReadFile(path + "swapped.mig")
+			if err != nil {
+				output[i]["status"] = "error"
+				output[i]["message"] = "failed to read swapped.mig"
+				continue
+			}
+			output[i]["swapped"] = string(swappedFile)
+			output[i]["message"] = "reindexing done"
+			output[i]["status"] = "done"
+
+		}
+
+		response := map[string]interface{}{
+			"shards": output,
+		}
+		json.NewEncoder(w).Encode(response)
+
+		w.WriteHeader(http.StatusOK)
 	}))
 
 	// newLogLevel can be one of: panic, fatal, error, warn, info, debug, trace (defaults to info)
