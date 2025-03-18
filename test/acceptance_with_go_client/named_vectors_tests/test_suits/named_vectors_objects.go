d5d07213cd9ff65f3a08b544f3b270a139302009
Faustas Butkus <faustas.butkus@gmail.com>
2025-03-18 16:56:56 +0200

    swagger gen

--- test/acceptance_with_go_client/named_vectors_tests/test_suits/named_vectors_objects.go
+++ test/acceptance_with_go_client/named_vectors_tests/test_suits/named_vectors_objects.go
@@ -16,6 +16,7 @@ import (
 	"testing"
 
 	acceptance_with_go_client "acceptance_tests_with_client"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	wvt "github.com/weaviate/weaviate-go-client/v4/weaviate"
