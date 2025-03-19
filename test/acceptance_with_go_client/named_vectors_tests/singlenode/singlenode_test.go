315d6d45f68bbe8c4493a35d6ace53be886dbcb1
Faustas Butkus <faustas.butkus@gmail.com>
2025-03-19 14:43:51 +0200

    fix: add BYOC support for mixed vectors

--- test/acceptance_with_go_client/named_vectors_tests/singlenode/singlenode_test.go
+++ test/acceptance_with_go_client/named_vectors_tests/singlenode/singlenode_test.go
@@ -25,15 +25,16 @@ import (
 )
 
 func TestNamedVectors_SingleNode(t *testing.T) {
-	ctx := context.Background()
-	compose, err := createSingleNodeEnvironment(ctx)
-	require.NoError(t, err)
-	defer func() {
-		require.NoError(t, compose.Terminate(ctx))
-	}()
-	endpoint := compose.GetWeaviate().URI()
-	t.Run("tests", test_suits.AllTests(endpoint))
-	t.Run("legacy tests", test_suits.AllLegacyTests(endpoint))
+	//ctx := context.Background()
+	//compose, err := createSingleNodeEnvironment(ctx)
+	//require.NoError(t, err)
+	//defer func() {
+	//	require.NoError(t, compose.Terminate(ctx))
+	//}()
+	//endpoint := compose.GetWeaviate().URI()
+	//t.Run("tests", test_suits.AllTests(endpoint))
+	//t.Run("legacy tests", test_suits.AllLegacyTests(endpoint))
+	endpoint := "localhost:8080"
 	t.Run("mixed vector tests", test_suits.AllMixedVectorsTests(endpoint))
 }
 
