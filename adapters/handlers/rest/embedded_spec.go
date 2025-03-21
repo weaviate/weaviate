2a887b375c01e024ca24a34a20b129e10da2f37f
Marcin Antas <antas.marcin@gmail.com>
2025-03-21 15:42:30 +0100

    chore: update version on main to v1.31.0-dev

--- adapters/handlers/rest/embedded_spec.go
+++ adapters/handlers/rest/embedded_spec.go
@@ -48,7 +48,7 @@ func init() {
       "url": "https://github.com/weaviate",
       "email": "hello@weaviate.io"
     },
-    "version": "1.30.0-rc.0"
+    "version": "1.31.0-dev"
   },
   "basePath": "/v1",
   "paths": {
@@ -7284,7 +7284,7 @@ func init() {
       "url": "https://github.com/weaviate",
       "email": "hello@weaviate.io"
     },
-    "version": "1.30.0-rc.0"
+    "version": "1.31.0-dev"
   },
   "basePath": "/v1",
   "paths": {
