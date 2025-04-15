81b84bf5c54970b07cd699ad641d78f729ae3d47
Roberto Esposito <robyesposito2000@gmail.com>
2025-04-15 19:22:50 +0200

    minor change

--- entities/vectorindex/hnsw/multivector_config.go
+++ entities/vectorindex/hnsw/multivector_config.go
@@ -33,7 +33,7 @@ const (
 // Multivector configuration
 type MultivectorConfig struct {
 	Enabled      bool         `json:"enabled"`
-	MuveraConfig MuveraConfig `json:"muvera"`
+	MuveraConfig MuveraConfig `json:"muveraConfig"`
 	Aggregation  string       `json:"aggregation"`
 }
 
@@ -88,7 +88,7 @@ func parseMultivectorMap(in map[string]interface{}, multivector *MultivectorConf
 		return err
 	}
 
-	muveraValue, ok := multivectorConfigMap["muvera"]
+	muveraValue, ok := multivectorConfigMap["muveraConfig"]
 	if !ok {
 		return nil
 	}
