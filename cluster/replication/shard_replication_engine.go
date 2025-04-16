b837d53e532b9797a3619e7805cc4592aa016a61
salvatorecampagna <salvatore@weaviate.io>
2025-04-16 10:13:21 +0200

    nit: improve logging

--- cluster/replication/shard_replication_engine.go
+++ cluster/replication/shard_replication_engine.go
@@ -709,6 +709,7 @@ func (c *CopyOpConsumer) processReplicationOp(ctx context.Context, workerId uint
 	eg.Go(func() error {
 		return backoff.Retry(func() error {
 			if ctx.Err() != nil {
+				logger.WithError(ctx.Err()).Error("permanent error while processing replication operation, shutting down")
 				return backoff.Permanent(ctx.Err())
 			}
 
@@ -721,7 +722,6 @@ func (c *CopyOpConsumer) processReplicationOp(ctx context.Context, workerId uint
 
 			if err := c.replicaCopier.CopyReplica(ctx, op.sourceShard.nodeId, op.sourceShard.collectionId, op.targetShard.shardId); err != nil {
 				logger.WithError(err).Error("failure while copying replica shard")
-
 				return err
 			}
 
