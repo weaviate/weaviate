ec278f605ff8b74929c8300a629a5c76beedbd67
salvatorecampagna <salvatore@weaviate.io>
2025-04-16 11:03:08 +0200

    fix: do not retry on failure

--- cluster/service.go
+++ cluster/service.go
@@ -78,7 +78,7 @@ func New(cfg Config, svrMetrics *monitoring.GRPCServerMetrics) *Service {
 		realTimeProvider,
 		cfg.NodeSelector.LocalName(),
 		tracker,
-		backoff.WithMaxRetries(backoff.NewConstantBackOff(5*time.Second), 1),
+		&backoff.StopBackOff{},
 		retentionPolicy,
 		24*time.Hour,
 		5,
