3e584e33b6ada6485b80ff932bebe40e2103388f
Tommy Smith <tommy@weaviate.io>
2025-05-18 10:26:46 +0200

    Improve clarity of state change check logic

--- cluster/replication/consumer.go
+++ cluster/replication/consumer.go
@@ -186,13 +186,18 @@ func (c *CopyOpConsumer) Consume(ctx context.Context, in <-chan ShardReplication
 					// for operations that are already in progress or completed.
 					c.logger.WithFields(logrus.Fields{"op": op}).Debug("replication op skipped as already running")
 					shouldSkip = true
-				} else if state, err := c.leaderClient.ReplicationGetReplicaOpStatus(op.Op.ID); err != nil {
+				} else {
 					// Check if the operation has had its state changed between being added to the channel and being processed
-					c.logger.WithFields(logrus.Fields{"op": op}).Error("error while checking status of replication op")
-					shouldSkip = true
-				} else if state.String() != op.Status.GetCurrent().State.String() {
-					c.logger.WithFields(logrus.Fields{"op": op}).Debug("replication op skipped as state has changed")
-					shouldSkip = true
+					// This is chatty and will likely cause a lot of unnecessary load on the leader
+					// For now, we need it to ensure eventual consistency between the FSM and the consumer
+					state, err := c.leaderClient.ReplicationGetReplicaOpStatus(op.Op.ID)
+					if err != nil {
+						c.logger.WithFields(logrus.Fields{"op": op}).Error("error while checking status of replication op")
+						shouldSkip = true
+					} else if state.String() != op.Status.GetCurrent().State.String() {
+						c.logger.WithFields(logrus.Fields{"op": op}).Debug("replication op skipped as state has changed")
+						shouldSkip = true
+					}
 				}
 				// TODO: Consider more optimal ways of checking that the state of the op has not changed between it being added to the channel
 				// and being processed here. Could use in-memory solution, e.g. using cache, or refactor consumer-producer to be event/notification-based
