7fb87bc5c2529d0a1fa88e638f3574cbfd2d01ac
Dirk Kulawiak <dirk@semi.technology>
2025-05-27 12:54:39 +0200

    Fix case of db users not activated

--- cluster/dynusers/dynamic_users.go
+++ cluster/dynusers/dynamic_users.go
@@ -138,9 +138,21 @@ func (m *Manager) CheckUserIdentifierExists(req *cmd.QueryRequest) ([]byte, erro
 }
 
 func (m *Manager) Snapshot() ([]byte, error) {
+	if m.dynUser == nil {
+		return nil, nil
+	}
 	return m.dynUser.Snapshot()
 }
 
 func (m *Manager) Restore(snapshot []byte) error {
-	return m.dynUser.Restore(snapshot)
+	if m.dynUser == nil {
+		return nil
+	}
+	err := m.dynUser.Restore(snapshot)
+	if err != nil {
+		m.logger.Errorf("restored db users from snapshot failed with: %v", err)
+		return err
+	}
+	m.logger.Info("successfully restored dynamic users from snapshot")
+	return nil
 }
