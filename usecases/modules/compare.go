f62ffd97a932aadbc26acf1ad125828ed1f7c862
Jeremy Price <donomii@gmail.com>
2025-05-28 15:33:09 +0200

    Update usecases/modules/compare.go
    Co-authored-by: dyma solovei <53943884+bevzzz@users.noreply.github.com>

--- usecases/modules/compare.go
+++ usecases/modules/compare.go
@@ -39,7 +39,7 @@ func reVectorize(ctx context.Context,
 	}
 
 	if ctx.Err() != nil {
-		return false, nil, nil, fmt.Errorf("context is done: %w", ctx.Err())
+		return false, nil, nil, ctx.Err()
 	}
 	shouldReVectorize, oldObject := reVectorizeEmbeddings(ctx, cfg, mod, object, class, sourceProperties, findObjectFn)
 	if shouldReVectorize {
