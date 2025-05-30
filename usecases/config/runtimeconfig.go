e3db119cced1226930e4ff9bda3fc79b9bcb42c7
Kaviraj <kavirajkanagaraj@gmail.com>
2025-05-30 17:57:53 +0200

    chore: Add INFO logs for every change in runtime overrides config.
    This is emitted only if fields are actually changed. If no change between relods, no logs are printed. This
    also includes if any configs were removed from the file. In that case it prints the default value as new value.
    
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

--- usecases/config/runtimeconfig.go
+++ usecases/config/runtimeconfig.go
@@ -19,6 +19,7 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 	"github.com/weaviate/weaviate/usecases/config/runtime"
 	"gopkg.in/yaml.v3"
 )
@@ -53,12 +54,12 @@ func ParseRuntimeConfig(buf []byte) (*WeaviateRuntimeConfig, error) {
 
 // UpdateConfig does in-place update of `source` config based on values available in
 // `parsed` config.
-func UpdateRuntimeConfig(source, parsed *WeaviateRuntimeConfig) error {
+func UpdateRuntimeConfig(log logrus.FieldLogger, source, parsed *WeaviateRuntimeConfig) error {
 	if source == nil || parsed == nil {
 		return fmt.Errorf("source and parsed cannot be nil")
 	}
 
-	updateRuntimeConfig(reflect.ValueOf(*source), reflect.ValueOf(*parsed))
+	updateRuntimeConfig(log, reflect.ValueOf(*source), reflect.ValueOf(*parsed))
 	return nil
 }
 
@@ -97,7 +98,7 @@ But this approach has two serious drawbacks
 With this reflection method, we avoided that extra step from the consumer. This reflection approach is "logically" same as above implementation.
 See "runtimeconfig_test.go" for more examples.
 */
-func updateRuntimeConfig(source, parsed reflect.Value) {
+func updateRuntimeConfig(log logrus.FieldLogger, source, parsed reflect.Value) {
 	// Basically we do following
 	//
 	// 1. Loop through all the `source` fields
@@ -106,43 +107,97 @@ func updateRuntimeConfig(source, parsed reflect.Value) {
 	//    so that it's default value takes preference.
 	// 4. If parsed config does contain the field from `source`, We update the value via `SetValue`.
 
+	logRecords := make([]updateLogRecord, 0)
+
 	for i := range source.NumField() {
 		sf := source.Field(i)
 		pf := parsed.Field(i)
 
-		if pf.IsNil() {
-			si := sf.Interface()
-			src, ok := si.(interface{ Reset() })
-			if !ok {
-				panic(fmt.Sprintf("type doesn't have Reset() method: %#v", si))
-			}
-			src.Reset()
-			continue
+		r := updateLogRecord{
+			field: source.Type().Field(i).Name,
 		}
 
 		si := sf.Interface()
-		pi := pf.Interface()
+		var pi any
+		if !pf.IsNil() {
+			pi = pf.Interface()
+		}
 
 		switch sv := si.(type) {
 		case *runtime.DynamicValue[int]:
-			// this casting is fine, because both `parsed` and `source` are same struct.
-			p := pi.(*runtime.DynamicValue[int])
-			sv.SetValue(p.Get())
+			r.oldV = sv.Get()
+			if pf.IsNil() {
+				// Means the config is removed
+				sv.Reset()
+			} else {
+				p := pi.(*runtime.DynamicValue[int])
+				sv.SetValue(p.Get())
+			}
+			r.newV = sv.Get()
 		case *runtime.DynamicValue[float64]:
-			p := pi.(*runtime.DynamicValue[float64])
-			sv.SetValue(p.Get())
+			r.oldV = sv.Get()
+			if pf.IsNil() {
+				// Means the config is removed
+				sv.Reset()
+			} else {
+				p := pi.(*runtime.DynamicValue[float64])
+				sv.SetValue(p.Get())
+			}
+			r.newV = sv.Get()
 		case *runtime.DynamicValue[bool]:
-			p := pi.(*runtime.DynamicValue[bool])
-			sv.SetValue(p.Get())
+			r.oldV = sv.Get()
+			if pf.IsNil() {
+				// Means the config is removed
+				sv.Reset()
+			} else {
+				p := pi.(*runtime.DynamicValue[bool])
+				sv.SetValue(p.Get())
+			}
+			r.newV = sv.Get()
 		case *runtime.DynamicValue[time.Duration]:
-			p := pi.(*runtime.DynamicValue[time.Duration])
-			sv.SetValue(p.Get())
+			r.oldV = sv.Get()
+			if pf.IsNil() {
+				// Means the config is removed
+				sv.Reset()
+			} else {
+				p := pi.(*runtime.DynamicValue[time.Duration])
+				sv.SetValue(p.Get())
+			}
+			r.newV = sv.Get()
 		case *runtime.DynamicValue[string]:
-			p := pi.(*runtime.DynamicValue[string])
-			sv.SetValue(p.Get())
+			r.oldV = sv.Get()
+			if pf.IsNil() {
+				// Means the config is removed
+				sv.Reset()
+			} else {
+				p := pi.(*runtime.DynamicValue[string])
+				sv.SetValue(p.Get())
+			}
+			r.newV = sv.Get()
 		default:
 			panic(fmt.Sprintf("not recognized type: %#v, %#v", pi, si))
 		}
 
+		if r.newV != r.oldV {
+			logRecords = append(logRecords, r)
+
+		}
+
 	}
+
+	// log the changes made as INFO for auditing.
+	for _, v := range logRecords {
+		log.WithFields(logrus.Fields{
+			"action":    "runtime_overrides_changed",
+			"field":     v.field,
+			"old_value": v.oldV,
+			"new_value": v.newV,
+		}).Infof("runtime overrides: config '%v' changed from '%v' to '%v'", v.field, v.oldV, v.newV)
+	}
+}
+
+// updateLogRecord is used to record changes during updating runtime config.
+type updateLogRecord struct {
+	field      string
+	oldV, newV any
 }
