bfe1d53cd27c34f6723b78ddee34ef5211fa22ac
Dirk Kulawiak <dirk@semi.technology>
2025-06-03 14:01:10 +0200

    Rebase lazy segment loading on 1.30

--- adapters/repos/db/lsmkv/bucket.go
+++ adapters/repos/db/lsmkv/bucket.go
@@ -227,7 +227,7 @@ func (*Bucket) NewBucket(ctx context.Context, dir, rootDir string, logger logrus
 			keepSegmentsInMemory:     b.keepSegmentsInMemory,
 			MinMMapSize:              b.minMMapSize,
 			bm25config:               b.bm25Config,
-		}, b.allocChecker)
+		}, b.allocChecker, false)
 	if err != nil {
 		return nil, fmt.Errorf("init disk segments: %w", err)
 	}
@@ -239,7 +239,7 @@ func (*Bucket) NewBucket(ctx context.Context, dir, rootDir string, logger logrus
 	// (memtables will be created later on, with already modified strategy)
 	// TODO what if only WAL files exists, and there is no segment to get actual strategy?
 	if b.strategy == StrategyRoaringSet && len(sg.segments) > 0 &&
-		sg.segments[0].strategy == segmentindex.StrategySetCollection {
+		sg.segments[0].getStrategy() == segmentindex.StrategySetCollection {
 		b.strategy = StrategySetCollection
 		b.desiredStrategy = StrategyRoaringSet
 		sg.strategy = StrategySetCollection
@@ -252,7 +252,7 @@ func (*Bucket) NewBucket(ctx context.Context, dir, rootDir string, logger logrus
 	// renamed on startup by migrator. Here actual strategy is set based on
 	// data found in segment files
 	if b.strategy == StrategyRoaringSet && len(sg.segments) > 0 &&
-		sg.segments[0].strategy == segmentindex.StrategyMapCollection {
+		sg.segments[0].getStrategy() == segmentindex.StrategyMapCollection {
 		b.strategy = StrategyMapCollection
 		b.desiredStrategy = StrategyRoaringSet
 		sg.strategy = StrategyMapCollection
@@ -265,12 +265,12 @@ func (*Bucket) NewBucket(ctx context.Context, dir, rootDir string, logger logrus
 	// The changes only apply when we have segments on disk,
 	// as the memtables will always be created with the MapCollection strategy.
 	if b.strategy == StrategyInverted && len(sg.segments) > 0 &&
-		sg.segments[0].strategy == segmentindex.StrategyMapCollection {
+		sg.segments[0].getStrategy() == segmentindex.StrategyMapCollection {
 		b.strategy = StrategyMapCollection
 		b.desiredStrategy = StrategyInverted
 		sg.strategy = StrategyMapCollection
 	} else if b.strategy == StrategyMapCollection && len(sg.segments) > 0 &&
-		sg.segments[0].strategy == segmentindex.StrategyInverted {
+		sg.segments[0].getStrategy() == segmentindex.StrategyInverted {
 		// TODO amourao: blockmax "else" to be removed before final release
 		// in case bucket was created as inverted and default strategy was reverted to map
 		// by unsetting corresponding env variable
@@ -296,7 +296,7 @@ func (*Bucket) NewBucket(ctx context.Context, dir, rootDir string, logger logrus
 	// and other operations are based on the creation order of the segments
 
 	sort.Slice(b.disk.segments, func(i, j int) bool {
-		return b.disk.segments[i].path < b.disk.segments[j].path
+		return b.disk.segments[i].getPath() < b.disk.segments[j].getPath()
 	})
 
 	if b.active == nil {
@@ -891,8 +891,9 @@ func (b *Bucket) MapList(ctx context.Context, key []byte, cfgs ...MapListOption)
 		}
 
 		propLengths := make(map[uint64]uint32)
-		if segmentsDisk[i].strategy == segmentindex.StrategyInverted {
-			propLengths, err = segmentsDisk[i].GetPropertyLengths()
+		if segmentsDisk[i].getStrategy() == segmentindex.StrategyInverted {
+			sgm := segmentsDisk[i].getSegment()
+			propLengths, err = sgm.GetPropertyLengths()
 			if err != nil {
 				return nil, err
 			}
@@ -902,7 +903,7 @@ func (b *Bucket) MapList(ctx context.Context, key []byte, cfgs ...MapListOption)
 		for j, v := range disk[i] {
 			// Inverted segments have a slightly different internal format
 			// and separate property lengths that need to be read.
-			if segmentsDisk[i].strategy == segmentindex.StrategyInverted {
+			if segmentsDisk[i].getStrategy() == segmentindex.StrategyInverted {
 				if err := segmentDecoded[j].FromBytesInverted(v.value, false); err != nil {
 					return nil, err
 				}
@@ -974,11 +975,11 @@ func (b *Bucket) MapList(ctx context.Context, key []byte, cfgs ...MapListOption)
 	return newSortedMapMerger().do(ctx, segments)
 }
 
-func (b *Bucket) loadAllTombstones(segmentsDisk []*segment) ([]*sroar.Bitmap, error) {
+func (b *Bucket) loadAllTombstones(segmentsDisk []Segment) ([]*sroar.Bitmap, error) {
 	hasTombstones := false
 	allTombstones := make([]*sroar.Bitmap, len(segmentsDisk)+2)
 	for i, segment := range segmentsDisk {
-		if segment.strategy == segmentindex.StrategyInverted {
+		if segment.getStrategy() == segmentindex.StrategyInverted {
 			tombstones, err := segment.ReadOnlyTombstones()
 			if err != nil {
 				return nil, err
@@ -1585,8 +1586,10 @@ func (b *Bucket) DocPointerWithScoreList(ctx context.Context, key []byte, propBo
 			return nil, ctx.Err()
 		}
 		propLengths := make(map[uint64]uint32)
-		if segmentsDisk[i].strategy == segmentindex.StrategyInverted {
-			propLengths, err = segmentsDisk[i].GetPropertyLengths()
+		if segmentsDisk[i].getStrategy() == segmentindex.StrategyInverted {
+			sgm := segmentsDisk[i].getSegment()
+
+			propLengths, err = sgm.GetPropertyLengths()
 			if err != nil {
 				return nil, err
 			}
@@ -1594,7 +1597,7 @@ func (b *Bucket) DocPointerWithScoreList(ctx context.Context, key []byte, propBo
 
 		segmentDecoded := make([]terms.DocPointerWithScore, len(disk[i]))
 		for j, v := range disk[i] {
-			if segmentsDisk[i].strategy == segmentindex.StrategyInverted {
+			if segmentsDisk[i].getStrategy() == segmentindex.StrategyInverted {
 				docId := binary.BigEndian.Uint64(v.value[:8])
 				propLen := propLengths[docId]
 				if err := segmentDecoded[j].FromBytesInverted(v.value, propBoost, float32(propLen)); err != nil {
@@ -1745,8 +1748,9 @@ func (b *Bucket) CreateDiskTerm(N float64, filterDocIds helpers.AllowList, query
 		}
 
 		for _, segment := range segmentsDisk {
-			if segment.strategy == segmentindex.StrategyInverted && segment.hasKey(key) {
-				n += segment.getDocCount(key)
+			sgm := segment.getSegment()
+			if segment.getStrategy() == segmentindex.StrategyInverted && sgm.hasKey(key) {
+				n += sgm.getDocCount(key)
 			}
 		}
 
@@ -1777,7 +1781,7 @@ func (b *Bucket) CreateDiskTerm(N float64, filterDocIds helpers.AllowList, query
 		}
 
 		for i, key := range query {
-			term := NewSegmentBlockMax(segment, []byte(key), i, idfs[i], propertyBoost, allTombstones, filterDocIds, averagePropLength, config)
+			term := NewSegmentBlockMax(segment.getSegment(), []byte(key), i, idfs[i], propertyBoost, allTombstones, filterDocIds, averagePropLength, config)
 			if term != nil {
 				output[j] = append(output[j], term)
 			}
--- adapters/repos/db/lsmkv/segment.go
+++ adapters/repos/db/lsmkv/segment.go
@@ -18,6 +18,9 @@ import (
 	"io"
 	"os"
 
+	"github.com/weaviate/weaviate/adapters/repos/db/roaringset"
+	"github.com/weaviate/weaviate/adapters/repos/db/roaringsetrange"
+
 	"github.com/pkg/errors"
 
 	"github.com/bits-and-blooms/bloom/v3"
@@ -32,6 +35,69 @@ import (
 	"github.com/weaviate/weaviate/usecases/mmap"
 	"github.com/weaviate/weaviate/usecases/monitoring"
 )
+
+type Segment interface {
+	getPath() string
+	setPath(path string)
+	getStrategy() segmentindex.Strategy
+	getSecondaryIndexCount() uint16
+	getCountNetAdditions() int
+	getLevel() uint16
+	getSize() int64
+	setSize(size int64)
+	getIndexSize() int
+
+	PayloadSize() int
+	Size() int
+	bloomFilterPath() string
+	bloomFilterSecondaryPath(pos int) string
+	bufferedReaderAt(offset uint64) (*bufio.Reader, error)
+	bytesReaderFrom(in []byte) (*bytes.Reader, error)
+	close() error
+	collectionStratParseData(in []byte) ([]value, error)
+	computeAndStoreBloomFilter(path string) error
+	computeAndStoreSecondaryBloomFilter(path string, pos int) error
+	copyNode(b []byte, offset nodeOffset) error
+	countNetPath() string
+	dropImmediately() error
+	dropMarked() error
+	exists(key []byte) (bool, error)
+	get(key []byte) ([]byte, error)
+	getBySecondaryIntoMemory(pos int, key []byte, buffer []byte) ([]byte, []byte, []byte, error)
+	getCollection(key []byte) ([]value, error)
+	getInvertedData() *segmentInvertedData
+	getSegment() *segment
+	initBloomFilter(overwrite bool) error
+	initBloomFilters(metrics *Metrics, overwrite bool) error
+	initCountNetAdditions(exists existsOnLowerSegmentsFn, overwrite bool) error
+	initSecondaryBloomFilter(pos int, overwrite bool) error
+	loadBloomFilterFromDisk() error
+	loadBloomFilterSecondaryFromDisk(pos int) error
+	loadCountNetFromDisk() error
+	markForDeletion() error
+	MergeTombstones(other *sroar.Bitmap) (*sroar.Bitmap, error)
+	newCollectionCursor() *segmentCursorCollection
+	newCollectionCursorReusable() *segmentCursorCollectionReusable
+	newCursor() *segmentCursorReplace
+	newCursorWithSecondaryIndex(pos int) *segmentCursorReplace
+	newMapCursor() *segmentCursorMap
+	newNodeReader(offset nodeOffset) (*nodeReader, error)
+	newRoaringSetCursor() *roaringset.SegmentCursor
+	newRoaringSetRangeCursor() roaringsetrange.SegmentCursor
+	newRoaringSetRangeReader() *roaringsetrange.SegmentReader
+	precomputeBloomFilter() error
+	precomputeBloomFilters() ([]string, error)
+	precomputeCountNetAdditions(updatedCountNetAdditions int) ([]string, error)
+	precomputeSecondaryBloomFilter(pos int) error
+	quantileKeys(q int) [][]byte
+	ReadOnlyTombstones() (*sroar.Bitmap, error)
+	replaceStratParseData(in []byte) ([]byte, []byte, error)
+	roaringSetGet(key []byte) (roaringset.BitmapLayer, error)
+	segmentNodeFromBuffer(offset nodeOffset) (*roaringset.SegmentNode, bool, error)
+	storeBloomFilterOnDisk(path string) error
+	storeBloomFilterSecondaryOnDisk(path string, pos int) error
+	storeCountNetOnDisk() error
+}
 
 type segment struct {
 	path                string
@@ -415,6 +481,50 @@ func (s *segment) markForDeletion() error {
 // and index
 func (s *segment) Size() int {
 	return int(s.size)
+}
+
+func (s *segment) getPath() string {
+	return s.path
+}
+
+func (s *segment) setPath(path string) {
+	s.path = path
+}
+
+func (s *segment) getStrategy() segmentindex.Strategy {
+	return s.strategy
+}
+
+func (s *segment) getSecondaryIndexCount() uint16 {
+	return s.secondaryIndexCount
+}
+
+func (s *segment) getCountNetAdditions() int {
+	return s.countNetAdditions
+}
+
+func (s *segment) getLevel() uint16 {
+	return s.level
+}
+
+func (s *segment) getSize() int64 {
+	return s.size
+}
+
+func (s *segment) setSize(size int64) {
+	s.size = size
+}
+
+func (s *segment) getIndexSize() int {
+	return s.index.Size()
+}
+
+func (s *segment) getInvertedData() *segmentInvertedData {
+	return s.invertedData
+}
+
+func (s *segment) getSegment() *segment {
+	return s
 }
 
 // PayloadSize is only the payload of the index, excluding the index
