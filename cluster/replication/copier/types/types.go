4d37a95b86a3e572464af946f81a16ed364aea7a
Jeronimo <jeronimo.irazabal@gmail.com>
2025-06-04 09:44:21 -0300

    chore: file replication over grpc
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>
    
    chore: file replication over grpc
    
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>
    
    chore: file replication over grpc
    
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>
    
    chore: file replication over grpc
    
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>
    
    chore: file replication over grpc
    
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>
    
    chore: mockable copier over grpc
    
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>
    
    chore: optional compression type
    
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>
    
    chore: optional compression type
    
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>
    
    test: mocked streams without concurrent workers
    
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>
    
    chore: fetch node grpc port from node metadata
    
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>
    
    chore: mockable copier over grpc
    
    chore: mockable copier over grpc
    
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>

--- cluster/replication/copier/types/types.go
+++ cluster/replication/copier/types/types.go
@@ -13,13 +13,11 @@ package types
 
 import (
 	"context"
-	"io"
 
 	"github.com/weaviate/weaviate/adapters/repos/db"
 	"github.com/weaviate/weaviate/entities/additional"
 	"github.com/weaviate/weaviate/entities/models"
 	"github.com/weaviate/weaviate/entities/schema"
-	"github.com/weaviate/weaviate/usecases/file"
 )
 
 // DbWrapper is a type that hides a db.DB, this is used to avoid a circular
@@ -42,19 +40,6 @@ type ShardLoader interface {
 // RemoteIndex is a type that can interact with a remote index, this is used to avoid a circular
 // dependency between the copier and the db package.
 type RemoteIndex interface {
-	// PauseFileActivity See adapters/clients.RemoteIndex.PauseFileActivity
-	PauseFileActivity(ctx context.Context, hostName, indexName, shardName string, schemaVersion uint64) error
-	// ResumeFileActivity See adapters/clients.RemoteIndex.ResumeFileActivity
-	ResumeFileActivity(ctx context.Context, hostName, indexName, shardName string) error
-	// ListFiles See adapters/clients.RemoteIndex.ListFiles
-	ListFiles(ctx context.Context,
-		hostName, indexName, shardName string) ([]string, error)
-	// GetFileMetadata See adapters/clients.RemoteIndex.GetFileMetadata
-	GetFileMetadata(ctx context.Context,
-		hostName, indexName, shardName, fileName string) (file.FileMetadata, error)
-	// GetFile See adapters/clients.RemoteIndex.GetFile
-	GetFile(ctx context.Context,
-		hostName, indexName, shardName, fileName string) (io.ReadCloser, error)
 	// AddAsyncReplicationTargetNode See adapters/clients.RemoteIndex.AddAsyncReplicationTargetNode
 	AddAsyncReplicationTargetNode(ctx context.Context,
 		hostName, indexName, shardName string, targetNodeOverride additional.AsyncReplicationTargetNodeOverride, schemaVersion uint64) error
