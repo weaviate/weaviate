55eb829652982f1fc9e7e9a5930b7a53ecabfdf3
Dirk Kulawiak <dirk@semi.technology>
2025-06-16 11:50:38 +0200

    Extend benchmark to more strategies

--- adapters/repos/db/lsmkv/compaction_bench_test.go
+++ adapters/repos/db/lsmkv/compaction_bench_test.go
@@ -27,56 +27,73 @@ func BenchmarkCompaction(b *testing.B) {
 	logger, _ := test.NewNullLogger()
 	ctx := context.Background()
 
-	opts := []BucketOption{
-		WithStrategy(StrategyMapCollection),
-		WithKeepTombstones(true),
-		WithPread(true),
-	}
-
 	valuesPerSegment := 10000
 
-	b.ResetTimer()
-	b.ReportAllocs()
-
-	for i := 0; i < b.N; i++ {
-		// avoid page cache by having a unique dir per run and have the content of the segments unique
-		b.StopTimer()
-		tmpDir := b.TempDir()
-
-		bu, err := NewBucketCreator().NewBucket(ctx, tmpDir, "", logger, nil,
-			cyclemanager.NewCallbackGroupNoop(), cyclemanager.NewCallbackGroupNoop(), opts...)
-		require.NoError(b, err)
-
-		for j := 0; j < valuesPerSegment; j++ {
-			require.NoError(b, bu.MapSet(
-				[]byte(fmt.Sprintf("key%d_%d", j, i)), MapPair{Key: []byte(fmt.Sprintf("keyMP%d_%d", j, i)), Value: []byte(fmt.Sprintf("value_%d_%d", j, i))}))
-			if j%10 == 0 {
-				require.NoError(b, bu.MapDeleteKey([]byte(fmt.Sprintf("key2_%d_%d", j, i)), []byte(fmt.Sprintf("keyMP2_%d_%d", j, i))))
+	for _, strategy := range []string{StrategyMapCollection, StrategyReplace} {
+		for _, pread := range []bool{true, false} {
+			opts := []BucketOption{
+				WithStrategy(strategy),
+				WithKeepTombstones(true),
+				WithPread(pread),
 			}
+
+			b.Run(fmt.Sprintf("strategy %s with pread: %t", strategy, pread), func(b *testing.B) {
+				b.ReportAllocs()
+
+				for i := 0; i < b.N; i++ {
+					// avoid page cache by having a unique dir per run and have the content of the segments unique
+					b.StopTimer()
+					tmpDir := b.TempDir()
+
+					bu, err := NewBucketCreator().NewBucket(ctx, tmpDir, "", logger, nil,
+						cyclemanager.NewCallbackGroupNoop(), cyclemanager.NewCallbackGroupNoop(), opts...)
+					require.NoError(b, err)
+
+					for j := 0; j < valuesPerSegment; j++ {
+						addData(b, bu, i, j, "key", "key2", strategy)
+					}
+					require.NoError(b, bu.FlushMemtable())
+
+					fileTypes := getFiles(b, tmpDir)
+					require.Equal(b, 1, fileTypes[".db"])
+
+					for j := 0; j < valuesPerSegment; j++ {
+						addData(b, bu, i, j, "key2", "key", strategy)
+					}
+					require.NoError(b, bu.FlushMemtable())
+
+					fileTypes = getFiles(b, tmpDir)
+					require.Equal(b, 2, fileTypes[".db"])
+					b.StartTimer()
+
+					once, err := bu.disk.compactOnce()
+					require.NoError(b, err)
+					require.True(b, once)
+
+					require.NoError(b, bu.Shutdown(ctx))
+				}
+			})
 		}
-		require.NoError(b, bu.FlushMemtable())
+	}
+}
 
-		fileTypes := getFiles(b, tmpDir)
-		require.Equal(b, 1, fileTypes[".db"])
-
-		for j := 0; j < valuesPerSegment; j++ {
-			require.NoError(b, bu.MapSet(
-				[]byte(fmt.Sprintf("key2%d_%d", j, i)), MapPair{Key: []byte(fmt.Sprintf("keyMP2%d_%d", j, i)), Value: []byte(fmt.Sprintf("value%d_%d", j, i))}))
-			if j%10 == 0 {
-				require.NoError(b, bu.MapDeleteKey([]byte(fmt.Sprintf("key%d_%d", j, i)), []byte(fmt.Sprintf("keyMP%d_%d", j, i))))
-			}
+func addData(b testing.TB, bu *Bucket, i, j int, prefixInsert, prefixDelete, strategy string) {
+	if strategy == StrategyReplace {
+		require.NoError(b, bu.Put([]byte(fmt.Sprintf("%s%d_%d", prefixInsert, j, i)), []byte(fmt.Sprintf("value%d_%d", j, i))))
+	} else if strategy == StrategySetCollection {
+		require.NoError(b, bu.SetAdd([]byte(fmt.Sprintf("%s%d_%d", prefixInsert, j, i)), [][]byte{[]byte(fmt.Sprintf("value%d_%d", j, i))}))
+	} else if strategy == StrategyRoaringSet {
+		require.NoError(b, bu.RoaringSetAddOne([]byte(fmt.Sprintf("%s%d_%d", prefixInsert, j, i)), uint64(i+j)))
+	} else if strategy == StrategyMapCollection {
+		require.NoError(b, bu.MapSet(
+			[]byte(fmt.Sprintf("%s%d_%d", prefixInsert, j, i)), MapPair{Key: []byte(fmt.Sprintf("%sMP%d_%d", prefixInsert, j, i)), Value: []byte(fmt.Sprintf("value%d_%d", j, i))}))
+		if j%10 == 0 {
+			require.NoError(b, bu.MapDeleteKey([]byte(fmt.Sprintf("%s%d_%d", prefixDelete, j, i)), []byte(fmt.Sprintf("%sMP%d_%d", prefixDelete, j, i))))
 		}
-		require.NoError(b, bu.FlushMemtable())
-
-		fileTypes = getFiles(b, tmpDir)
-		require.Equal(b, 2, fileTypes[".db"])
-		b.StartTimer()
-
-		once, err := bu.disk.compactOnce()
-		require.NoError(b, err)
-		require.True(b, once)
-
-		require.NoError(b, bu.Shutdown(ctx))
+	} else if strategy == StrategyRoaringSetRange {
+		require.NoError(b, bu.RoaringSetRangeAdd(uint64(i+j), uint64(i+j)))
+	} else {
+		require.Fail(b, "unknown strategy %s", strategy)
 	}
 }
 
