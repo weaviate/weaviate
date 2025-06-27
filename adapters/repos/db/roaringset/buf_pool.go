9380580b9474700bac8be15234c02d13b04ed0a1
Andrzej Liszka <andrzej.liszka@gmail.com>
2025-06-27 16:49:43 +0200

    filterable improvement: in memory buf pool for bigger segments

--- adapters/repos/db/roaringset/buf_pool.go
+++ adapters/repos/db/roaringset/buf_pool.go
@@ -12,15 +12,20 @@
 package roaringset
 
 import (
+	"context"
+	"fmt"
 	"math"
+	"math/bits"
+	"runtime"
 	"slices"
 	"sync"
 	"sync/atomic"
+	"time"
 
+	"github.com/sirupsen/logrus"
 	"github.com/weaviate/sroar"
+	"github.com/weaviate/weaviate/entities/errors"
 )
-
-// -----------------------------------------------------------------------------
 
 type BitmapBufPool interface {
 	Get(minCap int) (buf []byte, put func())
@@ -30,6 +35,60 @@ type BitmapBufPool interface {
 func cloneToBuf(pool BitmapBufPool, bm *sroar.Bitmap) (cloned *sroar.Bitmap, put func()) {
 	buf, put := pool.Get(bm.LenInBytes())
 	return bm.CloneToBuf(buf), put
+}
+
+func NewBitmapBufPoolDefault(logger logrus.FieldLogger, inMemoMaxBufSize int, maxMemoSizeForBufs int,
+) (pool BitmapBufPool, close func()) {
+	cleanupInterval := 10 * time.Second
+
+	syncMinRangeP2 := 9  // 2^9 = 512B
+	syncMaxRangeP2 := 20 // 2^20 = 1MB
+	syncRanges := calculateSyncBufferRanges(syncMinRangeP2, syncMaxRangeP2)
+	syncMaxBufSize := syncRanges[len(syncRanges)-1]
+
+	inMemoMinRangeP2 := syncMaxRangeP2 + 1
+	inMemoRanges, inMemoBufsLimits := calculateInMemoBufferRangesAndLimits(syncMaxBufSize, inMemoMinRangeP2,
+		inMemoMaxBufSize, maxMemoSizeForBufs)
+
+	allRanges := syncRanges
+	if len(inMemoRanges) > 0 {
+		allRanges = append(allRanges, inMemoRanges...)
+	}
+
+	fmt.Printf(". ==> all ranges %v\n\n", allRanges)
+	p := NewBitmapBufPoolRanged(syncMaxBufSize, inMemoBufsLimits, allRanges...)
+	stop := p.StartPeriodicCleanup(logger, cleanupInterval)
+
+	go func() {
+		var topAlloc uint64
+		var stats runtime.MemStats
+
+		i := 0
+		for {
+			time.Sleep(1 * time.Second)
+			runtime.ReadMemStats(&stats)
+			if stats.Alloc > topAlloc {
+				topAlloc = stats.Alloc
+			}
+			i++
+
+			if i%30 == 0 {
+				fmt.Printf("  ==> alloc: %.2fMB, top alloc: %.2fMB, total alloc: %.2fMB\n",
+					float64(stats.Alloc)/1024/1024, float64(topAlloc)/1024/1024, float64(stats.TotalAlloc)/1024/1024)
+
+				for _, pf := range p.poolsSync {
+					fmt.Printf("      pool [%d]: got [%d] created [%d] put [%d]\n",
+						pf.cap, pf.got.Load(), pf.created.Load(), pf.put.Load())
+				}
+				for _, pl := range p.poolsInMemo {
+					fmt.Printf("      pool [%d]: got [%d] created [%d] put [%d] outside [%d] cleaned [%d]\n",
+						pl.cap, pl.got.Load(), pl.created.Load(), pl.put.Load(), pl.outside.Load(), pl.cleaned.Load())
+				}
+			}
+		}
+	}()
+
+	return p, stop
 }
 
 // -----------------------------------------------------------------------------
@@ -50,123 +109,85 @@ func (p *bitmapBufPoolNoop) CloneToBuf(bm *sroar.Bitmap) (cloned *sroar.Bitmap, 
 
 // -----------------------------------------------------------------------------
 
-type bitmapBufPoolVar struct {
-	pool *sync.Pool
-}
-
-func NewBitmapBufPoolVar() *bitmapBufPoolVar {
-	return &bitmapBufPoolVar{
-		pool: &sync.Pool{
-			New: func() any {
-				dummyBuf := make([]byte, 0)
-				return &dummyBuf
-			},
-		},
-	}
-}
-
-func (p *bitmapBufPoolVar) Get(minCap int) (buf []byte, put func()) {
-	ptr := p.pool.Get().(*[]byte)
-	buf = *ptr
-	if cap(buf) < minCap {
-		buf = make([]byte, 0, minCap)
-		*ptr = buf
-	} else if len(buf) > 0 {
-		buf = buf[:0]
-		*ptr = buf
-	}
-	return buf, func() { p.pool.Put(ptr) }
-}
-
-func (p *bitmapBufPoolVar) CloneToBuf(bm *sroar.Bitmap) (cloned *sroar.Bitmap, put func()) {
-	return cloneToBuf(p, bm)
-}
-
-// -----------------------------------------------------------------------------
-
-type bitmapBufPoolFixed struct {
-	pool *sync.Pool
-}
-
-func NewBitmapBufPoolFixed(cap int) *bitmapBufPoolFixed {
-	return &bitmapBufPoolFixed{
-		pool: &sync.Pool{
-			New: func() any {
-				buf := make([]byte, 0, cap)
-				return &buf
-			},
-		},
-	}
-}
-
-func (p *bitmapBufPoolFixed) Get() (buf []byte, put func()) {
-	ptr := p.pool.Get().(*[]byte)
-	return *ptr, func() { p.pool.Put(ptr) }
-}
-
-func (p *bitmapBufPoolFixed) CloneToBuf(bm *sroar.Bitmap) (cloned *sroar.Bitmap, put func()) {
-	buf, put := p.Get()
-	if cap(buf) < bm.LenInBytes() {
-		panic("buffer too small to fit bitmap")
-	}
-	return bm.CloneToBuf(buf), put
-}
-
-// -----------------------------------------------------------------------------
-
 type bitmapBufPoolRanged struct {
-	ranges     []int
-	poolsFixed []*bitmapBufPoolFixed
-	poolVar    *bitmapBufPoolVar
+	ranges            []int
+	firstInMemoRngIdx int
+	poolsSync         []*BufPoolFixedSync
+	poolsInMemo       []*BufPoolFixedInMemory
 }
 
 // Creates multiple pools, one for specified range of sizes (given in bytes).
-// E.g. for ranges 1_000, 10_000 and 100_000, 4 internal buffer pools will be
-// created to handle listed ranges of sizes:
-// [0-1_000], [1_001-10_000], [10_001-100_000], [100_001-*]
+// E.g. for ranges 1024, 2048 and 4096, 3 internal buffer pools will be
+// created to handle ranges of size: [1-1024], [1025-2048], [2048-4096].
+// Buffers of sizes bigger than highest range will be created but not kept in pool
+// (to be removed by GC when no longer needed)
 // Ranges <=0 or duplicated will be ignored.
-func NewBitmapBufPoolRanged(ranges ...int) *bitmapBufPoolRanged {
-	if ln := len(ranges); ln > 0 {
-		// cleanup ranges, keep unique and > 0
-		unique_gt0 := map[int]struct{}{}
-		for i := 0; i < ln; i++ {
-			if r := ranges[i]; r > 0 {
-				unique_gt0[ranges[i]] = struct{}{}
-			}
-		}
-		i := 0
-		for r := range unique_gt0 {
-			ranges[i] = r
-			i++
-		}
-		ranges = ranges[:i]
-		slices.Sort(ranges)
-	}
+func NewBitmapBufPoolRanged(syncMaxBufSize int, inMemoBufsLimits map[int]int, ranges ...int) *bitmapBufPoolRanged {
+	ranges = validateBufferRanges(ranges)
+	poolsSync := []*BufPoolFixedSync{}
+	poolsInMemo := []*BufPoolFixedInMemory{}
 
-	poolsFixed := make([]*bitmapBufPoolFixed, len(ranges))
-	for i := range poolsFixed {
-		poolsFixed[i] = NewBitmapBufPoolFixed(ranges[i])
+	i := 0
+	for ; i < len(ranges) && ranges[i] <= syncMaxBufSize; i++ {
+		poolsSync = append(poolsSync, NewBufPoolFixedSync(ranges[i]))
 	}
-	poolVar := NewBitmapBufPoolVar()
+	firstInMemoRngIdx := i
+	for ; i < len(ranges); i++ {
+		if limit, ok := inMemoBufsLimits[ranges[i]]; ok {
+			poolsInMemo = append(poolsInMemo, NewBufPoolFixedInMemory(ranges[i], limit))
+		}
+	}
 
 	return &bitmapBufPoolRanged{
-		ranges:     ranges,
-		poolsFixed: poolsFixed,
-		poolVar:    poolVar,
+		ranges:            ranges,
+		firstInMemoRngIdx: firstInMemoRngIdx,
+		poolsSync:         poolsSync,
+		poolsInMemo:       poolsInMemo,
 	}
 }
 
 func (p *bitmapBufPoolRanged) Get(minCap int) (buf []byte, put func()) {
-	for i, rng := range p.ranges {
-		if minCap <= rng {
-			return p.poolsFixed[i].Get()
+	for i := 0; i < p.firstInMemoRngIdx; i++ {
+		if minCap <= p.ranges[i] {
+			return p.poolsSync[i].Get()
 		}
 	}
-	return p.poolVar.Get(minCap)
+	for i := p.firstInMemoRngIdx; i < len(p.ranges); i++ {
+		if minCap <= p.ranges[i] {
+			return p.poolsInMemo[i-p.firstInMemoRngIdx].Get()
+		}
+	}
+	return make([]byte, 0, minCap), func() {}
 }
 
 func (p *bitmapBufPoolRanged) CloneToBuf(bm *sroar.Bitmap) (cloned *sroar.Bitmap, put func()) {
 	return cloneToBuf(p, bm)
+}
+
+func (p *bitmapBufPoolRanged) cleanup() map[int]int {
+	cleaned := map[int]int{}
+	for i := p.firstInMemoRngIdx; i < len(p.ranges); i++ {
+		cleaned[p.ranges[i]] = p.poolsInMemo[i-p.firstInMemoRngIdx].Cleanup()
+	}
+	return cleaned
+}
+
+func (p *bitmapBufPoolRanged) StartPeriodicCleanup(logger logrus.FieldLogger, interval time.Duration) (stop func()) {
+	ctx, cancel := context.WithCancel(context.Background())
+	errors.GoWrapper(func() {
+		ticker := time.NewTicker(interval)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				p.cleanup()
+			}
+		}
+	}, logger)
+	return cancel
 }
 
 // -----------------------------------------------------------------------------
@@ -188,38 +209,199 @@ func (p *bitmapBufPoolFactorWrapper) Get(minCap int) (buf []byte, put func()) {
 
 func (p *bitmapBufPoolFactorWrapper) CloneToBuf(bm *sroar.Bitmap) (cloned *sroar.Bitmap, put func()) {
 	return cloneToBuf(p, bm)
-} // -----------------------------------------------------------------------------
-
-type BitmapBufPoolInUseCountingWrapper struct {
-	pool         BitmapBufPool
-	inUseCounter atomic.Int32
-}
-
-func NewBitmapBufPoolInUseCountingWrapper(pool BitmapBufPool) *BitmapBufPoolInUseCountingWrapper {
-	return &BitmapBufPoolInUseCountingWrapper{pool: pool}
-}
-
-func (p *BitmapBufPoolInUseCountingWrapper) Get(minCap int) (buf []byte, put func()) {
-	p.inUseCounter.Add(1)
-	bbuf, pput := p.pool.Get(minCap)
-	return bbuf, func() {
-		pput()
-		p.inUseCounter.Add(-1)
-	}
-}
-
-func (p *BitmapBufPoolInUseCountingWrapper) CloneToBuf(bm *sroar.Bitmap) (cloned *sroar.Bitmap, put func()) {
-	return cloneToBuf(p, bm)
-}
-
-func (p *BitmapBufPoolInUseCountingWrapper) Counter() int32 {
-	return p.inUseCounter.Load()
 }
 
 // -----------------------------------------------------------------------------
 
-func NewBitmapBufPoolDefault() BitmapBufPool {
-	k := 1_000
-	M := k * k
-	return NewBitmapBufPoolRanged(1*k, 10*k, 100*k, 500*k, 1*M, 5*M, 10*M, 25*M, 50*M, 75*M, 100*M)
+type BufPoolFixedSync struct {
+	pool *sync.Pool
+
+	cap               int
+	created, got, put atomic.Int32
+}
+
+func NewBufPoolFixedSync(cap int) *BufPoolFixedSync {
+	p := &BufPoolFixedSync{
+		cap: cap,
+	}
+
+	pool := &sync.Pool{
+		New: func() any {
+			p.created.Add(1)
+			buf := make([]byte, 0, cap)
+			return &buf
+		},
+	}
+
+	p.pool = pool
+	return p
+}
+
+func (p *BufPoolFixedSync) Get() (buf []byte, put func()) {
+	ptr := p.pool.Get().(*[]byte)
+	defer p.got.Add(1)
+	return *ptr, func() { p.pool.Put(ptr); p.put.Add(1) }
+}
+
+// -----------------------------------------------------------------------------
+
+type BufPoolFixedInMemory struct {
+	cap    int
+	limit  int
+	bufsCh chan *[]byte  // chan of free buffers
+	leftCh chan struct{} // chan of markings of buffers left to be created up to provided limit
+
+	got, put, created, cleaned, outside atomic.Int32
+}
+
+func NewBufPoolFixedInMemory(cap int, limit int) *BufPoolFixedInMemory {
+	bufCh := make(chan *[]byte, limit)
+	leftCh := make(chan struct{}, limit)
+	for i := 0; i < limit; i++ {
+		leftCh <- struct{}{}
+	}
+
+	return &BufPoolFixedInMemory{
+		cap:    cap,
+		limit:  limit,
+		bufsCh: bufCh,
+		leftCh: leftCh,
+	}
+}
+
+func (p *BufPoolFixedInMemory) Get() (buf []byte, put func()) {
+	// take free buffer if available
+	select {
+	case ptr := <-p.bufsCh:
+		defer p.got.Add(1)
+		return *ptr, func() {
+			select {
+			case p.bufsCh <- ptr:
+				p.put.Add(1)
+				// ok
+			default:
+				// should not happen
+			}
+		}
+	default:
+	}
+
+	// no free buffer
+	select {
+	case <-p.leftCh:
+		// create new buffer if limit not yet reached
+		buf := make([]byte, 0, p.cap)
+		p.created.Add(1)
+		ptr := &buf
+		defer p.got.Add(1)
+		return *ptr, func() {
+			select {
+			case p.bufsCh <- ptr:
+				p.put.Add(1)
+				// ok
+			default:
+				// should not happen
+			}
+		}
+	default:
+		// limit reached, create temp buffer outside of pool
+		p.outside.Add(1)
+		return make([]byte, 0, p.cap), func() {}
+	}
+}
+
+// Cleanup removes available buffers from the pool and returs number of buffers removed.
+// Buffers are removed up to configured limit to prevent indefinite removal in case
+// new ones are created in the parallel.
+func (p *BufPoolFixedInMemory) Cleanup() int {
+	i := 0
+	for ; i < p.limit; i++ {
+		select {
+		case <-p.bufsCh:
+			p.cleaned.Add(1)
+			p.leftCh <- struct{}{}
+		default:
+			return i
+		}
+	}
+	return i
+}
+
+// -----------------------------------------------------------------------------
+
+func validateBufferRanges(ranges []int) []int {
+	if ln := len(ranges); ln > 0 {
+		// cleanup ranges, keep unique and > 0
+		unique_gt0 := map[int]struct{}{}
+		for i := 0; i < ln; i++ {
+			if rng := ranges[i]; rng > 0 {
+				unique_gt0[rng] = struct{}{}
+			}
+		}
+		i := 0
+		for rng := range unique_gt0 {
+			ranges[i] = rng
+			i++
+		}
+		ranges = ranges[:i]
+		slices.Sort(ranges)
+	}
+	return ranges
+}
+
+func calculateSyncBufferRanges(minRangeP2, maxRangeP2 int) []int {
+	rangesLn := maxRangeP2 - minRangeP2 + 1
+	ranges := make([]int, rangesLn)
+	for i := range ranges {
+		ranges[i] = 1 << (i + minRangeP2)
+	}
+	return ranges
+}
+
+func calculateInMemoBufferRangesAndLimits(maxSyncBufSize, minRangeP2, maxBufSize, maxMemoSize int,
+) ([]int, map[int]int) {
+	var bufsLimits map[int]int // range -> limit
+	var ranges []int
+	if maxBufSize > maxSyncBufSize {
+		maxRangeP2 := 63 - bits.LeadingZeros64(uint64(maxBufSize))
+
+		rangesLn := maxRangeP2 - minRangeP2 + 1
+		ranges = make([]int, rangesLn, rangesLn+1)
+		for i := 0; i < rangesLn; i++ {
+			ranges[i] = 1 << (i + minRangeP2)
+		}
+		if maxBufSize != 1<<maxRangeP2 {
+			rangesLn++
+			ranges = append(ranges, maxBufSize)
+		}
+
+		sums := make([]int, rangesLn)
+		for i := 0; i < rangesLn; i++ {
+			sums[i] = ranges[i]
+			if i != 0 {
+				sums[i] += sums[i-1]
+			}
+			if sums[i] > maxMemoSize {
+				sums = sums[:i]
+				ranges = ranges[:i]
+				break
+			}
+		}
+
+		rangesLn = len(ranges)
+		bufsLimits = make(map[int]int, len(ranges))
+		for i := rangesLn - 1; i >= 0; i-- {
+			bufsLimits[ranges[i]] = maxMemoSize / sums[i]
+			maxMemoSize -= sums[i] * bufsLimits[ranges[i]]
+
+			if i != rangesLn-1 {
+				bufsLimits[ranges[i]] += bufsLimits[ranges[i+1]]
+			}
+		}
+
+		fmt.Printf("inMemoRanges %v\n", ranges)
+		fmt.Printf("sums %v\n", sums)
+		fmt.Printf("limits %v\n", bufsLimits)
+	}
+	return ranges, bufsLimits
 }
