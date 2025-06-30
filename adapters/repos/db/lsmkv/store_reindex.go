0a04b0903ff3a7b7bd42e9b3b6a9ab8b4a6966db
André Mourão <andre.mourao@weaviate.io>
2025-06-30 12:28:29 +0100

    feat: refactor pause and resume timer functionality for bucket compaction

--- adapters/repos/db/lsmkv/store_reindex.go
+++ adapters/repos/db/lsmkv/store_reindex.go
@@ -15,8 +15,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/weaviate/weaviate/usecases/monitoring"
+	"github.com/weaviate/weaviate/adapters/repos/db/helpers"
 )
 
 // PauseObjectBucketCompaction pauses the compaction cycle for the objects bucket.
@@ -26,16 +25,10 @@ func (s *Store) PauseObjectBucketCompaction(ctx context.Context) error {
 	s.bucketAccessLock.RLock()
 	defer s.bucketAccessLock.RUnlock()
 
-	b := s.bucketsByName["objects"]
+	b := s.Bucket(helpers.ObjectsBucketLSM)
 
 	b.disk.compactionCallbackCtrl.Deactivate(ctx)
-	label := b.GetDir()
-	if monitoring.GetMetrics().Group {
-		label = "n/a"
-	}
-	if metric, err := monitoring.GetMetrics().BucketPauseDurations.GetMetricWithLabelValues(label); err == nil {
-		b.pauseTimer = prometheus.NewTimer(metric)
-	}
+	b.doStartPauseTimer()
 	return nil
 }
 
@@ -44,7 +37,7 @@ func (s *Store) ResumeObjectBucketCompaction(ctx context.Context) error {
 	s.bucketAccessLock.RLock()
 	defer s.bucketAccessLock.RUnlock()
 
-	b := s.bucketsByName["objects"]
+	b := s.Bucket(helpers.ObjectsBucketLSM)
 	if b == nil {
 		return fmt.Errorf("no bucket named 'objects' found in store %s", s.dir)
 	}
@@ -52,12 +45,8 @@ func (s *Store) ResumeObjectBucketCompaction(ctx context.Context) error {
 	if err := b.disk.compactionCallbackCtrl.Activate(); err != nil {
 		return err
 	}
-	s.bucketAccessLock.RLock()
-	defer s.bucketAccessLock.RUnlock()
 
-	if b.pauseTimer != nil {
-		b.pauseTimer.ObserveDuration()
-	}
+	b.doStopPauseTimer()
 
 	return nil
 }
