b0d52ac44af6126c9183490730c1a9c3fcd49f76
salvatorecampagna <salvatore@weaviate.io>
2025-07-09 17:49:26 +0200

    fix: read just one replica per shard

--- cluster/router/types/read_strategy_test.go
+++ cluster/router/types/read_strategy_test.go
@@ -45,10 +45,10 @@ func TestDirectCandidateReadStrategy_PrefersCandidate(t *testing.T) {
 	// THEN
 	expectedShardToNode := map[string]string{
 		"S1": "B", // S1 should pick node B (direct candidate)
-		"S2": "",  // S2 has no candidate, could be either C or D
+		"S2": "C", // S2 has no candidate, should pick first available (C)
 	}
 
-	assertReadStrategyResult(t, rs, actual, expectedShardToNode)
+	assertReadStrategyResult(t, actual, expectedShardToNode)
 }
 
 func TestDirectCandidateReadStrategy_FallsBackToLocalNode(t *testing.T) {
@@ -63,11 +63,11 @@ func TestDirectCandidateReadStrategy_FallsBackToLocalNode(t *testing.T) {
 
 	// THEN
 	expectedShardToNode := map[string]string{
-		"S1": "",  // S1 has no local node D, could be A or B
+		"S1": "A", // S1 has no local node D, should pick first available (A)
 		"S2": "D", // S2 should pick node D (local node)
 	}
 
-	assertReadStrategyResult(t, rs, actual, expectedShardToNode)
+	assertReadStrategyResult(t, actual, expectedShardToNode)
 }
 
 func TestDirectCandidateReadStrategy_FallbackWhenAbsent(t *testing.T) {
@@ -82,11 +82,11 @@ func TestDirectCandidateReadStrategy_FallbackWhenAbsent(t *testing.T) {
 
 	// THEN
 	expectedShardToNode := map[string]string{
-		"S1": "", // Could be A or B
-		"S2": "", // Could be C or D
+		"S1": "A", // Should pick first available (A)
+		"S2": "C", // Should pick first available (C)
 	}
 
-	assertReadStrategyResult(t, rs, actual, expectedShardToNode)
+	assertReadStrategyResult(t, actual, expectedShardToNode)
 }
 
 func TestDirectCandidateReadStrategy_SingleReplica(t *testing.T) {
@@ -106,7 +106,7 @@ func TestDirectCandidateReadStrategy_SingleReplica(t *testing.T) {
 		"S1": "Solo",
 	}
 
-	assertReadStrategyResult(t, rs, actual, expectedShardToNode)
+	assertReadStrategyResult(t, actual, expectedShardToNode)
 }
 
 func TestDirectCandidateReadStrategy_EmptyReplicaSet(t *testing.T) {
@@ -120,11 +120,11 @@ func TestDirectCandidateReadStrategy_EmptyReplicaSet(t *testing.T) {
 	actual := strategy.Apply(rs, options)
 
 	// THEN
-	assertReadStrategyResult(t, rs, actual, map[string]string{})
+	assertReadStrategyResult(t, actual, map[string]string{})
 }
 
 func TestDirectCandidateReadStrategy_SingleReplicaPerShard(t *testing.T) {
-	// GIVEN - one replica per shard
+	// GIVEN
 	rs := types.ReadReplicaSet{Replicas: []types.Replica{
 		{NodeName: "A", ShardName: "S1", HostAddr: "10.12.135.19"},
 		{NodeName: "C", ShardName: "S2", HostAddr: "10.12.135.21"},
@@ -142,7 +142,113 @@ func TestDirectCandidateReadStrategy_SingleReplicaPerShard(t *testing.T) {
 		"S2": "C", // Only C available for S2
 	}
 
-	assertReadStrategyResult(t, rs, actual, expectedShardToNode)
+	assertReadStrategyResult(t, actual, expectedShardToNode)
+}
+
+func TestDirectCandidateReadStrategy_OptionsOverrideStrategyPreference(t *testing.T) {
+	// GIVEN
+	dc := types.NewDirectCandidate("B", "")
+	strategy := types.NewDirectCandidateReadStrategy(dc)
+	rs := testReadReplicaSet()
+	options := types.RoutingPlanBuildOptions{
+		DirectCandidateNode: "A",
+	}
+
+	// WHEN
+	actual := strategy.Apply(rs, options)
+
+	// THEN
+	expectedShardToNode := map[string]string{
+		"S1": "A", // Should prefer A from options over B from strategy
+		"S2": "C", // A not available in S2, should pick first available
+	}
+
+	assertReadStrategyResult(t, actual, expectedShardToNode)
+}
+
+func TestDirectCandidateReadStrategy_OptionsOverrideFallback(t *testing.T) {
+	// GIVEN
+	dc := types.NewDirectCandidate("", "D")
+	strategy := types.NewDirectCandidateReadStrategy(dc)
+	rs := testReadReplicaSet()
+	options := types.RoutingPlanBuildOptions{
+		DirectCandidateNode: "C",
+	}
+
+	// WHEN
+	actual := strategy.Apply(rs, options)
+
+	// THEN
+	expectedShardToNode := map[string]string{
+		"S1": "A", // C not available in S1, should pick first available
+		"S2": "C", // Should prefer C from options over D from strategy
+	}
+
+	assertReadStrategyResult(t, actual, expectedShardToNode)
+}
+
+func TestDirectCandidateReadStrategy_EmptyOptionsUsesStrategyDefault(t *testing.T) {
+	// GIVEN
+	dc := types.NewDirectCandidate("B", "")
+	strategy := types.NewDirectCandidateReadStrategy(dc)
+	rs := testReadReplicaSet()
+	options := types.RoutingPlanBuildOptions{
+		DirectCandidateNode: "",
+	}
+
+	// WHEN
+	actual := strategy.Apply(rs, options)
+
+	// THEN
+	expectedShardToNode := map[string]string{
+		"S1": "B", // Should use strategy default
+		"S2": "C", // B not available in S2, should pick first available
+	}
+
+	assertReadStrategyResult(t, actual, expectedShardToNode)
+}
+
+func TestDirectCandidateReadStrategy_OptionsOverrideToNonExistentNode(t *testing.T) {
+	// GIVEN
+	dc := types.NewDirectCandidate("B", "")
+	strategy := types.NewDirectCandidateReadStrategy(dc)
+	rs := testReadReplicaSet()
+	options := types.RoutingPlanBuildOptions{
+		DirectCandidateNode: "NonExistent",
+	}
+
+	// WHEN
+	actual := strategy.Apply(rs, options)
+
+	// THEN
+	expectedShardToNode := map[string]string{
+		"S1": "A", // NonExistent not available, should pick first available
+		"S2": "C", // NonExistent not available, should pick first available
+	}
+
+	assertReadStrategyResult(t, actual, expectedShardToNode)
+}
+
+func assertReadStrategyResult(t *testing.T, actual types.ReadReplicaSet, expectedShardToNode map[string]string) {
+	actualShards := make(map[string][]types.Replica)
+	for _, replica := range actual.Replicas {
+		actualShards[replica.ShardName] = append(actualShards[replica.ShardName], replica)
+	}
+
+	require.Len(t, actualShards, len(expectedShardToNode), "should have exactly the expected number of shards")
+
+	for shard, expectedNode := range expectedShardToNode {
+		actualReplicasForShard, exists := actualShards[shard]
+		require.True(t, exists, fmt.Sprintf("should have replicas for shard %s", shard))
+		require.Len(t, actualReplicasForShard, 1,
+			fmt.Sprintf("should have exactly one replica for shard %s, got %d", shard, len(actualReplicasForShard)))
+		require.Equal(t, expectedNode, actualReplicasForShard[0].NodeName,
+			fmt.Sprintf("for shard %s, expected node %s but got %s", shard, expectedNode, actualReplicasForShard[0].NodeName))
+	}
+
+	expectedTotalReplicas := len(expectedShardToNode)
+	require.Len(t, actual.Replicas, expectedTotalReplicas,
+		fmt.Sprintf("should have exactly %d total replicas (one per shard), got %d", expectedTotalReplicas, len(actual.Replicas)))
 }
 
 func TestNewDirectCandidate_WithDirectCandidate(t *testing.T) {
@@ -179,116 +285,4 @@ func TestNewDirectCandidate_BothEmpty(t *testing.T) {
 
 	// THEN
 	require.Equal(t, "", dc.PreferredNodeName)
-}
-
-func assertReadStrategyResult(t *testing.T, original types.ReadReplicaSet, actual types.ReadReplicaSet, expectedShardToNode map[string]string) {
-	originalShards := make(map[string][]types.Replica)
-	for _, replica := range original.Replicas {
-		originalShards[replica.ShardName] = append(originalShards[replica.ShardName], replica)
-	}
-
-	actualShards := make(map[string][]types.Replica)
-	for _, replica := range actual.Replicas {
-		actualShards[replica.ShardName] = append(actualShards[replica.ShardName], replica)
-	}
-
-	require.Len(t, actualShards, len(originalShards), "should have all shards represented")
-
-	for shard, expectedNode := range expectedShardToNode {
-		actualReplicasForShard, exists := actualShards[shard]
-		require.True(t, exists, fmt.Sprintf("should have replicas for shard %s", shard))
-
-		if expectedNode != "" {
-			require.Equal(t, expectedNode, actualReplicasForShard[0].NodeName,
-				fmt.Sprintf("for shard %s, expected preferred node %s to be first but got %s", shard, expectedNode, actualReplicasForShard[0].NodeName))
-		}
-
-		originalReplicasForShard := originalShards[shard]
-		require.Equal(t, len(originalReplicasForShard), len(actualReplicasForShard),
-			fmt.Sprintf("should return all replicas for shard %s", shard))
-	}
-}
-
-func TestDirectCandidateReadStrategy_OptionsOverrideStrategyPreference(t *testing.T) {
-	// GIVEN
-	dc := types.NewDirectCandidate("B", "")
-	strategy := types.NewDirectCandidateReadStrategy(dc)
-	rs := testReadReplicaSet()
-	options := types.RoutingPlanBuildOptions{
-		DirectCandidateNode: "A",
-	}
-
-	// WHEN
-	actual := strategy.Apply(rs, options)
-
-	// THEN
-	expectedShardToNode := map[string]string{
-		"S1": "A",
-		"S2": "",
-	}
-
-	assertReadStrategyResult(t, rs, actual, expectedShardToNode)
-}
-
-func TestDirectCandidateReadStrategy_OptionsOverrideFallback(t *testing.T) {
-	// GIVEN
-	dc := types.NewDirectCandidate("", "D")
-	strategy := types.NewDirectCandidateReadStrategy(dc)
-	rs := testReadReplicaSet()
-	options := types.RoutingPlanBuildOptions{
-		DirectCandidateNode: "C",
-	}
-
-	// WHEN
-	actual := strategy.Apply(rs, options)
-
-	// THEN
-	expectedShardToNode := map[string]string{
-		"S1": "",
-		"S2": "C",
-	}
-
-	assertReadStrategyResult(t, rs, actual, expectedShardToNode)
-}
-
-func TestDirectCandidateReadStrategy_EmptyOptionsUsesStrategyDefault(t *testing.T) {
-	// GIVEN
-	dc := types.NewDirectCandidate("B", "")
-	strategy := types.NewDirectCandidateReadStrategy(dc)
-	rs := testReadReplicaSet()
-	options := types.RoutingPlanBuildOptions{
-		DirectCandidateNode: "",
-	}
-
-	// WHEN
-	actual := strategy.Apply(rs, options)
-
-	// THEN
-	expectedShardToNode := map[string]string{
-		"S1": "B",
-		"S2": "",
-	}
-
-	assertReadStrategyResult(t, rs, actual, expectedShardToNode)
-}
-
-func TestDirectCandidateReadStrategy_OptionsOverrideToNonExistentNode(t *testing.T) {
-	// GIVEN
-	dc := types.NewDirectCandidate("B", "")
-	strategy := types.NewDirectCandidateReadStrategy(dc)
-	rs := testReadReplicaSet()
-	options := types.RoutingPlanBuildOptions{
-		DirectCandidateNode: "NonExistent",
-	}
-
-	// WHEN
-	actual := strategy.Apply(rs, options)
-
-	// THEN
-	expectedShardToNode := map[string]string{
-		"S1": "", // Could be A or B
-		"S2": "", // Could be C or D
-	}
-
-	assertReadStrategyResult(t, rs, actual, expectedShardToNode)
 }
