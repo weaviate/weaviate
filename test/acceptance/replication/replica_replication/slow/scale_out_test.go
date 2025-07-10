c879848c5e25582c9ddd36dd7423d8d1f55d889b
Tommy Smith <tommy@weaviate.io>
2025-07-10 23:58:24 +0100

    Reduce minimum async wait time in tests without mutating data

--- test/acceptance/replication/replica_replication/slow/scale_out_test.go
+++ test/acceptance/replication/replica_replication/slow/scale_out_test.go
@@ -41,6 +41,7 @@ func (suite *ReplicationTestSuite) TestReplicationReplicateScaleOut() {
 	compose, err := docker.New().
 		WithWeaviateCluster(3).
 		WithWeaviateEnv("REPLICATION_ENGINE_MAX_WORKERS", "10").
+		WithWeaviateEnv("REPLICA_MOVEMENT_MINIMUM_ASYNC_WAIT", "5s").
 		Start(mainCtx)
 	require.Nil(t, err)
 	defer func() {
