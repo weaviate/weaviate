86a756e977fdee5fd1528d425e66987bd32703a3
Tommy Smith <tommy@weaviate.io>
2025-07-11 15:41:03 +0100

    Increase async wait time again

--- test/acceptance/replication/replica_replication/long/mutating_data_test.go
+++ test/acceptance/replication/replica_replication/long/mutating_data_test.go
@@ -53,7 +53,7 @@ func (suite *ReplicationTestSuite) TestReplicationReplicateWhileMutatingData() {
 
 	compose, err := docker.New().
 		WithWeaviateCluster(3).
-		WithWeaviateEnv("REPLICA_MOVEMENT_MINIMUM_ASYNC_WAIT", "30s").
+		WithWeaviateEnv("REPLICA_MOVEMENT_MINIMUM_ASYNC_WAIT", "40s").
 		Start(mainCtx)
 	require.Nil(t, err)
 	defer func() {
