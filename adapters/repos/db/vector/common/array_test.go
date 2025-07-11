4ca793b892a97dbd290988b01f6c192bc6789f5a
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2025-07-11 18:01:14 +0800

    rename to array

--- adapters/repos/db/vector/common/array_test.go
+++ adapters/repos/db/vector/common/array_test.go
@@ -18,104 +18,19 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestPagedCache(t *testing.T) {
-	cache := NewPagedCacheWith[int](10, 2)
-	require.Len(t, cache.cache, 2, "wrong initial number of pages")
+func TestPagedArray(t *testing.T) {
+	arr := NewPagedArrayWith[int](10, 2)
+	require.Len(t, arr.array, 2, "wrong initial number of pages")
 
 	setN := func(n int) {
 		for i := 0; i < n; i++ {
-			cache.Set(i, &i)
+			arr.Set(i, i)
 		}
 	}
 
 	checkN := func(n int) {
 		for i := 0; i < n; i++ {
-			v := cache.Get(i)
-			if *v != i {
-				t.Errorf("expected %d, got %d", i, *v)
-			}
-		}
-	}
-
-	setN(10)
-	checkN(10)
-
-	setN(1000)
-	checkN(1000)
-
-	cache.Reset()
-
-	setN(1000)
-	checkN(1000)
-
-	cache.Reset()
-
-	setN(100)
-	require.Equal(t, 10, *cache.Get(10))
-	require.Nil(t, cache.Get(140))
-
-	cache.Reset()
-	for i := 0; i < 100; i += 2 {
-		cache.Set(i, &i)
-	}
-	for i := 0; i < 100; i += 2 {
-		require.Equal(t, i, *cache.Get(i))
-	}
-	for i := 1; i < 100; i += 2 {
-		require.Nil(t, cache.Get(i))
-	}
-}
-
-func BenchmarkPagedCache(b *testing.B) {
-	pageSize := 512
-	keys := make([]int, 10000)
-	values := make([]int, 10000)
-	for i := range 10000 {
-		keys[i] = int(rand.Int31n(500_000_000))
-		values[i] = rand.Int()
-	}
-
-	b.Run("PagedCache/Set", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			b.StopTimer()
-			cache := NewPagedCacheWith[int](pageSize, 10)
-			b.StartTimer()
-
-			for j := 0; j < 1000; j++ {
-				cache.Set(keys[j], &values[j])
-			}
-		}
-	})
-
-	cache := NewPagedCacheWith[int](pageSize, 10)
-
-	for j := 0; j < 10000; j++ {
-		cache.Set(keys[j], &values[j])
-	}
-
-	b.Run("PagedCache/Get", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			for j := 0; j < 10000; j++ {
-				_ = cache.Get(keys[j])
-			}
-		}
-	})
-}
-
-func TestFlatCache(t *testing.T) {
-	cache := NewFlatCache[int](10)
-
-	require.Len(t, cache.cache, 10, "wrong initial size of cache")
-
-	setN := func(n int) {
-		for i := 0; i < n; i++ {
-			cache.Set(i, i)
-		}
-	}
-
-	checkN := func(n int) {
-		for i := 0; i < n; i++ {
-			v := cache.Get(i)
+			v := arr.Get(i)
 			if v != i {
 				t.Errorf("expected %d, got %d", i, v)
 			}
@@ -128,30 +43,31 @@ func TestFlatCache(t *testing.T) {
 	setN(1000)
 	checkN(1000)
 
-	cache.Reset()
+	arr.Reset()
 
 	setN(1000)
 	checkN(1000)
 
-	cache.Reset()
+	arr.Reset()
 
 	setN(100)
-	require.Equal(t, 10, cache.Get(10))
-	require.Zero(t, cache.Get(140))
+	require.Equal(t, 10, arr.Get(10))
+	require.Nil(t, arr.Get(140))
 
-	cache.Reset()
+	arr.Reset()
 	for i := 0; i < 100; i += 2 {
-		cache.Set(i, i)
+		arr.Set(i, i)
 	}
 	for i := 0; i < 100; i += 2 {
-		require.Equal(t, i, cache.Get(i))
+		require.Equal(t, i, arr.Get(i))
 	}
 	for i := 1; i < 100; i += 2 {
-		require.Zero(t, cache.Get(i))
+		require.Nil(t, arr.Get(i))
 	}
 }
 
-func BenchmarkFlatCache(b *testing.B) {
+func BenchmarkPagedArray(b *testing.B) {
+	pageSize := 512
 	keys := make([]int, 10000)
 	values := make([]int, 10000)
 	for i := range 10000 {
@@ -159,28 +75,112 @@ func BenchmarkFlatCache(b *testing.B) {
 		values[i] = rand.Int()
 	}
 
-	b.Run("FlatCache/Set", func(b *testing.B) {
+	b.Run("PagedArray/Set", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			b.StopTimer()
-			cache := NewFlatCache[int](10)
+			arr := NewPagedArrayWith[int](pageSize, 10)
 			b.StartTimer()
 
 			for j := 0; j < 1000; j++ {
-				cache.Set(keys[j], values[j])
+				arr.Set(keys[j], values[j])
 			}
 		}
 	})
 
-	cache := NewFlatCache[int](10)
+	arr := NewPagedArrayWith[int](pageSize, 10)
 
 	for j := 0; j < 10000; j++ {
-		cache.Set(keys[j], values[j])
+		arr.Set(keys[j], values[j])
 	}
 
-	b.Run("FlatCache/Get", func(b *testing.B) {
+	b.Run("PagedArray/Get", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			for j := 0; j < 10000; j++ {
-				_ = cache.Get(keys[j])
+				_ = arr.Get(keys[j])
+			}
+		}
+	})
+}
+
+func TestFlatArray(t *testing.T) {
+	arr := NewFlatArray[int](10)
+
+	require.Len(t, arr.array, 10, "wrong initial size of arr")
+
+	setN := func(n int) {
+		for i := 0; i < n; i++ {
+			arr.Set(i, i)
+		}
+	}
+
+	checkN := func(n int) {
+		for i := 0; i < n; i++ {
+			v := arr.Get(i)
+			if v != i {
+				t.Errorf("expected %d, got %d", i, v)
+			}
+		}
+	}
+
+	setN(10)
+	checkN(10)
+
+	setN(1000)
+	checkN(1000)
+
+	arr.Reset()
+
+	setN(1000)
+	checkN(1000)
+
+	arr.Reset()
+
+	setN(100)
+	require.Equal(t, 10, arr.Get(10))
+	require.Zero(t, arr.Get(140))
+
+	arr.Reset()
+	for i := 0; i < 100; i += 2 {
+		arr.Set(i, i)
+	}
+	for i := 0; i < 100; i += 2 {
+		require.Equal(t, i, arr.Get(i))
+	}
+	for i := 1; i < 100; i += 2 {
+		require.Zero(t, arr.Get(i))
+	}
+}
+
+func BenchmarkFlatArray(b *testing.B) {
+	keys := make([]int, 10000)
+	values := make([]int, 10000)
+	for i := range 10000 {
+		keys[i] = int(rand.Int31n(500_000_000))
+		values[i] = rand.Int()
+	}
+
+	b.Run("FlatArray/Set", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			b.StopTimer()
+			arr := NewFlatArray[int](10)
+			b.StartTimer()
+
+			for j := 0; j < 1000; j++ {
+				arr.Set(keys[j], values[j])
+			}
+		}
+	})
+
+	arr := NewFlatArray[int](10)
+
+	for j := 0; j < 10000; j++ {
+		arr.Set(keys[j], values[j])
+	}
+
+	b.Run("FlatArray/Get", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			for j := 0; j < 10000; j++ {
+				_ = arr.Get(keys[j])
 			}
 		}
 	})
