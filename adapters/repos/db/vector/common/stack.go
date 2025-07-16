bc138ea6d63c32039a2ef11f371cee0295c9d63c
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2025-07-16 11:18:53 +0800

    add common.Stack and common.Pool

---
+++ adapters/repos/db/vector/common/stack.go
@@ -0,0 +1,82 @@
+package common
+
+import (
+	"math/rand/v2"
+	"sync/atomic"
+)
+
+type stackNode[T any] struct {
+	value T
+	next  *stackNode[T]
+}
+
+// Stack is a lock-free stack implementation using atomic operations.
+// It allows concurrent Push and Pop operations without locks.
+type Stack[T any] struct {
+	head atomic.Pointer[stackNode[T]]
+}
+
+func (s *Stack[T]) Push(value T) {
+	newNode := &stackNode[T]{value: value}
+	for {
+		oldHead := s.head.Load()
+		newNode.next = oldHead
+		if s.head.CompareAndSwap(oldHead, newNode) {
+			return
+		}
+	}
+}
+
+func (s *Stack[T]) Pop() (value T, ok bool) {
+	for {
+		oldHead := s.head.Load()
+		if oldHead == nil {
+			var zero T
+			return zero, false
+		}
+		newHead := oldHead.next
+		if s.head.CompareAndSwap(oldHead, newHead) {
+			return oldHead.value, true
+		}
+	}
+}
+
+// must be a power of two for the randomness to work correctly
+const numShards = 32
+
+// Pool is a sharded, lock-free freelist.
+type Pool[T any] struct {
+	shards [numShards]Stack[T]
+}
+
+func (p *Pool[T]) getShard() *Stack[T] {
+	shard := rand.Uint64() & (numShards - 1)
+	return &p.shards[shard]
+}
+
+// Put adds an object to a randomly selected shard.
+func (p *Pool[T]) Put(value T) {
+	p.getShard().Push(value)
+}
+
+// Get retrieves an object from a randomly selected shard.
+// It tries multiple shards if needed.
+func (p *Pool[T]) Get() (T, bool) {
+	// Start from a random shard
+	start := p.getShard()
+	if v, ok := start.Pop(); ok {
+		return v, true
+	}
+
+	// Try other shards sequentially
+	for i := range p.shards {
+		if &p.shards[i] == start {
+			continue
+		}
+		if v, ok := p.shards[i].Pop(); ok {
+			return v, true
+		}
+	}
+	var zero T
+	return zero, false
+}
