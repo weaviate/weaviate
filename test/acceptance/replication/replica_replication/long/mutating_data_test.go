dbbc220d111dabb03754c5ea55dfd765ceb94ffb
Nate Wilkinson <nathan@weaviate.io>
2025-07-17 11:36:58 -0600

    trigger move copy test for rf=2

--- test/acceptance/replication/replica_replication/long/mutating_data_test.go
+++ test/acceptance/replication/replica_replication/long/mutating_data_test.go
@@ -67,24 +67,8 @@ func (suite *ReplicationTestSuite) TestReplicationReplicateWhileMutatingData() {
 	}()
 
 	move := "MOVE"
-	// copy := "COPY"
+	copy := "COPY"
 
-	// t.Run("COPY, rf=2, no automated resolution", func(t *testing.T) {
-	// 	test(t, compose, copy, 2, models.ReplicationConfigDeletionStrategyNoAutomatedResolution)
-	// })
-	// t.Run("MOVE, rf=1, no automated resolution", func(t *testing.T) {
-	// 	test(t, compose, move, 1, models.ReplicationConfigDeletionStrategyNoAutomatedResolution)
-	// })
-	// t.Run("COPY, rf=1, no automated resolution", func(t *testing.T) {
-	// 	test(t, compose, copy, 1, models.ReplicationConfigDeletionStrategyNoAutomatedResolution)
-	// })
-
-	t.Run("MOVE, rf=2, delete on conflict", func(t *testing.T) {
-		test(t, compose, move, 2, models.ReplicationConfigDeletionStrategyDeleteOnConflict)
-	})
-	t.Run("MOVE, rf=2, time-based resolution", func(t *testing.T) {
-		test(t, compose, move, 2, models.ReplicationConfigDeletionStrategyTimeBasedResolution)
-	})
 	t.Run("MOVE, rf=2, delete on conflict", func(t *testing.T) {
 		test(t, compose, move, 2, models.ReplicationConfigDeletionStrategyDeleteOnConflict)
 	})
@@ -93,6 +77,15 @@ func (suite *ReplicationTestSuite) TestReplicationReplicateWhileMutatingData() {
 	})
 	t.Run("MOVE, rf=2, no automated resolution", func(t *testing.T) {
 		test(t, compose, move, 2, models.ReplicationConfigDeletionStrategyNoAutomatedResolution)
+	})
+	t.Run("COPY, rf=2, delete on conflict", func(t *testing.T) {
+		test(t, compose, copy, 2, models.ReplicationConfigDeletionStrategyDeleteOnConflict)
+	})
+	t.Run("COPY, rf=2, time-based resolution", func(t *testing.T) {
+		test(t, compose, copy, 2, models.ReplicationConfigDeletionStrategyTimeBasedResolution)
+	})
+	t.Run("COPY, rf=2, no automated resolution", func(t *testing.T) {
+		test(t, compose, copy, 2, models.ReplicationConfigDeletionStrategyNoAutomatedResolution)
 	})
 }
 
