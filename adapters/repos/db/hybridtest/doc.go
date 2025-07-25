351f42e4ed830d1152de226c59110efb96c25f71
Marcin Antas <antas.marcin@gmail.com>
2025-07-25 08:46:30 +0200

    add doc.go file to hybridtest package

---
+++ adapters/repos/db/hybridtest/doc.go
@@ -0,0 +1,13 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2024 Weaviate B.V. All rights reserved.
+//
+//  CONTACT: hello@weaviate.io
+//
+
+// hybridtest acts as a test package for hybrid related tests
+package hybridtest
