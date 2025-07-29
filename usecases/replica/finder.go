023924d0866c8c06b61fae7ca98949a79a8d4577
salvatorecampagna <salvatore@weaviate.io>
2025-07-29 09:40:25 +0200

    fix: replace Shards with Replicas

--- usecases/replica/finder.go
+++ usecases/replica/finder.go
@@ -386,7 +386,7 @@ func (f *Finder) CollectShardDifferences(ctx context.Context,
 		}
 	}
 
-	replicaNodeNames := make([]string, 0, len(routingPlan.Shards()))
+	replicaNodeNames := make([]string, 0, len(routingPlan.Replicas()))
 	replicasHostAddrs := make([]string, 0, len(routingPlan.HostAddresses()))
 	for _, replica := range targetNodesToUse {
 		replicaNodeNames = append(replicaNodeNames, replica)
