93d46ac78e8feee02332235a18e0105e12828186
Mohamed Badawi <contact@m-mooga.com>
2025-08-04 14:25:39 +0200

    chore(log): change hashbeat iteration log level to debug (#8805)

--- adapters/repos/db/shard_async_replication.go
+++ adapters/repos/db/shard_async_replication.go
@@ -29,7 +29,7 @@ import (
 	"github.com/go-openapi/strfmt"
 	"github.com/google/uuid"
 	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
+
 	"github.com/weaviate/weaviate/adapters/repos/db/helpers"
 	"github.com/weaviate/weaviate/adapters/repos/db/lsmkv"
 	"github.com/weaviate/weaviate/cluster/router/types"
