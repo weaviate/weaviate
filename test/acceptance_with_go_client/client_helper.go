430ac7f67725a5ce99a2cc6095dfa0444e52c578
Dirk Kulawiak <dirk@semi.technology>
2025-08-26 20:00:23 -0700

    Add test for unset vs empty

--- test/acceptance_with_go_client/client_helper.go
+++ test/acceptance_with_go_client/client_helper.go
@@ -1,1 +1,20 @@
 package acceptance_with_go_client
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	client "github.com/weaviate/weaviate-go-client/v5/weaviate"
+)
+
+func createClientWithClassName(t *testing.T) (*client.Client, string) {
+	c, err := client.NewClient(client.Config{Scheme: "http", Host: "localhost:8080"})
+	require.Nil(t, err)
+
+	ctx := context.Background()
+	className := t.Name()
+	require.NoError(t, c.Schema().ClassDeleter().WithClassName(className).Do(ctx))
+
+	return c, className
+}
--- test/acceptance_with_go_client/iterator_test.go
+++ test/acceptance_with_go_client/iterator_test.go
@@ -23,6 +23,50 @@ import (
 	"github.com/weaviate/weaviate/entities/schema"
 )
 
+func TestAfterUnsetVsEmpty(t *testing.T) {
+	c, className := createClientWithClassName(t)
+
+	class := models.Class{
+		Class: className,
+		Properties: []*models.Property{
+			{Name: "bool", DataType: []string{string(schema.DataTypeBoolean)}},
+			{Name: "counter", DataType: []string{string(schema.DataTypeInt)}},
+		},
+		Vectorizer: "none",
+	}
+	require.NoError(t, c.Schema().ClassCreator().WithClass(&class).Do(ctx))
+
+	numObjs := 100
+	for i := 0; i < numObjs; i++ {
+		_, err := c.Data().Creator().WithClassName(className).WithProperties(
+			map[string]interface{}{"counter": i, "bool": i%2 == 0},
+		).Do(ctx)
+		require.NoError(t, err)
+	}
+
+	getExplicitEmpty, err := c.GraphQL().Get().WithClassName(className).WithLimit(10).WithFields(
+		graphql.Field{
+			Name: "_additional", Fields: []graphql.Field{{Name: "id"}},
+		},
+		graphql.Field{Name: "counter"},
+	).WithAfter("").Do(ctx)
+	require.NoError(t, err)
+	require.Nil(t, getExplicitEmpty.Errors)
+	objExplicitEmpty := getExplicitEmpty.Data["Get"].(map[string]interface{})[className].([]interface{})
+
+	getUnset, err := c.GraphQL().Get().WithClassName(className).WithLimit(10).WithFields(
+		graphql.Field{
+			Name: "_additional", Fields: []graphql.Field{{Name: "id"}},
+		},
+		graphql.Field{Name: "counter"},
+	).Do(ctx)
+	require.NoError(t, err)
+	require.Nil(t, getUnset.Errors)
+	objUnset := getUnset.Data["Get"].(map[string]interface{})[className].([]interface{})
+
+	require.Equal(t, objExplicitEmpty, objUnset)
+}
+
 func TestIteratorWithFilter(t *testing.T) {
 	ctx := context.Background()
 	c, err := client.NewClient(client.Config{Scheme: "http", Host: "localhost:8080"})
