436abd0355b5c0b27d6f8cdc35a8874863db5561
moogacs <contact@m-mooga.com>
2025-09-02 22:03:10 +0200

    remove unnecessary locks to memberlist

--- usecases/cluster/state.go
+++ usecases/cluster/state.go
@@ -288,6 +288,11 @@ func (s *State) NodeHostname(nodeName string) (string, bool) {
 // NodeAddress is used to resolve the node name into an ip address without the port
 // TODO-RAFT-DB-63 : shall be replaced by Members() which returns members in the list
 func (s *State) NodeAddress(id string) string {
+	for _, mem := range s.list.Members() {
+		if mem.Name == id {
+			return mem.Addr.String()
+		}
+	}
 	// network interruption detection which can cause a single node to be isolated from the cluster (split brain)
 	nodeCount := len(s.list.Members())
 	var joinAddr []string
@@ -308,7 +313,8 @@ func (s *State) NodeAddress(id string) string {
 			}).WithError(err).Error("memberlist rejoin not successful")
 		} else {
 			s.delegate.log.WithFields(logrus.Fields{
-				"action": "memberlist_rejoin",
+				"action":     "memberlist_rejoin",
+				"node_count": s.list.NumMembers(),
 			}).Info("Successfully rejoined the memberlist cluster")
 		}
 	}
