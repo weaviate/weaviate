3e6e36734013b6326fcfcd4a4a214c47196de4b4
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2025-09-03 15:25:33 +0800

    add virtual fs

---
+++ adapters/repos/db/vector/common/fs.go
@@ -0,0 +1,73 @@
+package common
+
+import (
+	"io"
+	"os"
+)
+
+var _ File = (*os.File)(nil)
+var _ FS = (*osFS)(nil)
+
+type File interface {
+	io.Reader
+	io.Writer
+	io.Closer
+	io.ReaderAt
+	io.Seeker
+	Sync() error
+	Stat() (os.FileInfo, error)
+}
+
+type FS interface {
+	MkdirAll(path string, perm os.FileMode) error
+	OpenFile(name string, flag int, perm os.FileMode) (File, error)
+	ReadDir(name string) ([]os.DirEntry, error)
+	Stat(name string) (os.FileInfo, error)
+	Remove(name string) error
+	RemoveAll(path string) error
+	Create(name string) (File, error)
+	Open(name string) (File, error)
+	Rename(oldpath, newpath string) error
+}
+
+type osFS struct{}
+
+func NewOSFS() FS {
+	return &osFS{}
+}
+
+func (fs *osFS) MkdirAll(path string, perm os.FileMode) error {
+	return os.MkdirAll(path, perm)
+}
+
+func (fs *osFS) OpenFile(name string, flag int, perm os.FileMode) (File, error) {
+	return os.OpenFile(name, flag, perm)
+}
+
+func (fs *osFS) ReadDir(name string) ([]os.DirEntry, error) {
+	return os.ReadDir(name)
+}
+
+func (fs *osFS) Stat(name string) (os.FileInfo, error) {
+	return os.Stat(name)
+}
+
+func (fs *osFS) Remove(name string) error {
+	return os.Remove(name)
+}
+
+func (fs *osFS) RemoveAll(path string) error {
+	return os.RemoveAll(path)
+}
+
+func (fs *osFS) Create(name string) (File, error) {
+	return os.Create(name)
+}
+
+func (fs *osFS) Open(name string) (File, error) {
+	return os.Open(name)
+}
+
+func (fs *osFS) Rename(oldpath, newpath string) error {
+	return os.Rename(oldpath, newpath)
+}
