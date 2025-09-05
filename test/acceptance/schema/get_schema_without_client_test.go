1a682a958f0a4ba54c5494e4ddce590b2505b2d7
Dirk Kulawiak <dirk@semi.technology>
2025-09-05 15:07:28 +0200

    Change value in another test

--- test/acceptance/schema/get_schema_without_client_test.go
+++ test/acceptance/schema/get_schema_without_client_test.go
@@ -100,7 +100,7 @@ func testGetSchemaWithoutClient(t *testing.T) {
 					"virtualPerPhysical":  float64(128),
 				},
 				"replicationConfig": map[string]interface{}{
-					"asyncEnabled":     false,
+					"asyncEnabled":     true,
 					"factor":           float64(1),
 					"deletionStrategy": "NoAutomatedResolution",
 				},
