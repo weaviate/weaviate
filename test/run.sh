80da7f69c92cd8e723d2b10ca5ac1c85a601a76b
salvatorecampagna <salvatore@weaviate.io>
2025-09-12 19:00:57 +0200

    fix: use a test image

--- test/run.sh
+++ test/run.sh
@@ -610,6 +610,7 @@ function run_acceptance_go_client_named_vectors_cluster() {
 }
 
 function run_acceptance_graphql_tests() {
+  export TEST_WEAVIATE_IMAGE=weaviate/test-server
   for pkg in $(go list ./... | grep 'test/acceptance/graphql_resolvers'); do
     if ! go test -timeout=15m -count 1 -race "$pkg"; then
       echo "Test for $pkg failed" >&2
