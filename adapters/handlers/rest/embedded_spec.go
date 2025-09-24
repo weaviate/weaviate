111fe53892eac468b94fe9e234342e97f0e476ea
Parker Duckworth <parkerduckworth@gmail.com>
2025-09-24 14:05:18 -0400

    prepare release v1.33.0

--- adapters/handlers/rest/embedded_spec.go
+++ adapters/handlers/rest/embedded_spec.go
@@ -48,7 +48,7 @@ func init() {
       "url": "https://github.com/weaviate",
       "email": "hello@weaviate.io"
     },
-    "version": "1.33.0-rc.1"
+    "version": "1.33.0"
   },
   "basePath": "/v1",
   "paths": {
@@ -8914,7 +8914,7 @@ func init() {
       "url": "https://github.com/weaviate",
       "email": "hello@weaviate.io"
     },
-    "version": "1.33.0-rc.1"
+    "version": "1.33.0"
   },
   "basePath": "/v1",
   "paths": {
