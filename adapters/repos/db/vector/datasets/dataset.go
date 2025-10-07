8d525db004ea7725bb9189bef71d6342ba2c0a58
Tobias Christiani <tobias@weaviate.io>
2025-10-07 03:21:28 +0200

    Support for reading float32 vectors from parquet. (#9388)
    This patch modifies how the training data vectors are read. Instead of
    casting byte arrays we read the parquet values stored in the list
    representation from the parquet column and use the repetition level to
    build our collection of vectors. Currently we only support float32
    vectors but this approach can be extended to other Parquet by reading
    their value from the parquet.
    
    It seems that this is slightly less performant than casting byte arrays.
    This patch includes a benchmark that shows that reading ~54K 768-dim
    vectors takes approximately 275ms. The benefits of this format is that
    it makes the datasets integrate well with the huggingface ecosystem.
    I.e. datasets can be downloaded, processed, uploaded, and viewed online
    without any special knowledge about the encoding.
    
    In the (relatively small) benchmark we use ~6.6 nanoseconds per float32
    value when reading training data. If we interpolate to 1536-dimensional
    vectors we can read approximately 100K vectors per second. This is at
    least an order of magnitude faster than our indexing time, so this
    overhead should be tolerable.
    
    In this iteration we load the train dataset by searching for a file
    named train.parquet in the huggingface repo and downloading it, and
    similarly for the test datast. This currently means we have one dataset
    per repo, and one file per configuration/split. We may need to update
    this to e.g. support sharding or multiple splits, but it is easier to
    see the best structure once we have produced more datasets.

--- adapters/repos/db/vector/datasets/dataset.go
+++ adapters/repos/db/vector/datasets/dataset.go
@@ -12,12 +12,11 @@
 package datasets
 
 import (
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"io"
-	"math"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/gomlx/go-huggingface/hub"
@@ -27,59 +26,112 @@ import (
 
 const defaultBatchSize = 1000
 
-func convertBinaryToFloat32(data []byte) ([]float32, error) {
-	if len(data)%4 != 0 {
-		return nil, fmt.Errorf("binary data length %d is not divisible by 4 (float32 size)", len(data))
-	}
-
-	result := make([]float32, len(data)/4)
-	for i := 0; i < len(result); i++ {
-		offset := i * 4
-		bits := binary.LittleEndian.Uint32(data[offset : offset+4])
-		result[i] = math.Float32frombits(bits)
-	}
-	return result, nil
-}
-
 type HubDataset struct {
-	repo      *hub.Repo
-	datasetID string
-	subset    string
-	logger    logrus.FieldLogger
+	repo    *hub.Repo
+	hubPath string // Path to dataset on the huggingface Hub, e.g. <user>/<dataset>
+	logger  logrus.FieldLogger
 }
 
-func NewHubDataset(datasetID string, subset string, logger logrus.FieldLogger) *HubDataset {
+func NewHubDataset(hubPath string, logger logrus.FieldLogger) *HubDataset {
 	hfAuthToken := os.Getenv("HF_TOKEN")
 	if hfAuthToken == "" {
 		logger.Warn("HF_TOKEN environment variable not set. Some datasets may require authentication.")
 	}
-	repo := hub.New(datasetID).
+	repo := hub.New(hubPath).
 		WithType(hub.RepoTypeDataset).
 		WithAuth(hfAuthToken)
 	return &HubDataset{
-		repo:      repo,
-		datasetID: datasetID,
-		subset:    subset,
-		logger:    logger,
+		repo:    repo,
+		hubPath: hubPath,
+		logger:  logger,
 	}
 }
 
 func (h *HubDataset) downloadParquetFile(name string) (string, error) {
-	filePath := fmt.Sprintf("%s/%s/%s.parquet", h.subset, name, name)
+	var downloadPath string
+	for fileName, err := range h.repo.IterFileNames() {
+		if err != nil {
+			h.logger.Errorf("Failed to list files in repo: %v", err)
+			return "", err
+		}
+		if strings.HasSuffix(fileName, name+".parquet") {
+			downloadPath = fileName
+		}
+	}
 
-	h.logger.Infof("Starting download of %s", filePath)
+	if downloadPath == "" {
+		return "", fmt.Errorf("failed to find file %s.parquet in repo", name)
+	}
+
+	h.logger.Infof("Starting download of %s", downloadPath)
 	startTime := time.Now()
 
-	localPath, err := h.repo.DownloadFile(filePath)
+	localPath, err := h.repo.DownloadFile(downloadPath)
 
 	duration := time.Since(startTime)
 	if err != nil {
-		h.logger.Errorf("Failed to download %s after %v: %v", filePath, duration, err)
+		h.logger.Errorf("Failed to download %s after %v: %v", downloadPath, duration, err)
 		return "", err
 	}
 
-	h.logger.Infof("Successfully downloaded %s in %v", filePath, duration)
+	h.logger.Infof("Successfully downloaded %s in %v", downloadPath, duration)
 	return localPath, nil
+}
+
+// TODO: Clean up this abstraction..
+type ParquetFile struct {
+	localPath     string
+	file          *os.File
+	parquetFile   *parquet.File
+	columnIndices map[string]int
+	numRows       int
+}
+
+func (p *ParquetFile) Close() {
+	p.file.Close()
+}
+
+func (h *HubDataset) downloadAndOpenParquetFile(name string) (*ParquetFile, error) {
+	localFile, err := h.downloadParquetFile(name)
+	if err != nil {
+		return nil, fmt.Errorf("failed to download file %s: %w", localFile, err)
+	}
+	// Open the parquet file
+	file, err := os.Open(localFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file %s: %w", localFile, err)
+	}
+	shouldClose := true
+	defer func() {
+		if shouldClose {
+			file.Close()
+		}
+	}()
+
+	// Get file size
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get file info for %s: %w", localFile, err)
+	}
+	fileSize := fileInfo.Size()
+
+	// Open as parquet file with optimizations
+	pqFile, err := parquet.OpenFile(file, fileSize,
+		parquet.SkipPageIndex(true),
+		parquet.SkipBloomFilters(true),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open parquet file %s: %w", localFile, err)
+	}
+	shouldClose = false
+	columnIndices, numRows := h.parquetMetadata(pqFile)
+	return &ParquetFile{
+		localPath:     localFile,
+		file:          file,
+		parquetFile:   pqFile,
+		columnIndices: columnIndices,
+		numRows:       numRows,
+	}, nil
 }
 
 func (h *HubDataset) parquetMetadata(pqFile *parquet.File) (map[string]int, int) {
@@ -107,237 +159,123 @@ func (h *HubDataset) validateColumns(columnIndices map[string]int, requiredColum
 	return nil
 }
 
-func (h *HubDataset) LoadTrainingData() (ids []uint64, vectors [][]float32, err error) {
-	localFile, err := h.downloadParquetFile("train")
+func (h *HubDataset) LoadTrainData() (ids []uint64, vectors [][]float32, err error) {
+	file, err := h.downloadAndOpenParquetFile("train")
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to download file %s: %w", localFile, err)
-	}
-	// Open the parquet file
-	file, err := os.Open(localFile)
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed to open file %s: %w", localFile, err)
+		return nil, nil, err
 	}
 	defer file.Close()
 
-	// Get file size
-	fileInfo, err := file.Stat()
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed to get file info for %s: %w", localFile, err)
-	}
-	fileSize := fileInfo.Size()
-
-	// Open as parquet file with optimizations
-	pqFile, err := parquet.OpenFile(file, fileSize,
-		parquet.SkipPageIndex(true),
-		parquet.SkipBloomFilters(true),
-	)
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed to open parquet file %s: %w", localFile, err)
-	}
-
-	columnIndices, numRows := h.parquetMetadata(pqFile)
-
 	// Validate required columns exist
-	if err := h.validateColumns(columnIndices, "embedding", "id"); err != nil {
+	if err := h.validateColumns(file.columnIndices, "id", "embedding"); err != nil {
 		return nil, nil, err
 	}
 
-	// Create reader for the parquet file
-	reader := parquet.NewReader(pqFile)
-	defer reader.Close()
+	embeddingColIdx := file.columnIndices["embedding"]
+	idColIdx := file.columnIndices["id"]
 
-	vectors = make([][]float32, 0, numRows)
-	ids = make([]uint64, 0, numRows)
+	ids = make([]uint64, 0, file.numRows)
+	vectorBuilder := NewVectorBuilderFloat32(file.numRows)
 
-	embeddingColIndex := columnIndices["embedding"]
-	idColIndex := columnIndices["id"]
-
-	batchSize := defaultBatchSize
-	totalRead := 0
-
-	// Allocate rows slice once outside the loop
-	rows := make([]parquet.Row, batchSize)
-
-	for totalRead < numRows {
-		remaining := numRows - totalRead
-		currentBatchSize := batchSize
-		if remaining < batchSize {
-			currentBatchSize = remaining
-		}
-		n, err := reader.ReadRows(rows[:currentBatchSize])
-		if err != nil && !errors.Is(err, io.EOF) {
-			return nil, nil, fmt.Errorf("failed to read rows: %w", err)
-		}
-
-		if n == 0 {
-			break
-		}
-
-		// Process batch using Row.Range for proper column handling
-		for i := 0; i < n; i++ {
-			row := rows[i]
-
-			// Use Row.Range to properly handle columns
-			row.Range(func(columnIndex int, columnValues []parquet.Value) bool {
-				switch columnIndex {
-				case idColIndex:
-					// Extract ID
-					if len(columnValues) > 0 && columnValues[0].Kind() == parquet.Int64 {
-						ids = append(ids, uint64(columnValues[0].Int64()))
-					}
-				case embeddingColIndex:
-					// Extract embedding
-					if len(columnValues) > 0 {
-						binaryData := columnValues[0].ByteArray()
-						if len(binaryData) > 0 {
-							convertedVector, err := convertBinaryToFloat32(binaryData)
-							if err != nil {
-								return false // Stop processing on error
-							}
-							vectors = append(vectors, convertedVector)
+	const defaultValueBufferSize = 1024
+	buffer := make([]parquet.Value, defaultValueBufferSize)
+	for _, rowGroup := range file.parquetFile.RowGroups() {
+		for _, columnChunk := range rowGroup.ColumnChunks() {
+			colIdx := columnChunk.Column()
+			if colIdx != idColIdx && colIdx != embeddingColIdx {
+				continue
+			}
+			valueReader := parquet.NewColumnChunkValueReader(columnChunk)
+			defer valueReader.Close()
+			for {
+				n, err := valueReader.ReadValues(buffer)
+				if err != nil && !errors.Is(err, io.EOF) {
+					return nil, nil, fmt.Errorf("failed to read rows: %w", err)
+				}
+				if n > 0 {
+					switch colIdx {
+					case idColIdx:
+						for i := range n {
+							ids = append(ids, uint64(buffer[i].Int64()))
+						}
+					case embeddingColIdx:
+						for i := range n {
+							vectorBuilder.Add(buffer[i])
 						}
 					}
 				}
-				return true // Continue processing
-			})
-		}
-
-		totalRead += n
-
-		if totalRead%10000 == 0 {
-			h.logger.Debugf("Processed %d rows", totalRead)
-		}
-
-		// Break if we got EOF or read fewer rows than expected
-		if err != nil && errors.Is(err, io.EOF) {
-			break
+				if errors.Is(err, io.EOF) {
+					break
+				}
+			}
 		}
 	}
 
-	h.logger.Infof("Loaded %d vectors", len(vectors))
-
-	if len(vectors) == 0 {
-		return nil, nil, fmt.Errorf("no vectors loaded - check parquet schema and data")
+	// Returns an error if we did not produce numRows vectors of the same length
+	vectors, err = vectorBuilder.AllVectors()
+	if err != nil {
+		return nil, nil, err
 	}
-
+	if len(ids) != file.numRows {
+		return nil, nil, fmt.Errorf("read %d ids from parquet file, expected %d", len(ids), file.numRows)
+	}
 	return ids, vectors, nil
 }
 
 func (h *HubDataset) LoadTestData() (neighbors [][]uint64, vectors [][]float32, err error) {
-	localFile, err := h.downloadParquetFile("test")
+	file, err := h.downloadAndOpenParquetFile("test")
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to download file %s: %w", localFile, err)
-	}
-	// Open the parquet file
-	file, err := os.Open(localFile)
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed to open file %s: %w", localFile, err)
+		return nil, nil, err
 	}
 	defer file.Close()
 
-	// Get file size
-	fileInfo, err := file.Stat()
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed to get file info for %s: %w", localFile, err)
-	}
-	fileSize := fileInfo.Size()
-
-	// Open as parquet file with optimizations
-	pqFile, err := parquet.OpenFile(file, fileSize,
-		parquet.SkipPageIndex(true),
-		parquet.SkipBloomFilters(true),
-	)
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed to open parquet file %s: %w", localFile, err)
-	}
-
-	columnIndices, numRows := h.parquetMetadata(pqFile)
-
 	// Validate required columns exist
-	if err := h.validateColumns(columnIndices, "embedding", "neighbors"); err != nil {
+	if err := h.validateColumns(file.columnIndices, "embedding", "neighbors"); err != nil {
 		return nil, nil, err
 	}
 
 	// Create reader for the parquet file
-	reader := parquet.NewReader(pqFile)
+	reader := parquet.NewReader(file.parquetFile)
 	defer reader.Close()
 
-	vectors = make([][]float32, 0, numRows)
-	neighbors = make([][]uint64, 0, numRows)
+	vectors = make([][]float32, 0, file.numRows)
+	neighbors = make([][]uint64, 0, file.numRows)
 
-	embeddingColIndex := columnIndices["embedding"]
-	neighborsColIndex := columnIndices["neighbors"]
+	embeddingColIndex := file.columnIndices["embedding"]
+	neighborsColIndex := file.columnIndices["neighbors"]
 
-	batchSize := defaultBatchSize
-	totalRead := 0
-
-	// Allocate rows slice once outside the loop
-	rows := make([]parquet.Row, batchSize)
-
-	for totalRead < numRows {
-		remaining := numRows - totalRead
-		currentBatchSize := batchSize
-		if remaining < batchSize {
-			currentBatchSize = remaining
-		}
-
-		n, err := reader.ReadRows(rows[:currentBatchSize])
+	rows := make([]parquet.Row, defaultBatchSize)
+	for {
+		n, err := reader.ReadRows(rows)
 		if err != nil && !errors.Is(err, io.EOF) {
 			return nil, nil, fmt.Errorf("failed to read rows: %w", err)
 		}
-
-		if n == 0 {
-			break
-		}
-
-		// Process batch using Row.Range for proper column handling
 		for i := 0; i < n; i++ {
 			row := rows[i]
-			var currentNeighbors []uint64
-
-			// Use Row.Range to properly handle columns
 			row.Range(func(columnIndex int, columnValues []parquet.Value) bool {
 				switch columnIndex {
 				case embeddingColIndex:
 					// Extract embedding
-					if len(columnValues) > 0 {
-						binaryData := columnValues[0].ByteArray()
-						if len(binaryData) > 0 {
-							convertedVector, err := convertBinaryToFloat32(binaryData)
-							if err != nil {
-								return false // Stop processing on error
-							}
-							vectors = append(vectors, convertedVector)
-						}
+					vector := make([]float32, len(columnValues))
+					for j, value := range columnValues {
+						vector[j] = value.Float()
 					}
+					vectors = append(vectors, vector)
 				case neighborsColIndex:
 					// Extract all neighbor values for this row
+					var currentNeighbors []uint64
 					for _, value := range columnValues {
-						if value.Kind() == parquet.Int64 {
-							currentNeighbors = append(currentNeighbors, uint64(value.Int64()))
-						}
+						currentNeighbors = append(currentNeighbors, uint64(value.Int64()))
 					}
+					neighbors = append(neighbors, currentNeighbors)
 				}
 				return true // Continue processing
 			})
-
-			// Add the neighbors for this row
-			neighbors = append(neighbors, currentNeighbors)
 		}
-
-		totalRead += n
-
-		if totalRead%10000 == 0 {
-			h.logger.Debugf("Processed %d rows", totalRead)
-		}
-
-		// Break if we got EOF or read fewer rows than expected
-		if err != nil && errors.Is(err, io.EOF) {
+		if errors.Is(err, io.EOF) {
 			break
 		}
 	}
-
-	h.logger.Infof("Loaded %d vectors", len(vectors))
 
 	if len(vectors) == 0 {
 		return nil, nil, fmt.Errorf("no vectors loaded - check parquet schema and data")
