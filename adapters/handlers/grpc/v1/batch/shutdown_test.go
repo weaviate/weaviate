1bd05b252b8c64db6e91a17344e529a6cce0d410
Tommy Smith <tommy@weaviate.io>
2025-10-08 11:32:37 +0100

    Add test verifying grace period shutdown of hanging clients

--- adapters/handlers/grpc/v1/batch/shutdown_test.go
+++ adapters/handlers/grpc/v1/batch/shutdown_test.go
@@ -154,6 +154,7 @@ func TestShutdownAfterBrokenStream(t *testing.T) {
 		return msg.GetError().GetError() == "some error" &&
 			msg.GetError().GetObject() != nil
 	})).Return(nil).Times(numErrs)
+
 	stream.EXPECT().Send(mock.MatchedBy(func(msg *pb.BatchStreamReply) bool {
 		return msg.GetBackoff() != nil
 	})).Return(nil).Maybe()
@@ -165,6 +166,82 @@ func TestShutdownAfterBrokenStream(t *testing.T) {
 	require.NotNil(t, err, "handler should return error shutting down")
 	require.ErrorAs(t, err, &networkErr, "handler should return network error")
 	shutdown.Drain(logger)
+}
+
+func TestShutdownWithHangingClient(t *testing.T) {
+	testDuration := 5 * time.Minute
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, testDuration)
+	defer cancel()
+
+	logger := logrus.New()
+
+	mockBatcher := mocks.NewMockBatcher(t)
+
+	howManyObjs := 5000
+	numErrs := howManyObjs / 10
+	mockBatcher.EXPECT().BatchObjects(mock.Anything, mock.Anything).RunAndReturn(func(ctx context.Context, req *pb.BatchObjectsRequest) (*pb.BatchObjectsReply, error) {
+		time.Sleep(100 * time.Millisecond)
+		errors := make([]*pb.BatchObjectsReply_BatchError, 0, numErrs)
+		for i := 0; i < numErrs; i++ {
+			errors = append(errors, &pb.BatchObjectsReply_BatchError{
+				Error: "some error",
+				Index: int32(i),
+			})
+		}
+		return &pb.BatchObjectsReply{
+			Took:   float32(1),
+			Errors: errors,
+		}, nil
+	}).Once()
+
+	objs := make([]*pb.BatchObject, 0, howManyObjs)
+	for i := 0; i < howManyObjs; i++ {
+		objs = append(objs, &pb.BatchObject{})
+	}
+
+	stream := newMockStream(ctx, t)
+	var count int
+	stream.EXPECT().Recv().RunAndReturn(func() (*pb.BatchStreamRequest, error) {
+		count++
+		switch count {
+		case 1:
+			return newBatchStreamStartRequest(), nil
+		case 2:
+			return newBatchStreamObjsRequest(objs), nil
+		case 3:
+			// simulate a client that does not close the stream correctly
+			time.Sleep(testDuration)
+			return nil, io.EOF
+		}
+		panic("should not be called more than thrice")
+	}).Times(3)
+
+	stream.EXPECT().Send(mock.MatchedBy(func(msg *pb.BatchStreamReply) bool {
+		return msg.GetError().GetError() == "some error" &&
+			msg.GetError().GetObject() != nil
+	})).Return(nil).Times(numErrs)
+	stream.EXPECT().Send(newBatchStreamShuttingDownReply()).Return(nil).Once()
+	stream.EXPECT().Send(mock.MatchedBy(func(msg *pb.BatchStreamReply) bool {
+		return msg.GetBackoff() != nil
+	})).Return(nil).Maybe()
+
+	numWorkers := 1
+	shutdown := batch.NewShutdown(ctx)
+	handler, _ := batch.Start(nil, nil, mockBatcher, nil, shutdown, numWorkers, logger)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		time.Sleep(100 * time.Millisecond)
+		shutdown.Drain(logger)
+	}()
+	err := handler.Handle(stream)
+	require.NotNil(t, err, "handler should return error shutting down")
+	require.ErrorAs(t, err, &context.Canceled, "handler should return context.Canceled error")
+	// Should take two minutes to exhaust the grace period before force shutting down the recv loop
+	shutdown.Drain(logger)
+	wg.Wait()
 }
 
 func newBatchStreamShuttingDownReply() *pb.BatchStreamReply {
