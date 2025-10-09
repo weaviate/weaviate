3836fe63b2d505d23f0a97e2100ed94121a0add6
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2025-10-09 16:00:00 +0530

    use correct boundary for mix endianness detection

--- adapters/repos/db/vector/compressionhelpers/parallel_iterator.go
+++ adapters/repos/db/vector/compressionhelpers/parallel_iterator.go
@@ -112,7 +112,7 @@ func (cpi *parallelIterator[T]) IterateAll() chan []VecAndID[T] {
 				}).Warn("skipping compressed vector with unexpected length")
 				continue
 			}
-			if cpi.loadId(k) > (1 << 15) {
+			if cpi.loadId(k) > (1e15) {
 				cpi.logger.WithFields(logrus.Fields{
 					"action": "hnsw_compressed_vector_cache_prefill",
 					"len":    len(v),
@@ -154,7 +154,7 @@ func (cpi *parallelIterator[T]) IterateAll() chan []VecAndID[T] {
 					}).Warn("skipping compressed vector with unexpected length")
 					continue
 				}
-				if cpi.loadId(k) > (1 << 15) {
+				if cpi.loadId(k) > (1e15) {
 					cpi.logger.WithFields(logrus.Fields{
 						"action": "hnsw_compressed_vector_cache_prefill",
 						"len":    len(v),
@@ -193,7 +193,7 @@ func (cpi *parallelIterator[T]) IterateAll() chan []VecAndID[T] {
 				}).Warn("skipping compressed vector with unexpected length")
 				continue
 			}
-			if cpi.loadId(k) > (1 << 15) {
+			if cpi.loadId(k) > (1e15) {
 				cpi.logger.WithFields(logrus.Fields{
 					"action": "hnsw_compressed_vector_cache_prefill",
 					"len":    len(v),
