aa7c62ee3f4d0991f1a6bd795c0dbab8e666fd0d
salvatorecampagna <salvatore@weaviate.io>
2025-10-16 12:21:43 +0200

    test: add some unit and integration tests

---
+++ adapters/repos/db/batch_addbatch_integration_test.go
@@ -0,0 +1,378 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2024 Weaviate B.V. All rights reserved.
+//
+//  CONTACT: hello@weaviate.io
+//
+
+//go:build integrationTest
+
+package db
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"testing"
+
+	"github.com/go-openapi/strfmt"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/weaviate/weaviate/entities/dto"
+	"github.com/weaviate/weaviate/entities/filters"
+	"github.com/weaviate/weaviate/entities/models"
+	"github.com/weaviate/weaviate/entities/schema"
+	enthnsw "github.com/weaviate/weaviate/entities/vectorindex/hnsw"
+	"github.com/weaviate/weaviate/usecases/memwatch"
+	"github.com/weaviate/weaviate/usecases/objects"
+)
+
+func TestBatchPutObjects_ConcurrentBatches(t *testing.T) {
+	dirName := t.TempDir()
+
+	logger := logrus.New()
+	schemaGetter := &fakeSchemaGetter{
+		schema:     schema.Schema{Objects: &models.Schema{Classes: nil}},
+		shardState: singleShardState(),
+	}
+	repo, err := New(logger, Config{
+		MemtablesFlushDirtyAfter:  60,
+		RootPath:                  dirName,
+		QueryMaximumResults:       10000,
+		MaxImportGoroutinesFactor: 1,
+		TrackVectorDimensions:     true,
+	}, &fakeRemoteClient{}, &fakeNodeResolver{}, &fakeRemoteNodeClient{}, &fakeReplicationClient{}, nil, memwatch.NewDummyMonitor())
+	require.Nil(t, err)
+	repo.SetSchemaGetter(schemaGetter)
+	require.Nil(t, repo.WaitForStartup(testCtx()))
+
+	defer func() {
+		require.Nil(t, repo.Shutdown(context.Background()))
+	}()
+
+	migrator := NewMigrator(repo, logger)
+
+	class := &models.Class{
+		Class:               "ConcurrentBatch",
+		VectorIndexConfig:   enthnsw.NewDefaultUserConfig(),
+		InvertedIndexConfig: invertedConfig(),
+		Properties: []*models.Property{
+			{
+				Name:         "stringProp",
+				DataType:     schema.DataTypeText.PropString(),
+				Tokenization: models.PropertyTokenizationWhitespace,
+			},
+		},
+	}
+
+	require.Nil(t, migrator.AddClass(context.Background(), class, schemaGetter.shardState))
+	schemaGetter.schema.Objects = &models.Schema{Classes: []*models.Class{class}}
+
+	numGoroutines := 10
+	objectsPerGoroutine := 50
+
+	var wg sync.WaitGroup
+	wg.Add(numGoroutines)
+
+	errorChan := make(chan error, numGoroutines)
+
+	for g := 0; g < numGoroutines; g++ {
+		go func(goroutineID int) {
+			defer wg.Done()
+
+			batch := make(objects.BatchObjects, objectsPerGoroutine)
+			for i := 0; i < objectsPerGoroutine; i++ {
+				id := fmt.Sprintf("%08d-%04d-0000-0000-000000000000", goroutineID, i)
+				batch[i] = objects.BatchObject{
+					OriginalIndex: i,
+					Object: &models.Object{
+						Class: "ConcurrentBatch",
+						Properties: map[string]interface{}{
+							"stringProp": fmt.Sprintf("goroutine-%d-object-%d", goroutineID, i),
+						},
+						ID:     strfmt.UUID(id),
+						Vector: []float32{float32(goroutineID), float32(i), 0.1, 0.2},
+					},
+					UUID: strfmt.UUID(id),
+				}
+			}
+
+			batchRes, err := repo.BatchPutObjects(context.Background(), batch, nil, 0)
+			if err != nil {
+				errorChan <- err
+				return
+			}
+
+			for _, res := range batchRes {
+				if res.Err != nil {
+					errorChan <- res.Err
+					return
+				}
+			}
+		}(g)
+	}
+
+	wg.Wait()
+	close(errorChan)
+
+	for err := range errorChan {
+		t.Errorf("goroutine error: %v", err)
+	}
+
+	params := dto.GetParams{
+		ClassName:  "ConcurrentBatch",
+		Pagination: &filters.Pagination{Limit: 1000},
+	}
+	res, err := repo.Search(context.Background(), params)
+	require.Nil(t, err)
+
+	expectedTotal := numGoroutines * objectsPerGoroutine
+	assert.Equal(t, expectedTotal, len(res), "should have all objects")
+}
+
+func TestBatchPutObjects_LargeSequentialBatches(t *testing.T) {
+	dirName := t.TempDir()
+
+	logger := logrus.New()
+	schemaGetter := &fakeSchemaGetter{
+		schema:     schema.Schema{Objects: &models.Schema{Classes: nil}},
+		shardState: singleShardState(),
+	}
+	repo, err := New(logger, Config{
+		MemtablesFlushDirtyAfter:  60,
+		RootPath:                  dirName,
+		QueryMaximumResults:       10000,
+		MaxImportGoroutinesFactor: 1,
+		TrackVectorDimensions:     true,
+	}, &fakeRemoteClient{}, &fakeNodeResolver{}, &fakeRemoteNodeClient{}, &fakeReplicationClient{}, nil, memwatch.NewDummyMonitor())
+	require.Nil(t, err)
+	repo.SetSchemaGetter(schemaGetter)
+	require.Nil(t, repo.WaitForStartup(testCtx()))
+
+	defer func() {
+		require.Nil(t, repo.Shutdown(context.Background()))
+	}()
+
+	migrator := NewMigrator(repo, logger)
+
+	class := &models.Class{
+		Class:               "LargeBatch",
+		VectorIndexConfig:   enthnsw.NewDefaultUserConfig(),
+		InvertedIndexConfig: invertedConfig(),
+		Properties: []*models.Property{
+			{
+				Name:         "stringProp",
+				DataType:     schema.DataTypeText.PropString(),
+				Tokenization: models.PropertyTokenizationWhitespace,
+			},
+		},
+	}
+
+	require.Nil(t, migrator.AddClass(context.Background(), class, schemaGetter.shardState))
+	schemaGetter.schema.Objects = &models.Schema{Classes: []*models.Class{class}}
+
+	numBatches := 5
+	batchSize := 200
+
+	for batchNum := 0; batchNum < numBatches; batchNum++ {
+		batch := make(objects.BatchObjects, batchSize)
+		for i := 0; i < batchSize; i++ {
+			idx := batchNum*batchSize + i
+			id := fmt.Sprintf("00000000-0000-0000-0000-%012d", idx)
+			batch[i] = objects.BatchObject{
+				OriginalIndex: i,
+				Object: &models.Object{
+					Class: "LargeBatch",
+					Properties: map[string]interface{}{
+						"stringProp": fmt.Sprintf("batch-%d-object-%d", batchNum, i),
+					},
+					ID:     strfmt.UUID(id),
+					Vector: []float32{float32(idx), 0.1, 0.2, 0.3},
+				},
+				UUID: strfmt.UUID(id),
+			}
+		}
+
+		batchRes, err := repo.BatchPutObjects(context.Background(), batch, nil, 0)
+		require.Nil(t, err, "batch %d failed", batchNum)
+
+		for _, res := range batchRes {
+			assert.Nil(t, res.Err, "batch %d had error", batchNum)
+		}
+	}
+
+	params := dto.GetParams{
+		ClassName:  "LargeBatch",
+		Pagination: &filters.Pagination{Limit: 2000},
+	}
+	res, err := repo.Search(context.Background(), params)
+	require.Nil(t, err)
+
+	expectedTotal := numBatches * batchSize
+	assert.Equal(t, expectedTotal, len(res))
+}
+
+func TestBatchPutObjects_UpdatesSameObjectsMultipleTimes(t *testing.T) {
+	dirName := t.TempDir()
+
+	logger := logrus.New()
+	schemaGetter := &fakeSchemaGetter{
+		schema:     schema.Schema{Objects: &models.Schema{Classes: nil}},
+		shardState: singleShardState(),
+	}
+	repo, err := New(logger, Config{
+		MemtablesFlushDirtyAfter:  60,
+		RootPath:                  dirName,
+		QueryMaximumResults:       10000,
+		MaxImportGoroutinesFactor: 1,
+		TrackVectorDimensions:     true,
+	}, &fakeRemoteClient{}, &fakeNodeResolver{}, &fakeRemoteNodeClient{}, &fakeReplicationClient{}, nil, memwatch.NewDummyMonitor())
+	require.Nil(t, err)
+	repo.SetSchemaGetter(schemaGetter)
+	require.Nil(t, repo.WaitForStartup(testCtx()))
+
+	defer func() {
+		require.Nil(t, repo.Shutdown(context.Background()))
+	}()
+
+	migrator := NewMigrator(repo, logger)
+
+	class := &models.Class{
+		Class:               "UpdateTest",
+		VectorIndexConfig:   enthnsw.NewDefaultUserConfig(),
+		InvertedIndexConfig: invertedConfig(),
+		Properties: []*models.Property{
+			{
+				Name:         "version",
+				DataType:     schema.DataTypeInt.PropString(),
+				Tokenization: models.PropertyTokenizationWhitespace,
+			},
+		},
+	}
+
+	require.Nil(t, migrator.AddClass(context.Background(), class, schemaGetter.shardState))
+	schemaGetter.schema.Objects = &models.Schema{Classes: []*models.Class{class}}
+
+	numObjects := 10
+	numUpdates := 5
+
+	for updateRound := 0; updateRound < numUpdates; updateRound++ {
+		batch := make(objects.BatchObjects, numObjects)
+		for i := 0; i < numObjects; i++ {
+			id := fmt.Sprintf("00000000-0000-0000-0000-%012d", i)
+			batch[i] = objects.BatchObject{
+				OriginalIndex: i,
+				Object: &models.Object{
+					Class: "UpdateTest",
+					Properties: map[string]interface{}{
+						"version": int64(updateRound),
+					},
+					ID:     strfmt.UUID(id),
+					Vector: []float32{float32(updateRound), float32(i), 0.1, 0.2},
+				},
+				UUID: strfmt.UUID(id),
+			}
+		}
+
+		batchRes, err := repo.BatchPutObjects(context.Background(), batch, nil, 0)
+		require.Nil(t, err, "update round %d failed", updateRound)
+
+		for _, res := range batchRes {
+			assert.Nil(t, res.Err)
+		}
+	}
+
+	params := dto.GetParams{
+		ClassName:  "UpdateTest",
+		Pagination: &filters.Pagination{Limit: 100},
+	}
+	res, err := repo.Search(context.Background(), params)
+	require.Nil(t, err)
+
+	assert.Equal(t, numObjects, len(res), "should have exactly numObjects, not duplicates")
+
+	for _, obj := range res {
+		version := obj.Schema.(map[string]interface{})["version"]
+		assert.Equal(t, int64(numUpdates-1), version, "should have latest version")
+	}
+}
+
+func TestBatchPutObjects_ContextCancellation(t *testing.T) {
+	dirName := t.TempDir()
+
+	logger := logrus.New()
+	schemaGetter := &fakeSchemaGetter{
+		schema:     schema.Schema{Objects: &models.Schema{Classes: nil}},
+		shardState: singleShardState(),
+	}
+	repo, err := New(logger, Config{
+		MemtablesFlushDirtyAfter:  60,
+		RootPath:                  dirName,
+		QueryMaximumResults:       10000,
+		MaxImportGoroutinesFactor: 1,
+		TrackVectorDimensions:     true,
+	}, &fakeRemoteClient{}, &fakeNodeResolver{}, &fakeRemoteNodeClient{}, &fakeReplicationClient{}, nil, memwatch.NewDummyMonitor())
+	require.Nil(t, err)
+	repo.SetSchemaGetter(schemaGetter)
+	require.Nil(t, repo.WaitForStartup(testCtx()))
+
+	defer func() {
+		require.Nil(t, repo.Shutdown(context.Background()))
+	}()
+
+	migrator := NewMigrator(repo, logger)
+
+	class := &models.Class{
+		Class:               "ContextCancel",
+		VectorIndexConfig:   enthnsw.NewDefaultUserConfig(),
+		InvertedIndexConfig: invertedConfig(),
+		Properties: []*models.Property{
+			{
+				Name:         "stringProp",
+				DataType:     schema.DataTypeText.PropString(),
+				Tokenization: models.PropertyTokenizationWhitespace,
+			},
+		},
+	}
+
+	require.Nil(t, migrator.AddClass(context.Background(), class, schemaGetter.shardState))
+	schemaGetter.schema.Objects = &models.Schema{Classes: []*models.Class{class}}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	batchSize := 100
+	batch := make(objects.BatchObjects, batchSize)
+	for i := 0; i < batchSize; i++ {
+		id := fmt.Sprintf("00000000-0000-0000-0000-%012d", i)
+		batch[i] = objects.BatchObject{
+			OriginalIndex: i,
+			Object: &models.Object{
+				Class: "ContextCancel",
+				Properties: map[string]interface{}{
+					"stringProp": fmt.Sprintf("object-%d", i),
+				},
+				ID:     strfmt.UUID(id),
+				Vector: []float32{float32(i), 0.1, 0.2, 0.3},
+			},
+			UUID: strfmt.UUID(id),
+		}
+	}
+
+	batchRes, err := repo.BatchPutObjects(ctx, batch, nil, 0)
+	require.Nil(t, err)
+
+	errorCount := 0
+	for _, res := range batchRes {
+		if res.Err != nil {
+			errorCount++
+		}
+	}
+
+	assert.Greater(t, errorCount, 0, "should have context errors")
+}
---
+++ adapters/repos/db/vector/hnsw/add_batch_test.go
@@ -0,0 +1,392 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2024 Weaviate B.V. All rights reserved.
+//
+//  CONTACT: hello@weaviate.io
+//
+
+package hnsw
+
+import (
+	"context"
+	"sync"
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/weaviate/weaviate/adapters/repos/db/vector/hnsw/distancer"
+	"github.com/weaviate/weaviate/adapters/repos/db/vector/testinghelpers"
+	"github.com/weaviate/weaviate/entities/cyclemanager"
+	enterrors "github.com/weaviate/weaviate/entities/errors"
+	ent "github.com/weaviate/weaviate/entities/vectorindex/hnsw"
+	"github.com/weaviate/weaviate/usecases/memwatch"
+)
+
+type mockAllocChecker struct {
+	mu            sync.Mutex
+	shouldFail    bool
+	failAfter     int
+	callCount     int
+	allocRequests []int64
+}
+
+func newMockAllocChecker() *mockAllocChecker {
+	return &mockAllocChecker{
+		allocRequests: make([]int64, 0),
+	}
+}
+
+func (m *mockAllocChecker) CheckAlloc(sizeInBytes int64) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.allocRequests = append(m.allocRequests, sizeInBytes)
+	m.callCount++
+
+	if m.shouldFail && (m.failAfter == 0 || m.callCount >= m.failAfter) {
+		return enterrors.NewNotEnoughMemory("mock OOM")
+	}
+	return nil
+}
+
+func (m *mockAllocChecker) CheckMappingAndReserve(numberMappings int64, reservationTimeInS int) error {
+	return nil
+}
+
+func (m *mockAllocChecker) Refresh(updateMappings bool) {}
+
+func (m *mockAllocChecker) getAllocRequests() []int64 {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	result := make([]int64, len(m.allocRequests))
+	copy(result, m.allocRequests)
+	return result
+}
+
+func TestAddBatch_EmptyBatch(t *testing.T) {
+	index := testHNSW(t)
+	defer index.Shutdown(context.Background())
+
+	err := index.AddBatch(context.Background(), []uint64{}, [][]float32{})
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "empty lists")
+}
+
+func TestAddBatch_MismatchedLengths(t *testing.T) {
+	index := testHNSW(t)
+	defer index.Shutdown(context.Background())
+
+	ids := []uint64{1, 2, 3}
+	vectors := [][]float32{{0.1, 0.2}, {0.3, 0.4}}
+
+	err := index.AddBatch(context.Background(), ids, vectors)
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "sizes does not match")
+}
+
+func TestAddBatch_InconsistentVectorDimensions(t *testing.T) {
+	index := testHNSW(t)
+	defer index.Shutdown(context.Background())
+
+	ids := []uint64{1, 2, 3}
+	vectors := [][]float32{
+		{0.1, 0.2, 0.3},
+		{0.4, 0.5},
+		{0.6, 0.7, 0.8},
+	}
+
+	err := index.AddBatch(context.Background(), ids, vectors)
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "different lengths")
+}
+
+func TestAddBatch_EmptyVector(t *testing.T) {
+	index := testHNSW(t)
+	defer index.Shutdown(context.Background())
+
+	ids := []uint64{1, 2}
+	vectors := [][]float32{{0.1, 0.2}, {}}
+
+	err := index.AddBatch(context.Background(), ids, vectors)
+	require.Error(t, err)
+}
+
+func TestAddBatch_ContextCancellation(t *testing.T) {
+	index := testHNSW(t)
+	defer index.Shutdown(context.Background())
+
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	ids := []uint64{1, 2}
+	vectors := [][]float32{{0.1, 0.2}, {0.3, 0.4}}
+
+	err := index.AddBatch(ctx, ids, vectors)
+	require.Error(t, err)
+	assert.True(t, errors.Is(err, context.Canceled))
+}
+
+func TestAddBatch_SingleVector(t *testing.T) {
+	index := testHNSW(t)
+	defer index.Shutdown(context.Background())
+
+	ids := []uint64{1}
+	vectors := [][]float32{{0.1, 0.2, 0.3, 0.4}}
+
+	err := index.AddBatch(context.Background(), ids, vectors)
+	require.NoError(t, err)
+
+	assert.True(t, index.ContainsDoc(1))
+}
+
+func TestAddBatch_MultipleBatches(t *testing.T) {
+	index := testHNSW(t)
+	defer index.Shutdown(context.Background())
+
+	batch1IDs := []uint64{1, 2, 3}
+	batch1Vectors := [][]float32{
+		{0.1, 0.2, 0.3, 0.4},
+		{0.2, 0.3, 0.4, 0.5},
+		{0.3, 0.4, 0.5, 0.6},
+	}
+
+	err := index.AddBatch(context.Background(), batch1IDs, batch1Vectors)
+	require.NoError(t, err)
+
+	batch2IDs := []uint64{4, 5, 6}
+	batch2Vectors := [][]float32{
+		{0.4, 0.5, 0.6, 0.7},
+		{0.5, 0.6, 0.7, 0.8},
+		{0.6, 0.7, 0.8, 0.9},
+	}
+
+	err = index.AddBatch(context.Background(), batch2IDs, batch2Vectors)
+	require.NoError(t, err)
+
+	for _, id := range append(batch1IDs, batch2IDs...) {
+		assert.True(t, index.ContainsDoc(id), "missing doc %d", id)
+	}
+}
+
+func TestAddBatch_WrongDimensionsAfterInitialBatch(t *testing.T) {
+	index := testHNSW(t)
+	defer index.Shutdown(context.Background())
+
+	batch1IDs := []uint64{1, 2}
+	batch1Vectors := [][]float32{
+		{0.1, 0.2, 0.3, 0.4},
+		{0.2, 0.3, 0.4, 0.5},
+	}
+
+	err := index.AddBatch(context.Background(), batch1IDs, batch1Vectors)
+	require.NoError(t, err)
+
+	batch2IDs := []uint64{3, 4}
+	batch2Vectors := [][]float32{
+		{0.3, 0.4, 0.5},
+		{0.4, 0.5, 0.6},
+	}
+
+	err = index.AddBatch(context.Background(), batch2IDs, batch2Vectors)
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "vector lengths don't match")
+}
+
+func TestAddBatch_MemoryAllocationFailure(t *testing.T) {
+	allocChecker := newMockAllocChecker()
+	allocChecker.shouldFail = true
+
+	index := testHNSWWithAllocChecker(t, allocChecker)
+	defer index.Shutdown(context.Background())
+
+	ids := []uint64{1, 2, 3}
+	vectors := [][]float32{
+		{0.1, 0.2, 0.3, 0.4},
+		{0.2, 0.3, 0.4, 0.5},
+		{0.3, 0.4, 0.5, 0.6},
+	}
+
+	err := index.AddBatch(context.Background(), ids, vectors)
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "not enough memory")
+
+	requests := allocChecker.getAllocRequests()
+	require.Len(t, requests, 1)
+
+	expectedMemory := int64(3 * (4*4 + 30))
+	assert.Equal(t, expectedMemory, requests[0])
+}
+
+func TestAddBatch_LargeBatch(t *testing.T) {
+	batchSize := 1000
+	dims := 128
+
+	vectors := make(map[uint64][]float32)
+	for i := 0; i < batchSize; i++ {
+		vec := make([]float32, dims)
+		for j := 0; j < dims; j++ {
+			vec[j] = float32(i*dims+j) / float32(batchSize*dims)
+		}
+		vectors[uint64(i)] = vec
+	}
+
+	vecForIDFn := func(ctx context.Context, id uint64) ([]float32, error) {
+		return vectors[id], nil
+	}
+
+	cfg := Config{
+		RootPath:              t.TempDir(),
+		ID:                    "test-hnsw-large",
+		MakeCommitLoggerThunk: MakeNoopCommitLogger,
+		DistanceProvider:      distancer.NewCosineDistanceProvider(),
+		VectorForIDThunk:      vecForIDFn,
+		AllocChecker:          memwatch.NewDummyMonitor(),
+	}
+
+	index, err := New(cfg, ent.UserConfig{
+		MaxConnections: 30,
+		EFConstruction: 60,
+		EF:             36,
+	}, cyclemanager.NewCallbackGroupNoop(), testinghelpers.NewDummyStore(t))
+	require.NoError(t, err)
+	defer index.Shutdown(context.Background())
+
+	ids := make([]uint64, batchSize)
+	vecs := make([][]float32, batchSize)
+	for i := 0; i < batchSize; i++ {
+		ids[i] = uint64(i)
+		vecs[i] = vectors[uint64(i)]
+	}
+
+	err = index.AddBatch(context.Background(), ids, vecs)
+	require.NoError(t, err)
+
+	for _, id := range ids {
+		assert.True(t, index.ContainsDoc(id))
+	}
+}
+
+func TestAddBatch_WithGapInIDs(t *testing.T) {
+	index := testHNSW(t)
+	defer index.Shutdown(context.Background())
+
+	ids := []uint64{1, 100, 1000}
+	vectors := [][]float32{
+		{0.1, 0.2, 0.3, 0.4},
+		{0.2, 0.3, 0.4, 0.5},
+		{0.3, 0.4, 0.5, 0.6},
+	}
+
+	err := index.AddBatch(context.Background(), ids, vectors)
+	require.NoError(t, err)
+
+	for _, id := range ids {
+		assert.True(t, index.ContainsDoc(id))
+	}
+}
+
+func TestAddBatch_UpdateExistingVectors(t *testing.T) {
+	index := testHNSW(t)
+	defer index.Shutdown(context.Background())
+
+	ids := []uint64{1, 2, 3}
+	initialVectors := [][]float32{
+		{0.1, 0.2, 0.3, 0.4},
+		{0.2, 0.3, 0.4, 0.5},
+		{0.3, 0.4, 0.5, 0.6},
+	}
+
+	err := index.AddBatch(context.Background(), ids, initialVectors)
+	require.NoError(t, err)
+
+	updatedVectors := [][]float32{
+		{0.9, 0.8, 0.7, 0.6},
+		{0.8, 0.7, 0.6, 0.5},
+		{0.7, 0.6, 0.5, 0.4},
+	}
+
+	err = index.AddBatch(context.Background(), ids, updatedVectors)
+	require.NoError(t, err)
+
+	for _, id := range ids {
+		assert.True(t, index.ContainsDoc(id))
+	}
+}
+
+func TestAddBatch_ConcurrentAddBatchCalls(t *testing.T) {
+	index := testHNSW(t)
+	defer index.Shutdown(context.Background())
+
+	numGoroutines := 10
+	vectorsPerGoroutine := 100
+	dims := 64
+
+	var wg sync.WaitGroup
+	wg.Add(numGoroutines)
+
+	errors := make(chan error, numGoroutines)
+
+	for g := 0; g < numGoroutines; g++ {
+		go func(goroutineID int) {
+			defer wg.Done()
+
+			offset := uint64(goroutineID * vectorsPerGoroutine)
+			ids := make([]uint64, vectorsPerGoroutine)
+			vectors := make([][]float32, vectorsPerGoroutine)
+
+			for i := 0; i < vectorsPerGoroutine; i++ {
+				ids[i] = offset + uint64(i)
+				vectors[i] = make([]float32, dims)
+				for j := 0; j < dims; j++ {
+					vectors[i][j] = float32(goroutineID*vectorsPerGoroutine+i*dims+j) / 10000.0
+				}
+			}
+
+			err := index.AddBatch(context.Background(), ids, vectors)
+			if err != nil {
+				errors <- err
+			}
+		}(g)
+	}
+
+	wg.Wait()
+	close(errors)
+
+	for err := range errors {
+		t.Errorf("goroutine error: %v", err)
+	}
+
+	expectedTotal := numGoroutines * vectorsPerGoroutine
+	for i := 0; i < expectedTotal; i++ {
+		assert.True(t, index.ContainsDoc(uint64(i)), "missing doc %d", i)
+	}
+}
+
+func testHNSW(t *testing.T) *hnsw {
+	return testHNSWWithAllocChecker(t, memwatch.NewDummyMonitor())
+}
+
+func testHNSWWithAllocChecker(t *testing.T, allocChecker memwatch.AllocChecker) *hnsw {
+	cfg := Config{
+		RootPath:              t.TempDir(),
+		ID:                    "test-hnsw",
+		MakeCommitLoggerThunk: MakeNoopCommitLogger,
+		DistanceProvider:      distancer.NewCosineDistanceProvider(),
+		VectorForIDThunk:      testVectorForID,
+		AllocChecker:          allocChecker,
+	}
+
+	index, err := New(cfg, ent.UserConfig{
+		MaxConnections: 30,
+		EFConstruction: 60,
+		EF:             36,
+	}, cyclemanager.NewCallbackGroupNoop(), testinghelpers.NewDummyStore(t))
+	require.Nil(t, err)
+
+	return index
+}
