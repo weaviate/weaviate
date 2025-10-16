3c033bc8c7a51386b8163c9dcdc270e95bee28f0
moogacs <contact@m-mooga.com>
2025-10-16 16:04:39 +0200

    pass worker context to the retry

--- usecases/replica/coordinator.go
+++ usecases/replica/coordinator.go
@@ -19,11 +19,11 @@ import (
 	"time"
 
 	"github.com/cenkalti/backoff/v4"
+	"github.com/sirupsen/logrus"
+
 	"github.com/weaviate/weaviate/cluster/router/types"
 	"github.com/weaviate/weaviate/cluster/utils"
 	enterrors "github.com/weaviate/weaviate/entities/errors"
-
-	"github.com/sirupsen/logrus"
 )
 
 const (
@@ -341,9 +341,10 @@ func (c *coordinator[T]) Pull(ctx context.Context,
 					return
 				}
 				// this host failed op on the first try, put it on the retry queue
+				// tie backoff to the worker's context so cancellation stops retries immediately
 				hostRetryQueue <- hostRetry{
 					hosts[hostIndex],
-					backoff.WithContext(utils.NewExponentialBackoff(c.pullBackOffPreInitialInterval, c.pullBackOffMaxElapsedTime), ctx),
+					backoff.WithContext(utils.NewExponentialBackoff(c.pullBackOffPreInitialInterval, c.pullBackOffMaxElapsedTime), workerCtx),
 				}
 
 				// let's fallback to the backups in the retry queue
@@ -370,7 +371,13 @@ func (c *coordinator[T]) Pull(ctx context.Context,
 						replyCh <- _Result[T]{resp, err}
 						return
 					case <-timer.C:
-						hostRetryQueue <- hostRetry{hr.host, hr.currentBackOff}
+						// requeue only if still alive
+						select {
+						case <-workerCtx.Done():
+							// don't requeue on cancellation
+						default:
+							hostRetryQueue <- hostRetry{hr.host, hr.currentBackOff}
+						}
 					}
 					timer.Stop()
 				}
