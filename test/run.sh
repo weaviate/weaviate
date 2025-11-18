1b7d757c8a1f9dd7e4b0f27a7312e49997369d10
moogacs <contact@m-mooga.com>
2025-11-18 12:35:14 +0100

    increase pipeline step timeout and rest all to 30 min

--- test/run.sh
+++ test/run.sh
@@ -635,7 +635,7 @@ function run_acceptance_replica_replication_fast_tests() {
 
 function run_acceptance_replica_replication_slow_tests() {
   for pkg in $(go list ./.../ | grep 'test/acceptance/replication/replica_replication/slow'); do
-    if ! go test -timeout=45m -count 1 -race "$pkg"; then
+    if ! go test -timeout=30m -count 1 -race "$pkg"; then
       echo "Test for $pkg failed" >&2
       return 1
     fi
