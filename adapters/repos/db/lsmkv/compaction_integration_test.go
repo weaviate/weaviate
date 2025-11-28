0fed4527c8bd191916b39fa5f5bd2f8174a24dcd
André Mourão <andre.mourao@weaviate.io>
2025-11-28 15:28:41 +0000

    fix: add accidentally removed test

--- adapters/repos/db/lsmkv/compaction_integration_test.go
+++ adapters/repos/db/lsmkv/compaction_integration_test.go
@@ -24,6 +24,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/weaviate/weaviate/adapters/repos/db/lsmkv/segmentindex"
+	"github.com/weaviate/weaviate/adapters/repos/db/roaringset"
 )
 
 func testCtx() context.Context {
@@ -55,6 +56,359 @@ func (tests bucketIntegrationTests) run(ctx context.Context, t *testing.T) {
 func TestCompaction(t *testing.T) {
 	ctx := testCtx()
 	tests := bucketIntegrationTests{
+		{
+			name: "compactionReplaceStrategy",
+			f: func(ctx context.Context, t *testing.T, opts []BucketOption) {
+				compactionReplaceStrategy(ctx, t, opts, 12116, 12116)
+			},
+			opts: []BucketOption{
+				WithStrategy(StrategyReplace), WithCalcCountNetAdditions(true),
+			},
+		},
+		{
+			name: "compactionReplaceStrategy_KeepTombstones",
+			f: func(ctx context.Context, t *testing.T, opts []BucketOption) {
+				compactionReplaceStrategy(ctx, t, opts, 15266, 15266)
+			},
+			opts: []BucketOption{
+				WithStrategy(StrategyReplace),
+				WithKeepTombstones(true),
+				WithCalcCountNetAdditions(true),
+			},
+		},
+		{
+			name: "compactionReplaceStrategy_WithSecondaryKeys",
+			f:    compactionReplaceStrategy_WithSecondaryKeys,
+			opts: []BucketOption{
+				WithStrategy(StrategyReplace),
+				WithSecondaryIndices(1),
+			},
+		},
+		{
+			name: "compactionReplaceStrategy_WithSecondaryKeys_KeepTombstones",
+			f:    compactionReplaceStrategy_WithSecondaryKeys,
+			opts: []BucketOption{
+				WithStrategy(StrategyReplace),
+				WithSecondaryIndices(1),
+				WithKeepTombstones(true),
+			},
+		},
+		{
+			name: "compactionReplaceStrategy_RemoveUnnecessaryDeletes",
+			f:    compactionReplaceStrategy_RemoveUnnecessaryDeletes,
+			opts: []BucketOption{
+				WithStrategy(StrategyReplace),
+			},
+		},
+		{
+			name: "compactionReplaceStrategy_RemoveUnnecessaryDeletes_KeepTombstones",
+			f:    compactionReplaceStrategy_RemoveUnnecessaryDeletes,
+			opts: []BucketOption{
+				WithStrategy(StrategyReplace),
+				WithKeepTombstones(true),
+			},
+		},
+		{
+			name: "compactionReplaceStrategy_RemoveUnnecessaryUpdates",
+			f:    compactionReplaceStrategy_RemoveUnnecessaryUpdates,
+			opts: []BucketOption{
+				WithStrategy(StrategyReplace),
+			},
+		},
+		{
+			name: "compactionReplaceStrategy_RemoveUnnecessaryUpdates_KeepTombstones",
+			f:    compactionReplaceStrategy_RemoveUnnecessaryUpdates,
+			opts: []BucketOption{
+				WithStrategy(StrategyReplace),
+				WithKeepTombstones(true),
+			},
+		},
+		{
+			name: "compactionReplaceStrategy_FrequentPutDeleteOperations",
+			f:    compactionReplaceStrategy_FrequentPutDeleteOperations,
+			opts: []BucketOption{
+				WithStrategy(StrategyReplace),
+			},
+		},
+		{
+			name: "compactionReplaceStrategy_FrequentPutDeleteOperations_KeepTombstones",
+			f:    compactionReplaceStrategy_FrequentPutDeleteOperations,
+			opts: []BucketOption{
+				WithStrategy(StrategyReplace),
+				WithKeepTombstones(true),
+			},
+		},
+		{
+			name: "compactionReplaceStrategy_FrequentPutDeleteOperations_WithSecondaryKeys",
+			f:    compactionReplaceStrategy_FrequentPutDeleteOperations_WithSecondaryKeys,
+			opts: []BucketOption{
+				WithStrategy(StrategyReplace),
+				WithSecondaryIndices(1),
+			},
+		},
+		{
+			name: "compactionReplaceStrategy_FrequentPutDeleteOperations_WithSecondaryKeys_KeepTombstones",
+			f:    compactionReplaceStrategy_FrequentPutDeleteOperations_WithSecondaryKeys,
+			opts: []BucketOption{
+				WithStrategy(StrategyReplace),
+				WithSecondaryIndices(1),
+				WithKeepTombstones(true),
+			},
+		},
+
+		{
+			name: "compactionSetStrategy",
+			f: func(ctx context.Context, t *testing.T, opts []BucketOption) {
+				compactionSetStrategy(ctx, t, opts, 6836, 6836)
+			},
+			opts: []BucketOption{
+				WithStrategy(StrategySetCollection),
+			},
+		},
+		{
+			name: "compactionSetStrategy_KeepTombstones",
+			f: func(ctx context.Context, t *testing.T, opts []BucketOption) {
+				compactionSetStrategy(ctx, t, opts, 9756, 9756)
+			},
+			opts: []BucketOption{
+				WithStrategy(StrategySetCollection),
+				WithKeepTombstones(true),
+			},
+		},
+		{
+			name: "compactionSetStrategy_RemoveUnnecessary",
+			f:    compactionSetStrategy_RemoveUnnecessary,
+			opts: []BucketOption{
+				WithStrategy(StrategySetCollection),
+			},
+		},
+		{
+			name: "compactionSetStrategy_RemoveUnnecessary_KeepTombstones",
+			f:    compactionSetStrategy_RemoveUnnecessary,
+			opts: []BucketOption{
+				WithStrategy(StrategySetCollection),
+				WithKeepTombstones(true),
+			},
+		},
+		{
+			name: "compactionSetStrategy_FrequentPutDeleteOperations",
+			f:    compactionSetStrategy_FrequentPutDeleteOperations,
+			opts: []BucketOption{
+				WithStrategy(StrategySetCollection),
+			},
+		},
+		{
+			name: "compactionSetStrategy_FrequentPutDeleteOperations_KeepTombstones",
+			f:    compactionSetStrategy_FrequentPutDeleteOperations,
+			opts: []BucketOption{
+				WithStrategy(StrategySetCollection),
+				WithKeepTombstones(true),
+			},
+		},
+
+		{
+			name: "compactionMapStrategy",
+			f: func(ctx context.Context, t *testing.T, opts []BucketOption) {
+				compactionMapStrategy(ctx, t, opts, 10676, 10676)
+			},
+			opts: []BucketOption{
+				WithStrategy(StrategyMapCollection),
+			},
+		},
+		{
+			name: "compactionMapStrategy_KeepTombstones",
+			f: func(ctx context.Context, t *testing.T, opts []BucketOption) {
+				compactionMapStrategy(ctx, t, opts, 13416, 13416)
+			},
+			opts: []BucketOption{
+				WithStrategy(StrategyMapCollection),
+				WithKeepTombstones(true),
+			},
+		},
+		{
+			name: "compactionMapStrategy_RemoveUnnecessary",
+			f:    compactionMapStrategy_RemoveUnnecessary,
+			opts: []BucketOption{
+				WithStrategy(StrategyMapCollection),
+			},
+		},
+		{
+			name: "compactionMapStrategy_HugeEntries",
+			f:    compactionMapStrategy_HugeEntries,
+			opts: []BucketOption{
+				WithStrategy(StrategyMapCollection),
+			},
+		},
+		{
+			name: "compactionMapStrategy_RemoveUnnecessary_KeepTombstones",
+			f:    compactionMapStrategy_RemoveUnnecessary,
+			opts: []BucketOption{
+				WithStrategy(StrategyMapCollection),
+				WithKeepTombstones(true),
+			},
+		},
+		{
+			name: "compactionMapStrategy_FrequentPutDeleteOperations",
+			f:    compactionMapStrategy_FrequentPutDeleteOperations,
+			opts: []BucketOption{
+				WithStrategy(StrategyMapCollection),
+			},
+		},
+		{
+			name: "compactionMapStrategy_FrequentPutDeleteOperations_KeepTombstones",
+			f:    compactionMapStrategy_FrequentPutDeleteOperations,
+			opts: []BucketOption{
+				WithStrategy(StrategyMapCollection),
+				WithKeepTombstones(true),
+			},
+		},
+
+		// RoaringSet
+		{
+			name: "compactionRoaringSetStrategy_Random",
+			f:    compactionRoaringSetStrategy_Random,
+			opts: []BucketOption{
+				WithStrategy(StrategyRoaringSet),
+				WithBitmapBufPool(roaringset.NewBitmapBufPoolNoop()),
+			},
+		},
+		{
+			name: "compactionRoaringSetStrategy_Random_KeepTombstones",
+			f:    compactionRoaringSetStrategy_Random,
+			opts: []BucketOption{
+				WithStrategy(StrategyRoaringSet),
+				WithBitmapBufPool(roaringset.NewBitmapBufPoolNoop()),
+				WithKeepTombstones(true),
+			},
+		},
+		{
+			name: "compactionRoaringSetStrategy",
+			f: func(ctx context.Context, t *testing.T, opts []BucketOption) {
+				compactionRoaringSetStrategy(ctx, t, opts, 19168, 19168)
+			},
+			opts: []BucketOption{
+				WithStrategy(StrategyRoaringSet),
+				WithBitmapBufPool(roaringset.NewBitmapBufPoolNoop()),
+			},
+		},
+		{
+			name: "compactionRoaringSetStrategy_KeepTombstones",
+			f: func(ctx context.Context, t *testing.T, opts []BucketOption) {
+				compactionRoaringSetStrategy(ctx, t, opts, 29792, 29792)
+			},
+			opts: []BucketOption{
+				WithStrategy(StrategyRoaringSet),
+				WithBitmapBufPool(roaringset.NewBitmapBufPoolNoop()),
+				WithKeepTombstones(true),
+			},
+		},
+		{
+			name: "compactionRoaringSetStrategy_RemoveUnnecessary",
+			f:    compactionRoaringSetStrategy_RemoveUnnecessary,
+			opts: []BucketOption{
+				WithStrategy(StrategyRoaringSet),
+				WithBitmapBufPool(roaringset.NewBitmapBufPoolNoop()),
+			},
+		},
+		{
+			name: "compactionRoaringSetStrategy_RemoveUnnecessary_KeepTombstones",
+			f:    compactionRoaringSetStrategy_RemoveUnnecessary,
+			opts: []BucketOption{
+				WithStrategy(StrategyRoaringSet),
+				WithBitmapBufPool(roaringset.NewBitmapBufPoolNoop()),
+				WithKeepTombstones(true),
+			},
+		},
+		{
+			name: "compactionRoaringSetStrategy_FrequentPutDeleteOperations",
+			f:    compactionRoaringSetStrategy_FrequentPutDeleteOperations,
+			opts: []BucketOption{
+				WithStrategy(StrategyRoaringSet),
+				WithBitmapBufPool(roaringset.NewBitmapBufPoolNoop()),
+			},
+		},
+		{
+			name: "compactionRoaringSetStrategy_FrequentPutDeleteOperations_KeepTombstones",
+			f:    compactionRoaringSetStrategy_FrequentPutDeleteOperations,
+			opts: []BucketOption{
+				WithStrategy(StrategyRoaringSet),
+				WithBitmapBufPool(roaringset.NewBitmapBufPoolNoop()),
+				WithKeepTombstones(true),
+			},
+		},
+
+		// RoaringSetRange
+		{
+			name: "compactionRoaringSetRangeStrategy_Random",
+			f:    compactionRoaringSetRangeStrategy_Random,
+			opts: []BucketOption{
+				WithStrategy(StrategyRoaringSetRange),
+			},
+		},
+		{
+			name: "compactionRoaringSetRangeStrategy_Random_KeepTombstones",
+			f:    compactionRoaringSetRangeStrategy_Random,
+			opts: []BucketOption{
+				WithStrategy(StrategyRoaringSetRange),
+				WithKeepTombstones(true),
+			},
+		},
+		{
+			name: "compactionRoaringSetRangeStrategy",
+			f: func(ctx context.Context, t *testing.T, opts []BucketOption) {
+				compactionRoaringSetRangeStrategy(ctx, t, opts, 1824, 1824)
+			},
+			opts: []BucketOption{
+				WithStrategy(StrategyRoaringSetRange),
+			},
+		},
+		{
+			name: "compactionRoaringSetRangeStrategy_KeepTombstones",
+			f: func(ctx context.Context, t *testing.T, opts []BucketOption) {
+				compactionRoaringSetRangeStrategy(ctx, t, opts, 2384, 2384)
+			},
+			opts: []BucketOption{
+				WithStrategy(StrategyRoaringSetRange),
+				WithKeepTombstones(true),
+			},
+		},
+		{
+			name: "compactionRoaringSetRangeStrategy_RemoveUnnecessary",
+			f:    compactionRoaringSetRangeStrategy_RemoveUnnecessary,
+			opts: []BucketOption{
+				WithStrategy(StrategyRoaringSetRange),
+			},
+		},
+		{
+			name: "compactionRoaringSetRangeStrategy_RemoveUnnecessary_KeepTombstones",
+			f:    compactionRoaringSetRangeStrategy_RemoveUnnecessary,
+			opts: []BucketOption{
+				WithStrategy(StrategyRoaringSetRange),
+				WithKeepTombstones(true),
+			},
+		},
+		{
+			name: "compactionRoaringSetRangeStrategy_FrequentPutDeleteOperations",
+			f:    compactionRoaringSetRangeStrategy_FrequentPutDeleteOperations,
+			opts: []BucketOption{
+				WithStrategy(StrategyRoaringSetRange),
+			},
+		},
+		{
+			name: "compactionRoaringSetRangeStrategy_FrequentPutDeleteOperations_KeepTombstones",
+			f:    compactionRoaringSetRangeStrategy_FrequentPutDeleteOperations,
+			opts: []BucketOption{
+				WithStrategy(StrategyRoaringSetRange),
+				WithKeepTombstones(true),
+			},
+		},
+		{
+			name: "compactionRoaringSetRangeStrategy_BugfixOverwrittenBuffer",
+			f:    compactionRoaringSetRangeStrategy_BugfixOverwrittenBuffer,
+			opts: []BucketOption{
+				WithStrategy(StrategyRoaringSetRange),
+			},
+		},
 		// Inverted
 		{
 			name: "compactionInvertedStrategy",
