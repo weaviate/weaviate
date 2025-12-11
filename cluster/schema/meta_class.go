f4a78a97f7b856e2b885cd300c7db84a68f8fba7
Mohamed Badawi <contact@m-mooga.com>
2025-12-11 17:44:23 +0100

    fix(tenant-offload): frozen tenants activation after onloading (#9980)

--- cluster/schema/meta_class.go
+++ cluster/schema/meta_class.go
@@ -386,6 +386,7 @@ func (m *metaClass) UpdateTenants(nodeID string, req *command.UpdateTenantsReque
 			}
 		case types.TenantActivityStatusUNFREEZING:
 			// ignore multiple unfreezing
+			req.ImplicitUpdateRequest = true
 			var statusInProgress string
 			processes, exists := m.ShardProcesses[shardProcessID(req.Tenants[i].Name, command.TenantProcessRequest_ACTION_UNFREEZING)]
 			if exists {
