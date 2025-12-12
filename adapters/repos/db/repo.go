59b2b6e7f22da390940ff724bdbb68bc7b80dd69
Andrzej Liszka <andrzej.liszka@gmail.com>
2025-12-12 13:22:33 +0100

    env var conc factor

--- adapters/repos/db/repo.go
+++ adapters/repos/db/repo.go
@@ -291,6 +291,7 @@ type Config struct {
 	CycleManagerRoutinesFactor          int
 	IndexRangeableInMemory              bool
 	ObjectsTTLBatchSize                 int
+	ObjectsTTLConcurrencyFactor         float64
 
 	HNSWMaxLogSize                               int64
 	HNSWDisableSnapshots                         bool
