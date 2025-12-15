b41a65c0feafce87cc07c7f892ff9b3effa6dbed
moogacs <contact@m-mooga.com>
2025-12-15 14:28:54 +0100

    refact(replication): introduce replication error pkg and unify erroring

--- usecases/replica/replicator_stream.go
+++ usecases/replica/replicator_stream.go
@@ -15,7 +15,9 @@ import (
 	"fmt"
 
 	"github.com/go-openapi/strfmt"
+
 	"github.com/weaviate/weaviate/usecases/objects"
+	replicaerrors "github.com/weaviate/weaviate/usecases/replica/errors"
 )
 
 type (
@@ -46,7 +48,7 @@ func (r replicatorStream) readErrors(batchSize int,
 		}
 	}
 	if level > 0 && firstError == nil {
-		firstError = fmt.Errorf("commit: %w", ErrReplicas)
+		firstError = replicaerrors.NewReplicasError(fmt.Errorf("commit: %w", firstError))
 	}
 	return r.flattenErrors(batchSize, urs, firstError)
 }
@@ -75,7 +77,7 @@ func (r replicatorStream) readDeletions(batchSize int,
 		}
 	}
 	if level > 0 && firstError == nil {
-		firstError = fmt.Errorf("commit: %w", ErrReplicas)
+		firstError = replicaerrors.NewReplicasError(fmt.Errorf("commit: %w", firstError))
 	}
 	urs = append(urs, rs...)
 	return r.flattenDeletions(batchSize, urs, firstError)
