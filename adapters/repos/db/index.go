cade181e3ec160b4d57ed4e4094727b170f93252
moogacs <contact@m-mooga.com>
2025-12-16 16:15:32 +0100

    mustload shall  be true for lazy loaded shards loading

--- adapters/repos/db/index.go
+++ adapters/repos/db/index.go
@@ -2068,8 +2068,7 @@ func (i *Index) initLocalShard(ctx context.Context, shardName string) error {
 func (i *Index) LoadLocalShard(ctx context.Context, shardName string, implicitShardLoading bool) error {
 	// TODO: implicitShardLoading needs to be double checked if needed at all
 	// consalidate mustLoad and implicitShardLoading
-	mustLoad := implicitShardLoading
-	return i.initLocalShardWithForcedLoading(ctx, i.getClass(), shardName, mustLoad, implicitShardLoading)
+	return i.initLocalShardWithForcedLoading(ctx, i.getClass(), shardName, true, implicitShardLoading)
 }
 
 func (i *Index) initLocalShardWithForcedLoading(ctx context.Context, class *models.Class, shardName string, mustLoad bool, implicitShardLoading bool) error {
