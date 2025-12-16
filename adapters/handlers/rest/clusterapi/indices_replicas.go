e772c716349af866f313e1b10dd85645e1faca69
moogacs <contact@m-mooga.com>
2025-12-16 15:52:15 +0100

    untangle replication interfaces

--- adapters/handlers/rest/clusterapi/indices_replicas.go
+++ adapters/handlers/rest/clusterapi/indices_replicas.go
@@ -28,48 +28,14 @@ import (
 	"github.com/go-openapi/strfmt"
 	"github.com/sirupsen/logrus"
 
-	"github.com/weaviate/weaviate/cluster/router/types"
 	enterrors "github.com/weaviate/weaviate/entities/errors"
-	"github.com/weaviate/weaviate/entities/storobj"
 	"github.com/weaviate/weaviate/usecases/cluster"
-	"github.com/weaviate/weaviate/usecases/objects"
 	"github.com/weaviate/weaviate/usecases/replica"
 	"github.com/weaviate/weaviate/usecases/replica/hashtree"
 )
 
-type replicator interface {
-	// Write endpoints
-	ReplicateObject(ctx context.Context, indexName, shardName,
-		requestID string, object *storobj.Object, schemaVersion uint64) replica.SimpleResponse
-	ReplicateObjects(ctx context.Context, indexName, shardName,
-		requestID string, objects []*storobj.Object, schemaVersion uint64) replica.SimpleResponse
-	ReplicateUpdate(ctx context.Context, indexName, shardName,
-		requestID string, mergeDoc *objects.MergeDocument, schemaVersion uint64) replica.SimpleResponse
-	ReplicateDeletion(ctx context.Context, indexName, shardName,
-		requestID string, uuid strfmt.UUID, deletionTime time.Time, schemaVersion uint64) replica.SimpleResponse
-	ReplicateDeletions(ctx context.Context, indexName, shardName,
-		requestID string, uuids []strfmt.UUID, deletionTime time.Time, dryRun bool, schemaVersion uint64) replica.SimpleResponse
-	ReplicateReferences(ctx context.Context, indexName, shardName,
-		requestID string, refs []objects.BatchReference, schemaVersion uint64) replica.SimpleResponse
-	CommitReplication(indexName, shardName, requestID string) interface{}
-	AbortReplication(indexName, shardName, requestID string) interface{}
-	OverwriteObjects(ctx context.Context, index, shard string,
-		vobjects []*objects.VObject) ([]types.RepairResponse, error)
-	// Read endpoints
-	FetchObject(ctx context.Context, indexName,
-		shardName string, id strfmt.UUID) (replica.Replica, error)
-	FetchObjects(ctx context.Context, class,
-		shardName string, ids []strfmt.UUID) ([]replica.Replica, error)
-	DigestObjects(ctx context.Context, class, shardName string,
-		ids []strfmt.UUID) (result []types.RepairResponse, err error)
-	DigestObjectsInRange(ctx context.Context, class, shardName string,
-		initialUUID, finalUUID strfmt.UUID, limit int) (result []types.RepairResponse, err error)
-	HashTreeLevel(ctx context.Context, index, shard string,
-		level int, discriminant *hashtree.Bitset) (digests []hashtree.Digest, err error)
-}
-
 type replicatedIndices struct {
-	shards replicator
+	shards *replica.RemoteReplicaIncoming
 	auth   auth
 	// maintenanceModeEnabled is an experimental feature to allow the system to be
 	// put into a maintenance mode where all replicatedIndices requests just return a 418
@@ -122,7 +88,7 @@ var (
 )
 
 func NewReplicatedIndices(
-	shards replicator,
+	shards *replica.RemoteReplicaIncoming,
 	auth auth,
 	maintenanceModeEnabled func() bool,
 	requestQueueConfig cluster.RequestQueueConfig,
