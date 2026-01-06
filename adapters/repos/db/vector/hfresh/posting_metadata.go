7cca9293a81ace5e7585518114092a5e20062672
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2026-01-06 17:20:55 +0400

    add PostingMetadataStore

--- adapters/repos/db/vector/hfresh/posting_metadata.go
+++ adapters/repos/db/vector/hfresh/posting_metadata.go
@@ -1,9 +1,16 @@
 package hfresh
 
-// PostingMetadata holds various information about a posting.
-type PostingMetadata struct {
-	// Size keeps track of the number of vectors in the posting.
-	Size uint32
+import (
+	"context"
+	"encoding/binary"
+
+	"github.com/pkg/errors"
+	"github.com/vmihailenco/msgpack/v5"
+	"github.com/weaviate/weaviate/adapters/repos/db/lsmkv"
+)
+
+// postingMetadata holds various information about a posting.
+type postingMetadata struct {
 	// Vectors holds the IDs of the vectors in the posting.
 	Vectors []uint64
 	// Version keeps track of the version of the posting list.
@@ -12,4 +19,53 @@ type PostingMetadata struct {
 	// It uses a combination of an LSMKV store for persistence and an in-memory
 	// cache for fast access.
 	Version uint32
+}
+
+type PostingMetadataStore struct {
+	bucket    *lsmkv.Bucket
+	keyPrefix byte
+}
+
+func NewPostingMetadataStore(bucket *lsmkv.Bucket, keyPrefix byte) *PostingMetadataStore {
+	return &PostingMetadataStore{
+		bucket:    bucket,
+		keyPrefix: keyPrefix,
+	}
+}
+
+func (p *PostingMetadataStore) key(postingID uint64) [9]byte {
+	var buf [9]byte
+	buf[0] = p.keyPrefix
+	binary.LittleEndian.PutUint64(buf[1:], postingID)
+	return buf
+}
+
+func (p *PostingMetadataStore) Get(ctx context.Context, postingID uint64) (*postingMetadata, error) {
+	key := p.key(postingID)
+	v, err := p.bucket.Get(key[:])
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get posting size for %d", postingID)
+	}
+	if len(v) == 0 {
+		return nil, ErrPostingNotFound
+	}
+
+	var metadata postingMetadata
+	err = msgpack.Unmarshal(v, &metadata)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to unmarshal posting metadata for %d", postingID)
+	}
+
+	return &metadata, nil
+}
+
+func (p *PostingMetadataStore) Set(ctx context.Context, postingID uint64, metadata *postingMetadata) error {
+	key := p.key(postingID)
+
+	data, err := msgpack.Marshal(metadata)
+	if err != nil {
+		return errors.Wrapf(err, "failed to marshal posting metadata for %d", postingID)
+	}
+
+	return p.bucket.Put(key[:], data)
 }
