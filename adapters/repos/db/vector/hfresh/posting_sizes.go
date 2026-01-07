2ed8773014786276935807c617c302b6dbd840b8
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2026-01-07 11:51:30 +0400

    remove posting sizes bucket

--- adapters/repos/db/vector/hfresh/posting_sizes.go
+++ adapters/repos/db/vector/hfresh/posting_sizes.go
@@ -13,10 +13,8 @@ package hfresh
 
 import (
 	"context"
-	"encoding/binary"
 	"fmt"
 
-	"github.com/maypok86/otter/v2"
 	"github.com/pkg/errors"
 	"github.com/weaviate/weaviate/adapters/repos/db/lsmkv"
 )
@@ -24,8 +22,6 @@ import (
 // These constants define the prefixes used in the
 // lsmkv bucket to namespace different types of data.
 const (
-	postingSizeBucketPrefix     = 's'
-	postingVersionBucketPrefix  = 'l'
 	versionMapBucketPrefix      = 'v'
 	metadataBucketPrefix        = 'm'
 	postingMetadataBucketPrefix = 'p'
@@ -49,123 +45,4 @@ func NewSharedBucket(store *lsmkv.Store, indexID string, cfg StoreConfig) (*lsmk
 
 func sharedBucketName(id string) string {
 	return fmt.Sprintf("hfresh_shared_%s", id)
-}
-
-// PostingSizes keeps track of the number of vectors in each posting.
-// It uses a combination of an LSMKV store for persistence and an in-memory
-// cache for fast access.
-type PostingSizes struct {
-	metrics *Metrics
-	cache   *otter.Cache[uint64, uint32]
-	store   *PostingSizeStore
-}
-
-func NewPostingSizes(bucket *lsmkv.Bucket, metrics *Metrics) (*PostingSizes, error) {
-	pStore := NewPostingSizeStore(bucket, postingSizeBucketPrefix)
-
-	cache, err := otter.New(&otter.Options[uint64, uint32]{
-		MaximumSize: 10_000,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	return &PostingSizes{
-		cache:   cache,
-		metrics: metrics,
-		store:   pStore,
-	}, nil
-}
-
-// Get returns the size of the posting with the given ID.
-func (v *PostingSizes) Get(ctx context.Context, postingID uint64) (uint32, error) {
-	size, err := v.cache.Get(ctx, postingID, otter.LoaderFunc[uint64, uint32](func(ctx context.Context, key uint64) (uint32, error) {
-		size, err := v.store.Get(ctx, postingID)
-		if err != nil {
-			if errors.Is(err, ErrPostingNotFound) {
-				return 0, otter.ErrNotFound
-			}
-
-			return 0, err
-		}
-
-		return size, nil
-	}))
-	if errors.Is(err, otter.ErrNotFound) {
-		return 0, ErrPostingNotFound
-	}
-
-	return size, err
-}
-
-// Sets the size of the posting to newSize.
-// This method assumes the posting has been locked for writing by the caller.
-func (v *PostingSizes) Set(ctx context.Context, postingID uint64, newSize uint32) error {
-	err := v.store.Set(ctx, postingID, newSize)
-	if err != nil {
-		return err
-	}
-	v.cache.Set(postingID, newSize)
-	v.metrics.ObservePostingSize(float64(newSize))
-	return nil
-}
-
-// Incr increments the size of the posting by delta and returns the new size.
-// This method assumes the posting has been locked for writing by the caller.
-func (v *PostingSizes) Inc(ctx context.Context, postingID uint64, delta uint32) (uint32, error) {
-	old, err := v.Get(ctx, postingID)
-	if err != nil {
-		if !errors.Is(err, ErrPostingNotFound) {
-			return 0, err
-		}
-	}
-
-	newSize := old + delta
-	err = v.store.Set(ctx, postingID, newSize)
-	if err != nil {
-		return 0, err
-	}
-
-	v.cache.Set(postingID, newSize)
-	v.metrics.ObservePostingSize(float64(newSize))
-	return newSize, nil
-}
-
-// PostingSizeStore is a persistent store for posting sizes.
-// It stores the sizes in an LSMKV bucket.
-type PostingSizeStore struct {
-	bucket    *lsmkv.Bucket
-	keyPrefix byte
-}
-
-func NewPostingSizeStore(bucket *lsmkv.Bucket, keyPrefix byte) *PostingSizeStore {
-	return &PostingSizeStore{
-		bucket:    bucket,
-		keyPrefix: keyPrefix,
-	}
-}
-
-func (p *PostingSizeStore) key(postingID uint64) [9]byte {
-	var buf [9]byte
-	buf[0] = p.keyPrefix
-	binary.LittleEndian.PutUint64(buf[1:], postingID)
-	return buf
-}
-
-func (p *PostingSizeStore) Get(ctx context.Context, postingID uint64) (uint32, error) {
-	key := p.key(postingID)
-	v, err := p.bucket.Get(key[:])
-	if err != nil {
-		return 0, errors.Wrapf(err, "failed to get posting size for %d", postingID)
-	}
-	if len(v) == 0 {
-		return 0, ErrPostingNotFound
-	}
-
-	return binary.LittleEndian.Uint32(v), nil
-}
-
-func (p *PostingSizeStore) Set(ctx context.Context, postingID uint64, size uint32) error {
-	key := p.key(postingID)
-	return p.bucket.Put(key[:], binary.LittleEndian.AppendUint32(nil, size))
 }
