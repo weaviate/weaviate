761608dda13bee330a1fabbb49d81cfc70996e8f
Roberto Esposito <robyesposito2000@gmail.com>
2026-01-19 12:36:28 +0100

    add metrics rather than set

--- adapters/repos/db/vector/hfresh/metrics.go
+++ adapters/repos/db/vector/hfresh/metrics.go
@@ -277,7 +277,7 @@ func (m *Metrics) SetSplitCount(count int64) {
 		return
 	}
 
-	m.splitsPending.Set(float64(count))
+	m.splitsPending.Add(float64(count))
 }
 
 func (m *Metrics) EnqueueMergeTask() {
@@ -317,7 +317,7 @@ func (m *Metrics) SetMergeCount(count int64) {
 		return
 	}
 
-	m.mergesPending.Set(float64(count))
+	m.mergesPending.Add(float64(count))
 }
 
 func (m *Metrics) EnqueueReassignTask() {
@@ -357,7 +357,7 @@ func (m *Metrics) SetReassignCount(count int64) {
 		return
 	}
 
-	m.reassignsPending.Set(float64(count))
+	m.reassignsPending.Add(float64(count))
 }
 
 func (m *Metrics) CentroidSearchDuration(start time.Time) {
