147d87208b9eb4e39f83e8bd7e2dc713670e5f6a
moogacs <contact@m-mooga.com>
2026-01-21 10:43:01 +0100

    Revert "allow incoming get"
    This reverts commit 34338f92356b2b3a97b0d8e1ded1438c98d160bb.

--- adapters/repos/db/index.go
+++ adapters/repos/db/index.go
@@ -1498,6 +1498,10 @@ func (i *Index) IncomingGetObject(ctx context.Context, shardName string,
 		return nil, enterrors.NewErrUnprocessable(fmt.Errorf("local %s shard does not exist", shardName))
 	}
 
+	if shard.GetStatus() == storagestate.StatusLoading {
+		return nil, enterrors.NewErrUnprocessable(fmt.Errorf("local %s shard is not ready", shardName))
+	}
+
 	return shard.ObjectByID(ctx, id, props, additional)
 }
 
@@ -1512,6 +1516,10 @@ func (i *Index) IncomingMultiGetObjects(ctx context.Context, shardName string,
 
 	if shard == nil {
 		return nil, enterrors.NewErrUnprocessable(fmt.Errorf("local %s shard does not exist", shardName))
+	}
+
+	if shard.GetStatus() == storagestate.StatusLoading {
+		return nil, enterrors.NewErrUnprocessable(fmt.Errorf("local %s shard is not ready", shardName))
 	}
 
 	return shard.MultiObjectByID(ctx, wrapIDsInMulti(ids))
