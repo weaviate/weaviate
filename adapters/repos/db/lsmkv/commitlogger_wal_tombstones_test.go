a8f8b90d7681a64ddaeb47c7970d1be5698ca5d3
André Mourão <andre.mourao@weaviate.io>
2026-01-21 17:35:43 +0000

    test: also test no wal reuse

--- adapters/repos/db/lsmkv/commitlogger_wal_tombstones_test.go
+++ adapters/repos/db/lsmkv/commitlogger_wal_tombstones_test.go
@@ -14,6 +14,7 @@ package lsmkv
 import (
 	"context"
 	"encoding/binary"
+	"fmt"
 	"testing"
 
 	"github.com/sirupsen/logrus/hooks/test"
@@ -22,97 +23,126 @@ import (
 	"github.com/weaviate/weaviate/entities/cyclemanager"
 )
 
-func TestTombstoneWALReuseUpdate(t *testing.T) {
-	strategy := StrategyInverted
-	ctx := context.Background()
-	dirName := t.TempDir()
+func TestTombstoneWALReuse(t *testing.T) {
+	walThresholds := []int{0, 4096}
 
-	logger, _ := test.NewNullLogger()
+	for _, thresh := range walThresholds {
+		opts := []BucketOption{
+			WithStrategy(StrategyInverted),
+			WithBitmapBufPool(roaringset.NewBitmapBufPoolNoop()),
+			WithMinWalThreshold(int64(thresh)),
+		}
+		t.Run(fmt.Sprintf("update with threshold %d", thresh), func(t *testing.T) {
+			ctx := context.Background()
+			dirName := t.TempDir()
 
-	// initial bucket, always create segment, even if it is just a single entry
-	b, err := NewBucketCreator().NewBucket(ctx, dirName, "", logger, nil,
-		cyclemanager.NewCallbackGroupNoop(), cyclemanager.NewCallbackGroupNoop(),
-		WithStrategy(strategy), WithMinWalThreshold(4096),
-		WithBitmapBufPool(roaringset.NewBitmapBufPoolNoop()))
-	require.NoError(t, err)
+			logger, _ := test.NewNullLogger()
 
-	docId := uint64(1)
-	require.NoError(t, b.MapSet([]byte("word1"), NewMapPairFromDocIdAndTf(docId, 1, 1, false)))
-	require.NoError(t, b.MapSet([]byte("word2"), NewMapPairFromDocIdAndTf(docId, 2, 2, false)))
+			// initial bucket, always create segment, even if it is just a single entry
+			b, err := NewBucketCreator().NewBucket(ctx, dirName, "", logger, nil,
+				cyclemanager.NewCallbackGroupNoop(), cyclemanager.NewCallbackGroupNoop(), opts...)
+			require.NoError(t, err)
 
-	err = b.FlushAndSwitch()
-	require.NoError(t, err)
+			docId := uint64(1)
+			require.NoError(t, b.MapSet([]byte("word1"), NewMapPairFromDocIdAndTf(docId, 1, 1, false)))
+			require.NoError(t, b.MapSet([]byte("word2"), NewMapPairFromDocIdAndTf(docId, 2, 2, false)))
 
-	require.NoError(t, b.Shutdown(ctx))
-	dbFiles, walFiles := countDbAndWalFiles(t, dirName)
-	require.Equal(t, dbFiles, 1)
-	require.Equal(t, walFiles, 0)
+			err = b.FlushAndSwitch()
+			require.NoError(t, err)
 
-	// test after reload
-	b, err = NewBucketCreator().NewBucket(ctx, dirName, "", logger, nil,
-		cyclemanager.NewCallbackGroupNoop(), cyclemanager.NewCallbackGroupNoop(),
-		WithStrategy(strategy), WithMinWalThreshold(4096),
-		WithBitmapBufPool(roaringset.NewBitmapBufPoolNoop()))
-	require.NoError(t, err)
+			require.NoError(t, b.Shutdown(ctx))
+			dbFiles, walFiles := countDbAndWalFiles(t, dirName)
 
-	key := make([]byte, 8)
-	binary.BigEndian.PutUint64(key, docId)
-	// for the inverted strategy, this adds a tombstone for docId 1
-	// and expects all entries for docId 1 to be ignored from now on
-	require.NoError(t, b.MapDeleteKey([]byte("word1"), key))
+			require.Equal(t, dbFiles, 1)
+			require.Equal(t, walFiles, 0)
 
-	require.NoError(t, b.Shutdown(ctx))
-	dbFiles, walFiles = countDbAndWalFiles(t, dirName)
-	require.Equal(t, dbFiles, 1)
-	require.Equal(t, walFiles, 1)
+			// test after reload
+			b, err = NewBucketCreator().NewBucket(ctx, dirName, "", logger, nil,
+				cyclemanager.NewCallbackGroupNoop(), cyclemanager.NewCallbackGroupNoop(), opts...,
+			)
+			require.NoError(t, err)
 
-	b, err = NewBucketCreator().NewBucket(ctx, dirName, "", logger, nil,
-		cyclemanager.NewCallbackGroupNoop(), cyclemanager.NewCallbackGroupNoop(),
-		WithStrategy(strategy), WithMinWalThreshold(4096),
-		WithBitmapBufPool(roaringset.NewBitmapBufPoolNoop()))
-	require.NoError(t, err)
+			key := make([]byte, 8)
+			binary.BigEndian.PutUint64(key, docId)
+			// for the inverted strategy, this adds a tombstone for docId 1
+			// and expects all entries for docId 1 to be ignored from now on
+			require.NoError(t, b.MapDeleteKey([]byte("word1"), key))
+			require.NoError(t, b.Shutdown(ctx))
 
-	res, err := b.MapList(ctx, []byte("word2"))
-	require.NoError(t, err)
-	require.Len(t, res, 0)
-}
+			dbFiles, walFiles = countDbAndWalFiles(t, dirName)
 
-func TestTombstoneWALReuseDelete(t *testing.T) {
-	strategy := StrategyInverted
-	ctx := context.Background()
-	dirName := t.TempDir()
+			if thresh == 0 {
+				// with threshold 0, we always flush to a segment on shutdown
+				require.Equal(t, dbFiles, 2)
+				require.Equal(t, walFiles, 0)
+			} else {
+				// with higher thresholds, we may not have flushed yet
+				require.Equal(t, dbFiles, 1)
+				require.Equal(t, walFiles, 1)
+			}
 
-	logger, _ := test.NewNullLogger()
+			b, err = NewBucketCreator().NewBucket(ctx, dirName, "", logger, nil,
+				cyclemanager.NewCallbackGroupNoop(), cyclemanager.NewCallbackGroupNoop(), opts...)
+			require.NoError(t, err)
 
-	// initial bucket, always create segment, even if it is just a single entry
-	b, err := NewBucketCreator().NewBucket(ctx, dirName, "", logger, nil,
-		cyclemanager.NewCallbackGroupNoop(), cyclemanager.NewCallbackGroupNoop(),
-		WithStrategy(strategy), WithMinWalThreshold(4096),
-		WithBitmapBufPool(roaringset.NewBitmapBufPoolNoop()))
-	require.NoError(t, err)
+			res, err := b.MapList(ctx, []byte("word1"))
+			require.NoError(t, err)
+			require.Len(t, res, 0)
 
-	docId := uint64(1)
-	docIdKey := make([]byte, 8)
-	binary.BigEndian.PutUint64(docIdKey, docId)
-	require.NoError(t, b.MapSet([]byte("word1"), NewMapPairFromDocIdAndTf(docId, 1, 1, false)))
-	require.NoError(t, b.MapSet([]byte("word2"), NewMapPairFromDocIdAndTf(docId, 2, 2, false)))
-	require.NoError(t, b.MapDeleteKey([]byte("word1"), docIdKey))
+			res, err = b.MapList(ctx, []byte("word2"))
+			require.NoError(t, err)
+			require.Len(t, res, 0)
+		})
 
-	require.NoError(t, err)
+		t.Run(fmt.Sprintf("delete with threshold %d", thresh), func(t *testing.T) {
+			opts := []BucketOption{
+				WithStrategy(StrategyInverted),
+				WithBitmapBufPool(roaringset.NewBitmapBufPoolNoop()),
+				WithMinWalThreshold(int64(thresh)),
+			}
+			ctx := context.Background()
+			dirName := t.TempDir()
 
-	require.NoError(t, b.Shutdown(ctx))
-	dbFiles, walFiles := countDbAndWalFiles(t, dirName)
-	require.Equal(t, dbFiles, 0)
-	require.Equal(t, walFiles, 1)
+			logger, _ := test.NewNullLogger()
 
-	// test after reload
-	b, err = NewBucketCreator().NewBucket(ctx, dirName, "", logger, nil,
-		cyclemanager.NewCallbackGroupNoop(), cyclemanager.NewCallbackGroupNoop(),
-		WithStrategy(strategy), WithMinWalThreshold(4096),
-		WithBitmapBufPool(roaringset.NewBitmapBufPoolNoop()))
-	require.NoError(t, err)
+			// initial bucket, always create segment, even if it is just a single entry
+			b, err := NewBucketCreator().NewBucket(ctx, dirName, "", logger, nil,
+				cyclemanager.NewCallbackGroupNoop(), cyclemanager.NewCallbackGroupNoop(), opts...)
+			require.NoError(t, err)
 
-	res, err := b.MapList(ctx, []byte("word2"))
-	require.NoError(t, err)
-	require.Len(t, res, 0)
+			docId := uint64(1)
+			docIdKey := make([]byte, 8)
+			binary.BigEndian.PutUint64(docIdKey, docId)
+			require.NoError(t, b.MapSet([]byte("word1"), NewMapPairFromDocIdAndTf(docId, 1, 1, false)))
+			require.NoError(t, b.MapSet([]byte("word2"), NewMapPairFromDocIdAndTf(docId, 2, 2, false)))
+			require.NoError(t, b.MapDeleteKey([]byte("word1"), docIdKey))
+
+			require.NoError(t, err)
+
+			require.NoError(t, b.Shutdown(ctx))
+			dbFiles, walFiles := countDbAndWalFiles(t, dirName)
+			if thresh == 0 {
+				// with threshold 0, we always flush to a segment on shutdown
+				require.Equal(t, dbFiles, 1)
+				require.Equal(t, walFiles, 0)
+			} else {
+				// with higher thresholds, we may not have flushed yet
+				require.Equal(t, dbFiles, 0)
+				require.Equal(t, walFiles, 1)
+			}
+
+			// test after reload
+			b, err = NewBucketCreator().NewBucket(ctx, dirName, "", logger, nil,
+				cyclemanager.NewCallbackGroupNoop(), cyclemanager.NewCallbackGroupNoop(), opts...)
+			require.NoError(t, err)
+
+			res, err := b.MapList(ctx, []byte("word1"))
+			require.NoError(t, err)
+			require.Len(t, res, 0)
+
+			res, err = b.MapList(ctx, []byte("word2"))
+			require.NoError(t, err)
+			require.Len(t, res, 0)
+		})
+	}
 }
