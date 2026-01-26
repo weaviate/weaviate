5c1b691dda274894872866c4c890890c81c31919
André Mourão <andre.mourao@weaviate.io>
2026-01-26 23:47:33 +0000

    fix: always send al least an empty dict

--- entities/storobj/storage_object.go
+++ entities/storobj/storage_object.go
@@ -817,6 +817,14 @@ func (ko *Object) MarshalBinaryOptional(addProps additional.Properties) ([]byte,
 			return nil, fmt.Errorf("could not marshal '%s' max length exceeded (%d/%d)", "schema", len(schema), maxSchemaLength)
 		}
 		schemaLength = uint32(len(schema))
+	} else {
+		// marshall empty object so that we don't break unmarshalling during upgrades
+		// where some nodes don't have the empty check on the unmarshal side yet
+		schema, err = json.Marshal(map[string]interface{}{})
+		if err != nil {
+			return nil, err
+		}
+		schemaLength = uint32(len(schema))
 	}
 
 	meta, err := json.Marshal(ko.AdditionalProperties())
