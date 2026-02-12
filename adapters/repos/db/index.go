f8795a44f6d720d2ee78fe90da856ec8f2c73639
moogacs <contact@m-mooga.com>
2026-02-12 01:32:13 +0100

    resolve comments

--- adapters/repos/db/index.go
+++ adapters/repos/db/index.go
@@ -1951,7 +1951,7 @@ func (i *Index) localShardSearch(ctx context.Context, searchVectors []models.Vec
 	sort []filters.Sort, groupBy *searchparams.GroupBy, additionalProps additional.Properties,
 	targetCombination *dto.TargetCombination, properties []string, tenantName string, shardName string,
 ) ([]*storobj.Object, []float32, error) {
-	shard, release, err := i.getOptInitLocalShard(ctx, shardName, i.Config.AutoTenantActivation)
+	shard, release, err := i.GetShard(ctx, shardName)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -1982,13 +1982,11 @@ func (i *Index) remoteShardSearch(ctx context.Context, searchVectors []models.Ve
 	var outObjects []*storobj.Object
 	var outScores []float32
 
-	shard, release, err := i.getOptInitLocalShard(ctx, shardName, i.Config.AutoTenantActivation)
+	shard, release, err := i.GetShard(ctx, shardName)
 	if err != nil {
 		return nil, nil, err
 	}
-	if shard != nil {
-		defer release()
-	}
+	defer release()
 
 	if i.Config.ForceFullReplicasSearch {
 		// Force a search on all the replicas for the shard
@@ -2088,11 +2086,12 @@ func (i *Index) objectVectorSearch(ctx context.Context, searchVectors []models.V
 		return nil
 	}
 	localSearch := func(shardName string) error {
-		shard, release, err := i.getOptInitLocalShard(ctx, shardName, i.Config.AutoTenantActivation)
-		defer release()
+		shard, release, err := i.GetShard(ctx, shardName)
 		if err != nil {
 			return err
 		}
+		defer release()
+
 		if shard != nil {
 			localShardResult, localShardScores, err1 := i.localShardSearch(ctx, searchVectors, targetVectors, dist, limit, localFilters, sort, groupBy, additionalProps, targetCombination, properties, tenant, shardName)
 			if err1 != nil {
