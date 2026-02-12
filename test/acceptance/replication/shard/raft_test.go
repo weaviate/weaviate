35a7c4054acece61f133d60f838ea1a9f1f0097e
Tommy Smith <tommy@weaviate.io>
2026-02-12 13:37:00 +0000

    Add more acceptance tests of shard raft

--- test/acceptance/replication/shard/raft_test.go
+++ test/acceptance/replication/shard/raft_test.go
@@ -12,7 +12,10 @@ import (
 	"github.com/weaviate/weaviate/test/helper/sample-schema/articles"
 )
 
-const UUID = strfmt.UUID("73f2eb5f-5abf-447a-81ca-74b1dd168241")
+const (
+	UUID1 = strfmt.UUID("73f2eb5f-5abf-447a-81ca-74b1dd168241")
+	UUID2 = strfmt.UUID("d1c9e5b8-9a3e-4c8b-9f1e-2a5f6b7c8d9e")
+)
 
 func Test_RaftShardReplication(t *testing.T) {
 	helper.SetupClient("localhost:8080")
@@ -25,20 +28,60 @@ func Test_RaftShardReplication(t *testing.T) {
 
 	helper.DeleteClass(t, cls.Class)
 	helper.CreateClass(t, cls)
-	helper.CreateObject(t, articles.NewParagraph().WithContents("RAFT").WithID(UUID).Object())
 
-	// Verify the object eventually exists on all nodes
-	require.EventuallyWithT(t, func(ct *assert.CollectT) {
-		for _, port := range []string{"8080", "8081", "8082"} {
-			obj := getObj(t, cls.Class, port)
-			assert.NotNil(ct, obj, "Object should exist on node at port %s", port)
-		}
-	}, 60*time.Second, 1*time.Second)
+	t.Run("add object", func(t *testing.T) {
+		helper.CreateObject(t, articles.NewParagraph().WithContents("RAFT").WithID(UUID1).Object())
+		// Verify the object eventually exists on all nodes
+		require.EventuallyWithT(t, func(ct *assert.CollectT) {
+			for _, port := range []string{"8080", "8081", "8082"} {
+				obj, err := getObj(t, port, cls.Class, UUID1)
+				assert.Nil(ct, err, "Object should exist on node at port %s", port)
+				assert.NotNil(ct, obj, "Object should exist on node at port %s", port)
+			}
+		}, 10*time.Second, 1*time.Second)
+	})
+
+	t.Run("update object", func(t *testing.T) {
+		helper.UpdateObject(t, articles.NewParagraph().WithContents("RAFT Updated").WithID(UUID1).Object())
+		// Verify the update eventually exists on all nodes
+		require.EventuallyWithT(t, func(ct *assert.CollectT) {
+			for _, port := range []string{"8080", "8081", "8082"} {
+				obj, err := getObj(t, port, cls.Class, UUID1)
+				assert.Nil(ct, err, "Object should exist on node at port %s", port)
+				assert.NotNil(ct, obj, "Object should exist on node at port %s", port)
+				assert.Equal(ct, "RAFT Updated", obj.Properties.(map[string]any)["contents"], "Object should be updated on node at port %s", port)
+			}
+		}, 10*time.Second, 1*time.Second)
+	})
+
+	t.Run("create and update object", func(t *testing.T) {
+		helper.CreateObject(t, articles.NewParagraph().WithContents("RAFT New").WithID(UUID2).Object())
+		helper.UpdateObject(t, articles.NewParagraph().WithContents("RAFT New Updated").WithID(UUID2).Object())
+
+		// Verify the new object and update eventually exist on all nodes
+		require.EventuallyWithT(t, func(ct *assert.CollectT) {
+			for _, port := range []string{"8080", "8081", "8082"} {
+				obj, err := getObj(t, port, cls.Class, UUID2)
+				assert.Nil(ct, err, "Object should exist on node at port %s", port)
+				assert.NotNil(ct, obj, "Object should exist on node at port %s", port)
+				assert.Equal(ct, "RAFT New Updated", obj.Properties.(map[string]any)["contents"], "Object should be updated on node at port %s", port)
+			}
+		}, 10*time.Second, 1*time.Second)
+	})
+
+	t.Run("delete object", func(t *testing.T) {
+		helper.DeleteObject(t, &models.Object{Class: cls.Class, ID: UUID2})
+		// Verify the object is eventually deleted on all nodes
+		require.EventuallyWithT(t, func(ct *assert.CollectT) {
+			for _, port := range []string{"8080", "8081", "8082"} {
+				_, err := getObj(t, port, cls.Class, UUID2)
+				assert.NotNil(ct, err, "Object should be deleted on node at port %s", port)
+			}
+		}, 10*time.Second, 1*time.Second)
+	})
 }
 
-func getObj(t *testing.T, cls, port string) *models.Object {
+func getObj(t *testing.T, port, cls string, uuid strfmt.UUID) (*models.Object, error) {
 	helper.SetupClient("localhost:" + port)
-	obj, err := helper.GetObject(t, cls, UUID)
-	require.NoError(t, err)
-	return obj
+	return helper.GetObject(t, cls, uuid)
 }
