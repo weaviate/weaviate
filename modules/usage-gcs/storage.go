8fcb5b99da0f7a7852ab08c6429717760d9ffb17
gkampitakis <gkabitakis@gmail.com>
2026-02-24 17:13:09 +0200

    chore: rename for consistency

--- modules/usage-gcs/storage.go
+++ modules/usage-gcs/storage.go
@@ -46,12 +46,12 @@ func NewGCSStorage(ctx context.Context, logger logrus.FieldLogger, metrics *comm
 		options = append(options, option.WithoutAuthentication())
 	}
 
-	useTokenSource := os.Getenv("GCS_AUTH_PROXY_ENDPOINT") != ""
+	useTokenSource := os.Getenv("USAGE_GCS_AUTH_PROXY_ENDPOINT") != ""
 	if useTokenSource {
 		options = append(
 			options,
 			option.WithTokenSource(
-				oauth2.ReuseTokenSource(nil, gcpcommon.NewAuthBrokerTokenSource(os.Getenv("GCS_AUTH_PROXY_ENDPOINT"))),
+				oauth2.ReuseTokenSource(nil, gcpcommon.NewAuthBrokerTokenSource(os.Getenv("USAGE_GCS_AUTH_PROXY_ENDPOINT"))),
 			),
 		)
 	}
