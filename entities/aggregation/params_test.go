58d9af8bae2eac707f2e4cae8e170e1c9637d664
dyma solovei <dyma@weaviate.io>
2026-03-19 19:34:08 +0100

    test: unit test IsCountStar

--- entities/aggregation/params_test.go
+++ entities/aggregation/params_test.go
@@ -17,6 +17,8 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/weaviate/weaviate/entities/filters"
+	"github.com/weaviate/weaviate/entities/searchparams"
 )
 
 func Test_Params_Unmarshal(t *testing.T) {
@@ -57,6 +59,106 @@ func Test_Params_Unmarshal(t *testing.T) {
 				assert.True(t, ok)
 				assert.True(t, len(vector) > 0)
 			}
+		})
+	}
+}
+
+func TestIsCountStar(t *testing.T) {
+	limit := 92
+	for _, tt := range []struct {
+		name   string
+		params Params
+		want   bool
+	}{
+		{
+			name: "count(*)",
+			params: Params{
+				IncludeMetaCount: true,
+			},
+			want: true,
+		},
+		{
+			name: "filtered",
+			params: Params{
+				IncludeMetaCount: true,
+				Filters:          new(filters.LocalFilter),
+			},
+		},
+		{
+			name: "requests properties",
+			params: Params{
+				IncludeMetaCount: true,
+				Properties:       make([]ParamProperty, 5),
+			},
+		},
+		{
+			name: "has limit",
+			params: Params{
+				IncludeMetaCount: true,
+				Limit:            &limit,
+			},
+		},
+		{
+			name: "has object limit",
+			params: Params{
+				IncludeMetaCount: true,
+				ObjectLimit:      &limit,
+			},
+		},
+		{
+			name: "has vector",
+			params: Params{
+				IncludeMetaCount: true,
+				SearchVector:     []float32{1, 2, 3},
+			},
+		},
+		{
+			name: "has target vector",
+			params: Params{
+				IncludeMetaCount: true,
+				TargetVector:     "v1",
+			},
+		},
+		{
+			name: "has module params",
+			params: Params{
+				IncludeMetaCount: true,
+				ModuleParams: map[string]interface{}{
+					"nearText": "sunshine",
+				},
+			},
+		},
+		{
+			name: "has near vector",
+			params: Params{
+				IncludeMetaCount: true,
+				NearVector:       new(searchparams.NearVector),
+			},
+		},
+		{
+			name: "has near object",
+			params: Params{
+				IncludeMetaCount: true,
+				NearObject:       new(searchparams.NearObject),
+			},
+		},
+		{
+			name: "has hybrid",
+			params: Params{
+				IncludeMetaCount: true,
+				Hybrid:           new(searchparams.HybridSearch),
+			},
+		},
+		{
+			name: "grouped",
+			params: Params{
+				IncludeMetaCount: true,
+				GroupBy:          new(filters.Path),
+			},
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			require.Equal(t, tt.want, IsCountStar(&tt.params))
 		})
 	}
 }
