c1a05c102ad07d69cf3914140479e28a810cd3f2
Marcin Antas <antas.marcin@gmail.com>
2026-03-19 12:30:57 +0100

    Revert "Merge pull request #10803 from weaviate/prepare-release-v1.35.15"
    This reverts commit b720a7ba0f70d227db289455d566a4cfec939939, reversing
    changes made to 0b1d665373ebc64d3ea87dde92e6c6d9640ddadd.

--- adapters/handlers/rest/embedded_spec.go
+++ adapters/handlers/rest/embedded_spec.go
@@ -48,7 +48,7 @@ func init() {
       "url": "https://github.com/weaviate",
       "email": "hello@weaviate.io"
     },
-    "version": "1.35.15"
+    "version": "1.35.14"
   },
   "basePath": "/v1",
   "paths": {
@@ -9427,7 +9427,7 @@ func init() {
       "url": "https://github.com/weaviate",
       "email": "hello@weaviate.io"
     },
-    "version": "1.35.15"
+    "version": "1.35.14"
   },
   "basePath": "/v1",
   "paths": {
