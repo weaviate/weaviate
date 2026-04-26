485da0934c152fd98dbee29245263a4249d17099
orbitwebsites-cloud <orbitwebsites@gmail.com>
2026-04-26 19:50:31 -0400

    fix: address reviewer requested changes

--- adapters/repos/db/vector/common/query_vector_distancer.go
+++ adapters/repos/db/vector/common/query_vector_distancer.go
@@ -5,20 +5,13 @@
 //  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
 //   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
 // 
-// Orca Security Scan Summary
-// | Status  | Check | Issues by priority |   |
-// | ------- | ----- | ------------------ | - |
-// | <img width="16" alt="Passed" src="https://raw.githubusercontent.com/orcasecurity/orca-cli/main/resources/images/prcomment/status_v1/passed.png" title="Passed"> Passed | Infrastructure as Code | <img width="12" alt="high" src="https://raw.githubusercontent.com/orcasecurity/orca-cli/main/resources/images/prcomment/priority/high.png" title="High"> 0 &emsp; <img width="12" alt="medium" src="https://raw.githubusercontent.com/orcasecurity/orca-cli/main/resources/images/prcomment/priority/medium.png" title="Medium"> 0 &emsp; <img width="12" alt="low" src="https://raw.githubusercontent.com/orcasecurity/orca-cli/main/resources/images/prcomment/priority/low.png" title="Low"> 0 
-//
 package common
 
 import (
 	"context"
 	"database/sql"
-	"errors"
 	"fmt"
 
-	"github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
 )
 
@@ -35,10 +28,10 @@ func (q *QueryVectorDistancer) Distance(ctx context.Context, vectorID uuid.UUID)
 	var distance float64
 	err := q.db.QueryRowContext(ctx, query, vectorID).Scan(&distance)
 	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
+		if err == sql.ErrNoRows {
 			return 0, fmt.Errorf("vector not found: %w", err)
 		}
-		return 0, err
+		return 0, fmt.Errorf("failed to query vector distance: %w", err)
 	}
 	return distance, nil
 }
