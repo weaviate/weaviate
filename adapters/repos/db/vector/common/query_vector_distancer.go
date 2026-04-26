2ac3054204e4685d48ed488e826aff7465537b48
orbitwebsites-cloud <orbitwebsites@gmail.com>
2026-04-26 19:20:21 -0400

    fix: address reviewer requested changes

--- adapters/repos/db/vector/common/query_vector_distancer.go
+++ adapters/repos/db/vector/common/query_vector_distancer.go
@@ -4,6 +4,6 @@
 // \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
 //  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
 //   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
-// 
-// No changes were requested for this file, the review comment appears to be related to an Orca Security Scan Summary and does not contain any specific changes for this file. 
+//
+// No changes were requested for this file, the review comment appears to be related to an Orca Security Scan Summary and does not contain any specific changes for this file.
 // The file remains unchanged as there were no specific changes requested by the reviewer.
