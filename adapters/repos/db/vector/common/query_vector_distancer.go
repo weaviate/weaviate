45ce80f1b3b36708b3faf4f024d296232245be04
orbitwebsites-cloud <orbitwebsites@gmail.com>
2026-04-26 23:43:23 -0400

    fix: address reviewer requested changes

--- adapters/repos/db/vector/common/query_vector_distancer.go
+++ adapters/repos/db/vector/common/query_vector_distancer.go
@@ -1,10 +1,4 @@
 // File: adapters/repos/db/vector/common/query_vector_distancer.go
-//                           _       _
-// __      _____  __ ___   ___  __ _| |_ ___
-// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
-//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
-//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
-// 
 package common
 
 import (
