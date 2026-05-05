41efd3cbb74297b3a86aa514eeb8558d2121d094
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2026-05-05 13:19:38 +0530

    change prefix

--- adapters/repos/db/vector/hfresh/index_metadata.go
+++ adapters/repos/db/vector/hfresh/index_metadata.go
@@ -42,9 +42,8 @@ const (
 var (
 	indexMetadataBucketPrefix  = []byte{sharedBucketVersionV1, 0}
 	versionMapBucketPrefix     = []byte{sharedBucketVersionV1, 1}
-	postingMapBucketPrefix     = []byte{sharedBucketVersionV1, 2}
+	postingSizesBucketPrefix   = []byte{sharedBucketVersionV1, 2}
 	postingVersionBucketPrefix = []byte{sharedBucketVersionV1, 3}
-	postingSizesBucketPrefix   = []byte{sharedBucketVersionV1, 4}
 )
 
 // reassignBucketKey is used to track vectors that need to be reassigned to new postings.
