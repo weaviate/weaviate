b2fdf32356b2496654e546688eb94d108d4b7b3e
Michelangelo Partipilo <michelangelo.partipilo@gmail.com>
2026-05-11 11:25:25 +0200

    prepare release v1.37.3

--- adapters/handlers/rest/embedded_spec.go
+++ adapters/handlers/rest/embedded_spec.go
@@ -48,7 +48,7 @@ func init() {
       "url": "https://github.com/weaviate",
       "email": "hello@weaviate.io"
     },
-    "version": "1.37.2"
+    "version": "1.37.3"
   },
   "basePath": "/v1",
   "paths": {
@@ -10127,7 +10127,7 @@ func init() {
       "url": "https://github.com/weaviate",
       "email": "hello@weaviate.io"
     },
-    "version": "1.37.2"
+    "version": "1.37.3"
   },
   "basePath": "/v1",
   "paths": {
