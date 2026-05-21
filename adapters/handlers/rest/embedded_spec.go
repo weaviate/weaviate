2a403cacbb3e8c203b5e90c6a87069e48b244ff5
Marcin Antas <antas.marcin@gmail.com>
2026-05-21 11:33:29 +0200

    temp version bump

--- adapters/handlers/rest/embedded_spec.go
+++ adapters/handlers/rest/embedded_spec.go
@@ -48,7 +48,7 @@ func init() {
       "url": "https://github.com/weaviate",
       "email": "hello@weaviate.io"
     },
-    "version": "1.37.4"
+    "version": "1.37.5"
   },
   "basePath": "/v1",
   "paths": {
@@ -10231,7 +10231,7 @@ func init() {
       "url": "https://github.com/weaviate",
       "email": "hello@weaviate.io"
     },
-    "version": "1.37.4"
+    "version": "1.37.5"
   },
   "basePath": "/v1",
   "paths": {
