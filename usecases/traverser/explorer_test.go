69bb65ea37c000eb1af481e642be03cbef48545f
root <root@KennedyDs.localdomain>
2026-05-21 18:13:33 +0800

    feat: refactor queryVector tests to table-driven to reduce duplication

--- usecases/traverser/explorer_test.go
+++ usecases/traverser/explorer_test.go
@@ -2908,94 +2908,77 @@ func getFakeModulesProvider() ModulesProvider {
 }
 
 func Test_Explorer_GetClass_QueryVector(t *testing.T) {
-	params := dto.GetParams{
-		ClassName: "BestClass",
-		NearVector: &searchparams.NearVector{
-			Vectors: []models.Vector{[]float32{0.1, 0.2, 0.3}},
-		},
-		Pagination: &filters.Pagination{Limit: 100},
-		AdditionalProperties: additional.Properties{
-			QueryVector: true,
-		},
+	type testCase struct {
+		name         string
+		queryVector  bool
+		expectVector interface{}
 	}
 
-	searchResults := []search.Result{
+	tests := []testCase{
 		{
-			ID: "id1",
-			Schema: map[string]interface{}{
-				"name": "Foo",
-			},
-			Dist: 0.5,
+			name:         "queryVector is returned in _additional when requested",
+			queryVector:  true,
+			expectVector: []float32{0.1, 0.2, 0.3},
 		},
-	}
-
-	search := &fakeVectorSearcher{}
-	metrics := &fakeMetrics{}
-	log, _ := test.NewNullLogger()
-	explorer := NewExplorer(search, log, getFakeModulesProvider(), metrics, defaultConfig)
-	explorer.SetSchemaGetter(&fakeSchemaGetter{
-		schema: schema.Schema{Objects: &models.Schema{Classes: []*models.Class{
-			{Class: "BestClass"},
-		}}},
-	})
-	expectedParamsToSearch := params
-	search.
-		On("VectorSearch", expectedParamsToSearch, []models.Vector{[]float32{0.1, 0.2, 0.3}}).
-		Return(searchResults, nil)
-
-	metrics.On("AddUsageDimensions", "BestClass", "get_graphql", "nearVector", 0)
-	res, err := explorer.GetClass(context.Background(), params)
-
-	t.Run("queryVector is returned in _additional", func(t *testing.T) {
-		require.Nil(t, err)
-		require.Len(t, res, 1)
-		result := res[0].(map[string]interface{})
-		additional := result["_additional"].(map[string]interface{})
-		assert.Equal(t, []float32{0.1, 0.2, 0.3}, additional["queryVector"])
-	})
-}
-
-func Test_Explorer_GetClass_QueryVector_NotRequested(t *testing.T) {
-	params := dto.GetParams{
-		ClassName: "BestClass",
-		NearVector: &searchparams.NearVector{
-			Vectors: []models.Vector{[]float32{0.1, 0.2, 0.3}},
-		},
-		Pagination: &filters.Pagination{Limit: 100},
-	}
-
-	searchResults := []search.Result{
 		{
-			ID: "id1",
-			Schema: map[string]interface{}{
-				"name": "Foo",
-			},
-			Dist: 0.5,
+			name:         "queryVector is not in _additional when not requested",
+			queryVector:  false,
+			expectVector: nil,
 		},
 	}
 
-	search := &fakeVectorSearcher{}
-	metrics := &fakeMetrics{}
-	log, _ := test.NewNullLogger()
-	explorer := NewExplorer(search, log, getFakeModulesProvider(), metrics, defaultConfig)
-	explorer.SetSchemaGetter(&fakeSchemaGetter{
-		schema: schema.Schema{Objects: &models.Schema{Classes: []*models.Class{
-			{Class: "BestClass"},
-		}}},
-	})
-	expectedParamsToSearch := params
-	search.
-		On("VectorSearch", expectedParamsToSearch, []models.Vector{[]float32{0.1, 0.2, 0.3}}).
-		Return(searchResults, nil)
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			params := dto.GetParams{
+				ClassName: "BestClass",
+				NearVector: &searchparams.NearVector{
+					Vectors: []models.Vector{[]float32{0.1, 0.2, 0.3}},
+				},
+				Pagination: &filters.Pagination{Limit: 100},
+			}
+			if tc.queryVector {
+				params.AdditionalProperties = additional.Properties{QueryVector: true}
+			}
 
-	metrics.On("AddUsageDimensions", "BestClass", "get_graphql", "nearVector", 0)
-	res, err := explorer.GetClass(context.Background(), params)
+			searchResults := []search.Result{
+				{
+					ID: "id1",
+					Schema: map[string]interface{}{
+						"name": "Foo",
+					},
+					Dist: 0.5,
+				},
+			}
 
-	t.Run("queryVector is not in _additional when not requested", func(t *testing.T) {
-		require.Nil(t, err)
-		require.Len(t, res, 1)
-		result := res[0].(map[string]interface{})
-		_, hasAdditional := result["_additional"]
-		assert.False(t, hasAdditional, "_additional should not be present when only queryVector could be added")
-	})
+			search := &fakeVectorSearcher{}
+			metrics := &fakeMetrics{}
+			log, _ := test.NewNullLogger()
+			explorer := NewExplorer(search, log, getFakeModulesProvider(), metrics, defaultConfig)
+			explorer.SetSchemaGetter(&fakeSchemaGetter{
+				schema: schema.Schema{Objects: &models.Schema{Classes: []*models.Class{
+					{Class: "BestClass"},
+				}}},
+			})
+
+			expectedParamsToSearch := params
+			search.
+				On("VectorSearch", expectedParamsToSearch, []models.Vector{[]float32{0.1, 0.2, 0.3}}).
+				Return(searchResults, nil)
+
+			metrics.On("AddUsageDimensions", "BestClass", "get_graphql", "nearVector", 0)
+			res, err := explorer.GetClass(context.Background(), params)
+
+			require.Nil(t, err)
+			require.Len(t, res, 1)
+			result := res[0].(map[string]interface{})
+
+			if tc.queryVector {
+				additional := result["_additional"].(map[string]interface{})
+				assert.Equal(t, []float32{0.1, 0.2, 0.3}, additional["queryVector"])
+			} else {
+				_, hasAdditional := result["_additional"]
+				assert.False(t, hasAdditional, "_additional should not be present when only queryVector could be added")
+			}
+		})
+	}
 }
