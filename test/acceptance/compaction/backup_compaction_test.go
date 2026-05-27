0a73922858aff6ffb59f005879bb93d3c10e8c88
Tommy Smith <tommy@weaviate.io>
2026-05-27 09:14:53 +0100

    Reduce amount of data being backed-up to speed up test in ci

--- test/acceptance/compaction/backup_compaction_test.go
+++ test/acceptance/compaction/backup_compaction_test.go
@@ -102,8 +102,8 @@ func TestBackup_CompactionRunsDuringBackup(t *testing.T) {
 	})
 	defer helper.DeleteClass(t, backupCollection)
 
-	// 2. Import initial data — 1000 batches to make the backup take long enough to observe compactions during its execution
-	for i := 0; i < 1000; i++ {
+	// 2. Import initial data — 100 batches of 200 objects to make the backup take long enough to observe compactions during its execution
+	for i := 0; i < 100; i++ {
 		importBatchForClass(t, backupCollection)
 	}
 
