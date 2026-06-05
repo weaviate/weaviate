a88e7a686c1263e927542d6a1eb53bf89d53a476
Dirk Kulawiak <dirk@weaviate.io>
2026-06-05 08:51:02 +0200

    More test speedups by reusing containers (#11619)
    * Share testcontainer for snapshot tests
    
    * Share testcontainer images for replication tests
    
    * Run async replication tests with same container
    
    * Convert more replication test to use a single node
    
    * And more replication tests reuse

--- test/acceptance/replication/read_repair/read_repair_deleteonconflict_test.go
+++ test/acceptance/replication/read_repair/read_repair_deleteonconflict_test.go
@@ -22,7 +22,6 @@ import (
 	"github.com/weaviate/weaviate/cluster/router/types"
 	"github.com/weaviate/weaviate/entities/models"
 	"github.com/weaviate/weaviate/test/acceptance/replication/common"
-	"github.com/weaviate/weaviate/test/docker"
 	"github.com/weaviate/weaviate/test/helper"
 	"github.com/weaviate/weaviate/test/helper/sample-schema/articles"
 )
@@ -34,16 +33,7 @@ func (suite *ReplicationTestSuite) TestReadRepairDeleteOnConflict() {
 	ctx, cancel := context.WithTimeout(mainCtx, 15*time.Minute)
 	defer cancel()
 
-	compose, err := docker.New().
-		With3NodeCluster().
-		WithText2VecContextionary().
-		Start(ctx)
-	require.Nil(t, err)
-	defer func() {
-		if err := compose.Terminate(ctx); err != nil {
-			t.Fatalf("failed to terminate test containers: %s", err.Error())
-		}
-	}()
+	compose := suite.compose
 
 	helper.SetupClient(compose.GetWeaviate().URI())
 	paragraphClass := articles.ParagraphsClass()
