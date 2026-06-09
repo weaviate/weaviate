b43d015f48ef3d1fc3d21864fe80c87caae96360
Marcin Antas <antas.marcin@gmail.com>
2026-06-09 16:43:01 +0200

    prepare release v1.37.8

--- adapters/handlers/rest/embedded_spec.go
+++ adapters/handlers/rest/embedded_spec.go
@@ -48,7 +48,7 @@ func init() {
       "url": "https://github.com/weaviate",
       "email": "hello@weaviate.io"
     },
-    "version": "1.37.7"
+    "version": "1.37.8"
   },
   "basePath": "/v1",
   "paths": {
@@ -10239,7 +10239,7 @@ func init() {
       "url": "https://github.com/weaviate",
       "email": "hello@weaviate.io"
     },
-    "version": "1.37.7"
+    "version": "1.37.8"
   },
   "basePath": "/v1",
   "paths": {
