39c285aa44f23ac671b94e1dfff41b6e62dbaeda
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2026-06-18 17:00:26 +0400

    fix: route multivector queue deletes through DeleteMulti

---
+++ adapters/repos/db/vector_index_queue_unit_test.go
@@ -0,0 +1,75 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/weaviate/weaviate/adapters/repos/db/vector/noop"
+)
+
+func TestVectorIndexQueueDeleteUsesDeleteMultiForSyncMultivectorIndex(t *testing.T) {
+	index := newRecordingMultiVectorIndex()
+	queue := &VectorIndexQueue{vectorIndex: index}
+
+	err := queue.Delete(10, 11)
+
+	require.NoError(t, err)
+	require.Empty(t, index.deleteCalls)
+	require.Equal(t, [][]uint64{{10, 11}}, index.deleteMultiCalls)
+}
+
+func TestVectorIndexQueueMultiDeleteTaskUsesDeleteMulti(t *testing.T) {
+	index := newRecordingMultiVectorIndex()
+	task := &Task[[][]float32]{
+		op:  vectorIndexQueueMultiDeleteOp,
+		id:  12,
+		idx: index,
+	}
+
+	err := task.Execute(context.Background())
+
+	require.NoError(t, err)
+	require.Empty(t, index.deleteCalls)
+	require.Equal(t, [][]uint64{{12}}, index.deleteMultiCalls)
+}
+
+func TestVectorIndexQueueMultiDeleteTaskGroupUsesDeleteMulti(t *testing.T) {
+	index := newRecordingMultiVectorIndex()
+	taskGroup := &TaskGroup[[][]float32]{
+		op:  vectorIndexQueueMultiDeleteOp,
+		ids: []uint64{13, 14},
+		idx: index,
+	}
+
+	err := taskGroup.Execute(context.Background())
+
+	require.NoError(t, err)
+	require.Empty(t, index.deleteCalls)
+	require.Equal(t, [][]uint64{{13, 14}}, index.deleteMultiCalls)
+}
+
+type recordingMultiVectorIndex struct {
+	*noop.Index
+
+	deleteCalls      [][]uint64
+	deleteMultiCalls [][]uint64
+}
+
+func newRecordingMultiVectorIndex() *recordingMultiVectorIndex {
+	return &recordingMultiVectorIndex{Index: noop.NewIndex()}
+}
+
+func (r *recordingMultiVectorIndex) Delete(ids ...uint64) error {
+	r.deleteCalls = append(r.deleteCalls, append([]uint64(nil), ids...))
+	return nil
+}
+
+func (r *recordingMultiVectorIndex) DeleteMulti(ids ...uint64) error {
+	r.deleteMultiCalls = append(r.deleteMultiCalls, append([]uint64(nil), ids...))
+	return nil
+}
+
+func (r *recordingMultiVectorIndex) Multivector() bool {
+	return true
+}
