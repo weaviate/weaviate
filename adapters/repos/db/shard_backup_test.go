c8987aff3263a3776ccafabc8ff0400a5f4f0707
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2026-07-01 14:00:10 +0400

    Add header to shard backup test

--- adapters/repos/db/shard_backup_test.go
+++ adapters/repos/db/shard_backup_test.go
@@ -1,5 +1,16 @@
 //go:build !integrationTest
 
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2026 Weaviate B.V. All rights reserved.
+//
+//  CONTACT: hello@weaviate.io
+//
+
 package db
 
 import (
