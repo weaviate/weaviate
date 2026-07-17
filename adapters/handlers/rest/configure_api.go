01ef917c3452e9bc565eb7f0fd9abe6d113c0ac5
Mohamed Badawi <contact@m-mooga.com>
2026-07-17 15:21:27 +0200

    fix(memberlist): don't fail startup when a single node can't join (self-heal) (#12243)
    * fix(memberlist): don't fail startup when a single node can't join (self-heal)

--- adapters/handlers/rest/configure_api.go
+++ adapters/handlers/rest/configure_api.go
@@ -1163,6 +1163,7 @@ func startupRoutine(ctx, serverShutdownCtx context.Context, options *swag.Comman
 	}
 
 	serverConfig.Config.Cluster.RaftBootstrapExpect = serverConfig.Config.Raft.BootstrapExpect
+	serverConfig.Config.Cluster.RaftBootstrapTimeout = serverConfig.Config.Raft.BootstrapTimeout
 	clusterState, err := cluster.Init(serverConfig.Config.Cluster, serverConfig.Config.Raft.TimeoutsMultiplier.Get(), dataPath, nonStorageNodes, logger)
 	if err != nil {
 		logger.WithField("action", "startup").WithError(err).
