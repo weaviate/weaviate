a2e9520eef5f3fb2d56a7405cc41af69e834bc2b
Tommy Smith <tommy@weaviate.io>
2026-07-22 15:46:53 +0100

    Fix broken test

--- test/acceptance/schema/get_schema_without_client_test.go
+++ test/acceptance/schema/get_schema_without_client_test.go
@@ -103,6 +103,7 @@ func testGetSchemaWithoutClient(t *testing.T) {
 					"factor":           float64(1),
 					"deletionStrategy": "TimeBasedResolution",
 					"raftEnabled":      false,
+					"asyncEnabled":     false,
 				},
 				"vectorizer": "text2vec-contextionary", // global default from env var, see docker-compose-test.yml
 				"invertedIndexConfig": map[string]interface{}{
