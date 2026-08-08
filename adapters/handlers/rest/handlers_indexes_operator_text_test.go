6657a38c539af171a523ffe8e208edde7c18a1f0
etiennedi <etienne@dilocker.de>
2026-08-08 16:26:43 +0200

    Revert "test: restore lost REST and probe-route assertions"
    This reverts commit 4726d71a0a0d5ec755274889b43f1946fff1cb0b.

--- adapters/handlers/rest/handlers_indexes_operator_text_test.go
+++ adapters/handlers/rest/handlers_indexes_operator_text_test.go
@@ -39,10 +39,6 @@ func TestPastCancellationRefusalsDoNotPromiseThatPollingEndsIt(t *testing.T) {
 			name:      "the task names a collection",
 			responder: reindexCancelPastCancellationPoint(&models.Principal{Username: "u1"}),
 		},
-		{
-			name:      "the task names no collection",
-			responder: reindexCancelUnattributablePastCancellationPoint(&models.Principal{Username: "u1"}),
-		},
 	}
 
 	for _, tc := range tests {
