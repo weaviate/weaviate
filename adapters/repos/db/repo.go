b7a5a675085597fbdf2e5e3de791df5fe6093d98
Dirk Kulawiak <dirk@semi.technology>
2026-08-11 14:11:28 +0200

    Add optional know to disable background loading of lazy shards

--- adapters/repos/db/repo.go
+++ adapters/repos/db/repo.go
@@ -422,6 +422,7 @@ type Config struct {
 	EnableLazyLoadShards                *bool
 	LazyLoadShardCountThreshold         int
 	LazyLoadShardSizeThresholdGB        float64
+	LazyLoadShardWarmupDisabled         bool
 	ForceFullReplicasSearch             bool
 	TransferInactivityTimeout           time.Duration
 	HaltForTransferTimeout              time.Duration
