weaviate/weaviate

commit 2af7c29d31f20c5ee5fc4d4d149c49f6cdc7de4c

perf: pre-compute avg proplength (#10596)
* perf: pre-compute avg proplength

* fix: update average property length calculation during WAL recovery

* refactor: remove default average property length variable

* test: add proplength to test init

* refactor: define err at top

* fix: sum prop lengths

* refactor: unneeded err

* refactor: do a single check with Set

* fix: properly lock memtables

adapters/repos/db/lsmkv/memtable.go
@@ -65,7 +65,7 @@ type memtable interface {
 
 	ReadOnlyTombstones() (*sroar.Bitmap, error)
 	SetTombstone(docId uint64) error
-	GetPropLengths() (uint64, uint64, error)
+	GetPropLengths() (uint64, uint64)
 
 	newCursor() innerCursorReplace
 	newBlockingCursor() (innerCursorReplace, func())
@@ -142,6 +142,11 @@ type Memtable struct {
 	// the memtable. This prevents the memtable from being flushed while writers
 	// are active, ensuring data consistency during concurrent operations.
 	writerCount atomic.Int64
+
+	// Keep track of the current prop length count and sum in the memtable,
+	// so that we don't have to compute it from scratch for search, flush and compaction.
+	currPropLengthCount, currPropLengthSum uint64
+	propLengthExists                       *sroar.Bitmap
 }
 
 func newMemtable(path string, strategy string, secondaryIndices uint16,
@@ -183,6 +188,7 @@ func newMemtable(path string, strategy string, secondaryIndices uint16,
 
 	if m.strategy == StrategyInverted {
 		m.tombstones = sroar.NewBitmap()
+		m.propLengthExists = sroar.NewBitmap()
 	}
 
 	return m, nil
@@ -499,6 +505,15 @@ func (m *Memtable) appendMapSorted(key []byte, pair MapPair) error {
 	m.metrics.observeSize(m.size)
 	m.updateDirtyAt()
 
+	if m.strategy == StrategyInverted && !pair.Tombstone {
+		docID := binary.LittleEndian.Uint64(pair.Key)
+		fieldLength := math.Float32frombits(binary.LittleEndian.Uint32(pair.Value[4:]))
+		if m.propLengthExists.Set(docID) {
+			m.currPropLengthSum += uint64(fieldLength)
+			m.currPropLengthCount++
+		}
+	}
+
 	return nil
 }
 
@@ -584,34 +599,13 @@ func (m *Memtable) SetTombstone(docId uint64) error {
 	defer m.Unlock()
 
 	m.tombstones.Set(docId)
+	m.propLengthExists.Remove(docId)
 
 	return nil
 }
 
-func (m *Memtable) GetPropLengths() (uint64, uint64, error) {
-	m.RLock()
-	flatA := m.keyMap.flattenInOrder()
-	m.RUnlock()
-
-	docIdsLengths := make(map[uint64]uint32)
-	propLengthSum := uint64(0)
-	propLengthCount := uint64(0)
-
-	for _, mapNode := range flatA {
-		for j := range mapNode.values {
-			docId := binary.BigEndian.Uint64(mapNode.values[j].Key)
-			if !mapNode.values[j].Tombstone {
-				fieldLength := math.Float32frombits(binary.LittleEndian.Uint32(mapNode.values[j].Value[4:]))
-				if _, ok := docIdsLengths[docId]; !ok {
-					propLengthSum += uint64(fieldLength)
-					propLengthCount++
-				}
-				docIdsLengths[docId] = uint32(fieldLength)
-			}
-		}
-	}
-
-	return propLengthSum, propLengthCount, nil
+func (m *Memtable) GetPropLengths() (uint64, uint64) {
+	return m.currPropLengthSum, m.currPropLengthCount
 }
 
 func (m *Memtable) incWriterCount() {

adapters/repos/db/lsmkv/bucket.go
@@ -1441,9 +1441,26 @@ func (b *Bucket) Shutdown(ctx context.Context) (err error) {
 
 	b.flushLock.Lock()
 	if b.active.getStrategy() == StrategyInverted {
-		avgPropLength, propLengthCount := b.disk.GetAveragePropertyLength()
+
+		// we need to calculate it outside of b.GetAveragePropertyLength()
+		// to avoid locking flushLock twice
+		propLengthSum, propLengthCount := b.getMemtablePropertyLengths()
+
+		// weighted average of m.averagePropLength and the average of the current flush
+		// averaged by propLengthCount and m.propLengthCount
+		segmentAveragePropLength, segmentPropCount := b.disk.GetAveragePropertyLength()
+
+		if segmentPropCount != 0 {
+			propLengthSum += uint64(segmentAveragePropLength * float64(segmentPropCount))
+			propLengthCount += segmentPropCount
+		}
+		avgPropLength := float64(0)
+		if propLengthCount != 0 {
+			avgPropLength = float64(propLengthSum) / float64(propLengthCount)
+		}
 		b.active.setAveragePropertyLength(avgPropLength, propLengthCount)
 	}
+
 	if b.shouldReuseWAL() {
 		if err := b.active.flushWAL(); err != nil {
 			b.flushLock.Unlock()
@@ -1805,6 +1822,15 @@ func (b *Bucket) atomicallyAddDiskSegmentAndRemoveFlushing(seg Segment) error {
 		if b.keepSegmentsInMemory {
 			b.disk.roaringSetRangeSegmentInMemory.MergeMemtableEventually(flushing.extractRoaringSetRange())
 		}
+	case StrategyInverted:
+		// update property length only on flush
+		// we don't need to do it on compactions,
+		// as it is not currently tracking deletions
+		avg, count := seg.getInvertedData().avgPropertyLengthsAvg, seg.getInvertedData().avgPropertyLengthsCount
+		if count > 0 {
+			b.disk.averagePropSum += uint64(avg * float64(count))
+			b.disk.averagePropCount += count
+		}
 	}
 
 	return nil
@@ -1949,12 +1975,9 @@ func (b *Bucket) createDiskTermFromCV(ctx context.Context, view BucketConsistent
 			}
 		}
 	}()
+	var err error
 
-	averagePropLength, err := b.GetAveragePropertyLength()
-	if err != nil {
-		view.ReleaseView()
-		return nil, nil, func() {}, err
-	}
+	averagePropLength, _ := b.GetAveragePropertyLength()
 
 	// Synchronization was reworked as part of
 	// https://github.com/weaviate/weaviate/pull/9104.
@@ -1996,7 +2019,6 @@ func (b *Bucket) createDiskTermFromCV(ctx context.Context, view BucketConsistent
 			}
 			n += n2
 
-			var err error
 			activeTombstones, err = view.Active.ReadOnlyTombstones()
 			if err != nil {
 				view.ReleaseView()
@@ -2137,30 +2159,35 @@ func addDataToTerm(mem []MapPair, filterDocIds helpers.AllowList, term *SegmentB
 	return n, nil
 }
 
-func (b *Bucket) GetAveragePropertyLength() (float64, error) {
-	if b.strategy != StrategyInverted {
-		return 0, fmt.Errorf("active memtable is not inverted")
-	}
-
-	var err error
+func (b *Bucket) getMemtablePropertyLengths() (uint64, uint64) {
+	// fix potential race with buckets flushing
 	propLengthCount := uint64(0)
 	propLengthSum := uint64(0)
+
 	if b.flushing != nil {
-		propLengthSum, propLengthCount, err = b.flushing.GetPropLengths()
-		if err != nil {
-			return 0, err
-		}
+		propLengthSum, propLengthCount = b.flushing.GetPropLengths()
 	}
+
 	// if the active memtable is inverted, we need to get the average property
 	if b.active != nil {
-		propLengthSum2, propLengthCount2, err := b.active.GetPropLengths()
-		if err != nil {
-			return 0, err
-		}
+		propLengthSum2, propLengthCount2 := b.active.GetPropLengths()
 		propLengthCount += propLengthCount2
 		propLengthSum += propLengthSum2
 	}
+	if propLengthCount == 0 {
+		return 0, 0
+	}
+	return propLengthSum, propLengthCount
+}
 
+func (b *Bucket) GetAveragePropertyLength() (float64, uint64) {
+	if b.strategy != StrategyInverted {
+		return 0, 0
+	}
+
+	b.flushLock.RLock()
+	propLengthSum, propLengthCount := b.getMemtablePropertyLengths()
+	b.flushLock.RUnlock()
 	// weighted average of m.averagePropLength and the average of the current flush
 	// averaged by propLengthCount and m.propLengthCount
 	segmentAveragePropLength, segmentPropCount := b.disk.GetAveragePropertyLength()
@@ -2170,9 +2197,9 @@ func (b *Bucket) GetAveragePropertyLength() (float64, error) {
 		propLengthCount += segmentPropCount
 	}
 	if propLengthCount == 0 {
-		return 0, nil
+		return 0, 0
 	}
-	return float64(propLengthSum) / float64(propLengthCount), nil
+	return float64(propLengthSum) / float64(propLengthCount), propLengthCount
 }
 
 func DetermineUnloadedBucketStrategy(bucketPath string) (string, error) {

adapters/repos/db/lsmkv/bucket_recover_from_wal.go
@@ -127,7 +127,7 @@ func (b *Bucket) mayRecoverFromCommitLogs(ctx context.Context, sg *SegmentGroup,
 			}
 
 			if mt.strategy == StrategyInverted {
-				mt.averagePropLength, _ = sg.GetAveragePropertyLength()
+				mt.averagePropLength, mt.propLengthCount = sg.GetAveragePropertyLength()
 			}
 
 			// immediately flush the .wal file if there have been any damages during recovery. This means that the file is

adapters/repos/db/lsmkv/bucket_test.go
@@ -1989,11 +1989,12 @@ func newTestMemtableInverted(initialData map[string][]MapPair) *testMemtable {
 	}
 
 	m := &Memtable{
-		strategy:   StrategyInverted,
-		keyMap:     &binarySearchTreeMap{},
-		commitlog:  newDummyCommitLogger(),
-		metrics:    metrics,
-		tombstones: sroar.NewBitmap(),
+		strategy:         StrategyInverted,
+		keyMap:           &binarySearchTreeMap{},
+		commitlog:        newDummyCommitLogger(),
+		metrics:          metrics,
+		tombstones:       sroar.NewBitmap(),
+		propLengthExists: sroar.NewBitmap(),
 	}
 
 	for k, v := range initialData {

adapters/repos/db/lsmkv/compactor_proplength_test.go
@@ -50,7 +50,7 @@ func TestInvertedNaNPropLength(t *testing.T) {
 	err := bucket.FlushAndSwitch()
 	require.Nil(t, err)
 	avg, sum := bucket.disk.GetAveragePropertyLength()
-	bavg, err := bucket.GetAveragePropertyLength()
+	bavg, _ := bucket.GetAveragePropertyLength()
 	require.Nil(t, err)
 	require.Equal(t, 1.0, avg)
 	require.Equal(t, 1.0, bavg)
@@ -65,8 +65,7 @@ func TestInvertedNaNPropLength(t *testing.T) {
 	err = bucket.FlushAndSwitch()
 	require.Nil(t, err)
 	avg, sum = bucket.disk.GetAveragePropertyLength()
-	bavg, err = bucket.GetAveragePropertyLength()
-	require.Nil(t, err)
+	bavg, _ = bucket.GetAveragePropertyLength()
 	require.Equal(t, 1.0, avg)
 	require.Equal(t, 1.0, bavg)
 	require.Equal(t, uint64(size), sum)
@@ -80,8 +79,7 @@ func TestInvertedNaNPropLength(t *testing.T) {
 	err = bucket.FlushAndSwitch()
 	require.Nil(t, err)
 	avg, sum = bucket.disk.GetAveragePropertyLength()
-	bavg, err = bucket.GetAveragePropertyLength()
-	require.Nil(t, err)
+	bavg, _ = bucket.GetAveragePropertyLength()
 	require.Equal(t, 1.0, avg)
 	require.Equal(t, 1.0, bavg)
 	require.Equal(t, uint64(size), sum)
@@ -99,8 +97,7 @@ func TestInvertedNaNPropLength(t *testing.T) {
 	})
 
 	avg, sum = bucket.disk.GetAveragePropertyLength()
-	bavg, err = bucket.GetAveragePropertyLength()
-	require.Nil(t, err)
+	bavg, _ = bucket.GetAveragePropertyLength()
 	require.Equal(t, 1.0, avg)
 	require.Equal(t, 1.0, bavg)
 	require.Equal(t, uint64(size), sum)
@@ -117,8 +114,7 @@ func TestInvertedNaNPropLength(t *testing.T) {
 	}
 
 	avg, sum = bucket.disk.GetAveragePropertyLength()
-	bavg, err = bucket.GetAveragePropertyLength()
-	require.Nil(t, err)
+	bavg, _ = bucket.GetAveragePropertyLength()
 	require.Equal(t, 1.0, avg)
 	require.Equal(t, 1.0, bavg)
 	require.Equal(t, uint64(size), sum)
@@ -136,8 +132,7 @@ func TestInvertedNaNPropLength(t *testing.T) {
 	})
 
 	avg, sum = bucket.disk.GetAveragePropertyLength()
-	bavg, err = bucket.GetAveragePropertyLength()
-	require.Nil(t, err)
+	bavg, _ = bucket.GetAveragePropertyLength()
 	require.Equal(t, 1.0, avg)
 	require.Equal(t, 1.0, bavg)
 	require.Equal(t, uint64(size), sum)

adapters/repos/db/lsmkv/memtable_flush_inverted.go
@@ -81,6 +81,10 @@ func (m *Memtable) flushDataInverted(f *segmentindex.SegmentFile, ogF *diskio.Me
 		m.propLengthCount += propLengthCount
 	}
 
+	if math.IsNaN(m.averagePropLength) {
+		m.averagePropLength = 0
+	}
+
 	tombstoneBuffer := make([]byte, 0)
 	if !tombstones.IsEmpty() {
 		tombstoneBuffer = tombstones.ToBuffer()

adapters/repos/db/lsmkv/segment_group.go
@@ -100,6 +100,12 @@ type SegmentGroup struct {
 	bm25config                     *schema.BM25Config
 	writeSegmentInfoIntoFileName   bool
 	writeMetadata                  bool
+
+	// Store the average property length for segments in this sg,
+	// to be used for BM25 scoring.
+	// This avoids recalculating the average property length for each segment during scoring.
+	averagePropSum   uint64
+	averagePropCount uint64
 }
 
 type sgConfig struct {
@@ -450,6 +456,15 @@ func newSegmentGroup(ctx context.Context, logger logrus.FieldLogger, metrics *Me
 
 	switch sg.strategy {
 	case StrategyInverted:
+		if len(sg.segments) == 0 {
+			break
+		}
+		avg, count := sg.segments[len(sg.segments)-1].getInvertedData().avgPropertyLengthsAvg, sg.segments[len(sg.segments)-1].getInvertedData().avgPropertyLengthsCount
+
+		if count > 0 {
+			sg.averagePropSum = uint64(avg * float64(count))
+			sg.averagePropCount = count
+		}
 		// start with last but one segment, as the last one doesn't need tombstones for now
 		for i := len(sg.segments) - 2; i >= 0; i-- {
 			// avoid crashing if segment has no tombstones
@@ -459,6 +474,13 @@ func newSegmentGroup(ctx context.Context, logger logrus.FieldLogger, metrics *Me
 			}
 			if _, err := sg.segments[i].MergeTombstones(tombstonesNext); err != nil {
 				return nil, fmt.Errorf("init segment %s: merge tombstones %w", sg.segments[i].getPath(), err)
+			}
+
+			avg, count := sg.segments[i].getInvertedData().avgPropertyLengthsAvg, sg.segments[i].getInvertedData().avgPropertyLengthsCount
+
+			if count > 0 {
+				sg.averagePropSum += uint64(avg * float64(count))
+				sg.averagePropCount += count
 			}
 		}
 
@@ -935,28 +957,8 @@ func (sg *SegmentGroup) Len() int {
 }
 
 func (sg *SegmentGroup) GetAveragePropertyLength() (float64, uint64) {
-	segments, release := sg.getConsistentViewOfSegments()
-	defer release()
-
-	if len(segments) == 0 {
+	if sg.averagePropCount == 0 {
 		return 0, 0
 	}
-
-	totalDocCount := uint64(0)
-	for _, segment := range segments {
-		invertedData := segment.getInvertedData()
-		totalDocCount += invertedData.avgPropertyLengthsCount
-	}
-
-	if totalDocCount == 0 {
-		return defaultAveragePropLength, 0
-	}
-
-	weightedAverage := 0.0
-	for _, segment := range segments {
-		invertedData := segment.getInvertedData()
-		weightedAverage += float64(invertedData.avgPropertyLengthsCount) / float64(totalDocCount) * invertedData.avgPropertyLengthsAvg
-	}
-
-	return weightedAverage, totalDocCount
+	return float64(sg.averagePropSum) / float64(sg.averagePropCount), sg.averagePropCount
 }

adapters/repos/db/lsmkv/segment_serialization_inverted.go
@@ -23,8 +23,6 @@ import (
 	"github.com/weaviate/weaviate/adapters/repos/db/lsmkv/varenc"
 )
 
-var defaultAveragePropLength = float64(40.0)
-
 func extractTombstones(nodes []MapPair) (*sroar.Bitmap, []MapPair) {
 	out := sroar.NewBitmap()
 	values := make([]MapPair, 0, len(nodes))

commit 21761ecf9f344363a72702b830fafd4c65974043

fix(lsmkv): decode inverted docID as BigEndian in appendMapSorted's prop-length tracking
appendMapSorted decoded a MapPair's docID key with LittleEndian while every
other reader of that key (SetTombstone via MapDeleteKey, WAL replay,
on-disk encoding) uses BigEndian, the encoding StrategyInverted requires
for byte-wise-sortable keys. This was a regression from #10596: the
propLengthExists dedup gate this decode feeds was originally BigEndian
(3c0f85ab5e) and got flipped when the prop-length pre-computation was
reintroduced.

propLengthExists.Set/Remove address different bitmap positions for the
same doc as a result, so SetTombstone's Remove can never re-arm the dedup
gate Set uses. A doc re-added after being tombstoned within the same
still-active memtable (e.g. an object update) has its new prop length
silently dropped from GetPropLengths' running sum/count instead of being
recounted, skewing the live contribution to BM25's average property
length until the memtable flushes.

adapters/repos/db/lsmkv/memtable.go
@@ -595,7 +595,13 @@ func (m *Memtable) appendMapSorted(key []byte, pair MapPair) error {
 	m.updateDirtyAt()
 
 	if m.strategy == StrategyInverted && !pair.Tombstone {
-		docID := binary.LittleEndian.Uint64(pair.Key)
+		// Must match SetTombstone's decode (BigEndian): StrategyInverted keys are
+		// always BigEndian, required for byte-wise sortability. A mismatch here
+		// makes propLengthExists.Set/Remove address different bitmap positions for
+		// the same doc, so SetTombstone can't re-arm the dedup gate below — a doc
+		// re-added after being tombstoned in the same memtable (e.g. an update)
+		// has its new prop length silently dropped instead of recounted.
+		docID := binary.BigEndian.Uint64(pair.Key)
 		fieldLength := math.Float32frombits(binary.LittleEndian.Uint32(pair.Value[4:]))
 		// propLengthExists + currPropLength* are shared with SetTombstone, which no
 		// longer holds the tree lock; guard them with invMu (nested inside m.Lock).

adapters/repos/db/lsmkv/memtable_prop_length_docid_test.go
@@ -1,0 +1,56 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2026 Weaviate B.V. All rights reserved.
+//
+//  CONTACT: hello@weaviate.io
+//
+
+package lsmkv
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// TestAppendMapSortedPropLengthDocIDMatchesSetTombstone pins that
+// appendMapSorted and SetTombstone agree on how a MapPair.Key encodes a
+// docID for StrategyInverted. Every other docID key in the package (bucket.go
+// MapDeleteKey, commitlogger_parser_collection.go WAL replay,
+// segment_serialization_inverted.go on-disk encoding) uses BigEndian, since
+// StrategyInverted requires byte-wise-sortable keys.
+//
+// SetTombstone's propLengthExists.Remove(docId) exists to re-arm the
+// propLengthExists.Set(docId) dedup gate in appendMapSorted, so that a doc
+// re-indexed after being tombstoned within the same still-active memtable
+// (delete immediately followed by re-add, e.g. an object update) has its new
+// prop length counted again rather than silently dropped as a "duplicate."
+// If appendMapSorted decodes the docID with a different endianness than
+// SetTombstone, Remove operates on a different bitmap position than the one
+// Set used, so the re-arm never happens: the update's new prop length is
+// dropped from GetPropLengths' running sum/count instead of replacing the
+// stale one.
+func TestAppendMapSortedPropLengthDocIDMatchesSetTombstone(t *testing.T) {
+	m := newTestMemtableInverted(nil)
+	rowKey := []byte("key1")
+	docID := uint64(42)
+
+	require.NoError(t, m.appendMapSorted(rowKey, NewMapPairFromDocIdAndTf(docID, 3, 7, false)))
+	sum, count := m.GetPropLengths()
+	require.Equal(t, uint64(7), sum)
+	require.Equal(t, uint64(1), count)
+
+	require.NoError(t, m.SetTombstone(docID))
+
+	// Re-add the same docID with a different prop length, simulating an
+	// update applied within the same live memtable.
+	require.NoError(t, m.appendMapSorted(rowKey, NewMapPairFromDocIdAndTf(docID, 3, 20, false)))
+
+	sum, count = m.GetPropLengths()
+	require.Equal(t, uint64(27), sum, "the re-add after tombstone must be counted, not silently dropped")
+	require.Equal(t, uint64(2), count, "the re-add after tombstone must be counted, not silently dropped")
+}