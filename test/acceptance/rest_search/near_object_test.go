10c825f036dae8e768f43a3a0d0d2a973ff895fd
Marcin Antas <antas.marcin@gmail.com>
2026-09-24 17:29:44 +0200

    test(acceptance): cut fast-group-5 test time by over a third

--- test/acceptance/rest_search/near_object_test.go
+++ test/acceptance/rest_search/near_object_test.go
@@ -17,7 +17,6 @@
 package rest_search
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 	"testing"
@@ -28,7 +27,6 @@ import (
 
 	"github.com/weaviate/weaviate/entities/models"
 	"github.com/weaviate/weaviate/entities/schema"
-	"github.com/weaviate/weaviate/test/docker"
 	"github.com/weaviate/weaviate/test/helper"
 )
 
@@ -57,19 +55,8 @@ func postNearObject(t *testing.T, collection string, body map[string]any) (int, 
 }
 
 func TestRESTSearchNearObject(t *testing.T) {
-	ctx := context.Background()
-	compose, err := docker.New().
-		WithWeaviate().
-		// the endpoint is experimental and off by default; enable it
-		WithWeaviateEnv("EXPERIMENTAL_REST_SEARCH_ENABLED", "true").
-		Start(ctx)
-	require.NoError(t, err)
-	defer func() {
-		require.NoError(t, compose.Terminate(ctx))
-	}()
-
 	defer helper.SetupClient(fmt.Sprintf("%s:%s", helper.ServerHost, helper.ServerPort))
-	helper.SetupClient(compose.GetWeaviate().URI())
+	helper.SetupClient(restSearchServerURI(t))
 
 	poemClass := &models.Class{
 		Class:      "Poem",
--- test/acceptance/usage_limits/tenant_cap_cluster_test.go
+++ test/acceptance/usage_limits/tenant_cap_cluster_test.go
@@ -56,6 +56,7 @@ func TestTenantCap_HardUnderClusterConcurrency(t *testing.T) {
 			t.Logf("failed to terminate cluster: %v", err)
 		}
 	}()
+	defer helper.ResetClient()
 
 	nodes := []string{
 		"http://" + compose.GetWeaviate().URI(),
